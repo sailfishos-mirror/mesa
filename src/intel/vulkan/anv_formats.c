0e1cc2216d0a06a54077f1faa67af1698ba08537
Nanley Chery <nanley.g.chery@intel.com>
2025-10-17 15:56:46 -0400

    anv: Disable multisampled host transfer support
    We don't actually handle this case. The next patch will limit the amount
    of tilings used when an image is created with
    VK_IMAGE_USAGE_HOST_TRANSFER_BIT_EXT. This prevents zink failures on DG2
    for various multisampled test cases. For example:
    
       arb_internalformat_query2-internalformat-size-checks -auto -fbo
    
    Reviewed-by: Rohan Garg <rohan.garg@intel.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38063>

--- src/intel/vulkan/anv_formats.c
+++ src/intel/vulkan/anv_formats.c
@@ -1774,6 +1774,7 @@ anv_get_image_format_properties(
                                 VK_FORMAT_FEATURE_2_DEPTH_STENCIL_ATTACHMENT_BIT)) &&
        !(info->flags & VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT) &&
        !(info->usage & VK_IMAGE_USAGE_STORAGE_BIT) &&
+       !(info->usage & VK_IMAGE_USAGE_HOST_TRANSFER_BIT_EXT) &&
        isl_format_supports_multisampling(devinfo, format->planes[0].isl_format)) {
       sampleCounts = isl_device_get_sample_counts(&physical_device->isl_dev);
    }
