3f3b7e4aa3e15b021ce18a5be3677e6042d0e3da
Emma Anholt <emma@anholt.net>
2026-01-06 12:59:25 -0800

    nir: Fix constant evaluation of non-32-bit bitfield_extract.
    Caught by nir_opt_algebraic_pattern_tests.
    
    Fixes: 226b0e28dbe9 ("nir: generalize bitfield insert/extract sizes")
    Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
    (cherry picked from commit f6008645f6e2bad4aeb7bdacfbddbf0217754b3c)
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39462>

--- src/compiler/nir/nir_opcodes.py
+++ src/compiler/nir/nir_opcodes.py
@@ -1168,7 +1168,7 @@ unsigned base = src0;
 int offset = src1, bits = src2;
 if (bits == 0) {
    dst = 0;
-} else if (bits < 0 || offset < 0 || offset + bits > 32) {
+} else if (bits < 0 || offset < 0 || offset + bits > bit_size) {
    dst = 0; /* undefined per the spec */
 } else {
    dst = (base >> offset) & ((1ull << bits) - 1);
@@ -1180,7 +1180,7 @@ int base = src0;
 int offset = src1, bits = src2;
 if (bits == 0) {
    dst = 0;
-} else if (offset < 0 || bits < 0 || offset + bits > 32) {
+} else if (offset < 0 || bits < 0 || offset + bits > bit_size) {
    dst = 0;
 } else {
    dst = (base << (32 - offset - bits)) >> (32 - bits); /* use sign-extending shift */
