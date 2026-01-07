eb37d6ceb749aa4b6c06db75bfa81a1f93cb4a26
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2026-01-07 17:24:19 +0100

    ac/perfcounter: fix computing number of 16-bit/32-bit SPM counters
    Determine them only when both are explicitly 0.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39199>

--- src/amd/common/ac_perfcounter.c
+++ src/amd/common/ac_perfcounter.c
@@ -755,10 +755,11 @@ bool ac_init_perfcounters(const struct radeon_info *info,
          const uint32_t rb_per_sa = num_rb_per_se / info->max_sa_per_se;
 
          /* Determine the number of 16-bit/32-bit SPM counters. */
-         if (!block->b->b->num_16bit_spm_counters)
+         if (!block->b->b->num_16bit_spm_counters &&
+             !block->b->b->num_32bit_spm_counters) {
             block->b->b->num_16bit_spm_counters = block->b->b->num_spm_counters * 4;
-         if (!block->b->b->num_32bit_spm_counters)
             block->b->b->num_32bit_spm_counters = block->b->b->num_spm_counters * 2;
+         }
 
          switch (block->b->b->gpu_block) {
          case CB:
