e943ba122d0ee9135a25a05918558c4bfc48f29a
Connor Abbott <cwabbott0@gmail.com>
2026-01-12 17:38:08 -0500

    ir3: Stop asserting tess levels are scalar
    Gallium gives us vectors, but guarantees that inner/outer levels are in
    separate locations still.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39328>

--- src/freedreno/ir3/ir3_nir_lower_tess.c
+++ src/freedreno/ir3/ir3_nir_lower_tess.c
@@ -571,7 +571,6 @@ lower_tess_ctrl_block(nir_block *block, nir_builder *b, struct state *state)
 
          gl_varying_slot location = nir_intrinsic_io_semantics(intr).location;
          if (is_tess_levels(location)) {
-            assert(intr->def.num_components == 1);
             address = load_tess_factor_base(b);
             offset = build_tessfactor_base(
                b, location, nir_intrinsic_component(intr), state);
@@ -602,8 +601,6 @@ lower_tess_ctrl_block(nir_block *block, nir_builder *b, struct state *state)
          if (is_tess_levels(location)) {
             uint32_t inner_levels, outer_levels, levels;
             tess_level_components(state, &inner_levels, &outer_levels);
-
-            assert(intr->src[0].ssa->num_components == 1);
 
             nir_if *nif = NULL;
             if (location != VARYING_SLOT_PRIMITIVE_ID) {
@@ -756,7 +753,6 @@ lower_tess_eval_block(nir_block *block, nir_builder *b, struct state *state)
 
          gl_varying_slot location = nir_intrinsic_io_semantics(intr).location;
          if (is_tess_levels(location)) {
-            assert(intr->def.num_components == 1);
             address = load_tess_factor_base(b);
             offset = build_tessfactor_base(
                b, location, nir_intrinsic_component(intr), state);
