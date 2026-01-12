bminor/mesa-mesa

commit acd00c07f68aea0f5ab4641f4749c8f49b8927b0

panvk: Initialize the disk cache earlier
We want to know whether or not we successfully initialized before
filling out physical device properties.

Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38284>

src/panfrost/vulkan/panvk_physical_device.c
@@ -136,8 +136,8 @@ get_drm_device_ids(struct panvk_physical_device *device,
 }
 
 static void
-get_cache_sha1(struct panvk_physical_device *device,
-               const struct panvk_instance *instance)
+init_shader_caches(struct panvk_physical_device *device,
+                   const struct panvk_instance *instance)
 {
    struct mesa_sha1 sha_ctx;
    _mesa_sha1_init(&sha_ctx);
@@ -153,12 +153,7 @@ get_cache_sha1(struct panvk_physical_device *device,
 
    STATIC_ASSERT(VK_UUID_SIZE <= SHA1_DIGEST_LENGTH);
    memcpy(device->cache_uuid, sha, VK_UUID_SIZE);
-}
 
-static void
-init_disk_cache(struct panvk_physical_device *device,
-                const struct panvk_instance *instance)
-{
 #ifdef ENABLE_SHADER_CACHE
    char renderer[17];
    ASSERTED int len = snprintf(renderer, sizeof(renderer), "panvk_0x%08x",
@@ -336,7 +331,7 @@ panvk_physical_device_init(struct panvk_physical_device *device,
    memset(device->name, 0, sizeof(device->name));
    sprintf(device->name, "%s", device->model->name);
 
-   get_cache_sha1(device, instance);
+   init_shader_caches(device, instance);
 
    result = get_core_masks(device, instance);
    if (result != VK_SUCCESS)
@@ -382,8 +377,6 @@ panvk_physical_device_init(struct panvk_physical_device *device,
       goto fail;
 
    device->vk.supported_sync_types = device->sync_types;
-
-   init_disk_cache(device, instance);
 
    result = panvk_wsi_init(device);
    if (result != VK_SUCCESS)

sailfishos-mirror/mesa

commit b41f4660458e90b357a09da81fa3d061062d1d42

panvk: fix to defer disk cache init after vk_physical_device_init
Instead of initializing the disk cache earlier, the vk_properties
filling can be deferred after disk cache init is done. Otherwise, the
created disk cache will be zero'ed out in vk_physical_device_init,
ending up with leaked alloc and disabled disk cache (though advertised).

Fixes: acd00c07f68 ("panvk: Initialize the disk cache earlier")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39248>

src/panfrost/vulkan/panvk_physical_device.c
@@ -434,8 +434,6 @@ panvk_physical_device_init(struct panvk_physical_device *device,
    memset(device->name, 0, sizeof(device->name));
    sprintf(device->name, "%s", device->model->name);
 
-   init_shader_caches(device, instance);
-
    result = get_core_masks(device, instance);
    if (result != VK_SUCCESS)
       goto fail;
@@ -466,22 +464,25 @@ panvk_physical_device_init(struct panvk_physical_device *device,
    panvk_arch_dispatch(arch, get_physical_device_features, instance,
                        device, &supported_features);
 
-   struct vk_properties properties;
-   panvk_arch_dispatch(arch, get_physical_device_properties, instance,
-                       device, &properties);
-
    struct vk_physical_device_dispatch_table dispatch_table;
    vk_physical_device_dispatch_table_from_entrypoints(
       &dispatch_table, &panvk_physical_device_entrypoints, true);
    vk_physical_device_dispatch_table_from_entrypoints(
       &dispatch_table, &wsi_physical_device_entrypoints, false);
 
-   result = vk_physical_device_init(&device->vk, &instance->vk,
-                                    &supported_extensions, &supported_features,
-                                    &properties, &dispatch_table);
+   result =
+      vk_physical_device_init(&device->vk, &instance->vk, &supported_extensions,
+                              &supported_features, NULL, &dispatch_table);
 
    if (result != VK_SUCCESS)
       goto fail;
+
+   /* initialize disk cache after vk_physical_device_init */
+   init_shader_caches(device, instance);
+
+   /* pipeline binary props rely on disk cache init state */
+   panvk_arch_dispatch(arch, get_physical_device_properties, instance, device,
+                       &device->vk.properties);
 
    device->vk.supported_sync_types = device->sync_types;
 