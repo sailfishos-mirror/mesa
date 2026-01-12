adfb1ddebfa1f9297db98bca85be765f55e0c952
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2026-01-12 16:10:07 +0000

    ac/perfcounter: use GFX11 definition for GFX11.5
    Pretty much everything seems similar.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39270>

--- src/amd/common/ac_perfcounter.c
+++ src/amd/common/ac_perfcounter.c
@@ -725,6 +725,7 @@ bool ac_init_perfcounters(const struct radeon_info *info,
       blocks = ac_gfx103_get_perfcounters(&num_blocks);
       break;
    case GFX11:
+   case GFX11_5:
       blocks = ac_gfx11_get_perfcounters(&num_blocks);
       break;
    case GFX12:
