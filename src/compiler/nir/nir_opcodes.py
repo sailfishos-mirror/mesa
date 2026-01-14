f82621bfdce63106895f73f748c909c14ce96ffd
Emma Anholt <emma@anholt.net>
2026-01-13 17:24:09 -0800

    nir/opcodes: Define the mul/mad_relaxed opcodes to return poison for OOB.
    For the "defined results for >24b", you want the non-relaxed opcodes.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39369>

--- src/compiler/nir/nir_opcodes.py
+++ src/compiler/nir/nir_opcodes.py
@@ -1542,10 +1542,23 @@ triop("umad24", tuint32, _2src_commutative,
 binop("umul24", tint32, _2src_commutative,
       "(((uint32_t)src0 << 8) >> 8) * (((uint32_t)src1 << 8) >> 8)")
 
-# relaxed versions of the above, which assume input is in the 24bit range (no clamping)
-binop("imul24_relaxed", tint32, _2src_commutative, "src0 * src1")
-triop("umad24_relaxed", tuint32, _2src_commutative, "src0 * src1 + src2")
-binop("umul24_relaxed", tuint32, _2src_commutative, "src0 * src1")
+# relaxed versions of the above, which assume input is in the 24bit range (no clamping).
+binop("imul24_relaxed", tint32, _2src_commutative, """
+dst = src0 * src1;
+if (util_mask_sign_extend(src0, 24) != src0 || util_mask_sign_extend(src1, 24) != src1)
+    poison = true;
+""")
+triop("umad24_relaxed", tuint32, _2src_commutative, """
+dst = src0 * src1 + src2;
+if ((src0 | src1) & ~BITFIELD_MASK(24))
+    poison = true;
+""")
+binop("umul24_relaxed", tuint32, _2src_commutative, """
+dst = src0 * src1;
+if ((src0 | src1) & ~BITFIELD_MASK(24))
+    poison = true;
+""")
+
 
 unop_convert("fisnormal", tbool1, tfloat, "isnormal(src0)")
 unop_convert("fisfinite", tbool1, tfloat, "isfinite(src0)")
