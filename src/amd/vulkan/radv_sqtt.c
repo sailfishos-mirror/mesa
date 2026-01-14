feed5fbc7187f0fa3d2499e527026b6f98b6c7ce
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2026-01-14 14:53:57 +0100

    radv/sqtt: delay VMID reservation at capture time
    If two devices/instances are created, the VMID reservation will just
    fail. It seems fine as long as it's reserved before SPM is used.
    
    Fixes: a7a4abc8d87 ("radv: Move VMID reservation to vkCreateDevice")
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39311>

--- src/amd/vulkan/radv_sqtt.c
+++ src/amd/vulkan/radv_sqtt.c
@@ -515,11 +515,6 @@ radv_sqtt_init(struct radv_device *device)
    /* Whether to use a staging buffer for faster reads on dGPUs. */
    device->rgp_use_staging_buffer = pdev->info.has_dedicated_vram;
 
-   if (device->ws->reserve_vmid(device->ws) < 0) {
-      fprintf(stderr, "radv: Failed to reserve VMID for SQTT tracing.\n");
-      return false;
-   }
-
    if (!radv_sqtt_init_bo(device))
       return false;
 
@@ -555,9 +550,6 @@ radv_sqtt_finish(struct radv_device *device)
    radv_unregister_queues(device, sqtt);
 
    ac_sqtt_finish(sqtt);
-
-   if (device->ws)
-      device->ws->unreserve_vmid(device->ws);
 }
 
 static bool
@@ -809,6 +801,12 @@ radv_sqtt_start_capturing(struct radv_queue *queue)
       return;
    }
 
+   /* Reserve a VMID to allow the KMD to update SPM_VMID accordingly. */
+   if (device->ws->reserve_vmid(device->ws) < 0) {
+      fprintf(stderr, "radv: Failed to reserve VMID for SQTT tracing.\n");
+      return;
+   }
+
    /* Sample CPU/GPU clocks before starting the trace. */
    if (!radv_sqtt_sample_clocks(device)) {
       fprintf(stderr, "radv: Failed to sample clocks\n");
@@ -833,6 +831,8 @@ radv_sqtt_stop_capturing(struct radv_queue *queue)
 
    /* TODO: Do something better than this whole sync. */
    device->vk.dispatch_table.QueueWaitIdle(radv_queue_to_handle(queue));
+
+   device->ws->unreserve_vmid(device->ws);
 
    if (radv_get_sqtt_trace(queue, &sqtt_trace) && (!device->spm.bo || radv_get_spm_trace(queue, &spm_trace))) {
       ac_dump_rgp_capture(&pdev->info, &sqtt_trace, device->spm.bo ? &spm_trace : NULL);
