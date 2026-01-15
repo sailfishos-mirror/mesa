sailfishos-mirror/mesa

commit f4a0e059703a85a3d595db51da21c7cbd3d8169d

anv/brw/iris: get rid of param array on prog_data
Drivers can do all the lowering to push constants to find the only
value useful in that array (subgroup_id). Then drivers call into
brw_cs_fill_push_const_info() to get the cross/per thread constant
layout computed in the prog_data.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38975>

src/gallium/drivers/iris/iris_program.c
@@ -165,9 +165,8 @@ iris_apply_brw_cs_prog_data(struct iris_compiled_shader *shader,
    iris->uses_sampler      = brw->uses_sampler;
    iris->prog_mask         = brw->prog_mask;
 
-   iris->first_param_is_builtin_subgroup_id =
-      brw->base.nr_params > 0 &&
-      brw->base.param[0] == BRW_PARAM_BUILTIN_SUBGROUP_ID;
+   /* The pushed constants only contain the subgroup_id */
+   iris->first_param_is_builtin_subgroup_id = brw->base.nr_params > 0;
 }
 
 static void
@@ -294,7 +293,6 @@ iris_apply_brw_prog_data(struct iris_compiled_shader *shader,
 
    ralloc_steal(shader, shader->brw_prog_data);
    ralloc_steal(shader->brw_prog_data, (void *)brw->relocs);
-   ralloc_steal(shader->brw_prog_data, brw->param);
 }
 
 #ifdef INTEL_USE_ELK
@@ -1212,13 +1210,6 @@ iris_setup_uniforms(ASSERTED const struct intel_device_info *devinfo,
 
    assert(num_cbufs < PIPE_MAX_CONSTANT_BUFFERS);
    nir_validate_shader(nir, "after remap");
-
-   /* We don't use params[] but gallium leaves num_uniforms set.  We use this
-    * to detect when cbuf0 exists but we don't need it anymore when we get
-    * here.  Instead, zero it out so that the back-end doesn't get confused
-    * when nr_params * 4 != num_uniforms != nr_params * 4.
-    */
-   nir->num_uniforms = 0;
 
    *out_system_values = system_values;
    *out_num_system_values = num_system_values;
@@ -3110,6 +3101,15 @@ iris_compile_cs(struct iris_screen *screen,
 
       struct brw_cs_prog_data *brw_prog_data =
          rzalloc(mem_ctx, struct brw_cs_prog_data);
+
+      bool subgroup_id_lowered = false;
+      NIR_PASS(subgroup_id_lowered, nir, brw_nir_lower_cs_subgroup_id, devinfo, 0);
+      if (subgroup_id_lowered) {
+         brw_prog_data->base.nr_params = 1;
+         brw_cs_fill_push_const_info(devinfo, brw_prog_data, 0);
+      } else {
+         brw_cs_fill_push_const_info(devinfo, brw_prog_data, -1);
+      }
 
       struct brw_compile_cs_params params = {
          .base = {

src/gallium/drivers/iris/iris_disk_cache.c
@@ -128,7 +128,6 @@ iris_disk_cache_store(struct disk_cache *cache,
       union brw_any_prog_data serializable;
       assert(prog_data_s <= sizeof(serializable));
       memcpy(&serializable, shader->brw_prog_data, prog_data_s);
-      serializable.base.param = NULL;
       serializable.base.relocs = NULL;
       blob_write_bytes(&blob, &serializable, prog_data_s);
    } else {
@@ -152,8 +151,6 @@ iris_disk_cache_store(struct disk_cache *cache,
    if (brw) {
       blob_write_bytes(&blob, brw->relocs,
                        brw->num_relocs * sizeof(struct intel_shader_reloc));
-      blob_write_bytes(&blob, brw->param,
-                       brw->nr_params * sizeof(uint32_t));
    } else {
 #ifdef INTEL_USE_ELK
       blob_write_bytes(&blob, elk->relocs,
@@ -264,12 +261,6 @@ iris_disk_cache_retrieve(struct iris_screen *screen,
          blob_copy_bytes(&blob, relocs,
                          brw->num_relocs * sizeof(struct intel_shader_reloc));
          brw->relocs = relocs;
-      }
-
-      brw->param = NULL;
-      if (brw->nr_params) {
-         brw->param = ralloc_array(NULL, uint32_t, brw->nr_params);
-         blob_copy_bytes(&blob, brw->param, brw->nr_params * sizeof(uint32_t));
       }
    } else {
 #ifdef INTEL_USE_ELK

src/gallium/drivers/iris/iris_state.c
@@ -9410,21 +9410,34 @@ iris_upload_gpgpu_walker(struct iris_context *ice,
    if ((stage_dirty & IRIS_STAGE_DIRTY_CS) ||
        (GFX_VER == 12 && !batch->contains_draw) ||
        cs_data->local_size[0] == 0 /* Variable local group size */) {
-      uint32_t curbe_data_offset = 0;
-      assert(cs_data->push.cross_thread.dwords == 0 &&
-             cs_data->push.per_thread.dwords == 1 &&
-             cs_data->first_param_is_builtin_subgroup_id);
-      const unsigned push_const_size =
-         iris_cs_push_const_total_size(shader, dispatch.threads);
-      uint32_t *curbe_data_map =
-         stream_state(batch, ice->state.dynamic_uploader,
-                      &ice->state.last_res.cs_thread_ids,
-                      align(push_const_size, 64), 64,
-                      &curbe_data_offset);
-      assert(curbe_data_map);
-      memset(curbe_data_map, 0x5a, align(push_const_size, 64));
-      iris_fill_cs_push_const_buffer(screen, shader, dispatch.threads,
-                                     curbe_data_map);
+      uint32_t curbe_data_offset, push_const_size;
+      uint32_t *curbe_data_map;
+      if (cs_data->push.cross_thread.dwords == 0 &&
+          cs_data->push.per_thread.dwords == 0) {
+         push_const_size = 64;
+         curbe_data_map =
+            stream_state(batch, ice->state.dynamic_uploader,
+                         &ice->state.last_res.cs_thread_ids,
+                         align(push_const_size, 64), 64,
+                         &curbe_data_offset);
+         assert(curbe_data_map);
+         memset(curbe_data_map, 0x5a, align(push_const_size, 64));
+      } else {
+         assert(cs_data->push.cross_thread.dwords == 0 &&
+                cs_data->push.per_thread.dwords == 1 &&
+                cs_data->first_param_is_builtin_subgroup_id);
+         push_const_size =
+            iris_cs_push_const_total_size(shader, dispatch.threads);
+         curbe_data_map =
+            stream_state(batch, ice->state.dynamic_uploader,
+                         &ice->state.last_res.cs_thread_ids,
+                         align(push_const_size, 64), 64,
+                         &curbe_data_offset);
+         assert(curbe_data_map);
+         memset(curbe_data_map, 0x5a, align(push_const_size, 64));
+         iris_fill_cs_push_const_buffer(screen, shader, dispatch.threads,
+                                        curbe_data_map);
+      }
 
       iris_emit_cmd(batch, GENX(MEDIA_CURBE_LOAD), curbe) {
          curbe.CURBETotalDataLength = align(push_const_size, 64);

src/intel/blorp/blorp_brw.c
@@ -28,7 +28,6 @@ blorp_compile_fs_brw(struct blorp_context *blorp, void *mem_ctx,
 
    struct brw_wm_prog_data *wm_prog_data = rzalloc(mem_ctx, struct brw_wm_prog_data);
    wm_prog_data->base.nr_params = 0;
-   wm_prog_data->base.param = NULL;
 
    struct brw_nir_compiler_opts opts = {
       .softfp64 = blorp->get_fp64_nir ? blorp->get_fp64_nir(blorp) : NULL,
@@ -147,10 +146,12 @@ blorp_compile_cs_brw(struct blorp_context *blorp, void *mem_ctx,
 
    struct brw_cs_prog_data *cs_prog_data = rzalloc(mem_ctx, struct brw_cs_prog_data);
    cs_prog_data->base.nr_params = nr_params;
-   cs_prog_data->base.param = rzalloc_array(NULL, uint32_t, nr_params);
+   brw_cs_fill_push_const_info(compiler->devinfo, cs_prog_data, nr_params);
 
    NIR_PASS(_, nir, brw_nir_lower_cs_intrinsics, compiler->devinfo,
               cs_prog_data);
+   NIR_PASS(_, nir, brw_nir_lower_cs_subgroup_id, compiler->devinfo,
+            offsetof(struct blorp_wm_inputs, subgroup_id));
    NIR_PASS(_, nir, nir_shader_intrinsics_pass, lower_base_workgroup_id,
               nir_metadata_control_flow, NULL);
 
@@ -169,9 +170,6 @@ blorp_compile_cs_brw(struct blorp_context *blorp, void *mem_ctx,
    };
 
    const unsigned *kernel = brw_compile_cs(compiler, &params);
-
-   ralloc_free(cs_prog_data->base.param);
-   cs_prog_data->base.param = NULL;
 
    return (struct blorp_program) {
       .kernel         = kernel,

src/intel/compiler/brw/brw_compile_cs.cpp
@@ -25,19 +25,15 @@ fill_push_const_block_info(struct brw_push_const_block *block, unsigned dwords)
    block->size = block->regs * 32;
 }
 
-static void
-cs_fill_push_const_info(const struct intel_device_info *devinfo,
-                        struct brw_cs_prog_data *cs_prog_data)
+extern "C" void
+brw_cs_fill_push_const_info(const struct intel_device_info *devinfo,
+                            struct brw_cs_prog_data *cs_prog_data,
+                            int subgroup_id_index)
 {
    const struct brw_stage_prog_data *prog_data = &cs_prog_data->base;
-   int subgroup_id_index = brw_get_subgroup_id_param_index(devinfo, prog_data);
-
-   /* The thread ID should be stored in the last param dword */
-   assert(subgroup_id_index == -1 ||
-          subgroup_id_index == (int)prog_data->nr_params - 1);
 
    unsigned cross_thread_dwords, per_thread_dwords;
-   if (subgroup_id_index >= 0) {
+   if (devinfo->verx10 < 125 && subgroup_id_index >= 0) {
       /* Fill all but the last register with cross-thread payload */
       cross_thread_dwords = 8 * (subgroup_id_index / 8);
       per_thread_dwords = prog_data->nr_params - cross_thread_dwords;
@@ -120,41 +116,6 @@ brw_nir_uses_sampler(nir_shader *shader)
                                        NULL);
 }
 
-static inline uint32_t *
-brw_stage_prog_data_add_params(struct brw_stage_prog_data *prog_data,
-                               unsigned nr_new_params)
-{
-   unsigned old_nr_params = prog_data->nr_params;
-   prog_data->nr_params += nr_new_params;
-   prog_data->param = reralloc(ralloc_parent(prog_data->param),
-                               prog_data->param, uint32_t,
-                               prog_data->nr_params);
-   return prog_data->param + old_nr_params;
-}
-
-static void
-brw_adjust_uniforms(brw_shader &s)
-{
-   if (s.devinfo->verx10 >= 125)
-      return;
-
-   assert(mesa_shader_stage_is_compute(s.stage));
-
-   if (brw_get_subgroup_id_param_index(s.devinfo, s.prog_data) == -1) {
-      /* Add uniforms for builtins after regular NIR uniforms. */
-      assert(s.uniforms == s.prog_data->nr_params);
-
-      /* Subgroup ID must be the last uniform on the list.  This will make
-       * easier later to split between cross thread and per thread
-       * uniforms.
-       */
-      uint32_t *param = brw_stage_prog_data_add_params(s.prog_data, 1);
-      *param = BRW_PARAM_BUILTIN_SUBGROUP_ID;
-   }
-
-   s.uniforms = s.prog_data->nr_params;
-}
-
 const unsigned *
 brw_compile_cs(const struct brw_compiler *compiler,
                struct brw_compile_cs_params *params)
@@ -233,7 +194,6 @@ brw_compile_cs(const struct brw_compiler *compiler,
          .archiver                = params->base.archiver,
       };
       v[simd] = std::make_unique<brw_shader>(&shader_params);
-      brw_adjust_uniforms(*v[simd]);
 
       const bool allow_spilling = simd == 0 ||
          (!simd_state.compiled[simd - 1] && !brw_simd_should_compile(simd_state, simd - 1)) ||
@@ -245,8 +205,6 @@ brw_compile_cs(const struct brw_compiler *compiler,
       }
 
       if (run_cs(*v[simd], allow_spilling)) {
-         cs_fill_push_const_info(compiler->devinfo, prog_data);
-
          brw_simd_mark_compiled(simd_state, simd, v[simd]->spilled_any_registers);
 
          if (devinfo->ver >= 30 && !v[simd]->spilled_any_registers &&

src/intel/compiler/brw/brw_compiler.h
@@ -543,7 +543,6 @@ enum brw_param_builtin {
    BRW_PARAM_BUILTIN_BASE_WORK_GROUP_ID_X,
    BRW_PARAM_BUILTIN_BASE_WORK_GROUP_ID_Y,
    BRW_PARAM_BUILTIN_BASE_WORK_GROUP_ID_Z,
-   BRW_PARAM_BUILTIN_SUBGROUP_ID,
    BRW_PARAM_BUILTIN_WORK_GROUP_SIZE_X,
    BRW_PARAM_BUILTIN_WORK_GROUP_SIZE_Y,
    BRW_PARAM_BUILTIN_WORK_GROUP_SIZE_Z,
@@ -615,14 +614,6 @@ struct brw_stage_prog_data {
    bool use_alt_mode; /**< Use ALT floating point mode?  Otherwise, IEEE. */
 
    uint32_t source_hash;
-
-   /* 32-bit identifiers for all push/pull parameters.  These can be anything
-    * the driver wishes them to be; the core of the back-end compiler simply
-    * re-arranges them.  The one restriction is that the bottom 2^16 values
-    * are reserved for builtins defined in the brw_param_builtin enum defined
-    * above.
-    */
-   uint32_t *param;
 
    /* Whether shader uses atomic operations. */
    bool uses_atomic_load_store;
@@ -1671,6 +1662,11 @@ void brw_debug_key_recompile(const struct brw_compiler *c, void *log,
 unsigned
 brw_cs_push_const_total_size(const struct brw_cs_prog_data *cs_prog_data,
                              unsigned threads);
+
+void
+brw_cs_fill_push_const_info(const struct intel_device_info *devinfo,
+                            struct brw_cs_prog_data *cs_prog_data,
+                            int subgroup_id_index);
 
 void
 brw_write_shader_relocs(const struct brw_isa_info *isa,

src/intel/compiler/brw/brw_nir.h
@@ -179,6 +179,10 @@ brw_nir_link_shaders(const struct brw_compiler *compiler,
 bool brw_nir_lower_cs_intrinsics(nir_shader *nir,
                                  const struct intel_device_info *devinfo,
                                  struct brw_cs_prog_data *prog_data);
+bool brw_nir_lower_cs_subgroup_id(nir_shader *nir,
+                                  const struct intel_device_info *devinfo,
+                                  unsigned subgroup_id_offset);
+
 bool brw_nir_lower_alpha_to_coverage(nir_shader *shader);
 bool brw_needs_vertex_attributes_bypass(const nir_shader *shader);
 void brw_nir_lower_fs_barycentrics(nir_shader *shader);

src/intel/compiler/brw/brw_nir_lower_cs_intrinsics.c
@@ -387,3 +387,36 @@ brw_nir_lower_cs_intrinsics(nir_shader *nir,
 
    return state.progress;
 }
+
+static bool
+lower_cs_subgroup_id_instr(nir_builder *b,
+                           nir_intrinsic_instr *intrin,
+                           void *data)
+{
+   if (intrin->intrinsic != nir_intrinsic_load_subgroup_id)
+      return false;
+
+   const unsigned *subgroup_id_offset_ptr = data;
+
+   b->cursor = nir_before_instr(&intrin->instr);
+   nir_def_replace(&intrin->def,
+                   nir_load_uniform(
+                      b, 1, 32, nir_imm_int(b, 0),
+                      .base = *subgroup_id_offset_ptr,
+                      .range = 4));
+
+   return true;
+}
+
+bool
+brw_nir_lower_cs_subgroup_id(nir_shader *nir,
+                             const struct intel_device_info *devinfo,
+                             unsigned subgroup_id_offset)
+{
+   if (devinfo->verx10 >= 125)
+      return false;
+
+   return nir_shader_intrinsics_pass(nir, lower_cs_subgroup_id_instr,
+                                     nir_metadata_control_flow,
+                                     &subgroup_id_offset);
+}

commit d19ea686d3e4d8ddb765a985e4d0f2516a84bb9c

iris/elk: Restore setting nir->num_uniforms to zero.
Elk still uses param array, so it depends on this behavior. This fixes
an assertion failure on Broadwell in gfxbench4/carchase/339.shader_test.

src/intel/compiler/elk/elk_fs_nir.cpp:148: void fs_nir_setup_uniforms(elk_fs_visitor&): Assertion `s.uniforms == s.prog_data->nr_params' failed.

Fixes: f4a0e059703 ("anv/brw/iris: get rid of param array on prog_data")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39280>

src/gallium/drivers/iris/iris_program.c
@@ -1212,6 +1212,17 @@ iris_setup_uniforms(ASSERTED const struct intel_device_info *devinfo,
    assert(num_cbufs < PIPE_MAX_CONSTANT_BUFFERS);
    nir_validate_shader(nir, "after remap");
 
+#ifdef INTEL_USE_ELK
+   /* We don't use params[] but gallium leaves num_uniforms set.  We use this
+    * to detect when cbuf0 exists but we don't need it anymore when we get
+    * here.  Instead, zero it out so that the back-end doesn't get confused
+    * when nr_params * 4 != num_uniforms != nr_params * 4.
+    *
+    * Elk still depends on this behavior.
+    */
+   nir->num_uniforms = 0;
+#endif
+
    *out_system_values = system_values;
    *out_num_system_values = num_system_values;
    *out_num_cbufs = num_cbufs;