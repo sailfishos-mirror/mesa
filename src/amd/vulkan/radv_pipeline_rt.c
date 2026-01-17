sailfishos-mirror/mesa

commit 0a1911b22049d1a72a922e88429c6b345390b837

radv,aco: Use function call structure for RT programs
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29580>

src/amd/vulkan/radv_pipeline_rt.c
@@ -931,9 +931,13 @@ static void
 compile_rt_prolog(struct radv_device *device, struct radv_ray_tracing_pipeline *pipeline)
 {
    const struct radv_physical_device *pdev = radv_device_physical(device);
+   struct nir_function raygen_stub = {};
    uint32_t push_constant_size = 0;
 
-   pipeline->prolog = radv_create_rt_prolog(device);
+   /* Create a dummy function signature for raygen shaders in order to pass parameter info to the prolog */
+   radv_nir_init_rt_function_params(&raygen_stub, MESA_SHADER_RAYGEN, 0);
+   radv_nir_lower_callee_signature(&raygen_stub);
+   pipeline->prolog = radv_create_rt_prolog(device, raygen_stub.num_params, raygen_stub.params);
 
    /* create combined config */
    struct ac_shader_config *config = &pipeline->prolog->config;

src/amd/common/ac_shader_args.h
@@ -188,15 +188,12 @@ struct ac_shader_args {
 
    /* RT */
    struct {
-      struct ac_arg uniform_shader_addr;
       struct ac_arg sbt_descriptors;
       struct ac_arg launch_sizes[3];
       struct ac_arg launch_size_addr;
       struct ac_arg launch_ids[3];
       struct ac_arg dynamic_callable_stack_base;
       struct ac_arg traversal_shader_addr;
-      struct ac_arg shader_addr;
-      struct ac_arg shader_record;
       struct ac_arg payload_offset;
    } rt;
 };

src/amd/compiler/aco_assembler.cpp
@@ -1837,7 +1837,8 @@ emit_program(Program* program, std::vector<uint32_t>& code, std::vector<struct a
                (uint32_t*)(program->constant_data.data() + program->constant_data.size()));
 
    program->config->scratch_bytes_per_wave =
-      align(program->config->scratch_bytes_per_wave, program->dev.scratch_alloc_granule);
+      align(program->config->scratch_bytes_per_wave + program->scratch_arg_size,
+            program->dev.scratch_alloc_granule);
    program->config->wgp_mode = program->wgp_mode;
 
    return exec_size;

src/amd/compiler/aco_insert_waitcnt.cpp
@@ -575,6 +575,10 @@ kill(wait_imm& imm, depctr_wait& depctr, Instruction* instr, wait_ctx& ctx,
        */
       force_waitcnt(ctx, imm);
    }
+   if (instr->opcode == aco_opcode::s_swappc_b64) {
+      u_foreach_bit (i, ctx.nonzero & ~counter_vs)
+         imm[i] = 0;
+   }
 
    check_instr(ctx, imm, instr);
 

src/amd/compiler/aco_interface.cpp
@@ -273,8 +273,8 @@ aco_compile_shader(const struct aco_compiler_options* options, const struct aco_
 void
 aco_compile_rt_prolog(const struct aco_compiler_options* options,
                       const struct aco_shader_info* info, const struct ac_shader_args* in_args,
-                      const struct ac_shader_args* out_args, aco_callback* build_prolog,
-                      void** binary)
+                      const struct ac_arg* descriptors, unsigned raygen_param_count,
+                      nir_parameter* raygen_params, aco_callback* build_prolog, void** binary)
 {
    init();
 
@@ -285,7 +285,8 @@ aco_compile_rt_prolog(const struct aco_compiler_options* options,
    program->debug.func = NULL;
    program->debug.private_data = NULL;
 
-   select_rt_prolog(program.get(), &config, options, info, in_args, out_args);
+   select_rt_prolog(program.get(), &config, options, info, in_args, descriptors, raygen_param_count,
+                    raygen_params);
    validate(program.get());
    insert_waitcnt(program.get());
    insert_NOPs(program.get());

src/amd/compiler/aco_interface.h
@@ -18,6 +18,8 @@
 extern "C" {
 #endif
 
+struct nir_parameter;
+typedef struct nir_parameter nir_parameter;
 struct ac_shader_config;
 struct aco_shader_info;
 struct aco_vs_prolog_info;
@@ -42,8 +44,8 @@ void aco_compile_shader(const struct aco_compiler_options* options,
 
 void aco_compile_rt_prolog(const struct aco_compiler_options* options,
                            const struct aco_shader_info* info, const struct ac_shader_args* in_args,
-                           const struct ac_shader_args* out_args, aco_callback* build_prolog,
-                           void** binary);
+                           const struct ac_arg* descriptors, unsigned raygen_param_count,
+                           nir_parameter* raygen_params, aco_callback* build_prolog, void** binary);
 
 void aco_compile_vs_prolog(const struct aco_compiler_options* options,
                            const struct aco_shader_info* info,

src/amd/compiler/aco_ir.h
@@ -26,6 +26,7 @@
 #include <vector>
 
 typedef struct nir_shader nir_shader;
+typedef struct nir_parameter nir_parameter;
 
 namespace aco {
 
@@ -2337,6 +2338,7 @@ public:
    bool has_call = false;
    ABI callee_abi = {};
    RegisterDemand callee_param_demand = RegisterDemand();
+   unsigned scratch_arg_size = 0;
 
    struct {
       monotonic_buffer_resource memory;
@@ -2409,7 +2411,8 @@ void select_trap_handler_shader(Program* program, ac_shader_config* config,
 void select_rt_prolog(Program* program, ac_shader_config* config,
                       const struct aco_compiler_options* options,
                       const struct aco_shader_info* info, const struct ac_shader_args* in_args,
-                      const struct ac_shader_args* out_args);
+                      const struct ac_arg* descriptors, unsigned raygen_param_count,
+                      nir_parameter* raygen_params);
 void select_vs_prolog(Program* program, const struct aco_vs_prolog_info* pinfo,
                       ac_shader_config* config, const struct aco_compiler_options* options,
                       const struct aco_shader_info* info, const struct ac_shader_args* args);

src/amd/compiler/aco_validate.cpp
@@ -438,7 +438,10 @@ validate_ir(Program* program)
                   ((instr->isMUBUF() || instr->isMTBUF()) && i == 1) ||
                   (instr->isScratch() && i == 0) || (instr->isDS() && i == 0) ||
                   (instr->opcode == aco_opcode::p_init_scratch && i == 0) ||
-                  (instr_disables_wqm(instr.get()) && i + 2 >= instr->operands.size());
+                  (instr_disables_wqm(instr.get()) && i + 2 >= instr->operands.size()) ||
+                  ((instr->opcode == aco_opcode::p_return ||
+                    instr->opcode == aco_opcode::p_reload_preserved) &&
+                   i == 0);
                check(can_be_undef, "Undefs can only be used in certain operands", instr.get());
             } else {
                check(instr->operands[i].isFixed() || instr->operands[i].isTemp() ||

commit 197cb437cdfd0f1ae28a4cf1d38611e5a216e318

radv/rt: Fix gnu-empty-initializer error in radv_pipeline_rt.c
Fixes the following building error happening with clang:

FAILED: src/amd/vulkan/libvulkan_radeon.so.p/radv_pipeline_rt.c.o
...
../src/amd/vulkan/radv_pipeline_rt.c:934:38: error: use of GNU empty initializer extension [-Werror,-Wgnu-empty-initializer]
   struct nir_function raygen_stub = {};
                                     ^
1 error generated.

Fixes: 0a1911b2 ("radv,aco: Use function call structure for RT programs")
Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39370>

src/amd/vulkan/radv_pipeline_rt.c
@@ -931,7 +931,7 @@ static void
 compile_rt_prolog(struct radv_device *device, struct radv_ray_tracing_pipeline *pipeline)
 {
    const struct radv_physical_device *pdev = radv_device_physical(device);
-   struct nir_function raygen_stub = {};
+   struct nir_function raygen_stub = {0};
    uint32_t push_constant_size = 0;
 
    /* Create a dummy function signature for raygen shaders in order to pass parameter info to the prolog */