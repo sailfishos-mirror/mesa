88ae2365b26cca549cfce5cfd5cec0b92af6c50c
Rob Clark <rob.clark@oss.qualcomm.com>
2026-01-17 10:18:53 -0800

    tu: Re-enable LRZ for gen8
    Now that the FC/status memory layout size is correct, we no longer get
    lrz related iova faults.  So re-enable lrz for gen8.
    
    Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39375>

--- src/freedreno/vulkan/tu_device.cc
+++ src/freedreno/vulkan/tu_device.cc
@@ -2704,7 +2704,6 @@ tu_CreateDevice(VkPhysicalDevice physicalDevice,
       break;
    case 8:
       /* gen8 TODO: */
-      tu_env.debug |= TU_DEBUG_NOLRZ;     /* WRITE iova faults from UCHE */
       tu_env.debug |= TU_DEBUG_FLUSHALL;  /* dEQP-VK.draw.\*from_compute\* */
       vk_device_dispatch_table_from_entrypoints(
          &dispatch_table, &tu_device_entrypoints_a8xx, false);
