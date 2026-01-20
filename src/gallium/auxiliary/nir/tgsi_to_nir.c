bminor/mesa-mesa

commit 885323ea3ab15d736645ac7da8ce60fa66b217b8

tgsi/nir: Handling TGSI_OPCODE_RET in tgsi_to_nir
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11554

The nir_push_if is needed as more instructions will added after `RET`.

Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Reviewed-by: Jose Fonseca <jfonseca@vmware.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37170>

src/gallium/auxiliary/nir/tgsi_to_nir.c
@@ -1693,7 +1693,7 @@ static const nir_op op_trans[TGSI_OPCODE_LAST] = {
 
    /* No function calls, yet. */
    [TGSI_OPCODE_CAL] = 0, /* XXX */
-   [TGSI_OPCODE_RET] = 0, /* XXX */
+   [TGSI_OPCODE_RET] = 0, /* Handled with nir_lower_returns */
 
    [TGSI_OPCODE_SSG] = nir_op_fsign,
    [TGSI_OPCODE_CMP] = 0,
@@ -2028,6 +2028,16 @@ ttn_emit_instruction(struct ttn_compile *c)
 
    case TGSI_OPCODE_DDY_FINE:
       dst = nir_ddy_fine(b, src[0]);
+      break;
+
+   case TGSI_OPCODE_RET:
+      /* NIR returns must be at the end of the block, while TGSI returns may not
+       * be. Guarantee that by putting it inside a trivial if, which will be
+       * cleaned up by nir_opt_dead_cf.
+       */
+      nir_push_if(b, nir_imm_true(b));
+      nir_jump(b, nir_jump_return);
+      nir_pop_if(b, NULL);
       break;
 
    default:
@@ -2530,6 +2540,7 @@ ttn_finalize_nir(struct ttn_compile *c, struct pipe_screen *screen)
 
    MESA_TRACE_FUNC();
 
+   NIR_PASS(_, nir, nir_lower_returns);
    NIR_PASS(_, nir, nir_lower_vars_to_ssa);
    NIR_PASS(_, nir, nir_lower_reg_intrinsics_to_ssa);
 

sailfishos-mirror/mesa

commit 7300aa34a06b8db16e4cf31af146b37a60058397

tgsi/nir: Store output variables before each TGSI_OPCODE_RET
Fixes: 885323ea3ab15d736645ac7da8ce60fa66b217b8
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39379>

src/gallium/auxiliary/nir/tgsi_to_nir.c
@@ -2202,6 +2202,11 @@ ttn_parse_tgsi(struct ttn_compile *c, const void *tgsi_tokens)
          break;
 
       case TGSI_TOKEN_TYPE_INSTRUCTION:
+         if (parser.FullToken.FullInstruction.Instruction.Opcode == TGSI_OPCODE_RET) {
+            /* We have to be conservative and add output stores before each return.
+             * Hopefully stores will be optimized out later if not actually required */
+            ttn_add_output_stores(c);
+         }
          ttn_emit_instruction(c);
          break;
 