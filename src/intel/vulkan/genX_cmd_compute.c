840e6e855bcafa52180a5beb6cfe9e4abd1f8367
Tapani Pälli <tapani.palli@intel.com>
2026-01-20 07:49:41 +0200

    anv: add handling for Wa_14026600921
    This is the Xe3 version of the earlier workaround.
    
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39404>

--- src/intel/vulkan/genX_cmd_compute.c
+++ src/intel/vulkan/genX_cmd_compute.c
@@ -77,15 +77,16 @@ genX(cmd_buffer_ensure_cfe_state)(struct anv_cmd_buffer *cmd_buffer,
       default:   UNREACHABLE("invalid stack_ids value");
       }
 
-#if INTEL_WA_14021821874_GFX_VER || INTEL_WA_14018813551_GFX_VER
-      /* Wa_14021821874, Wa_14018813551:
+#if INTEL_WA_14021821874_GFX_VER || INTEL_WA_14018813551_GFX_VER || INTEL_WA_14026600921_GFX_VER
+      /* Wa_14021821874, Wa_14018813551, Wa_14026600921:
        *
        * "StackIDControlOverride_RTGlobals = 0 (i.e. 2k)". We
        * already set stack size per ray to 64 in brw_nir_lower_rt_intrinsics
        * as the workaround also requires.
        */
       if (intel_needs_workaround(cmd_buffer->device->info, 14021821874) ||
-          intel_needs_workaround(cmd_buffer->device->info, 14018813551))
+          intel_needs_workaround(cmd_buffer->device->info, 14018813551) ||
+          intel_needs_workaround(cmd_buffer->device->info, 14026600921))
          cfe.StackIDControl = StackIDs2048;
 #endif
 
