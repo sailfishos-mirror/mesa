bminor/mesa-mesa

commit 63cac09d748d66efeca34da2c213a9cc476345d6

venus: improve image memory requirement cache for image aliasing
We can strip the alias bit as the memory requirements are identical
between the bound image and the aliased image per spec.

Acked-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34135>

src/virtio/vulkan/vn_image.c
@@ -97,6 +97,14 @@ vn_image_get_image_reqs_key(struct vn_device *dev,
    if (!dev->image_reqs_cache.ht)
       return false;
 
+   /* Strip the alias bit as the memory requirements are identical. */
+   VkImageCreateInfo local_info;
+   if (create_info->flags & VK_IMAGE_CREATE_ALIAS_BIT) {
+      local_info = *create_info;
+      local_info.flags &= ~VK_IMAGE_CREATE_ALIAS_BIT;
+      create_info = &local_info;
+   }
+
    _mesa_sha1_init(&sha1_ctx);
 
    /* Hash relevant fields in the pNext chain */

sailfishos-mirror/mesa

commit 2e683eb7385c54f872acc47b371210d2282bc103

venus: fix aliased image memory requirement caching
The spec doesn't guarantee that the aliased and non-alised image shares
the same memory requirement.

Fixes: 63cac09d748 ("venus: improve image memory requirement cache for image aliasing")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39398>

src/virtio/vulkan/vn_image.c
@@ -97,20 +97,6 @@ vn_image_get_image_reqs_key(struct vn_device *dev,
    if (!dev->image_reqs_cache.ht)
       return false;
 
-   /* Strip the alias bit as the memory requirements are identical.
-    *
-    * Except on:
-    * - ANV: https://gitlab.freedesktop.org/mesa/mesa/-/issues/14671
-    */
-   VkImageCreateInfo local_info;
-   if ((create_info->flags & VK_IMAGE_CREATE_ALIAS_BIT) &&
-       (dev->physical_device->renderer_driver_id !=
-        VK_DRIVER_ID_INTEL_OPEN_SOURCE_MESA)) {
-      local_info = *create_info;
-      local_info.flags &= ~VK_IMAGE_CREATE_ALIAS_BIT;
-      create_info = &local_info;
-   }
-
    _mesa_sha1_init(&sha1_ctx);
 
    /* Hash relevant fields in the pNext chain */