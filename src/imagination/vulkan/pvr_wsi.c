bminor/mesa-mesa

commit 6bda88bfdbc06a238d910034442bf3b2b0ccdf00

pvr: copy WSI can_present_on_device function from PanVK
Both PVR and PanVK are drivers for generic embedded GPU IP cores, so
just take the can_present_on_device implementation from PanVK, which
allows any platform devices for presentation.

Signed-off-by: Icenowy Zheng <uwu@icenowy.me>
Reviewed-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38985>

src/imagination/vulkan/pvr_wsi.c
@@ -30,6 +30,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <vulkan/vulkan.h>
+#include <xf86drm.h>
 
 #include "pvr_device.h"
 #include "pvr_entrypoints.h"
@@ -50,6 +51,16 @@ static PFN_vkVoidFunction pvr_wsi_proc_addr(VkPhysicalDevice physicalDevice,
    return vk_instance_get_proc_addr_unchecked(&pdevice->instance->vk, pName);
 }
 
+static bool pvr_can_present_on_device(VkPhysicalDevice pdevice, int fd)
+{
+   drmDevicePtr device;
+   if (drmGetDevice2(fd, 0, &device) != 0)
+      return false;
+   /* Allow on-device presentation for all devices with bus type PLATFORM.
+    * Other device types such as PCI or USB should use the PRIME blit path. */
+   return device->bustype == DRM_BUS_PLATFORM;
+}
+
 VkResult pvr_wsi_init(struct pvr_physical_device *pdevice)
 {
    VkResult result;
@@ -65,6 +76,7 @@ VkResult pvr_wsi_init(struct pvr_physical_device *pdevice)
       return result;
 
    pdevice->wsi_device.supports_modifiers = false;
+   pdevice->wsi_device.can_present_on_device = pvr_can_present_on_device;
    pdevice->vk.wsi_device = &pdevice->wsi_device;
 
    return VK_SUCCESS;

sailfishos-mirror/mesa

commit b18b52e61d46be2e02d371a692839e3c78bba171

pvr: Free drm device in can_present_on_device
Fixes: 6bda88bfdbc ("pvr: copy WSI can_present_on_device function from PanVK")
Signed-off-by: Kitlith <kitlith@kitl.pw>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39415>

src/imagination/vulkan/pvr_wsi.c
@@ -58,7 +58,11 @@ static bool pvr_can_present_on_device(VkPhysicalDevice pdevice, int fd)
       return false;
    /* Allow on-device presentation for all devices with bus type PLATFORM.
     * Other device types such as PCI or USB should use the PRIME blit path. */
-   return device->bustype == DRM_BUS_PLATFORM;
+   bool match = device->bustype == DRM_BUS_PLATFORM;
+
+   drmFreeDevice(&device);
+
+   return match;
 }
 
 VkResult pvr_wsi_init(struct pvr_physical_device *pdevice)