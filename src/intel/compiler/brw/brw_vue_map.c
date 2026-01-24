74f1d4f47b675e842af432fd1e4ca24fbbd6d584
Caio Oliveira <caio.oliveira@intel.com>
2026-01-23 17:03:58 -0800

    intel/compiler: Use SPDX annotations
    Minor adjustments to formatting of the copyright line, but keep
    dates and holders.  "Authors" entries that could be
    obtained via Git logs were also removed.
    
    The license in brw_disasm.c and elk_disasm.c don't match directly
    any SPDX pattern I could find, so kept as is.
    
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39503>

--- src/intel/compiler/brw/brw_vue_map.c
+++ src/intel/compiler/brw/brw_vue_map.c
@@ -1,24 +1,6 @@
 /*
  * Copyright © 2011 Intel Corporation
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
+ * SPDX-License-Identifier: MIT
  */
 
 /**
@@ -32,13 +14,7 @@
  * have to agree on the layout.  However, there is also a "VUE Header" that
  * prescribes a fixed-layout for items that interact with fixed function
  * hardware, such as the clipper and rasterizer.
- *
- * Authors:
- *   Paul Berry <stereotype441@gmail.com>
- *   Chris Forbes <chrisf@ijw.co.nz>
- *   Eric Anholt <eric@anholt.net>
  */
-
 
 #include "brw_compiler.h"
 #include "dev/intel_debug.h"
