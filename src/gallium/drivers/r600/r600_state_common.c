yuq/mesa-lima

commit 6c1432f0be9c7772573bb86b997c9f085d16c204

r600/eg: fix cube map array buffer images.
This fixes a crash in:
KHR-GL45.texture_cube_map_array.texture_size_compute_sh.

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Signed-off-by: Dave Airlie <airlied@redhat.com>

src/gallium/drivers/r600/r600_state_common.c
@@ -1421,7 +1421,7 @@ void eg_setup_buffer_constants(struct r600_context *rctx, int shader_type)
 			int idx = i - sview_bits;
 			if (images->enabled_mask & (1 << idx)) {
 				uint32_t offset = (base_offset / 4) + i;
-				constants[offset] = images->views[i].base.resource->array_size / 6;
+				constants[offset] = images->views[idx].base.resource->array_size / 6;
 			}
 		}
 	}

sailfishos-mirror/mesa

commit 0b8d8f2b17719d51f43896f8c63b64ec00250186

r600: update cubearray imagesize calculation
The previous method to calculate imageSize().z was
incorrect for a cubearray view.

This change was tested on palm and cayman. Here is the test fixed:
spec/arb_texture_view/rendering-layers-image/layers rendering of imagecubearray: fail pass

Fixes: 6c1432f0be9c ("r600/eg: fix cube map array buffer images.")
Signed-off-by: Patrick Lerda <patrick9876@free.fr>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39063>

src/gallium/drivers/r600/r600_state_common.c
@@ -1525,7 +1525,8 @@ void eg_setup_buffer_constants(struct r600_context *rctx, int shader_type)
 			int idx = i - sview_bits;
 			if (images->enabled_mask & (1 << idx)) {
 				uint32_t offset = (base_offset / 4) + i;
-				constants[offset] = images->views[idx].base.resource->array_size / 6;
+				constants[offset] = (G_038014_LAST_ARRAY(images->views[idx].resource_words[5]) -
+						     G_038014_BASE_ARRAY(images->views[idx].resource_words[5]) + 1) / 6;
 			}
 		}
 	}