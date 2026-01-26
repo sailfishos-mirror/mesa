bminor/mesa-mesa

commit 42ef23ecd1a077a5564c6cd4a31ecf00f234a615

intel/blorp: Don't redescribe some Tile64 clears
We don't support redescribing Tile64 and 3D due to interleaved depth
planes.

Fixes: 312952048b1 ("intel/blorp: Redescribe gfx12.5 surfaces for CCS fast clears")
Tested-by: Ian Romanick <ian.d.romanick@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35619>

src/intel/blorp/blorp_clear.c
@@ -526,6 +526,15 @@ blorp_fast_clear(struct blorp_batch *batch,
                                           &start_tile_B, &end_tile_B)) {
          size_B = end_tile_B - start_tile_B;
          addr.offset += start_tile_B;
+      } else if (isl_tiling_is_64(surf->surf->tiling)) {
+         /* If not supported above, clear the range without redescription. If
+          * the image is 3D, redescription is not possible because multiple
+          * depth slices are non-trivially interleaved into one plane. If the
+          * image is part of a miptail, there should be no benefit from
+          * redescription.
+          */
+         assert(surf->surf->logical_level0_px.d > 1 ||
+                level <= surf->surf->miptail_start_level);
       } else if (level == 0 && start_layer == 0 && num_layers == 1) {
          assert(surf->surf->tiling == ISL_TILING_4 ||
                 surf->surf->tiling == ISL_TILING_Y0);

sailfishos-mirror/mesa

commit 6fc0e5c0aa008cc76785106d598584dd1eef8f01

blorp: Fix Tile64 clear redescription assertion
Prevent assert failures in a future commit where Tile64 will be selected
more often.

Fixes: 42ef23ecd1a ("intel/blorp: Don't redescribe some Tile64 clears")
Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38063>

src/intel/blorp/blorp_clear.c
@@ -550,14 +550,11 @@ blorp_fast_clear(struct blorp_batch *batch,
          size_B = end_tile_B - start_tile_B;
          addr.offset += start_tile_B;
       } else if (isl_tiling_is_64(surf->surf->tiling)) {
-         /* If not supported above, clear the range without redescription. If
-          * the image is 3D, redescription is not possible because multiple
-          * depth slices are non-trivially interleaved into one plane. If the
-          * image is part of a miptail, there should be no benefit from
-          * redescription.
+         /* If not supported above, clear the range without redescription.
+          * Thankfully, we haven't run into this outside of conformance tests.
           */
-         assert(surf->surf->logical_level0_px.d > 1 ||
-                level <= surf->surf->miptail_start_level);
+         assert(surf->surf->levels > 1 ||
+                surf->surf->logical_level0_px.d != num_layers);
       } else if (level == 0 && start_layer == 0 && num_layers == 1) {
          assert(surf->surf->tiling == ISL_TILING_4 ||
                 surf->surf->tiling == ISL_TILING_Y0);