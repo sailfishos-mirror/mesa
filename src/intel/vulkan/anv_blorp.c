sailfishos-mirror/mesa

commit ab9d3528dc839a7b3a7c0c960dcba78bbd9e4a4c

anv: fix queue check in anv_blorp_execute_on_companion on xe3
Fixes:
   dEQP-VK.api.copy_and_blit.dedicated_allocation.resolve_image.whole_copy_before_resolving_transfer.2_bit

Otherwise we attempt to use blorp and hit various asserts later in:

- blorp_copy_supports_blitter
- blorp_xy_block_copy_blt

Fixes: 61287b00f3b ("anv: Stop using RCS companion for MSAA copy/clear on Xe3+")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39346>

src/intel/vulkan/anv_blorp.c
@@ -595,7 +595,7 @@ anv_blorp_execute_on_companion(struct anv_cmd_buffer *cmd_buffer,
       if (devinfo->ver < 30)
          return true;
       /* Even on Xe3, no support for MSAA on BCS. */
-      if (anv_cmd_buffer_is_compute_queue(cmd_buffer))
+      if (anv_cmd_buffer_is_blitter_queue(cmd_buffer))
          return true;
    }
 

commit 85978ccd2859b6fd9813699963752dda43e9debe

anv: route clear operations on compute to companion
This fixes bunch of cts tests hitting issues when attempting
anv_image_mcs_op with compute.

Fixes: ab9d3528dc8 ("anv: fix queue check in anv_blorp_execute_on_companion on xe3")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39581>

src/intel/vulkan/anv_blorp.c
@@ -601,6 +601,10 @@ anv_blorp_execute_on_companion(struct anv_cmd_buffer *cmd_buffer,
       /* Even on Xe3, no support for MSAA on BCS. */
       if (anv_cmd_buffer_is_blitter_queue(cmd_buffer))
          return true;
+
+      /* On Xe3 compute supports blits but not clear operations. */
+      if (!src_image)
+         return true;
    }
 
    if (anv_cmd_buffer_is_blitter_queue(cmd_buffer)) {