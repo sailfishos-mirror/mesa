5926209996c56ee335470a7c437ef2c27fd7928b
Georg Lehmann <dadschoorse@gmail.com>
2026-02-02 17:42:18 +0100

    brw/nir_lower_fsign: try to fix NaN correctness
    Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39641>

--- src/intel/compiler/brw/brw_nir_lower_fsign.py
+++ src/intel/compiler/brw/brw_nir_lower_fsign.py
@@ -17,18 +17,18 @@ lower_fsign = [
     #
     # NOTE: fcsel opcodes are currently limited to float32 in NIR.
     (('fmul@32(is_only_used_as_float)', ('fsign(is_used_once)', 'a(is_not_negative)'), b), ('fcsel_gt',          a ,          b , ('fmul', b, 0.0       ))),
-    (('~fmul@32',                       ('fsign(is_used_once)', 'a(is_not_negative)'), b), ('fcsel_gt',          a ,          b ,             0.0        )),
+    (('~fmul@32(nsz,nnan)',             ('fsign(is_used_once)', 'a(is_not_negative)'), b), ('fcsel_gt',          a ,          b ,             0.0        )),
     (('fmul@32(is_only_used_as_float)', ('fsign(is_used_once)', 'a(is_not_positive)'), b), ('fcsel_gt', ('fneg', a), ('fneg', b), ('fmul', b, 0x80000000))),
-    (('~fmul@32',                       ('fsign(is_used_once)', 'a(is_not_positive)'), b), ('fcsel_gt', ('fneg', a), ('fneg', b),             0x80000000 )),
+    (('~fmul@32(nsz,nnan)',             ('fsign(is_used_once)', 'a(is_not_positive)'), b), ('fcsel_gt', ('fneg', a), ('fneg', b),             0x80000000 )),
 
     (('fmul@16(is_only_used_as_float)', ('fsign(is_used_once)', 'a(is_not_negative)'), b), ('bcsel', ('!flt', 0,          a ),          b , ('fmul', b, 0.0   ))),
-    (('~fmul@16',                       ('fsign(is_used_once)', 'a(is_not_negative)'), b), ('bcsel', ('!flt', 0,          a ),          b ,             0.0    )),
+    (('~fmul@16(nsz,nnan)',             ('fsign(is_used_once)', 'a(is_not_negative)'), b), ('bcsel', ('!flt', 0,          a ),          b ,             0.0    )),
     (('fmul@16(is_only_used_as_float)', ('fsign(is_used_once)', 'a(is_not_positive)'), b), ('bcsel', ('!flt', 0, ('fneg', a)), ('fneg', b), ('fmul', b, 0x8000))),
-    (('~fmul@16',                       ('fsign(is_used_once)', 'a(is_not_positive)'), b), ('bcsel', ('!flt', 0, ('fneg', a)), ('fneg', b),             0x8000 )),
+    (('~fmul@16(nsz,nnan)',             ('fsign(is_used_once)', 'a(is_not_positive)'), b), ('bcsel', ('!flt', 0, ('fneg', a)), ('fneg', b),             0x8000 )),
 
     (('fmul@32(is_only_used_as_float,nsz)',      ('fsign(is_used_once)', a), 'b(is_finite)'), ('fcsel_gt', a, b, ('fcsel_gt', ('fneg', a), ('fneg', b), 0.0))),
     (('fmul@32(is_only_used_as_float,nsz,nnan)', ('fsign(is_used_once)', a),  b            ), ('fcsel_gt', a, b, ('fcsel_gt', ('fneg', a), ('fneg', b), 0.0))),
-    (('~fmul@32',                                ('fsign(is_used_once)', a),  b            ), ('fcsel_gt', a, b, ('fcsel_gt', ('fneg', a), ('fneg', b), 0.0))),
+    (('~fmul@32(nsz,nnan)',                      ('fsign(is_used_once)', a),  b            ), ('fcsel_gt', a, b, ('fcsel_gt', ('fneg', a), ('fneg', b), 0.0))),
 
     # This is 99.99% strictly correct for OpenCL. It will provide correctly
     # signed zero for ±0 inputs, and it will provide zero for NaN inputs. The
@@ -37,10 +37,10 @@ lower_fsign = [
     (('fsign@16', a), ('bcsel', ('!flt', 0, ('fabs', a)), ('ior', ('iand', a, 0x8000    ), 0x3c00    ), ('iand', a, 0x8000    ))),
 
     # The only effect a*0.0 should have is when 'a' is infinity, -0.0 or NaN
-    (('fmul(nsz,nnan)', 'a', 0.0), 0.0),
+    (('fmul(nsz,nnan)', a, 0.0), 0.0),
+    (('fmul(nsz,nnan)', a, -0.0), 0.0),
     (('fmul(nsz)', 'a(is_finite)', 0.0), 0.0),
-    (('fmul(nsz,nnan)', 'a@32', 0x80000000), 0.0),
-    (('fmul(nsz,nnan)', 'a@16', 0x8000    ), 0.0),
+    (('fmul(nsz)', 'a(is_finite)', -0.0), 0.0),
 ]
 
 def main():
