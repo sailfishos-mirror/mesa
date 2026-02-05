181bb1fc939edc4b190526941ea6a4dccb9fbd17
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2026-02-05 16:32:53 +0100

    radv/meta: remove dead code for VK_FORMAT_R4G4_UNORM_PACK8
    This isn't supported at all.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39689>

--- src/amd/vulkan/meta/radv_meta_clear.c
+++ src/amd/vulkan/meta/radv_meta_clear.c
@@ -1925,14 +1925,6 @@ radv_cmd_clear_image(struct radv_cmd_buffer *cmd_buffer, struct radv_image *imag
       }
    }
 
-   if (format == VK_FORMAT_R4G4_UNORM_PACK8) {
-      uint8_t r, g;
-      format = VK_FORMAT_R8_UINT;
-      r = float_to_ubyte(clear_value->color.float32[0]) >> 4;
-      g = float_to_ubyte(clear_value->color.float32[1]) >> 4;
-      internal_clear_value.color.uint32[0] = (r << 4) | (g & 0xf);
-   }
-
    for (uint32_t r = 0; r < range_count; r++) {
       const VkImageSubresourceRange *range = &ranges[r];
 
