4f475789d59a867c66c3cd94cfa439478d8135d3
Yiwei Zhang <zzyiwei@chromium.org>
2026-02-08 23:56:04 -0800

    venus: ensure descriptor writes invariance
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39762>

--- src/virtio/vulkan/vn_descriptor_heap.c
+++ src/virtio/vulkan/vn_descriptor_heap.c
@@ -7,6 +7,7 @@
 
 #include "vn_descriptor.h"
 #include "vn_device.h"
+#include "vn_physical_device.h"
 
 /* descriptor heap commands */
 
@@ -17,11 +18,18 @@ vn_WriteSamplerDescriptorsEXT(VkDevice device,
                               const VkHostAddressRangeEXT *pDescriptors)
 {
    struct vn_device *dev = vn_device_from_handle(device);
+   VkPhysicalDevice physical_dev_handle =
+      vn_physical_device_to_handle(dev->physical_device);
+
+   const VkDeviceSize size = vn_GetPhysicalDeviceDescriptorSizeEXT(
+      physical_dev_handle, VK_DESCRIPTOR_TYPE_SAMPLER);
 
    /* TODO move out from primary ring? */
    for (uint32_t i = 0; i < samplerCount; i++) {
+      assert(pDescriptors[i].size >= size);
+
       VkResult result = vn_call_vkWriteSamplerDescriptorMESA(
-         dev->primary_ring, device, &pSamplers[i], pDescriptors[i].size,
+         dev->primary_ring, device, &pSamplers[i], size,
          pDescriptors[i].address);
       if (result != VK_SUCCESS)
          return vn_error(dev->instance, result);
@@ -37,11 +45,17 @@ vn_WriteResourceDescriptorsEXT(VkDevice device,
                                const VkHostAddressRangeEXT *pDescriptors)
 {
    struct vn_device *dev = vn_device_from_handle(device);
+   VkPhysicalDevice physical_dev_handle =
+      vn_physical_device_to_handle(dev->physical_device);
 
    /* TODO move out from primary ring? */
    for (uint32_t i = 0; i < resourceCount; i++) {
+      const VkDeviceSize size = vn_GetPhysicalDeviceDescriptorSizeEXT(
+         physical_dev_handle, pResources[i].type);
+
+      assert(pDescriptors[i].size >= size);
       VkResult result = vn_call_vkWriteResourceDescriptorMESA(
-         dev->primary_ring, device, &pResources[i], pDescriptors[i].size,
+         dev->primary_ring, device, &pResources[i], size,
          pDescriptors[i].address);
       if (result != VK_SUCCESS)
          return vn_error(dev->instance, result);
