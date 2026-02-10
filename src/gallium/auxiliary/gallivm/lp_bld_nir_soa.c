bminor/mesa-mesa

commit e28cfb2bada2398a9ddcadd03fac060d5654a15e

gallivm: handle u8/u16 const loads properly on big-endian.
Turns out just putting the u32 in doesn't work on big endian, so
put the proper u8/u16 values in.

Got a report that since the loop limiter got removed, a gtk4 blur
shader was looping forever on s390x. Turns out it was using a 16-bit
loop variable (because why wouldn't you), and the loop counter was
just staying at 0 all the time.

Cc: mesa-stable
Acked-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37593>

src/gallium/auxiliary/gallivm/lp_bld_nir_soa.c
@@ -1267,8 +1267,10 @@ emit_load_const(struct lp_build_nir_soa_context *bld,
 
    for (unsigned i = 0; i < instr->def.num_components; i++) {
      outval[i] = lp_build_const_int_vec(bld->base.gallivm, int_bld->type,
-                                        bits == 32 ? instr->value[i].u32
-                                                   : instr->value[i].u64);
+                                        bits == 8 ? instr->value[i].u8 :
+                                        bits == 16 ? instr->value[i].u32 :
+                                        bits == 32 ? instr->value[i].u32 :
+                                                     instr->value[i].u64);
    }
    for (unsigned i = instr->def.num_components; i < NIR_MAX_VEC_COMPONENTS; i++) {
       outval[i] = NULL;

sailfishos-mirror/mesa

commit c016346b50e9085b531f9bcbd7cfd63d3806a3e1

gallivm: handle u16 correct on const loads.
I somehow screwed this up on my previous attempt at fixing this bug,

This should fix the loop limiter bug on big endian properly.

Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Cc: mesa-stable
Fixes: e28cfb2bada2 ("gallivm: handle u8/u16 const loads properly on big-endian.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39813>

src/gallium/auxiliary/gallivm/lp_bld_nir_soa.c
@@ -1269,10 +1269,7 @@ emit_load_const(struct lp_build_nir_soa_context *bld,
 
    for (unsigned i = 0; i < instr->def.num_components; i++) {
      outval[i] = lp_build_const_int_vec(bld->base.gallivm, int_bld->type,
-                                        bits == 8 ? instr->value[i].u8 :
-                                        bits == 16 ? instr->value[i].u32 :
-                                        bits == 32 ? instr->value[i].u32 :
-                                                     instr->value[i].u64);
+                                        nir_const_value_as_uint(instr->value[i], bits));
    }
    for (unsigned i = instr->def.num_components; i < NIR_MAX_VEC_COMPONENTS; i++) {
       outval[i] = NULL;