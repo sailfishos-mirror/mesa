f21bc439a1bf46c1721693f8f84fa74221800e26
Ian Romanick <ian.d.romanick@intel.com>
2026-02-12 17:07:45 -0800

    brw: Don't mark_invalid in update_for_reads for non-VGRF destination
    This can occur if NULL or an accumulator is an explicit destination.
    update_for_reads still needs to process the sources.
    
    v2: Pass a brw_reg to ::mark_invalid, and do the VGRF check in that one
    place.
    
    Fixes: 0d144821f0f ("intel/brw: Add a new def analysis pass")
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    (cherry picked from commit a548466186a8703264f335f626f3e1a8cbfd0df8)
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40359>

---
+++ src/intel/compiler/brw/test_def_analysis.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2026 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "test_helpers.h"
+#include "brw_builder.h"
+
+class defs_test : public brw_shader_pass_test {};
+
+TEST_F(defs_test, dst_is_acc0)
+{
+   set_gfx_verx10(125);
+
+   brw_builder bld = make_shader(MESA_SHADER_FRAGMENT, 16);
+   brw_reg acc0 = retype(brw_acc_reg(16), BRW_TYPE_F);
+   brw_reg dst0;
+
+   for (unsigned i = 0; i < 1024; i++) {
+      dst0 = vgrf(bld, BRW_TYPE_F);
+
+      if (dst0.nr == acc0.nr)
+         break;
+   }
+
+   ASSERT_EQ(dst0.nr, acc0.nr);
+
+   brw_reg src0 = vgrf(bld, BRW_TYPE_F);
+
+   brw_inst *inst = bld.MOV(dst0, brw_imm_f(1.0));
+   bld.MOV(src0, brw_imm_f(2.0));
+   bld.MAC(acc0, src0, brw_imm_f(3.0));
+
+   brw_calculate_cfg(*bld.shader);
+   brw_validate(*bld.shader);
+
+   const brw_def_analysis &defs = bld.shader->def_analysis.require();
+
+   EXPECT_EQ(inst, defs.get(dst0));
+}
+
+TEST_F(defs_test, dst_and_src_are_acc0)
+{
+   set_gfx_verx10(125);
+
+   brw_builder bld = make_shader(MESA_SHADER_FRAGMENT, 16);
+   brw_reg acc0 = retype(brw_acc_reg(16), BRW_TYPE_F);
+   brw_reg dst0;
+
+   for (unsigned i = 0; i < 1024; i++) {
+      dst0 = vgrf(bld, BRW_TYPE_F);
+
+      if (dst0.nr == acc0.nr)
+         break;
+   }
+
+   ASSERT_EQ(dst0.nr, acc0.nr);
+
+   brw_inst *inst = bld.MOV(dst0, brw_imm_f(1.0));
+   bld.MOV(acc0, brw_imm_f(2.0));
+   bld.MUL(acc0, acc0, brw_imm_f(3.0));
+
+   brw_calculate_cfg(*bld.shader);
+   brw_validate(*bld.shader);
+
+   const brw_def_analysis &defs = bld.shader->def_analysis.require();
+
+   EXPECT_EQ(inst, defs.get(dst0));
+}
