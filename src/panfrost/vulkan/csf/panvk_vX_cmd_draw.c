sailfishos-mirror/mesa

commit a4670a67e0ea5fa4ed3885d0d3bd7b008f14abf1

panvk/csf: Set the correct DCD_FLAGS_1.render_rarget_mask
Outputs may be re-mapped by VK_KHR_dynamic_rendering_local_read, in
which case outputs_written won't actually correspond to render targets.
Fortunately, we already have rt_written, which is properly remapped.

Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39274>

src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
@@ -2105,12 +2105,7 @@ prepare_dcd(struct panvk_cmd_buffer *cmdbuf,
       struct mali_dcd_flags_1_packed dcd1;
       pan_pack(&dcd1, DCD_FLAGS_1, cfg) {
          cfg.sample_mask = msaa ? dyns->ms.sample_mask : UINT16_MAX;
-
-         if (fs) {
-            cfg.render_target_mask =
-               (fs->info.outputs_written >> FRAG_RESULT_DATA0) &
-               cmdbuf->state.gfx.render.bound_attachments;
-         }
+         cfg.render_target_mask = rt_written;
       }
 
       cs_update_vt_ctx(b)

commit 75242b18623aa7316520e6e81fdb8b9c7a3ec43b

panvk: Fix dcd_flags1 dirty bit
dcd_flags1 was not counted as dirty in case the color attachment map was
updated. This could lead to an outdated value for render_target_mask.

Fixes: a4670a67e0e ("panvk/csf: Set the correct DCD_FLAGS_1.render_rarget_mask")
Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39615>

src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
@@ -1995,6 +1995,7 @@ prepare_dcd(struct panvk_cmd_buffer *cmdbuf,
                      /* line mode needs primitive topology */
                      dyn_gfx_state_dirty(cmdbuf, RS_LINE_MODE) ||
                      dyn_gfx_state_dirty(cmdbuf, IA_PRIMITIVE_TOPOLOGY) ||
+                     dyn_gfx_state_dirty(cmdbuf, COLOR_ATTACHMENT_MAP) ||
                      fs_user_dirty(cmdbuf) ||
                      gfx_state_dirty(cmdbuf, RENDER_STATE);
 