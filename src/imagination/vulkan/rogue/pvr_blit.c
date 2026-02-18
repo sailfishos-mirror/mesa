sailfishos-mirror/mesa

commit 6a65b5dd4de93ae84047ccba92a32a66c9f140a4

pvr: fix src/dst image formats for DS resolve ops
This change only addresses the clear of one channel via the TQ for DS
formats. This is exercised by VK_KHR_depth_stencil_resolve in two ways:
resolve depth and clear stencil, or resolve stencil and clear depth.

When resolving, we need to propagate source and destination format if the
DS format is combined because we need either combination of both for cases
where the DSMERGE and PICKD flags are set.

- Resolve op
  + For combined DS formats
    1. resolve the stencil from the source merging it with the depth of the
       destination. Leave source depth unchanged.
    2. resolve the depth from the source merging it with the stencil of the
       destination. Leave the source stencil untouched.
  + For non-combined formats
    1. we can use the source for all aspects / channels, this ensures the
       size to blit the source to is compatible with the destination. Note
       that the TQ doesn't require src/dst to be single channel formats.

- Non resolve op
  + Not part of this change.

Fix for deqp:
  dEQP-VK.renderpass2.depth_stencil_resolve.*.*.d24_unorm_s8_uint.compatibility*
  dEQP-VK.renderpass2.depth_stencil_resolve.*.*.d32_sfloat_s8_uint.compatibility*

Signed-off-by: Luigi Santivetti <luigi.santivetti@imgtec.com>
Co-authored-by: Leon Perianu <leon.perianu@imgtec.com>
Reviewed-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39654>

src/imagination/vulkan/rogue/pvr_blit.c
@@ -598,19 +598,20 @@ pvr_copy_or_resolve_image_region(struct pvr_cmd_buffer *cmd_buffer,
    }
 
    if (src->vk.samples > dst->vk.samples) {
-      /* Resolve op needs to know the actual format. */
-      dst_format =
-         vk_format_get_plane_aspect_format(dst->vk.format,
-                                           region->dstSubresource.aspectMask);
+      src_format = src->vk.format;
+      if (pvr_vk_format_is_combined_ds(src->vk.format) &&
+          dst->vk.format != VK_FORMAT_X8_D24_UNORM_PACK32) {
+         dst_format = dst->vk.format;
+      } else {
+         dst_format = src_format;
+      }
    } else {
       /* We don't care what format dst is as it's guaranteed to be size
        * compatible with src.
        */
-      dst_format = pvr_get_raw_copy_format(
-         vk_format_get_plane_aspect_format(src->vk.format,
-                                           region->srcSubresource.aspectMask));
+      dst_format = pvr_get_raw_copy_format(src->vk.format);
+      src_format = dst_format;
    }
-   src_format = dst_format;
 
    src_layers =
       vk_image_subresource_layer_count(&src->vk, &region->srcSubresource);

commit c489e0f45ef1b250f7c41a11ab20e63ca20d79cf

pvr: fixup for wrong conflict resolution in rebase
Caused by a bad rebase in !39654 on top of ycbcr change fa6704a523e.

Fixes: 6a65b5dd4d ("pvr: fix src/dst image formats for DS resolve ops")
Signed-off-by: Luigi Santivetti <luigi.santivetti@imgtec.com>
Reviewed-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39916>

src/imagination/vulkan/rogue/pvr_blit.c
@@ -598,7 +598,9 @@ pvr_copy_or_resolve_image_region(struct pvr_cmd_buffer *cmd_buffer,
    }
 
    if (src->vk.samples > dst->vk.samples) {
-      src_format = src->vk.format;
+      src_format =
+         vk_format_get_plane_aspect_format(src->vk.format,
+                                           region->srcSubresource.aspectMask);
       if (pvr_vk_format_is_combined_ds(src->vk.format) &&
           dst->vk.format != VK_FORMAT_X8_D24_UNORM_PACK32) {
          dst_format = dst->vk.format;
@@ -609,7 +611,10 @@ pvr_copy_or_resolve_image_region(struct pvr_cmd_buffer *cmd_buffer,
       /* We don't care what format dst is as it's guaranteed to be size
        * compatible with src.
        */
-      dst_format = pvr_get_raw_copy_format(src->vk.format);
+      dst_format =
+         pvr_get_raw_copy_format(
+            vk_format_get_plane_aspect_format(src->vk.format,
+                                              region->srcSubresource.aspectMask));
       src_format = dst_format;
    }
 