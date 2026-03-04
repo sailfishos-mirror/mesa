a83a88df91a1c877f13b5df847bdef39a142aba5
Collabora's Gfx CI Team <gfx-ci-bot-collabora@contact.daniels.if.required.fdo.invalid>
2026-03-04 00:09:32 +0000

    Uprev VVL to adfdda5b665f59aae31acf5c12c73e64a8f89553
    https://github.com/KhronosGroup/Vulkan-ValidationLayers/compare/f020266adee4bb87e8fde219f6fb31f8f141213e...adfdda5b665f59aae31acf5c12c73e64a8f89553
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40209>

--- .gitlab-ci/container/build-vulkan-validation.sh
+++ .gitlab-ci/container/build-vulkan-validation.sh
@@ -8,7 +8,7 @@ set -uex
 
 section_start vulkan-validation "Building Vulkan validation layers"
 
-VALIDATION_TAG="f020266adee4bb87e8fde219f6fb31f8f141213e"
+VALIDATION_TAG="adfdda5b665f59aae31acf5c12c73e64a8f89553"
 
 mkdir Vulkan-ValidationLayers
 pushd Vulkan-ValidationLayers
