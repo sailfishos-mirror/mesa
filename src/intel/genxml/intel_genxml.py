123e553b30ba923a93af69c982b1b122a3fedfbe
Jordan Justen <jordan.l.justen@intel.com>
2026-03-04 02:04:18 -0800

    intel/genxml: Add gen125_rt.xml to default_imports in intel_genxml.py
    This doesn't have a functional impact, but when this command is run:
    
    $ src/intel/genxml/genxml_import.py --import
    
    It causes the gen125_rt.xml file to be processed in the expected
    order, before xe2.xml.
    
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40217>

--- src/intel/genxml/intel_genxml.py
+++ src/intel/genxml/intel_genxml.py
@@ -205,6 +205,7 @@ default_imports = OrderedDict([
     ('gen110.xml', ('gen90.xml',)),
     ('gen120.xml', ('gen110.xml',)),
     ('gen125.xml', ('gen120.xml',)),
+    ('gen125_rt.xml', ()),
     ('xe2.xml', ('gen125.xml',)),
     ('xe2_rt.xml', ('gen125_rt.xml',)),
     ('xe3.xml', ('xe2.xml',)),
