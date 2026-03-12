sailfishos-mirror/mesa

commit 0988c68eeb1a075b38a010b20eeada5c566c15df

hasvk: Advertise VK_KHR_maintenance6
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37434>

src/intel/vulkan_hasvk/anv_device.c
@@ -231,6 +231,7 @@ get_device_extensions(const struct anv_physical_device *device,
       .KHR_maintenance3                      = true,
       .KHR_maintenance4                      = true,
       .KHR_maintenance5                      = true,
+      .KHR_maintenance6                      = true,
       .KHR_multiview                         = true,
       .KHR_performance_query =
          !anv_use_relocations(device) && device->perf &&
@@ -667,6 +668,9 @@ get_features(const struct anv_physical_device *pdevice,
 
       /* VK_KHR_maintenance5 */
       .maintenance5 = true,
+
+      /* VK_KHR_maintenance6 */
+      .maintenance6 = true,
    };
 
    /* We can't do image stores in vec4 shaders */
@@ -1271,6 +1275,13 @@ get_properties(const struct anv_physical_device *pdevice,
       props->polygonModePointSize = true;
       props->nonStrictSinglePixelWideLinesUseParallelogram = false;
       props->nonStrictWideLinesUseParallelogram = false;
+   }
+
+   /* VK_KHR_maintenance6 */
+   {
+      props->blockTexelViewCompatibleMultipleLayers = true;
+      props->maxCombinedImageSamplerDescriptorCount = 3;
+      props->fragmentShadingRateClampCombinerInputs = false;
    }
 }
 

commit bdf13a71859b8116d7ca2096ac6d189d708792dd

hasvk: Stop advertising blockTexelViewCompatibleMultipleLayers
This isn't supported until Gfx9.

Fixes: 0988c68eeb1a075b38a010b20eeada5c566c15df ("hasvk: Advertise VK_KHR_maintenance6")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/15042
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40335>

src/intel/vulkan_hasvk/anv_device.c
@@ -1279,7 +1279,7 @@ get_properties(const struct anv_physical_device *pdevice,
 
    /* VK_KHR_maintenance6 */
    {
-      props->blockTexelViewCompatibleMultipleLayers = true;
+      props->blockTexelViewCompatibleMultipleLayers = false;
       props->maxCombinedImageSamplerDescriptorCount = 3;
       props->fragmentShadingRateClampCombinerInputs = false;
    }