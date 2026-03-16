6da8492699bbbe2b349f7f7ae0823954d60eee70
Emma Anholt <emma@anholt.net>
2026-03-16 16:52:53 -0700

    ci/gfxreconstruct: Disable OpenXR support.
    We aren't keeping the shared lib, so gfxrecon-replay would fail due to the
    missing lib.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40959>

--- .gitlab-ci/container/build-gfxreconstruct.sh
+++ .gitlab-ci/container/build-gfxreconstruct.sh
@@ -19,7 +19,7 @@ pushd /gfxreconstruct
 git checkout "$GFXRECONSTRUCT_VERSION"
 git submodule update --init
 git submodule update
-cmake -S . -B _build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=/gfxreconstruct/build -DBUILD_WERROR=OFF
+cmake -S . -B _build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=/gfxreconstruct/build -DBUILD_WERROR=OFF -DGFXRECON_ENABLE_OPENXR=OFF
 cmake --build _build --parallel --target tools/{replay,info}/install/strip
 find . -not -path './build' -not -path './build/*' -delete
 popd
