d4c73521c5437929494b33bbd560556eeb5d0589
Mary Guillemard <mary@mary.zone>
2026-03-20 18:22:37 +0100

    nir/dead_cf: Add missing load_global_bounded handling
    Signed-off-by: Mary Guillemard <mary@mary.zone>
    Fixes: caa0854da87 ("nir: plumb load_global_bounded")
    Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
    Reviewed-by: Mel Henning <mhenning@darkrefraction.com>
    (cherry picked from commit bb6fc8cc2064580e506fcc626d56eee38de9924e)
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40752>

--- src/compiler/nir/nir_opt_dead_cf.c
+++ src/compiler/nir/nir_opt_dead_cf.c
@@ -217,6 +217,7 @@ node_is_dead(nir_cf_node *node)
             case nir_intrinsic_load_deref:
             case nir_intrinsic_load_ssbo:
             case nir_intrinsic_load_global:
+            case nir_intrinsic_load_global_bounded:
             case nir_intrinsic_load_ssbo_intel:
             case nir_intrinsic_load_ssbo_ir3:
                /* If there's a memory barrier after the loop, a load might be
