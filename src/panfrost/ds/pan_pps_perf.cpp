afaa1b6d333b15cd75730e6494cf24190a44b9a1
Christoph Pillmayer <christoph.pillmayer@arm.com>
2026-03-20 11:03:17 +0100

    pan/pps: Output counters per block
    Instead of summing counters from shader cores, and outputting only
    the counters from the first l2 slice, use the memory layout provided
    from the kmod to output individual counters for each (category, block,
    counter) combination.
    
    Co-Authored-by: Lukas Zapolskas <lukas.zapolskas@arm.com>
    Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41030>

--- src/panfrost/ds/pan_pps_perf.cpp
+++ src/panfrost/ds/pan_pps_perf.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2021 Collabora, Ltd.
+ * Copyright © 2026 Arm, Ltd.
  * SPDX-License-Identifier: MIT
  */
 
@@ -78,39 +79,98 @@ PanfrostPerf::get_subinstance() {
    return perf;
 }
 
+std::string
+format_suffix(const char *fmt, uint8_t idx)
+{
+   assert(strlen(fmt) < 200 && "fmt unreasonably long");
+   char buf[256];
+   std::snprintf(buf, sizeof(buf), fmt, idx);
+
+   return std::string(buf);
+}
+
+const char *
+get_block_suffix(uint8_t category)
+{
+   assert(category <= MALI_PERF_BLOCK_TYPE_COUNT);
+
+   switch (category) {
+   case MALI_PERF_BLOCK_MEMSYS:
+      return " (slice %u)";
+   case MALI_PERF_BLOCK_SHADER_CORE:
+      return " (core %u)";
+   default:
+      return nullptr;
+   }
+
+   return nullptr;
+}
+
+Counter::Units
+convert_pan_units(enum mali_perf_counter_units unit)
+{
+   switch (unit) {
+   case MALI_PERF_COUNTER_UNITS_PRIMITIVES:
+      return Counter::Units::Primitive;
+   case MALI_PERF_COUNTER_UNITS_INSTRUCTIONS:
+      return Counter::Units::Instruction;
+   case MALI_PERF_COUNTER_UNITS_BYTES:
+      return Counter::Units::Byte;
+   case MALI_PERF_COUNTER_UNITS_PIXELS:
+      return Counter::Units::Pixel;
+   default:
+      return Counter::Units::None;
+   }
+}
+
 std::pair<std::vector<CounterGroup>, std::vector<Counter>>
 PanfrostPerf::create_available_counters() const
 {
    std::pair<std::vector<CounterGroup>, std::vector<Counter>> ret;
    auto &[groups, counters] = ret;
 
-   size_t cid = 0;
+   uint32_t global_counter_id = 0;
 
-   for (uint32_t gid = 0; gid < perf->cfg->n_categories; ++gid) {
-      const auto &category = perf->cfg->categories[gid];
+   const struct pan_perf_category *category = NULL;
+   for (uint32_t cat_idx = 0; cat_idx < perf->cfg->n_categories; ++cat_idx) {
+      assert(cat_idx < MALI_PERF_BLOCK_TYPE_COUNT);
+      category = &perf->cfg->categories[cat_idx];
+
+      if (!category->n_counters)
+         continue;
+
+      enum mali_perf_block_type blk_type = category->counters[0].category_id;
       CounterGroup group = {};
-      group.id = gid;
-      group.name = category.name;
+      group.id = blk_type;
+      group.name = mali_perf_block_type_str(blk_type);
 
-      for (size_t id = 0; cid < category.n_counters; ++cid) {
-         Counter counter = {};
-         counter.id = cid;
-         counter.group = gid;
+      uint32_t n_blocks = mali_perf_block_count(blk_type, &perf->constants);
+      for (uint32_t counter_idx = 0; counter_idx < category->n_counters;
+           ++counter_idx) {
+         const struct pan_perf_counter *cinfo =
+            &category->counters[counter_idx];
 
-         counter.name = category.counters[id].name;
+         for (uint32_t block_idx = 0; block_idx < n_blocks; ++block_idx) {
+            const char *suffix = get_block_suffix(group.id);
+            const std::string name =
+               cinfo->name + (suffix ? format_suffix(suffix, block_idx) : "");
 
-         counter.set_getter([=](const Counter &c, const Driver &d) {
-            auto &pan_driver = PanfrostDriver::into(d);
-            struct pan_perf *perf = static_cast<struct pan_perf *>(
+            Counter counter = {};
+            counter.id = global_counter_id++;
+            counter.name = name;
+            counter.group = group.id;
+            counter.units = convert_pan_units(cinfo->units);
+
+            counter.set_getter([=](const Counter &c, const Driver &d) {
+               auto &pan_driver = PanfrostDriver::into(d);
+               struct pan_perf *perf = static_cast<struct pan_perf *>(
                   pan_driver.perf->get_subinstance());
-            const auto counter =
-               &perf->cfg->categories[gid].counters[id];
-            return pan_perf_counter_read_sum(perf, counter);
-         });
+               return pan_perf_counter_read(perf, cinfo, block_idx);
+            });
 
-         group.counters.push_back(cid++);
-
-         counters.emplace_back(counter);
+            group.counters.push_back(counter.id);
+            counters.emplace_back(counter);
+         }
       }
 
       groups.push_back(group);
