fa2750344d1ca4150e5f26fccef919c139d2b251
Emma Anholt <emma@anholt.net>
2026-03-23 12:59:09 -0700

    ci/piglit-traces: Remove ANGLE trace support.
    All consumers are now switched over to gpu-trace-replay.sh.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41115>

--- .gitlab-ci/piglit/piglit-traces.sh
+++ .gitlab-ci/piglit/piglit-traces.sh
@@ -167,16 +167,6 @@ RUN_CMD="export LD_LIBRARY_PATH=$__LD_LIBRARY_PATH; $SANITY_MESA_VERSION_CMD && 
 # run.
 rm -rf replayer-db
 
-# ANGLE: download compiled ANGLE runtime and the compiled restricted traces (all-in-one package)
-if [ -n "$PIGLIT_REPLAY_ANGLE_ARCH" ]; then
-  FILE="angle-bin-${PIGLIT_REPLAY_ANGLE_ARCH}-${ANGLE_TRACE_FILES_TAG}.tar.zst"
-  curl --location --fail --retry-all-errors --retry 4 --retry-delay 60 \
-    --header "Authorization: Bearer $(cat "${S3_JWT_FILE}")" \
-    "https://s3.freedesktop.org/mesa-tracie-private/${FILE}" --output "${FILE}"
-  mkdir -p replayer-db/angle
-  tar --zstd -xf ${FILE} -C replayer-db/angle/
-fi
-
 PIGLIT_RESULTS="${PIGLIT_RESULTS:-replay}"
 RESULTSFILE="$RESULTS_DIR/$PIGLIT_RESULTS.txt"
 mkdir -p .gitlab-ci/piglit
