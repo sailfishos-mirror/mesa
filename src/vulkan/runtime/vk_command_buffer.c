1cbb8c82b782aad1bbab4b456ad82823bee39b6e
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2026-03-23 23:04:13 +0200

    vulkan/runtime: fix missing copy image layout
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Fixes: a8e49be9d9 ("vulkan/runtime: add implementation of older entrypoints using KHR_DAC")
    Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40587>

--- src/vulkan/runtime/vk_command_buffer.c
+++ src/vulkan/runtime/vk_command_buffer.c
@@ -851,6 +851,7 @@ vk_common_CmdCopyBufferToImage2(
          .addressRowLength = pCopyBufferToImageInfo->pRegions[r].bufferRowLength,
          .addressImageHeight = pCopyBufferToImageInfo->pRegions[r].bufferImageHeight,
          .imageSubresource = pCopyBufferToImageInfo->pRegions[r].imageSubresource,
+         .imageLayout = pCopyBufferToImageInfo->dstImageLayout,
          .imageOffset = pCopyBufferToImageInfo->pRegions[r].imageOffset,
          .imageExtent = pCopyBufferToImageInfo->pRegions[r].imageExtent,
       };
@@ -891,6 +892,7 @@ vk_common_CmdCopyImageToBuffer2(
          .addressRowLength = pCopyImageToBufferInfo->pRegions[r].bufferRowLength,
          .addressImageHeight = pCopyImageToBufferInfo->pRegions[r].bufferImageHeight,
          .imageSubresource = pCopyImageToBufferInfo->pRegions[r].imageSubresource,
+         .imageLayout = pCopyImageToBufferInfo->srcImageLayout,
          .imageOffset = pCopyImageToBufferInfo->pRegions[r].imageOffset,
          .imageExtent = pCopyImageToBufferInfo->pRegions[r].imageExtent,
       };
