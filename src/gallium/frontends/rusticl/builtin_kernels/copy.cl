bc92fa4f28bbbafdd5febae0ed5538aa683144f9
Karol Herbst <kherbst@redhat.com>
2026-03-25 22:50:48 +0100

    rusticl/meta: add builtin kernels for buffer fills
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40663>

--- src/gallium/frontends/rusticl/builtin_kernels/copy.cl
+++ src/gallium/frontends/rusticl/builtin_kernels/copy.cl
@@ -1,1 +1,14 @@
+#define create_clear_buffer_kernel(size, type) \
+    __kernel void clear_buffer## size (type global* input, type clear_value) { \
+        size_t idx = get_global_id(0); \
+        input[idx] = clear_value; \
+    }
 
+create_clear_buffer_kernel(1, uchar);
+create_clear_buffer_kernel(2, ushort);
+create_clear_buffer_kernel(4, uint);
+create_clear_buffer_kernel(8, uint2);
+create_clear_buffer_kernel(16, uint4);
+create_clear_buffer_kernel(32, uint8);
+create_clear_buffer_kernel(64, uint16);
+create_clear_buffer_kernel(128, ulong16);
--- src/gallium/frontends/rusticl/core/meta.rs
+++ src/gallium/frontends/rusticl/core/meta.rs
@@ -2,12 +2,18 @@ use std::{collections::HashMap, ffi::CString, sync::Arc};
 
 use mesa_rust_util::properties::Properties;
 use rusticl_kernels::RUSTICL_BUILTIN_KERNELS_SPV;
+use rusticl_opencl_gen::*;
 
-use crate::core::{
-    context::Context,
-    device::Device,
-    kernel::Kernel,
-    program::{CompileOptions, Program},
+use crate::{
+    api::icd::CLResult,
+    core::{
+        context::Context,
+        device::Device,
+        event::EventSig,
+        kernel::{Kernel, KernelArgValue},
+        memory::Buffer,
+        program::{CompileOptions, Program},
+    },
 };
 
 pub struct Meta {
@@ -33,5 +39,65 @@ impl Meta {
         }
 
         Meta { kernels: kernels }
+    }
+
+    fn run_clear_buffer(
+        &self,
+        dev: &Device,
+        pattern: Vec<u8>,
+        size: usize,
+        mem: KernelArgValue,
+    ) -> CLResult<EventSig> {
+        let len = pattern.len();
+
+        if len > 128 || !len.is_power_of_two() {
+            return Err(CL_INVALID_VALUE);
+        }
+
+        let kernel_name = CString::new(format!("clear_buffer{len}")).unwrap();
+        let kernel = self.kernels.get(&kernel_name).unwrap();
+        kernel.launch_with_args(
+            dev,
+            3,
+            &[0, 0, 0],
+            &[size / len, 1, 1],
+            &[0, 0, 0],
+            vec![Some(mem), Some(KernelArgValue::Constant(pattern))],
+        )
+    }
+
+    /// Executes an internal helper kernel to fill `buffer` with `pattern`.
+    ///
+    /// `pattern.len()` must be a power of two value.
+    pub fn clear_buffer(
+        &self,
+        dev: &Device,
+        buffer: &Arc<Buffer>,
+        pattern: Vec<u8>,
+        offset: usize,
+        size: usize,
+    ) -> CLResult<EventSig> {
+        self.run_clear_buffer(
+            dev,
+            pattern,
+            size,
+            KernelArgValue::Buffer {
+                buffer: Arc::downgrade(buffer),
+                offset: offset,
+            },
+        )
+    }
+
+    /// Executes an internal helper kernel to fill `svm_ptr` with `pattern`.
+    ///
+    /// `pattern.len()` must be a power of two value.
+    pub fn clear_svm(
+        &self,
+        dev: &Device,
+        svm_ptr: usize,
+        pattern: Vec<u8>,
+        size: usize,
+    ) -> CLResult<EventSig> {
+        self.run_clear_buffer(dev, pattern, size, KernelArgValue::SVM(svm_ptr))
     }
 }
