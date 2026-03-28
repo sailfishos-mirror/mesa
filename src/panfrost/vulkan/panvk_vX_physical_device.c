sailfishos-mirror/mesa

commit 9ec387efb140ad8f1f70c4e8dbc8159e593eb116

panvk: advertise wsi maintenance extensions
These are already implemented by common code, so there's nothing to be
done here, really.

A few tests fail due to timeouts. But this seems no different than on
other drivers, we just skip less WSI tests than most drivers does. Skip
those for now.

Reviewed-by: Christian Gmeiner <cgmeiner@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40502>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -119,6 +119,7 @@ panvk_per_arch(get_physical_device_extensions)(
       .KHR_present_wait2 = true,
       .KHR_swapchain = true,
       .KHR_swapchain_mutable_format = true,
+      .KHR_swapchain_maintenance1 = true,
 #endif
       .KHR_synchronization2 = true,
       .KHR_timeline_semaphore = true,
@@ -194,6 +195,9 @@ panvk_per_arch(get_physical_device_extensions)(
       .EXT_shader_subgroup_ballot = true,
       .EXT_shader_subgroup_vote = true,
       .EXT_subgroup_size_control = has_vk1_1,
+#ifdef PANVK_USE_WSI_PLATFORM
+      .EXT_swapchain_maintenance1 = true,
+#endif
       .EXT_texel_buffer_alignment = true,
       .EXT_astc_decode_mode = PAN_ARCH >= 9,
       .EXT_texture_compression_astc_hdr = true,
@@ -615,6 +619,9 @@ panvk_per_arch(get_physical_device_features)(
 
       /* VK_ARM_scheduling_controls */
       .schedulingControls = PAN_ARCH >= 10,
+
+      /* KHR_swapchain_maintenance1 */
+      .swapchainMaintenance1 = true,
 
       /* VK_EXT_multisampled_render_to_single_sampled */
       .multisampledRenderToSingleSampled = true,

src/panfrost/vulkan/panvk_instance.c
@@ -100,6 +100,8 @@ static const struct vk_instance_extension_table panvk_instance_extensions = {
 #ifdef PANVK_USE_WSI_PLATFORM
    .KHR_get_surface_capabilities2 = true,
    .KHR_surface = true,
+   .KHR_surface_maintenance1 = true,
+   .EXT_surface_maintenance1 = true,
 #endif
 #ifdef VK_USE_PLATFORM_DISPLAY_KHR
    .KHR_display = true,

commit 73c9d35644595ed23faa9d35ec3d17c35e46c712

panvk: hide swapchainMaintenance1 behind WSI guard
Fixes: 9ec387efb14 ("panvk: advertise wsi maintenance extensions")
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40682>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -635,8 +635,10 @@ panvk_per_arch(get_physical_device_features)(
       /* VK_ARM_scheduling_controls */
       .schedulingControls = PAN_ARCH >= 10,
 
+#ifdef PANVK_USE_WSI_PLATFORM
       /* KHR_swapchain_maintenance1 */
       .swapchainMaintenance1 = true,
+#endif
 
       /* VK_EXT_multisampled_render_to_single_sampled */
       .multisampledRenderToSingleSampled = true,