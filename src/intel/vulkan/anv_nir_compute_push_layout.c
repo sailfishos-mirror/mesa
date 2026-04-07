sailfishos-mirror/mesa

commit a8ba682919590da0b6c47197755cf7f02ab52c71

anv: assert we haven't gone over the maximum number of push_buffers
Coverity notes that we break out of the loop walking the analysis_ranges
early if n_push_ranges >= max_push_buffers, so it notes that
n_push_ranges could already be 3 or 4 (depending on whether we're doing
mesh), and that then if we need the padding we insert another, which
would write past the end of the array.

I don't think this is actually possible in practice, but we can add an
assert to both keep coverity happy and detect that this has actually
happened.

CID: 1681478
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40147>

src/intel/vulkan/anv_nir_compute_push_layout.c
@@ -549,6 +549,7 @@ anv_nir_compute_push_layout(nir_shader *nir,
    }
 
    if (needs_padding_per_primitive) {
+      assert(n_push_ranges < max_push_buffers);
       struct anv_push_range push_constant_padding_range = {
          .set = ANV_DESCRIPTOR_SET_PER_PRIM_PADDING,
          .start = 0,

commit e382d82ca97df2290dd3da2398f0a1f4e1791856

anv: Fix assert in anv_nir_compute_push_layout
When per-primitive padding is needed, max_push_buffers is set to 3
(instead of 4) to reserve the last slot for it.

The assert was requiring `n_push_ranges < max_push_buffers`, which
incorrectly fired when the 3 ranges were used.

Fixes: a8ba6829195 ("anv: assert we haven't gone over the maximum number of push_buffers")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15155
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Dylan Baker <dylan.c.baker@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40803>

src/intel/vulkan/anv_nir_compute_push_layout.c
@@ -624,7 +624,7 @@ anv_nir_compute_push_layout(nir_shader *nir,
    }
 
    if (needs_padding_per_primitive) {
-      assert(n_push_ranges < max_push_buffers);
+      assert(n_push_ranges < ARRAY_SIZE(map->push_ranges));
       struct anv_push_range push_constant_padding_range = {
          .set = ANV_DESCRIPTOR_SET_PER_PRIM_PADDING,
          .start = 0,