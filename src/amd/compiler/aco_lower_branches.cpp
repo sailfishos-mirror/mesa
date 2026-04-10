sailfishos-mirror/mesa

commit 97f095f6e060e9a3bbddf7536a734ccb20b9eeb8

aco/lower_branches: Add try_rotate_latch_block() optimization
This optimization looks for unconditional back-edges and aims
to rotate the loop in a way that the final block is emitted
before the loop header, essentially turning

BB1:
  if ()
    goto BB3;
BB2:
  <loop body>
  goto BB1;
BB3:
  ...

into

  goto BB1;
BB2:
  <loop body>
BB1:
  if(!cond)
    goto BB2;
BB3:
  ...

Totals from 4969 (5.89% of 84383) affected shaders: (Navi48)

Instrs: 15253038 -> 15254019 (+0.01%); split: -0.00%, +0.01%
CodeSize: 81225300 -> 81227696 (+0.00%); split: -0.02%, +0.02%
Latency: 320796283 -> 320693480 (-0.03%); split: -0.03%, +0.00%
InvThroughput: 51395922 -> 51376156 (-0.04%); split: -0.04%, +0.00%
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39519>

src/amd/compiler/aco_lower_branches.cpp
@@ -555,6 +555,76 @@ try_stitch_linear_block(branch_ctx& ctx, Block& block)
    }
 }
 
+void
+try_rotate_latch_block(branch_ctx& ctx, Block& header)
+{
+   /* For now, only allow exactly one predecessor block from the loop header
+    * to become the new loop latch.
+    */
+   if (!(header.kind & block_kind_loop_latch))
+      return;
+
+   assert(header.linear_preds.size() > 1);
+   Block& block = ctx.program->blocks[header.linear_preds.back()];
+
+   if (block.instructions.empty() || block.instructions.back()->opcode != aco_opcode::s_branch)
+      return;
+
+   /* Check for all predecessors, if they could actually jump back to the loop header. */
+   for (unsigned pred_idx : block.linear_preds) {
+      Block& pred = ctx.program->blocks[pred_idx];
+      assert(pred.index < block.index);
+      if (pred.instructions.empty() || !pred.instructions.back()->isSOPP())
+         return;
+
+      SALU_instruction* branch = &pred.instructions.back()->salu();
+      aco_opcode invert = aco_opcode::num_opcodes;
+      switch (branch->opcode) {
+      case aco_opcode::s_branch: continue;
+      case aco_opcode::s_cbranch_execz: invert = aco_opcode::s_cbranch_execnz; break;
+      case aco_opcode::s_cbranch_execnz: invert = aco_opcode::s_cbranch_execz; break;
+      case aco_opcode::s_cbranch_vccz: invert = aco_opcode::s_cbranch_vccnz; break;
+      case aco_opcode::s_cbranch_vccnz: invert = aco_opcode::s_cbranch_vccz; break;
+      case aco_opcode::s_cbranch_scc0: invert = aco_opcode::s_cbranch_scc1; break;
+      case aco_opcode::s_cbranch_scc1: invert = aco_opcode::s_cbranch_scc0; break;
+      default: return;
+      }
+      assert(pred.linear_succs.size() >= 2);
+      if (pred.linear_succs[1] == block.index) {
+         assert(branch->imm == block.index);
+         continue;
+      } else if (pred.linear_succs[0] == block.index) {
+         /* Check if there is a fall-through path for the jump target. */
+         if (block.index > pred.linear_succs[1] || (pred.linear_succs[1] & block_kind_loop_latch))
+            return;
+
+         for (unsigned j = block.index + 1; j < pred.linear_succs[1]; j++) {
+            if (!ctx.program->blocks[j].instructions.empty())
+               return;
+         }
+
+         /* There can be at most one branch which falls through,
+          * so just update it directly.
+          */
+         pred.linear_succs[0] = pred.linear_succs[1];
+         pred.linear_succs[1] = block.index;
+         branch->opcode = invert;
+         branch->imm = block.index;
+      }
+   }
+
+   header.kind &= ~block_kind_loop_latch;
+   block.kind |= block_kind_loop_latch;
+   block.instructions.pop_back();
+   assert(!block.instructions.empty());
+
+   /* Insert a new branch at the loop preheader: */
+   Builder(ctx.program, &ctx.program->blocks[header.index - 1])
+      .sopp(aco_opcode::s_branch, header.index);
+
+   return;
+}
+
 } /* end namespace */
 
 void
@@ -574,8 +644,12 @@ lower_branches(Program* program)
          try_remove_simple_block(ctx, block);
    }
 
-   for (Block& block : program->blocks) {
-      try_stitch_linear_block(ctx, block);
+   for (int i = program->blocks.size() - 1; i >= 0; i--) {
+      Block& block = program->blocks[i];
+      if (block.kind & block_kind_loop_header)
+         try_rotate_latch_block(ctx, block);
+      else
+         try_stitch_linear_block(ctx, block);
    }
 }
 

commit 5e89be331f6d00cf15278c50277cce2e1b5f769b

aco/lower_branches: Fix try_rotate_latch_block()
Found by inspection.

Fixes: 97f095f6e060e9a3bbddf7536a734ccb20b9eeb8 ('aco/lower_branches: Add try_rotate_latch_block() optimization')
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40628>

src/amd/compiler/aco_lower_branches.cpp
@@ -599,7 +599,8 @@ try_rotate_latch_block(branch_ctx& ctx, Block& header)
          continue;
       } else if (pred.linear_succs[0] == block.index) {
          /* Check if there is a fall-through path for the jump target. */
-         if (block.index > pred.linear_succs[1] || (pred.linear_succs[1] & block_kind_loop_latch))
+         if (block.index > pred.linear_succs[1] ||
+             (ctx.program->blocks[pred.linear_succs[1]].kind & block_kind_loop_latch))
             return;
 
          for (unsigned j = block.index + 1; j < pred.linear_succs[1]; j++) {