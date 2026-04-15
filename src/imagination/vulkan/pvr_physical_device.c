b205c7d592d08e485b0b2deb6e1cb94141ca97db
Nick Hamilton <nick.hamilton@imgtec.com>
2026-04-15 15:24:10 +0100

    pvr: Enable shaderImageGatherExtended
    Enables the shaderImageGatherExtended feature and sets the
    {min,max}TexelGatherOffset physical device properties.
    
    The properties are queried via Zink and are expected to be non-zero.
    
    Signed-off-by: Nick Hamilton <nick.hamilton@imgtec.com>
    Reviewed-by: Frank Binns <frank.binns@imgtec.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40687>

--- src/imagination/vulkan/pvr_physical_device.c
+++ src/imagination/vulkan/pvr_physical_device.c
@@ -241,7 +241,7 @@ static void pvr_physical_device_get_supported_features(
       .vertexPipelineStoresAndAtomics = false,
       .fragmentStoresAndAtomics = false,
       .shaderTessellationAndGeometryPointSize = false,
-      .shaderImageGatherExtended = false,
+      .shaderImageGatherExtended = true,
       .shaderStorageImageExtendedFormats = true,
       .shaderStorageImageMultisample = false,
       .shaderStorageImageReadWithoutFormat = true,
@@ -666,8 +666,8 @@ static bool pvr_physical_device_get_properties(
       .maxTexelOffset = 7U,
 
       /* Requires shaderImageGatherExtended */
-      .minTexelGatherOffset = 0,
-      .maxTexelGatherOffset = 0U,
+      .minTexelGatherOffset = -8,
+      .maxTexelGatherOffset = 7U,
 
       /* Requires sampleRateShading */
       .minInterpolationOffset = -0.5f,
