74d1b6a909f0803df629dd157850810bc4210e7d
Erik Faye-Lund <erik.faye-lund@collabora.com>
2026-04-17 15:31:16 +0200

    panvk: do not enable extension without required feature
    The Vulkan spec states that if VK_ARM_shader_core_builtins is supported,
    shaderCoreBuiltins is a required feature. So let's not enable that
    extension unless we can...
    
    Fixes: dff1d91c64e ("panvk: Enable VK_ARM_shader_core_builtins")
    Reviewed-by: Daniel Stone <daniels@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40944>
    (cherry picked from commit 8cb89853b8f9993febb848ac83c5f6cf85aec11a)

--- src/panfrost/vulkan/panvk_vX_physical_device.c
+++ src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -192,7 +192,7 @@ panvk_per_arch(get_physical_device_extensions)(
       .GOOGLE_hlsl_functionality1 = true,
       .GOOGLE_user_type = true,
 
-      .ARM_shader_core_builtins = true,
+      .ARM_shader_core_builtins = PAN_ARCH >= 9,
       .ARM_shader_core_properties = has_vk1_1,
    };
 }
