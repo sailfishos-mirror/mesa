bminor/mesa-mesa

commit e9c2c3240949196a5584ed1c54df4b9bb75dcbf3

panvk: enable VK_KHR_shader_clock
CTS passing and manual tests show it's working well

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Signed-off-by: Ashley Smith <ashley.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35488>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -87,6 +87,7 @@ panvk_per_arch(get_physical_device_extensions)(
       .KHR_sampler_mirror_clamp_to_edge = true,
       .KHR_sampler_ycbcr_conversion = PAN_ARCH >= 10,
       .KHR_separate_depth_stencil_layouts = true,
+      .KHR_shader_clock = true,
       .KHR_shader_draw_parameters = true,
       .KHR_shader_expect_assume = true,
       .KHR_shader_float_controls = true,
@@ -424,6 +425,10 @@ panvk_per_arch(get_physical_device_features)(
 
       /* VK_EXT_pipeline_robustness */
       .pipelineRobustness = true,
+
+      /* VK_KHR_shader_clock */
+      .shaderSubgroupClock = device->kmod.props.gpu_can_query_timestamp,
+      .shaderDeviceClock = device->kmod.props.gpu_can_query_timestamp,
 
       /* VK_KHR_shader_float_controls2 */
       .shaderFloatControls2 = true,

sailfishos-mirror/mesa

commit c8ae72f51d5bdea0472b059d382769c3421a066a

panvk: do not enable extension without required feature
The Vulkan spec states that if VK_KHR_shader_clock is supported,
shaderSubgroupClock is a required feature. So let's not enable that
extension unless we can...

Fixes: e9c2c324094 ("panvk: enable VK_KHR_shader_clock")
Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Reviewed-by: Ashley Smith <ashley.smith@collabora.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40944>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -95,7 +95,7 @@ panvk_per_arch(get_physical_device_extensions)(
       .KHR_sampler_mirror_clamp_to_edge = true,
       .KHR_sampler_ycbcr_conversion = true,
       .KHR_separate_depth_stencil_layouts = true,
-      .KHR_shader_clock = true,
+      .KHR_shader_clock = device->kmod.dev->props.gpu_can_query_timestamp,
       .KHR_shader_draw_parameters = true,
       .KHR_shader_expect_assume = true,
       .KHR_shader_float_controls = true,