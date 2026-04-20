bminor/mesa-mesa

commit dff1d91c64e55541174141b528d90a7aeab9b0da

panvk: Enable VK_ARM_shader_core_builtins
While the shaderCoreBuiltins feature is only supported on v9+, the
extension itself provides some useful physical device properties and is
supported on all GPUs.

Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36019>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -179,6 +179,7 @@ panvk_per_arch(get_physical_device_extensions)(
       .GOOGLE_hlsl_functionality1 = true,
       .GOOGLE_user_type = true,
 
+      .ARM_shader_core_builtins = true,
       .ARM_shader_core_properties = has_vk1_1,
    };
 }
@@ -503,6 +504,9 @@ panvk_per_arch(get_physical_device_features)(
       /* VK_KHR_present_wait2 */
       .presentWait2 = true,
 #endif
+
+      /* VK_ARM_shader_core_builtins */
+      .shaderCoreBuiltins = PAN_ARCH >= 9,
    };
 }
 
@@ -962,6 +966,12 @@ panvk_per_arch(get_physical_device_properties)(
 
       /* VK_EXT_host_image_copy */
       .identicalMemoryTypeRequirements = true,
+
+      /* VK_ARM_shader_core_builtins */
+      .shaderCoreMask = device->kmod.props.shader_present,
+      .shaderCoreCount = util_bitcount(device->kmod.props.shader_present),
+      .shaderWarpsPerCore = device->kmod.props.max_threads_per_core /
+                            (pan_subgroup_size(PAN_ARCH) * 2),
    };
 
    snprintf(properties->deviceName, sizeof(properties->deviceName), "%s",

src/panfrost/vulkan/panvk_vX_shader.c
@@ -385,10 +385,12 @@ panvk_get_spirv_options(UNUSED struct vk_physical_device *vk_pdev,
 }
 
 static void
-panvk_preprocess_nir(UNUSED struct vk_physical_device *vk_pdev,
+panvk_preprocess_nir(struct vk_physical_device *vk_pdev,
                      nir_shader *nir,
                      UNUSED const struct vk_pipeline_robustness_state *rs)
 {
+   struct panvk_physical_device *pdev = to_panvk_physical_device(vk_pdev);
+
    /* Ensure to regroup output variables at the same location */
    if (nir->info.stage == MESA_SHADER_FRAGMENT)
       NIR_PASS(_, nir, nir_opt_vectorize_io_vars, nir_var_shader_out);
@@ -456,6 +458,10 @@ panvk_preprocess_nir(UNUSED struct vk_physical_device *vk_pdev,
 
    NIR_PASS(_, nir, nir_split_var_copies);
    NIR_PASS(_, nir, nir_lower_var_copies);
+
+   uint64_t core_max_id = util_last_bit(pdev->kmod.props.shader_present) - 1;
+   NIR_PASS(_, nir, nir_inline_sysval, nir_intrinsic_load_core_max_id_arm,
+            core_max_id);
 }
 
 static void

sailfishos-mirror/mesa

commit 8cb89853b8f9993febb848ac83c5f6cf85aec11a

panvk: do not enable extension without required feature
The Vulkan spec states that if VK_ARM_shader_core_builtins is supported,
shaderCoreBuiltins is a required feature. So let's not enable that
extension unless we can...

Fixes: dff1d91c64e ("panvk: Enable VK_ARM_shader_core_builtins")
Reviewed-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40944>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -222,7 +222,7 @@ panvk_per_arch(get_physical_device_extensions)(
 
       .VALVE_mutable_descriptor_type = PAN_ARCH >= 9,
 
-      .ARM_shader_core_builtins = true,
+      .ARM_shader_core_builtins = PAN_ARCH >= 9,
       .ARM_shader_core_properties = has_vk1_1,
       .ARM_scheduling_controls = PAN_ARCH >= 10,
    };