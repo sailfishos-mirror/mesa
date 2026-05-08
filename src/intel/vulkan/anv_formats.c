alexvorxx/zink-xlib-termux

commit c94cd1235f436098d3b0a27ad5d13b9d3ac830c3

anv: implement VK_EXT_image_compression_control
Limited to vkd3d right now, there are specific use cases there.

We don't want any app to disable compression, it should be mostly
transparent and we better be aware of potential bugs.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28632>

src/intel/vulkan/anv_formats.c
@@ -1285,6 +1285,7 @@ anv_get_image_format_properties(
    VkSamplerYcbcrConversionImageFormatProperties *ycbcr_props = NULL;
    VkAndroidHardwareBufferUsageANDROID *android_usage = NULL;
    VkTextureLODGatherFormatPropertiesAMD *texture_lod_gather_props = NULL;
+   VkImageCompressionPropertiesEXT *comp_props = NULL;
    bool from_wsi = false;
 
    /* Extract input structs */
@@ -1308,6 +1309,9 @@ anv_get_image_format_properties(
       case VK_STRUCTURE_TYPE_VIDEO_PROFILE_LIST_INFO_KHR:
          /* Ignore but don't warn */
          break;
+      case VK_STRUCTURE_TYPE_IMAGE_COMPRESSION_CONTROL_EXT:
+         /* Ignore but don't warn */
+         break;
       default:
          anv_debug_ignored_stype(s->sType);
          break;
@@ -1328,6 +1332,9 @@ anv_get_image_format_properties(
          break;
       case VK_STRUCTURE_TYPE_TEXTURE_LOD_GATHER_FORMAT_PROPERTIES_AMD:
          texture_lod_gather_props = (void *) s;
+         break;
+      case VK_STRUCTURE_TYPE_IMAGE_COMPRESSION_PROPERTIES_EXT:
+         comp_props = (void *) s;
          break;
       default:
          anv_debug_ignored_stype(s->sType);
@@ -1744,6 +1751,18 @@ anv_get_image_format_properties(
       }
    }
 
+   if (comp_props) {
+      bool ccs_supported =
+         anv_formats_ccs_e_compatible(devinfo, info->flags, info->format,
+                                      info->tiling, info->usage,
+                                      format_list_info);
+      comp_props->imageCompressionFixedRateFlags =
+         VK_IMAGE_COMPRESSION_FIXED_RATE_NONE_EXT;
+      comp_props->imageCompressionFlags = ccs_supported ?
+         VK_IMAGE_COMPRESSION_DEFAULT_EXT :
+         VK_IMAGE_COMPRESSION_DISABLED_EXT;
+   }
+
    return VK_SUCCESS;
 
 unsupported:
@@ -1845,7 +1864,8 @@ void anv_GetPhysicalDeviceSparseImageFormatProperties2(
       isl_surf_usage_flags_t isl_usage =
          anv_image_choose_isl_surf_usage(physical_device,
                                          vk_create_flags, pFormatInfo->usage,
-                                         0, aspect);
+                                         0, aspect,
+                                         VK_IMAGE_COMPRESSION_DEFAULT_EXT);
 
       const enum isl_surf_dim isl_surf_dim =
          pFormatInfo->type == VK_IMAGE_TYPE_1D ? ISL_SURF_DIM_1D :

src/intel/vulkan/anv_device.c
@@ -91,6 +91,7 @@ static const driOptionDescription anv_dri_options[] = {
       DRI_CONF_ANV_FORCE_INDIRECT_DESCRIPTORS(false)
       DRI_CONF_SHADER_SPILLING_RATE(0)
       DRI_CONF_OPT_B(intel_tbimr, true, "Enable TBIMR tiled rendering")
+      DRI_CONF_ANV_COMPRESSION_CONTROL_ENABLED(false)
    DRI_CONF_SECTION_END
 
    DRI_CONF_SECTION_DEBUG
@@ -362,6 +363,7 @@ get_device_extensions(const struct anv_physical_device *device,
       .EXT_graphics_pipeline_library         = !debug_get_bool_option("ANV_NO_GPL", false),
       .EXT_host_query_reset                  = true,
       .EXT_image_2d_view_of_3d               = true,
+      .EXT_image_compression_control         = device->instance->compression_control_enabled,
       .EXT_image_robustness                  = true,
       .EXT_image_drm_format_modifier         = true,
       .EXT_image_sliced_view_of_3d           = true,
@@ -923,6 +925,9 @@ get_features(const struct anv_physical_device *pdevice,
       /* VK_EXT_swapchain_maintenance1 */
       .swapchainMaintenance1 = true,
 #endif
+
+      /* VK_EXT_image_compression_control */
+      .imageCompressionControl = true,
    };
 
    /* The new DOOM and Wolfenstein games require depthBounds without
@@ -2628,6 +2633,8 @@ anv_init_dri_options(struct anv_instance *instance)
             driQueryOptionb(&instance->dri_options, "anv_disable_fcv");
     instance->external_memory_implicit_sync =
             driQueryOptionb(&instance->dri_options, "anv_external_memory_implicit_sync");
+    instance->compression_control_enabled =
+       driQueryOptionb(&instance->dri_options, "compression_control_enabled");
 }
 
 VkResult anv_CreateInstance(

src/intel/vulkan/anv_image.c
@@ -207,7 +207,8 @@ anv_image_choose_isl_surf_usage(struct anv_physical_device *device,
                                 VkImageCreateFlags vk_create_flags,
                                 VkImageUsageFlags vk_usage,
                                 isl_surf_usage_flags_t isl_extra_usage,
-                                VkImageAspectFlagBits aspect)
+                                VkImageAspectFlagBits aspect,
+                                VkImageCompressionFlagsEXT comp_flags)
 {
    isl_surf_usage_flags_t isl_usage = isl_extra_usage;
 
@@ -282,6 +283,9 @@ anv_image_choose_isl_surf_usage(struct anv_physical_device *device,
        * formats differently. */
       isl_usage |= ISL_SURF_USAGE_RENDER_TARGET_BIT;
    }
+
+   if (comp_flags & VK_IMAGE_COMPRESSION_DISABLED_EXT)
+      isl_usage |= ISL_SURF_USAGE_DISABLE_AUX_BIT;
 
    return isl_usage;
 }
@@ -1324,7 +1328,8 @@ add_all_surfaces_implicit_layout(
       isl_surf_usage_flags_t isl_usage =
          anv_image_choose_isl_surf_usage(device->physical,
                                          image->vk.create_flags, vk_usage,
-                                         isl_extra_usage_flags, aspect);
+                                         isl_extra_usage_flags, aspect,
+                                         image->vk.compr_flags);
 
       result = add_primary_surface(device, image, plane, plane_format,
                                    ANV_OFFSET_IMPLICIT, plane_stride,
@@ -1722,7 +1727,8 @@ anv_image_init(struct anv_device *device, struct anv_image *image,
 
       isl_surf_usage_flags_t isl_usage = anv_image_choose_isl_surf_usage(
          device->physical, image->vk.create_flags, image->vk.usage,
-         isl_extra_usage_flags, VK_IMAGE_ASPECT_COLOR_BIT);
+         isl_extra_usage_flags, VK_IMAGE_ASPECT_COLOR_BIT,
+         image->vk.compr_flags);
 
       r = add_primary_surface(device, image, plane, plane_format,
                               ANV_OFFSET_IMPLICIT, 0,
@@ -2645,6 +2651,20 @@ anv_get_image_subresource_layout(const struct anv_image *image,
          image->vk.extent.depth;
    } else {
       layout->subresourceLayout.size = mem_range->size;
+   }
+
+   VkImageCompressionPropertiesEXT *comp_props =
+      vk_find_struct(layout->pNext, IMAGE_COMPRESSION_PROPERTIES_EXT);
+   if (comp_props) {
+      comp_props->imageCompressionFixedRateFlags =
+         VK_IMAGE_COMPRESSION_FIXED_RATE_NONE_EXT;
+      comp_props->imageCompressionFlags = VK_IMAGE_COMPRESSION_DISABLED_EXT;
+      for (uint32_t p = 0; p < image->n_planes; p++) {
+         if (image->planes[p].aux_usage != ISL_AUX_USAGE_NONE) {
+            comp_props->imageCompressionFlags = VK_IMAGE_COMPRESSION_DEFAULT_EXT;
+            break;
+         }
+      }
    }
 }
 

src/intel/vulkan/anv_private.h
@@ -1270,6 +1270,7 @@ struct anv_instance {
     unsigned                                    force_vk_vendor;
     bool                                        has_fake_sparse;
     bool                                        disable_fcv;
+    bool                                        compression_control_enabled;
 
     /* HW workarounds */
     bool                                        no_16bit;
@@ -5491,7 +5492,8 @@ anv_image_choose_isl_surf_usage(struct anv_physical_device *device,
                                 VkImageCreateFlags vk_create_flags,
                                 VkImageUsageFlags vk_usage,
                                 isl_surf_usage_flags_t isl_extra_usage,
-                                VkImageAspectFlagBits aspect);
+                                VkImageAspectFlagBits aspect,
+                                VkImageCompressionFlagsEXT comp_flags);
 
 void
 anv_cmd_buffer_fill_area(struct anv_cmd_buffer *cmd_buffer,

src/util/00-mesa-defaults.conf
@@ -1262,6 +1262,10 @@ TODO: document the other workarounds.
             <option name="anv_disable_fcv" value="true" />
             <option name="anv_assume_full_subgroups" value="16" />
         </engine>
+        <!-- We trust vkd3d to not disable compression all the time. -->
+        <engine engine_name_match="vkd3d">
+            <option name="compression_control_enabled" value="true" />
+        </engine>
     </device>
     <device driver="dzn">
         <application name="DOOMEternal" executable="DOOMEternalx64vk.exe">

src/util/driconf.h
@@ -787,6 +787,9 @@
 #define DRI_CONF_ANV_EXTERNAL_MEMORY_IMPLICIT_SYNC(def) \
    DRI_CONF_OPT_B(anv_external_memory_implicit_sync, def, "Implicit sync on external BOs")
 
+#define DRI_CONF_ANV_COMPRESSION_CONTROL_ENABLED(def) \
+   DRI_CONF_OPT_B(compression_control_enabled, def, "Enable VK_EXT_image_compression_control support")
+
 /**
  * \brief HASVK specific configuration options
  */

sailfishos-mirror/mesa

commit 7094ad91e326b2113157fd7f87f0b165d71e6eef

anv: implement missing device image property compression filtering
We want to avoid reporting support for disabling compression with
compressed drm modifier.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: c94cd1235f ("anv: implement VK_EXT_image_compression_control")
Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40429>

src/intel/vulkan/anv_formats.c
@@ -744,6 +744,7 @@ anv_get_depth_stencil_format_features(const struct anv_physical_device *physical
                                       const struct anv_format *anv_format,
                                       const VkImageTiling vk_tiling,
                                       const VkImageAspectFlags aspects,
+                                      VkImageCompressionFlagsEXT comp_flags,
                                       const struct isl_drm_modifier_info *isl_mod_info)
 {
    const struct intel_device_info *devinfo = &physical_device->info;
@@ -761,6 +762,11 @@ anv_get_depth_stencil_format_features(const struct anv_physical_device *physical
          return 0;
 
       if (devinfo->ver <= 12 &&
+          isl_drm_modifier_has_aux(isl_mod_info->modifier))
+         return 0;
+
+      /* Don't support compression disabling with AUX modifier */
+      if ((comp_flags & VK_IMAGE_COMPRESSION_DISABLED_EXT) &&
           isl_drm_modifier_has_aux(isl_mod_info->modifier))
          return 0;
    }
@@ -801,6 +807,7 @@ anv_get_color_format_features(const struct anv_physical_device *physical_device,
                               const VkImageTiling vk_tiling,
                               VkImageUsageFlags usage,
                               VkImageCreateFlags create_flags,
+                              VkImageCompressionFlagsEXT comp_flags,
                               const struct isl_drm_modifier_info *isl_mod_info)
 {
    const struct intel_device_info *devinfo = &physical_device->info;
@@ -1049,6 +1056,11 @@ anv_get_color_format_features(const struct anv_physical_device *physical_device,
                                                          isl_mod_info))
          return 0;
 
+      /* Don't support compression disabling with AUX modifier */
+      if ((comp_flags & VK_IMAGE_COMPRESSION_DISABLED_EXT) &&
+          isl_drm_modifier_has_aux(isl_mod_info->modifier))
+         return 0;
+
       /* For simplicity, keep DISJOINT disabled for multi-planar format. */
       if (anv_format->n_planes > 1)
          flags &= ~VK_FORMAT_FEATURE_2_DISJOINT_BIT;
@@ -1106,6 +1118,7 @@ anv_get_image_format_features2(const struct anv_physical_device *physical_device
                                VkImageTiling vk_tiling,
                                VkImageUsageFlags usage,
                                VkImageCreateFlags create_flags,
+                               VkImageCompressionFlagsEXT comp_flags,
                                const struct isl_drm_modifier_info *isl_mod_info)
 {
    const struct intel_device_info *devinfo = &physical_device->info;
@@ -1125,6 +1138,9 @@ anv_get_image_format_features2(const struct anv_physical_device *physical_device
    }
 
    if (anv_is_compressed_format_emulated(physical_device, vk_format)) {
+      /* comp_flags is ignored in this case since we always disable
+       * compression for emulation.
+       */
       return anv_get_compressed_emulated_format_features(anv_format,
                                                          vk_tiling);
    }
@@ -1134,13 +1150,14 @@ anv_get_image_format_features2(const struct anv_physical_device *physical_device
    if (aspects & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT)) {
       return anv_get_depth_stencil_format_features(physical_device,
                                                    anv_format, vk_tiling,
-                                                   aspects, isl_mod_info);
+                                                   aspects, comp_flags,
+                                                   isl_mod_info);
    }
 
    assert(aspects & VK_IMAGE_ASPECT_ANY_COLOR_BIT_ANV);
    return anv_get_color_format_features(physical_device, anv_format,
                                         vk_tiling, usage, create_flags,
-                                        isl_mod_info);
+                                        comp_flags, isl_mod_info);
 }
 
 static VkFormatFeatureFlags2
@@ -1216,6 +1233,7 @@ get_drm_format_modifier_properties_list(const struct anv_physical_device *physic
          anv_get_image_format_features2(physical_device, anv_format,
                                         VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT,
                                         0 /* usage */, 0 /* create_flags */,
+                                        VK_IMAGE_COMPRESSION_DEFAULT_EXT,
                                         isl_mod_info);
       VkFormatFeatureFlags features = vk_format_features2_to_features(features2);
       if (!features)
@@ -1257,6 +1275,7 @@ get_drm_format_modifier_properties_list_2(const struct anv_physical_device *phys
          anv_get_image_format_features2(physical_device, anv_format,
                                         VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT,
                                         0 /* usage */, 0 /* create_flags */,
+                                        VK_IMAGE_COMPRESSION_DEFAULT_EXT,
                                         isl_mod_info);
       if (!features2)
          continue;
@@ -1290,11 +1309,15 @@ void anv_GetPhysicalDeviceFormatProperties2(
    linear2 = anv_get_image_format_features2(physical_device, anv_format,
                                             VK_IMAGE_TILING_LINEAR,
                                             0 /* usage */,
-                                            0 /* create_flags */, NULL);
+                                            0 /* create_flags */,
+                                            VK_IMAGE_COMPRESSION_DEFAULT_EXT,
+                                            NULL);
    optimal2 = anv_get_image_format_features2(physical_device, anv_format,
                                              VK_IMAGE_TILING_OPTIMAL,
                                              0 /* usage */,
-                                             0 /* create_flags */, NULL);
+                                             0 /* create_flags */,
+                                             VK_IMAGE_COMPRESSION_DEFAULT_EXT,
+                                             NULL);
    buffer2 = get_buffer_format_features2(physical_device, vk_format, anv_format);
 
    pFormatProperties->formatProperties = (VkFormatProperties) {
@@ -1515,6 +1538,7 @@ anv_formats_gather_format_features(
                   anv_get_image_format_features2(physical_device,
                                                  possible_anv_format, tiling,
                                                  usage, create_flags,
+                                                 VK_IMAGE_COMPRESSION_DEFAULT_EXT,
                                                  isl_mod_info);
                all_formats_feature_flags |= view_format_features;
             }
@@ -1533,6 +1557,7 @@ anv_formats_gather_format_features(
          VkFormatFeatureFlags2 view_format_features =
             anv_get_image_format_features2(physical_device, anv_view_format,
                                            tiling, usage, create_flags,
+                                           VK_IMAGE_COMPRESSION_DEFAULT_EXT,
                                            isl_mod_info);
          all_formats_feature_flags |= view_format_features;
       }
@@ -1598,6 +1623,7 @@ anv_get_image_format_properties(
    const VkPhysicalDeviceImageDrmFormatModifierInfoEXT *modifier_info = NULL;
    const VkImageFormatListCreateInfo *format_list_info = NULL;
    const VkPhysicalDeviceExternalImageFormatInfo *external_info = NULL;
+   const VkImageCompressionControlEXT *comp_info = NULL;
    VkExternalImageFormatProperties *external_props = NULL;
    VkSamplerYcbcrConversionImageFormatProperties *ycbcr_props = NULL;
    VkTextureLODGatherFormatPropertiesAMD *texture_lod_gather_props = NULL;
@@ -1628,7 +1654,7 @@ anv_get_image_format_properties(
          /* Ignore but don't warn */
          break;
       case VK_STRUCTURE_TYPE_IMAGE_COMPRESSION_CONTROL_EXT:
-         /* Ignore but don't warn */
+         comp_info = (const void *)s;
          break;
       default:
          vk_debug_ignored_stype(s->sType);
@@ -1748,6 +1774,9 @@ anv_get_image_format_properties(
                                                          info->tiling,
                                                          info->usage,
                                                          info->flags,
+                                                         comp_info ?
+                                                         comp_info->flags :
+                                                         VK_IMAGE_COMPRESSION_DEFAULT_EXT,
                                                          isl_mod_info);
 
    if (!anv_format_supports_usage(format_feature_flags, info->usage)) {