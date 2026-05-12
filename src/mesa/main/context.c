aerisarn/mesa-uwp

commit 37190ad932a8e7e67c69343249e895313790ef7b

mesa: drop texformat code this isn't used.
v2: dropped comment (Tapani)

Reviewed-by: Kristian H. Kristensen <hoegsberg@google.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14286>

src/mesa/main/context.c
@@ -1061,8 +1061,7 @@ _mesa_initialize_context(struct gl_context *ctx,
 
    /* Mesa core handles all the formats that mesa core knows about.
     * Drivers will want to override this list with just the formats
-    * they can handle, and confirm that appropriate fallbacks exist in
-    * _mesa_choose_tex_format().
+    * they can handle.
     */
    memset(&ctx->TextureFormatSupported, GL_TRUE,
           sizeof(ctx->TextureFormatSupported));

src/mesa/main/texformat.h
@@ -1,38 +1,0 @@
-/*
- * Mesa 3-D graphics library
- *
- * Copyright (C) 1999-2008  Brian Paul   All Rights Reserved.
- * Copyright (c) 2008-2009 VMware, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
-
-#ifndef TEXFORMAT_H
-#define TEXFORMAT_H
-
-
-#include "formats.h"
-
-struct gl_context;
-
-extern mesa_format
-_mesa_choose_tex_format(struct gl_context *ctx, GLenum target,
-                        GLint internalFormat, GLenum format, GLenum type);
-
-#endif

src/mesa/meson.build
@@ -224,8 +224,6 @@ files_libmesa = files(
   'main/texcompress_s3tc.h',
   'main/texenv.c',
   'main/texenvprogram.h',
-  'main/texformat.c',
-  'main/texformat.h',
   'main/texgen.c',
   'main/texgetimage.c',
   'main/texgetimage.h',

src/mesa/state_tracker/st_cb_drawpixels.c
@@ -43,7 +43,6 @@
 #include "main/pbo.h"
 #include "main/readpix.h"
 #include "main/state.h"
-#include "main/texformat.h"
 #include "main/teximage.h"
 #include "main/texstore.h"
 #include "main/glformats.h"

sailfishos-mirror/mesa

commit 4916cee43968cbd86588aa5caa3552df5818d1c1

mesa/main: remove unused array
The values here are only ever set to GL_TRUE, and never ever changed, so
let's just rip it out.

Fixes: 37190ad932a ("mesa: drop texformat code this isn't used.")
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38656>

src/mesa/main/context.c
@@ -1004,13 +1004,6 @@ _mesa_initialize_context(struct gl_context *ctx,
 
    _mesa_reset_vertex_processing_mode(ctx);
 
-   /* Mesa core handles all the formats that mesa core knows about.
-    * Drivers will want to override this list with just the formats
-    * they can handle.
-    */
-   memset(&ctx->TextureFormatSupported, GL_TRUE,
-          sizeof(ctx->TextureFormatSupported));
-
    switch (ctx->API) {
    case API_OPENGL_COMPAT:
    case API_OPENGL_CORE:

src/mesa/main/glformats.c
@@ -4176,7 +4176,7 @@ _mesa_tex_format_from_format_and_type(const struct gl_context *ctx,
    if (_mesa_format_is_mesa_array_format(format))
       format = _mesa_format_from_array_format(format);
       
-   if (format == MESA_FORMAT_NONE || !ctx->TextureFormatSupported[format])
+   if (format == MESA_FORMAT_NONE)
       return MESA_FORMAT_NONE;
 
    return format;

src/mesa/main/mtypes.h
@@ -3582,8 +3582,6 @@ struct gl_context
     */
    GLboolean HasConfig;
 
-   GLboolean TextureFormatSupported[MESA_FORMAT_COUNT];
-
    GLboolean RasterDiscard;  /**< GL_RASTERIZER_DISCARD */
    GLboolean IntelConservativeRasterization; /**< GL_CONSERVATIVE_RASTERIZATION_INTEL */
    GLboolean ConservativeRasterization; /**< GL_CONSERVATIVE_RASTERIZATION_NV */