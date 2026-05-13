9b33ba5a7392a4d2f82769f8cd74a9d2ba118812
Faith Ekstrand <faith.ekstrand@collabora.com>
2026-05-13 13:57:19 -0400

    kraid: Add a GPU model abstraction
    This is sort of our version of pan_model.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41841>

--- src/panfrost/compiler/kraid/compile.rs
+++ src/panfrost/compiler/kraid/compile.rs
@@ -1,15 +1,18 @@
 // Copyright © 2026 Collabora, Ltd.
 // SPDX-License-Identifier: MIT
 
+use crate::model::model_for_gpu_id;
 use compiler::bindings::*;
 use kraid_bindings::*;
 
 #[no_mangle]
 pub extern "C" fn kraid_compile_nir(
     _nir: &mut nir_shader,
-    _inputs: &pan_compile_inputs,
+    inputs: &pan_compile_inputs,
     _binary: &mut util_dynarray,
     _info: &mut pan_shader_info,
 ) {
+    let _model = model_for_gpu_id(inputs.gpu_id).unwrap();
+
     todo!("Implement a compiler");
 }
---
+++ src/panfrost/compiler/kraid/model.rs
@@ -0,0 +1,31 @@
+// Copyright © 2026 Collabora, Ltd.
+// SPDX-License-Identifier: MIT
+
+use kraid_bindings::*;
+
+pub trait Model {
+    fn arch(&self) -> u8;
+}
+
+struct ValhallModel {
+    arch: u8,
+}
+
+impl Model for ValhallModel {
+    fn arch(&self) -> u8 {
+        self.arch
+    }
+}
+
+pub fn model_for_gpu_id(gpu_id: u64) -> Result<Box<dyn Model>, &'static str> {
+    // SAFETY: pan_arch() just translates one integer to another
+    let arch = u8::try_from(unsafe { pan_arch(gpu_id) }).unwrap();
+
+    if arch >= 15 {
+        Err("Kraid does not yet support this GPU")
+    } else if arch >= 9 {
+        Ok(Box::new(ValhallModel { arch }))
+    } else {
+        Err("Kraid only supports Valhall (v9) and later GPUs")
+    }
+}
