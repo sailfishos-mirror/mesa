5415e6779c2cd8e85e92e733dbdaf4e8029d1f47
Emma Anholt <emma@anholt.net>
2026-05-14 09:13:42 -0700

    weston: Run at a more reasonable 1920x1080 resolution, not 1024x640.
    The headless backend's defaults are quite low, and it was preventing
    renderdoc replays from running any larger than that, which in turn cut off
    the screenshots we wanted to take.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41662>

--- .gitlab-ci/common/weston.sh
+++ .gitlab-ci/common/weston.sh
@@ -6,7 +6,12 @@ mkdir -p /tmp/.X11-unix
 export DISPLAY=:0
 
 WAYLAND_DISPLAY=wayland-0
-weston --config="$CI_COMMON_DIR/weston.ini" --socket="$WAYLAND_DISPLAY" --log "$RESULTS_DIR/weston.log" --logger-scopes=log,xwm-wm-x11 "$@" &
+weston --config="$CI_COMMON_DIR/weston.ini" \
+  --socket="$WAYLAND_DISPLAY" \
+  --log "$RESULTS_DIR/weston.log" \
+  --logger-scopes=log,xwm-wm-x11 \
+  --width 1920 --height 1080 \
+  "$@" &
 export WAYLAND_DISPLAY
 
 while [ ! -S /tmp/.X11-unix/X0 ]; do sleep 1; done
