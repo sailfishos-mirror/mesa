sailfishos-mirror/mesa

commit 465c186fc5f72c51bda943ac0e19f6512f8e6262

anv: Prepare for format width changes in blorp_copy()
blorp_copy() will soon gain the ability to increase the format bpb.
Prepare anv by replicating the clear color pixel on gfx12.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39974>

src/intel/vulkan/anv_image.c
@@ -1959,7 +1959,8 @@ anv_image_init(struct anv_device *device, struct anv_image *image,
             blorp_copy_get_color_format(&device->isl_dev, image_format);
          add_image_view_format(image, blorp_copy_format);
 
-         if (vk_format_is_color_depth_stencil_capable(image->vk.format))
+         if ((device->info->ver == 12 && image->vk.samples == 1) ||
+             vk_format_is_color_depth_stencil_capable(image->vk.format))
             add_image_view_format(image, ISL_FORMAT_RAW);
       } else {
          /* We don't have a blorp_copy format query for depth-stencil formats. */

src/intel/vulkan/anv_blorp.c
@@ -311,9 +311,9 @@ get_blorp_surf_for_anv_image(const struct anv_cmd_buffer *cmd_buffer,
    }
 
    const bool is_dest = usage & VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+   const bool is_depth = aspect & VK_IMAGE_ASPECT_DEPTH_BIT;
    isl_surf_usage_flags_t isl_usage =
-      get_usage_flag_for_cmd_buffer(cmd_buffer, is_dest,
-                                    aspect & VK_IMAGE_ASPECT_DEPTH_BIT,
+      get_usage_flag_for_cmd_buffer(cmd_buffer, is_dest, is_depth,
                                     anv_image_is_protected(image));
    const struct anv_surface *surface = &image->planes[plane].primary_surface;
    const struct anv_address address =
@@ -344,11 +344,20 @@ get_blorp_surf_for_anv_image(const struct anv_cmd_buffer *cmd_buffer,
          };
       }
 
+      enum isl_format fmt_override = view_fmt;
+      if (cross_aspect && !is_depth) {
+         fmt_override = ISL_FORMAT_RAW;
+      } else if (!is_dest && device->info->ver == 12 &&
+                 !anv_image_view_formats_incomplete(image) &&
+                 isl_aux_usage_has_ccs_e(aux_usage)) {
+         fmt_override = ISL_FORMAT_RAW;
+      }
+
       const struct anv_address clear_color_addr =
          anv_image_get_clear_color_addr(
-            device, image, cross_aspect ? ISL_FORMAT_RAW : view_fmt, aspect,
-            !is_dest);
+            device, image, fmt_override, aspect, !is_dest);
       blorp_surf->clear_color_addr = anv_to_blorp_address(clear_color_addr);
+      blorp_surf->has_replicated_pixel = fmt_override == ISL_FORMAT_RAW;
 
       if (aspect & VK_IMAGE_ASPECT_ANY_COLOR_BIT_ANV) {
          blorp_surf->clear_color =

src/intel/vulkan/anv_private.h
@@ -5991,6 +5991,8 @@ anv_image_get_clear_color_addr(UNUSED const struct anv_device *device,
       }
    }
 
+   /* Don't fail silently for the special raw format. */
+   assert(view_format != ISL_FORMAT_RAW);
    assert(anv_image_view_formats_incomplete(image));
    return anv_address_add(base_addr, access_offset);
 }

src/intel/vulkan/genX_cmd_buffer.c
@@ -933,13 +933,19 @@ set_image_clear_color(struct anv_cmd_buffer *cmd_buffer,
                       const VkImageAspectFlags aspect,
                       const uint32_t *pixel)
 {
+   uint8_t replicated_pixel[16];
+   int pixel_size_B = vk_format_get_blocksize(image->vk.format);
+   for (int p = 0; p < 16 / pixel_size_B; p++)
+      memcpy(&replicated_pixel[p * pixel_size_B], pixel, pixel_size_B);
+
    for (int i = 0; i < image->num_view_formats; i++) {
       union isl_color_value clear_color;
       if (image->view_formats[i] == ISL_FORMAT_RAW) {
-         /* Only used for cross aspect copies (color <-> depth/stencil) */
-         assert(vk_format_is_color_depth_stencil_capable(image->vk.format));
-         assert(vk_format_get_blocksizebits(image->vk.format) <= 32);
-         memcpy(clear_color.u32, pixel, sizeof(clear_color.u32));
+         /* Used for cross aspect copies (color <-> depth/stencil) on all
+          * platforms and for blorp_copy() sources on gfx12 (see
+          * blorp_surf::has_replicated_pixel).
+          */
+         memcpy(clear_color.u32, replicated_pixel, sizeof(clear_color.u32));
       } else {
          isl_color_value_unpack(&clear_color, image->view_formats[i], pixel);
       }
@@ -966,8 +972,8 @@ set_image_clear_color(struct anv_cmd_buffer *cmd_buffer,
       dw[6] = clear_color.u32[3];
       dw[7] = 0;
       dw[8] = 0;
-      dw[3 + sampler_offset / 4] = pixel[0];
-      dw[4 + sampler_offset / 4] = pixel[1];
+      dw[3 + sampler_offset / 4] = ((uint32_t *)replicated_pixel)[0];
+      dw[4 + sampler_offset / 4] = ((uint32_t *)replicated_pixel)[1];
 #else
       assert(cmd_buffer->device->isl_dev.ss.clear_color_state_size == 0);
       assert(cmd_buffer->device->isl_dev.ss.clear_value_size == 16);

commit ec40c9538565ab2eb373d72e454f507d85f7d267

anv: Add transfer_src usage for ANDROID_external_format_resolve
The android extension enables the driver to blit from single-sampled
color attachments.

Adding this image usage expressess that functionality and causes anv to
generate the ISL_FORMAT_RAW-formatted clear color during fast-clears.
This fixes an assert failure when anv tries to override the clear color
format used for a blorp_blit() call to ISL_FORMAT_RAW.

There are other ways to handle this, but this solution is consistent
with our handling of multisample images (which may be resolved as well).

Fixes: 465c186fc5f ("anv: Prepare for format width changes in blorp_copy()")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/15463
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41650>

src/intel/vulkan/anv_image.c
@@ -1655,13 +1655,14 @@ choose_drm_format_mod(const struct anv_physical_device *device,
 }
 
 static VkImageUsageFlags
-anv_image_create_usage(const VkImageCreateInfo *pCreateInfo,
+anv_image_create_usage(const struct anv_device *device,
+                       const VkImageCreateInfo *pCreateInfo,
                        VkImageUsageFlags usage)
 {
-   /* Add TRANSFER_SRC usage for multisample attachment images. This is
-    * because we might internally use the TRANSFER_SRC layout on them for
-    * blorp operations associated with resolving those into other attachments
-    * at the end of a subpass.
+   /* Add TRANSFER_SRC usage for some attachments. This is because we might
+    * internally use the TRANSFER_SRC layout on them for blorp operations
+    * associated with resolving those into other attachments at the end of a
+    * subpass.
     *
     * Without this additional usage, we compute an incorrect AUX state in
     * anv_layout_to_aux_state().
@@ -1670,6 +1671,12 @@ anv_image_create_usage(const VkImageCreateInfo *pCreateInfo,
        (usage & (VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
                  VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT)))
       usage |= VK_IMAGE_USAGE_TRANSFER_SRC_BIT;
+
+   if (device->vk.enabled_extensions.ANDROID_external_format_resolve &&
+       pCreateInfo->samples == VK_SAMPLE_COUNT_1_BIT &&
+       (usage & VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT))
+      usage |= VK_IMAGE_USAGE_TRANSFER_SRC_BIT;
+
    return usage;
 }
 
@@ -1855,9 +1862,10 @@ anv_image_init(struct anv_device *device, struct anv_image *image,
 
    vk_image_init(&device->vk, &image->vk, pCreateInfo);
 
-   image->vk.usage = anv_image_create_usage(pCreateInfo, image->vk.usage);
+   image->vk.usage =
+      anv_image_create_usage(device, pCreateInfo, image->vk.usage);
    image->vk.stencil_usage =
-      anv_image_create_usage(pCreateInfo, image->vk.stencil_usage);
+      anv_image_create_usage(device, pCreateInfo, image->vk.stencil_usage);
 
    isl_surf_usage_flags_t isl_extra_usage_flags =
       create_info->isl_extra_usage_flags;