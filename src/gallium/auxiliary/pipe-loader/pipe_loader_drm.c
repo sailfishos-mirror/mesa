7a6bff8b39d05dcffab0528877b66c7fddc2c692
Icenowy Zheng <zhengxingda@iscas.ac.cn>
2026-05-22 01:27:02 +0800

    pipe-loader: use zink for powervr device nodes
    The Mesa PowerVR driver is Vulkan only, and Zink is used solely for
    APIs implemented by Gallium.
    
    Change the driver name to "zink" if the device node's driver is
    "powervr".
    
    Backport-to: 26.1
    Signed-off-by: Icenowy Zheng <zhengxingda@iscas.ac.cn>
    Acked-by: Frank Binns <frank.binns@imgtec.com>
    (cherry picked from commit 19d786ae6ae635a17c6fd217ef7b569763618d28)
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41985>

--- src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
+++ src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
@@ -150,6 +150,12 @@ pipe_loader_drm_probe_fd_nodup(struct pipe_loader_device **dev, int fd, bool zin
       ddev->base.driver_name = strdup("radeonsi");
    }
 
+   /* powervr has no Gallium driver and relies on Zink */
+   if (strcmp(ddev->base.driver_name, "powervr") == 0) {
+      FREE(ddev->base.driver_name);
+      ddev->base.driver_name = strdup("zink");
+   }
+
    if (strcmp(ddev->base.driver_name, "virtio_gpu") == 0) {
       struct virgl_renderer_capset_drm caps;
       if (get_nctx_caps(fd, &caps) == 0) {
