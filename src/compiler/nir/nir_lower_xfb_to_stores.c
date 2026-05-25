9a80fe85f0614fdfcd83b1bc152dc8c466ba7491
Lorenzo Rossi <lorenzo.rossi@collabora.com>
2026-05-25 18:51:53 +0200

    nir/lower_xfb_to_stores: Support non-zero store_output component
    Signed-off-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
    Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
    Acked-by: Eric R. Smith <eric.smith@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41578>

--- src/compiler/nir/nir_lower_xfb_to_stores.c
+++ src/compiler/nir/nir_lower_xfb_to_stores.c
@@ -35,7 +35,6 @@ lower_xfb_output(nir_builder *b, nir_intrinsic_instr *intr,
    unsigned address_bit_size = nir_address_format_bit_size(address_format);
 
    assert(buffer < MAX_XFB_BUFFERS);
-   assert(nir_intrinsic_component(intr) == 0); // TODO
 
    /* Transform feedback info in units of words, convert to bytes. */
    uint16_t stride = b->shader->info.xfb_stride[buffer] * 4;
@@ -58,7 +57,8 @@ lower_xfb_output(nir_builder *b, nir_intrinsic_instr *intr,
 
    nir_def *src = intr->src[0].ssa;
    nir_component_mask_t mask = nir_component_mask(num_components);
-   nir_def *value = nir_channels(b, src, mask << start_component);
+   mask = (mask << start_component) >> nir_intrinsic_component(intr);
+   nir_def *value = nir_channels(b, src, mask);
    nir_store_global(b, value, addr);
 }
 
