009af15ae2b2fdd60038cb1b4b0b1f595179939c
Simon Perretta <simon.perretta@imgtec.com>
2026-05-26 14:05:26 +0100

    pvr: advertise VK_EXT_shader_subgroup_ballot and ballot feature
    Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
    Acked-by: Frank Binns <frank.binns@imgtec.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41833>

--- src/imagination/vulkan/pvr_physical_device.c
+++ src/imagination/vulkan/pvr_physical_device.c
@@ -203,6 +203,7 @@ static void pvr_physical_device_get_supported_extensions(
       .EXT_separate_stencil_usage = true,
       .EXT_shader_demote_to_helper_invocation = true,
       .EXT_shader_replicated_composites = true,
+      .EXT_shader_subgroup_ballot = true,
       .EXT_texel_buffer_alignment = false,
       .EXT_tooling_info = true,
       .EXT_vertex_attribute_divisor = true,
@@ -732,7 +733,8 @@ static bool pvr_physical_device_get_properties(
       /* Vulkan 1.1 */
       .subgroupSize = ROGUE_MAX_INSTANCES_PER_TASK,
       .subgroupSupportedStages = VK_SHADER_STAGE_COMPUTE_BIT,
-      .subgroupSupportedOperations = VK_SUBGROUP_FEATURE_BASIC_BIT,
+      .subgroupSupportedOperations = VK_SUBGROUP_FEATURE_BASIC_BIT |
+                                     VK_SUBGROUP_FEATURE_BALLOT_BIT,
       .subgroupQuadOperationsInAllStages = false,
       .protectedNoFault = false,
 
