e962477875b402cc762f073d81c03170c19f8deb
Caio Oliveira <caio.oliveira@intel.com>
2026-05-29 07:28:07 -0700

    Revert "intel/dev: Remove unused intel_get_device_info_for_build() function"
    This reverts commit 0cc89ca03ad99796d73a3bf16b74b98ca820f64c.
    
    This allows to use PCI IDs without having to set INTEL_FORCE_PROBE environment
    variable.  Useful for tests and tools, I'm assuming the original user of it is gone
    or out-of-tree.
    
    Acked-by: Jordan Justen <jordan.l.justen@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41413>

--- src/intel/dev/intel_device_info.c
+++ src/intel/dev/intel_device_info.c
@@ -1703,6 +1703,13 @@ intel_get_device_info_from_pci_id(int pci_id,
 }
 
 bool
+intel_get_device_info_for_build(int pci_id,
+                                struct intel_device_info *devinfo)
+{
+   return intel_get_device_info_from_pci_id_common(pci_id, true, devinfo);
+}
+
+bool
 intel_device_info_compute_system_memory(struct intel_device_info *devinfo, bool update)
 {
    if (!update) {
