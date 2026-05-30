8711a88d965f14708e5df73550d94c905d65c85d
Karol Herbst <kherbst@redhat.com>
2026-05-30 18:33:41 +0200

    rusticl/util: add Traits to help with usage of CString
    Reviewed-by: @LingMan
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41905>

--- src/gallium/frontends/rusticl/util/string.rs
+++ src/gallium/frontends/rusticl/util/string.rs
@@ -1,7 +1,8 @@
 // Copyright 2020 Red Hat.
 // SPDX-License-Identifier: MIT
 
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
+use std::mem;
 use std::os::raw::c_char;
 
 #[allow(clippy::not_unsafe_ptr_arg_deref)]
@@ -26,4 +27,76 @@ pub unsafe fn char_arr_to_cstr(c_str: &[c_char]) -> &CStr {
     // there is another nul within the slice.
     debug_assert_eq!(c_str.last().copied(), Some(b'\0' as c_char));
     unsafe { CStr::from_ptr(c_str.as_ptr()) }
+}
+
+pub trait CStringExt {
+    fn push_cstr(&mut self, other: &CStr);
+}
+
+impl CStringExt for CString {
+    fn push_cstr(&mut self, other: &CStr) {
+        let mut buffer = mem::take(self).into_bytes();
+        buffer.extend(other.to_bytes_with_nul());
+
+        // SAFETY: The only 0 byte in buffer is at the end
+        *self = unsafe { CString::from_vec_with_nul_unchecked(buffer) };
+    }
+}
+
+pub trait Join<Separator> {
+    type Output;
+
+    fn join(&self, sep: Separator) -> Self::Output;
+}
+
+impl Join<&CStr> for [CString] {
+    type Output = CString;
+
+    fn join(&self, sep: &CStr) -> Self::Output {
+        let Some((first, rest)) = self.split_first() else {
+            return CString::default();
+        };
+
+        let size = self.iter().map(|v| v.count_bytes()).sum::<usize>()
+            + sep.count_bytes() * (self.len() - 1)
+            + 1;
+
+        let mut buffer = Vec::with_capacity(size);
+        buffer.extend(first.to_bytes());
+
+        let sep = sep.to_bytes();
+        for string in rest {
+            buffer.extend_from_slice(sep);
+            buffer.extend_from_slice(string.to_bytes());
+        }
+
+        // SAFETY: There are no interior 0 bytes in buffer
+        unsafe { CString::from_vec_unchecked(buffer) }
+    }
+}
+
+#[test]
+fn test_push_cstr() {
+    let mut owned = c"SomeString".to_owned();
+    owned.push_cstr(c"MoreString");
+    assert_eq!(owned, c"SomeStringMoreString".into());
+}
+
+#[test]
+fn test_join_c_strings() {
+    assert_eq!(Join::join([].as_slice(), c";"), c"".into());
+    assert_eq!([c"test".to_owned()].join(c";"), c"test".into());
+    assert_eq!(
+        [c"String".to_owned(), c"AnotherString".to_owned()].join(c";"),
+        c"String;AnotherString".into()
+    );
+    assert_eq!(
+        [
+            c"String".to_owned(),
+            c"AnotherString".to_owned(),
+            c"Testing".to_owned()
+        ]
+        .join(c"###"),
+        c"String###AnotherString###Testing".into(),
+    );
 }
