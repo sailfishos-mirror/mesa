171f379e0ad1e9575d637547c1b6bf7fc23e2d00
Icenowy Zheng <zhengxingda@iscas.ac.cn>
2026-05-31 21:56:32 +0800

    zink: check Z/S aspect before creating Z/S image view
    When moving the code of creating image views to the init_sampler_view()
    function, the check for Z/S aspect bits was forgotten to be added
    (because it was a big if gating a lot of code originally).
    
    When the driver doesn't have needs_zs_shader_swizzle set, this is not
    problematic, because the condition for creating Z/S view is to have only
    Z aspect; however the needs_zs_shader_swizzle case now fails because Z/S
    views are now created for color images.
    
    Fix the issue by re-adding the Z/S aspect check before checking
    needs_zs_shader_swizzle flag.
    
    Fixes: cafa22142bf1 ("zink: create views for samplers lazily")
    Signed-off-by: Icenowy Zheng <zhengxingda@iscas.ac.cn>
    (cherry picked from commit d55418dae753c8daacf66ab81a7481094808576a)
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41985>

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -1170,7 +1170,8 @@ init_sampler_view(struct zink_context *ctx, struct zink_sampler_view *sv)
 
    bool red_depth_sampler_view = false;
    if (sv->ivci.subresourceRange.aspectMask == VK_IMAGE_ASPECT_DEPTH_BIT ||
-       screen->driver_compiler_workarounds.needs_zs_shader_swizzle) {
+       ((sv->ivci.subresourceRange.aspectMask & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT)) &&
+        screen->driver_compiler_workarounds.needs_zs_shader_swizzle)) {
       VkComponentSwizzle *swizzle = (VkComponentSwizzle*)&sv->ivci.components;
       for (unsigned i = 0; i < 4; i++) {
          if (swizzle[i] == VK_COMPONENT_SWIZZLE_ONE ||
