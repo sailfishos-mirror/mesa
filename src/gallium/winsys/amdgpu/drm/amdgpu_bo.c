6ae0114b0588da0ff43a4aa7aa8709ad61a2725e
Gleb Popov <6yearold@gmail.com>
2026-05-31 20:55:30 +0300

    Rename the CACHE_LINE_SIZE define to MESA_CACHE_LINE_SIZE
    The former clashes with a define with the same name that comes from FreeBSD
    base headers.
    
    Closes #5737
    
    Reviewed-by: Marek Olšák <maraeo@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41906>

--- src/gallium/winsys/amdgpu/drm/amdgpu_bo.c
+++ src/gallium/winsys/amdgpu/drm/amdgpu_bo.c
@@ -861,7 +861,7 @@ struct pb_slab *amdgpu_bo_slab_alloc(void *priv, unsigned heap, unsigned entry_s
    slab_bo->slab.group_index = group_index;
    slab_bo->slab.entry_size = entry_size;
    slab_bo->entries = os_malloc_aligned(slab_bo->slab.num_entries * sizeof(*slab_bo->entries),
-                                        CACHE_LINE_SIZE);
+                                        MESA_CACHE_LINE_SIZE);
    if (!slab_bo->entries)
       goto fail;
 
--- src/vulkan/vram-report-limit-layer/vram_report_limit_layer.c
+++ src/vulkan/vram-report-limit-layer/vram_report_limit_layer.c
@@ -447,7 +447,7 @@ vram_report_limit_CreateInstance(const VkInstanceCreateInfo *pCreateInfo,
    struct vram_report_limit_instance_data *instance_data = os_malloc_aligned(
       sizeof(*instance_data) + sizeof(instance_data->active_pdevices_array[0]) *
                                   active_pdevices_count,
-      CACHE_LINE_SIZE);
+      MESA_CACHE_LINE_SIZE);
    if (instance_data == NULL) {
       result = VK_ERROR_OUT_OF_HOST_MEMORY;
       goto err_free_is_pdevice_active_array;
