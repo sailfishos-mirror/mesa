cc02826f4e35ae9d3e93659db6e0815d534c0fcf
Karmjit Mahil <karmjit.mahil@igalia.com>
2026-06-01 15:47:18 +0100

    docs: Fix struct redefinition errors
    Signed-off-by: Karmjit Mahil <karmjit.mahil@igalia.com>
    Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41933>

--- docs/header-stubs/vk_dispatch_table.h
+++ docs/header-stubs/vk_dispatch_table.h
@@ -1,1 +1,6 @@
+#ifndef STUB_VK_DISPLATCH_TABLE_H
+#define STUB_VK_DISPLATCH_TABLE_H
+
 struct vk_instance_dispatch_table {};
+
+#endif /* STUB_VK_DISPLATCH_TABLE_H */
--- docs/header-stubs/vk_extensions.h
+++ docs/header-stubs/vk_extensions.h
@@ -1,2 +1,7 @@
+#ifndef STUB_VK_EXTENSIONS_H
+#define STUB_VK_EXTENSIONS_H
+
 struct vk_instance_extension_table {};
 struct vk_instance_entrypoint_table {};
+
+#endif /* STUB_VK_EXTENSIONS_H */
