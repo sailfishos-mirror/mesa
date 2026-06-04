b1c829a8af590736bd93787a7e21f078ae1184d7
Faith Ekstrand <faith.ekstrand@collabora.com>
2026-06-04 14:50:18 -0400

    compiler/rust/as_slice: Document AsSlice
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42200>

--- src/compiler/rust/as_slice.rs
+++ src/compiler/rust/as_slice.rs
@@ -4,11 +4,20 @@
 use std::ops::Deref;
 use std::ops::DerefMut;
 
+/// A trait which indicates that the given type may be viewed as a slice of
+/// (some of) its fields.
 pub trait AsSlice<T> {
+    /// The type of field attributes.  If no field attributes are needed, this
+    /// can be `()`.
     type Attr;
 
+    /// Returns a slice representing the fields of type `T`.
     fn as_slice(&self) -> &[T];
+
+    /// Returns a mut slice representing the fields of type `T`.
     fn as_mut_slice(&mut self) -> &mut [T];
+
+    /// Returns a slice containing the field attributes for fields of type T.
     fn attrs(&self) -> &'static [Self::Attr];
 }
 
