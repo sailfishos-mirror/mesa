bminor/mesa-mesa

commit de30bfd5b84535faeb3392173714cc4088c31abd

nir/loop_analyze: ignore terminating induction variable in guess_loop_limit()
The array access might be using a different induction variable.

Totals from 23 (0.03% of 79395) affected shaders: (Navi31)
Instrs: 113742 -> 121017 (+6.40%)
CodeSize: 592152 -> 636228 (+7.44%)
Latency: 439244 -> 426784 (-2.84%)
InvThroughput: 36264 -> 35199 (-2.94%)
SClause: 3048 -> 3426 (+12.40%)
Copies: 10630 -> 10733 (+0.97%)
Branches: 3774 -> 4310 (+14.20%)
PreSGPRs: 1683 -> 1696 (+0.77%)
PreVGPRs: 1230 -> 1232 (+0.16%)
VALU: 51026 -> 55912 (+9.58%)
SALU: 15270 -> 15638 (+2.41%)
SMEM: 4456 -> 5149 (+15.55%)

Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33131>

src/compiler/nir/nir_loop_analyze.c
@@ -447,11 +447,10 @@ find_array_access_via_induction(loop_info_state *state,
    return 0;
 }
 
-static bool
-guess_loop_limit(loop_info_state *state, nir_const_value *limit_val,
-                 nir_scalar basic_ind)
+static unsigned
+guess_loop_limit(loop_info_state *state)
 {
-   unsigned min_array_size = 0;
+   unsigned min_array_size = UINT_MAX;
 
    nir_foreach_block_in_cf_node(block, &state->loop->cf_node) {
       nir_foreach_instr(instr, block) {
@@ -470,12 +469,8 @@ guess_loop_limit(loop_info_state *state, nir_const_value *limit_val,
                find_array_access_via_induction(state,
                                                nir_src_as_deref(intrin->src[0]),
                                                &array_idx);
-            if (array_idx && basic_ind.def == array_idx->def &&
-                (min_array_size == 0 || min_array_size > array_size)) {
-               /* Array indices are scalars */
-               assert(basic_ind.def->num_components == 1);
-               min_array_size = array_size;
-            }
+            if (array_idx)
+               min_array_size = MIN2(min_array_size, array_size);
 
             if (intrin->intrinsic != nir_intrinsic_copy_deref)
                continue;
@@ -484,23 +479,16 @@ guess_loop_limit(loop_info_state *state, nir_const_value *limit_val,
                find_array_access_via_induction(state,
                                                nir_src_as_deref(intrin->src[1]),
                                                &array_idx);
-            if (array_idx && basic_ind.def == array_idx->def &&
-                (min_array_size == 0 || min_array_size > array_size)) {
-               /* Array indices are scalars */
-               assert(basic_ind.def->num_components == 1);
-               min_array_size = array_size;
-            }
+            if (array_idx)
+               min_array_size = MIN2(min_array_size, array_size);
          }
       }
    }
 
-   if (min_array_size) {
-      *limit_val = nir_const_value_for_uint(min_array_size,
-                                            basic_ind.def->bit_size);
-      return true;
-   }
-
-   return false;
+   if (min_array_size != UINT_MAX)
+      return min_array_size;
+   else
+      return 0;
 }
 
 static nir_op invert_comparison_if_needed(nir_op alu_op, bool invert);
@@ -1194,7 +1182,11 @@ find_trip_count(loop_info_state *state, unsigned execution_mode,
 
          if (!try_find_limit_of_alu(limit, &limit_val, alu_op, invert_cond, terminator, state)) {
             /* Guess loop limit based on array access */
-            if (!guess_loop_limit(state, &limit_val, basic_ind)) {
+            unsigned guessed_loop_limit = guess_loop_limit(state);
+            if (guessed_loop_limit) {
+               limit_val = nir_const_value_for_uint(guessed_loop_limit,
+                                                    basic_ind.def->bit_size);
+            } else {
                terminator->exact_trip_count_unknown = true;
                continue;
             }

sailfishos-mirror/mesa

commit 14a6aa3c15e797f1458b08658384b8438cbf25ca

nir: use the correct induction var when guessing loop iterations
Previously when loop analyze was guessing loop iterations based
on array/sample access, we incorrectly used the induction variable of
the loop terminator to calculate loop iterations, rather than
the induction variable that accesses the array/sample.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15550
Fixes: de30bfd5b845 ("nir/loop_analyze: ignore terminating induction variable in guess_loop_limit()")

Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42038>

src/compiler/nir/nir_loop_analyze.c
@@ -407,7 +407,8 @@ find_array_access_via_induction(loop_info_state *state,
 }
 
 static void
-guess_loop_limit_by_intrinsic(loop_info_state *state, nir_intrinsic_instr *intrin, unsigned *min_loop_count)
+guess_loop_limit_by_intrinsic(loop_info_state *state, nir_intrinsic_instr *intrin,
+                              nir_scalar *ind, unsigned *min_loop_count)
 {
    /* Check for arrays variably-indexed by a loop induction variable. */
    if (intrin->intrinsic == nir_intrinsic_load_deref ||
@@ -419,27 +420,40 @@ guess_loop_limit_by_intrinsic(loop_info_state *state, nir_intrinsic_instr *intri
          find_array_access_via_induction(state,
                                          nir_src_as_deref(intrin->src[0]),
                                          &array_idx);
-      if (array_idx)
-         *min_loop_count = MIN2(*min_loop_count, array_size);
+      if (array_idx) {
+         if (*min_loop_count > array_size) {
+            nir_scalar lp_ind = { array_idx->basis, 0 };
+            *ind = lp_ind;
+            *min_loop_count = array_size;
+         }
+      }
 
       if (intrin->intrinsic == nir_intrinsic_copy_deref) {
          array_size =
             find_array_access_via_induction(state,
                                             nir_src_as_deref(intrin->src[1]),
                                             &array_idx);
-         if (array_idx)
-            *min_loop_count = MIN2(*min_loop_count, array_size);
+         if (array_idx) {
+            if (*min_loop_count > array_size) {
+               nir_scalar lp_ind = { array_idx->basis, 0 };
+               *ind = lp_ind;
+               *min_loop_count = array_size;
+            }
+         }
       }
    }
 }
 
 static void
-guess_loop_limit_by_tex(loop_info_state *state, nir_tex_instr *tex, unsigned *min_loop_count)
+guess_loop_limit_by_tex(loop_info_state *state, nir_tex_instr *tex,
+                        nir_scalar *ind, unsigned *min_loop_count)
 {
    nir_def *sample = nir_get_tex_src(tex, nir_tex_src_ms_index);
    if (sample) {
       nir_loop_induction_variable *var = get_loop_var(sample, state);
       if (var) {
+         nir_scalar lp_ind = { var->basis, 0 };
+         *ind = lp_ind;
          const nir_function_impl *impl = nir_cf_node_get_function(&state->loop->cf_node);
          const nir_shader_compiler_options *options = impl->function->shader->options;
          *min_loop_count = MIN2(*min_loop_count, options->max_samples);
@@ -448,7 +462,7 @@ guess_loop_limit_by_tex(loop_info_state *state, nir_tex_instr *tex, unsigned *mi
 }
 
 static unsigned
-guess_loop_limit(loop_info_state *state)
+guess_loop_limit(loop_info_state *state, nir_scalar *ind)
 {
    unsigned min_loop_count = UINT_MAX;
 
@@ -456,10 +470,12 @@ guess_loop_limit(loop_info_state *state)
       nir_foreach_instr(instr, block) {
          switch (instr->type) {
          case nir_instr_type_intrinsic:
-            guess_loop_limit_by_intrinsic(state, nir_instr_as_intrinsic(instr), &min_loop_count);
+            guess_loop_limit_by_intrinsic(state, nir_instr_as_intrinsic(instr),
+                                          ind, &min_loop_count);
             break;
          case nir_instr_type_tex:
-            guess_loop_limit_by_tex(state, nir_instr_as_tex(instr), &min_loop_count);
+            guess_loop_limit_by_tex(state, nir_instr_as_tex(instr), ind,
+                                    &min_loop_count);
             break;
          default:
             break;
@@ -1130,6 +1146,7 @@ find_trip_count(loop_info_state *state, unsigned execution_mode,
 
       bool limit_rhs;
       nir_scalar basic_ind = { NULL, 0 };
+      nir_scalar terminator_ind = { NULL, 0 };
       nir_scalar limit;
 
       if ((alu_op == nir_op_inot || alu_op == nir_op_ieq || alu_op == nir_op_ior) &&
@@ -1183,8 +1200,12 @@ find_trip_count(loop_info_state *state, unsigned execution_mode,
          trip_count_known = false;
 
          if (!try_find_limit(limit, &limit_val, alu_op, invert_cond, terminator, state)) {
-            /* Guess loop limit based on array access */
-            unsigned guessed_loop_limit = guess_loop_limit(state);
+            /* Guess loop limit based on array access, and get the induction
+             * var from the array access which is not necessarily the same
+             * induction var used by the loop terminator.
+             */
+            terminator_ind = basic_ind;
+            unsigned guessed_loop_limit = guess_loop_limit(state, &basic_ind);
             if (guessed_loop_limit) {
                limit_val = nir_const_value_for_uint(guessed_loop_limit,
                                                     basic_ind.def->bit_size);
@@ -1268,13 +1289,27 @@ find_trip_count(loop_info_state *state, unsigned execution_mode,
       }
       nir_const_value step_val = nir_scalar_as_const_value(alu_s);
 
-      int iterations = calculate_iterations(nir_get_scalar(lv->basis, basic_ind.comp), limit,
-                                            initial_val, step_val, limit_val,
-                                            step_alu, cond,
-                                            alu_op, limit_rhs,
-                                            invert_cond,
-                                            execution_mode,
-                                            max_unroll_iterations);
+      int iterations;
+      if (guessed_trip_count && terminator_ind.def != basic_ind.def) {
+         /* TODO: expand to support additional step combinations where needed */
+         if (step_alu->op != nir_op_iadd ||
+             (nir_const_value_as_int(step_val, basic_ind.def->bit_size) <= 0) ||
+             (nir_const_value_as_int(initial_val, basic_ind.def->bit_size) < 0)) {
+            iterations = -1;
+         } else {
+            iterations = get_iteration(nir_op_ilt, initial_val, step_val,
+                                       limit_val, false,
+                                       basic_ind.def->bit_size, execution_mode);
+         }
+      } else {
+         iterations = calculate_iterations(nir_get_scalar(lv->basis, basic_ind.comp), limit,
+                                           initial_val, step_val, limit_val,
+                                           step_alu, cond,
+                                           alu_op, limit_rhs,
+                                           invert_cond,
+                                           execution_mode,
+                                           max_unroll_iterations);
+      }
 
       /* Where we not able to calculate the iteration count */
       if (iterations == -1) {

src/compiler/nir/tests/loop_analyze_tests.cpp
@@ -40,6 +40,7 @@ nir_loop_analyze_test::nir_loop_analyze_test()
    static nir_shader_compiler_options options = { };
 
    options.max_unroll_iterations = 32;
+   options.max_samples = 8;
 
    b = nir_builder_init_simple_shader(MESA_SHADER_VERTEX, &options,
                                       "loop analyze");
@@ -2009,3 +2010,135 @@ TWO_TERMINATOR_INEXACT_COUNT_TEST(input_unsigned_limit_j_limit, false, true, fal
  *    }
  */
 TWO_TERMINATOR_INEXACT_COUNT_TEST(input_unsigned_j_limit2, false, true, true, false, uge, 4, 30, 6, 4)
+
+static nir_loop *
+loop_builder_guessed_trip_count(nir_builder *b, bool array_access)
+{
+   nir_def *unknown_limit =
+      nir_load_input(b, 1, 32, nir_imm_int(b, 0));
+
+   nir_def *zero = nir_imm_int(b, 0);
+   nir_def *one = nir_imm_int(b, 1);
+   nir_def *four = nir_imm_int(b, 4);
+
+   nir_variable *var =
+      nir_local_variable_create(b->impl,
+                                glsl_array_type(glsl_float_type(), 8, 0),
+                                "tmp");
+
+   nir_phi_instr *phi = nir_phi_instr_create(b->shader);
+   nir_phi_instr *phi2 = nir_phi_instr_create(b->shader);
+
+   nir_loop *loop = nir_push_loop(b);
+   {
+      /* Loop terminator induction variable */
+      nir_def_init(&phi->instr, &phi->def, 1, 32);
+      nir_phi_instr_add_src(phi, nir_def_block(zero), zero);
+
+      /* Array/sample access induction variable */
+      nir_def_init(&phi2->instr, &phi2->def, 1, 32);
+      nir_phi_instr_add_src(phi2, nir_def_block(zero), zero);
+
+      nir_def *i = &phi->def;
+      nir_def *i_arr = &phi2->def;
+
+      nir_def *cond = nir_ige(b, i, unknown_limit);
+
+      nir_if *nif = nir_push_if(b, cond);
+      {
+         nir_jump_instr *jump =
+            nir_jump_instr_create(b->shader, nir_jump_break);
+         nir_builder_instr_insert(b, &jump->instr);
+      }
+      nir_pop_if(b, nif);
+
+      if (array_access) {
+         nir_deref_instr *var_deref = nir_build_deref_var(b, var);
+         nir_deref_instr *arr_deref =
+            nir_build_deref_array(b, var_deref, i_arr);
+
+         nir_store_deref(b, arr_deref, nir_imm_float(b, 1.0f), 0x1);
+      } else {
+         nir_tex_instr *tex = nir_tex_instr_create(b->shader, 1);
+         nir_def_init(&tex->instr, &tex->def, 4, 32);
+         tex->op = nir_texop_txf_ms;
+         tex->sampler_dim = GLSL_SAMPLER_DIM_MS;
+         tex->coord_components = 2;
+
+         tex->src[0].src_type = nir_tex_src_ms_index;
+         tex->src[0].src = nir_src_for_ssa(i_arr);
+
+         nir_builder_instr_insert(b, &tex->instr);
+      }
+
+      /* Increment i by 1 and array index/sample by 4 */
+      nir_def *next = nir_iadd(b, i, one);
+      nir_def *next_idx = nir_iadd(b, i_arr, four);
+
+      nir_phi_instr_add_src(phi, nir_def_block(next), next);
+      nir_phi_instr_add_src(phi2, nir_def_block(next_idx), next_idx);
+   }
+   nir_pop_loop(b, loop);
+
+   b->cursor = nir_before_block(nir_loop_first_block(loop));
+   nir_builder_instr_insert(b, &phi->instr);
+   nir_builder_instr_insert(b, &phi2->instr);
+
+   return loop;
+}
+
+TEST_F(nir_loop_analyze_test, guessed_trip_count_array_access)
+{
+   /* Test that we use the correct induction variable when calculating the
+    * guessed trip count based on array access.
+    *
+    *    in uint unknown_limit;
+    *    uint i = 0;
+    *    uint iarr = 0;
+    *    float tmp[8];
+    *    while (true) {
+    *       if (i >= unknown_limit)
+    *          break;
+    *
+    *       tmp[i_arr] = 1.0;
+    *       i++;
+    *       i_arr += 4;
+    *    }
+    */
+   nir_loop *loop = loop_builder_guessed_trip_count(&b, true);
+
+   nir_validate_shader(b.shader, "input");
+
+   nir_loop_analyze_impl(b.impl, nir_var_all, false);
+
+   ASSERT_NE((void *)0, loop->info);
+
+   EXPECT_FALSE(loop->info->exact_trip_count_known);
+   EXPECT_EQ(2u, loop->info->guessed_trip_count);
+
+   EXPECT_EQ((void *)0, loop->info->limiting_terminator);
+   EXPECT_EQ(0, loop->info->max_trip_count);
+
+   ASSERT_NE((void *)0, loop->info->induction_vars);
+   EXPECT_GE(_mesa_hash_table_num_entries(loop->info->induction_vars), 4);
+}
+
+TEST_F(nir_loop_analyze_test, guessed_trip_count_sampler_access)
+{
+   nir_loop *loop = loop_builder_guessed_trip_count(&b, false);
+
+   nir_validate_shader(b.shader, "input");
+
+   nir_loop_analyze_impl(b.impl, nir_var_all, false);
+
+   ASSERT_NE((void *)0, loop->info);
+
+   EXPECT_FALSE(loop->info->exact_trip_count_known);
+   EXPECT_EQ(2u, loop->info->guessed_trip_count);
+
+   EXPECT_EQ((void *)0, loop->info->limiting_terminator);
+   EXPECT_EQ(0, loop->info->max_trip_count);
+
+   ASSERT_NE((void *)0, loop->info->induction_vars);
+   EXPECT_GE(_mesa_hash_table_num_entries(loop->info->induction_vars), 4);
+}