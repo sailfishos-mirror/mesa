38a98a48035aa25d6187fcd01eb69d28b0f98fbd
yserrr <dlwognsdc610@naver.com>
2026-06-10 09:40:54 +0000

    v3d: remove duplicate util_blitter_save_so_targets() call
    v3d_blitter_save() saves the stream output targets twice.
    
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42139>

--- src/gallium/drivers/v3d/v3d_blit.c
+++ src/gallium/drivers/v3d/v3d_blit.c
@@ -59,8 +59,6 @@ v3d_blitter_save(struct v3d_context *v3d, enum v3d_blitter_op op)
         util_blitter_save_depth_stencil_alpha(v3d->blitter, v3d->zsa);
         util_blitter_save_stencil_ref(v3d->blitter, &v3d->stencil_ref);
         util_blitter_save_sample_mask(v3d->blitter, v3d->sample_mask, 0);
-        util_blitter_save_so_targets(v3d->blitter, v3d->streamout.num_targets,
-                                     v3d->streamout.targets, MESA_PRIM_UNKNOWN);
 
         if (op & V3D_SAVE_FRAMEBUFFER)
                 util_blitter_save_framebuffer(v3d->blitter, &v3d->framebuffer);
