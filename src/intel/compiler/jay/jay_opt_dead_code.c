6a694c20a45e04e6e72b8ea4f2972df98e00399d
Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
2026-06-15 18:37:30 -0400

    jay/opt_dead_code: handle phi properly
    loop to handle loops properly and fix the pass order to do forward edges in O(1)
    passes.
    
    No shaderdb changes.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42287>

--- src/intel/compiler/jay/jay_opt_dead_code.c
+++ src/intel/compiler/jay/jay_opt_dead_code.c
@@ -12,53 +12,60 @@
 static void
 pass(jay_function *f)
 {
-   BITSET_WORD *live_set = BITSET_CALLOC(f->ssa_alloc);
+   bool dead_phis;
+   do {
+      BITSET_WORD *live_set = BITSET_CALLOC(f->ssa_alloc);
+      dead_phis = false;
 
-   jay_foreach_inst_in_func_safe_rev(f, block, I) {
-      if (!BITSET_TEST_COUNT(live_set, jay_base_index(I->dst),
-                             jay_num_values(I->dst)) &&
-          I->op != JAY_OPCODE_SEND) {
+      jay_foreach_block_rev(f, block) {
+         jay_foreach_inst_in_block_safe_rev(block, I) {
+            unsigned dst = jay_base_index(I->dst);
+            if (!BITSET_TEST_COUNT(live_set, dst, jay_num_values(I->dst)) &&
+                I->op != JAY_OPCODE_SEND) {
 
-         if (I->predication == JAY_PREDICATED_DEFAULT && !jay_is_null(I->dst)) {
-            jay_shrink_sources(I, I->num_srcs - 1);
-            I->predication = JAY_PREDICATED;
+               if (I->predication == JAY_PREDICATED_DEFAULT &&
+                   !jay_is_null(I->dst)) {
+                  jay_shrink_sources(I, I->num_srcs - 1);
+                  I->predication = JAY_PREDICATED;
+               }
+
+               I->dst = jay_null();
+            }
+
+            if (!jay_is_null(I->cond_flag) &&
+                !BITSET_TEST(live_set, jay_index(I->cond_flag)) &&
+                (I->op != JAY_OPCODE_CMP || jay_is_null(I->dst))) {
+
+               I->cond_flag = jay_null();
+               I->conditional_mod = 0;
+            }
+
+            bool no_dest = jay_is_null(I->dst) && jay_is_null(I->cond_flag);
+            bool side_effects = jay_opcode_infos[I->op].side_effects;
+
+            if (no_dest && !side_effects) {
+               jay_remove_instruction(I);
+               dead_phis |= (I->op == JAY_OPCODE_PHI_DST);
+            } else {
+               jay_foreach_src_index(I, s, _, index) {
+                  BITSET_SET(live_set, index);
+               }
+            }
          }
 
-         I->dst = jay_null();
-      }
-
-      if (!jay_is_null(I->cond_flag) &&
-          !BITSET_TEST(live_set, jay_index(I->cond_flag)) &&
-          (I->op != JAY_OPCODE_CMP || jay_is_null(I->dst))) {
-
-         I->cond_flag = jay_null();
-         I->conditional_mod = 0;
-      }
-
-      bool no_dest = jay_is_null(I->dst) && jay_is_null(I->cond_flag);
-      bool side_effects = jay_opcode_infos[I->op].side_effects;
-
-      if (no_dest && !side_effects) {
-         jay_remove_instruction(I);
-      } else {
-         jay_foreach_src_index(I, s, _, index) {
-            BITSET_SET(live_set, index);
+         if (dead_phis) {
+            jay_foreach_predecessor(block, pred, UGPR) {
+               jay_foreach_phi_src_in_block(*pred, phi_src) {
+                  if (!BITSET_TEST(live_set, jay_phi_src_index(phi_src))) {
+                     jay_remove_instruction(phi_src);
+                  }
+               }
+            }
          }
       }
-   }
 
-   /* Eliminate phis. This step may leave dead code but it's good enough in
-    * practice since NIR already eliminated dead phis.
-    */
-   jay_foreach_block(f, block) {
-      jay_foreach_phi_src_in_block(block, I) {
-         if (!BITSET_TEST(live_set, jay_phi_src_index(I))) {
-            jay_remove_instruction(I);
-         }
-      }
-   }
-
-   free(live_set);
+      free(live_set);
+   } while (dead_phis);
 }
 
 JAY_DEFINE_FUNCTION_PASS(jay_opt_dead_code, pass)
