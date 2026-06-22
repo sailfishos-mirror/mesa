sailfishos-mirror/mesa

commit ca9191a8a7e49a34e12ea7d09454ab29999d1723

radv: use nir_opt_uub
fossil-db (navi21):
Totals from 3894 (4.62% of 84369) affected shaders:
MaxWaves: 86615 -> 86637 (+0.03%)
Instrs: 7490485 -> 7475241 (-0.20%); split: -0.22%, +0.01%
CodeSize: 39623244 -> 39530064 (-0.24%); split: -0.25%, +0.01%
VGPRs: 208256 -> 208176 (-0.04%); split: -0.06%, +0.02%
SpillSGPRs: 1000 -> 996 (-0.40%)
Latency: 70318585 -> 70222574 (-0.14%); split: -0.18%, +0.05%
InvThroughput: 18896416 -> 18879399 (-0.09%); split: -0.12%, +0.03%
VClause: 148207 -> 148155 (-0.04%); split: -0.07%, +0.04%
SClause: 168961 -> 168957 (-0.00%); split: -0.06%, +0.06%
Copies: 488812 -> 488065 (-0.15%); split: -0.30%, +0.15%
Branches: 206484 -> 205549 (-0.45%); split: -0.48%, +0.03%
PreSGPRs: 179557 -> 179512 (-0.03%); split: -0.19%, +0.17%
PreVGPRs: 178818 -> 178735 (-0.05%); split: -0.05%, +0.00%
VALU: 4587157 -> 4580140 (-0.15%); split: -0.17%, +0.01%
SALU: 1646841 -> 1640392 (-0.39%); split: -0.43%, +0.04%
VMEM: 245983 -> 245974 (-0.00%)
SMEM: 256836 -> 256596 (-0.09%); split: -0.10%, +0.00%

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38399>

src/amd/vulkan/radv_pipeline.c
@@ -559,6 +559,8 @@ radv_postprocess_nir(const struct radv_compiler_info *compiler_info, const struc
    if (opt_intrinsics) /* optimize inot(inverse_ballot) */
       NIR_PASS(_, stage->nir, nir_opt_intrinsics);
 
+   NIR_PASS(_, stage->nir, nir_opt_uub, &(nir_opt_uub_options){});
+
    radv_optimize_nir_algebraic(
       stage->nir, io_to_mem || lowered_ngg || stage->stage == MESA_SHADER_COMPUTE || stage->stage == MESA_SHADER_TASK,
       gfx_level >= GFX8, gfx_level);

src/amd/vulkan/radv_shader.c
@@ -201,6 +201,7 @@ radv_optimize_nir(struct nir_shader *shader, bool optimize_conservatively)
       }
       NIR_LOOP_PASS_NOT_IDEMPOTENT(progress, skip, shader, nir_opt_if, nir_opt_if_optimize_phi_true_false);
       NIR_LOOP_PASS(progress, skip, shader, nir_opt_cse);
+      NIR_LOOP_PASS(progress, skip, shader, nir_opt_uub, &(nir_opt_uub_options){});
 
       nir_opt_peephole_select_options peephole_select_options = {
          .limit = 8,

commit 22abfc06d7cf4c115ca2e368505d75ba4af174b4

radv: Fix gnu-empty-initializer errors in ca9191a8
Fixes the following building errors:

../src/amd/vulkan/radv_pipeline.c:562:63: error: use of GNU empty initializer extension [-Werror,-Wgnu-empty-initializer]
   NIR_PASS(_, stage->nir, nir_opt_uub, &(nir_opt_uub_options){});
                                                              ^
1 error generated.

../src/amd/vulkan/radv_shader.c:204:80: error: use of GNU empty initializer extension [-Werror,-Wgnu-empty-initializer]
      NIR_LOOP_PASS(progress, skip, shader, nir_opt_uub, &(nir_opt_uub_options){});
                                                                               ^
1 error generated.

Fixes: ca9191a8 ("radv: use nir_opt_uub")
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42367>

src/amd/vulkan/radv_pipeline.c
@@ -559,7 +559,7 @@ radv_postprocess_nir(const struct radv_compiler_info *compiler_info, const struc
    if (opt_intrinsics) /* optimize inot(inverse_ballot) */
       NIR_PASS(_, stage->nir, nir_opt_intrinsics);
 
-   NIR_PASS(_, stage->nir, nir_opt_uub, &(nir_opt_uub_options){});
+   NIR_PASS(_, stage->nir, nir_opt_uub, &(nir_opt_uub_options){0});
 
    radv_optimize_nir_algebraic(
       stage->nir, io_to_mem || lowered_ngg || stage->stage == MESA_SHADER_COMPUTE || stage->stage == MESA_SHADER_TASK,

src/amd/vulkan/radv_shader.c
@@ -201,7 +201,7 @@ radv_optimize_nir(struct nir_shader *shader, bool optimize_conservatively)
       }
       NIR_LOOP_PASS_NOT_IDEMPOTENT(progress, skip, shader, nir_opt_if, nir_opt_if_optimize_phi_true_false);
       NIR_LOOP_PASS(progress, skip, shader, nir_opt_cse);
-      NIR_LOOP_PASS(progress, skip, shader, nir_opt_uub, &(nir_opt_uub_options){});
+      NIR_LOOP_PASS(progress, skip, shader, nir_opt_uub, &(nir_opt_uub_options){0});
 
       nir_opt_peephole_select_options peephole_select_options = {
          .limit = 8,