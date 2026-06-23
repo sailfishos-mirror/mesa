bminor/mesa-mesa

commit ffd02b8ad84728d3529f1beeb06fb9212d31113b

meson/megadriver: support various lib suffixes
Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32333>

bin/install_megadrivers.py
@@ -42,7 +42,12 @@ def main():
     parser.add_argument('libdir')
     parser.add_argument('drivers', nargs='+')
     parser.add_argument('--megadriver-libdir')
+    parser.add_argument('--libname-suffix', required=True)
     args = parser.parse_args()
+
+    # Not neccesarily at the end, there might be a version suffix, but let's
+    # make sure that the same suffix is in the megadriver lib name.
+    assert '.' + args.libname_suffix in args.megadriver
 
     to = resolve_libdir(args.libdir)
     if args.megadriver_libdir:
@@ -74,7 +79,7 @@ def main():
             os.chdir(to)
 
             name, ext = os.path.splitext(driver)
-            while ext != '.so':
+            while ext != '.' + args.libname_suffix:
                 if os.path.lexists(name):
                     os.unlink(name)
                 os.symlink(driver, name)
@@ -82,9 +87,9 @@ def main():
         finally:
             os.chdir(ret)
 
-    # Remove meson-created .so symlinks
+    # Remove meson-created symlinks
     name, ext = os.path.splitext(master)
-    while ext != '.so':
+    while ext != '.' + args.libname_suffix:
         if os.path.lexists(name):
             os.unlink(name)
         name, ext = os.path.splitext(name)

bin/meson.build
@@ -13,4 +13,5 @@ symbols_check = find_program('symbols-check.py')
 install_megadrivers_py = find_program('install_megadrivers.py')
 install_megadrivers = [
   install_megadrivers_py.full_path(),
+  '--libname-suffix', libname_suffix,
 ]

meson.build
@@ -20,6 +20,11 @@ project(
 if host_machine.system() == 'darwin'
   add_languages('objc', native : false)
   add_project_arguments('-fobjc-arc', language : 'objc')
+  libname_suffix = 'dylib'
+elif host_machine.system() == 'windows'
+  libname_suffix = 'dll'
+else
+  libname_suffix = 'so'
 endif
 
 cc = meson.get_compiler('c')

src/gallium/targets/dri/meson.build
@@ -67,7 +67,7 @@ libgallium_dri = shared_library(
     driver_asahi, driver_crocus
   ],
   install : true,
-  name_suffix : 'so',
+  name_suffix : libname_suffix,
 )
 
 if with_gallium_va or with_gallium_vdpau

src/gallium/targets/dril/meson.build
@@ -59,7 +59,7 @@ dril_dri = shared_library(
   # Will be deleted during installation, see install_megadrivers.py
   install : true,
   install_dir : dri_drivers_path,
-  name_suffix : 'so',
+  name_suffix : libname_suffix,
 )
 
 foreach d : [[with_gallium_kmsro, [

src/gallium/targets/va/meson.build
@@ -84,7 +84,7 @@ else
     link_depends : va_link_depends,
     install : true,
     install_dir : va_drivers_path,
-    name_suffix : 'so',
+    name_suffix : libname_suffix,
   )
 
   foreach d : [[with_gallium_r600, 'r600'],

src/gallium/targets/vdpau/meson.build
@@ -47,7 +47,7 @@ libvdpau_gallium = shared_library(
   soversion : '@0@.@1@.0'.format(VDPAU_MAJOR, VDPAU_MINOR),
   install : true,
   install_dir : vdpau_drivers_path,
-  name_suffix : 'so',
+  name_suffix : libname_suffix,
 )
 foreach d : [[with_gallium_r600, 'r600'],
              [with_gallium_radeonsi, 'radeonsi'],

sailfishos-mirror/mesa

commit c8f01b7648ec3f2c1ce497343401d00c67c17e8c

bin/install_megadrivers: Bail out if libname suffix is never reached
The symlink-stripping loops walk a versioned driver name down to its base suffix one extension
at a time. If the driver name doesn't contain the configured suffix at all, splitext eventually
returns an empty extension and the loop spins forever instead of making progress. Detect that
case and exit with failure so the install fails loudly rather than hanging.

CC: mesa-stable
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15528
Fixes: ffd02b8ad84728d3529f1beeb06fb9212d31113b
Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42357>

bin/install_megadrivers.py
@@ -24,6 +24,7 @@
 
 import argparse
 import os
+import sys
 
 
 def resolve_libdir(libdir):
@@ -80,6 +81,10 @@ def main():
 
             name, ext = os.path.splitext(driver)
             while ext != '.' + args.libname_suffix:
+                if not ext:
+                    sys.exit(
+                        f"install_megadrivers: driver '{driver}' does not "
+                        f"contain '.{args.libname_suffix}'")
                 if os.path.lexists(name):
                     os.unlink(name)
                 os.symlink(driver, name)
@@ -90,6 +95,10 @@ def main():
     # Remove meson-created symlinks
     name, ext = os.path.splitext(master)
     while ext != '.' + args.libname_suffix:
+        if not ext:
+            sys.exit(
+                f"install_megadrivers: megadriver '{basename}' does not "
+                f"contain '.{args.libname_suffix}'")
         if os.path.lexists(name):
             os.unlink(name)
         name, ext = os.path.splitext(name)