0b3aef90fc9bc46b43b132cbb6c880c118cf48b9
Rhys Perry <pendingchaos02@gmail.com>
2026-06-24 10:05:48 +0100

    ac/nir: skip SMEM fixup for more 32-bit load_global addresses
    Usually, this is later optimized out, but that can't be done if the
    pack_64_2x32 is followed by an addition.
    
    fossil-db (polaris10):
    Totals from 390 (0.54% of 71628) affected shaders:
    Instrs: 278442 -> 278441 (-0.00%); split: -0.00%, +0.00%
    CodeSize: 1464944 -> 1465036 (+0.01%); split: -0.00%, +0.01%
    SGPRs: 28112 -> 28096 (-0.06%)
    Latency: 6590441 -> 6590265 (-0.00%); split: -0.01%, +0.00%
    InvThroughput: 3470063 -> 3469943 (-0.00%); split: -0.00%, +0.00%
    SClause: 10180 -> 10177 (-0.03%); split: -0.06%, +0.03%
    PreSGPRs: 16818 -> 16823 (+0.03%)
    
    fossil-db (navi33):
    Totals from 5844 (2.79% of 209622) affected shaders:
    Instrs: 6662488 -> 6661046 (-0.02%); split: -0.05%, +0.03%
    CodeSize: 35890164 -> 35887576 (-0.01%); split: -0.04%, +0.03%
    Latency: 61294857 -> 61287169 (-0.01%); split: -0.02%, +0.01%
    InvThroughput: 13740346 -> 13739892 (-0.00%); split: -0.01%, +0.01%
    VClause: 150588 -> 150533 (-0.04%); split: -0.04%, +0.01%
    SClause: 160250 -> 159814 (-0.27%); split: -0.36%, +0.09%
    Copies: 542881 -> 543195 (+0.06%); split: -0.19%, +0.25%
    Branches: 124747 -> 124761 (+0.01%)
    PreSGPRs: 312950 -> 313072 (+0.04%); split: -0.02%, +0.06%
    VALU: 3933930 -> 3933958 (+0.00%); split: -0.00%, +0.00%
    SALU: 835254 -> 835617 (+0.04%); split: -0.12%, +0.17%
    VOPD: 109043 -> 108960 (-0.08%); split: +0.04%, -0.12%
    
    fossil-db (gfx1201):
    Totals from 5928 (2.82% of 210263) affected shaders:
    Instrs: 6848881 -> 6847159 (-0.03%); split: -0.05%, +0.03%
    CodeSize: 37120904 -> 37117080 (-0.01%); split: -0.05%, +0.04%
    Latency: 59105713 -> 59096328 (-0.02%); split: -0.02%, +0.00%
    InvThroughput: 9738519 -> 9737449 (-0.01%); split: -0.01%, +0.00%
    SClause: 163841 -> 163463 (-0.23%); split: -0.33%, +0.10%
    Copies: 538245 -> 538137 (-0.02%); split: -0.23%, +0.21%
    Branches: 125543 -> 125541 (-0.00%); split: -0.01%, +0.01%
    PreSGPRs: 320249 -> 320364 (+0.04%); split: -0.02%, +0.06%
    VALU: 3791086 -> 3791084 (-0.00%)
    SALU: 1034686 -> 1034575 (-0.01%); split: -0.12%, +0.11%
    VOPD: 113465 -> 113467 (+0.00%)
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42431>

--- src/amd/common/nir/ac_nir_fixup_smem_loads_null_prt.c
+++ src/amd/common/nir/ac_nir_fixup_smem_loads_null_prt.c
@@ -27,6 +27,48 @@ typedef struct {
    uint8_t control_bit;
 } fixup_smem_loads_null_prt_state;
 
+static uint64_t bits_may_be_1(nir_scalar scalar, unsigned depth)
+{
+   assert(scalar.def->bit_size == 64);
+   scalar = nir_scalar_chase_movs(scalar);
+
+   if (nir_scalar_is_const(scalar))
+      return nir_scalar_as_uint(scalar);
+   else if (!nir_scalar_is_alu(scalar) || depth >= 4)
+      return UINT64_MAX;
+
+   nir_op op = nir_scalar_alu_op(scalar);
+   switch (op) {
+   case nir_op_u2u64:
+      return UINT32_MAX;
+   case nir_op_pack_64_2x32_split: {
+      nir_scalar src1 = nir_scalar_chase_movs(nir_scalar_chase_alu_src(scalar, 1));
+      if (nir_scalar_is_const(src1))
+         return nir_scalar_as_uint(src1) << 32 | UINT32_MAX;
+      break;
+   }
+   case nir_op_iadd: {
+      nir_scalar src0 = nir_scalar_chase_alu_src(scalar, 0);
+      nir_scalar src1 = nir_scalar_chase_alu_src(scalar, 1);
+      uint64_t src0_bits = bits_may_be_1(src0, depth + 1);
+      uint64_t src1_bits = bits_may_be_1(src1, depth + 1);
+      uint64_t res = src0_bits | src1_bits;
+      bool carry_in = false;
+      for (unsigned i = 0; i < 64; i++) {
+         uint64_t bit = BITFIELD64_BIT(i);
+         bool carry_out = (src0_bits & src1_bits & bit) || (carry_in && (res & bit));
+         res |= carry_in ? bit : 0;
+         carry_in = carry_out;
+      }
+      return res;
+   }
+   default:
+      break;
+   }
+
+   return UINT64_MAX;
+}
+
 static bool
 fixup_smem_loads_null_prt(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
 {
@@ -46,7 +88,18 @@ fixup_smem_loads_null_prt(nir_builder *b, nir_intrinsic_instr *intrin, void *dat
    nir_def *new_src;
 
    if (intrin->intrinsic == nir_intrinsic_load_global_amd) {
-      new_src = nir_iand_imm(b, src, ~(1ull << state->control_bit));
+      /* Check if the control bit is definitely zero. This can happen if the address is a 32-bit one,
+       * which are created using pack_64_2x32_split. The high 32-bits are a constant, and so in this
+       * case, we can usually omit masking out the control bit.
+       *
+       * Usually, this check is unnecessary and the mask is later optimized out, but that doesn't
+       * happen if the pack is followed by an addition.
+       */
+      uint64_t may_be_1 = bits_may_be_1(nir_get_scalar(src, 0), 0);
+      uint64_t bit = (1ull << state->control_bit);
+      if (!(may_be_1 & bit))
+         return false;
+      new_src = nir_iand_imm(b, src, ~bit);
    } else {
       assert(state->control_bit >= 32);
       nir_def *new_addr_hi = nir_iand_imm(b, nir_channel(b, src, 1), ~(1u << (state->control_bit - 32)));
