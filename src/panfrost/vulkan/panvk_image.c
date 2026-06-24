bminor/mesa-mesa

commit 11fe21d6591f7a56fd656debb4432c5a7d8ad295

panvk: change AFBC subresource layout pitches to byte sizes
External importers of AFBC dmabufs expect these to be in terms
of bytesizes instead of direct superblock counts. This makes these
calculations aligned with panfrost and fixes WSI imports.

Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37771>

src/panfrost/vulkan/panvk_image.c
@@ -738,11 +738,10 @@ get_image_subresource_layout(const struct panvk_image *image,
    layout->arrayPitch = image->planes[plane].plane.layout.array_stride_B;
 
    if (drm_is_afbc(image->vk.drm_format_mod)) {
-      /* row/depth pitch expressed in AFBC superblocks. */
-      layout->rowPitch = pan_afbc_stride_blocks(
-         image->vk.drm_format_mod, slice_layout->afbc.header.row_stride_B);
-      layout->depthPitch = pan_afbc_stride_blocks(
-         image->vk.drm_format_mod, slice_layout->afbc.header.surface_size_B);
+      /* row/depth pitch expressed in (AFBC superblocks * payload size). */
+      layout->rowPitch = pan_image_get_wsi_row_pitch(
+         &image->planes[plane].image, plane, subres->mipLevel);
+      layout->depthPitch = slice_layout->afbc.surface_stride_B;
    } else {
       layout->rowPitch = slice_layout->tiled_or_linear.row_stride_B;
       layout->depthPitch = slice_layout->tiled_or_linear.surface_stride_B;

sailfishos-mirror/mesa

commit 38994cb56e738a1533b2e4e94fb41ab3fd7014f2

panvk: fix plane indexing for afbc image subres layout
The current path follows below mapping:
- 1 x panvk_image -> N x (1 x pan_image -> 1 x pan_image_plane)

Fixes: 11fe21d6591 ("panvk: change AFBC subresource layout pitches to byte sizes")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Marc Alcala Prieto <marc.alcalaprieto@arm.com>
Tested-by: Marc Alcala Prieto <marc.alcalaprieto@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42353>

src/panfrost/vulkan/panvk_image.c
@@ -1,6 +1,7 @@
 /*
  * Copyright © 2025 Arm Ltd.
  * Copyright © 2021 Collabora Ltd.
+ * Copyright © 2026 Google LLC
  *
  * Derived from tu_image.c which is:
  * Copyright © 2016 Red Hat.
@@ -823,7 +824,7 @@ get_image_subresource_layout(const struct panvk_image *image,
    if (drm_is_afbc(image->vk.drm_format_mod)) {
       /* row/depth pitch expressed in (AFBC superblocks * payload size). */
       layout->rowPitch = pan_image_get_wsi_row_pitch(
-         &image->planes[plane].image, plane, subres->mipLevel);
+         &image->planes[plane].image, 0, subres->mipLevel);
       layout->depthPitch = slice_layout->afbc.surface_stride_B;
    } else {
       layout->rowPitch = slice_layout->tiled_or_linear.row_stride_B;