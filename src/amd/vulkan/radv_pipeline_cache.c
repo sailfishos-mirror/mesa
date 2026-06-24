sailfishos-mirror/mesa

commit 64b4cb001c329a94e1bb9ca3c104c42dabb8c0fb

radv: cache shader IR, asm and spir-v
This improves RADV_DEBUG=hang's pipeline.log when shader caching is not
disabled.

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42175>

src/amd/vulkan/radv_pipeline_cache.c
@@ -73,9 +73,23 @@ radv_shader_deserialize(struct radv_device *device, const void *key_data, size_t
    if (!shader)
       return NULL;
 
+   radv_parse_binary_debug_info(&device->compiler_info, binary, &shader->dbg);
+
    assert(key_size == sizeof(shader->hash));
    memcpy(shader->hash, key_data, key_size);
    blob_skip_bytes(blob, binary->total_size - sizeof(struct radv_shader_binary));
+
+   /* serialize_binary() has a fast path for when this debug information doesn't exist. */
+   if (blob->current != blob->end) {
+      shader->dbg.spirv_size = blob_read_uint32(blob);
+      shader->dbg.spirv = malloc(shader->dbg.spirv_size);
+      blob_copy_bytes(blob, shader->dbg.spirv, shader->dbg.spirv_size);
+
+      const char *nir_string = blob_read_string(blob);
+      const char *args_string = blob_read_string(blob);
+      shader->dbg.nir_string = (nir_string && nir_string[0]) ? strdup(nir_string) : NULL;
+      shader->dbg.args_string = (args_string && args_string[0]) ? strdup(args_string) : NULL;
+   }
 
    return shader;
 }
@@ -92,12 +106,41 @@ radv_shader_cache_deserialize(struct vk_pipeline_cache *cache, const void *key_d
    return shader ? &shader->base : NULL;
 }
 
+static void
+serialize_shader_debug_info(struct blob *blob, const struct radv_shader_debug_info *dbg)
+{
+   blob_write_uint32(blob, dbg->spirv_size);
+   blob_write_bytes(blob, dbg->spirv, dbg->spirv_size);
+   blob_write_string(blob, dbg->nir_string ? dbg->nir_string : "");
+   blob_write_string(blob, dbg->args_string ? dbg->args_string : "");
+}
+
+static void
+serialize_binary(struct blob *blob, const struct radv_shader_binary *binary, const struct radv_shader_debug_info *dbg)
+{
+   if (dbg && (dbg->spirv_size || dbg->nir_string || dbg->args_string)) {
+      blob_init(blob);
+      blob_write_bytes(blob, binary, binary->total_size);
+      serialize_shader_debug_info(blob, dbg);
+      free(dbg->spirv);
+      free(dbg->nir_string);
+      free(dbg->args_string);
+   } else {
+      blob_init_fixed(blob, (void *)binary, binary->total_size);
+      blob_reserve_bytes(blob, binary->total_size);
+   }
+}
+
 void
 radv_shader_serialize(struct radv_shader *shader, struct blob *blob)
 {
    size_t stats_size = shader->dbg.statistics ? sizeof(struct amd_stats) : 0;
+   size_t ir_size = shader->dbg.ir_string ? strlen(shader->dbg.ir_string) + 1 : 0;
+   size_t disasm_size = shader->dbg.disasm_string ? strlen(shader->dbg.disasm_string) + 1 : 0;
+   size_t debug_info_size = shader->dbg.debug_info_count * sizeof(struct ac_shader_debug_info);
    size_t code_size = shader->code_size;
-   uint32_t total_size = sizeof(struct radv_shader_binary_legacy) + code_size + stats_size;
+   uint32_t total_size =
+      sizeof(struct radv_shader_binary_legacy) + code_size + stats_size + ir_size + disasm_size + debug_info_size;
 
    struct radv_shader_binary_legacy binary = {
       .base =
@@ -109,14 +152,21 @@ radv_shader_serialize(struct radv_shader *shader, struct blob *blob)
          },
       .code_size = code_size,
       .exec_size = shader->exec_size,
-      .ir_size = 0,
-      .disasm_size = 0,
+      .ir_size = ir_size,
+      .disasm_size = disasm_size,
       .stats_size = stats_size,
+      .debug_info_size = debug_info_size,
    };
 
    blob_write_bytes(blob, &binary, sizeof(struct radv_shader_binary_legacy));
    blob_write_bytes(blob, shader->dbg.statistics, stats_size);
    blob_write_bytes(blob, shader->code, code_size);
+   blob_write_bytes(blob, shader->dbg.ir_string, binary.ir_size);
+   blob_write_bytes(blob, shader->dbg.disasm_string, binary.disasm_size);
+   blob_write_bytes(blob, shader->dbg.debug_info, binary.debug_info_size);
+
+   if (shader->dbg.spirv_size || shader->dbg.nir_string || shader->dbg.args_string)
+      serialize_shader_debug_info(blob, &shader->dbg);
 }
 
 static bool
@@ -153,6 +203,7 @@ radv_shader_create(struct radv_device *device, struct vk_pipeline_cache *cache, 
    if (radv_is_cache_disabled(&device->compiler_info, cache) || skip_cache || (dbg && dbg->dump_shader)) {
       struct radv_shader *shader;
       radv_shader_create_uncached(device, binary, false, NULL, dbg, &shader);
+      radv_parse_binary_debug_info(&device->compiler_info, binary, &shader->dbg);
       return shader;
    }
 
@@ -162,9 +213,14 @@ radv_shader_create(struct radv_device *device, struct vk_pipeline_cache *cache, 
    blake3_hash hash;
    _mesa_blake3_compute(binary, binary->total_size, hash);
 
+   struct blob blob;
+   serialize_binary(&blob, binary, dbg);
+
    struct vk_pipeline_cache_object *shader_obj;
-   shader_obj = vk_pipeline_cache_create_and_insert_object(cache, hash, sizeof(hash), binary, binary->total_size,
-                                                           &radv_shader_ops);
+   shader_obj =
+      vk_pipeline_cache_create_and_insert_object(cache, hash, sizeof(hash), blob.data, blob.size, &radv_shader_ops);
+
+   blob_finish(&blob);
 
    return shader_obj ? container_of(shader_obj, struct radv_shader, base) : NULL;
 }

src/amd/vulkan/radv_pipeline_compute.c
@@ -130,8 +130,6 @@ radv_compile_cs(const struct radv_compiler_info *compiler_info, struct radv_shad
    if (cs_stage->key.keep_executable_info)
       nir_string = radv_dump_nir_shaders(compiler_info, &cs_stage->nir, 1);
 
-   radv_parse_binary_debug_info(compiler_info, cs_binary, dbg);
-
    dbg->nir_string = nir_string;
    dbg->stages = 1 << MESA_SHADER_COMPUTE;
 

src/amd/vulkan/radv_pipeline_graphics.c
@@ -2114,7 +2114,6 @@ radv_create_gs_copy_shader(const struct radv_compiler_info *compiler_info, struc
    if (gs_copy_stage.key.keep_executable_info)
       nir_string = radv_dump_nir_shaders(compiler_info, &nir, 1);
 
-   radv_parse_binary_debug_info(compiler_info, gs_copy_binary, gs_copy_debug);
    gs_copy_debug->nir_string = nir_string;
    gs_copy_debug->stages = 1 << MESA_SHADER_VERTEX;
    radv_shader_dump_asm(compiler_info, gs_copy_debug, &gs_copy_stage.info);
@@ -2180,7 +2179,6 @@ radv_graphics_shaders_nir_to_asm(const struct radv_compiler_info *compiler_info,
       if (stages[s].key.keep_executable_info)
          nir_string = radv_dump_nir_shaders(compiler_info, nir_shaders, shader_count);
 
-      radv_parse_binary_debug_info(compiler_info, binaries[s], &debug[s]);
       debug[s].nir_string = nir_string;
       for (uint32_t i = 0; i < shader_count; i++)
          debug[s].stages |= 1 << nir_shaders[i]->info.stage;

src/amd/vulkan/radv_pipeline_rt.c
@@ -502,7 +502,6 @@ radv_rt_nir_to_asm(const struct radv_compiler_info *compiler_info, struct radv_r
    if (stage->key.keep_executable_info)
       debug->nir_string = radv_dump_nir_shaders(compiler_info, shaders, num_shaders);
 
-   radv_parse_binary_debug_info(compiler_info, *binary, debug);
    debug->stages = 1 << shaders[0]->info.stage;
 
    radv_shader_dump_asm(compiler_info, debug, &stage->info);

src/amd/vulkan/radv_shader.h
@@ -426,8 +426,11 @@ struct radv_serialized_shader_arena_block {
 };
 
 struct radv_shader_debug_info {
+   /* These are uncached. */
    bool dump_shader;
    uint32_t stages; /* mesa_shader_stage */
+
+   /* The rest of these are cached. */
    char *spirv;
    uint32_t spirv_size;
    char *nir_string;

src/amd/vulkan/radv_pipeline.c
@@ -46,14 +46,11 @@ radv_pipeline_skip_shaders_cache(const struct radv_device *device, const struct 
    const struct radv_instance *instance = radv_physical_device_instance(pdev);
 
    /* Skip the shaders cache when any of the below are true:
-    * - trap handler is present
     * - shaders are dumped for debugging (RADV_DEBUG=shaders)
-    * - shaders IR are captured (NIR, backend IR and ASM)
     * - binaries are captured (driver shouldn't store data to an internal cache)
     */
-   return device->trap_handler_shader || (instance->debug_flags & RADV_DEBUG_DUMP_SHADERS) ||
-          (pipeline->create_flags &
-           (VK_PIPELINE_CREATE_2_CAPTURE_INTERNAL_REPRESENTATIONS_BIT_KHR | VK_PIPELINE_CREATE_2_CAPTURE_DATA_BIT_KHR));
+   return (instance->debug_flags & RADV_DEBUG_DUMP_SHADERS) ||
+          (pipeline->create_flags & VK_PIPELINE_CREATE_2_CAPTURE_DATA_BIT_KHR);
 }
 
 void

commit 4509b7e082845c941b878c6d8713ed5bce09d82e

radv: do radv_parse_binary_debug_info in radv_shader_dump_asm
radv_shader_dump_asm() can't be moved later because of shader_dump_mtx.

Fixes RADV_DEBUG=asm.

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Fixes: 64b4cb001c3 ("radv: cache shader IR, asm and spir-v")
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42411>

src/amd/vulkan/radv_pipeline_cache.c
@@ -203,7 +203,10 @@ radv_shader_create(struct radv_device *device, struct vk_pipeline_cache *cache, 
    if (radv_is_cache_disabled(&device->compiler_info, cache) || skip_cache || (dbg && dbg->dump_shader)) {
       struct radv_shader *shader;
       radv_shader_create_uncached(device, binary, false, NULL, dbg, &shader);
-      radv_parse_binary_debug_info(&device->compiler_info, binary, &shader->dbg);
+      /* radv_parse_binary_debug_info() might have been done earlier by radv_shader_dump_asm(). Skip it in that case to
+       * avoid memory leaks. */
+      if (!dbg || (!dbg->statistics && !dbg->ir_string && !dbg->disasm_string && !dbg->debug_info_count))
+         radv_parse_binary_debug_info(&device->compiler_info, binary, &shader->dbg);
       return shader;
    }
 

src/amd/vulkan/radv_pipeline_compute.c
@@ -133,7 +133,7 @@ radv_compile_cs(const struct radv_compiler_info *compiler_info, struct radv_shad
    dbg->nir_string = nir_string;
    dbg->stages = 1 << MESA_SHADER_COMPUTE;
 
-   radv_shader_dump_asm(compiler_info, dbg, &cs_stage->info);
+   radv_shader_dump_asm(compiler_info, dbg, cs_binary, &cs_stage->info);
 
    if (dbg->dump_shader)
       simple_mtx_unlock(compiler_info->debug.shader_dump_mtx);

src/amd/vulkan/radv_pipeline_graphics.c
@@ -2116,7 +2116,7 @@ radv_create_gs_copy_shader(const struct radv_compiler_info *compiler_info, struc
 
    gs_copy_debug->nir_string = nir_string;
    gs_copy_debug->stages = 1 << MESA_SHADER_VERTEX;
-   radv_shader_dump_asm(compiler_info, gs_copy_debug, &gs_copy_stage.info);
+   radv_shader_dump_asm(compiler_info, gs_copy_debug, gs_copy_binary, &gs_copy_stage.info);
 
    if (gs_copy_debug->dump_shader)
       simple_mtx_unlock(compiler_info->debug.shader_dump_mtx);
@@ -2183,7 +2183,7 @@ radv_graphics_shaders_nir_to_asm(const struct radv_compiler_info *compiler_info,
       for (uint32_t i = 0; i < shader_count; i++)
          debug[s].stages |= 1 << nir_shaders[i]->info.stage;
 
-      radv_shader_dump_asm(compiler_info, &debug[s], &stages[s].info);
+      radv_shader_dump_asm(compiler_info, &debug[s], binaries[s], &stages[s].info);
 
       if (debug[s].dump_shader)
          simple_mtx_unlock(compiler_info->debug.shader_dump_mtx);

src/amd/vulkan/radv_pipeline_rt.c
@@ -506,7 +506,7 @@ radv_rt_nir_to_asm(const struct radv_compiler_info *compiler_info, struct radv_r
 
    debug->stages = 1 << shaders[0]->info.stage;
 
-   radv_shader_dump_asm(compiler_info, debug, &stage->info);
+   radv_shader_dump_asm(compiler_info, debug, *binary, &stage->info);
 
    if (stage->key.keep_executable_info && stage->spirv.size) {
       debug->spirv = malloc(stage->spirv.size);

src/amd/vulkan/radv_shader.c
@@ -3475,19 +3475,20 @@ radv_shader_nir_to_asm(const struct radv_compiler_info *compiler_info, struct ra
 }
 
 void
-radv_shader_dump_asm(const struct radv_compiler_info *compiler_info, const struct radv_shader_debug_info *debug,
-                     const struct radv_shader_info *info)
+radv_shader_dump_asm(const struct radv_compiler_info *compiler_info, struct radv_shader_debug_info *debug,
+                     const struct radv_shader_binary *binary, const struct radv_shader_info *info)
 {
-   if (debug->dump_shader) {
-      if (compiler_info->debug.dump_asm) {
-         const char *sep = "";
-         u_foreach_bit (stage, debug->stages) {
-            fprintf(stderr, "%s%s", sep, radv_get_shader_name(info, stage));
-            sep = " + ";
-         }
+   if (debug->dump_shader && compiler_info->debug.dump_asm) {
+      assert(!debug->disasm_string);
+      radv_parse_binary_debug_info(compiler_info, binary, debug);
 
-         fprintf(stderr, "\ndisasm:\n%s\n", debug->disasm_string);
+      const char *sep = "";
+      u_foreach_bit (stage, debug->stages) {
+         fprintf(stderr, "%s%s", sep, radv_get_shader_name(info, stage));
+         sep = " + ";
       }
+
+      fprintf(stderr, "\ndisasm:\n%s\n", debug->disasm_string);
    }
 }
 

src/amd/vulkan/radv_shader.h
@@ -668,8 +668,8 @@ struct radv_shader_binary *radv_shader_nir_to_asm(const struct radv_compiler_inf
                                                   struct radv_shader_stage *pl_stage, struct nir_shader *const *shaders,
                                                   int shader_count, const struct radv_graphics_state_key *gfx_state);
 
-void radv_shader_dump_asm(const struct radv_compiler_info *compiler_info, const struct radv_shader_debug_info *debug,
-                          const struct radv_shader_info *info);
+void radv_shader_dump_asm(const struct radv_compiler_info *compiler_info, struct radv_shader_debug_info *debug,
+                          const struct radv_shader_binary *binary, const struct radv_shader_info *info);
 
 char *radv_dump_nir_shaders(const struct radv_compiler_info *compiler_info, struct nir_shader *const *shaders, int shader_count);
 