sailfishos-mirror/mesa

commit 11f8f333e23dd6bcbeff6a831c67d1acb3cb67d2

anv: set a private binding when the image is not externally shared
This allows anv to use a suballocator for the clear color address, which
should decrease our memory requirement.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13091
Signed-off-by: Rohan Garg <rohan.garg@intel.com>
Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39562>

src/intel/vulkan/anv_image.c
@@ -650,17 +650,12 @@ add_aux_state_tracking_buffer(struct anv_device *device,
    const struct isl_drm_modifier_info *mod_info =
       isl_drm_modifier_get_info(image->vk.drm_format_mod);
 
-   /* If an auxiliary surface is used for an externally-shareable image,
-    * we have to hide this from the memory of the image since other
-    * processes with access to the memory may not be aware of it or of
-    * its current state. So put that auxiliary data into a separate
-    * buffer (ANV_IMAGE_MEMORY_BINDING_PRIVATE).
-    *
-    * But when the image is created with a drm modifier that supports
-    * clear color, it will be exported along with main surface.
+   /* If the image is created with a drm modifier that supports clear color,
+    * it will be exported along with main surface. Otherwise, place the
+    * aux-tracking state in a separate, suballocated buffer to achieve better
+    * memory utilization.
     */
-   if (anv_image_is_externally_shared(image) &&
-       !mod_info->supports_clear_color)
+   if (!mod_info || !mod_info->supports_clear_color)
       binding = ANV_IMAGE_MEMORY_BINDING_PRIVATE;
 
    /* The indirect clear color BO requires 64B-alignment on gfx11+. If we're
@@ -1146,20 +1141,16 @@ check_memory_bindings(const struct anv_device *device,
       /* Check fast clear state */
       if (plane->fast_clear_memory_range.size > 0) {
          enum anv_image_memory_binding binding = primary_binding;
+         const struct isl_drm_modifier_info *mod_info =
+            isl_drm_modifier_get_info(image->vk.drm_format_mod);
 
-         /* If an auxiliary surface is used for an externally-shareable image,
-          * we have to hide this from the memory of the image since other
-          * processes with access to the memory may not be aware of it or of
-          * its current state. So put that auxiliary data into a separate
-          * buffer (ANV_IMAGE_MEMORY_BINDING_PRIVATE).
-          *
-          * But when the image is created with a drm modifier that supports
-          * clear color, it will be exported along with main surface.
+         /* If the image is created with a drm modifier that supports clear
+          * color, it will be exported along with main surface. Otherwise,
+          * place the aux-tracking state in a separate, suballocated buffer
+          * to achieve better memory utilization.
           */
-         if (anv_image_is_externally_shared(image)
-             && !isl_drm_modifier_get_info(image->vk.drm_format_mod)->supports_clear_color) {
+         if (!mod_info || !mod_info->supports_clear_color)
             binding = ANV_IMAGE_MEMORY_BINDING_PRIVATE;
-         }
 
          /* The indirect clear color BO requires 64B-alignment on gfx11+. */
          assert(plane->fast_clear_memory_range.alignment % 64 == 0);

src/intel/vulkan/anv_private.h
@@ -5835,12 +5835,8 @@ anv_image_is_externally_shared(const struct anv_image *image)
 static inline bool
 anv_image_has_private_binding(const struct anv_image *image)
 {
-   if (image->bindings[ANV_IMAGE_MEMORY_BINDING_PRIVATE].memory_range.size > 0) {
-      assert(anv_image_is_externally_shared(image));
-      return true;
-   } else {
-      return false;
-   }
+   enum anv_image_memory_binding binding = ANV_IMAGE_MEMORY_BINDING_PRIVATE;
+   return image->bindings[binding].memory_range.size > 0;
 }
 
 /* The ordering of this enum is important */

commit 34b4e94f9f182337fa3754d01625dd7f917d400f

anv: don't put clear colors for aliased images in private bindings
According to the Vulkan spec:

  "If two aliases are both images that were created with identical
  creation parameters, both were created with the
  VK_IMAGE_CREATE_ALIAS_BIT flag set, and both are bound identically
  to memory except for
  VkBindImageMemoryDeviceGroupInfo::pDeviceIndices and
  VkBindImageMemoryDeviceGroupInfo::pSplitInstanceBindRegions, then
  they interpret the contents of the memory in consistent ways, and
  data written to one alias can be read by the other alias."

If we use private bindings for the clear colors, it could be that
VkImage A sets the clear color to red, VkImage B sets the clear color
to blue and clears it, then when A reads it, it should be blue, but it
will be red since its clear color is different.

On debug mode, before this patch we were hitting the first assertion
of check_memory_bindings():

  assert(!aliased || from_wsi || video_decode ||
         private_binding size is 0)

This could be reproduced, for example, in the Street Fighter 6 demo or
the VVL test --gtest_filter=NegativeSyncVal.CopyOptimalImageHazards.
Or by a simple Vulkan program that just tries to create a simple
multi-sampled 2D image with the alias flag set.

Reported-by: Spencer Fricke (@spencer-lunarg)
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/14926
Fixes: 11f8f333e23d ("anv: set a private binding when the image is not externally shared")
Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42399>

src/intel/vulkan/anv_image.c
@@ -643,12 +643,18 @@ add_aux_state_tracking_buffer(struct anv_device *device,
     * The indirect clear color BO requires 64B-alignment on gfx11+. If we're
     * using a modifier with clear color, then some kernels might require a 4k
     * alignment.
+    *
+    * If it's an aliased image, we can't use private bindings either since
+    * aliased images with the same parameters should be consistent (e.g., they
+    * can't have separate clear colors).
     */
    enum anv_image_memory_binding binding = ANV_IMAGE_MEMORY_BINDING_PRIVATE;
    uint32_t clear_color_alignment = 64;
    if (mod_info && mod_info->supports_clear_color) {
       binding = ANV_IMAGE_MEMORY_BINDING_PLANE_0 + plane;
       clear_color_alignment = 4096;
+   } else if (image->vk.create_flags & VK_IMAGE_CREATE_ALIAS_BIT) {
+      binding = ANV_IMAGE_MEMORY_BINDING_PLANE_0 + plane;
    }
 
    return image_binding_grow(device, image, binding,
@@ -1166,11 +1172,14 @@ check_memory_bindings(const struct anv_device *device,
             isl_drm_modifier_get_info(image->vk.drm_format_mod);
 
          /* If the image is created with a drm modifier that supports clear
-          * color, it will be exported along with main surface. Otherwise,
-          * place the aux-tracking state in a separate, suballocated buffer
-          * to achieve better memory utilization.
+          * color it will be exported along with main surface. If the image is
+          * aliased, it cannot be private since it must be consistent among
+          * all aliases. Otherwise, place the aux-tracking state in a
+          * separate, suballocated buffer to achieve better memory
+          * utilization.
           */
-         if (!mod_info || !mod_info->supports_clear_color)
+         if (!(mod_info && mod_info->supports_clear_color) &&
+             !(image->vk.create_flags & VK_IMAGE_CREATE_ALIAS_BIT))
             binding = ANV_IMAGE_MEMORY_BINDING_PRIVATE;
 
          /* The indirect clear color BO requires 64B-alignment on gfx11+. */