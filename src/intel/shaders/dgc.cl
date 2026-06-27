tranchikha/android_external_mesa3d-rpi

commit fb26ed6bf7b9ff5d89c128bad71a3d57d70f5def

anv: add indirect command layout support
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31384>

src/intel/vulkan/anv_private.h
@@ -87,6 +87,7 @@
 #include "vk_descriptor_set_layout.h"
 #include "vk_descriptor_update_template.h"
 #include "vk_device.h"
+#include "vk_device_generated_commands.h"
 #include "vk_device_memory.h"
 #include "vk_drm_syncobj.h"
 #include "vk_enum_defines.h"
@@ -6579,6 +6580,75 @@ enum anv_dgc_stage anv_vk_stage_to_dgc_stage(VkShaderStageFlags vk_stage);
 
 uint32_t anv_vk_stages_to_generated_stages(VkShaderStageFlags vk_stages);
 
+struct anv_indirect_command_layout {
+   struct vk_indirect_command_layout vk;
+
+   VkPipelineBindPoint bind_point;
+
+   struct anv_dgc_gfx_layout gfx_layout;
+   struct anv_dgc_cs_layout cs_layout;
+
+   /** Layout is as follow :
+    *  --------------------
+    *  |   prolog cmds    |
+    *  |------------------|
+    *  |  sequence cmds   |
+    *  |------------------|
+    *  |   epilog cmds    |
+    *  |------------------|
+    *  |  sideband data   |
+    *  |------------------|
+    *  |   prolog data    |
+    *  |------------------|
+    *  |  sequence data   |
+    *  --------------------
+    */
+
+   /**
+    * Non sequence related commands at the beginning of the preprocess buffer
+    */
+   uint32_t cmd_prolog_size;
+   /**
+    * Non sequence related commands at the end of the preprocess buffer (jump
+    * back to main batch)
+    */
+   uint32_t cmd_epilog_size;
+   /** Amount of command memory required for each sequence */
+   uint32_t cmd_size;
+   /** Non sequence related data */
+   uint32_t data_prolog_size;
+   /** Amount of data memory required (per sequence) */
+   uint32_t data_size;
+   /** Amount of sideband data memory required (not per sequence) */
+   uint32_t sideband_size;
+
+   /** Track if push constants are emitted at all */
+   bool emits_push_constants;
+
+   struct anv_indirect_command_layout_item {
+      const char *name;
+      uint32_t size;
+   } items[10];
+   uint32_t n_items;
+};
+
+void anv_dgc_print_gfx_state(FILE *f,
+                             const struct anv_dgc_gfx_layout *layout,
+                             const struct anv_indirect_command_layout *layout_obj);
+void anv_dgc_print_layout(FILE *f,
+                          const struct anv_indirect_command_layout *layout);
+
+void anv_dgc_fill_gfx_state(struct anv_dgc_gfx_state *state,
+                            struct anv_cmd_buffer *cmd_buffer,
+                            const struct anv_indirect_command_layout *layout_obj,
+                            struct anv_shader ** const shaders);
+
+uint32_t anv_dgc_fill_gfx_layout(struct anv_dgc_gfx_layout *layout,
+                                 const struct anv_device *device,
+                                 const struct anv_indirect_command_layout *layout_obj,
+                                 struct anv_shader ** const shaders);
+
+
 struct anv_vid_mem {
    struct anv_device_memory *mem;
    VkDeviceSize       offset;
@@ -6989,6 +7059,9 @@ VK_DEFINE_NONDISP_HANDLE_CASTS(anv_performance_configuration_intel, base,
 VK_DEFINE_NONDISP_HANDLE_CASTS(anv_video_session, vk.base,
                                VkVideoSessionKHR,
                                VK_OBJECT_TYPE_VIDEO_SESSION_KHR)
+VK_DEFINE_NONDISP_HANDLE_CASTS(anv_indirect_command_layout, vk.base,
+                               VkIndirectCommandsLayoutEXT,
+                               VK_OBJECT_TYPE_INDIRECT_COMMANDS_LAYOUT_EXT)
 VK_DEFINE_NONDISP_HANDLE_CASTS(anv_indirect_execution_set, base,
                                VkIndirectExecutionSetEXT,
                                VK_OBJECT_TYPE_INDIRECT_EXECUTION_SET_EXT)

src/intel/genxml/meson.build
@@ -40,6 +40,69 @@ genX_xml_h = custom_target(
 
 genX_bits_included_symbols = [
   # instructions
+  'COMPUTE_WALKER',
+  'GPGPU_WALKER',
+  'MEDIA_CURBE_LOAD',
+  'MEDIA_INTERFACE_DESCRIPTOR_LOAD',
+  'MEDIA_STATE_FLUSH',
+  'MEDIA_VFE_STATE',
+  'MI_ARB_CHECK',
+  '3DMESH_3D',
+  '3DPRIMITIVE',
+  '3DPRIMITIVE_EXTENDED',
+  '3DSTATE_CLEAR_PARAMS',
+  '3DSTATE_CLIP',
+  '3DSTATE_CLIP_MESH',
+  '3DSTATE_CONSTANT_ALL',
+  '3DSTATE_CONSTANT_VS',
+  '3DSTATE_DS',
+  '3DSTATE_GS',
+  '3DSTATE_HS',
+  '3DSTATE_INDEX_BUFFER',
+  '3DSTATE_MESH_CONTROL',
+  '3DSTATE_MESH_DISTRIB',
+  '3DSTATE_MESH_SHADER',
+  '3DSTATE_MESH_SHADER_DATA',
+  '3DSTATE_PRIMITIVE_REPLICATION',
+  '3DSTATE_PS',
+  '3DSTATE_PS_BLEND',
+  '3DSTATE_PS_EXTRA',
+  '3DSTATE_PUSH_CONSTANT_ALLOC_VS',
+  '3DSTATE_RASTER',
+  '3DSTATE_SBE',
+  '3DSTATE_SBE_MESH',
+  '3DSTATE_SBE_SWIZ',
+  '3DSTATE_SF',
+  '3DSTATE_STREAMOUT',
+  '3DSTATE_TASK_CONTROL',
+  '3DSTATE_TASK_REDISTRIB',
+  '3DSTATE_TASK_SHADER',
+  '3DSTATE_TASK_SHADER_DATA',
+  '3DSTATE_TE',
+  '3DSTATE_URB_ALLOC_MESH',
+  '3DSTATE_URB_ALLOC_TASK',
+  '3DSTATE_URB_ALLOC_VS',
+  '3DSTATE_URB_VS',
+  '3DSTATE_VF_INSTANCING',
+  '3DSTATE_VF_SGVS',
+  '3DSTATE_VF_SGVS_2',
+  '3DSTATE_VF_TOPOLOGY',
+  '3DSTATE_VFG',
+  '3DSTATE_WM',
+  '3DSTATE_VS',
+  'PIPE_CONTROL',
+  'RESOURCE_BARRIER',
+  # instruction fields
+  '3DSTATE_CPSIZE_CONTROL_BUFFER::Surface Base Address',
+  '3DSTATE_CPSIZE_CONTROL_BUFFER::Surface Pitch',
+  '3DSTATE_DEPTH_BUFFER::Surface Base Address',
+  '3DSTATE_DEPTH_BUFFER::Surface Pitch',
+  '3DSTATE_HIER_DEPTH_BUFFER::Surface Base Address',
+  '3DSTATE_HIER_DEPTH_BUFFER::Surface Pitch',
+  '3DSTATE_STENCIL_BUFFER::Surface Base Address',
+  '3DSTATE_STENCIL_BUFFER::Surface Pitch',
+  '3DSTATE_SO_BUFFER::Surface Base Address',
+  '3DSTATE_SO_BUFFER::Stream Offset',
   'MI_BATCH_BUFFER_START::Batch Buffer Start Address',
   'MI_REPORT_PERF_COUNT::Memory Address',
   'MI_STORE_DATA_IMM::Address',
@@ -61,6 +124,7 @@ genX_bits_included_symbols = [
   'COMPUTE_WALKER::body',
   'EXECUTE_INDIRECT_DISPATCH::body',
   # structures
+  '3DSTATE_CONSTANT_ALL_DATA',
   'RENDER_SURFACE_STATE::Surface Base Address',
   'RENDER_SURFACE_STATE::Surface Pitch',
   'RENDER_SURFACE_STATE::Surface QPitch',
@@ -81,6 +145,7 @@ genX_bits_included_symbols = [
   'CLEAR_COLOR',
   'VERTEX_BUFFER_STATE::Buffer Starting Address',
   'CPS_STATE',
+  'INTERFACE_DESCRIPTOR_DATA',
   'RT_DISPATCH_GLOBALS::Hit Group Table',
   'RT_DISPATCH_GLOBALS::Miss Group Table',
   'RT_DISPATCH_GLOBALS::Callable Group Table',
@@ -91,6 +156,7 @@ genX_bits_included_symbols = [
   'RT_DISPATCH_GLOBALS::Launch Height',
   'RT_DISPATCH_GLOBALS::Launch Depth',
   'SAMPLER_STATE',
+  'VERTEX_ELEMENT_STATE',
 ]
 
 genX_bits_h = custom_target(

src/intel/vulkan/meson.build
@@ -160,6 +160,7 @@ libanv_files = files(
   'anv_cmd_buffer.c',
   'anv_descriptor_set.c',
   'anv_device.c',
+  'anv_dgc_layout.c',
   'anv_dgc_set.c',
   'anv_embedded_sampler.c',
   'anv_event.c',

sailfishos-mirror/mesa

commit 1a35039f5aa0c7f30c220575e7a5df13fad46eff

anv: fix push constant address emission for gfx commands
We're only storing 4 pointers to push buffers. But the HW can handle 4
different pointers per stage.

I think we missed this because CTS doesn't use much more than just
vkCmdPushConstants().

The push pointer optimization likely made this a lot more problematic
as we're using more HW slots.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: fb26ed6bf7 ("anv: add indirect command layout support")
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42487>

src/intel/shaders/dgc.cl
@@ -39,7 +39,7 @@ write_3DSTATE_CONSTANT_ALL(global void *dst_ptr,
                            global void *push_data_addr,
                            global struct anv_dgc_push_stage_state *stage_state,
                            global struct anv_dgc_gfx_state *state,
-                           uint32_t stage_enabled)
+                           enum anv_dgc_stage stage)
 {
    uint32_t n_slots = stage_state->legacy.n_slots;
    struct GENX(3DSTATE_CONSTANT_ALL) v = {
@@ -47,7 +47,7 @@ write_3DSTATE_CONSTANT_ALL(global void *dst_ptr,
       .DWordLength        = GENX(3DSTATE_CONSTANT_ALL_length) -
                             GENX(3DSTATE_CONSTANT_ALL_length_bias) +
                             n_slots * GENX(3DSTATE_CONSTANT_ALL_DATA_length),
-      .ShaderUpdateEnable = stage_enabled,
+      .ShaderUpdateEnable = BITFIELD_BIT(stage),
       .MOCS               = state->layout.push_constants.mocs,
       .PointerBufferMask  = (1u << n_slots) - 1,
    };
@@ -67,7 +67,7 @@ write_3DSTATE_CONSTANT_ALL(global void *dst_ptr,
       } else {
          struct GENX(3DSTATE_CONSTANT_ALL_DATA) vd = {
             .ConstantBufferReadLength = slot.push_data_size / 32,
-            .PointerToConstantBuffer  = state->push_constants.addresses[i],
+            .PointerToConstantBuffer  = state->push_constants.stages[stage].addresses[i],
          };
          GENX(3DSTATE_CONSTANT_ALL_DATA_pack)(dst_ptr, &vd);
       }
@@ -96,19 +96,17 @@ write_3DSTATE_CONSTANT_XS(global void *dst_ptr,
                           global void *push_data_addr,
                           global struct anv_dgc_push_stage_state *stage_state,
                           global struct anv_dgc_gfx_state *state,
-                          uint32_t stage_enabled)
+                          enum anv_dgc_stage stage)
 {
    uint32_t opcode;
-   if (stage_enabled & BITFIELD_BIT(ANV_DGC_STAGE_VERTEX))
-      opcode = 21;
-   else if (stage_enabled & BITFIELD_BIT(ANV_DGC_STAGE_TESS_CTRL))
-      opcode = 25;
-   else if (stage_enabled & BITFIELD_BIT(ANV_DGC_STAGE_TESS_EVAL))
-      opcode = 26;
-   else if (stage_enabled & BITFIELD_BIT(ANV_DGC_STAGE_GEOMETRY))
-      opcode = 22;
-   else
-      opcode = 23;
+   switch (stage) {
+   case ANV_DGC_STAGE_VERTEX:    opcode = 21; break;
+   case ANV_DGC_STAGE_TESS_CTRL: opcode = 25; break;
+   case ANV_DGC_STAGE_TESS_EVAL: opcode = 26; break;
+   case ANV_DGC_STAGE_GEOMETRY:  opcode = 22; break;
+   case ANV_DGC_STAGE_FRAGMENT:  opcode = 23; break;
+   default:                      opcode = 0;  break;
+   }
 
    struct GENX(3DSTATE_CONSTANT_VS) v = {
       GENX(3DSTATE_CONSTANT_VS_header),
@@ -116,16 +114,16 @@ write_3DSTATE_CONSTANT_XS(global void *dst_ptr,
       .ConstantBody = {
          .Buffer = {
             pc_slot_address(&stage_state->legacy.slots[0],
-                            &state->push_constants.addresses[0],
+                            &state->push_constants.stages[stage].addresses[0],
                             push_data_addr),
             pc_slot_address(&stage_state->legacy.slots[1],
-                            &state->push_constants.addresses[1],
+                            &state->push_constants.stages[stage].addresses[1],
                             push_data_addr),
             pc_slot_address(&stage_state->legacy.slots[2],
-                            &state->push_constants.addresses[2],
+                            &state->push_constants.stages[stage].addresses[2],
                             push_data_addr),
             pc_slot_address(&stage_state->legacy.slots[3],
-                            &state->push_constants.addresses[3],
+                            &state->push_constants.stages[stage].addresses[3],
                             push_data_addr),
          },
          .ReadLength = {
@@ -249,14 +247,12 @@ write_gfx_push_constant_commands(global void *push_cmd_ptr,
       cmd_offset += write_3DSTATE_CONSTANT_ALL(push_cmd_ptr + cmd_offset,
                                                push_data_ptr,
                                                stage_state,
-                                               state,
-                                               BITFIELD_BIT(s));
+                                               state, s);
 #else
       cmd_offset += write_3DSTATE_CONSTANT_XS(push_cmd_ptr + cmd_offset,
                                               push_data_ptr,
                                               stage_state,
-                                               state,
-                                              BITFIELD_BIT(s));
+                                              state, s);
 #endif
 
       push_stages &= ~BITFIELD_BIT(s);

src/intel/vulkan/anv_dgc_layout.c
@@ -379,12 +379,13 @@ anv_dgc_fill_gfx_state(struct anv_dgc_gfx_state *state,
 
    if (vk_layout->dgc_info & (BITFIELD_BIT(MESA_VK_DGC_PC) |
                               BITFIELD_BIT(MESA_VK_DGC_SI))) {
-
-      for (uint32_t i = 0; i < ANV_GRAPHICS_SHADER_STAGE_COUNT; i++) {
-         if (shaders[i] == NULL)
+      for (uint32_t s = 0; s < ANV_GRAPHICS_SHADER_STAGE_COUNT; s++) {
+         if (shaders[s] == NULL)
             continue;
 
-         const struct anv_pipeline_bind_map *bind_map = &shaders[i]->bind_map;
+         const struct anv_pipeline_bind_map *bind_map = &shaders[s]->bind_map;
+
+         enum anv_dgc_stage gen_stage = anv_mesa_stage_to_dgc_stage(s);
          for (uint32_t i = 0; i < ARRAY_SIZE(bind_map->push_ranges); i++) {
             const struct anv_push_range *range = &bind_map->push_ranges[i];
             if (range->length == 0)
@@ -393,14 +394,14 @@ anv_dgc_fill_gfx_state(struct anv_dgc_gfx_state *state,
             switch (range->set) {
             case ANV_DESCRIPTOR_SET_DESCRIPTORS:
                if (bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_BUFFER) {
-                  state->push_constants.addresses[i] =
+                  state->push_constants.stages[gen_stage].addresses[i] =
                      anv_cmd_buffer_descriptor_buffer_address(
                         cmd_buffer,
                         gfx->base.descriptor_buffers[range->index].buffer_index) +
                      gfx->base.descriptor_buffers[range->index].buffer_offset;
                } else {
                   struct anv_descriptor_set *set = gfx->base.descriptors[range->index];
-                  state->push_constants.addresses[i] = anv_address_physical(
+                  state->push_constants.stages[gen_stage].addresses[i] = anv_address_physical(
                      anv_descriptor_set_address(set));
                }
                break;
@@ -410,8 +411,8 @@ anv_dgc_fill_gfx_state(struct anv_dgc_gfx_state *state,
 
             case ANV_DESCRIPTOR_SET_NULL:
             case ANV_DESCRIPTOR_SET_PER_PRIM_PADDING:
-               state->push_constants.addresses[i] =
-                     anv_address_physical(device->workaround_address);
+               state->push_constants.stages[gen_stage].addresses[i] =
+                  anv_address_physical(device->workaround_address);
                break;
 
             default: {
@@ -421,9 +422,10 @@ anv_dgc_fill_gfx_state(struct anv_dgc_gfx_state *state,
 
                if (desc->type == VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER) {
                   if (desc->buffer) {
-                     state->push_constants.addresses[i] = anv_address_physical(
-                        anv_address_add(desc->buffer->address,
-                                        desc->offset));
+                     state->push_constants.stages[gen_stage].addresses[i] =
+                        anv_address_physical(
+                           anv_address_add(desc->buffer->address,
+                                           desc->offset));
                   }
                } else {
                   assert(desc->type == VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC);
@@ -432,25 +434,27 @@ anv_dgc_fill_gfx_state(struct anv_dgc_gfx_state *state,
                      uint32_t dynamic_offset =
                         pipe_state->dynamic_offsets[
                            range->set].offsets[range->dynamic_offset_index];
-                     state->push_constants.addresses[i] = anv_address_physical(
-                        anv_address_add(desc->buffer->address,
+                     state->push_constants.stages[gen_stage].addresses[i] =
+                        anv_address_physical(
+                           anv_address_add(desc->buffer->address,
                                            desc->offset + dynamic_offset));
                   }
                }
 
-               if (state->push_constants.addresses[i] == 0) {
+               if (state->push_constants.stages[gen_stage].addresses[i] == 0) {
                   /* For NULL UBOs, we just return an address in the
                    * workaround BO. We do writes to it for workarounds but
                    * always at the bottom. The higher bytes should be all
                    * zeros.
                    */
                   assert(range->length * 32 <= 2048);
-                  state->push_constants.addresses[i] =
+                  state->push_constants.stages[gen_stage].addresses[i] =
                      anv_address_physical((struct anv_address) {
                            .bo = device->workaround_bo,
                            .offset = 1024,
                         });
                }
+               break;
             }
             }
          }

src/intel/vulkan/anv_dgc_set.c
@@ -10,6 +10,24 @@
 
 #include "anv_private.h"
 
+
+enum anv_dgc_stage
+anv_mesa_stage_to_dgc_stage(mesa_shader_stage stage)
+{
+   static const enum anv_dgc_stage stages[] = {
+      [MESA_SHADER_VERTEX] = ANV_DGC_STAGE_VERTEX,
+      [MESA_SHADER_TESS_CTRL] = ANV_DGC_STAGE_TESS_CTRL,
+      [MESA_SHADER_TESS_EVAL] = ANV_DGC_STAGE_TESS_EVAL,
+      [MESA_SHADER_GEOMETRY]  = ANV_DGC_STAGE_GEOMETRY,
+      [MESA_SHADER_FRAGMENT]  = ANV_DGC_STAGE_FRAGMENT,
+      [MESA_SHADER_MESH]      = ANV_DGC_STAGE_MESH,
+      [MESA_SHADER_TASK]      = ANV_DGC_STAGE_TASK,
+      [MESA_SHADER_COMPUTE]   = ANV_DGC_STAGE_COMPUTE,
+   };
+   assert(stage < ARRAY_SIZE(stages));
+   return stages[stage];
+}
+
 enum anv_dgc_stage
 anv_vk_stage_to_dgc_stage(VkShaderStageFlags vk_stage)
 {

src/intel/vulkan/anv_private.h
@@ -4682,6 +4682,12 @@ struct anv_cmd_graphics_state {
    enum anv_depth_reg_mode                      depth_reg_mode;
 
    struct anv_gfx_dynamic_state dyn_state;
+
+   /**
+    * Temporary state of DGC graphics preprocess emission (to avoid having it
+    * on the stack when calling vkCmdPreprocessGeneratedCommandsEXT)
+    */
+   struct anv_dgc_gfx_state dgc_state;
 };
 
 /** State tracking for compute pipeline
@@ -6708,6 +6714,7 @@ void anv_write_gfx_indirect_descriptor(struct anv_device *device,
                                        struct anv_dgc_gfx_descriptor *descriptor,
                                        struct anv_cmd_graphics_state *gfx);
 
+enum anv_dgc_stage anv_mesa_stage_to_dgc_stage(mesa_shader_stage stage);
 enum anv_dgc_stage anv_vk_stage_to_dgc_stage(VkShaderStageFlags vk_stage);
 
 uint32_t anv_vk_stages_to_generated_stages(VkShaderStageFlags vk_stages);

src/intel/vulkan/anv_types.h
@@ -410,7 +410,9 @@ struct anv_dgc_gfx_state {
    struct anv_dgc_gfx_descriptor descriptor;
 
    struct {
-      uint64_t addresses[4];
+      struct {
+         uint64_t addresses[4];
+      } stages[ANV_DGC_N_GFX_STAGES];
    } push_constants;
 
    struct {

src/intel/vulkan/genX_cmd_dgc.c
@@ -62,20 +62,21 @@ preprocess_gfx_sequences(struct anv_cmd_buffer *cmd_buffer,
       return NULL;
 
    /**/
-   struct anv_dgc_gfx_state gfx_state = {};
+   struct anv_dgc_gfx_state *gfx_state = &cmd_buffer->state.gfx.dgc_state;
+   memset(gfx_state, 0, sizeof(*gfx_state));
    struct anv_state gfx_state_state =
-      anv_cmd_buffer_alloc_temporary_state(cmd_buffer, sizeof(gfx_state), 8);
+      anv_cmd_buffer_alloc_temporary_state(cmd_buffer, sizeof(*gfx_state), 8);
    if (gfx_state_state.map == NULL)
       return NULL;
 
    uint32_t cmd_stride = anv_dgc_fill_gfx_layout(
-      &gfx_state.layout, device, layout, gfx->shaders);
+      &gfx_state->layout, device, layout, gfx->shaders);
    anv_dgc_fill_gfx_state(
-      &gfx_state, cmd_buffer_state, layout, gfx->shaders);
-   anv_write_gfx_indirect_descriptor(device, &gfx_state.descriptor,
+      gfx_state, cmd_buffer_state, layout, gfx->shaders);
+   anv_write_gfx_indirect_descriptor(device, &gfx_state->descriptor,
                                      &cmd_buffer_state->state.gfx);
 
-   memcpy(gfx_state_state.map, &gfx_state, sizeof(gfx_state));
+   memcpy(gfx_state_state.map, gfx_state, sizeof(*gfx_state));
 
    /**/
    struct anv_shader_internal *generate_kernel;