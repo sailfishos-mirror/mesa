alexvorxx/zink-xlib-termux

commit 899fcaf23744778bb16662185c26b9df982c5365

zink: Avoid issues when kopper tries using XCB WSI on Apple.
On macOS builds the code is tryng to use X11 and XCB code paths for WSI functions and structures,
i.e. `VkXcbSurfaceCreateInfoKHR` and `vkCreateXcbSurfaceKHR()`.
The MoltenVK implementation only supports `VkMacOSSurfaceCreateInfoMVK` and `vkCreateMacOSSurfaceMVK()`.
To get these working correctly Metal diusplay surfaces need to be setup, which xquarts does not expose.

Until this situation is resolved correctly the `VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR` code paths has been
changed to return `VK_INCOMPLETE`. This allows them to run without asserting and terminating the runtime.
This allows for non-display applicaitons, like `glxinfo`, to run without terminating.

Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28259>

src/gallium/drivers/zink/zink_kopper.c
@@ -87,8 +87,12 @@ kopper_CreateSurface(struct zink_screen *screen, struct kopper_displaytarget *cd
    switch (type) {
 #ifdef VK_USE_PLATFORM_XCB_KHR
    case VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR: {
+#ifdef GLX_USE_APPLE
+      error = VK_INCOMPLETE;
+#else
       VkXcbSurfaceCreateInfoKHR *xcb = (VkXcbSurfaceCreateInfoKHR *)&cdt->info.bos;
       error = VKSCR(CreateXcbSurfaceKHR)(screen->instance, xcb, NULL, &surface);
+#endif
       break;
    }
 #endif

sailfishos-mirror/mesa

commit 6a943a2660b0b4214fff0f1f7164b2f9bf569d0f

zink: fix GLX_USE_APPLE typo (should be GLX_USE_APPLEGL)
The intent of these guards is "skip on the AppleGL/XQuartz DRI platform"; the macro for that is
GLX_USE_APPLEGL, not GLX_USE_APPLE.

Fixes: 899fcaf2374 ("zink: Avoid issues when kopper tries using XCB WSI on Apple.")
Fixes: 191d7c6cb61 ("kopper: Add '#if' guard around `loader_dri3_get_pixmap_buffer` to stop missing symbol on MacOS.")
Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42541>

src/gallium/drivers/zink/zink_kopper.c
@@ -96,7 +96,7 @@ kopper_CreateSurface(struct zink_screen *screen, struct kopper_displaytarget *cd
    switch (type) {
 #ifdef VK_USE_PLATFORM_XCB_KHR
    case VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR: {
-#ifdef GLX_USE_APPLE
+#ifdef GLX_USE_APPLEGL
       error = VK_INCOMPLETE;
 #else
       VkXcbSurfaceCreateInfoKHR *xcb = (VkXcbSurfaceCreateInfoKHR *)&cdt->info.bos;

src/gallium/frontends/dri/kopper.c
@@ -152,7 +152,7 @@ kopper_get_pixmap_buffer(struct dri_drawable *drawable,
     */
    struct dri_screen *screen = drawable->screen;
 
-#ifndef GLX_USE_APPLE
+#ifndef GLX_USE_APPLEGL
    drawable->image = loader_dri3_get_pixmap_buffer(conn, pixmap, screen,
                                                    fourcc, drawable->screen->has_multibuffer, &width, &height, drawable);
 #else