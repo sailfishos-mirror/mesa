joshuaswarren/mesa-1

anv/brw: fixup 64bit array image accesses
We forgot to handle sliced view of an arrayed image.

The base address of the HW descriptor not changing we need to read the
base array index and add it to the Z coordinate to compute the right
texel location.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: ef026bb038 ("anv: Support putting image base address and image params in surface state")
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42522>

src/intel/vulkan/anv_nir_apply_pipeline_layout.c
@@ -1707,6 +1707,12 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
             offsetof(struct anv_storage_image_descriptor, format),
             1, 32, state);
          break;
+      case ISL_SURF_PARAM_MIN_ARRAY_ELEMENT:
+         desc = build_load_descriptor_mem_from_res_index(
+            b, res_index,
+            offsetof(struct anv_storage_image_descriptor, min_array_element),
+            1, 32, state);
+         break;
       default:
          UNREACHABLE("Invalid surface parameter");
       }
@@ -1717,7 +1723,7 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
       case ISL_SURF_PARAM_BASE_ADDRESSS: {
          desc = build_load_descriptor_mem_from_res_index(
             b, res_index,
-            RENDER_SURFACE_STATE_SurfaceBaseAddress_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfaceBaseAddress_start(devinfo) / 32),
             intrin->def.num_components,
             intrin->def.bit_size, state);
          break;
@@ -1726,7 +1732,8 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
          nir_def *dword =
             build_load_descriptor_mem_from_res_index(
                b, res_index,
-               RENDER_SURFACE_STATE_TileMode_start(devinfo) / 32, 1, 32, state);
+               4 * (RENDER_SURFACE_STATE_TileMode_start(devinfo) / 32),
+               1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, dword,
             RENDER_SURFACE_STATE_TileMode_start(devinfo) % 32,
@@ -1737,7 +1744,7 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
          assert(RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) % 32 == 0);
          nir_def *pitch_dword = build_load_descriptor_mem_from_res_index(
             b, res_index,
-            RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) / 32),
             1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, pitch_dword,
@@ -1751,7 +1758,7 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
          assert(RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) % 32 == 0);
          nir_def *pitch_dword = build_load_descriptor_mem_from_res_index(
             b, res_index,
-            RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) / 32),
             1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, pitch_dword,
@@ -1764,12 +1771,23 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
       case ISL_SURF_PARAM_FORMAT: {
          nir_def *format_dword = build_load_descriptor_mem_from_res_index(
             b, res_index,
-            RENDER_SURFACE_STATE_SurfaceFormat_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfaceFormat_start(devinfo) / 32),
             1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, format_dword,
             RENDER_SURFACE_STATE_SurfaceFormat_start(devinfo) % 32,
             RENDER_SURFACE_STATE_SurfaceFormat_bits(devinfo));
+         break;
+      }
+      case ISL_SURF_PARAM_MIN_ARRAY_ELEMENT: {
+         nir_def *min_arr_el_dword = build_load_descriptor_mem_from_res_index(
+            b, res_index,
+            4 * (RENDER_SURFACE_STATE_MinimumArrayElement_start(devinfo) / 32),
+            1, 32, state);
+         desc = nir_ubitfield_extract_imm(
+            b, min_arr_el_dword,
+            RENDER_SURFACE_STATE_MinimumArrayElement_start(devinfo) % 32,
+            RENDER_SURFACE_STATE_MinimumArrayElement_bits(devinfo));
          break;
       }
       default:

sailfishos-mirror/mesa

Fixes: 723e52cbcc ("anv: Support putting image base address and image params in surface state")
(cherry picked from commit be89a173d5000483ba08ef42f8a544b0789bc011)
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42587>

src/intel/vulkan/anv_nir_apply_pipeline_layout.c
@@ -1770,6 +1770,12 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
             offsetof(struct anv_storage_image_descriptor, format),
             1, 32, state);
          break;
+      case ISL_SURF_PARAM_MIN_ARRAY_ELEMENT:
+         desc = build_load_descriptor_mem(
+            b, desc_addr,
+            offsetof(struct anv_storage_image_descriptor, min_array_element),
+            1, 32, state);
+         break;
       default:
          UNREACHABLE("Invalid surface parameter");
       }
@@ -1780,7 +1786,7 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
       case ISL_SURF_PARAM_BASE_ADDRESSS: {
          desc = build_load_descriptor_mem(
             b, desc_addr,
-            RENDER_SURFACE_STATE_SurfaceBaseAddress_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfaceBaseAddress_start(devinfo) / 32),
             intrin->def.num_components,
             intrin->def.bit_size, state);
          break;
@@ -1789,7 +1795,8 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
          nir_def *dword =
             build_load_descriptor_mem(
                b, desc_addr,
-               RENDER_SURFACE_STATE_TileMode_start(devinfo) / 32, 1, 32, state);
+               4 * (RENDER_SURFACE_STATE_TileMode_start(devinfo) / 32),
+               1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, dword,
             RENDER_SURFACE_STATE_TileMode_start(devinfo) % 32,
@@ -1800,7 +1807,7 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
          assert(RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) % 32 == 0);
          nir_def *pitch_dword = build_load_descriptor_mem(
             b, desc_addr,
-            RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) / 32),
             1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, pitch_dword,
@@ -1814,7 +1821,7 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
          assert(RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) % 32 == 0);
          nir_def *pitch_dword = build_load_descriptor_mem(
             b, desc_addr,
-            RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) / 32),
             1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, pitch_dword,
@@ -1827,12 +1834,23 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
       case ISL_SURF_PARAM_FORMAT: {
          nir_def *format_dword = build_load_descriptor_mem(
             b, desc_addr,
-            RENDER_SURFACE_STATE_SurfaceFormat_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfaceFormat_start(devinfo) / 32),
             1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, format_dword,
             RENDER_SURFACE_STATE_SurfaceFormat_start(devinfo) % 32,
             RENDER_SURFACE_STATE_SurfaceFormat_bits(devinfo));
+         break;
+      }
+      case ISL_SURF_PARAM_MIN_ARRAY_ELEMENT: {
+         nir_def *min_arr_el_dword = build_load_descriptor_mem(
+            b, desc_addr,
+            4 * (RENDER_SURFACE_STATE_MinimumArrayElement_start(devinfo) / 32),
+            1, 32, state);
+         desc = nir_ubitfield_extract_imm(
+            b, min_arr_el_dword,
+            RENDER_SURFACE_STATE_MinimumArrayElement_start(devinfo) % 32,
+            RENDER_SURFACE_STATE_MinimumArrayElement_bits(devinfo));
          break;
       }
       default: