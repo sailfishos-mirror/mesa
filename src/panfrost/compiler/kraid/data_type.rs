sailfishos-mirror/mesa

commit 9b0f60a76e5b03ec9bbad415512910e2172ec0fe

kraid: Add a DataType::SR special case
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42608>

src/panfrost/compiler/kraid/data_type.rs
@@ -45,6 +45,7 @@ pub enum NumericType {
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, DataType)]
 pub enum PartialDataType {
     None,
+    SR,
     A16,
     A32,
     F16,
@@ -126,6 +127,8 @@ impl PartialDataType {
     pub fn specialize(self, other: DataType) -> DataType {
         if self == PartialDataType::None {
             return other;
+        } else if self == PartialDataType::SR {
+            return DataType::SR;
         }
 
         let (comps, num_type, bits) = self.to_pieces();
@@ -141,6 +144,11 @@ impl PartialDataType {
 /// Data type
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, DataType)]
 pub enum DataType {
+    /// A special data type for staging registers.  This type does not allow
+    /// swizzles of any sort and takes however many registers it's given.  This
+    /// is used for texture and similar ops which need both a variable number
+    /// of source and destination registers.
+    SR,
     A16,
     A32,
     F16,

src/panfrost/compiler/kraid/proc/data_type.rs
@@ -56,6 +56,17 @@ pub fn derive_data_type(input: TokenStream) -> TokenStream {
 
     for v in e.variants {
         let name = v.ident.to_string();
+        if name == "SR" {
+            to_cases.extend(quote! {
+                #enum_type::SR => (0, Some(NumericType::Auto), 32),
+            });
+            // from_cases is intentionally omitted
+            fmt_cases.extend(quote! {
+                #enum_type::SR => write!(f, "sr"),
+            });
+            continue;
+        }
+
         let (comps, num_type, bits) = parse_type_name(&name);
 
         // We need an actual ident for num_type

src/panfrost/compiler/kraid/ra.rs
@@ -426,9 +426,13 @@ impl LocalRegAlloc<'_> {
 
     fn choose_src_bytes(&self, op: &Op, src: &Src) -> Range<u16> {
         let vec = src.src_ref.as_ssa().unwrap();
+        let src_type = op.src_type(src);
         let bytes = vec.bytes();
 
-        let (align_mul, align_offsets) = if bytes > 4 {
+        let (align_mul, align_offsets) = if src_type == DataType::SR {
+            assert!(src.swizzle.is_none());
+            (bytes.next_power_of_two(), 1 << 0)
+        } else if bytes > 4 {
             (bytes.next_power_of_two(), 1 << 0)
         } else {
             let swizzles: &[(u8, Swizzle)] = match bytes {

commit 416e57df4951cce90dc830edd5c327b81dae0cb7

kraid/data_type: Handle SR in as_data_type()
Fixes: 9b0f60a76e5b ("kraid: Add a DataType::SR special case")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42686>

src/panfrost/compiler/kraid/data_type.rs
@@ -120,6 +120,9 @@ impl PartialDataType {
     }
 
     pub fn as_data_type(self) -> DataType {
+        if self == PartialDataType::SR {
+            return DataType::SR;
+        }
         let (comps, num_type, bits) = self.to_pieces();
         DataType::from_pieces(comps, num_type, bits)
     }