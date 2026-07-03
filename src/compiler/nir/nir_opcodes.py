sailfishos-mirror/mesa

commit 045ae759a5482bfa441cf41d5e878affbb432e1c

nir: Specify f2i/f2u as undefined if the float is out of range of the int.
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39076>

src/compiler/nir/nir_opcodes.py
@@ -336,6 +336,26 @@ for src_t in [tint, tuint, tfloat, tbool]:
               unop_numeric_convert("{0}2{1}{2}".format(src_t[0], dst_t[0],
                                                        dst_bit_size),
                                    dst_t + str(dst_bit_size), src_t, conv_expr)
+          elif src_t == tfloat and dst_t in [tint, tuint]:
+              # SPIRV specifies that OpConvertFToU and OpConvertFToS are UB if
+              # the input is out of range of the output.  glsl 4.60 just says
+              # "It is undefined to convert a negative floating-point value to
+              # an uint.", but we define the NIR opcodes the SPIRV way.
+              if dst_t == tuint:
+                   min = "0.0"
+                   max = "u_uintN_max({})".format(dst_bit_size)
+              else:
+                   min = "u_intN_min({})".format(dst_bit_size)
+                   max = "u_intN_max({})".format(dst_bit_size)
+              conv_expr = f"""
+                dst = src0;
+                if (src0 < {min} || src0 > {max}) {{
+                   poison = true;
+                }}
+              """
+              unop_numeric_convert("{0}2{1}{2}".format(src_t[0], dst_t[0],
+                                                       dst_bit_size),
+                                   dst_t + str(dst_bit_size), src_t, conv_expr)
           else:
               conv_expr = "src0 != 0" if dst_t == tbool else "src0"
               unop_numeric_convert("{0}2{1}{2}".format(src_t[0], dst_t[0],

commit dcf3bd4046148fc838a7dcfb941e26f6ae3ecd72

nir: fix f2u/f2i constant folding to poison NaN and out-of-range inputs
Two fixes to the conv_expr template used for f2u*/f2i* constant folding:

- The original code did not check for NaN inputs at all; a NaN src0
  would silently produce an undefined integer result.  Add an isnan()
  check so NaN inputs set the poison flag.

- The range check used `src0 > u_uintN_max(N)` / `src0 > u_intN_max(N)`.
  When these integer bounds are converted to float for comparison they
  round up to the next power of 2, making the `>` test false for inputs
  that are out of range but equal to that power of 2.  Use `>= 2^N` /
  `>= 2^(N-1)` instead; powers of 2 are exactly representable in IEEE
  754.

Fixes: 045ae759a54 ("nir: Specify f2i/f2u as undefined if the float is out of range of the int.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42236>

src/compiler/nir/nir_opcodes.py
@@ -362,14 +362,20 @@ for src_t in [tint, tuint, tfloat, tbool]:
               # an uint.", but we define the NIR opcodes the SPIRV way.
               if dst_t == tuint:
                    min = "0.0"
-                   max = "u_uintN_max({})".format(dst_bit_size)
+                   # 2^dst_bit_size is a power of 2 and exactly representable
+                   # in float, avoiding the rounding issue with u_uintN_max().
+                   max = "{}.0".format(2 ** dst_bit_size)
               else:
                    min = "u_intN_min({})".format(dst_bit_size)
-                   max = "u_intN_max({})".format(dst_bit_size)
+                   # 2^(dst_bit_size-1) is exactly representable in float,
+                   # unlike i_intN_max() which rounds up when converted.
+                   max = "{}.0".format(2 ** (dst_bit_size - 1))
               conv_expr = f"""
-                dst = src0;
-                if (src0 < {min} || src0 > {max}) {{
+                if (isnan(src0) || src0 < {min} || src0 >= {max}) {{
                    poison = true;
+                   dst = 0;
+                }} else {{
+                   dst = src0;
                 }}
               """
               unop_numeric_convert("{0}2{1}{2}".format(src_t[0], dst_t[0],