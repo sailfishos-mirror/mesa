sailfishos-mirror/mesa

commit 2d466545f07073dcbac422358bf14a6e480c2e21

pan/crc: Disallow CRC on sparse AFBC images
AFBC-P images are read only.

Signed-off-by: Loïc Molinari <loic.molinari@collabora.com>
Signed-off-by: Ashley Smith <ashley.smith@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Acked-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39216>

src/panfrost/lib/pan_desc.c
@@ -107,6 +107,10 @@ pan_fb_color_attachment_should_crc(const struct pan_fb_color_attachment *rt,
    if (!drm_is_afbc(mod))
       return true;
 
+   /* Disallow CRC on sparse AFBC images */
+   if (!(mod & AFBC_FORMAT_MOD_SPARSE))
+      return false;
+
    /* AFBC render block size must fit in a single pass. */
    if (pan_afbc_superblock_exceeds_tile_size(mod, tile_size))
       return false;

commit 2ff34486325f134e8696eda1086d9a222a3d08b6

pan/crc: Fix CRC check for sparse AFBC images
The check was accidentally reversed it looks like.

Fixes: 2d466545f070 (pan/crc: Disallow CRC on sparse AFBC images)
Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42753>

src/panfrost/lib/pan_desc.c
@@ -113,7 +113,7 @@ pan_fb_color_attachment_should_crc(const struct pan_fb_color_attachment *rt,
       return true;
 
    /* Disallow CRC on sparse AFBC images */
-   if (!(mod & AFBC_FORMAT_MOD_SPARSE))
+   if (mod & AFBC_FORMAT_MOD_SPARSE)
       return false;
 
    /* AFBC render block size must fit in a single pass. */