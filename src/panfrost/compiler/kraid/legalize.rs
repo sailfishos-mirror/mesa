c678732bd47d5072a934b46864f590afb1c9e00f
Faith Ekstrand <faith.ekstrand@collabora.com>
2026-07-09 02:44:47 -0400

    kraid: Merge legalize_immmmediates and legalize_vec_srcs
    This gives us a single pass that can invoke a variety of heuristics that
    we need to run right before RA.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42847>

--- src/panfrost/compiler/kraid/legalize.rs
+++ src/panfrost/compiler/kraid/legalize.rs
@@ -1,9 +1,32 @@
 // Copyright © 2026 Collabora, Ltd.
 // SPDX-License-Identifier: MIT
 
+use std::mem;
+
 use crate::builder::*;
 use crate::ir::*;
+use crate::ops::OpMov;
 use compiler::bitset::BitSet;
+
+fn legalize_imm_src(b: &mut impl SSABuilder, op: &mut Op, src_idx: usize) {
+    let src = &op.srcs()[src_idx];
+    let SrcRef::Imm32(imm32) = &src.src_ref else {
+        return;
+    };
+    if b.model().op_src_supports_imm32(op, src, (*imm32).into()) {
+        return;
+    }
+
+    let ssa = b.alloc_ssa(32);
+    let src = &mut op.srcs_mut()[src_idx];
+    let src_ref = mem::replace(&mut src.src_ref, ssa.into());
+
+    b.push_op(OpMov {
+        dst: ssa.into(),
+        dst_type: DataType::I32,
+        src: src_ref.into(),
+    });
+}
 
 #[derive(Default)]
 struct SSAValueSet {
@@ -36,7 +59,14 @@ impl SSAValueSet {
     }
 }
 
-fn legalize_vec_srcs_instr(
+/// Legalizes vector sources by ensuring the following
+///
+///  1. For any vector source (SSARef::comps() > 1), all the SSAValues
+///     referenced by the SSARef are unique.
+///
+///  2. For any two vector sources, either they are identical or they have
+///     no SSAValues in common.
+fn legalize_vec_srcs(
     b: &mut impl SSABuilder,
     instr: &mut Instr,
     ssa_used: &mut SSAValueSet,
@@ -79,19 +109,15 @@ fn legalize_vec_srcs_instr(
 }
 
 impl Shader<'_> {
-    /// Legalizes vector sources by ensuring the following
-    ///
-    ///  1. For any vector source (SSARef::comps() > 1), all the SSAValues
-    ///     referenced by the SSARef are unique.
-    ///
-    ///  2. For any two vector sources, either they are identical or they have
-    ///     no SSAValues in common.
-    pub fn legalize_vec_srcs(&mut self) {
+    pub fn legalize(&mut self) {
         let model = self.model;
         let mut ssa_used = SSAValueSet::new();
         self.map_instrs(|mut instr, ssa_alloc| {
             let mut b = SSAInstrBuilder::new(model, ssa_alloc);
-            legalize_vec_srcs_instr(&mut b, &mut instr, &mut ssa_used);
+            legalize_vec_srcs(&mut b, &mut instr, &mut ssa_used);
+            for src_idx in 0..instr.srcs().len() {
+                legalize_imm_src(&mut b, &mut instr, src_idx)
+            }
             b.push_instr(instr);
             b.into_mapped()
         });
