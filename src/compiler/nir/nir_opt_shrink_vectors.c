sailfishos-mirror/mesa

commit 2e271085cd3d836ae7c19eddff9f0876dbad0902

nir: Early out in nir_opt_shrink_vectors if all components are read
This potentially saves some processing and means we only have to think
about the case where things can actually be shrunk.

Acked-by: Emma Anholt <anholt@igalia.com>
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42464>

src/compiler/nir/nir_opt_shrink_vectors.c
@@ -87,8 +87,8 @@ shrink_dest_to_read_mask(nir_def *def, bool shrink_start)
 
    unsigned mask = nir_def_components_read(def);
 
-   /* If nothing was read, leave it up to DCE. */
-   if (!mask)
+   /* If nothing was read, DCE.  If everything was read, early out. */
+   if (!mask || mask == nir_component_mask(def->num_components))
       return false;
 
    nir_intrinsic_instr *intr = NULL;
@@ -435,8 +435,8 @@ opt_shrink_vectors_load_const(nir_load_const_instr *instr)
 
    unsigned mask = nir_def_components_read(def);
 
-   /* If nothing was read, leave it up to DCE. */
-   if (!mask)
+   /* If nothing was read, DCE.  If everything was read, early out. */
+   if (!mask || mask == nir_component_mask(def->num_components))
       return false;
 
    uint8_t reswizzle[NIR_MAX_VEC_COMPONENTS] = { 0 };
@@ -532,12 +532,8 @@ opt_shrink_vectors_phi(nir_builder *b, nir_phi_instr *instr)
       }
    }
 
-   /* DCE will handle this. */
-   if (mask == 0)
-      return false;
-
-   /* Nothing to shrink? */
-   if (BITFIELD_MASK(def->num_components) == mask)
+   /* If nothing was read, DCE.  If everything was read, early out. */
+   if (!mask || mask == nir_component_mask(def->num_components))
       return false;
 
    /* Set up the reswizzles. */

commit 55f3dff9d2f5594cdf1f7c89fa42a07060f74656

nir/opt_shrink_vectors: restore load_const deduplication
The early exit prevents trimming duplicated components
when all components are used.

Fixes: 2e271085cd3 ("nir: Early out in nir_opt_shrink_vectors if all components are read")
Tested-by: Michel Dänzer <mdaenzer@redhat.com>
Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42824>

src/compiler/nir/nir_opt_shrink_vectors.c
@@ -440,8 +440,8 @@ opt_shrink_vectors_load_const(nir_load_const_instr *instr)
 
    unsigned mask = nir_def_components_read(def);
 
-   /* If nothing was read, DCE.  If everything was read, early out. */
-   if (!mask || mask == nir_component_mask(def->num_components))
+   /* If nothing was read, DCE. */
+   if (!mask)
       return false;
 
    uint8_t reswizzle[NIR_MAX_VEC_COMPONENTS] = { 0 };