bminor/mesa-mesa

commit e0641fdac7a38a882c57d68282b020096902829d

zink: fix ZINK_RENDERDOC=all
the intent of this option is to create a single capture for the lifetime
of the app, which is great for unit test debugging, and this instead
created a capture for every queue submission, which is a nightmare

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35292>

src/gallium/drivers/zink/zink_context.c
@@ -121,6 +121,12 @@ zink_context_destroy(struct pipe_context *pctx)
    struct pipe_framebuffer_state fb = {0};
    pctx->set_framebuffer_state(pctx, &fb);
 
+   if (screen->base.num_contexts == 1 && screen->renderdoc_capturing) {
+      screen->renderdoc_capture_all = false;
+      ctx->bs->has_work = true;
+      pctx->flush(pctx, NULL, 0);
+   }
+
    if (util_queue_is_initialized(&screen->flush_queue))
       util_queue_finish(&screen->flush_queue);
    if (ctx->bs && !screen->device_lost) {

src/gallium/drivers/zink/zink_batch.c
@@ -937,7 +937,8 @@ zink_end_batch(struct zink_context *ctx)
       post_submit(bs, NULL, 0);
    }
 
-   if (!(ctx->flags & ZINK_CONTEXT_COPY_ONLY) && screen->renderdoc_capturing && p_atomic_read(&screen->renderdoc_frame) > screen->renderdoc_capture_end) {
+   if (!(ctx->flags & ZINK_CONTEXT_COPY_ONLY) && screen->renderdoc_capturing && !screen->renderdoc_capture_all &&
+       p_atomic_read(&screen->renderdoc_frame) > screen->renderdoc_capture_end) {
       screen->renderdoc_api->EndFrameCapture(RENDERDOC_DEVICEPOINTER_FROM_VKINSTANCE(screen->instance), NULL);
       screen->renderdoc_capturing = false;
    }

sailfishos-mirror/mesa

commit 551c8bbd3b16fd7bab40791e42fe0826352b2e3c

zink: fix the fix for ZINK_RENDERDOC=all
toggling the capture-all flag here breaks capture-all

Fixes: e0641fdac7a ("zink: fix ZINK_RENDERDOC=all")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42829>

src/gallium/drivers/zink/zink_context.c
@@ -108,7 +108,6 @@ zink_context_destroy(struct pipe_context *pctx)
 
 #if HAVE_RENDERDOC_INTEGRATION
    if (screen->base.num_contexts == 1 && screen->renderdoc_capturing) {
-      screen->renderdoc_capture_all = false;
       ctx->bs->has_work = true;
       pctx->flush(pctx, NULL, 0);
    }