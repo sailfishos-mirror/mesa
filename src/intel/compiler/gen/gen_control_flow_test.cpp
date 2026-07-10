sailfishos-mirror/mesa

commit 11dde62fcc877c471613f049fba9a8bcc9d1f9fb

intel/gen: Add function to finish structured control flow
Given an array of gen_insts representing a structured program,
fill in the missing JIPs and UIPs to follow that structure.

The input array must provide JIPs for the WHILE instructions (the
"back-edges", since there's no DO in Gfx9+).  It optionally can
provide other JIPs or UIPs, their values will be used instead of
the calculated one.

The input JIPs and UIPs are absolute index values in the array,
and after finish they will be converted into relative byte offsets,
which is what the hardware will use.

Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41413>

src/intel/compiler/gen/gen_control_flow.cpp
@@ -1,0 +1,287 @@
+/*
+ * Copyright © 2025 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <algorithm>
+#include <vector>
+
+#include "util/compiler.h"
+#include "gen.h"
+
+bool
+gen_finish_structured_cf(gen_inst *insts, int num_insts, int final_halt_idx)
+{
+   /* Any JIPs and UIPs provided by the input are expected as relative
+    * byte offsets, and will be respected as-is.  Any remaining JIPs and
+    * UIPs will be inferred and filled in as relative byte offsets.
+    */
+
+   struct branch_info {
+      gen_inst *inst;
+      int idx;
+      int uip_idx;
+      int jip_idx;
+
+      /* For loop headers. */
+      int loop_end_idx;
+
+      bool is_loop_header() const { return loop_end_idx != 0; }
+   };
+
+   /* Collect information about the control flow instructions and any
+    * instruction that are loop headers.  There might be multiple entries
+    * for instructions that act as loop header for multiple loops and/or that
+    * are control flow instruction themselves (e.g. IF as the loop header).
+    */
+   std::vector<branch_info> infos;
+   for (int idx = 0; idx < num_insts; idx++) {
+      gen_inst *inst = &insts[idx];
+
+      switch (inst->opcode) {
+      case GEN_OP_WHILE: {
+         /* Since there's no DO instruction, the caller must
+          * have set the JIP for WHILE insturctions, so that
+          * the start of loops can be identified.
+          */
+         assert(inst->src[0].file == GEN_IMM);
+         assert((int32_t)inst->src[0].imm % 16 == 0);
+         const int jip_idx = idx + (int32_t)inst->src[0].imm / 16;
+         assert(jip_idx < idx);
+         infos.push_back({
+            .inst = inst,
+            .idx = idx,
+            .jip_idx = jip_idx,
+         });
+         /* Add an entry for the loop header. */
+         infos.push_back({
+            .inst = &insts[jip_idx],
+            .idx = jip_idx,
+            .loop_end_idx = idx,
+         });
+         break;
+      }
+
+      case GEN_OP_HALT:
+      case GEN_OP_IF:
+      case GEN_OP_ELSE:
+      case GEN_OP_BREAK:
+      case GEN_OP_CONTINUE:
+      case GEN_OP_ENDIF:
+         infos.push_back({
+            .inst = &insts[idx],
+            .idx = idx,
+         });
+         break;
+
+      default:
+         /* Skip. */
+         continue;
+      }
+   }
+
+   /* Sort in scope order. */
+   std::sort(infos.begin(), infos.end(), [](const auto &a, const auto &b) {
+      if (a.idx != b.idx)
+         return a.idx < b.idx;
+      /* Note the flipped comparison: want to see the largest scope first,
+       * since it contains the other.
+       */
+      return a.loop_end_idx > b.loop_end_idx;
+   });
+
+   /* Identify the UIPs and JIPs that can be inferred by the
+    * structured control flow.
+    */
+   std::vector<std::pair<branch_info *, branch_info *>> if_stack;
+   std::vector<branch_info *> loop_stack;
+
+   for (unsigned i = 0; i < infos.size(); i++) {
+      /* This is a loop header for one or more loops. */
+      while (i < infos.size() && infos[i].loop_end_idx) {
+         loop_stack.push_back(&infos[i]);
+         i++;
+      }
+      assert(i < infos.size());
+
+      branch_info &info = infos[i];
+      const int idx = info.idx;
+
+      switch (info.inst->opcode) {
+      case GEN_OP_IF:
+         if_stack.push_back({&info, NULL});
+         break;
+
+      case GEN_OP_ELSE:
+         if_stack.back().second = &info;
+         break;
+
+      case GEN_OP_ENDIF: {
+         assert(!if_stack.empty());
+         auto [if_info, else_info] = if_stack.back();
+         if_stack.pop_back();
+
+         if (else_info == NULL) {
+            if_info->uip_idx = idx;
+            if_info->jip_idx = idx;
+         } else {
+            if_info->uip_idx = idx;
+            if_info->jip_idx = else_info->idx + 1;
+
+            else_info->uip_idx = idx;
+            else_info->jip_idx = idx;
+         }
+         break;
+      }
+
+      case GEN_OP_CONTINUE:
+      case GEN_OP_BREAK:
+         assert(!loop_stack.empty());
+         assert(loop_stack.back()->is_loop_header());
+         info.uip_idx = loop_stack.back()->loop_end_idx;
+         assert(idx < info.uip_idx);
+         break;
+
+      case GEN_OP_WHILE:
+         assert(!loop_stack.empty());
+         assert(loop_stack.back()->loop_end_idx == idx);
+         loop_stack.pop_back();
+         break;
+
+      case GEN_OP_HALT:
+         if (final_halt_idx != -1)
+            info.uip_idx = final_halt_idx + 1;
+         break;
+
+      default:
+         /* Nothing to do. */
+         break;
+      }
+   }
+
+   assert(if_stack.empty());
+   assert(loop_stack.empty());
+
+   struct scope {
+      int end_idx;
+   };
+
+   std::vector<scope> scopes;
+   scopes.push_back({-1});
+
+   /* Fill out the remaining JIPs, which are meant to "jump out of the current
+    * scope".  Walk backwards keeping track of the scopes.  This make easy to
+    * get the innermost end of scope and the innermost end of loop.
+    */
+   for (int i = infos.size() - 1; i >= 0; i--) {
+      branch_info &info = infos[i];
+      const int idx = info.idx;
+
+      /* Remove the scope once we walk back to its start. */
+      if (info.is_loop_header() || info.inst->opcode == GEN_OP_IF) {
+         scopes.pop_back();
+         continue;
+      }
+
+      switch (info.inst->opcode) {
+      case GEN_OP_IF:
+         /* Handled above together with loop headers. */
+         break;
+
+      case GEN_OP_ELSE:
+         /* For instructions before the ELSE in the conditional (i.e. the
+          * then-part of the loop), the scope ends here.
+          */
+         scopes.back().end_idx = idx;
+         break;
+
+      case GEN_OP_ENDIF: {
+         const int innermost_end_idx = scopes.back().end_idx;
+
+         if (innermost_end_idx != -1)
+            info.jip_idx = innermost_end_idx;
+         else if (final_halt_idx != -1)
+            info.jip_idx = final_halt_idx + 1;
+         else
+            info.jip_idx = idx + 1;
+
+         scopes.push_back({ idx });
+         break;
+      }
+
+      case GEN_OP_WHILE:
+         scopes.push_back({ idx });
+         break;
+
+      case GEN_OP_BREAK:
+      case GEN_OP_CONTINUE: {
+         const int outer_end_idx = scopes.back().end_idx;
+         info.jip_idx = outer_end_idx;
+
+         assert(info.uip_idx != 0);
+         assert(info.jip_idx != 0);
+         break;
+      }
+
+      case GEN_OP_HALT: {
+	 /* From the Sandy Bridge PRM (volume 4, part 2, section 8.3.19):
+	  *
+	  *    "In case of the halt instruction not inside any conditional
+	  *     code block, the value of <JIP> and <UIP> should be the
+	  *     same. In case of the halt instruction inside conditional code
+	  *     block, the <UIP> should be the end of the program, and the
+	  *     <JIP> should be end of the most inner conditional code block."
+	  */
+         const int innermost_end_idx = scopes.back().end_idx;
+         if (innermost_end_idx != -1)
+            info.jip_idx = innermost_end_idx;
+         else
+            info.jip_idx = info.uip_idx;
+         break;
+      }
+
+      default:
+         /* Nothing to do. */
+         break;
+      }
+   }
+
+   /* Finally, fill in any JIPs and UIPs not provided by the caller,
+    * as relative byte offsets.
+    */
+   for (auto &info : infos) {
+      gen_inst *inst = info.inst;
+      const int idx = info.idx;
+
+      switch (info.inst->opcode) {
+      case GEN_OP_IF:
+      case GEN_OP_ELSE:
+      case GEN_OP_BREAK:
+      case GEN_OP_CONTINUE:
+      case GEN_OP_HALT:
+         if (inst->src[1].file == GEN_BAD_FILE) {
+            inst->src[1].file = GEN_IMM;
+            inst->src[1].type = GEN_TYPE_D;
+            inst->src[1].imm = 16 * (info.uip_idx - idx);
+         }
+         assert((int32_t)inst->src[1].imm > 0);
+         FALLTHROUGH;
+
+      case GEN_OP_WHILE:
+      case GEN_OP_ENDIF:
+         if (inst->src[0].file == GEN_BAD_FILE) {
+            inst->src[0].file = GEN_IMM;
+            inst->src[0].type = GEN_TYPE_D;
+            inst->src[0].imm = 16 * (info.jip_idx - idx);
+         }
+         break;
+
+      default:
+         /* Nothing to do. */
+         break;
+      }
+   }
+
+   return true;
+}
+

src/intel/compiler/gen/gen_control_flow_test.cpp
@@ -1,0 +1,444 @@
+/*
+ * Copyright © 2025 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <gtest/gtest.h>
+
+#include "gen.h"
+
+struct gen_inst_vector {
+   static constexpr int CAP = 256;
+
+   gen_inst insts[CAP] = {};
+   int count = 0;
+
+   gen_inst *data() { return insts; }
+   int size()       { return count; }
+
+   struct gen_inst_info {
+      int ip;
+      gen_inst *inst;
+
+      /* Use array indices and absolute values to make comparisons easier. */
+      int uip() { assert(inst->src[1].imm % 16 == 0); return ip + inst->src[1].imm / 16; }
+      int jip() { assert(inst->src[0].imm % 16 == 0); return ip + inst->src[0].imm / 16; }
+   };
+
+   gen_inst_info append(gen_opcode op) {
+      assert(count < CAP);
+      const int idx = count++;
+      insts[idx].opcode = op;
+      return { idx, &insts[idx] };
+   }
+
+   gen_inst_info IF()       { return append(GEN_OP_IF); }
+   gen_inst_info ELSE()     { return append(GEN_OP_ELSE); }
+   gen_inst_info ENDIF()    { return append(GEN_OP_ENDIF); }
+   gen_inst_info BREAK()    { return append(GEN_OP_BREAK); }
+   gen_inst_info CONTINUE() { return append(GEN_OP_CONTINUE); }
+   gen_inst_info HALT()     { return append(GEN_OP_HALT); }
+
+   gen_inst_info WHILE(gen_inst_info header) {
+      gen_inst_info info = append(GEN_OP_WHILE);
+      info.inst->src[0].file = GEN_IMM;
+      info.inst->src[0].type = GEN_TYPE_D;
+      info.inst->src[0].imm = 16 * (int32_t)(header.ip - info.ip);
+      return info;
+   }
+
+   gen_inst_info NOP(int count = 1) {
+      gen_inst_info info = append(GEN_OP_NOP);
+      for (int i = 1; i < count; i++)
+         append(GEN_OP_NOP);
+      return info;
+   }
+};
+
+TEST(FinishStructuredCF, ControlFlowELSE)
+{
+   gen_inst_vector v;
+
+   v.NOP();
+   auto if1 = v.IF();
+   v.NOP();
+   auto else1 = v.ELSE();
+   v.NOP();
+   auto endif1 = v.ENDIF();
+   v.NOP();
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), -1);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(if1.uip(), endif1.ip);
+   EXPECT_EQ(if1.jip(), else1.ip + 1);
+
+   EXPECT_EQ(else1.uip(), endif1.ip);
+   EXPECT_EQ(else1.jip(), endif1.ip);
+
+   EXPECT_EQ(endif1.jip(), endif1.ip + 1);
+}
+
+TEST(FinishStructuredCF, ControlFlowSiblingIFs)
+{
+   gen_inst_vector v;
+
+   v.NOP();
+   auto if1 = v.IF();
+      v.NOP();
+   auto endif1 = v.ENDIF();
+   v.NOP();
+   auto if2 = v.IF();
+      v.NOP();
+   auto else2 = v.ELSE();
+      v.NOP();
+   auto endif2 = v.ENDIF();
+   v.NOP();
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), -1);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(if1.uip(), endif1.ip);
+   EXPECT_EQ(if1.jip(), endif1.ip);
+
+   EXPECT_EQ(endif1.jip(), endif1.ip + 1);
+
+   EXPECT_EQ(if2.uip(), endif2.ip);
+   EXPECT_EQ(if2.jip(), else2.ip + 1);
+
+   EXPECT_EQ(else2.uip(), endif2.ip);
+   EXPECT_EQ(else2.jip(), endif2.ip);
+
+   EXPECT_EQ(endif2.jip(), endif2.ip + 1);
+}
+
+TEST(FinishStructuredCF, ControlFlowNestedIFs)
+{
+   gen_inst_vector v;
+
+   v.NOP();
+   auto if1 = v.IF();
+      v.NOP();
+
+      auto if2 = v.IF();
+         v.NOP();
+      auto endif2 = v.ENDIF();
+
+      v.NOP();
+   auto endif1 = v.ENDIF();
+   v.NOP();
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), -1);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(if1.uip(), endif1.ip);
+   EXPECT_EQ(if1.jip(), endif1.ip);
+
+   EXPECT_EQ(if2.uip(), endif2.ip);
+   EXPECT_EQ(if2.jip(), endif2.ip);
+
+   EXPECT_EQ(endif2.jip(), endif1.ip);
+
+   EXPECT_EQ(endif1.jip(), endif1.ip + 1);
+}
+
+
+TEST(FinishStructuredCF, ControlFlowManyHALTs)
+{
+   gen_inst_vector v;
+
+   v.NOP();
+   auto header1 = v.NOP();
+      v.NOP();
+      v.IF();
+         v.NOP();
+         auto h1 = v.HALT();
+      auto els = v.ELSE();
+         v.NOP();
+         auto h2 = v.HALT();
+         v.NOP();
+      auto endif = v.ENDIF();
+      v.NOP();
+      auto h3 = v.HALT();
+      v.NOP();
+      auto header2 = v.NOP();
+         v.BREAK();
+         v.NOP();
+         auto h4 = v.HALT();
+         v.NOP();
+      auto while2 = v.WHILE(header2);
+      v.NOP();
+   auto while1 = v.WHILE(header1);
+   v.NOP();
+   auto final_halt = v.HALT();
+   v.NOP();
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), final_halt.ip);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(h1.uip(), final_halt.ip + 1);
+   EXPECT_EQ(h1.jip(), els.ip);
+
+   EXPECT_EQ(h2.uip(), final_halt.ip + 1);
+   EXPECT_EQ(h2.jip(), endif.ip);
+
+   EXPECT_EQ(h3.uip(), final_halt.ip + 1);
+   EXPECT_EQ(h3.jip(), while1.ip);
+
+   EXPECT_EQ(h4.uip(), final_halt.ip + 1);
+   EXPECT_EQ(h4.jip(), while2.ip);
+
+   EXPECT_EQ(final_halt.uip(), final_halt.ip + 1);
+   EXPECT_EQ(final_halt.jip(), final_halt.ip + 1);
+}
+
+TEST(FinishStructuredCF, ControlFlow1)
+{
+   gen_inst_vector v;
+
+   v.NOP();
+   auto header1 = v.NOP();
+      v.IF();
+         v.NOP();
+         v.IF();
+            v.NOP();
+            v.CONTINUE();
+         v.ENDIF();
+         v.NOP();
+         v.IF();
+            v.NOP();
+            auto h1 = v.HALT();
+         auto endif2 = v.ENDIF();
+      v.ELSE();
+         v.NOP();
+      v.ENDIF();
+      v.BREAK();
+   v.WHILE(header1);
+   v.NOP();
+   auto final_halt = v.HALT();
+   v.NOP();
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), final_halt.ip);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(h1.uip(), final_halt.ip + 1);
+   EXPECT_EQ(h1.jip(), endif2.ip);
+
+   EXPECT_EQ(final_halt.jip(), final_halt.ip + 1);
+   EXPECT_EQ(final_halt.uip(), final_halt.ip + 1);
+}
+
+TEST(FinishStructuredCF, ControlFlow2)
+{
+   gen_inst_vector v;
+
+   v.NOP(2);
+   auto header1 = v.NOP();
+      auto if1 = v.IF();
+         v.NOP(3);
+         auto if2 = v.IF();
+            v.NOP();
+            auto cont = v.CONTINUE();
+         auto endif2 = v.ENDIF();
+         v.NOP(2);
+         auto if3 = v.IF();
+            v.NOP();
+            auto halt = v.HALT();
+         auto endif3 = v.ENDIF();
+      auto else1 = v.ELSE();
+         v.NOP(2);
+      auto endif1 = v.ENDIF();
+      auto break1 = v.BREAK();
+   auto while1 = v.WHILE(header1);
+   v.NOP(6);
+   auto last_halt = v.HALT();
+   v.NOP(5);
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), last_halt.ip);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(if1.uip(), endif1.ip);
+   EXPECT_EQ(if1.jip(), else1.ip + 1);
+
+   EXPECT_EQ(if2.uip(), endif2.ip);
+   EXPECT_EQ(if2.jip(), endif2.ip);
+
+   EXPECT_EQ(if3.uip(), endif3.ip);
+   EXPECT_EQ(if3.jip(), endif3.ip);
+
+   EXPECT_EQ(halt.jip(), endif3.ip);
+   EXPECT_EQ(halt.uip(), last_halt.ip + 1);
+
+   EXPECT_EQ(cont.uip(), while1.ip);
+   EXPECT_EQ(cont.jip(), endif2.ip);
+
+   EXPECT_EQ(break1.uip(), while1.ip);
+   EXPECT_EQ(break1.jip(), while1.ip);
+
+   EXPECT_EQ(last_halt.jip(), last_halt.ip + 1);
+   EXPECT_EQ(last_halt.uip(), last_halt.ip + 1);
+}
+
+TEST(FinishStructuredCF, ControlFlow3)
+{
+   gen_inst_vector v;
+
+   v.NOP(9);
+   auto header1 = v.NOP();
+      v.NOP(2);
+      v.IF();
+         v.NOP();
+         auto h1 = v.HALT();
+      auto els = v.ELSE();
+         v.NOP();
+         auto h2 = v.HALT();
+         v.NOP(7);
+      auto endif = v.ENDIF();
+      v.NOP(2);
+      auto h3 = v.HALT();
+      v.NOP();
+      auto header2 = v.NOP();
+         v.NOP();
+         auto brk = v.BREAK();
+         v.NOP();
+         auto h4 = v.HALT();
+         v.NOP();
+      auto while2 = v.WHILE(header2);
+      v.NOP();
+   auto while1 = v.WHILE(header1);
+   v.NOP(5);
+   auto final_halt = v.HALT();
+   v.NOP(3);
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), final_halt.ip);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(brk.jip(), while2.ip);
+   EXPECT_EQ(brk.uip(), while2.ip);
+
+   EXPECT_EQ(h1.jip(), els.ip);
+   EXPECT_EQ(h1.uip(), final_halt.ip + 1);
+
+   EXPECT_EQ(h2.jip(), endif.ip);
+   EXPECT_EQ(h2.uip(), final_halt.ip + 1);
+
+   EXPECT_EQ(h3.jip(), while1.ip);
+   EXPECT_EQ(h3.uip(), final_halt.ip + 1);
+
+   EXPECT_EQ(h4.jip(), while2.ip);
+   EXPECT_EQ(h4.uip(), final_halt.ip + 1);
+
+   EXPECT_EQ(final_halt.jip(), final_halt.ip + 1);
+   EXPECT_EQ(final_halt.uip(), final_halt.ip + 1);
+}
+
+TEST(FinishStructuredCF, ControlFlow4)
+{
+   gen_inst_vector v;
+
+   v.NOP(2);
+
+   auto label10 = v.NOP();
+      auto if1 = v.IF();
+         v.NOP();
+         auto brk1 = v.BREAK();
+      auto endif1 = v.ENDIF();
+      v.NOP();
+      auto if2 = v.IF();
+         v.NOP(3);
+         auto label8 = v.NOP();
+            auto brk2 = v.BREAK();
+            v.NOP();
+            auto label7 = v.NOP(2);
+               auto brk3 = v.BREAK();
+               auto label6 = v.NOP();
+                  auto brk4 = v.BREAK();
+                  v.NOP(2);
+               auto while6 = v.WHILE(label6);
+               v.NOP();
+            auto while7 = v.WHILE(label7);
+            v.NOP();
+         auto while8 = v.WHILE(label8);
+         v.NOP();
+         auto if3 = v.IF();
+            v.NOP();
+            auto brk5 = v.BREAK();
+         auto endif3 = v.ENDIF();
+         v.NOP();
+      auto endif2 = v.ENDIF();
+   auto while10 = v.WHILE(label10);
+   v.NOP(9);
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), -1);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(if1.uip(), endif1.ip);
+   EXPECT_EQ(if1.jip(), endif1.ip);
+
+   EXPECT_EQ(brk1.uip(), while10.ip);
+   EXPECT_EQ(brk1.jip(), endif1.ip);
+
+   EXPECT_EQ(endif1.jip(), while10.ip);
+
+   EXPECT_EQ(if2.uip(), endif2.ip);
+   EXPECT_EQ(if2.jip(), endif2.ip);
+
+   EXPECT_EQ(brk2.uip(), while8.ip);
+   EXPECT_EQ(brk2.jip(), while8.ip);
+
+   EXPECT_EQ(brk3.uip(), while7.ip);
+   EXPECT_EQ(brk3.jip(), while7.ip);
+
+   EXPECT_EQ(brk4.uip(), while6.ip);
+   EXPECT_EQ(brk4.jip(), while6.ip);
+
+   EXPECT_EQ(if3.uip(), endif3.ip);
+   EXPECT_EQ(if3.jip(), endif3.ip);
+
+   EXPECT_EQ(brk5.uip(), while10.ip);
+   EXPECT_EQ(brk5.jip(), endif3.ip);
+
+   EXPECT_EQ(endif3.jip(), endif2.ip);
+   EXPECT_EQ(endif2.jip(), while10.ip);
+}
+
+TEST(FinishStructuredCF, ControlFlow5)
+{
+   gen_inst_vector v;
+
+                 v.NOP(7);
+   auto if1    = v.IF();
+                 v.NOP(5);
+   auto else1  = v.ELSE();
+                 v.NOP(3);
+                 v.NOP();
+   auto endif1 = v.ENDIF();
+                 v.NOP(5);
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), -1);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(if1.jip(), else1.ip + 1);
+   EXPECT_EQ(if1.uip(), endif1.ip);
+
+   EXPECT_EQ(else1.jip(), endif1.ip);
+   EXPECT_EQ(else1.uip(), endif1.ip);
+
+   EXPECT_EQ(endif1.jip(), endif1.ip + 1);
+}
+
+TEST(FinishStructuredCF, ControlFlowEndsWithENDIF)
+{
+   gen_inst_vector v;
+
+   v.NOP();
+   auto if1 = v.IF();
+   v.NOP();
+   auto endif1 = v.ENDIF();
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), -1);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(if1.uip(), endif1.ip);
+   EXPECT_EQ(if1.jip(), endif1.ip);
+}

src/intel/compiler/gen/gen.h
@@ -19,6 +19,25 @@
 extern "C" {
 #endif
 
+/* Finish structured control flow instructions by filling in any missing
+ * JIPs and UIPs.
+ *
+ * Any JIPs or UIPs already set by the caller must be relative byte
+ * offsets, and will be respected as-is.
+ *
+ * The caller must ensure that the JIP for WHILE instructions is set.
+ * It represents the "back-edge" and can't be inferred since there's no
+ * DO instruction marking the start of a loop.
+ *
+ * Any other JIPs and UIPs will be inferred from the structure of the
+ * program and filled in as relative byte offsets.
+ *
+ * If a final_halt_idx is provided, that will act as a final synchronization
+ * point for the halts and JIPs filled in the instructions.
+ */
+bool gen_finish_structured_cf(gen_inst *insts, int num_insts, int final_halt_idx);
+
+
 typedef struct gen_encode_params {
    const struct intel_device_info *devinfo;
 

src/intel/compiler/gen/meson.build
@@ -11,6 +11,7 @@ libintel_compiler_gen_files = files(
   'gen_private.h',
 
   'gen_names.c',
+  'gen_control_flow.cpp',
   'gen_encoding.cpp',
   'gen_encoding_pre_xe.cpp',
   'gen_validate.cpp',
@@ -40,6 +41,7 @@ if with_tests
     executable(
       'intel_compiler_gen_tests',
       files(
+        'gen_control_flow_test.cpp',
         'gen_encoding_test.cpp',
         'gen_validate_test.cpp',
       ),

commit e6e3f33e46f46e28224d0117ab283c3761c380a3

intel/gen: Fix case where IF is a loop header
Every loop header (for each loop) gets an extra branch_info entry that
points to the header instruction.  Those extra entries should not get
processed for updating JIP/UIP.  These were harmless noise for regular
instructions, but for branch instructions like IF it could corrupt their
final JIP/UIP.

Fixes: 11dde62fcc87 ("intel/gen: Add function to finish structured control flow")
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Tested-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42848>

src/intel/compiler/gen/gen_control_flow.cpp
@@ -250,6 +250,9 @@ gen_finish_structured_cf(gen_inst *insts, int num_insts, int final_halt_idx)
     * as relative byte offsets.
     */
    for (auto &info : infos) {
+      if (info.is_loop_header())
+         continue;
+
       gen_inst *inst = info.inst;
       const int idx = info.idx;
 

src/intel/compiler/gen/gen_control_flow_test.cpp
@@ -442,3 +442,26 @@ TEST(FinishStructuredCF, ControlFlowEndsWithENDIF)
    EXPECT_EQ(if1.uip(), endif1.ip);
    EXPECT_EQ(if1.jip(), endif1.ip);
 }
+
+TEST(FinishStructuredCF, ControlFlowLoopStartsWithIF)
+{
+   gen_inst_vector v;
+
+   auto if1 = v.IF();
+      auto brk = v.BREAK();
+   auto endif1 = v.ENDIF();
+   auto while1 = v.WHILE(if1);
+
+   bool ok = gen_finish_structured_cf(v.data(), v.size(), -1);
+   ASSERT_TRUE(ok);
+
+   EXPECT_EQ(if1.uip(), endif1.ip);
+   EXPECT_EQ(if1.jip(), endif1.ip);
+
+   EXPECT_EQ(brk.uip(), while1.ip);
+   EXPECT_EQ(brk.jip(), endif1.ip);
+
+   EXPECT_EQ(endif1.jip(), while1.ip);
+
+   EXPECT_EQ(while1.jip(), if1.ip);
+}