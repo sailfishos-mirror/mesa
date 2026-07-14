be269027d76853d1a75cae09e99c0c2b719fbe2b
Karol Herbst <kherbst@redhat.com>
2026-07-14 14:31:43 +0200

    rusticl/mesa: compile 32 bit version
    Backport-to: 26.2
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42948>

--- src/gallium/frontends/rusticl/builtin_kernels/lib.rs
+++ src/gallium/frontends/rusticl/builtin_kernels/lib.rs
@@ -1,2 +1,4 @@
 pub const RUSTICL_BUILTIN_KERNELS_SPV64: &'static [u8] =
     include_bytes!("gen/rusticl_kernels.spv64").as_slice();
+pub const RUSTICL_BUILTIN_KERNELS_SPV32: &'static [u8] =
+    include_bytes!("gen/rusticl_kernels.spv32").as_slice();
