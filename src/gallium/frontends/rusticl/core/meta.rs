1321fd2ec6c66afc172096ed3f8f785e213b1be9
Karol Herbst <kherbst@redhat.com>
2026-07-14 17:58:43 +0200

    rusticl/meta: split out ulong kernels
    Backport-to: 26.2
    (cherry picked from commit b2c832063be1b2f56019a6b9739bae42649541e6)
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43133>

--- src/gallium/frontends/rusticl/core/meta.rs
+++ src/gallium/frontends/rusticl/core/meta.rs
@@ -1,7 +1,10 @@
 use std::{collections::HashMap, ffi::CString, sync::Arc};
 
 use mesa_rust_util::properties::Properties;
-use rusticl_kernels::{RUSTICL_BUILTIN_KERNELS_SPV32, RUSTICL_BUILTIN_KERNELS_SPV64};
+use rusticl_kernels::{
+    RUSTICL_BUILTIN_KERNELS_SPV32, RUSTICL_BUILTIN_KERNELS_SPV32_ULONG,
+    RUSTICL_BUILTIN_KERNELS_SPV64, RUSTICL_BUILTIN_KERNELS_SPV64_ULONG,
+};
 use rusticl_opencl_gen::*;
 
 use crate::{
@@ -27,29 +30,37 @@ impl Meta {
         let mut kernels64 = HashMap::new();
         let mut kernels32 = HashMap::new();
 
-        for (bits, spirv, kernels) in [
-            (32, RUSTICL_BUILTIN_KERNELS_SPV32, &mut kernels32),
-            (64, RUSTICL_BUILTIN_KERNELS_SPV64, &mut kernels64),
-        ] {
-            let devs = devs
-                .iter()
-                .filter(|dev| dev.address_bits() == bits)
-                .collect::<Vec<_>>();
+        for (bits, kernels) in [(32, &mut kernels32), (64, &mut kernels64)] {
+            for long in [true, false] {
+                let spirv = match (bits, long) {
+                    (32, false) => RUSTICL_BUILTIN_KERNELS_SPV32,
+                    (32, true) => RUSTICL_BUILTIN_KERNELS_SPV32_ULONG,
+                    (64, false) => RUSTICL_BUILTIN_KERNELS_SPV64,
+                    (64, true) => RUSTICL_BUILTIN_KERNELS_SPV64_ULONG,
+                    (_, _) => unreachable!(),
+                };
 
-            if devs.is_empty() {
-                continue;
-            }
+                let devs = devs
+                    .iter()
+                    .filter(|dev| dev.address_bits() == bits)
+                    .filter(|dev| !long || dev.int64_supported())
+                    .collect::<Vec<_>>();
 
-            let prog = Program::from_spirv_with_devs(devs.clone(), Arc::clone(&ctx), spirv);
-            let options = CompileOptions::new(c"", -1).unwrap();
-            prog.build(devs, options, None).unwrap();
-            let build = prog.build_info();
+                if devs.is_empty() {
+                    continue;
+                }
 
-            for kernel in build.kernels() {
-                kernels.insert(
-                    kernel.to_owned(),
-                    Kernel::new(kernel.to_owned(), Arc::clone(&prog), &build),
-                );
+                let prog = Program::from_spirv_with_devs(devs.clone(), Arc::clone(&ctx), spirv);
+                let options = CompileOptions::new(c"", -1).unwrap();
+                prog.build(devs, options, None).unwrap();
+                let build = prog.build_info();
+
+                for kernel in build.kernels() {
+                    kernels.insert(
+                        kernel.to_owned(),
+                        Kernel::new(kernel.to_owned(), Arc::clone(&prog), &build),
+                    );
+                }
             }
         }
 
@@ -69,6 +80,10 @@ impl Meta {
         let len = pattern.len();
 
         if len > 128 || !len.is_power_of_two() {
+            return Err(CL_INVALID_VALUE);
+        }
+
+        if len > 64 && !dev.int64_supported() {
             return Err(CL_INVALID_VALUE);
         }
 
