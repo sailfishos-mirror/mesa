9b00652555f12a28736b0ce6d22295fc7148ec0c
Georg Lehmann <dadschoorse@gmail.com>
2026-07-15 13:06:57 +0200

    nir/opt_dead_write_vars: handle atomics as reads
    As long as there is no cross invocation access, write, followed by atomic,
    followed by write is valid. We can not remove the first write in this case:
    
    shared_array[invocation_index] = 0;
    res = atomicAdd(shared_array[invocation_index], 2);
    shared_array[invocation_index] = 0;
    
    Cc: mesa-stable
    
    Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42978>
    (cherry picked from commit 1bc7b09d583b4f623fe18983f048014c508a2364)

--- src/compiler/nir/nir_opt_dead_write_vars.c
+++ src/compiler/nir/nir_opt_dead_write_vars.c
@@ -187,6 +187,8 @@ remove_dead_write_vars_local(nir_shader *shader, nir_block *block,
          break;
       }
 
+      case nir_intrinsic_deref_atomic:
+      case nir_intrinsic_deref_atomic_swap:
       case nir_intrinsic_load_deref: {
          nir_deref_instr *src = nir_src_as_deref(intrin->src[0]);
          if (nir_deref_mode_must_be(src, nir_var_read_only_modes))
