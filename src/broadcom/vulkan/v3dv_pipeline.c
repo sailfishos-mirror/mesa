Grima04/mesa-turnip-kgsl

commit 02de1d9ab16f4818a9da29f96d222f19b11a106b

v3dv: add a no-op fragment shader if we don't have one
Fixes: dEQP-VK.pipeline.vertex_only.position_to_ssbo
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6766>

src/broadcom/vulkan/v3dv_pipeline.c
@@ -1043,6 +1043,26 @@ pipeline_compile_graphics(struct v3dv_pipeline *pipeline,
       stages[stage] = p_stage;
    }
 
+   /* Add a no-op fragment shader if needed */
+   if (!stages[MESA_SHADER_FRAGMENT]) {
+      nir_builder b;
+      nir_builder_init_simple_shader(&b, NULL, MESA_SHADER_FRAGMENT,
+                                     &v3dv_nir_options);
+      b.shader->info.name = ralloc_strdup(b.shader, "noop_fs");
+
+      struct v3dv_pipeline_stage *p_stage =
+         vk_zalloc2(&device->alloc, alloc, sizeof(*p_stage), 8,
+                    VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+
+      p_stage->pipeline = pipeline;
+      p_stage->stage = MESA_SHADER_FRAGMENT;
+      p_stage->entrypoint = "main";
+      p_stage->module = 0;
+      p_stage->nir = b.shader;
+
+      stages[MESA_SHADER_FRAGMENT] = p_stage;
+      pipeline->active_stages |= MESA_SHADER_FRAGMENT;
+   }
 
    /* Linking */
    struct v3dv_pipeline_stage *next_stage = NULL;

sailfishos-mirror/mesa

commit fb7300c6da2b740b0605b1562fe569391f33be94

v3dv: record the no-op fragment shader with its Vulkan stage bit
Same as the previous commit: MESA_SHADER_FRAGMENT sets
TESSELLATION_EVALUATION instead of FRAGMENT.

Fixes: 02de1d9ab16f ("v3dv: add a no-op fragment shader if we don't have one")
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42966>

src/broadcom/vulkan/v3dv_pipeline.c
@@ -2531,7 +2531,7 @@ pipeline_compile_graphics(struct v3dv_pipeline *pipeline,
          p_atomic_inc_return(&physical_device->next_program_id);
 
       pipeline->stages[BROADCOM_SHADER_FRAGMENT] = p_stage;
-      pipeline->active_stages |= MESA_SHADER_FRAGMENT;
+      pipeline->active_stages |= VK_SHADER_STAGE_FRAGMENT_BIT;
    }
 
    /* If multiview is enabled, we inject a custom passthrough geometry shader