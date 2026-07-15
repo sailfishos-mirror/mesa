sailfishos-mirror/mesa

commit baf2f0ea4a9911553ca2acac56dbd6333e507c11

intel: Replace INTEL_DEBUG=do32 with INTEL_SIMD_DEBUG
We now track whether INTEL_SIMD_DEBUG specified any overrides for a
given shader stage.  If so, we consider it to force-enable a mode,
even if we wouldn't normally use it.  If no modes are specified in
INTEL_SIMD_DEBUG, then everything is allowed and we choose via our
usual heuristics.

Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41587>

src/intel/dev/intel_debug.c
@@ -85,7 +85,6 @@ static const struct debug_control_bitset debug_control[] = {
    OPT1("ds",                DEBUG_TES),
    OPT1("tes",               DEBUG_TES),
    OPT1("l3",                DEBUG_L3),
-   OPT1("do32",              DEBUG_DO32),
    OPT1("norbc",             DEBUG_NO_CCS),
    OPT1("noccs",             DEBUG_NO_CCS),
    OPT1("noccs-modifier",    DEBUG_NO_CCS_MODIFIER),
@@ -132,6 +131,7 @@ static const struct debug_control_bitset debug_control[] = {
 #undef OPT2
 };
 uint64_t intel_simd = 0;
+unsigned intel_simd_overridden = 0;
 
 static const struct debug_control simd_control[] = {
    { "fs8",    DEBUG_FS_SIMD8 },
@@ -258,6 +258,18 @@ process_intel_debug_variable_once(void)
    /* If INTEL_SIMD_DEBUG doesn't specify any options for a stage, then all
     * are allowed, except FS currently disables multipolygon modes by default.
     */
+   intel_simd_overridden =
+      (intel_simd & DEBUG_FS_SIMD) ? (1 << MESA_SHADER_FRAGMENT) : 0 |
+      (intel_simd & DEBUG_CS_SIMD) ? (1 << MESA_SHADER_COMPUTE)  : 0 |
+      (intel_simd & DEBUG_TS_SIMD) ? (1 << MESA_SHADER_TASK)     : 0 |
+      (intel_simd & DEBUG_MS_SIMD) ? (1 << MESA_SHADER_MESH)     : 0 |
+      (intel_simd & DEBUG_RT_SIMD) ? (1 << MESA_SHADER_RAYGEN |
+                                      1 << MESA_SHADER_ANY_HIT |
+                                      1 << MESA_SHADER_CLOSEST_HIT |
+                                      1 << MESA_SHADER_MISS |
+                                      1 << MESA_SHADER_INTERSECTION |
+                                      1 << MESA_SHADER_CALLABLE) : 0;
+
    if (!(intel_simd & DEBUG_FS_SIMD))
       intel_simd |=   DEBUG_FS_SIMD8 | DEBUG_FS_SIMD16 | DEBUG_FS_SIMD32;
    if (!(intel_simd & DEBUG_CS_SIMD))

src/intel/compiler/brw/brw_compile_fs.cpp
@@ -1322,6 +1322,9 @@ limit_fs_dispatch_width(const struct intel_device_info *devinfo,
    return limit;
 }
 
+#define INTEL_SIMD_FORCE(x) \
+   ((intel_simd_overridden & (1 << MESA_SHADER_FRAGMENT)) && INTEL_SIMD(FS, x))
+
 const unsigned *
 brw_compile_fs(const struct brw_compiler *compiler,
                struct brw_compile_fs_params *params)
@@ -1643,7 +1646,7 @@ brw_compile_fs(const struct brw_compiler *compiler,
          } else {
             const brw_performance &perf = v32->performance_analysis.require();
 
-            if (!INTEL_DEBUG(DEBUG_DO32) && throughput >= perf.throughput) {
+            if (!INTEL_SIMD_FORCE(32) && throughput >= perf.throughput) {
                brw_shader_perf_log(compiler, params->base.log_data,
                                    "SIMD32 shader inefficient\n");
                v32.reset();

src/intel/compiler/brw/brw_compiler.c
@@ -267,7 +267,6 @@ brw_get_compiler_config_value(const struct brw_compiler *compiler)
       DEBUG_SPILL_FS,
       DEBUG_SPILL_VEC4,
       DEBUG_NO_COMPACTION,
-      DEBUG_DO32,
       DEBUG_SOFT64,
       DEBUG_NO_SEND_GATHER,
       DEBUG_NO_VRT,
@@ -285,6 +284,9 @@ brw_get_compiler_config_value(const struct brw_compiler *compiler)
       insert_u64_bit(&config, (intel_simd & (1ULL << bit)) != 0);
 
    for (unsigned i = 0; i < MESA_VULKAN_SHADER_STAGES; i++) {
+      insert_u64_bit(&config, (intel_simd_overridden & (1 << i)) != 0);
+      bits++;
+
       insert_u64_bit(&config, intel_use_jay(compiler->devinfo, i) != 0);
       bits++;
    }

src/intel/compiler/brw/brw_simd_selection.cpp
@@ -89,6 +89,16 @@ simd_debug_allowed(mesa_shader_stage stage, unsigned simd)
    return intel_simd & (start << simd);
 }
 
+/**
+ * Return true if INTEL_SIMD_DEBUG force-enables the given SIMD mode.
+ */
+static bool
+simd_debug_forced(mesa_shader_stage stage, unsigned simd)
+{
+   return (intel_simd_overridden & (1 << stage)) &&
+          simd_debug_allowed(stage, simd);
+}
+
 bool
 brw_simd_should_compile(brw_simd_selection_state &state, unsigned simd)
 {
@@ -147,11 +157,11 @@ brw_simd_should_compile(brw_simd_selection_state &state, unsigned simd)
        *
        * TODO: Use performance_analysis and drop this rule.
        */
-      if (width == 32 && state.devinfo->ver < 20) {
-         if (!INTEL_DEBUG(DEBUG_DO32) && (state.compiled[0] || state.compiled[1])) {
-            state.error[simd] = "SIMD32 not required (use INTEL_DEBUG=do32 to force)";
-            return false;
-         }
+      if (width == 32 && state.devinfo->ver < 20 &&
+          (state.compiled[0] || state.compiled[1]) &&
+          !simd_debug_forced(prog_data->stage, 2)) {
+         state.error[simd] = "SIMD32 not required (use INTEL_SIMD_DEBUG to force)";
+         return false;
       }
    }
 

src/intel/compiler/brw/test_simd_selection.cpp
@@ -253,7 +253,7 @@ TEST_F(SIMDSelectionCS, SpillAtSIMD16)
 
 TEST_F(SIMDSelectionCS, EnvironmentVariable32)
 {
-   BITSET_SET(intel_debug, DEBUG_DO32);
+   intel_simd_overridden |= BITFIELD_BIT(MESA_SHADER_COMPUTE);
 
    ASSERT_TRUE(brw_simd_should_compile(simd_state, SIMD8));
    brw_simd_mark_compiled(simd_state, SIMD8, not_spilled);
@@ -267,7 +267,7 @@ TEST_F(SIMDSelectionCS, EnvironmentVariable32)
 
 TEST_F(SIMDSelectionCS, EnvironmentVariable32ButSpills)
 {
-   BITSET_SET(intel_debug, DEBUG_DO32);
+   intel_simd_overridden |= BITFIELD_BIT(MESA_SHADER_COMPUTE);
 
    ASSERT_TRUE(brw_simd_should_compile(simd_state, SIMD8));
    brw_simd_mark_compiled(simd_state, SIMD8, not_spilled);

src/intel/compiler/elk/elk_compiler.c
@@ -146,7 +146,6 @@ elk_get_compiler_config_value(const struct elk_compiler *compiler)
       DEBUG_SPILL_FS,
       DEBUG_SPILL_VEC4,
       DEBUG_NO_COMPACTION,
-      DEBUG_DO32,
       DEBUG_SOFT64,
       DEBUG_NO_SEND_GATHER,
    };
@@ -160,6 +159,11 @@ elk_get_compiler_config_value(const struct elk_compiler *compiler)
 
    u_foreach_bit64(bit, mask)
       insert_u64_bit(&config, (intel_simd & (1ULL << bit)) != 0);
+
+   for (unsigned i = 0; i < MESA_SHADER_STAGES; i++) {
+      insert_u64_bit(&config, (intel_simd_overridden & (1 << i)) != 0);
+      bits++;
+   }
 
    mask = 3;
    bits += util_bitcount64(mask);

src/intel/compiler/elk/elk_fs.cpp
@@ -6713,8 +6713,11 @@ elk_compile_fs(const struct elk_compiler *compiler,
                              v32->fail_msg);
       } else {
          const performance &perf = v32->performance_analysis.require();
+         bool force_simd32 =
+            intel_simd_overridden & (1 << MESA_SHADER_FRAGMENT);
 
-         if (!INTEL_DEBUG(DEBUG_DO32) && throughput >= perf.throughput) {
+
+         if (!force_simd32 && throughput >= perf.throughput) {
             elk_shader_perf_log(compiler, params->base.log_data,
                                 "SIMD32 shader inefficient\n");
          } else {

src/intel/compiler/elk/elk_simd_selection.cpp
@@ -47,6 +47,16 @@ simd_debug_allowed(unsigned simd)
    return intel_simd & (DEBUG_CS_SIMD8 << simd);
 }
 
+/**
+ * Return true if INTEL_SIMD_DEBUG force-enables the given SIMD mode.
+ */
+static bool
+simd_debug_forced(unsigned simd)
+{
+   return (intel_simd_overridden & (1 << MESA_SHADER_COMPUTE)) &&
+          simd_debug_allowed(simd);
+}
+
 bool
 elk_simd_should_compile(elk_simd_selection_state &state, unsigned simd)
 {
@@ -97,8 +107,9 @@ elk_simd_should_compile(elk_simd_selection_state &state, unsigned simd)
        * TODO: Use performance_analysis and drop this rule.
        */
       if (width == 32) {
-         if (!INTEL_DEBUG(DEBUG_DO32) && (state.compiled[0] || state.compiled[1])) {
-            state.error[simd] = "SIMD32 not required (use INTEL_DEBUG=do32 to force)";
+         if (!simd_debug_forced(2) &&
+             (state.compiled[0] || state.compiled[1])) {
+            state.error[simd] = "SIMD32 not required (use INTEL_SIMD_DEBUG=cs32 to force)";
             return false;
          }
       }

commit 48297c8344669c4a867fae28e2cafb646218016e

intel: Fix operator precedence in intel_simd_overridden
| binds more tightly than ?:, so this chain of ternaries wasn't OR'd
together but parsed as a nested ternary that returns only the first
matching stage's bit.  With more than one stage requested, the later
stages were silently dropped (e.g. INTEL_SIMD_DEBUG=cs32,ts16 gave
0x20, COMPUTE only, instead of 0x60).

Wrap each ternary in parentheses.

Fixes: baf2f0ea4a9 ("intel: Replace INTEL_DEBUG=do32 with INTEL_SIMD_DEBUG")

Signed-off-by: Michael Cheng <michael.cheng@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42996>

src/intel/dev/intel_debug.c
@@ -260,16 +260,16 @@ process_intel_debug_variable_once(void)
     * are allowed, except FS currently disables multipolygon modes by default.
     */
    intel_simd_overridden =
-      (intel_simd & DEBUG_FS_SIMD) ? (1 << MESA_SHADER_FRAGMENT) : 0 |
-      (intel_simd & DEBUG_CS_SIMD) ? (1 << MESA_SHADER_COMPUTE)  : 0 |
-      (intel_simd & DEBUG_TS_SIMD) ? (1 << MESA_SHADER_TASK)     : 0 |
-      (intel_simd & DEBUG_MS_SIMD) ? (1 << MESA_SHADER_MESH)     : 0 |
-      (intel_simd & DEBUG_RT_SIMD) ? (1 << MESA_SHADER_RAYGEN |
-                                      1 << MESA_SHADER_ANY_HIT |
-                                      1 << MESA_SHADER_CLOSEST_HIT |
-                                      1 << MESA_SHADER_MISS |
-                                      1 << MESA_SHADER_INTERSECTION |
-                                      1 << MESA_SHADER_CALLABLE) : 0;
+      ((intel_simd & DEBUG_FS_SIMD) ? (1 << MESA_SHADER_FRAGMENT) : 0) |
+      ((intel_simd & DEBUG_CS_SIMD) ? (1 << MESA_SHADER_COMPUTE)  : 0) |
+      ((intel_simd & DEBUG_TS_SIMD) ? (1 << MESA_SHADER_TASK)     : 0) |
+      ((intel_simd & DEBUG_MS_SIMD) ? (1 << MESA_SHADER_MESH)     : 0) |
+      ((intel_simd & DEBUG_RT_SIMD) ? (1 << MESA_SHADER_RAYGEN |
+                                       1 << MESA_SHADER_ANY_HIT |
+                                       1 << MESA_SHADER_CLOSEST_HIT |
+                                       1 << MESA_SHADER_MISS |
+                                       1 << MESA_SHADER_INTERSECTION |
+                                       1 << MESA_SHADER_CALLABLE) : 0);
 
    if (!(intel_simd & DEBUG_FS_SIMD))
       intel_simd |=   DEBUG_FS_SIMD8 | DEBUG_FS_SIMD16 | DEBUG_FS_SIMD32;