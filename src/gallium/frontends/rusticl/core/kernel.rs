joshuaswarren/mesa-1

rusticl: return Result instead of Option from convert_spirv_to_nir
Backport-to: *
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42646>

src/gallium/frontends/rusticl/core/kernel.rs
@@ -1223,16 +1223,18 @@ pub(super) fn convert_spirv_to_nir(
     args: &[spirv::SPIRVKernelArg],
     spec_constants: &mut HashMap<u32, Vec<u8>>,
     dev: &'static Device,
-) -> Option<SPIRVToNirResult> {
+) -> Result<SPIRVToNirResult, &'static CStr> {
     let cache = dev.screen().shader_cache();
     let key = build.hash_key(cache.as_ref(), name, spec_constants);
     let spirv_info = build.kernel_info(name).unwrap();
 
-    cache
+    match cache
         .as_ref()
         .and_then(|cache| cache.get(&mut key?))
         .and_then(|entry| SPIRVToNirResult::deserialize(&entry, dev, spirv_info))
-        .or_else(|| {
+    {
+        Some(entry) => Ok(entry),
+        None => {
             let nir = build.to_nir(name, dev, spec_constants)?;
 
             if Platform::dbg().nir {
@@ -1266,14 +1268,15 @@ pub(super) fn convert_spirv_to_nir(
                 }
             }
 
-            Some(SPIRVToNirResult::new(
+            Ok(SPIRVToNirResult::new(
                 dev,
                 spirv_info,
                 args,
                 default_build,
                 optimized,
             ))
-        })
+        }
+    }
 }
 
 fn extract<'a, const S: usize>(buf: &'a mut &[u8]) -> &'a [u8; S] {

src/gallium/frontends/rusticl/core/program.rs
@@ -142,12 +142,19 @@ impl ProgramBuild {
                     continue;
                 }
 
-                let Some(build_result) =
-                    convert_spirv_to_nir(build, kernel_name, &args, &mut self.spec_constants, dev)
-                else {
-                    build.status = CL_BUILD_ERROR;
-                    build.log = c"Internal compilation error".to_owned();
-                    return;
+                let build_result = match convert_spirv_to_nir(
+                    build,
+                    kernel_name,
+                    &args,
+                    &mut self.spec_constants,
+                    dev,
+                ) {
+                    Ok(build_result) => build_result,
+                    Err(err) => {
+                        build.status = CL_BUILD_ERROR;
+                        build.log = c"Internal compilation error: ".concat(err);
+                        return;
+                    }
                 };
                 kernel_info_set.insert(build_result.kernel_info);
 
@@ -246,7 +253,7 @@ impl DeviceProgramBuild {
         kernel: &CStr,
         device: &Device,
         spec_constants: &mut HashMap<u32, Vec<u8>>,
-    ) -> Option<NirShader> {
+    ) -> Result<NirShader, &'static CStr> {
         assert_eq!(self.status, CL_BUILD_SUCCESS as cl_build_status);
 
         let mut spec_constants: Vec<_> = spec_constants
@@ -265,16 +272,21 @@ impl DeviceProgramBuild {
         };
 
         let mut log = Platform::dbg().program.then(Vec::new);
-        let nir = self.spirv.as_ref().unwrap().to_nir(
-            kernel,
-            device
-                .screen
-                .nir_shader_compiler_options(mesa_shader_stage::MESA_SHADER_COMPUTE),
-            device.spirv_to_nir_opts(),
-            &device.lib_clc,
-            &mut spec_constants,
-            log.as_mut(),
-        );
+        let nir = self
+            .spirv
+            .as_ref()
+            .unwrap()
+            .to_nir(
+                kernel,
+                device
+                    .screen
+                    .nir_shader_compiler_options(mesa_shader_stage::MESA_SHADER_COMPUTE),
+                device.spirv_to_nir_opts(),
+                &device.lib_clc,
+                &mut spec_constants,
+                log.as_mut(),
+            )
+            .ok_or(c"spirv_to_nir failed");
 
         if let Some(log) = log {
             for line in log {

sailfishos-mirror/mesa

(cherry picked from commit 1fe2442367288fbf3e95ece8e8e1ca33ea6abcbb)
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42986>

src/gallium/frontends/rusticl/core/kernel.rs
@@ -1227,16 +1227,18 @@ pub(super) fn convert_spirv_to_nir(
     args: &[spirv::SPIRVKernelArg],
     spec_constants: &HashMap<u32, nir_const_value>,
     dev: &'static Device,
-) -> Option<SPIRVToNirResult> {
+) -> Result<SPIRVToNirResult, &'static str> {
     let cache = dev.screen().shader_cache();
     let key = build.hash_key(cache.as_ref(), name, spec_constants);
     let spirv_info = build.kernel_info(name).unwrap();
 
-    cache
+    match cache
         .as_ref()
         .and_then(|cache| cache.get(&mut key?))
         .and_then(|entry| SPIRVToNirResult::deserialize(&entry, dev, spirv_info))
-        .or_else(|| {
+    {
+        Some(entry) => Ok(entry),
+        None => {
             let nir = build.to_nir(name, dev, spec_constants)?;
 
             if Platform::dbg().nir {
@@ -1270,14 +1272,15 @@ pub(super) fn convert_spirv_to_nir(
                 }
             }
 
-            Some(SPIRVToNirResult::new(
+            Ok(SPIRVToNirResult::new(
                 dev,
                 spirv_info,
                 args,
                 default_build,
                 optimized,
             ))
-        })
+        }
+    }
 }
 
 fn extract<'a, const S: usize>(buf: &'a mut &[u8]) -> &'a [u8; S] {

src/gallium/frontends/rusticl/core/program.rs
@@ -137,12 +137,19 @@ impl ProgramBuild {
                     continue;
                 }
 
-                let Some(build_result) =
-                    convert_spirv_to_nir(build, kernel_name, &args, &self.spec_constants, dev)
-                else {
-                    build.status = CL_BUILD_ERROR;
-                    build.log = "Internal compilation error".to_owned();
-                    return;
+                let build_result = match convert_spirv_to_nir(
+                    build,
+                    kernel_name,
+                    &args,
+                    &self.spec_constants,
+                    dev,
+                ) {
+                    Ok(build_result) => build_result,
+                    Err(err) => {
+                        build.status = CL_BUILD_ERROR;
+                        build.log = format!("Internal compilation error: {err}");
+                        return;
+                    }
                 };
                 kernel_info_set.insert(build_result.kernel_info);
 
@@ -236,7 +243,7 @@ impl DeviceProgramBuild {
         kernel: &str,
         device: &Device,
         spec_constants: &HashMap<u32, nir_const_value>,
-    ) -> Option<NirShader> {
+    ) -> Result<NirShader, &'static str> {
         assert_eq!(self.status, CL_BUILD_SUCCESS as cl_build_status);
 
         let mut spec_constants: Vec<_> = spec_constants
@@ -249,17 +256,22 @@ impl DeviceProgramBuild {
             .collect();
 
         let mut log = Platform::dbg().program.then(Vec::new);
-        let nir = self.spirv.as_ref().unwrap().to_nir(
-            kernel,
-            device
-                .screen
-                .nir_shader_compiler_options(mesa_shader_stage::MESA_SHADER_COMPUTE),
-            &device.spirv_caps,
-            &device.lib_clc,
-            &mut spec_constants,
-            device.address_bits(),
-            log.as_mut(),
-        );
+        let nir = self
+            .spirv
+            .as_ref()
+            .unwrap()
+            .to_nir(
+                kernel,
+                device
+                    .screen
+                    .nir_shader_compiler_options(mesa_shader_stage::MESA_SHADER_COMPUTE),
+                &device.spirv_caps,
+                &device.lib_clc,
+                &mut spec_constants,
+                device.address_bits(),
+                log.as_mut(),
+            )
+            .ok_or("spirv_to_nir failed");
 
         if let Some(log) = log {
             for line in log {