7268ad5fa8de4a5b0828ac151310a5923a12c5bb
Nanley Chery <nanley.g.chery@intel.com>
2026-07-16 15:27:13 -0400

    anv: Allow 2MB page oversubscription for 1MB+ BOs
    Improves the geomean of workloads on the PTL in the performance CI by
    1.13% (n=4). Five traces are improved from 0.75-1.39%. The following
    traces are most impacted:
    
    * Hellblade2              +16.99%
    * Returnal                + 4.63%
    * EldenRingNightreign     + 3.14%
    * DeepRockGalactic        + 3.05%
    * KingdomComeDeliverance2 + 2.35%
    * ClairObscur             + 2.09%
    
    This resolves an issue, reported by Felix DeGrood, where some HiZ clears
    took a very long time in the DeepRockGalactic trace. By using more 2MB
    pages, the performance of the HiZ clears with iommu=on is comparable to
    the same clears performed with iommu=pt (~16x improvement).
    
    See the equivalent iris change in 0b6693a3a14
    ("iris: Align fresh BO allocations to 2MB in size").
    
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43087>

--- src/intel/vulkan/anv_allocator.c
+++ src/intel/vulkan/anv_allocator.c
@@ -491,6 +491,11 @@ anv_device_has_perf_improvement_with_2mb_pages(struct anv_device *device)
 static bool
 anv_device_has_perf_improvement_with_64k_pages(struct anv_device *device)
 {
+   /* Whether to reduce page count at the cost of increased memory footprint.
+    * On MTL(Xe KMD only)/LNL platforms, there is a large perf penalty from
+    * page misses. On PTL platforms, there is a large perf penalty from IOMMU
+    * TLB thrashing.
+    */
    if (device->info->has_local_mem || device->info->verx10 < 110)
       return false;
 
@@ -1690,23 +1695,13 @@ anv_device_alloc_bo(struct anv_device *device,
       size = align64(size, 4096);
    }
 
-   /* Round up allocations to 2MB intervals so long as increase doesn't
-    * increase allocation by more than 1.33%. This reduces page
-    * count at the cost of increased memory footprint. Only apply on
-    * MTL(Xe KMD only)/LNL platforms, which incur largest perf penalty from
-    * page misses.
+   /* Try to allocate memory in multiples of 2MB, as this allows us to use
+    * 2MB pages rather than the less-efficient 4K pages.
     */
    if (!ANV_DEBUG(NO_ALLOC_OVER_SUBSCRIPTION)) {
-      if (align64(size, 2 * 1024 * 1024) <= (size * 4 / 3) &&
+      if (size >= 1 * 1024 * 1024 &&
           anv_device_has_perf_improvement_with_2mb_pages_oversubscription(device))
          size = align64(size, 2 * 1024 * 1024);
-      /* bos larger than 1MB can't be allocated with slab but to reduce pages we
-       * could align size to 64k pages to gain performance with minimum memory
-       * waste.
-       */
-      else if ((size > (1 * 1024 * 1024)) &&
-               anv_device_has_perf_improvement_with_64k_pages(device))
-         size = align64(size, 64 * 1024);
    }
 
    const struct intel_memory_class_instance *regions[2];
