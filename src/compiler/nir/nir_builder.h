39c018b9af8f01dcd5a86309a7db603762e10b1e
Calder Young <calder.young@intel.com>
2026-07-21 10:24:01 -0700

    nir/builder: Add nir_halt_if helper
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42265>

--- src/compiler/nir/nir_builder.h
+++ src/compiler/nir/nir_builder.h
@@ -2351,6 +2351,16 @@ nir_break_if(nir_builder *build, nir_def *cond)
 }
 
 static inline void
+nir_halt_if(nir_builder *build, nir_def *cond)
+{
+   nir_if *nif = nir_push_if(build, cond);
+   {
+      nir_jump(build, nir_jump_halt);
+   }
+   nir_pop_if(build, nif);
+}
+
+static inline void
 nir_build_call(nir_builder *build, nir_function *func, size_t count,
                nir_def **args)
 {
