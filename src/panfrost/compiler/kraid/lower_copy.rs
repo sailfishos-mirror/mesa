ab59562ea6ef70763d9d38a2a841d38056495ca7
Faith Ekstrand <faith.ekstrand@collabora.com>
2026-07-23 02:16:55 -0400

    kraid: Use the builder in lower_copy()
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43211>

--- src/panfrost/compiler/kraid/lower_copy.rs
+++ src/panfrost/compiler/kraid/lower_copy.rs
@@ -1,10 +1,11 @@
 // Copyright © 2026 Collabora, Ltd.
 // SPDX-License-Identifier: MIT
 
+use crate::builder::*;
 use crate::ir::*;
 use crate::ops::*;
 
-fn lower_copy(copy: OpCopy) -> MappedInstrs {
+fn lower_copy(b: &mut impl Builder, copy: OpCopy) {
     debug_assert!(copy.src.src_mod.is_none());
     debug_assert!(copy.src_supports_swizzle(&copy.src, copy.src.swizzle));
 
@@ -22,7 +23,7 @@ fn lower_copy(copy: OpCopy) -> MappedInstrs {
             let mask = copy.dst.lanes.u32_mask().unwrap();
 
             // For an immediate copy, we can just do and AND+OR
-            let and = OpShiftLop {
+            b.push_op(OpShiftLop {
                 dst: dst_reg.into(),
                 dst_type: DataType::U32,
                 shift_op: ShiftOp::None,
@@ -31,21 +32,18 @@ fn lower_copy(copy: OpCopy) -> MappedInstrs {
                 src0: dst_reg.into(),
                 shift: Src::imm_u8(0),
                 src2: (!mask).into(),
-            };
-            let or = OpShiftLop {
-                dst: dst_reg.into(),
-                dst_type: DataType::U32,
-                shift_op: ShiftOp::None,
-                logic_op: LogicOp::Or,
-                not_result: false,
-                src0: dst_reg.into(),
-                shift: Src::imm_u8(0),
-                src2: (imm & mask).into(),
-            };
-            if imm == 0 {
-                [Instr::from(and)].into()
-            } else {
-                [Instr::from(and), Instr::from(or)].into()
+            });
+            if imm != 0 {
+                b.push_op(OpShiftLop {
+                    dst: dst_reg.into(),
+                    dst_type: DataType::U32,
+                    shift_op: ShiftOp::None,
+                    logic_op: LogicOp::Or,
+                    not_result: false,
+                    src0: dst_reg.into(),
+                    shift: Src::imm_u8(0),
+                    src2: (imm & mask).into(),
+                });
             }
         } else {
             let dst_b = copy.dst.lanes.as_byte_range().unwrap().start;
@@ -67,33 +65,31 @@ fn lower_copy(copy: OpCopy) -> MappedInstrs {
                     [Src::from(dst_reg).byte(0), Src::from(dst_reg).byte(1)];
                 srcs[usize::from(dst_b)] = copy.src;
 
-                let mkvec = OpMkVecV2I8I16 {
+                b.push_op(OpMkVecV2I8I16 {
                     dst: dst_reg.into(),
                     srcs,
                     accum: Src::imm_u16(0),
-                };
-                [Instr::from(mkvec)].into()
+                });
             } else {
                 let mut srcs =
                     [Src::from(dst_reg).byte(2), Src::from(dst_reg).byte(3)];
                 srcs[usize::from(dst_b - 2)] = copy.src;
 
-                let mkvec = OpMkVecV2I8I16 {
+                b.push_op(OpMkVecV2I8I16 {
                     dst: dst_reg.into(),
                     srcs,
                     accum: Src::from(dst_reg).half(0),
-                };
+                });
 
                 // We now have byte 2 or 3 swapped out for the selected src
                 // byte by the two halves have been flipped.
-                let swap = OpMkVecV2I16 {
+                b.push_op(OpMkVecV2I16 {
                     dst: dst_reg.into(),
                     srcs: [
                         Src::from(dst_reg).half(1),
                         Src::from(dst_reg).half(0),
                     ],
-                };
-                [Instr::from(mkvec), Instr::from(swap)].into()
+                });
             }
         }
     } else {
@@ -109,21 +105,19 @@ fn lower_copy(copy: OpCopy) -> MappedInstrs {
             } else {
                 DataType::I32
             };
-            let mov = OpMov {
+            b.push_op(OpMov {
                 dst: copy.dst,
                 dst_type: mov_type,
                 src: imm.into(),
-            };
-            [Instr::from(mov)].into()
+            });
         } else if copy.dst_type.total_bits() <= 32
             && copy.src.swizzle == Swizzle::NONE
         {
-            let mov = OpMov {
+            b.push_op(OpMov {
                 dst: copy.dst,
                 dst_type: DataType::I32,
                 src: copy.src,
-            };
-            [Instr::from(mov)].into()
+            });
         } else {
             // Everything else is ShiftLop
 
@@ -134,7 +128,7 @@ fn lower_copy(copy: OpCopy) -> MappedInstrs {
             let comps = 32_u8.div_ceil(bits);
             let dst_type = DataType::v(comps, DataType::u(bits));
 
-            let lop = OpShiftLop {
+            b.push_op(OpShiftLop {
                 dst: copy.dst,
                 dst_type,
                 shift_op: ShiftOp::None,
@@ -143,16 +137,20 @@ fn lower_copy(copy: OpCopy) -> MappedInstrs {
                 src0: copy.src,
                 shift: Src::imm_u8(0),
                 src2: 0.into(),
-            };
-            [Instr::from(lop)].into()
+            });
         }
     }
 }
 
 impl Shader<'_> {
     pub fn lower_copy(&mut self) {
+        let model = self.model;
         self.map_instrs(|instr, _| match instr.op {
-            Op::Copy(op) => lower_copy(*op),
+            Op::Copy(op) => {
+                let mut b = InstrBuilder::new(model);
+                lower_copy(&mut b, *op);
+                b.into_mapped()
+            }
             _ => [instr].into(),
         })
     }
