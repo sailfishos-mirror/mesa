sailfishos-mirror/mesa

commit 45582c89a8c64f55784ace8daec70a6bf361e75d

pan: in register allocation, consider spilling the node being allocated
In some cases, we may be trying to allocate a node that is
used in a very large number of blocks. In that case, it may be
better to spill that node rather than going through the blocks
one by one spilling other registers. This actually comes up
fairly rarely in practice (because the SSA register spiller usually
prevents us from needing spills) but there are edge cases where the
LCRA and SSA allocators differ on the register pressure and LCRA
wants to spill.

Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Signed-off-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42888>

src/panfrost/compiler/bifrost/bi_ra.c
@@ -628,6 +628,15 @@ bi_choose_spill_node(bi_context *ctx, struct lcra_state *l)
    unsigned best_benefit = 0;
    signed best_node = -1;
 
+   /* consider spilling the node that we're trying to allocate,
+    * rather the ones interfering with it, if that would be
+    * better
+    */
+   if (!BITSET_TEST(no_spill, l->spill_node)) {
+      best_node = l->spill_node;
+      best_benefit = lcra_count_constraints(l, best_node);
+   }
+
    if (nodearray_is_sparse(&l->linear[l->spill_node])) {
       nodearray_sparse_foreach(&l->linear[l->spill_node], elem) {
          unsigned i = nodearray_sparse_key(elem);
@@ -1222,7 +1231,7 @@ bi_register_allocate(bi_context *ctx)
    bool success = false;
 
    unsigned iter_count = 0;
-   unsigned max_iters = 2000;
+   unsigned max_iters = 400;
    /* Number of bytes of memory we've spilled into */
    unsigned spill_count = ctx->info.tls_size;
 

commit b84ccacd138bfb362ac25ca3a5a222955e4e2df1

panfrost: restore max retry count for bifrost LCRA spills
We incorrectly thought the maximum retries could be reduced with a
recent LCRA spill fix. That was wrong (it's only reduced in some cases.
in others it still needs the same number of retries as before).

Fixes: 45582c89a8c ("pan: in register allocation, consider spilling the node being allocated")
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Signed-off-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43168>

src/panfrost/compiler/bifrost/bi_ra.c
@@ -1231,7 +1231,7 @@ bi_register_allocate(bi_context *ctx)
    bool success = false;
 
    unsigned iter_count = 0;
-   unsigned max_iters = 400;
+   unsigned max_iters = 2000;
    /* Number of bytes of memory we've spilled into */
    unsigned spill_count = ctx->info.tls_size;
 