ac6a47fbf5fac98c0c27ba9e2b6d602a5df8b33a
Faith Ekstrand <faith.ekstrand@collabora.com>
2026-07-25 19:57:39 -0400

    kraid: Don't re-materialize constants interleaved with phis
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43211>

--- src/panfrost/compiler/kraid/remat_constants.rs
+++ src/panfrost/compiler/kraid/remat_constants.rs
@@ -23,9 +23,39 @@ fn add_copy(ssa_const: &mut FxHashMap<SSAValue, u32>, copy: &OpCopy) {
     ssa_const.insert(vec[0], copy.src.swizzle.fold_u32(u).unwrap());
 }
 
+fn remat_src(
+    b: &mut impl SSABuilder,
+    ssa_const: &FxHashMap<SSAValue, u32>,
+    src: &mut Src,
+    is_sr_src: bool,
+) {
+    let SrcRef::SSA(vec) = &mut src.src_ref else {
+        return;
+    };
+
+    let comps = vec.comps();
+    for ssa in vec {
+        let Some(u) = ssa_const.get(ssa) else {
+            continue;
+        };
+
+        if comps == 1 && *u == 0 && !is_sr_src {
+            src.src_ref = SrcRef::Zero;
+            break;
+        }
+
+        *ssa = match ssa.bits() {
+            8 => b.copy_i8(Src::imm_u8(*u as u8)),
+            16 => b.copy_i16(Src::imm_u16(*u as u16)),
+            32 => b.copy_i32(Src::from(*u)),
+            bits => panic!("Invalid SSAValue size: {bits}"),
+        };
+    }
+}
+
 fn remat_copies(
     b: &mut impl SSABuilder,
-    ssa_const: &mut FxHashMap<SSAValue, u32>,
+    ssa_const: &FxHashMap<SSAValue, u32>,
     op: &mut Op,
 ) {
     let mut sr_srcs = 0_u8;
@@ -36,29 +66,8 @@ fn remat_copies(
     }
 
     for (i, src) in op.srcs_mut().iter_mut().enumerate() {
-        let SrcRef::SSA(vec) = &mut src.src_ref else {
-            continue;
-        };
-
-        let comps = vec.comps();
-        for ssa in vec {
-            let Some(u) = ssa_const.get(ssa) else {
-                continue;
-            };
-
-            let is_sr_src = ((sr_srcs >> i) & 1) != 0;
-            if comps == 1 && *u == 0 && !is_sr_src {
-                src.src_ref = SrcRef::Zero;
-                break;
-            }
-
-            *ssa = match ssa.bits() {
-                8 => b.copy_i8(Src::imm_u8(*u as u8)),
-                16 => b.copy_i16(Src::imm_u16(*u as u16)),
-                32 => b.copy_i32(Src::from(*u)),
-                bits => panic!("Invalid SSAValue size: {bits}"),
-            };
-        }
+        let is_sr_src = ((sr_srcs >> i) & 1) != 0;
+        remat_src(b, ssa_const, src, is_sr_src);
     }
 }
 
@@ -66,15 +75,29 @@ impl Shader<'_> {
     pub fn remat_constants(&mut self) {
         let model = self.model;
         let mut ssa_const: FxHashMap<SSAValue, u32> = Default::default();
+
         self.map_instrs(|mut instr, ssa_alloc| {
+            // We have to handle these separately
+            if matches!(&instr.op, Op::PhiSrc(_) | Op::RegOut(_)) {
+                return [instr].into();
+            }
+
             if let Op::Copy(op) = &instr.op {
                 add_copy(&mut ssa_const, op);
             }
 
             let mut b = SSAInstrBuilder::new(model, ssa_alloc);
-            remat_copies(&mut b, &mut ssa_const, &mut instr.op);
+            remat_copies(&mut b, &ssa_const, &mut instr.op);
             b.push_instr(instr);
             b.into_mapped()
-        })
+        });
+
+        for block in self.blocks.iter_mut() {
+            let mut b = SSAInstrBuilder::new(model, &mut self.ssa_alloc);
+            for instr in block.iter_postlude_mut() {
+                remat_copies(&mut b, &ssa_const, &mut instr.op);
+            }
+            block.insert_before_postlude(b.into_mapped())
+        }
     }
 }
