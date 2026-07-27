sailfishos-mirror/mesa

commit 5c15ef5057b7ebc8cb8771acb4ffb2ed602d3665

pan/perf: Transition to auto-generated derived counters
Now that we have everything in place, let's transition to
derived counters using the auto-generated counter definitions.

Co-authored-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41030>

src/panfrost/perf/quick.c
@@ -39,17 +39,11 @@ main(void)
 
    pan_perf_dump(perf);
 
-   for (unsigned i = 0; i < perf->cfg->n_categories; ++i) {
-      const struct pan_perf_category *cat = &perf->cfg->categories[i];
-      printf("%s\n", cat->name);
+   for (const struct mali_perf_counter *ctr = perf->info->counters; ctr->name;
+        ctr++) {
+      int64_t val = pan_perf_counter_read_sum(perf, ctr);
 
-      for (unsigned j = 0; j < cat->n_counters; ++j) {
-         const struct pan_perf_counter *ctr = &cat->counters[j];
-         int64_t val = pan_perf_counter_read_sum(perf, ctr);
-         printf("%s (%s): %ld\n", ctr->name, ctr->symbol_name, val);
-      }
-
-      printf("\n");
+      printf("%s: %ld\n", ctr->name, val);
    }
 
    if (pan_perf_disable(perf) < 0) {

src/panfrost/ds/pan_pps_perf.cpp
@@ -14,6 +14,8 @@
 #include <util/ralloc.h>
 #include <util/timespec.h>
 
+#include <sstream>
+
 namespace pps {
 
 PanfrostPerf::PanfrostPerf(const PanfrostDevice &dev)
@@ -80,31 +82,26 @@ PanfrostPerf::get_subinstance() {
    return perf;
 }
 
-std::string
-format_suffix(const char *fmt, uint8_t idx)
+static std::string
+get_counter_name(const char *base_name, enum mali_perf_block_type blk_type,
+                 uint8_t blk_idx)
 {
-   assert(strlen(fmt) < 200 && "fmt unreasonably long");
-   char buf[256];
-   std::snprintf(buf, sizeof(buf), fmt, idx);
+   std::stringstream ss;
 
-   return std::string(buf);
-}
-
-const char *
-get_block_suffix(uint8_t category)
-{
-   assert(category <= MALI_PERF_BLOCK_TYPE_COUNT);
-
-   switch (category) {
+   switch (blk_type) {
    case MALI_PERF_BLOCK_MEMSYS:
-      return " (slice %u)";
+      ss << base_name << " (slice " << (unsigned)blk_idx << ")";
+      break;
    case MALI_PERF_BLOCK_SHADER_CORE:
-      return " (core %u)";
+      ss << base_name << " (core " << (unsigned)blk_idx << ")";
+      break;
    default:
-      return nullptr;
+      assert(!blk_idx);
+      ss << base_name;
+      break;
    }
 
-   return nullptr;
+   return ss.str();
 }
 
 Counter::Units
@@ -124,57 +121,80 @@ convert_pan_units(enum mali_perf_counter_units unit)
    }
 }
 
+static uint32_t
+convert_classes(uint64_t classes)
+{
+   static_assert((int)MALI_PERF_UNCLASSIFIED == (int)Perfettogrp::UNCLASSIFIED);
+   static_assert((int)MALI_PERF_SYSTEM == (int)Perfettogrp::SYSTEM);
+   static_assert((int)MALI_PERF_VERTICES == (int)Perfettogrp::VERTICES);
+   static_assert((int)MALI_PERF_FRAGMENTS == (int)Perfettogrp::FRAGMENTS);
+   static_assert((int)MALI_PERF_PRIMITIVES == (int)Perfettogrp::PRIMITIVES);
+   static_assert((int)MALI_PERF_MEMORY == (int)Perfettogrp::MEMORY);
+   static_assert((int)MALI_PERF_COMPUTE == (int)Perfettogrp::COMPUTE);
+
+   /* RAY_TRACING not yet exposed by pps_counter. */
+   if (classes & MALI_PERF_RAY_TRACING)
+      classes &= ~MALI_PERF_RAY_TRACING;
+
+   assert(!(classes &
+            ~(BITFIELD64_BIT(MALI_PERF_UNCLASSIFIED) |
+              BITFIELD64_BIT(MALI_PERF_SYSTEM) |
+              BITFIELD64_BIT(MALI_PERF_VERTICES) |
+              BITFIELD64_BIT(MALI_PERF_FRAGMENTS) |
+              BITFIELD64_BIT(MALI_PERF_PRIMITIVES) |
+              BITFIELD64_BIT(MALI_PERF_MEMORY) |
+              BITFIELD64_BIT(MALI_PERF_COMPUTE) |
+              BITFIELD64_BIT(MALI_PERF_RAY_TRACING))));
+
+   if (!classes)
+      classes = BITFIELD64_BIT(MALI_PERF_UNCLASSIFIED);
+
+   assert(classes <= UINT32_MAX);
+   return classes;
+}
+
 std::pair<std::vector<CounterGroup>, std::vector<Counter>>
 PanfrostPerf::create_available_counters() const
 {
    std::pair<std::vector<CounterGroup>, std::vector<Counter>> ret;
    auto &[groups, counters] = ret;
-
    uint32_t global_counter_id = 0;
 
-   const struct pan_perf_category *category = NULL;
-   for (uint32_t cat_idx = 0; cat_idx < perf->cfg->n_categories; ++cat_idx) {
-      assert(cat_idx < MALI_PERF_BLOCK_TYPE_COUNT);
-      category = &perf->cfg->categories[cat_idx];
+   for (const struct mali_perf_counter *cinfo = perf->info->counters;
+        cinfo->name; cinfo++) {
+      for (uint32_t j = 0;
+           j < mali_perf_block_count(cinfo->block, &perf->constants); j++) {
 
-      if (!category->n_counters)
-         continue;
+         Counter counter = {};
+         counter.id = global_counter_id++;
+         counter.name = get_counter_name(cinfo->name, cinfo->block, j);
+         counter.description = cinfo->desc;
+         counter.group = cinfo->block;
+         counter.group_mask = convert_classes(cinfo->classes);
+         counter.units = convert_pan_units(cinfo->units);
+         counter.set_getter([=](const Counter &c, const Driver &d) {
+            auto &pan_driver = PanfrostDriver::into(d);
+            struct pan_perf *perf = static_cast<struct pan_perf *>(
+               pan_driver.perf->get_subinstance());
+            return pan_perf_counter_read(perf, cinfo, j);
+         });
+         counters.emplace_back(counter);
+      }
+   }
 
-      enum mali_perf_block_type blk_type = category->counters[0].category_id;
+   for (uint32_t i = 0; i < MALI_PERF_BLOCK_TYPE_COUNT; i++) {
       CounterGroup group = {};
-      group.id = blk_type;
-      group.name = mali_perf_block_type_str(blk_type);
 
-      uint32_t n_blocks = mali_perf_block_count(blk_type, &perf->constants);
-      for (uint32_t counter_idx = 0; counter_idx < category->n_counters;
-           ++counter_idx) {
-         const struct pan_perf_counter *cinfo =
-            &category->counters[counter_idx];
+      group.id = i;
+      group.name = mali_perf_block_type_str((enum mali_perf_block_type)i);
 
-         for (uint32_t block_idx = 0; block_idx < n_blocks; ++block_idx) {
-            const char *suffix = get_block_suffix(group.id);
-            const std::string name =
-               cinfo->name + (suffix ? format_suffix(suffix, block_idx) : "");
-
-            Counter counter = {};
-            counter.id = global_counter_id++;
-            counter.name = name;
-            counter.group = group.id;
-            counter.units = convert_pan_units(cinfo->units);
-
-            counter.set_getter([=](const Counter &c, const Driver &d) {
-               auto &pan_driver = PanfrostDriver::into(d);
-               struct pan_perf *perf = static_cast<struct pan_perf *>(
-                  pan_driver.perf->get_subinstance());
-               return pan_perf_counter_read(perf, cinfo, block_idx);
-            });
-
+      for (auto &counter : counters) {
+         if (counter.group == (int32_t)group.id)
             group.counters.push_back(counter.id);
-            counters.emplace_back(counter);
-         }
       }
 
-      groups.push_back(group);
+      if (group.counters.size() > 0)
+         groups.push_back(group);
    }
 
    return ret;

src/panfrost/model/pan_model.c
@@ -63,53 +63,53 @@ const struct pan_model pan_model_list[] = {
    MIDGARD_MODEL(0x600,     "T600",   "T60x", MODEL_ANISO(NONE), MODEL_TB_SIZES( 4096,  4096),
                                               MODEL_QUIRKS( .max_4x_msaa = true )),
    MIDGARD_MODEL(0x620,     "T620",   "T62x", MODEL_ANISO(NONE), MODEL_TB_SIZES( 4096,  4096)),
-   MIDGARD_MODEL(0x720,     "T720",   "T72x", MODEL_ANISO(NONE), MODEL_TB_SIZES( 4096,  4096),
+   MIDGARD_MODEL(0x720,     "T720",   "T720", MODEL_ANISO(NONE), MODEL_TB_SIZES( 4096,  4096),
                                               MODEL_QUIRKS( .no_hierarchical_tiling = true, .max_4x_msaa = true )),
-   MIDGARD_MODEL(0x750,     "T760",   "T76x", MODEL_ANISO(NONE), MODEL_TB_SIZES( 8192,  8192)),
-   MIDGARD_MODEL(0x820,     "T820",   "T82x", MODEL_ANISO(NONE), MODEL_TB_SIZES( 8192,  8192),
+   MIDGARD_MODEL(0x750,     "T760",   "T760", MODEL_ANISO(NONE), MODEL_TB_SIZES( 8192,  8192)),
+   MIDGARD_MODEL(0x820,     "T820",   "T830", MODEL_ANISO(NONE), MODEL_TB_SIZES( 8192,  8192),
                                               MODEL_QUIRKS( .no_hierarchical_tiling = true, .max_4x_msaa = true )),
-   MIDGARD_MODEL(0x830,     "T830",   "T83x", MODEL_ANISO(NONE), MODEL_TB_SIZES( 8192,  8192),
+   MIDGARD_MODEL(0x830,     "T830",   "T830", MODEL_ANISO(NONE), MODEL_TB_SIZES( 8192,  8192),
                                               MODEL_QUIRKS( .no_hierarchical_tiling = true, .max_4x_msaa = true )),
-   MIDGARD_MODEL(0x860,     "T860",   "T86x", MODEL_ANISO(NONE), MODEL_TB_SIZES( 8192,  8192)),
-   MIDGARD_MODEL(0x880,     "T880",   "T88x", MODEL_ANISO(NONE), MODEL_TB_SIZES( 8192,  8192)),
+   MIDGARD_MODEL(0x860,     "T860",   "T880", MODEL_ANISO(NONE), MODEL_TB_SIZES( 8192,  8192)),
+   MIDGARD_MODEL(0x880,     "T880",   "T880", MODEL_ANISO(NONE), MODEL_TB_SIZES( 8192,  8192)),
 
-   BIFROST_MODEL(PAN_PROD_ID(6, 0, 0),    "G71",    "TMIx", MODEL_ANISO(NONE), MODEL_TB_SIZES( 4096,  4096)),
-   BIFROST_MODEL(PAN_PROD_ID(6, 2, 1),    "G72",    "THEx", MODEL_ANISO(R0P3), MODEL_TB_SIZES( 8192,  4096)),
-   BIFROST_MODEL(PAN_PROD_ID(7, 0, 0),    "G51",    "TSIx", MODEL_ANISO(R1P1), MODEL_TB_SIZES( 8192,  8192)),
-   BIFROST_MODEL(PAN_PROD_ID(7, 0, 3),    "G31",    "TDVx", MODEL_ANISO(ALL),  MODEL_TB_SIZES( 8192,  8192)),
-   BIFROST_MODEL(PAN_PROD_ID(7, 2, 1),    "G76",    "TNOx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192)),
-   BIFROST_MODEL(PAN_PROD_ID(7, 2, 2),    "G52",    "TGOx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192)),
-   BIFROST_MODEL(PAN_PROD_ID(7, 4, 2),    "G52 r1", "TGOx", MODEL_ANISO(ALL),  MODEL_TB_SIZES( 8192,  8192)),
+   BIFROST_MODEL(PAN_PROD_ID(6, 0, 0),    "G71",    "G71", MODEL_ANISO(NONE), MODEL_TB_SIZES( 4096,  4096)),
+   BIFROST_MODEL(PAN_PROD_ID(6, 2, 1),    "G72",    "G72", MODEL_ANISO(R0P3), MODEL_TB_SIZES( 8192,  4096)),
+   BIFROST_MODEL(PAN_PROD_ID(7, 0, 0),    "G51",    "G51", MODEL_ANISO(R1P1), MODEL_TB_SIZES( 8192,  8192)),
+   BIFROST_MODEL(PAN_PROD_ID(7, 0, 3),    "G31",    "G31", MODEL_ANISO(ALL),  MODEL_TB_SIZES( 8192,  8192)),
+   BIFROST_MODEL(PAN_PROD_ID(7, 2, 1),    "G76",    "G76", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192)),
+   BIFROST_MODEL(PAN_PROD_ID(7, 2, 2),    "G52",    "G52", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192)),
+   BIFROST_MODEL(PAN_PROD_ID(7, 4, 2),    "G52 r1", "G52", MODEL_ANISO(ALL),  MODEL_TB_SIZES( 8192,  8192)),
 
-   VALHALL_MODEL(PAN_PROD_ID(9, 0, 1), 0, "G57",    "TNAx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192),
+   VALHALL_MODEL(PAN_PROD_ID(9, 0, 1), 0, "G57",    "G77", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192),
                                               MODEL_RATES_X(2, 4, 8,  32,  32, 8)),
-   VALHALL_MODEL(PAN_PROD_ID(9, 0, 3), 0, "G57",    "TNAx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192),
+   VALHALL_MODEL(PAN_PROD_ID(9, 0, 3), 0, "G57",    "G77", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192),
                                               MODEL_RATES_X(2, 4, 8,  32,  32, 8)),
-   VALHALL_MODEL(PAN_PROD_ID(10, 8, 7), 0, "G610",   "TVIx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(32768, 16384),
+   VALHALL_MODEL(PAN_PROD_ID(10, 8, 7), 0, "G610",   "G710", MODEL_ANISO(ALL),  MODEL_TB_SIZES(32768, 16384),
                                               MODEL_RATES_X(4, 8, 16,  64,  64, 16)),
    /* var/cvt/sfu rates might not be correct (we haven't found any detailed documentation) */
-   VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 0, "G310v1",   "TVAx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192),
+   VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 0, "G310v1",   "G710", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192),
                                               MODEL_RATES_X(2, 2,  8,  16,  16, 4)),
-   VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 1, "G310v2",   "TVAx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192),
+   VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 1, "G310v2",   "G710", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192),
                                               MODEL_RATES_X(2, 4, 16,  32,  32, 8)),
-   VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 2, "G310v3",   "TVAx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192),
+   VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 2, "G310v3",   "G710", MODEL_ANISO(ALL),  MODEL_TB_SIZES(16384,  8192),
                                               MODEL_RATES_X(4, 4, 16,  48,  48, 12)),
-   VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 3, "G310v4",   "TVAx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(32768, 16384),
+   VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 3, "G310v4",   "G710", MODEL_ANISO(ALL),  MODEL_TB_SIZES(32768, 16384),
                                               MODEL_RATES_X(4, 8, 16,  48,  48, 12)),
-   VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 4, "G310v5",   "TVAx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(32768, 16384),
+   VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 4, "G310v5",   "G710", MODEL_ANISO(ALL),  MODEL_TB_SIZES(32768, 16384),
                                               MODEL_RATES_X(4, 8, 16,  64,  64, 16)),
 
-   FIFTHGEN_MODEL(PAN_PROD_ID(12, 8, 0), 4, "G720",  "TTIx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 32768),
+   FIFTHGEN_MODEL(PAN_PROD_ID(12, 8, 0), 4, "G720",  "G720", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 32768),
                                               MODEL_RATES_X(4, 8, 32,  64, 128, 8)),
-   FIFTHGEN_MODEL(PAN_PROD_ID(13, 8, 0), 4, "G725",  "TKRx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 65536),
+   FIFTHGEN_MODEL(PAN_PROD_ID(13, 8, 0), 4, "G725",  "G725", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 65536),
                                               MODEL_RATES_X(4, 8, 32, 128, 128, 8)),
-   FIFTHGEN_MODEL(PAN_PROD_ID(14, 8, 0), 4, "G1-Ultra",  "TDRx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 65536),
+   FIFTHGEN_MODEL(PAN_PROD_ID(14, 8, 0), 4, "G1-Ultra",  "G1", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 65536),
                                               MODEL_RATES(4, 8, 128)),
-   FIFTHGEN_MODEL(PAN_PROD_ID(14, 8, 1), 4, "G1-Premium",  "TDRx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 65536),
+   FIFTHGEN_MODEL(PAN_PROD_ID(14, 8, 1), 4, "G1-Premium",  "G1", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 65536),
                                               MODEL_RATES(4, 8, 128)),
-   FIFTHGEN_MODEL(PAN_PROD_ID(14, 8, 3), 1, "G1-Pro",  "TDRx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 65536),
+   FIFTHGEN_MODEL(PAN_PROD_ID(14, 8, 3), 1, "G1-Pro",  "G1", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 65536),
                                               MODEL_RATES(4, 4, 64)),
-   FIFTHGEN_MODEL(PAN_PROD_ID(14, 8, 3), 4, "G1-Pro",  "TDRx", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 65536),
+   FIFTHGEN_MODEL(PAN_PROD_ID(14, 8, 3), 4, "G1-Pro",  "G1", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 65536),
                                               MODEL_RATES(4, 8, 128)),
 };
 /* clang-format on */

commit 6015d5e538d531f230bb102e0af10eed566e6900

pan/perf: Fix 32bit build of panquick
Fixes: 5c15ef50 ("pan/perf: Transition to auto-generated derived counters")
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43040>

src/panfrost/perf/quick.c
@@ -4,6 +4,7 @@
  */
 
 #include <xf86drm.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <lib/kmod/pan_kmod.h>
 #include <lib/pan_props.h>
@@ -43,7 +44,7 @@ main(void)
         ctr++) {
       int64_t val = pan_perf_counter_read_sum(perf, ctr);
 
-      printf("%s: %ld\n", ctr->name, val);
+      printf("%s: %" PRId64 "\n", ctr->name, val);
    }
 
    if (pan_perf_disable(perf) < 0) {