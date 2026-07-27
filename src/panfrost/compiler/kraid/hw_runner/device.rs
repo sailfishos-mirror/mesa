sailfishos-mirror/mesa

commit 1d171a4174454045385986b52c872bac31052f8b

kraid: Add hw_runner
This is a very small driver that just sends compute jobs to the graphics
card without any of the Vulkan or OpenGL indirections.  For now it only
supports v10-v13 since it's what Kraid is targeting.  Lots of the
low-level code that handles CSF encoding and descriptor handling is in C
foir semplicity (and because there is no genxml equivalent for rust yet).

device.rs also implements a barebone memory-safe Rust abstraction for
mali GPUs, as a treat.

Signed-off-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42189>

src/panfrost/compiler/kraid/hw_runner/device.rs
@@ -1,0 +1,588 @@
+// Copyright © 2026 Collabora, Ltd.
+// SPDX-License-Identifier: MIT
+
+//! Abstraction layer on top of pan_kmod and libdrm.
+//!
+//! TODO docs
+
+use kraid_hw_runner_bindings::*;
+
+use std::ffi::CStr;
+use std::ops::Range;
+use std::os::fd::{FromRawFd, OwnedFd};
+use std::os::raw::c_void;
+use std::ptr::{NonNull, null, null_mut};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::time::Duration;
+use std::{fmt, io};
+
+const MAP_FAILED: *mut c_void = -1isize as *mut _;
+
+#[derive(Debug)]
+pub enum HwError {
+    OsError(io::Error),
+    DeviceLost {
+        fatal: bool,
+        timeout: bool,
+        innocent: bool,
+        fatal_queues: u32,
+    },
+}
+
+impl fmt::Display for HwError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            HwError::OsError(error) => error.fmt(f)?,
+            HwError::DeviceLost {
+                fatal,
+                timeout,
+                innocent,
+                fatal_queues,
+            } => {
+                write!(f, "Device lost: ")?;
+                if *fatal {
+                    write!(f, "fatal, ")?;
+                }
+                if *timeout {
+                    write!(f, "timeout, ")?;
+                }
+                if *innocent {
+                    write!(f, "innocent, ")?;
+                }
+                write!(f, "fatal queues: {}", *fatal_queues)?;
+            }
+        };
+        Ok(())
+    }
+}
+
+impl std::error::Error for HwError {}
+
+impl From<io::Error> for HwError {
+    fn from(value: io::Error) -> Self {
+        HwError::OsError(value)
+    }
+}
+
+pub struct Device {
+    dev: NonNull<pan_kmod_dev>,
+    debug: Option<NonNull<pandecode_context>>,
+}
+
+impl Device {
+    pub fn new(debug: bool) -> io::Result<Arc<Self>> {
+        unsafe {
+            let drm_fd = drmOpenWithType(
+                c"panthor".as_ptr(),
+                null(),
+                DRM_NODE_RENDER as i32,
+            );
+
+            if drm_fd < 0 {
+                return Err(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    "Failed to find a panthor device",
+                ));
+            }
+
+            let dev =
+                pan_kmod_dev_create(drm_fd, PAN_KMOD_DEV_FLAG_OWNS_FD, null());
+
+            let Some(dev) = NonNull::new(dev) else {
+                drop(OwnedFd::from_raw_fd(drm_fd));
+                return Err(io::Error::last_os_error());
+            };
+
+            let debug = debug.then(|| {
+                NonNull::new(pandecode_create_context(true))
+                    .expect("Failed to create debug context")
+            });
+
+            Ok(Arc::new(Device { dev, debug }))
+        }
+    }
+
+    unsafe fn io_ctl(&self, request: u64, arg: *mut c_void) -> i32 {
+        unsafe { drmIoctl(self.fd(), request, arg) }
+    }
+
+    pub fn user_va_range(&self) -> Range<u64> {
+        let res = unsafe { pan_kmod_dev_query_user_va_range(self.kdev()) };
+
+        res.start..(res.start + res.size)
+    }
+
+    pub fn props(&self) -> &pan_kmod_dev_props {
+        unsafe { &self.dev.as_ref().props }
+    }
+
+    pub fn fd(&self) -> i32 {
+        unsafe { self.dev.as_ref().fd }
+    }
+
+    pub fn kdev(&self) -> *mut pan_kmod_dev {
+        self.dev.as_ptr()
+    }
+}
+
+impl Drop for Device {
+    fn drop(&mut self) {
+        unsafe {
+            if let Some(ctx) = self.debug {
+                pandecode_destroy_context(ctx.as_ptr());
+            }
+            pan_kmod_dev_destroy(self.kdev());
+        }
+    }
+}
+
+pub struct VirtualMemory {
+    dev: Arc<Device>,
+    handle: NonNull<pan_kmod_vm>,
+    start: u64,
+    end: u64,
+    next_addr: AtomicU64,
+}
+
+impl VirtualMemory {
+    pub fn new(dev: Arc<Device>) -> io::Result<Self> {
+        let usable_range = dev.user_va_range();
+
+        let start = usable_range.start.max(32 * 1024 * 1024);
+        let end = usable_range.end.min(1 << 32);
+        let handle =
+            unsafe { pan_kmod_vm_create(dev.kdev(), 0, start, end - start) };
+
+        let Some(handle) = NonNull::new(handle) else {
+            return Err(io::Error::last_os_error());
+        };
+
+        let next_addr = AtomicU64::new(start.next_multiple_of(0x1000));
+
+        let mem = VirtualMemory {
+            dev,
+            handle,
+            start,
+            end,
+            next_addr,
+        };
+        Ok(mem)
+    }
+
+    pub fn allocate_buffer(
+        self: &Arc<VirtualMemory>,
+        size: u64,
+        label: &CStr,
+        device_flags: u32,
+    ) -> io::Result<MemoryBuffer> {
+        let size = size.next_multiple_of(0x1000);
+
+        let addr = self.next_addr.fetch_add(size, Ordering::Relaxed);
+        assert!(self.start <= addr && addr < self.end);
+
+        let mut buf = MemoryBuffer::new(self.clone(), size, device_flags)?;
+        buf.set_label(label);
+        buf.cpu_mmap((PROT_READ | PROT_WRITE) as i32, MAP_SHARED as i32)?;
+        buf.gpu_bind_immediate(addr)?;
+        buf.inject_debug(label);
+
+        Ok(buf)
+    }
+
+    fn kvm(&self) -> *mut pan_kmod_vm {
+        self.handle.as_ptr()
+    }
+
+    pub fn dev(&self) -> &Arc<Device> {
+        &self.dev
+    }
+
+    pub fn handle(&self) -> u32 {
+        unsafe { self.handle.as_ref().handle }
+    }
+}
+
+impl Drop for VirtualMemory {
+    fn drop(&mut self) {
+        unsafe {
+            pan_kmod_vm_destroy(self.kvm());
+        }
+    }
+}
+
+pub struct MemoryBuffer {
+    mem: Arc<VirtualMemory>,
+    handle: NonNull<pan_kmod_bo>,
+    device_addr: u64,
+    host_addr: *mut c_void,
+    debug_inj: bool,
+}
+
+impl MemoryBuffer {
+    pub fn new(
+        mem: Arc<VirtualMemory>,
+        size: u64,
+        flags: u32,
+    ) -> io::Result<Self> {
+        let handle = unsafe {
+            pan_kmod_bo_alloc(mem.dev.kdev(), mem.kvm(), size, flags)
+        };
+        let Some(handle) = NonNull::new(handle) else {
+            return Err(io::Error::last_os_error());
+        };
+
+        Ok(Self {
+            mem,
+            handle,
+            device_addr: 0,
+            host_addr: null_mut(),
+            debug_inj: false,
+        })
+    }
+
+    fn set_label(&mut self, label: &CStr) {
+        unsafe {
+            pan_kmod_set_bo_label(
+                self.mem.dev().kdev(),
+                self.kbo(),
+                label.as_ptr(),
+            );
+        }
+    }
+
+    fn cpu_mmap(&mut self, prot: i32, flags: i32) -> io::Result<()> {
+        assert!(self.host_addr.is_null(), "Already mapped");
+        let res =
+            unsafe { pan_kmod_bo_mmap(self.kbo(), prot, flags, null_mut()) };
+        if res == MAP_FAILED {
+            return Err(io::Error::new(
+                io::ErrorKind::OutOfMemory,
+                "Failed to mmap BO",
+            ));
+        }
+        self.host_addr = res;
+        Ok(())
+    }
+
+    fn gpu_bind_immediate(&mut self, addr: u64) -> io::Result<()> {
+        assert!(self.device_addr == 0, "Already bound");
+
+        let vm_ops = [pan_kmod_vm_op {
+            type_: PAN_KMOD_VM_OP_TYPE_MAP,
+            va: pan_kmod_vm_op__bindgen_ty_1 {
+                start: addr,
+                size: self.size(),
+            },
+            __bindgen_anon_1: pan_kmod_vm_op__bindgen_ty_2 {
+                map: pan_kmod_vm_op__bindgen_ty_2__bindgen_ty_1 {
+                    bo: self.kbo(),
+                    bo_offset: 0,
+                },
+            },
+            syncs: Default::default(),
+        }];
+
+        let res = unsafe {
+            pan_kmod_vm_bind(
+                self.mem.kvm(),
+                PAN_KMOD_VM_OP_MODE_IMMEDIATE,
+                &vm_ops as *const _ as *mut _,
+                vm_ops.len().try_into().unwrap(),
+            )
+        };
+
+        if res != 0 {
+            return Err(io::Error::new(
+                io::ErrorKind::OutOfMemory,
+                "Failed to bind BO",
+            ));
+        }
+        self.device_addr = addr;
+        Ok(())
+    }
+
+    fn inject_debug(&mut self, name: &CStr) {
+        assert!(self.host_addr != null_mut() && self.device_addr != 0);
+        if let Some(dctx) = &self.mem.dev().debug {
+            unsafe {
+                pandecode_inject_mmap(
+                    dctx.as_ptr(),
+                    self.device_addr,
+                    self.host_addr,
+                    self.size() as u32,
+                    name.as_ptr(),
+                );
+            }
+            self.debug_inj = true;
+        }
+    }
+
+    pub fn sync(&self) {
+        unsafe {
+            pan_kmod_queue_bo_map_sync(
+                self.kbo(),
+                0,
+                self.host_addr,
+                self.size(),
+                PAN_KMOD_BO_SYNC_CPU_CACHE_FLUSH_AND_INVALIDATE,
+            );
+            pan_kmod_flush_bo_map_syncs(self.mem.dev().kdev());
+        }
+    }
+
+    pub fn host_addr(&self) -> *mut c_void {
+        self.host_addr
+    }
+
+    pub fn device_addr(&self) -> u64 {
+        self.device_addr
+    }
+
+    pub fn size(&self) -> u64 {
+        unsafe { self.handle.as_ref().size }
+    }
+
+    fn kbo(&self) -> *mut pan_kmod_bo {
+        self.handle.as_ptr()
+    }
+}
+
+impl Drop for MemoryBuffer {
+    fn drop(&mut self) {
+        unsafe {
+            if self.debug_inj {
+                let dctx = &self.mem.dev().debug.unwrap();
+
+                pandecode_inject_free(
+                    dctx.as_ptr(),
+                    self.device_addr,
+                    self.size() as u32,
+                );
+            }
+            pan_kmod_bo_put(self.kbo());
+        }
+    }
+}
+
+fn drm_array<T>(arr: &[T]) -> drm_panthor_obj_array {
+    drm_panthor_obj_array {
+        stride: size_of::<T>().try_into().unwrap(),
+        count: arr.len().try_into().unwrap(),
+        array: arr.as_ptr() as u64,
+    }
+}
+
+/* Both a CSF Group and a submit queue actually */
+pub struct SubmitGroup {
+    vm: Arc<VirtualMemory>,
+    handle: u32,
+}
+
+impl SubmitGroup {
+    pub fn new(vm: &Arc<VirtualMemory>) -> io::Result<Self> {
+        let vm = vm.clone();
+        let shader_present = vm.dev.props().shader_present;
+
+        let queues = [drm_panthor_queue_create {
+            priority: 1,
+            ringbuf_size: 64 * 1024,
+            ..Default::default()
+        }];
+
+        let mut args = drm_panthor_group_create {
+            queues: drm_array(&queues),
+            max_compute_cores: shader_present.count_ones().try_into().unwrap(),
+            max_fragment_cores: 0,
+            max_tiler_cores: 0,
+            priority: PANTHOR_GROUP_PRIORITY_MEDIUM as u8,
+            compute_core_mask: shader_present,
+            fragment_core_mask: 0,
+            tiler_core_mask: 0,
+            vm_id: vm.handle(),
+            group_handle: 0,
+            ..Default::default()
+        };
+
+        let res = unsafe {
+            let io_args = &mut args as *mut _ as *mut c_void;
+            vm.dev()
+                .io_ctl(DRM_IOCTL_PANTHOR_GROUP_CREATE.into(), io_args)
+        };
+        assert_eq!(res, 0, "Failed to initialize Group");
+        let handle = args.group_handle;
+
+        Ok(SubmitGroup { vm, handle })
+    }
+
+    pub fn check_state(&self) -> Result<(), HwError> {
+        let mut args = drm_panthor_group_get_state {
+            group_handle: self.handle,
+            ..Default::default()
+        };
+        let res = unsafe {
+            let kargs = &mut args as *mut _ as *mut c_void;
+            self.vm
+                .dev()
+                .io_ctl(DRM_IOCTL_PANTHOR_GROUP_GET_STATE.into(), kargs)
+        };
+        if res == 0 && args.state == 0 {
+            return Ok(());
+        }
+        if res != 0 {
+            return Err(io::Error::last_os_error().into());
+        }
+        let fatal = (args.state & DRM_PANTHOR_GROUP_STATE_FATAL_FAULT) != 0;
+        let timeout = (args.state & DRM_PANTHOR_GROUP_STATE_TIMEDOUT) != 0;
+        let innocent = (args.state & DRM_PANTHOR_GROUP_STATE_INNOCENT) != 0;
+
+        Err(HwError::DeviceLost {
+            fatal,
+            timeout,
+            innocent,
+            fatal_queues: args.fatal_queues,
+        })
+    }
+
+    pub fn submit(
+        &self,
+        stream_addr: u64,
+        stream_size: u32,
+        syncs: &[drm_panthor_sync_op],
+    ) -> io::Result<()> {
+        let submits = [drm_panthor_queue_submit {
+            queue_index: 0,
+            stream_addr,
+            stream_size,
+            latest_flush: 0,
+            syncs: drm_array(syncs),
+            ..Default::default()
+        }];
+
+        let mut args = drm_panthor_group_submit {
+            group_handle: self.handle,
+            queue_submits: drm_array(&submits),
+            ..Default::default()
+        };
+        if let Some(dctx) = self.vm.dev.debug {
+            unsafe {
+                eprintln!("\nCS binary:\n");
+                pandecode_cs_binary(
+                    dctx.as_ptr(),
+                    stream_addr,
+                    stream_size,
+                    self.vm.dev.props().gpu_id,
+                );
+            }
+        }
+        let res = unsafe {
+            let kargs = &mut args as *mut _ as *mut c_void;
+            self.vm
+                .dev()
+                .io_ctl(DRM_IOCTL_PANTHOR_GROUP_SUBMIT.into(), kargs)
+        };
+        if res != 0 {
+            return Err(io::Error::last_os_error());
+        }
+        Ok(())
+    }
+}
+
+impl Drop for SubmitGroup {
+    fn drop(&mut self) {
+        let mut args = drm_panthor_group_destroy {
+            group_handle: self.handle,
+            pad: 0,
+        };
+        unsafe {
+            let kargs = &mut args as *mut _ as *mut c_void;
+            let res = self
+                .vm
+                .dev()
+                .io_ctl(DRM_IOCTL_PANTHOR_GROUP_DESTROY.into(), kargs);
+            assert_eq!(res, 0, "Failed to destroy Group");
+        }
+    }
+}
+
+pub struct TimelineSyncobj {
+    dev: Arc<Device>,
+    handle: u32,
+    value: AtomicU64,
+}
+
+impl TimelineSyncobj {
+    pub fn new(dev: Arc<Device>) -> io::Result<Self> {
+        let mut handle = 0u32;
+        let res = unsafe {
+            drmSyncobjCreate(dev.fd(), DRM_SYNCOBJ_CREATE_SIGNALED, &mut handle)
+        };
+        if res != 0 {
+            return Err(io::Error::last_os_error());
+        };
+        Ok(TimelineSyncobj {
+            dev,
+            handle,
+            value: AtomicU64::new(0),
+        })
+    }
+
+    pub fn signal(&self) -> TimelineMoment<'_> {
+        TimelineMoment {
+            timeline: self,
+            value: self.value.fetch_add(1, Ordering::SeqCst) + 1,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct TimelineMoment<'a> {
+    timeline: &'a TimelineSyncobj,
+    value: u64,
+}
+
+impl TimelineMoment<'_> {
+    pub fn wait(&self, timeout: Duration) -> io::Result<()> {
+        let res = unsafe {
+            drmSyncobjTimelineWait(
+                self.timeline.dev.fd(),
+                &self.timeline.handle as *const _ as *mut _,
+                &self.value as *const _ as *mut _,
+                1,
+                timeout.as_nanos().min(i64::MAX as u128) as i64,
+                DRM_SYNCOBJ_WAIT_FLAGS_WAIT_ALL,
+                null_mut(),
+            )
+        };
+        if res == -(ETIME as i32) {
+            return Err(io::Error::new(
+                io::ErrorKind::TimedOut,
+                "Timeline syncobj timed out",
+            ));
+        }
+        if res < 0 {
+            return Err(io::Error::last_os_error());
+        };
+        Ok(())
+    }
+}
+
+impl From<TimelineMoment<'_>> for drm_panthor_sync_op {
+    fn from(value: TimelineMoment) -> Self {
+        let flags = (DRM_PANTHOR_SYNC_OP_HANDLE_TYPE_TIMELINE_SYNCOBJ
+            | DRM_PANTHOR_SYNC_OP_SIGNAL) as u32;
+
+        drm_panthor_sync_op {
+            flags,
+            handle: value.timeline.handle,
+            timeline_value: value.value,
+        }
+    }
+}
+
+impl Drop for TimelineSyncobj {
+    fn drop(&mut self) {
+        let res = unsafe { drmSyncobjDestroy(self.dev.fd(), self.handle) };
+        assert_eq!(res, 0, "Failed to destroy syncobj")
+    }
+}

src/panfrost/compiler/kraid/hw_runner/meson.build
@@ -1,0 +1,103 @@
+# Copyright © 2026 Collabora, Ltd.
+# SPDX-License-Identifier: MIT
+
+
+_hw_runner_bindings_rs = rust.bindgen(
+  input : 'hw_runner_bindings.h',
+  output : 'kraid_hw_runner_bindings.rs',
+  output_inline_wrapper : 'hw_runner_bindings.c',
+  include_directories : [inc_include, inc_src, inc_panfrost],
+  c_args : [pre_args],
+  args : [
+    bindgen_output_args,
+    compiler_rs_bindgen_blocklist,
+    '--raw-line', 'use compiler::bindings::*;',
+    '--allowlist-type', 'pan_kmod_.*',
+    '--allowlist-type', 'pandecode_.*',
+    '--allowlist-type', 'drm.*',
+    '--allowlist-type', '.*',
+    '--allowlist-function', 'pan_arch',
+    '--allowlist-function', 'pan_kmod_.*',
+    '--allowlist-function', 'pandecode_.*',
+    '--allowlist-function', 'hw_runner_.*',
+    '--allowlist-function', 'drm.*',
+    '--allowlist-var', 'DRM_.*',
+    '--allowlist-var', 'ETIME',
+    '--allowlist-var', 'PROT_.*',
+    '--allowlist-var', 'MAP_SHARED',
+    '--no-prepend-enum-name',
+    '--with-derive-default',
+    '--wrap-static-fns-suffix', '_kraid_hw_runner_extern'
+  ],
+  dependencies : [
+    dep_libdrm,
+    idep_mesautil,
+  ],
+)
+
+_libkraid_hw_runner_bindings_rs = static_library(
+  'kraid_hw_runner_bindings',
+  _hw_runner_bindings_rs[0],
+  gnu_symbol_visibility : 'hidden',
+  rust_abi : 'rust',
+  dependencies : [idep_compiler_rs],
+)
+
+_hw_runner_per_arch_libs = []
+foreach ver : ['10', '12', '13']
+  _hw_runner_per_arch_libs += static_library(
+    'kraid_hw_runner_c_v' + ver,
+    files('hw_runner_vX.c'),
+    include_directories : [inc_include, inc_src, inc_panfrost],
+    c_args : [
+      no_override_init_args,
+      cc.get_supported_arguments('-Wno-missing-prototypes'),
+      '-DPAN_ARCH=' + ver,
+    ],
+    gnu_symbol_visibility : 'hidden',
+    dependencies : [dep_libdrm, idep_mesautil, idep_pan_packers],
+    build_by_default : false,
+  )
+endforeach
+
+_libkraid_hw_runner_c = static_library(
+  'kraid_hw_runner_c',
+  files('hw_runner.c') + _hw_runner_bindings_rs[1],
+  include_directories : [
+    inc_include, inc_src, inc_panfrost,
+    fs.relative_to(meson.project_build_root(), meson.current_source_dir()),
+  ],
+  c_args : [no_override_init_args, cc.get_supported_arguments('-Wno-missing-prototypes'),],
+  dependencies : [dep_libdrm, idep_mesautil, idep_pan_packers],
+  link_with : [
+    libpankmod_lib,
+    libpanfrost_lib,
+    libpanfrost_decode,
+    _hw_runner_per_arch_libs,
+  ],
+  build_by_default : false,
+)
+
+libkraid_hw_runner_rs = static_library(
+  'kraid_hw_runner',
+  files('lib.rs'),
+  gnu_symbol_visibility : 'hidden',
+  rust_abi : 'rust',
+  rust_args : [
+    panfrost_rust_args,
+    # Otherwise, rustc trips up on -pthread
+    '-Clink-arg=-Wno-unused-command-line-argument',
+  ],
+  dependencies : [
+    idep_compiler_rs,
+  ],
+  link_with : [
+    _libkraid_hw_runner_bindings_rs,
+    _libkraid_hw_runner_c,
+  ],
+  build_by_default : false,
+)
+
+idep_kraid_hw_runner_rs = declare_dependency(
+  link_with : [libkraid_hw_runner_rs],
+)

src/panfrost/compiler/kraid/meson.build
@@ -142,6 +142,8 @@ endif
 # with the sanitizers because meson doesn't know to pass -fsanitize to the
 # Rust linker.  See also https://github.com/mesonbuild/meson/issues/11741
 if with_tests and get_option('b_sanitize') == 'none'
+  subdir('hw_runner')
+
   rust.test(
     'kraid_test',
     libpanfrost_kraid,
@@ -157,5 +159,6 @@ if with_tests and get_option('b_sanitize') == 'none'
       # Meson adds --test but no --cfg test, do it manually to help rust-analyzer
       '--cfg', 'test',
     ],
+    dependencies : [idep_acorn_rs, idep_kraid_hw_runner_rs],
   )
 endif

src/panfrost/compiler/kraid/hw_runner/hw_runner.c
@@ -1,0 +1,23 @@
+/*
+ * Copyright © 2026 Collabora, Ltd.
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "hw_runner_priv.h"
+#include "pan_model.h"
+#include "kmod/pan_kmod.h"
+#include "util/macros.h"
+
+void
+hw_runner_new_cmd_stream(struct pan_kmod_dev *kdev,
+                         struct hw_runner_invocation_info *info,
+                         struct hw_runner_layout_info *out)
+{
+   unsigned arch = pan_arch(kdev->props.gpu_id);
+   switch (arch) {
+   case 10: hw_runner_new_cmd_stream_v10(kdev, info, out); break;
+   case 12: hw_runner_new_cmd_stream_v12(kdev, info, out); break;
+   case 13: hw_runner_new_cmd_stream_v13(kdev, info, out); break;
+   default: UNREACHABLE("Unsupported architecture");
+   }
+}

src/panfrost/compiler/kraid/hw_runner/hw_runner.h
@@ -1,0 +1,59 @@
+/*
+ * Copyright © 2026 Collabora, Ltd.
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef HW_RUNNER_H
+#define HW_RUNNER_H
+
+#include <stdint.h>
+#include "kmod/pan_kmod.h"
+
+/* Arguments passed in FAU to the compute shader (aka CB0) */
+struct hw_runner_shader_args {
+   uint64_t data_addr;
+   uint32_t data_stride;
+};
+
+/* All data required to run a compute shader, every pointer is a read-only
+ * pointer in CPU-space, except for data_ptr that will be uploaded to the GPU
+ * for execution and copied back when the shader has run (read-write).
+ */
+struct hw_runner_invocation_info {
+   /* These BO pointers might be null, if they are the function will exit early
+    * and only write the layout information so that the caller can allocate
+    * buffer objects and call back.
+    */
+   void *descr_bo_host_ptr;
+   uint64_t descr_bo_device_ptr;
+
+   void *data_bo_host_ptr;
+   uint64_t data_bo_device_ptr;
+
+   void *code_ptr;
+   uint64_t code_size_B;
+
+   void *fau_ptr;
+   uint64_t fau_size_B;
+   /* Offset of hw_runner_shader_args in fau */
+   uint64_t args_fau_offset;
+
+   void *data_ptr;
+   uint64_t data_size_B;
+   uint32_t data_stride_B;
+
+   uint16_t register_count;
+   uint16_t invocations;
+};
+
+struct hw_runner_layout_info {
+   uint64_t descr_bo_size_B;
+   uint64_t cs_offset;
+   uint64_t cs_size_B;
+};
+
+void hw_runner_new_cmd_stream(struct pan_kmod_dev *kdev,
+                              struct hw_runner_invocation_info *info,
+                              struct hw_runner_layout_info *out);
+
+# endif /* HW_RUNNER_H */

src/panfrost/compiler/kraid/hw_runner/hw_runner_bindings.h
@@ -1,0 +1,16 @@
+/*
+ * Copyright © 2026 Collabora, Ltd.
+ * SPDX-License-Identifier: MIT
+ */
+
+/*
+ * Bindgen input header: exposes C APIs to Rust.
+ */
+
+#include "kmod/pan_kmod.h"
+#include "hw_runner.h"
+#include "pan_props.h"
+#include "genxml/decode.h"
+#include <xf86drm.h>
+#include "drm-uapi/panthor_drm.h"
+#include <sys/mman.h>

src/panfrost/compiler/kraid/hw_runner/hw_runner_priv.h
@@ -1,0 +1,20 @@
+
+
+#ifndef HW_RUNNER_PRIV_H
+#define HW_RUNNER_PRIV_H
+
+#include "hw_runner.h"
+#include "kmod/pan_kmod.h"
+#include <stdint.h>
+#include <stdatomic.h>
+
+#define HW_RUNNER_ARCH_DECLS(ver)                                              \
+   void hw_runner_new_cmd_stream_v##ver(struct pan_kmod_dev *kdev,             \
+                                        struct hw_runner_invocation_info *info,\
+                                        struct hw_runner_layout_info *out);
+
+HW_RUNNER_ARCH_DECLS(10);
+HW_RUNNER_ARCH_DECLS(12);
+HW_RUNNER_ARCH_DECLS(13);
+
+#endif /* HW_RUNNER_PRIV_H */

commit 6cc038ecd1fdf6b654d7b976752a68bc60ba7b50

pan/kraid: Fix 32bit hw_runner builds
On armhf, unsigned long is uint32_t, so passing a u64 into drmIoctl
doesnt work.
Also don't pull in everything into bindgen.

Fixes: 1d171a41 ("kraid: Add hw_runner")
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43040>

src/panfrost/compiler/kraid/hw_runner/device.rs
@@ -10,7 +10,7 @@ use kraid_hw_runner_bindings::*;
 use std::ffi::CStr;
 use std::ops::Range;
 use std::os::fd::{FromRawFd, OwnedFd};
-use std::os::raw::c_void;
+use std::os::raw::{c_ulong, c_void};
 use std::ptr::{NonNull, null, null_mut};
 use std::sync::Arc;
 use std::sync::atomic::{AtomicU64, Ordering};
@@ -103,7 +103,7 @@ impl Device {
         }
     }
 
-    unsafe fn io_ctl(&self, request: u64, arg: *mut c_void) -> i32 {
+    unsafe fn io_ctl(&self, request: c_ulong, arg: *mut c_void) -> i32 {
         unsafe { drmIoctl(self.fd(), request, arg) }
     }
 

src/panfrost/compiler/kraid/hw_runner/meson.build
@@ -15,7 +15,7 @@ _hw_runner_bindings_rs = rust.bindgen(
     '--allowlist-type', 'pan_kmod_.*',
     '--allowlist-type', 'pandecode_.*',
     '--allowlist-type', 'drm.*',
-    '--allowlist-type', '.*',
+    '--allowlist-type', 'hw_runner_.*',
     '--allowlist-function', 'pan_arch',
     '--allowlist-function', 'munmap',
     '--allowlist-function', 'pan_kmod_.*',