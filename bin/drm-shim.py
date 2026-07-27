ad4dcba94148faca1ca9db395713b5d477ac713f
Christian Gmeiner <cgmeiner@igalia.com>
2026-07-27 15:08:59 +0200

    drm-shim: Add etnaviv
    Model and revision is what src/etnaviv/ci names its files after and
    what the hardware reports.
    
    Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
    Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
    Reviewed-by: Daniel Lang <dalang@gmx.at>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43258>

--- bin/drm-shim.py
+++ bin/drm-shim.py
@@ -11,6 +11,7 @@ import subprocess
 ALIASES = {
     "amd": "gfx1201",
     "asahi": "m1",
+    "etnaviv": "gc2000-r5108",
     "freedreno": "a750",
     "intel": "lnl",
     "lima": "lima-mali450",
@@ -22,6 +23,9 @@ ALIASES = {
     "a5xx": "a530",
     "a6xx": "a660",
     "a7xx": "a750",
+
+    "gc2000": "gc2000-r5108",
+    "gc3000": "gc3000-r5450",
 
     "lima-mali450": "mali450",
     "panfrost-t860": "t860",
@@ -43,6 +47,11 @@ TARGETS = {
     "gfx1201": ["amd", "GFX1201"],
 
     "m1": ["asahi", None],
+
+    "gc2000-r5108": ["etnaviv", "2000:5108"],
+    "gc3000-r5450": ["etnaviv", "3000:5450"],
+    "gc7000-r6204": ["etnaviv", "7000:6204:70003:11"],
+    "gc7000-r6214": ["etnaviv", "7000:6214:70002:30"],
 
     "a200": ["freedreno", "200"],
     "a201": ["freedreno", "201"],
@@ -107,6 +116,7 @@ TARGETS = {
 LD_PRELOAD = {
     "amd": "src/amd/drm-shim/libamdgpu_noop_drm_shim.so",
     "asahi": "src/asahi/drm-shim/libasahi_noop_drm_shim.so",
+    "etnaviv": "src/etnaviv/drm-shim/libetnaviv_noop_drm_shim.so",
     "freedreno": "src/freedreno/drm-shim/libfreedreno_noop_drm_shim.so",
     "intel": "src/intel/tools/libintel_noop_drm_shim.so",
     "nouveau": "src/nouveau/drm-shim/libnouveau_noop_drm_shim.so",
@@ -117,6 +127,7 @@ LD_PRELOAD = {
 
 SHIM_VAR = {
     "amd": "AMDGPU_GPU_ID",
+    "etnaviv": "ETNA_SHIM_GPU",
     "freedreno": "FD_GPU_ID",
     "intel": "INTEL_STUB_GPU_PLATFORM",
     "nouveau": "NOUVEAU_CHIPSET",
@@ -127,6 +138,7 @@ SHIM_VAR = {
 DISASM = {
     "amd": [("AMD_DEBUG", "vs,ps,gs,tcs,tes,cs,ts,ms,nir,aco,asm"), ("RADV_DEBUG", "shaders")],
     "asahi": [("AGX_MESA_DEBUG", "shaders")],
+    "etnaviv": [("ETNA_MESA_DEBUG", "dump_shaders")],
     "freedreno": [("IR3_SHADER_DEBUG", "disasm")],
     "intel": [("INTEL_DEBUG", "vs,tcs,tes,gs,fs,cs,mesh,task,rt")],
     "nouveau": [("NAK_DEBUG", "print")],
