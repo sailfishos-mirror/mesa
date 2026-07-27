bminor/mesa-mesa

commit 7ee9835475c8209745f953806f31dae990e31cff

tu/lrz: Disable LRZ when CmdSetRenderingAttachmentLocations is used
Same case as a drawcall not writing to some color attachments, but not
trying to make LRZ work in cases where we can prove that LRZ can work.

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38034>

src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -6148,6 +6148,12 @@ tu_CmdSetRenderingAttachmentLocationsKHR(
    tu6_emit_mrt<CHIP>(cmd, cmd->state.subpass, &cmd->draw_cs);
    tu6_emit_render_cntl<CHIP>(cmd, cmd->state.subpass, &cmd->draw_cs, false);
 
+   /* Same case as a drawcall not writing to some color attachments, but not
+    * trying to make LRZ work in cases where we can prove that LRZ can work.
+    */
+   if (cmd->state.lrz.valid)
+      tu_lrz_disable_write_for_rp(cmd, "CmdSetRenderingAttachmentLocations");
+
    /* Because this is just a remapping and not a different "reference", there
     * doesn't need to be a barrier between accesses to the same attachment
     * with a different index. This is different from "classic" renderpasses.

src/freedreno/vulkan/tu_lrz.cc
@@ -91,7 +91,7 @@ tu_lrz_disable_reason(struct tu_cmd_buffer *cmd, const char *reason) {
               cmd->state.rp.lrz_disabled_at_draw);
 }
 
-static inline void
+void
 tu_lrz_disable_write_for_rp(struct tu_cmd_buffer *cmd, const char *reason)
 {
    if (cmd->state.lrz.disable_write_for_rp)

src/freedreno/vulkan/tu_lrz.h
@@ -112,4 +112,7 @@ void
 tu_lrz_flush_valid_during_renderpass(struct tu_cmd_buffer *cmd,
                                      struct tu_cs *cs);
 
+void
+tu_lrz_disable_write_for_rp(struct tu_cmd_buffer *cmd, const char *reason);
+
 #endif /* TU_LRZ_H */

sailfishos-mirror/mesa

commit 3c0803f5a17f13bad5c2f2da7cdeaee4f792eeb2

tu: Dirty LRZ after changing attachment locations disable LRZ writes
We called tu_lrz_disable_write_for_rp, but didn't trigger LRZ
state reemission.

Fixes: 7ee9835475c ("tu/lrz: Disable LRZ when CmdSetRenderingAttachmentLocations is used")
Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43239>

src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -7312,8 +7312,10 @@ tu_CmdSetRenderingAttachmentLocationsKHR(
    /* Same case as a drawcall not writing to some color attachments, but not
     * trying to make LRZ work in cases where we can prove that LRZ can work.
     */
-   if (cmd->state.lrz.valid)
+   if (cmd->state.lrz.valid && !cmd->state.lrz.disable_write_for_rp) {
       tu_lrz_disable_write_for_rp(cmd, "CmdSetRenderingAttachmentLocations");
+      cmd->state.dirty |= TU_CMD_DIRTY_LRZ;
+   }
 
    /* Because this is just a remapping and not a different "reference", there
     * doesn't need to be a barrier between accesses to the same attachment