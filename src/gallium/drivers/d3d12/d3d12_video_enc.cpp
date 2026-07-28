sailfishos-mirror/mesa

commit 42f636bee95409f8ac89a453ab0edcbe58530180

d3d12/video: Don't reset batches in fence_wait
va frontend unlocks driver mutex while waiting on fences, so there
is a race accessing codec when trying to reset batches.

Cc: mesa-stable
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15886
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43005>

src/gallium/drivers/d3d12/d3d12_video_enc.cpp
@@ -5288,11 +5288,6 @@ d3d12_video_encoder_fence_wait(struct pipe_video_codec *codec,
    assert(fence);
 
    bool wait_res = d3d12_fence_finish(fence, timeout);
-   if (wait_res) {
-      // Opportunistically reset batches
-      for (uint32_t i = 0; i < pD3D12Enc->m_MaxQueueAsyncDepth; ++i)
-         d3d12_video_encoder_sync_completion(codec, i, 0);
-   }
 
    // Return semantics based on p_video_codec interface
    // ret == 0 -> Encode in progress

src/gallium/drivers/d3d12/d3d12_video_dec.cpp
@@ -759,11 +759,6 @@ d3d12_video_decoder_fence_wait(struct pipe_video_codec *codec, struct pipe_fence
    assert(fenceValueToWaitOn);
 
    bool wait_res = d3d12_fence_finish(fenceValueToWaitOn, timeout);
-   if (wait_res) {
-      // Opportunistically reset batches
-      for (uint32_t i = 0; i < D3D12_VIDEO_DEC_ASYNC_DEPTH; ++i)
-         (void)d3d12_video_decoder_sync_completion(codec, i, 0);
-   }
 
    // Return semantics based on p_video_codec interface
    // ret == 0 -> Decode in progress

commit a25e41df17602b3c3962805be6fe748ad226abbc

d3d12: fix msvc build warning C4819
Fixes: 42f636bee95 ("d3d12/video: Don't reset batches in fence_wait")

Reviewed-by: Sil Vilerino <sivileri@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43199>

src/gallium/drivers/d3d12/d3d12_video_enc.cpp
@@ -5278,12 +5278,10 @@ d3d12_video_encoder_update_picparams_region_of_interest_qpmap(struct d3d12_video
 }
 
 int
-d3d12_video_encoder_fence_wait(struct pipe_video_codec *codec,
+d3d12_video_encoder_fence_wait([[maybe_unused]] struct pipe_video_codec *codec,
                                struct pipe_fence_handle *_fence,
                                uint64_t timeout)
 {
-   struct d3d12_video_encoder *pD3D12Enc = (struct d3d12_video_encoder *) codec;
-   assert(pD3D12Enc);
    struct d3d12_fence *fence = (struct d3d12_fence *) _fence;
    assert(fence);
 