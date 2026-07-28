sailfishos-mirror/mesa

commit 850768bacbf7ef1c9f45bba58f3979a98d4a8655

nir/opt_vectorize: prefer to swizzle vector phis at the destination, not the source
Phi sources can't be coalesced if they have swizzles, while users of the phi likely
support swizzles just fine.

This prevents instruction count regressions from enabling phi vectorization for aco.

Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42526>

src/compiler/nir/nir_opt_vectorize.c
@@ -33,6 +33,7 @@
  */
 
 #include "util/u_dynarray.h"
+#include "util/u_qsort.h"
 #include "nir.h"
 #include "nir_builder.h"
 #include "nir_vla.h"
@@ -389,6 +390,43 @@ rewrite_uses(nir_builder *b, struct set *instr_set, nir_def *def1,
    nir_instr_remove(nir_def_instr(def2));
 }
 
+static nir_block *
+get_first_non_const_phi_pred(nir_phi_instr *phi)
+{
+   nir_block *result = NULL;
+   nir_foreach_phi_src(src, phi) {
+      /* Loop back edge source might not be vectors. */
+      if (phi->instr.block->index < src->pred->index)
+         continue;
+
+      /* Find the first predecessor in source order. */
+      if (result && src->pred->index > result->index)
+         continue;
+
+      /* Ignore constant phi sources. */
+      if (nir_scalar_is_const(nir_scalar_resolved(src->src.ssa, 0)))
+         continue;
+
+      result = src->pred;
+   }
+
+   return result;
+}
+
+static int
+compare_scalar(const void *p1, const void *p2, void *data)
+{
+   uint8_t idx1 = *(const uint8_t *)p1;
+   uint8_t idx2 = *(const uint8_t *)p2;
+
+   const nir_scalar *scalars = data;
+
+   if (scalars[idx1].comp == scalars[idx2].comp)
+      return idx1 - idx2;
+
+   return scalars[idx1].comp - scalars[idx2].comp;
+}
+
 static nir_instr *
 instr_try_combine_phi(struct set *instr_set, nir_phi_instr *phi1, nir_phi_instr *phi2)
 {
@@ -413,20 +451,38 @@ instr_try_combine_phi(struct set *instr_set, nir_phi_instr *phi1, nir_phi_instr 
 
    assert(exec_list_length(&phi1->srcs) == exec_list_length(&phi2->srcs));
 
+   uint8_t dest_swizzle[NIR_MAX_VEC_COMPONENTS] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+   nir_block *swizzle_pred = get_first_non_const_phi_pred(phi1);
+   /* Try to avoid reverse swizzles at the new phi sources. */
+   if (swizzle_pred) {
+      nir_phi_src *src1 = nir_phi_get_src_from_block(phi1, swizzle_pred);
+      nir_phi_src *src2 = nir_phi_get_src_from_block(phi2, swizzle_pred);
+
+      nir_scalar new_srcs[NIR_MAX_VEC_COMPONENTS];
+
+      for (unsigned i = 0; i < phi1_components; i++)
+         new_srcs[i] = nir_scalar_resolved(src1->src.ssa, i);
+
+      for (unsigned i = 0; i < phi2_components; i++)
+         new_srcs[phi1_components + i] = nir_scalar_resolved(src2->src.ssa, i);
+
+      util_qsort_r(dest_swizzle, total_components, sizeof(dest_swizzle[0]),
+                   compare_scalar, new_srcs);
+   }
+
    nir_foreach_phi_src(src1, phi1) {
       nir_phi_src *src2 = nir_phi_get_src_from_block(phi2, src1->pred);
       nir_block *pred_block = src1->pred;
 
       nir_scalar new_srcs[NIR_MAX_VEC_COMPONENTS];
 
-      for (unsigned i = 0; i < phi1_components; i++) {
-         nir_scalar s = nir_get_scalar(src1->src.ssa, i);
-         new_srcs[i] = nir_scalar_chase_movs(s);
-      }
-
-      for (unsigned i = 0; i < phi2_components; i++) {
-         nir_scalar s = nir_get_scalar(src2->src.ssa, i);
-         new_srcs[phi1_components + i] = nir_scalar_chase_movs(s);
+      for (unsigned i = 0; i < total_components; i++) {
+         unsigned comp = dest_swizzle[i];
+         if (comp < phi1_components)
+            new_srcs[i] = nir_scalar_resolved(src1->src.ssa, comp);
+         else
+            new_srcs[i] = nir_scalar_resolved(src2->src.ssa, comp - phi1_components);
       }
 
       nir_def *new_src;
@@ -444,15 +500,15 @@ instr_try_combine_phi(struct set *instr_set, nir_phi_instr *phi1, nir_phi_instr 
          new_src = nir_build_imm(&b, total_components, bit_size, value);
       } else if (pred_block->index < block->index) {
          nir_def *def = new_srcs[0].def;
-         unsigned swizzle[NIR_MAX_VEC_COMPONENTS];
+         unsigned src_swizzle[NIR_MAX_VEC_COMPONENTS];
 
          for (unsigned i = 0; i < total_components; i++) {
             assert(new_srcs[i].def == def);
-            swizzle[i] = new_srcs[i].comp;
+            src_swizzle[i] = new_srcs[i].comp;
          }
 
          b.cursor = nir_after_instr_and_phis(nir_def_instr(def));
-         new_src = nir_swizzle(&b, def, swizzle, total_components);
+         new_src = nir_swizzle(&b, def, src_swizzle, total_components);
       } else {
          /* This is a loop back-edge so we haven't vectorized the sources yet.
           * Combine them in a vec which, if they are vectorized later, will be
@@ -468,7 +524,20 @@ instr_try_combine_phi(struct set *instr_set, nir_phi_instr *phi1, nir_phi_instr 
    }
 
    b.cursor = nir_after_phis(block);
-   rewrite_uses(&b, instr_set, &phi1->def, &phi2->def, &new_phi->def);
+
+   nir_alu_src replace_phi1 = { nir_src_for_ssa(&new_phi->def) };
+   nir_alu_src replace_phi2 = { nir_src_for_ssa(&new_phi->def) };
+
+   for (unsigned i = 0; i < total_components; i++) {
+      unsigned comp = dest_swizzle[i];
+      if (comp < phi1_components)
+         replace_phi1.swizzle[comp] = i;
+      else
+         replace_phi2.swizzle[comp - phi1_components] = i;
+   }
+
+   nir_def_rewrite_uses_with_alu_src(&b, &phi1->def, replace_phi1);
+   nir_def_rewrite_uses_with_alu_src(&b, &phi2->def, replace_phi2);
 
    return &new_phi->instr;
 }

commit 829e6d16301d3daf83743f4edc777567cf76d8f4

nir/opt_vectorize: Remove the phis that were combined
Combining two phis rewrote their uses but left the phis themselves in
the block. They still hash equal and still dominate, so the next run of
the pass combined them again, reported progress again and added another
dead phi.

The ALU path already drops its inputs, in rewrite_uses().

Fixes: 850768bacbf ("nir/opt_vectorize: prefer to swizzle vector phis at the destination, not the source")
Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43231>

src/compiler/nir/nir_opt_vectorize.c
@@ -565,6 +565,9 @@ instr_try_combine_phi(struct set *instr_set, nir_phi_instr *phi1, nir_phi_instr 
    nir_def_rewrite_uses_with_alu_src(&b, &phi1->def, replace_phi1);
    nir_def_rewrite_uses_with_alu_src(&b, &phi2->def, replace_phi2);
 
+   nir_instr_remove(&phi1->instr);
+   nir_instr_remove(&phi2->instr);
+
    return &new_phi->instr;
 }
 

src/compiler/nir/tests/opt_vectorize_tests.cpp
@@ -175,4 +175,42 @@ TEST_F(nir_opt_vectorize_test, iadd3_swapped_last_two_sources_do_not_vectorize)
    )"));
 }
 
+TEST_F(nir_opt_vectorize_test, combining_loop_phis_converges)
+{
+   nir_variable *out = nir_variable_create(b->shader, nir_var_shader_out,
+                                           glsl_uint_type(), "out");
+   nir_variable *sum = nir_local_variable_create(b->impl, glsl_uint_type(), "sum");
+   nir_variable *idx = nir_local_variable_create(b->impl, glsl_uint_type(), "idx");
+
+   nir_store_var(b, sum, nir_imm_int(b, 0), 0x1);
+   nir_store_var(b, idx, nir_imm_int(b, 0), 0x1);
+
+   /* Two scalar values carried around a loop become two scalar phis in the
+    * loop header, which the pass combines into one 2-component phi.
+    */
+   nir_loop *loop = nir_push_loop(b);
+   {
+      nir_def *cur_idx = nir_load_var(b, idx);
+      nir_def *cur_sum = nir_load_var(b, sum);
+
+      nir_push_if(b, nir_uge_imm(b, cur_idx, 4));
+      nir_jump(b, nir_jump_break);
+      nir_pop_if(b, NULL);
+
+      nir_store_var(b, sum, nir_iadd(b, cur_sum, cur_idx), 0x1);
+      nir_store_var(b, idx, nir_iadd_imm(b, cur_idx, 1), 0x1);
+   }
+   nir_pop_loop(b, loop);
+
+   nir_store_var(b, out, nir_load_var(b, sum), 0x1);
+
+   NIR_PASS(_, b->shader, nir_lower_vars_to_ssa);
+
+   ASSERT_TRUE(nir_opt_vectorize(b->shader, NULL, NULL));
+   nir_validate_shader(b->shader, NULL);
+
+   ASSERT_FALSE(nir_opt_vectorize(b->shader, NULL, NULL));
+   nir_validate_shader(b->shader, NULL);
+}
+
 } // namespace