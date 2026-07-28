sailfishos-mirror/mesa

commit 23f8fde15a8aae3968bf262b18614ab108e0b2e2

kraid: Add OpFLogD and wire it up with tests
Signed-off-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42633>

src/panfrost/compiler/kraid/builder.rs
@@ -214,6 +214,45 @@ pub trait SSABuilder: Builder + AllocSSA {
             expf: arg,
         });
     }
+
+    // Computes log2(x)
+    fn flog2_32_to(&mut self, dst: Dst, arg: Src) {
+        let frexp = self.alloc_ssa(32);
+        self.push_op(OpFrexpE {
+            dst: frexp.into(),
+            src: arg.clone(),
+            mode: FrexpMode::Log,
+            neg_result: false,
+        });
+        let frexpi = self.alloc_ssa(32);
+        self.push_op(OpIToF32 {
+            dst: frexpi.into(),
+            src_type: DataType::S32,
+            src: frexp.into(),
+            round: FRound::NearestEven,
+        });
+
+        let flogd = self.alloc_ssa(32);
+        self.push_op(OpFLogD {
+            dst: flogd.into(),
+            src: arg.clone(),
+        });
+        let lscale = self.alloc_ssa(32);
+        self.push_op(OpFAddLScale {
+            dst: lscale.into(),
+            round: FRound::NearestEven,
+            clamp: FClamp::None,
+            srcs: [Src::from(-1.0), arg],
+        });
+
+        self.fma_32_to(dst, flogd.into(), lscale.into(), frexpi.into());
+    }
+
+    fn flog2_32(&mut self, src: Src) -> SSAValue {
+        let def = self.alloc_ssa(32);
+        self.flog2_32_to(def.into(), src);
+        def
+    }
 }
 
 impl<T: Builder + AllocSSA> SSABuilder for T {}

src/panfrost/compiler/kraid/encode_v9.rs
@@ -1109,6 +1109,25 @@ impl V9Instr for OpFExp32 {
     }
 }
 
+impl V9Instr for OpFLogD {
+    fn get_info(&self, arch: u8) -> Option<V9InstrInfo> {
+        V9InstrInfo::from_isa(
+            Flogd::get_info(FlogdVariant::F32, arch),
+            src_map! {
+                src0: src,
+            },
+        )
+    }
+
+    fn encode(&self, e: V9Encoder) -> EncodedInstr {
+        e.encode(Flogd {
+            variant: FlogdVariant::F32,
+            dst: op_encode_dst(self, &self.dst),
+            src0: op_encode_src(self, &self.src),
+        })
+    }
+}
+
 impl From<FlushNanMode> for FlushNanM {
     fn from(value: FlushNanMode) -> Self {
         match value {
@@ -2495,6 +2514,7 @@ macro_rules! v9_op_match_else {
             Op::FAddLScale($x) => $y,
             Op::FCmp($x) => $y,
             Op::FExp32($x) => $y,
+            Op::FLogD($x) => $y,
             Op::Flush($x) => $y,
             Op::Fma($x) => $y,
             Op::FmaRScale($x) => $y,

src/panfrost/compiler/kraid/hw_tests.rs
@@ -1207,4 +1207,52 @@ mod builder {
             assert_feq!(comp, res, prec, "fexp({base_log2}, {arg})");
         }
     }
+
+    #[test]
+    fn test_flog2() {
+        const RANGE: Range<f32> = -1000.0..1000.0;
+
+        let run = RunSingleton::get();
+        let shader = {
+            let mut b = TestShaderBuilder::new(&*run.model);
+            let input = b.ld_test_data(0, 32);
+
+            let dst = b.flog2_32(input.into());
+            b.st_test_data(4, dst.into());
+            b.compile()
+        };
+
+        let mut rng = Acorn::new();
+        let log_case = |arg: f32| [arg.to_bits(), 0];
+        // Notable cases
+        let mut data = vec![
+            log_case(0.5),
+            log_case(1.0),
+            log_case(2.0),
+            log_case(4.0),
+            log_case(E),
+            log_case(10.0),
+            log_case(-1.0),
+            log_case(f32::NAN),
+        ];
+
+        for _ in 0..1000 {
+            let x = sample_f32_range(&mut rng, RANGE);
+            data.push([x.to_bits(), 0]);
+        }
+
+        let case = shader.with_args(FAU_ONLY_ARGS, &mut data);
+        run.execute(case);
+        for arr in data {
+            let [input, comp] = arr.map(f32::from_bits);
+            let res = input.log2();
+
+            let prec = if (0.5..=2.0).contains(&input) {
+                FPrecision::Abs(2f32.powi(-21))
+            } else {
+                FPrecision::Ulp(3)
+            };
+            assert_feq!(comp, res, prec, "flog2({input})");
+        }
+    }
 }

src/panfrost/compiler/kraid/nir.rs
@@ -793,6 +793,23 @@ impl<'a> ShaderFromNir<'a> {
                 assert!(alu.get_src(0).bit_size() == 32);
                 b.fexp_32_to(dst.into(), srcs(0), (1.0).into());
             }
+            nir_op_flog2 => {
+                // TODO: wire up flog2.f16
+                assert!(alu.get_src(0).bit_size() == 32);
+                b.flog2_32_to(dst.into(), srcs(0));
+            }
+            nir_op_fpow => {
+                assert!(alu.get_src(0).bit_size() == 32);
+                assert!(alu.get_src(1).bit_size() == 32);
+
+                // Constant-propagate the base when possible
+                let log2_base = match alu.get_src(0).comp_as_uint(0) {
+                    Some(c) => Src::from(f32::from_bits(c as u32).log2()),
+                    None => b.flog2_32(srcs(0)).into(),
+                };
+
+                b.fexp_32_to(dst.into(), srcs(1), log2_base);
+            }
             nir_op_frexp_exp => {
                 assert!(alu.get_src(0).bit_size() == 32);
                 b.push_op(OpFrexpE {

src/panfrost/compiler/kraid/ops.rs
@@ -944,6 +944,23 @@ impl fmt::Display for OpFExp32 {
     }
 }
 
+/// Performs log2(x) / (x-1)
+#[repr(C)]
+#[derive(Clone, Opcode)]
+pub struct OpFLogD {
+    #[dst_type(F32)]
+    pub dst: Dst,
+
+    #[src_type(F32)]
+    pub src: Src,
+}
+
+impl fmt::Display for OpFLogD {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} = FLOGD.f32 {}", &self.dst, self.fmt_src(&self.src))
+    }
+}
+
 #[derive(Clone, Copy, Default, Eq, Hash, PartialEq)]
 pub enum FlushNanMode {
     #[default]
@@ -2888,6 +2905,7 @@ pub enum Op {
     FAddLScale(Box<OpFAddLScale>),
     FCmp(Box<OpFCmp>),
     FExp32(Box<OpFExp32>),
+    FLogD(Box<OpFLogD>),
     Flush(Box<OpFlush>),
     Fma(Box<OpFma>),
     FmaRScale(Box<OpFmaRScale>),

commit d265315064bc0b27a82362d550049e7b3c42abd4

kraid: Pass scale to FEXP.f32 instead of arg
This fixes NaN handling with nir_op_fpow when the first argument is a
NaN by basing the final NaN on the the fma instead of the exponent
alone.

Fixes: 23f8fde15a8a ("kraid: Add OpFLogD and wire it up with tests")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43286>

src/panfrost/compiler/kraid/builder.rs
@@ -237,7 +237,10 @@ pub trait SSABuilder: Builder + AllocSSA {
         self.push_op(OpFExp32 {
             dst,
             expx: scale_fixp.into(),
-            expf: arg,
+            // The expf field is only used for NaN propagation.  By passing
+            // `scale` in here, we ensure that the result is NaN if either
+            // `arg` or `log2_base` are NaN.
+            expf: scale.into(),
         });
     }
 