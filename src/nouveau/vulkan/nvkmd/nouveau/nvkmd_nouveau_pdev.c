sailfishos-mirror/mesa

commit e81a292165add2997276dd30cd65381aa49ca72d

nvk: Disable compression on Turing
We've had multiple reports that this is broken on Turing right now.
Disable it until someone figures out what's wrong.

See also: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15358

Fixes: b524bf368e8 ("nvk: Reenable compression support with nouveau 1.4.2")
Reviewed-by: Karol Herbst <kherbst@redhat.com>
Reviewed-by: Mary Guillemard <mary@mary.zone>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41332>

src/nouveau/vulkan/nvkmd/nouveau/nvkmd_nouveau_pdev.c
@@ -16,7 +16,7 @@
 #include <sys/stat.h>
 #include <xf86drm.h>
 
-#include "clc597.h"
+#include "clc697.h"
 
 static bool
 drm_device_is_nouveau(const char *path)
@@ -102,7 +102,7 @@ nvkmd_nouveau_try_create_pdev(struct _drmDevice *drm_device,
       .has_map_fixed = true,
       .has_overmap = true,
       .has_compression = ws_dev->nouveau_version >= 0x01000402 &&
-                         ws_dev->info.cls_eng3d >= TURING_A,
+                         ws_dev->info.cls_eng3d >= AMPERE_A,
    };
 
    /* We get this ourselves */

commit 68ee90c22ecf30fa8135b37cd8abbee8b33b73aa

nvk: Reenable compression support on Turing with nouveau 1.4.3
Dave landed a proper fix for it that landed, let's reenable it again as
nouveau got another version bump for NVDEC support.

Signed-off-by: Mary Guillemard <mary@mary.zone>
Fixes: e81a292165a ("nvk: Disable compression on Turing")
Reviewed-by: Mel Henning <mhenning@darkrefraction.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43274>

src/nouveau/vulkan/nvkmd/nouveau/nvkmd_nouveau_pdev.c
@@ -16,6 +16,7 @@
 #include <sys/stat.h>
 #include <xf86drm.h>
 
+#include "clc597.h"
 #include "clc697.h"
 
 static bool
@@ -101,8 +102,10 @@ nvkmd_nouveau_try_create_pdev(struct _drmDevice *drm_device,
       .has_alloc_tiled = nouveau_ws_device_has_tiled_bo(ws_dev),
       .has_map_fixed = true,
       .has_overmap = true,
-      .has_compression = ws_dev->nouveau_version >= 0x01000402 &&
-                         ws_dev->info.cls_eng3d >= AMPERE_A,
+      .has_compression = (ws_dev->nouveau_version >= 0x01000403 &&
+                          ws_dev->info.cls_eng3d >= TURING_A) ||
+                         (ws_dev->nouveau_version >= 0x01000402 &&
+                          ws_dev->info.cls_eng3d >= AMPERE_A),
    };
 
    /* We get this ourselves */