50c2c924f8e4a1c0f2ef2caa42fa9710591a3bbd
Georg Lehmann <dadschoorse@gmail.com>
2026-07-29 18:55:21 +0200

    nir/to_lcssa: move constants before the loop instead of creating a phi
    Various NIR instructions really need constant sources, so replacing them with
    phis breaks all kinds of assumptions.
    
    In case of loop unrolling, this creates phis that will be cleaned up later,
    so often the harmful effect is hidden.
    In case of nir_simplify_loop(loop, nir_jump_break),
    the new phis can't be removed later, so everything breaks as soon
    as there is a nir_op_extract that uses a constant from the loop.
    
    Backport-to: 26.2
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15947
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15278
    
    Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43308>

--- src/compiler/nir/nir_to_lcssa.c
+++ src/compiler/nir/nir_to_lcssa.c
@@ -231,6 +231,14 @@ convert_loop_exit_for_ssa(nir_def *def, void *void_state)
       ASSERTED bool progress = nir_rematerialize_deref_in_use_blocks(nir_def_as_deref(def));
       assert(progress);
       return true;
+   } else if (nir_def_is_const(def)) {
+      /* Various things in NIR depend on constant sources,
+       * so move the constant before the loop to prevent creating phis.
+       */
+      nir_block *before_loop = nir_cf_node_as_block(nir_cf_node_prev(&state->loop->cf_node));
+      nir_cursor cursor = nir_after_block(before_loop);
+      nir_instr_move(cursor, nir_def_instr(def));
+      return true;
    }
 
    /* Initialize a phi-instruction */
