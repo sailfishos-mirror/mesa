aerisarn/mesa-uwp

commit ba0336ab3f69f958e4346c1465c2fe0859d4ac59

anv: Reduce RHWO optimization (Wa_1508744258)
Implement Wa_1508744258:

   Disable RHWO by setting 0x7010[14] by default except during resolve
   pass.

Disable the RCC RHWO optimization at all times except when resolving
single sampled color surfaces.

v2: Move stalling to genX(cmd_buffer_apply_pipe_flushes) for clarity (Mark)

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Mark Janes <markjanes@swizzler.org>
Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19450>

src/intel/vulkan/genX_cmd_buffer.c
@@ -2098,6 +2098,21 @@ genX(emit_apply_pipe_flushes)(struct anv_batch *batch,
 ALWAYS_INLINE void
 genX(cmd_buffer_apply_pipe_flushes)(struct anv_cmd_buffer *cmd_buffer)
 {
+#if GFX_VERx10 == 120
+   /* If we're changing the state of the RHWO optimization, we need to have
+    * sb_stall+cs_stall.
+    */
+   const bool rhwo_opt_change =
+      cmd_buffer->state.rhwo_optimization_enabled !=
+      cmd_buffer->state.pending_rhwo_optimization_enabled;
+   if (rhwo_opt_change) {
+      anv_add_pending_pipe_bits(cmd_buffer,
+                                ANV_PIPE_STALL_AT_SCOREBOARD_BIT |
+                                ANV_PIPE_END_OF_PIPE_SYNC_BIT,
+                                "change RHWO optimization");
+   }
+#endif
+
    enum anv_pipe_bits bits = cmd_buffer->state.pending_pipe_bits;
 
    if (unlikely(cmd_buffer->device->physical->always_flush_cache))
@@ -2127,6 +2142,19 @@ genX(cmd_buffer_apply_pipe_flushes)(struct anv_cmd_buffer *cmd_buffer)
                                     cmd_buffer->device,
                                     cmd_buffer->state.current_pipeline,
                                     bits);
+
+#if GFX_VERx10 == 120
+   /* Wa_1508744258 handling */
+   if (rhwo_opt_change) {
+      anv_batch_write_reg(&cmd_buffer->batch, GENX(COMMON_SLICE_CHICKEN1), c1) {
+         c1.RCCRHWOOptimizationDisable =
+            !cmd_buffer->state.pending_rhwo_optimization_enabled;
+         c1.RCCRHWOOptimizationDisableMask = true;
+      }
+      cmd_buffer->state.rhwo_optimization_enabled =
+         cmd_buffer->state.pending_rhwo_optimization_enabled;
+   }
+#endif
 
    if (trace_flush) {
       trace_intel_end_stall(&cmd_buffer->trace, bits,

src/intel/vulkan/anv_blorp.c
@@ -1962,10 +1962,20 @@ anv_image_ccs_op(struct anv_cmd_buffer *cmd_buffer,
                        0, 0, level_width, level_height);
       break;
    case ISL_AUX_OP_FULL_RESOLVE:
-   case ISL_AUX_OP_PARTIAL_RESOLVE:
+   case ISL_AUX_OP_PARTIAL_RESOLVE: {
+      /* Wa_1508744258: Enable RHWO optimization for resolves */
+      const bool enable_rhwo_opt = cmd_buffer->device->info->verx10 == 120;
+
+      if (enable_rhwo_opt)
+         cmd_buffer->state.pending_rhwo_optimization_enabled = true;
+
       blorp_ccs_resolve(&batch, &surf, level, base_layer, layer_count,
                         format, ccs_op);
+
+      if (enable_rhwo_opt)
+         cmd_buffer->state.pending_rhwo_optimization_enabled = false;
       break;
+   }
    case ISL_AUX_OP_AMBIGUATE:
       for (uint32_t a = 0; a < layer_count; a++) {
          const uint32_t layer = base_layer + a;

src/intel/vulkan/anv_private.h
@@ -2574,6 +2574,17 @@ struct anv_cmd_state {
     */
    enum anv_depth_reg_mode                      depth_reg_mode;
 
+   /**
+    * Whether RHWO optimization is enabled (Wa_1508744258).
+    */
+   bool                                         rhwo_optimization_enabled;
+
+   /**
+    * Pending state of the RHWO optimization, to be applied at the next
+    * genX(cmd_buffer_apply_pipe_flushes).
+    */
+   bool                                         pending_rhwo_optimization_enabled;
+
    bool                                         conditional_render_enabled;
 
    /**

src/intel/vulkan/genX_state.c
@@ -374,6 +374,19 @@ init_render_queue_state(struct anv_queue *queue)
       reg.HZDepthTestLEGEOptimizationDisable = true;
       reg.HZDepthTestLEGEOptimizationDisableMask = true;
    }
+
+   /* Wa_1508744258
+    *
+    *    Disable RHWO by setting 0x7010[14] by default except during resolve
+    *    pass.
+    *
+    * We implement global disabling of the optimization here and we toggle it
+    * in anv_image_ccs_op().
+    */
+   anv_batch_write_reg(&batch, GENX(COMMON_SLICE_CHICKEN1), c1) {
+      c1.RCCRHWOOptimizationDisable = true;
+      c1.RCCRHWOOptimizationDisableMask = true;
+   }
 #endif
 
 #if GFX_VERx10 < 125

sailfishos-mirror/mesa

commit 7c73cd054a3d70cb5cf12fa4eeb9561d0849abc4

anv: fix barrier for Wa_1508744258 / Wa_14024015672
We need to flush the RCC writes because the register programming for
Wa_1508744258 and 3DSTATE_3D_MODE Wa_14024015672 do not.

Otherwise we might trigger the HW mode while some RCC operations are
still going.

Fixes some hard hangs on BMG with applications using MSAA.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: ba0336ab3f ("anv: Reduce RHWO optimization (Wa_1508744258)")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43290>

src/intel/vulkan/genX_cmd_buffer.c
@@ -2411,18 +2411,24 @@ ALWAYS_INLINE void
 genX(cmd_buffer_apply_pipe_flushes)(struct anv_cmd_buffer *cmd_buffer)
 {
 #if INTEL_WA_1508744258_GFX_VER || INTEL_WA_14024015672_GFX_VER
-   /* If we're changing the state of the RHWO optimization, we need to have
-    * sb_stall+cs_stall.
+   /* If we're changing the state of the RHWO optimization we have to :
+    *
+    *    - on >= Gfx12.5+, RT flush, 3DSTATE_3D_MODE being non pipelined,
+    *      it'll fence following shader RCC operations
+    *
+    *    - on < Gfx12.5, RT flush + CS stall because we need to make sure all
+    *      previous RCC operations have completed before we touch the
+    *      COMMON_SLICE_CHICKEN1 register with MI commands
     */
    const bool rhwo_opt_change =
       cmd_buffer->state.rhwo_optimization_enabled !=
       cmd_buffer->state.pending_rhwo_optimization_enabled;
    if (rhwo_opt_change) {
       anv_add_pending_pipe_bits(cmd_buffer,
-                                VK_PIPELINE_STAGE_2_TOP_OF_PIPE_BIT,
-                                VK_PIPELINE_STAGE_2_TOP_OF_PIPE_BIT,
-                                ANV_PIPE_STALL_AT_SCOREBOARD_BIT |
-                                ANV_PIPE_END_OF_PIPE_SYNC_BIT,
+                                VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT,
+                                VK_PIPELINE_STAGE_2_FRAGMENT_SHADER_BIT,
+                                ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT |
+                                (GFX_VERx10 >= 125 ? 0 : ANV_PIPE_CS_STALL_BIT),
                                 "change RHWO optimization");
    }
 #endif