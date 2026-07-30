85c082ddbed727940535911e6bf87f7d274525bf
Karol Herbst <kherbst@redhat.com>
2026-07-30 21:17:26 +0200

    ci: download libclc artifacts from mesa-libclc
    Reviewed-by: Valentine Burley <valentine.burley@collabora.com>
    Reviewed-by: Eric Engestrom <eric@engestrom.ch>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43020>

--- .gitlab-ci/container/build-libclc.sh
+++ .gitlab-ci/container/build-libclc.sh
@@ -4,32 +4,15 @@ set -uex
 
 section_start libclc "Building libclc"
 
-export LLVM_CONFIG="llvm-config-${LLVM_VERSION:?"llvm unset!"}"
-LLVM_TAG="llvmorg-15.0.7"
-
-$LLVM_CONFIG --version
-
-git config --global user.email "mesa@example.com"
-git config --global user.name "Mesa CI"
-git clone \
-    https://github.com/llvm/llvm-project \
-    --depth 1 \
-    -b "${LLVM_TAG}" \
-    /llvm-project
+MESA_LIBCLC_PROJECT_ID=27606
+MESA_LIBCLC_VERSION=22.1.8.3
 
 mkdir /libclc
-pushd /libclc
-cmake -S /llvm-project/libclc -B . -G Ninja -DLLVM_CONFIG="$LLVM_CONFIG" -DLIBCLC_TARGETS_TO_BUILD="spirv-mesa3d-;spirv64-mesa3d-" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DLLVM_SPIRV=/usr/bin/llvm-spirv
-ninja
-ninja install
-popd
+for file in spirv64-mesa3d-.spv spirv-mesa3d-.spv mesa-libclc.pc.in; do
+  curl-with-retry "https://gitlab.freedesktop.org/api/v4/projects/${MESA_LIBCLC_PROJECT_ID}/packages/generic/mesa-libclc/${MESA_LIBCLC_VERSION}/${file}" -o /libclc/${file}
+done
 
-# workaroud cmake vs debian packaging.
-mkdir -p /usr/lib/clc
-ln -s /usr/share/clc/spirv64-mesa3d-.spv /usr/lib/clc/
-ln -s /usr/share/clc/spirv-mesa3d-.spv /usr/lib/clc/
-
-du -sh ./*
-rm -rf /libclc /llvm-project
+sed /libclc/mesa-libclc.pc.in -e 's/libexecdir=.*/libexecdir=\/libclc/g' > /usr/share/pkgconfig/mesa-libclc.pc
+rm /libclc/mesa-libclc.pc.in
 
 section_end libclc
