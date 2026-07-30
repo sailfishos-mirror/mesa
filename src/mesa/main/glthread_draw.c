sailfishos-mirror/mesa

commit b5f388a22205aa5af37f1a667fdeeb9892270db6

mesa: replace gl ctx direct access
Replaces all ctx->API calls with appropriate helpers.
Discussed here: https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21343

Signed-off-by: Michal Vanis <mik@vanis.sh>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38981>

src/mesa/main/glthread_draw.c
@@ -755,7 +755,7 @@ should_convert_to_begin_end(struct gl_context *ctx, unsigned count,
     * Others prevent syncing, such as disallowing buffer objects because we
     * can't map them without syncing.
     */
-   return ctx->API == API_OPENGL_COMPAT &&
+   return !_mesa_is_desktop_gl_compat(ctx) &&
           util_is_vbo_upload_ratio_too_large(count, num_upload_vertices) &&
           instance_count == 1 &&                /* no instancing */
           vao->CurrentElementBufferName == 0 && /* only user indices */

src/mesa/main/glformats.c
@@ -2415,14 +2415,14 @@ _mesa_base_tex_format(const struct gl_context *ctx, GLint internalFormat)
    case GL_ALPHA8:
    case GL_ALPHA12:
    case GL_ALPHA16:
-      return (ctx->API != API_OPENGL_CORE) ? GL_ALPHA : -1;
+      return (!_mesa_is_desktop_gl_core(ctx)) ? GL_ALPHA : -1;
    case 1:
    case GL_LUMINANCE:
    case GL_LUMINANCE4:
    case GL_LUMINANCE8:
    case GL_LUMINANCE12:
    case GL_LUMINANCE16:
-      return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE : -1;
+      return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE : -1;
    case 2:
    case GL_LUMINANCE_ALPHA:
    case GL_LUMINANCE4_ALPHA4:
@@ -2431,15 +2431,15 @@ _mesa_base_tex_format(const struct gl_context *ctx, GLint internalFormat)
    case GL_LUMINANCE12_ALPHA4:
    case GL_LUMINANCE12_ALPHA12:
    case GL_LUMINANCE16_ALPHA16:
-      return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE_ALPHA : -1;
+      return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE_ALPHA : -1;
    case GL_INTENSITY:
    case GL_INTENSITY4:
    case GL_INTENSITY8:
    case GL_INTENSITY12:
    case GL_INTENSITY16:
-      return (ctx->API != API_OPENGL_CORE) ? GL_INTENSITY : -1;
+      return (!_mesa_is_desktop_gl_core(ctx)) ? GL_INTENSITY : -1;
    case 3:
-      return (ctx->API != API_OPENGL_CORE) ? GL_RGB : -1;
+      return (!_mesa_is_desktop_gl_core(ctx)) ? GL_RGB : -1;
    case GL_RGB:
    case GL_R3_G3_B2:
    case GL_RGB4:
@@ -2450,7 +2450,7 @@ _mesa_base_tex_format(const struct gl_context *ctx, GLint internalFormat)
    case GL_RGB16:
       return GL_RGB;
    case 4:
-      return (ctx->API != API_OPENGL_CORE) ? GL_RGBA : -1;
+      return (!_mesa_is_desktop_gl_core(ctx)) ? GL_RGBA : -1;
    case GL_RGBA:
    case GL_RGBA2:
    case GL_RGBA4:
@@ -2518,13 +2518,13 @@ _mesa_base_tex_format(const struct gl_context *ctx, GLint internalFormat)
 
    switch (internalFormat) {
    case GL_COMPRESSED_ALPHA:
-      return (ctx->API != API_OPENGL_CORE) ? GL_ALPHA : -1;
+      return (!_mesa_is_desktop_gl_core(ctx)) ? GL_ALPHA : -1;
    case GL_COMPRESSED_LUMINANCE:
-      return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE : -1;
+      return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE : -1;
    case GL_COMPRESSED_LUMINANCE_ALPHA:
-      return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE_ALPHA : -1;
+      return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE_ALPHA : -1;
    case GL_COMPRESSED_INTENSITY:
-      return (ctx->API != API_OPENGL_CORE) ? GL_INTENSITY : -1;
+      return (!_mesa_is_desktop_gl_core(ctx)) ? GL_INTENSITY : -1;
    case GL_COMPRESSED_RGB:
       return GL_RGB;
    case GL_COMPRESSED_RGBA:
@@ -2554,34 +2554,34 @@ _mesa_base_tex_format(const struct gl_context *ctx, GLint internalFormat)
    if (_mesa_has_half_float_textures(ctx)) {
       switch (internalFormat) {
       case GL_ALPHA16F_ARB:
-         return (ctx->API != API_OPENGL_CORE) ? GL_ALPHA : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_ALPHA : -1;
       case GL_RGBA16F_ARB:
          return GL_RGBA;
       case GL_RGB16F_ARB:
          return GL_RGB;
       case GL_INTENSITY16F_ARB:
-         return (ctx->API != API_OPENGL_CORE) ? GL_INTENSITY : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_INTENSITY : -1;
       case GL_LUMINANCE16F_ARB:
-         return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE : -1;
       case GL_LUMINANCE_ALPHA16F_ARB:
-         return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE_ALPHA : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE_ALPHA : -1;
       }
    }
 
    if (_mesa_has_float_textures(ctx)) {
       switch (internalFormat) {
       case GL_ALPHA32F_ARB:
-         return (ctx->API != API_OPENGL_CORE) ? GL_ALPHA : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_ALPHA : -1;
       case GL_RGBA32F_ARB:
          return GL_RGBA;
       case GL_RGB32F_ARB:
          return GL_RGB;
       case GL_INTENSITY32F_ARB:
-         return (ctx->API != API_OPENGL_CORE) ? GL_INTENSITY : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_INTENSITY : -1;
       case GL_LUMINANCE32F_ARB:
-         return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE : -1;
       case GL_LUMINANCE_ALPHA32F_ARB:
-         return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE_ALPHA : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE_ALPHA : -1;
       }
    }
 
@@ -2606,19 +2606,19 @@ _mesa_base_tex_format(const struct gl_context *ctx, GLint internalFormat)
       case GL_ALPHA_SNORM:
       case GL_ALPHA8_SNORM:
       case GL_ALPHA16_SNORM:
-         return (ctx->API != API_OPENGL_CORE) ? GL_ALPHA : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_ALPHA : -1;
       case GL_LUMINANCE_SNORM:
       case GL_LUMINANCE8_SNORM:
       case GL_LUMINANCE16_SNORM:
-         return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE : -1;
       case GL_LUMINANCE_ALPHA_SNORM:
       case GL_LUMINANCE8_ALPHA8_SNORM:
       case GL_LUMINANCE16_ALPHA16_SNORM:
-         return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE_ALPHA : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE_ALPHA : -1;
       case GL_INTENSITY_SNORM:
       case GL_INTENSITY8_SNORM:
       case GL_INTENSITY16_SNORM:
-         return (ctx->API != API_OPENGL_CORE) ? GL_INTENSITY : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_INTENSITY : -1;
       default:
          ; /* fallthrough */
       }
@@ -2647,11 +2647,11 @@ _mesa_base_tex_format(const struct gl_context *ctx, GLint internalFormat)
       case GL_SLUMINANCE_ALPHA:
       case GL_SLUMINANCE8_ALPHA8:
       case GL_COMPRESSED_SLUMINANCE_ALPHA:
-         return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE_ALPHA : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE_ALPHA : -1;
       case GL_SLUMINANCE:
       case GL_SLUMINANCE8:
       case GL_COMPRESSED_SLUMINANCE:
-         return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE : -1;
       default:
          ; /* fallthrough */
       }
@@ -2709,28 +2709,28 @@ _mesa_base_tex_format(const struct gl_context *ctx, GLint internalFormat)
       case GL_ALPHA8I_EXT:
       case GL_ALPHA16I_EXT:
       case GL_ALPHA32I_EXT:
-         return (ctx->API != API_OPENGL_CORE) ? GL_ALPHA : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_ALPHA : -1;
       case GL_INTENSITY8UI_EXT:
       case GL_INTENSITY16UI_EXT:
       case GL_INTENSITY32UI_EXT:
       case GL_INTENSITY8I_EXT:
       case GL_INTENSITY16I_EXT:
       case GL_INTENSITY32I_EXT:
-         return (ctx->API != API_OPENGL_CORE) ? GL_INTENSITY : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_INTENSITY : -1;
       case GL_LUMINANCE8UI_EXT:
       case GL_LUMINANCE16UI_EXT:
       case GL_LUMINANCE32UI_EXT:
       case GL_LUMINANCE8I_EXT:
       case GL_LUMINANCE16I_EXT:
       case GL_LUMINANCE32I_EXT:
-         return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE : -1;
       case GL_LUMINANCE_ALPHA8UI_EXT:
       case GL_LUMINANCE_ALPHA16UI_EXT:
       case GL_LUMINANCE_ALPHA32UI_EXT:
       case GL_LUMINANCE_ALPHA8I_EXT:
       case GL_LUMINANCE_ALPHA16I_EXT:
       case GL_LUMINANCE_ALPHA32I_EXT:
-         return (ctx->API != API_OPENGL_CORE) ? GL_LUMINANCE_ALPHA : -1;
+         return (!_mesa_is_desktop_gl_core(ctx)) ? GL_LUMINANCE_ALPHA : -1;
       default:
          ; /* fallthrough */
       }

src/mesa/main/texparam.c
@@ -79,7 +79,7 @@ validate_texture_wrap_mode(struct gl_context * ctx, GLenum target, GLenum wrap)
       break;
 
    case GL_CLAMP_TO_BORDER:
-      supported = ctx->API != API_OPENGLES
+      supported = !_mesa_is_gles1(ctx)
          && (target != GL_TEXTURE_EXTERNAL_OES);
       break;
 
@@ -461,7 +461,7 @@ set_tex_parameteri(struct gl_context *ctx,
       return GL_TRUE;
 
    case GL_GENERATE_MIPMAP_SGIS:
-      if (!_mesa_is_desktop_gl_compat(ctx) && ctx->API != API_OPENGLES)
+      if (!_mesa_is_desktop_gl_compat(ctx) && !_mesa_is_gles1(ctx))
          goto invalid_pname;
 
       if (params[0] && texObj->Target == GL_TEXTURE_EXTERNAL_OES)
@@ -2423,7 +2423,7 @@ get_tex_parameterfv(struct gl_context *ctx,
          *params = obj->Sampler.Attrib.MaxAnisotropy;
          break;
       case GL_GENERATE_MIPMAP_SGIS:
-         if (!_mesa_is_desktop_gl_compat(ctx) && ctx->API != API_OPENGLES)
+         if (!_mesa_is_desktop_gl_compat(ctx) && !_mesa_is_gles1(ctx))
             goto invalid_pname;
 
 	 *params = (GLfloat) obj->Attrib.GenerateMipmap;
@@ -2711,7 +2711,7 @@ get_tex_parameteriv(struct gl_context *ctx,
          *params = LCLAMPF(obj->Sampler.Attrib.MaxAnisotropy, INT32_MIN, INT32_MAX);
          break;
       case GL_GENERATE_MIPMAP_SGIS:
-         if (!_mesa_is_desktop_gl_compat(ctx) && ctx->API != API_OPENGLES)
+         if (!_mesa_is_desktop_gl_compat(ctx) && !_mesa_is_gles1(ctx))
             goto invalid_pname;
 
 	 *params = (GLint) obj->Attrib.GenerateMipmap;

commit e79dfca9f9f2eba934d35dd77206503c5f9e8def

glthread: fix check for unroll draws using user VBOs when the ctx supports GLES
This was originally fixed by 56b4d19 but when the gl ctx direct
accesses were replaced in b5f388a the condition was mistakenly
inverted.

Fixes: b5f388a2220 ("mesa: replace gl ctx direct access")

Signed-off-by: Nick Hamilton <nick.hamilton@imgtec.com>
Reviewed-by: Marek Olšák <maraeo@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43242>

src/mesa/main/glthread_draw.c
@@ -755,7 +755,7 @@ should_convert_to_begin_end(struct gl_context *ctx, unsigned count,
     * Others prevent syncing, such as disallowing buffer objects because we
     * can't map them without syncing.
     */
-   return !_mesa_is_desktop_gl_compat(ctx) &&
+   return _mesa_is_desktop_gl_compat(ctx) &&
           util_is_vbo_upload_ratio_too_large(count, num_upload_vertices) &&
           instance_count == 1 &&                /* no instancing */
           vao->CurrentElementBufferName == 0 && /* only user indices */