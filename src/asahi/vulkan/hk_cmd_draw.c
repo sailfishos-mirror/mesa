sailfishos-mirror/mesa

commit 8bb4f24ef1bd991003befcc9b14094e66f695fcb

poly: allow specifying component count in passthrough GS key
Currently the passthrough GS always emits 4-component varying outputs,
regardless of how many components were in outputs in the original VS.
On panvk, we see failures with this behavior in some CTS tests that have
array outputs with <4 component vectors. These are fixed by preserving
component counts from the original VS through the passthrough GS.

This affects
dEQP-VK.draw.renderpass.output_location.array.r16g16-sfloat-highp.*

Signed-off-by: Olivia Lee <olivia.lee@collabora.com>
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41192>

src/asahi/vulkan/hk_cmd_draw.c
@@ -3245,9 +3245,14 @@ hk_handle_passthrough_gs(struct hk_cmd_buffer *cmd, struct agx_draw draw)
       .cull_distance_array_size = last_sw->info.cull_distance_array_size,
    };
 
-   /* We don't care about VS varying types in AGX, just set everything to
-    * uint32 to improve cache hits. */
    for (uint32_t i = 0; i < NUM_TOTAL_VARYING_SLOTS; i++) {
+      key->output_components[i] =
+         nir_slot_num_components(i, MESA_SHADER_VERTEX);
+      if (key->output_components[i] == 0)
+         key->output_components[i] = 4;
+
+      /* We don't care about VS varying types in AGX, just set everything to
+       * uint32 to improve cache hits. */
       key->output_types[i] = nir_type_uint32;
    }
 

src/poly/nir/poly_nir.h
@@ -70,6 +70,9 @@ struct poly_passthrough_gs_key {
    /* Type of each output varying written by the VS/TES */
    nir_alu_type output_types[NUM_TOTAL_VARYING_SLOTS];
 
+   /* Number of components written for each output by the VS/TES */
+   unsigned output_components[NUM_TOTAL_VARYING_SLOTS];
+
    /* Clip/cull sizes, implies clip/cull written in output */
    uint8_t clip_distance_array_size;
    uint8_t cull_distance_array_size;

src/poly/nir/poly_nir_passthrough_gs.c
@@ -69,9 +69,7 @@ poly_nir_passthrough_gs(nir_builder *b, const void *key_)
          unsigned adjusted_loc = loc;
          nir_def *offset = zero;
          unsigned num_slots = 1;
-
-         bool scalar = nir_slot_num_components(loc, MESA_SHADER_GEOMETRY) == 1;
-         unsigned comps = scalar ? 1 : 4;
+         unsigned comps = key->output_components[loc];
 
          /* We use combined, compact clip/cull */
          if (loc == VARYING_SLOT_CLIP_DIST1 || loc == VARYING_SLOT_CULL_DIST1) {

commit d4d8b05477ab399db283d28838de2228bf17effd

hk: xfb: Avoid assertions in nir_slot_num_components
Avoids assertions that the shader stage is MESA_SHADER_TESS_CTRL for
VARYING_SLOT_TESS_LEVEL_OUTER, VARYING_SLOT_TESS_LEVEL_INNER and
VARYING_SLOT_BOUNDING_BOX1 when calling
`nir_slot_num_components(i, MESA_SHADER_VERTEX)` in
hk_handle_passthrough_gs().

Fixes: 8bb4f24ef1bd ("poly: allow specifying component count in passthrough GS key")
Signed-off-by: Janne Grunau <j@jannau.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43310>

src/asahi/vulkan/hk_cmd_draw.c
@@ -3246,10 +3246,18 @@ hk_handle_passthrough_gs(struct hk_cmd_buffer *cmd, struct agx_draw draw)
    };
 
    for (uint32_t i = 0; i < NUM_TOTAL_VARYING_SLOTS; i++) {
-      key->output_components[i] =
-         nir_slot_num_components(i, MESA_SHADER_VERTEX);
-      if (key->output_components[i] == 0)
-         key->output_components[i] = 4;
+      unsigned num_comps = 0;
+      switch (i) {
+         case VARYING_SLOT_TESS_LEVEL_OUTER:
+         case VARYING_SLOT_TESS_LEVEL_INNER:
+         case VARYING_SLOT_BOUNDING_BOX1:
+            num_comps = 4;
+            break;
+         default:
+            num_comps = nir_slot_num_components(i, MESA_SHADER_VERTEX);
+            break;
+      }
+      key->output_components[i] = num_comps ?: 4;
 
       /* We don't care about VS varying types in AGX, just set everything to
        * uint32 to improve cache hits. */