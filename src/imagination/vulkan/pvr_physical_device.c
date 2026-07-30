096b8f2c092268287b2d4f7f71deaa7007c54a9f
Frank Binns <frank.binns@imgtec.com>
2026-07-30 10:19:07 +0100

    pvr: re-enable VK_KHR_format_feature_flags2
    This was originally disabled by a22ad99bdd2 ("pvr: set device
    features/props/extensions to Vulkan 1.0 minimums (unless implemented)") as part
    of focusing on passing "base" Vulkan 1.0 conformance. It's now safe to re-enable
    this extension.
    
    Signed-off-by: Frank Binns <frank.binns@imgtec.com>
    Reviewed-by: Luigi Santivetti <luigi.santivetti@imgtec.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43336>

--- src/imagination/vulkan/pvr_physical_device.c
+++ src/imagination/vulkan/pvr_physical_device.c
@@ -139,7 +139,7 @@ static void pvr_physical_device_get_supported_extensions(
       .KHR_external_memory_fd = true,
       .KHR_external_semaphore = true,
       .KHR_external_semaphore_fd = true,
-      .KHR_format_feature_flags2 = false,
+      .KHR_format_feature_flags2 = true,
       .KHR_get_memory_requirements2 = true,
       .KHR_incremental_present = PVR_USE_WSI_PLATFORM,
       .KHR_image_format_list = true,
