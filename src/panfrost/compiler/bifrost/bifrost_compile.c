sailfishos-mirror/mesa

commit 320667060c5a6c2cd78580020d8c171f7b234df9

panfrost/compiler: Properly compute Valhall ALU bound
Signed-off-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
Reviewed-by: Ashley Smith <ashley.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42419>

src/panfrost/compiler/bifrost/bifrost_compile.c
@@ -3986,6 +3986,52 @@ bi_gather_stats(bi_context *ctx, unsigned size, struct bifrost_stats *out)
    out->cycles = MAX2(out->arith, MAX3(out->t, out->v, out->ldst));
 }
 
+static float
+va_compute_alu_bound(unsigned arch, float fma, float cvt, float sfu)
+{
+   switch (arch) {
+   case 9:
+      return MAX3(fma, cvt, sfu);
+   case 10:
+      /* Each fetch unit has local arithmetic pipelines for FMA and CVT, and a
+       * shared 1/4 width SFU.  It can only issue a single instruction at a time.
+       */
+      return MAX2(fma + cvt + (sfu / 4.0f), sfu);
+   case 11:
+   case 12: {
+      /* Each fetch unit can issue two operations, one to each issue port, per
+       * clock.
+       * Port 0 can handle any instruction type.
+       * Port 1 can handle only FMA instructions.
+       */
+      fma *= 2.0f;
+      float port0_base = cvt + (sfu / 4.0f);
+      float port0_fma = MAX2(0.0f, fma - port0_base);
+      float port_cost = port0_base + port0_fma;
+      return MAX2(port_cost, fma);
+   }
+   case 13: {
+      /* Arithmetic units each fetch unit can issue two operations, one to each
+       * issue port, per clock.
+       * Port 0 can handle FMA+CVT+SFU instruction types.
+       * Port 1 can handle FMA+CVT instruction types.
+       */
+      fma *= 2.0f;
+      cvt *= 2.0f;
+      float port0_base = sfu / 4.0f;
+      float port0_other = MAX2(0.0f, fma + cvt - port0_base) / 2.0f;
+      float port_cost = port0_base + port0_other;
+      return MAX2(port_cost, sfu);
+   }
+   case 14:
+   case 15:
+      return fma + cvt + sfu;
+   default:
+      assert(!"Unsupported architecture");
+      return fma + cvt + sfu;
+   }
+}
+
 static void
 va_count_stats(bi_context *ctx, unsigned nr_ins, unsigned size,
                const struct va_stats *counts,
@@ -3999,8 +4045,9 @@ va_count_stats(bi_context *ctx, unsigned nr_ins, unsigned size,
       model = pan_get_model(((uint64_t)0x9001) << 16, 0);
       assert(model);
    }
+   unsigned arch = pan_arch(ctx->inputs->gpu_id);
 
-   struct valhall_stats stats_abs = {
+   struct valhall_stats stats = {
       .instrs = nr_ins,
       .code_size = size,
       .fma = ((float)counts->fma),
@@ -4017,20 +4064,27 @@ va_count_stats(bi_context *ctx, unsigned nr_ins, unsigned size,
       .registers_used = util_bitcount64(counts->reg_mask),
       .uniforms_used = counts->nr_fau_uniforms,
    };
-   struct valhall_stats stats = stats_abs;
-   stats.fma /= model->rates.fma;
-   stats.cvt /= model->rates.fma;
-   stats.sfu /= (model->rates.fma / 4.0);
-   stats.v /= 16.0;
-   stats.t /= model->rates.texel;
-   stats.ls /= 1.0;
 
-   const bool output_normalized = !(bifrost_debug & BIFROST_DBG_STATSABS);
-   *out = output_normalized ? stats : stats_abs;
+   /* v14 doesn't have rates anymore */
+   const bool output_normalized =
+      !(arch >= 14 || !!(bifrost_debug & BIFROST_DBG_STATSABS));
 
-   /* Calculate the bound */
-   out->cycles = MAX2(MAX3(stats.fma, stats.cvt, stats.sfu),
-                      MAX3(stats.v, stats.t, stats.ls));
+   if (output_normalized) {
+      assert(model->rates.fma != 0 && model->rates.cvt != 0 &&
+             model->rates.sfu != 0 && model->rates.varying != 0 &&
+             model->rates.texel != 0);
+      stats.fma /= model->rates.fma;
+      stats.cvt /= model->rates.cvt;
+      stats.sfu /= model->rates.sfu;
+      stats.v /= model->rates.varying;
+      stats.t /= model->rates.texel;
+      stats.ls /= 1.0;
+   }
+
+   stats.alu = va_compute_alu_bound(arch, stats.fma, stats.cvt, stats.sfu);
+   stats.cycles = MAX4(out->alu, stats.v, stats.t, stats.ls);
+
+   *out = stats;
 }
 
 static unsigned
@@ -4789,21 +4843,18 @@ valhall_stats_verbose(FILE *f, bi_context *ctx, const struct valhall_stats *stat
    va_gather_stats(ctx, stats->code_size, &max_stats, GATHER_STATS_MAX);
 
    /* now print instruction statistics */
-   float arith = MAX3(stats->fma, stats->sfu, stats->cvt);
-   float min_arith = MAX3(min_stats.fma, min_stats.sfu, min_stats.cvt);
-   float max_arith = MAX3(max_stats.fma, max_stats.sfu, max_stats.cvt);
    static const char *statname[] = {
       "A", "FMA", "CVT", "SFU", "LS", "V", "T"
    };
    float statval[] = {
-      arith, stats->fma, stats->cvt, stats->sfu, stats->ls, stats->v, stats->t,
+      stats->alu, stats->fma, stats->cvt, stats->sfu, stats->ls, stats->v, stats->t,
    };
    float min_statval[] = {
-      min_arith, min_stats.fma, min_stats.cvt, min_stats.sfu,
+      min_stats.alu, min_stats.fma, min_stats.cvt, min_stats.sfu,
       min_stats.ls, min_stats.v, min_stats.t,
    };
    float max_statval[] = {
-      max_arith, max_stats.fma, max_stats.cvt, max_stats.sfu,
+      max_stats.alu, max_stats.fma, max_stats.cvt, max_stats.sfu,
       max_stats.ls, max_stats.v, max_stats.t,
    };
    unsigned n = ARRAY_SIZE(statval);

src/util/shader_stats.xml
@@ -77,6 +77,7 @@
         <stat name="FMA" type="float">Estimated normalized FMA (Fused Multiply-Add) cycles</stat>
         <stat name="CVT" type="float">Estimated normalized CVT (ConVerT) cycles</stat>
         <stat name="SFU" type="float">Estimated normalized SFU (Special Function Unit) cycles</stat>
+        <stat name="ALU" type="float">Estimated normalized ALU (Arithmetic Logical Unit) cycles</stat>
         <stat name="Varying" display="V" type="float">Estimated normalized Varying cycles</stat>
         <stat name="Texture" display="T" type="float">Estimated normalized Texture cycles</stat>
         <stat name="Load/store" display="LS" type="float">Estimated normalized Load/Store cycles</stat>

commit cc82e157111d488c5c7540f4042884803293ebf7

pan/compiler/stats: Fix ALU not being used in instruction bounds
A last minute typo made the MAX bound read zeroed memory instead of the
previously written ALU cycles.

Signed-off-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
Fixes: 320667060c5 ("panfrost/compiler: Properly compute Valhall ALU bound")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43370>

src/panfrost/compiler/bifrost/bifrost_compile.c
@@ -4086,7 +4086,7 @@ va_count_stats(bi_context *ctx, unsigned nr_ins, unsigned size,
    }
 
    stats.alu = va_compute_alu_bound(arch, stats.fma, stats.cvt, stats.sfu);
-   stats.cycles = MAX4(out->alu, stats.v, stats.t, stats.ls);
+   stats.cycles = MAX4(stats.alu, stats.v, stats.t, stats.ls);
 
    *out = stats;
 }