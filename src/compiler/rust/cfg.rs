sailfishos-mirror/mesa

commit 3467bdbb352e29da3ae8b08b4af82b5099088b96

compiler/rust/cfg: Make sorting optional in CFGBuilder::as_cfg()
We still remove unreachable blocks but we otherwise leave the blocks in
their original order.  For now, all users of CFGBuilder are fixed to
pass true to as_cfg() to maintain the current behavior.

Reviewed-by: Mel Henning <mhenning@darkrefraction.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42730>

src/compiler/rust/cfg.rs
@@ -120,6 +120,36 @@ fn rev_post_order_sort<N>(nodes: &mut Vec<CFGNode<N>>) {
     std::mem::swap(nodes, &mut sorted);
 }
 
+struct ReachableDFS<'a, N> {
+    nodes: &'a [CFGNode<N>],
+    reachable: BitSet<usize>,
+}
+
+impl<'a, N> DepthFirstSearch for ReachableDFS<'a, N> {
+    type ChildIter = Cloned<std::slice::Iter<'a, usize>>;
+
+    fn pre(&mut self, id: usize) -> Self::ChildIter {
+        self.reachable.insert(id);
+        self.nodes[id].succ.iter().cloned()
+    }
+}
+
+fn remove_unreachable<N>(nodes: &mut Vec<CFGNode<N>>) {
+    let mut reachable_dfs = ReachableDFS {
+        nodes,
+        reachable: Default::default(),
+    };
+    dfs(&mut reachable_dfs, 0);
+
+    // The Vec::retain() method guarantees that each item is visited once,
+    // in-order so it's safe to use an external iterator like this.
+    let mut idx_iter = (0_usize..).into_iter();
+    nodes.retain(|_| {
+        let i = idx_iter.next().unwrap();
+        reachable_dfs.reachable.contains(i)
+    });
+}
+
 fn find_common_dom<N>(
     nodes: &[CFGNode<N>],
     mut a: usize,
@@ -340,6 +370,7 @@ impl<N> CFG<N> {
     fn from_blocks_edges(
         nodes: impl IntoIterator<Item = N>,
         edges: impl IntoIterator<Item = (usize, usize)>,
+        sort_nodes: bool,
     ) -> Self {
         let mut nodes = Vec::from_iter(nodes.into_iter().map(|n| CFGNode {
             node: n,
@@ -356,7 +387,11 @@ impl<N> CFG<N> {
             nodes[p].succ.push(s);
         }
 
-        rev_post_order_sort(&mut nodes);
+        if sort_nodes {
+            rev_post_order_sort(&mut nodes);
+        } else {
+            remove_unreachable(&mut nodes);
+        }
         calc_dominance(&mut nodes);
         let has_loop = detect_loops(&mut nodes);
 
@@ -561,14 +596,18 @@ impl<K: Eq + Hash, N, H: BuildHasher + Default> CFGBuilder<K, N, H> {
         self.edges.push((s, p));
     }
 
-    /// Destroys this builder and returns a CFG.
-    pub fn as_cfg(mut self) -> CFG<N> {
+    /// Destroys this builder and returns a CFG.  If `sort_nodes` is true, the
+    /// nodes will be re-sorted into a dominance-preserving order when creating
+    /// the CFG.  If `sort_nodes` is false, unreachable nodes will be removed
+    /// but the node order of the returned CFG will otherwise be the same as
+    /// the order in which `add_node()` was called.
+    pub fn as_cfg(mut self, sort_nodes: bool) -> CFG<N> {
         let edges = self.edges.drain(..).map(|(s, p)| {
             let s = *self.key_map.get(&s).unwrap();
             let p = *self.key_map.get(&p).unwrap();
             (s, p)
         });
-        CFG::from_blocks_edges(self.nodes, edges)
+        CFG::from_blocks_edges(self.nodes, edges, sort_nodes)
     }
 }
 
@@ -591,7 +630,7 @@ mod tests {
         for &(a, b) in edges {
             builder.add_edge(a, b);
         }
-        let cfg = builder.as_cfg();
+        let cfg = builder.as_cfg(true);
 
         let mut lphs = Vec::new();
         lphs.resize(expected.len(), None);

src/compiler/rust/dataflow.rs
@@ -202,7 +202,7 @@ mod tests {
         for &(a, b) in edges {
             builder.add_edge(a, b);
         }
-        let cfg = builder.as_cfg();
+        let cfg = builder.as_cfg(true);
 
         let mut reachable_in: Vec<BitSet> =
             (0..cfg.len()).map(|_| Default::default()).collect();
@@ -265,7 +265,7 @@ mod tests {
         for &(a, b) in edges {
             builder.add_edge(a, b);
         }
-        let cfg = builder.as_cfg();
+        let cfg = builder.as_cfg(true);
 
         let mut reachable_in: Vec<BitSet> =
             (0..cfg.len()).map(|_| Default::default()).collect();
@@ -328,7 +328,7 @@ mod tests {
         for &(a, b) in edges {
             builder.add_edge(a, b);
         }
-        let cfg = builder.as_cfg();
+        let cfg = builder.as_cfg(true);
 
         let mut iter_in: Vec<u32> = vec![0; cfg.len()];
         let mut iter_out: Vec<u32> = vec![0; cfg.len()];

src/nouveau/compiler/nak/from_nir.rs
@@ -4606,7 +4606,7 @@ impl<'a> ShaderFromNir<'a> {
 
         self.parse_cf_list(&mut ssa_alloc, &mut phi_map, nfi.iter_body());
 
-        let cfg = std::mem::take(&mut self.cfg).as_cfg();
+        let cfg = std::mem::take(&mut self.cfg).as_cfg(true);
         assert!(cfg.len() > 0);
         for i in 0..cfg.len() {
             if cfg[i].falls_through() {

src/nouveau/compiler/nak/hw_tests.rs
@@ -203,7 +203,7 @@ impl<'a> TestShaderBuilder<'a> {
         let f = Function {
             ssa_alloc: self.alloc,
             phi_alloc: PhiAllocator::new(),
-            blocks: cfg.as_cfg(),
+            blocks: cfg.as_cfg(true),
         };
 
         let cs_info = ComputeShaderInfo {

src/nouveau/compiler/nak/nvdisasm_tests.rs
@@ -61,7 +61,7 @@ fn disassemble_instrs(instrs: Vec<Instr>, sm: u8) -> Vec<String> {
     let f = Function {
         ssa_alloc: SSAValueAllocator::new(),
         phi_alloc: PhiAllocator::new(),
-        blocks: cfg.as_cfg(),
+        blocks: cfg.as_cfg(true),
     };
 
     let cs_info = ComputeShaderInfo {

src/nouveau/compiler/nak/opt_jump_thread.rs
@@ -135,7 +135,7 @@ fn rewrite_cfg(func: &mut Function) {
     for block in func.blocks.drain() {
         builder.add_node(block.label, block);
     }
-    let _ = std::mem::replace(&mut func.blocks, builder.as_cfg());
+    let _ = std::mem::replace(&mut func.blocks, builder.as_cfg(true));
 }
 
 /// Replace jumps to the following block with fall-through

src/panfrost/compiler/kraid/hw_tests.rs
@@ -198,7 +198,7 @@ impl<'a> TestShaderBuilder<'a> {
             model,
             ssa_alloc,
             phi_alloc: Default::default(),
-            blocks: cfg.as_cfg(),
+            blocks: cfg.as_cfg(true),
             info,
         };
         s.validate();

src/panfrost/compiler/kraid/nir.rs
@@ -1638,7 +1638,7 @@ impl<'a> ShaderFromNir<'a> {
                 nb,
             );
         }
-        let mut blocks = cfg.as_cfg();
+        let mut blocks = cfg.as_cfg(true);
 
         // If there are any preload registers, splice them before the
         // starting block

commit a8259a857aa9c90eef3ffecca4ccea35a3492b55

compiler/rust/cfg: Remap node edges in remove_unreachable()
It's not enough to just delete the unused nodes.  We also need to remap
edges.  We weren't remapping edges before, so if we had an unreachable
node, any edges to or from nodes after that node got screwed up.  This
means we need the same algorithm for remove_unreachable() as we use for
rev_post_order_sort().

Fixes: 3467bdbb352e ("compiler/rust/cfg: Make sorting optional in CFGBuilder::as_cfg()")
Reviewed-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43322>

src/compiler/rust/cfg.rs
@@ -34,6 +34,45 @@ impl<N> DerefMut for CFGNode<N> {
     }
 }
 
+// SAFETY: The caller must guarantee that:
+//
+//  - For each `i in 0..nodes.len()`, `remap_idx(i)` returns `None` or
+//    `Some(j)` where `(0..count).contains(j)`
+//
+//  - For each `j in 0..count`, `Some(j)` is returned exactly once
+//
+unsafe fn reorder_nodes<N>(
+    nodes: &mut Vec<CFGNode<N>>,
+    remap_idx: impl Fn(usize) -> Option<usize>,
+    count: usize,
+) {
+    // Re-map edges to use post-index numbering
+    for n in nodes.iter_mut() {
+        let remap_filter_idx = |i: &mut usize| {
+            if let Some(r) = remap_idx(*i) {
+                *i = r;
+                true
+            } else {
+                false
+            }
+        };
+        n.pred.retain_mut(remap_filter_idx);
+        n.succ.retain_mut(remap_filter_idx);
+    }
+
+    // We know a priori that each non-MAX post_idx is unique so we can sort the
+    // nodes by inserting them into a new array by index.
+    let mut sorted: Vec<CFGNode<N>> = Vec::with_capacity(count);
+    for (i, n) in nodes.drain(..).enumerate() {
+        if let Some(r) = remap_idx(i) {
+            unsafe { sorted.as_mut_ptr().add(r).write(n) };
+        }
+    }
+    unsafe { sorted.set_len(count) };
+
+    *nodes = sorted;
+}
+
 struct PostOrderSort {
     post_idx: Vec<usize>,
     count: usize,
@@ -93,31 +132,7 @@ fn rev_post_order_sort<N>(nodes: &mut Vec<CFGNode<N>>) {
     };
     assert!(remap_idx(0) == Some(0));
 
-    // Re-map edges to use post-index numbering
-    for n in nodes.iter_mut() {
-        let remap_filter_idx = |i: &mut usize| {
-            if let Some(r) = remap_idx(*i) {
-                *i = r;
-                true
-            } else {
-                false
-            }
-        };
-        n.pred.retain_mut(remap_filter_idx);
-        n.succ.retain_mut(remap_filter_idx);
-    }
-
-    // We know a priori that each non-MAX post_idx is unique so we can sort the
-    // nodes by inserting them into a new array by index.
-    let mut sorted: Vec<CFGNode<N>> = Vec::with_capacity(sort.count);
-    for (i, n) in nodes.drain(..).enumerate() {
-        if let Some(r) = remap_idx(i) {
-            unsafe { sorted.as_mut_ptr().add(r).write(n) };
-        }
-    }
-    unsafe { sorted.set_len(sort.count) };
-
-    std::mem::swap(nodes, &mut sorted);
+    unsafe { reorder_nodes(nodes, remap_idx, sort.count) };
 }
 
 struct ReachableDFS<'a, N> {
@@ -141,13 +156,28 @@ fn remove_unreachable<N>(nodes: &mut Vec<CFGNode<N>>) {
     };
     dfs(&mut reachable_dfs, 0);
 
-    // The Vec::retain() method guarantees that each item is visited once,
-    // in-order so it's safe to use an external iterator like this.
-    let mut idx_iter = (0_usize..).into_iter();
-    nodes.retain(|_| {
-        let i = idx_iter.next().unwrap();
-        reachable_dfs.reachable.contains(i)
-    });
+    let mut count = 0;
+    let mut order = Vec::with_capacity(nodes.len());
+    for i in 0..nodes.len() {
+        if reachable_dfs.reachable.contains(i) {
+            order.push(count);
+            count += 1;
+        } else {
+            order.push(usize::MAX);
+        }
+    }
+
+    let remap_idx = |i: usize| {
+        let j = order[i];
+        if j == usize::MAX {
+            None
+        } else {
+            Some(j)
+        }
+    };
+    assert!(remap_idx(0) == Some(0));
+
+    unsafe { reorder_nodes(nodes, remap_idx, count) };
 }
 
 fn find_common_dom<N>(
@@ -620,6 +650,7 @@ impl<K, N, H: BuildHasher + Default> Default for CFGBuilder<K, N, H> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashSet;
     use std::hash::RandomState;
 
     fn test_loop_nesting(edges: &[(usize, usize)], expected: &[Option<usize>]) {
@@ -871,6 +902,103 @@ mod tests {
         test_loop_nesting(
             &[(0, 1), (0, 2), (1, 3), (1, 2), (2, 1)],
             &[None, Some(1), None, None, None, None],
+        );
+    }
+
+    fn test_no_sort(edges: &[(usize, usize)], expected: &[usize]) {
+        let mut nodes = BitSet::new();
+        for &(a, b) in edges {
+            nodes.insert(a);
+            nodes.insert(b);
+        }
+
+        let mut builder = CFGBuilder::<_, _, RandomState>::new();
+        for i in nodes.iter() {
+            builder.add_node(i, i);
+        }
+        for &(a, b) in edges {
+            builder.add_edge(a, b);
+        }
+        let cfg = builder.as_cfg(false);
+
+        // We don't sort but we do delete dead blocks
+        assert_eq!(cfg.len(), expected.len());
+        assert_eq!(cfg[0], 0);
+        for i in 1..cfg.len() {
+            assert!(i <= cfg[i]);
+            assert!(cfg[i - 1] < cfg[i], "{} >= {}", cfg[i - 1], cfg[i]);
+            assert_eq!(cfg[i], expected[i]);
+        }
+
+        // Check that the new CFG is a subgraph of the old.
+        let edges: HashSet<_> = edges.iter().cloned().collect();
+        for p in 0..cfg.len() {
+            for &s in cfg.succ_indices(p) {
+                assert!(edges.contains(&(cfg[p], cfg[s])));
+            }
+        }
+        for s in 0..cfg.len() {
+            for &p in cfg.pred_indices(s) {
+                assert!(edges.contains(&(cfg[p], cfg[s])));
+            }
+        }
+    }
+
+    #[test]
+    fn test_no_sort_if_else() {
+        // block 0
+        // if ... {
+        //     block 1
+        // }
+        // block 2
+        // if ... {
+        //     block 3
+        // }
+        // block 4
+        test_no_sort(
+            &[(0, 1), (0, 2), (1, 2), (2, 3), (2, 4), (3, 4)],
+            &[0, 1, 2, 3, 4],
+        );
+    }
+
+    #[test]
+    fn test_no_sort_loop() {
+        // block 0
+        // loop {
+        //     block 1
+        //     if ... {
+        //         block 2
+        //         break;
+        //     } else {
+        //         block 3
+        //     }
+        //     block 4
+        // }
+        // block 5
+        test_no_sort(
+            &[(0, 1), (1, 2), (1, 3), (2, 5), (3, 4), (4, 1)],
+            &[0, 1, 2, 3, 4, 5],
+        );
+    }
+
+    #[test]
+    fn test_no_sort_loop_dead_block() {
+        // block 0
+        // loop {
+        //     block 1
+        //     if ... {
+        //         block 2
+        //         break;
+        //     } else {
+        //         block 3
+        //         continue;
+        //     }
+        //     block 4
+        // }
+        // block 5
+        test_no_sort(
+            &[(0, 1), (1, 2), (1, 3), (2, 5), (3, 1), (4, 1)],
+            &[0, 1, 2, 3, 5],
         );
     }
 }