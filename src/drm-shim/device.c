6347bff12949e19c08946c7d5cf3a6c210afa337
Gyeyoung Baek <gye976@gmail.com>
2026-08-02 21:33:53 +0900

    drm-shim: fix inverted BO refcounting
    drm_shim_bo_put() returned when the refcount reached zero instead of
    freeing the object. and drm_shim_bo_init() never took an initial
    reference.
    
    Fix this by making drm_shim_bo_init() take the initial reference and
    freeing the object when the refcount reaches zero in drm_shim_bo_put().
    
    Fixes: 82bf1979d7b7 ("v3d: Introduce a DRM shim for calling out to the simulator.")
    Signed-off-by: Gyeyoung Baek <gye976@gmail.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Marek Olšák <maraeo@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43401>

--- src/drm-shim/device.c
+++ src/drm-shim/device.c
@@ -349,6 +349,7 @@ drm_shim_bo_init(struct shim_bo *bo, size_t size)
       return -ENOMEM;
 
    bo->size = size;
+   drm_shim_bo_get(bo);
 
    return 0;
 }
@@ -380,7 +381,9 @@ drm_shim_bo_get(struct shim_bo *bo)
 void
 drm_shim_bo_put(struct shim_bo *bo)
 {
-   if (p_atomic_dec_return(&bo->refcount) == 0)
+   assert(p_atomic_read(&bo->refcount) > 0);
+
+   if (p_atomic_dec_return(&bo->refcount) > 0)
       return;
 
    if (shim_device.driver_bo_free)
