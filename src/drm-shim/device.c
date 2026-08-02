643438faa37a9b81b9b58674f44418bd88704501
Gyeyoung Baek <gye976@gmail.com>
2026-08-02 17:43:03 +0900

    drm-shim: remove the BO's `offset_map` entry on free
    Fixes: c4b5ebe1fc91 ("drm-shim: Better mmap offsets")
    Signed-off-by: Gyeyoung Baek <gye976@gmail.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Marek Olšák <maraeo@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43401>

--- src/drm-shim/device.c
+++ src/drm-shim/device.c
@@ -387,6 +387,7 @@ drm_shim_bo_put(struct shim_bo *bo)
       shim_device.driver_bo_free(bo);
 
    mtx_lock(&shim_device.lock);
+   _mesa_hash_table_u64_remove(shim_device.offset_map, bo->mem_addr);
    util_vma_heap_free(&shim_device.mem_heap, bo->mem_addr, bo->size);
    mtx_unlock(&shim_device.lock);
    free(bo);
