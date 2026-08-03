alexvorxx/Mesa-VirGL

commit 778cb590862c50eaf817ae5a2bdb2d24a8fed192

anv: optimize STATE_BYTE_STRIDE emission
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30803>

src/intel/vulkan/anv_private.h
@@ -3327,6 +3327,7 @@ enum anv_cmd_dirty_bits {
    ANV_CMD_DIRTY_OCCLUSION_QUERY_ACTIVE              = 1 << 6,
    ANV_CMD_DIRTY_FS_MSAA_FLAGS                       = 1 << 7,
    ANV_CMD_DIRTY_COARSE_PIXEL_ACTIVE                 = 1 << 8,
+   ANV_CMD_DIRTY_INDIRECT_DATA_STRIDE                = 1 << 9,
 };
 typedef enum anv_cmd_dirty_bits anv_cmd_dirty_mask_t;
 
@@ -3846,6 +3847,9 @@ struct anv_cmd_graphics_state {
    uint32_t index_type; /**< 3DSTATE_INDEX_BUFFER.IndexFormat */
    uint32_t index_offset;
    uint32_t index_size;
+
+   uint32_t indirect_data_stride;
+   bool indirect_data_stride_aligned;
 
    struct vk_vertex_input_state vertex_input;
    struct vk_sample_locations_state sample_locations;

src/intel/vulkan/genX_cmd_draw.c
@@ -942,6 +942,15 @@ genX(cmd_buffer_flush_gfx_state)(struct anv_cmd_buffer *cmd_buffer)
                                           dirty & VK_SHADER_STAGE_ALL_GRAPHICS);
    }
 
+#if GFX_VER >= 20
+   if (cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_INDIRECT_DATA_STRIDE) {
+      anv_batch_emit(&cmd_buffer->batch, GENX(STATE_BYTE_STRIDE), sb_stride) {
+         sb_stride.ByteStride = cmd_buffer->state.gfx.indirect_data_stride;
+         sb_stride.ByteStrideEnable = !cmd_buffer->state.gfx.indirect_data_stride_aligned;
+      }
+   }
+#endif
+
    /* When we're done, only thing left is the possible dirty state
     * returned by cmd_buffer_flush_gfx_runtime_state.
     */
@@ -1750,25 +1759,55 @@ static inline const uint32_t xi_argument_format_for_vk_cmd(enum vk_cmd_type cmd)
 #endif
 }
 
-static inline const bool
-stride_aligned_for_vk_cmd(uint32_t stride, enum vk_cmd_type cmd)
+static inline bool
+cmd_buffer_set_indirect_stride(struct anv_cmd_buffer *cmd_buffer,
+                               uint32_t stride, enum vk_cmd_type cmd)
 {
-   if (stride == 0)
-      return true;
+   /* Should have been sanitized by the caller */
+   assert(stride != 0);
+
+   uint32_t data_stride = 0;
 
    switch (cmd) {
-      case VK_CMD_DRAW_INDIRECT:
-      case VK_CMD_DRAW_INDIRECT_COUNT:
-         return stride == sizeof(VkDrawIndirectCommand);
-      case VK_CMD_DRAW_INDEXED_INDIRECT:
-      case VK_CMD_DRAW_INDEXED_INDIRECT_COUNT:
-         return stride == sizeof(VkDrawIndexedIndirectCommand);
-      case VK_CMD_DRAW_MESH_TASKS_INDIRECT_EXT:
-      case VK_CMD_DRAW_MESH_TASKS_INDIRECT_COUNT_EXT:
-         return stride == sizeof(VkDrawMeshTasksIndirectCommandEXT);
-      default:
-         unreachable("unhandled cmd type");
+   case VK_CMD_DRAW_INDIRECT:
+   case VK_CMD_DRAW_INDIRECT_COUNT:
+      data_stride = sizeof(VkDrawIndirectCommand);
+      break;
+   case VK_CMD_DRAW_INDEXED_INDIRECT:
+   case VK_CMD_DRAW_INDEXED_INDIRECT_COUNT:
+      data_stride = sizeof(VkDrawIndexedIndirectCommand);
+      break;
+   case VK_CMD_DRAW_MESH_TASKS_INDIRECT_EXT:
+   case VK_CMD_DRAW_MESH_TASKS_INDIRECT_COUNT_EXT:
+      data_stride = sizeof(VkDrawMeshTasksIndirectCommandEXT);
+      break;
+   default:
+      unreachable("unhandled cmd type");
    }
+
+   bool aligned = stride == data_stride;
+
+#if GFX_VER >= 20
+   /* The stride can change as long as it matches the default command stride
+    * and STATE_BYTE_STRIDE::ByteStrideEnable=false, we can just do nothing.
+    *
+    * Otheriwse STATE_BYTE_STRIDE::ByteStrideEnable=true, any stride change
+    * should be signaled.
+    */
+   struct anv_cmd_graphics_state *gfx_state = &cmd_buffer->state.gfx;
+   if (gfx_state->indirect_data_stride_aligned != aligned) {
+      gfx_state->indirect_data_stride = stride;
+      gfx_state->indirect_data_stride_aligned = aligned;
+      gfx_state->dirty |= ANV_CMD_DIRTY_INDIRECT_DATA_STRIDE;
+   } else if (!gfx_state->indirect_data_stride_aligned &&
+              gfx_state->indirect_data_stride != stride) {
+      gfx_state->indirect_data_stride = stride;
+      gfx_state->indirect_data_stride_aligned = aligned;
+      gfx_state->dirty |= ANV_CMD_DIRTY_INDIRECT_DATA_STRIDE;
+   }
+#endif
+
+   return aligned;
 }
 
 static void
@@ -1780,20 +1819,14 @@ genX(cmd_buffer_emit_execute_indirect_draws)(struct anv_cmd_buffer *cmd_buffer,
                                              enum vk_cmd_type cmd)
 {
 #if GFX_VERx10 >= 125
+   bool aligned_stride =
+      cmd_buffer_set_indirect_stride(cmd_buffer, indirect_data_stride, cmd);
+
    genX(cmd_buffer_flush_gfx_state)(cmd_buffer);
 
    if (cmd_buffer->state.conditional_render_enabled)
       genX(cmd_emit_conditional_render_predicate)(cmd_buffer);
 
-   const bool aligned_stride =
-      stride_aligned_for_vk_cmd(indirect_data_stride, cmd);
-
-#if GFX_VER >= 20
-   anv_batch_emit(&cmd_buffer->batch, GENX(STATE_BYTE_STRIDE), sb_stride) {
-      sb_stride.ByteStride = indirect_data_stride;
-      sb_stride.ByteStrideEnable = !aligned_stride;
-   }
-#endif
    uint32_t offset = 0;
    for (uint32_t i = 0; i < max_draw_count; i++) {
       struct anv_address draw = anv_address_add(indirect_data_addr, offset);

sailfishos-mirror/mesa

commit b683b5a33e7a9fba20bb872c0772c886dcfc8ace

anv: fixup the logic dealing with STATE_BYTE_STRIDE
First simplify the logic with just a boolean tracking whether a
special stride was programmed.

Second always unset STATE_BYTE_STRIDE as the end of primary command
buffers.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 778cb59086 ("anv: optimize STATE_BYTE_STRIDE emission")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15803
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43344>

src/intel/vulkan/anv_private.h
@@ -4655,8 +4655,7 @@ struct anv_cmd_graphics_state {
    struct intel_urb_config urb_cfg;
 
    uint32_t indirect_data_stride;
-   enum u_tristate indirect_data_stride_aligned : 2;
-
+   bool indirect_data_stride_set : 1;
 
    bool kill_pixel : 1;
    bool uses_xfb : 1;

src/intel/vulkan/genX_cmd_buffer.c
@@ -4217,6 +4217,14 @@ end_command_buffer(struct anv_cmd_buffer *cmd_buffer,
       genX(cmd_buffer_set_protected_memory)(cmd_buffer, false);
 #endif
 
+#if GFX_VER >= 20
+   if (cmd_buffer->vk.level == VK_COMMAND_BUFFER_LEVEL_PRIMARY &&
+       anv_cmd_buffer_is_render_queue(cmd_buffer) &&
+       cmd_buffer->state.gfx.indirect_data_stride_set) {
+      anv_batch_emit(&cmd_buffer->batch, GENX(STATE_BYTE_STRIDE), sb_stride);
+   }
+#endif
+
    trace_intel_end_cmd_buffer(&cmd_buffer->trace,
                               (uintptr_t)(vk_command_buffer_to_handle(&cmd_buffer->vk)),
                               cmd_buffer->vk.level);
@@ -4419,10 +4427,8 @@ genX(CmdExecuteCommands)(
       /* Copy the mode of the secondary if set, at the next draw if things
        * don't match we will reprogram.
        */
-      if (secondary->state.gfx.indirect_data_stride_aligned !=
-          U_TRISTATE_UNSET) {
-         container->state.gfx.indirect_data_stride_aligned =
-            secondary->state.gfx.indirect_data_stride_aligned;
+      if (secondary->state.gfx.indirect_data_stride_set) {
+         container->state.gfx.indirect_data_stride_set = true;
          container->state.gfx.indirect_data_stride =
             secondary->state.gfx.indirect_data_stride;
       }

src/intel/vulkan/genX_cmd_draw.c
@@ -974,8 +974,7 @@ cmd_buffer_flush_gfx_state(struct anv_cmd_buffer *cmd_buffer)
    if (cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_INDIRECT_DATA_STRIDE) {
       anv_batch_emit(&cmd_buffer->batch, GENX(STATE_BYTE_STRIDE), sb_stride) {
          sb_stride.ByteStride = cmd_buffer->state.gfx.indirect_data_stride >> 2;
-         sb_stride.ByteStrideEnable =
-            cmd_buffer->state.gfx.indirect_data_stride_aligned == U_TRISTATE_NO;
+         sb_stride.ByteStrideEnable = cmd_buffer->state.gfx.indirect_data_stride_set;
       }
    }
 #endif
@@ -1987,6 +1986,9 @@ static inline uint32_t xi_argument_format_for_vk_cmd(enum vk_cmd_type cmd)
 #endif
 }
 
+/* Return whether EXECUTE_INDIRECT_DRAW can unroll all the draw calls or
+ * whether we need to emit the max count.
+ */
 static inline bool
 cmd_buffer_set_indirect_stride(struct anv_cmd_buffer *cmd_buffer,
                                uint32_t stride, enum vk_cmd_type cmd)
@@ -2013,29 +2015,28 @@ cmd_buffer_set_indirect_stride(struct anv_cmd_buffer *cmd_buffer,
       UNREACHABLE("unhandled cmd type");
    }
 
-   enum u_tristate aligned = u_tristate_make(stride == data_stride);
+   const bool aligned = stride == data_stride;
 
 #if GFX_VER >= 20
-   /* The stride can change as long as it matches the default command stride
-    * and STATE_BYTE_STRIDE::ByteStrideEnable=false, we can just do nothing.
-    *
-    * Otheriwse STATE_BYTE_STRIDE::ByteStrideEnable=true, any stride change
-    * should be signaled.
+   /* If STATE_BYTE_STRIDE was never set and the indirect data stride is
+    * aligned, no need to do anything. Otherwise check if we need to reprogram
+    * STATE_BYTE_STRIDE.
     */
    struct anv_cmd_graphics_state *gfx_state = &cmd_buffer->state.gfx;
-   if (gfx_state->indirect_data_stride_aligned != aligned) {
-      gfx_state->indirect_data_stride = stride;
-      gfx_state->indirect_data_stride_aligned = aligned;
-      gfx_state->dirty |= ANV_CMD_DIRTY_INDIRECT_DATA_STRIDE;
-   } else if (gfx_state->indirect_data_stride_aligned == U_TRISTATE_NO &&
-              gfx_state->indirect_data_stride != stride) {
-      gfx_state->indirect_data_stride = stride;
-      gfx_state->indirect_data_stride_aligned = aligned;
+   if (aligned && !gfx_state->indirect_data_stride_set)
+      return false;
+
+   if (gfx_state->indirect_data_stride != stride) {
+      gfx_state->indirect_data_stride = aligned ? 0 : stride;
+      gfx_state->indirect_data_stride_set = !aligned;
       gfx_state->dirty |= ANV_CMD_DIRTY_INDIRECT_DATA_STRIDE;
    }
 #endif
 
-   return aligned == U_TRISTATE_YES;
+   /* Gfx20+ can accomodate any stride through programming STATE_BYTE_STRIDE,
+    * ARL cannot unless indirect data is aligned.
+    */
+   return GFX_VER >= 20 ? false : aligned;
 }
 
 static void
@@ -2047,7 +2048,7 @@ genX(cmd_buffer_emit_execute_indirect_draws)(struct anv_cmd_buffer *cmd_buffer,
                                              enum vk_cmd_type cmd)
 {
 #if GFX_VERx10 >= 125
-   bool aligned_stride =
+   const bool needs_software_unroll =
       cmd_buffer_set_indirect_stride(cmd_buffer, indirect_data_stride, cmd);
 
    cmd_buffer_flush_gfx(cmd_buffer);
@@ -2066,7 +2067,7 @@ genX(cmd_buffer_emit_execute_indirect_draws)(struct anv_cmd_buffer *cmd_buffer,
          ind.TBIMREnabled               = cmd_buffer->state.gfx.dyn_state.use_tbimr;
          ind.PredicateEnable            =
             cmd_buffer->state.conditional_render_enabled;
-         ind.MaxCount                   = aligned_stride ? max_draw_count : 1;
+         ind.MaxCount                   = needs_software_unroll ? 1 : max_draw_count;
          ind.ArgumentBufferStartAddress = draw;
          ind.CountBufferAddress         = count_addr;
          ind.CountBufferIndirectEnable  = !anv_address_is_null(count_addr);
@@ -2078,12 +2079,7 @@ genX(cmd_buffer_emit_execute_indirect_draws)(struct anv_cmd_buffer *cmd_buffer,
       cmd_buffer_post_draw_wa(cmd_buffer, 1,
                               0 /* Doesn't matter for GFX_VER > 9 */);
 
-      /* If all the indirect structures are aligned, then we can let the HW
-       * do the unrolling and we only need one instruction. Otherwise we
-       * need to emit one instruction per draw, but we're still avoiding
-       * the register loads with MI commands.
-       */
-      if (aligned_stride || GFX_VER >= 20)
+      if (!needs_software_unroll)
          break;
 
       offset += indirect_data_stride;