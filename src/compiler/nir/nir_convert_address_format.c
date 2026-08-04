sailfishos-mirror/mesa

commit e714497981912849d2c146575077eb2c7daf2731

nir: add nir_convert_address_format pass
This pass can be used to convert the nir_address_format used by derefs.
All derefs (that use a specified mode) will be updated to use the new
format and their users are updated as follows:
- *_deref* intrinsics: will also use the new format.
- others: converted back to the old format before the use.

Currently, this pass only supports converting from/to 64bit_global and
64bit_global_32bit_offset but it should be easy to add support for other
formats if that becomes necessary.

The main use case for creating this pass is to support backends that use
64b global addresses but support instructions with a 64b base address
and 32b offset. Converting 64bit_global to 64bit_global_32bit_offset
before calling nir_lower_explicit_io will allow them to take advantage
of nir_lower_explicit_io separating the 32b offset in that case. This
allows for easier codegen as it's not always easy (or even possible
soundly) to do when the only thing you see is 64b address arithmetic.

Signed-off-by: Job Noorman <jnoorman@igalia.com>
Reviewed-by: Connor Abbott <cwabbott0@gmail.com>
Reviewed-by: Rob Clark <robclark@freedesktop.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37138>

src/compiler/nir/nir_convert_address_format.c
@@ -1,0 +1,190 @@
+/*
+ * Copyright 2026 Igalia S.L.
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "nir.h"
+#include "nir_builder.h"
+
+typedef struct {
+   nir_variable_mode modes;
+   nir_address_format from;
+   nir_address_format to;
+} convert_address_format_state;
+
+static bool
+has_address_format(nir_def *def, nir_address_format format)
+{
+   return def->bit_size == nir_address_format_bit_size(format) &&
+          def->num_components == nir_address_format_num_components(format);
+}
+
+nir_def *
+nir_build_convert_address_format(nir_builder *b, nir_def *addr,
+                                 nir_address_format from, nir_address_format to)
+{
+   assert(has_address_format(addr, from));
+
+   switch (from) {
+   case nir_address_format_64bit_global:
+      switch (to) {
+      case nir_address_format_64bit_global_32bit_offset:
+         return nir_vec4(b, nir_unpack_64_2x32_split_x(b, addr),
+                         nir_unpack_64_2x32_split_y(b, addr), nir_imm_int(b, 0),
+                         nir_imm_int(b, 0));
+
+      default:
+         UNREACHABLE("unsupported address format");
+      }
+
+   case nir_address_format_64bit_global_32bit_offset:
+      switch (to) {
+      case nir_address_format_64bit_global:
+         return nir_iadd(b, nir_pack_64_2x32(b, nir_trim_vector(b, addr, 2)),
+                         nir_u2u64(b, nir_channel(b, addr, 3)));
+
+      default:
+         UNREACHABLE("unsupported address format");
+      }
+
+   default:
+      UNREACHABLE("unsupported address format");
+   }
+}
+
+static void
+convert_def(nir_builder *b, nir_def *def, convert_address_format_state *state)
+{
+   unsigned num_components = nir_address_format_num_components(state->to);
+   unsigned bit_size = nir_address_format_bit_size(state->to);
+
+   def->num_components = num_components;
+   def->bit_size = bit_size;
+
+   /* Check if all uses support the new address format. If not, convert them
+    * back to the old format.
+    */
+   nir_def *addr = NULL;
+
+   nir_foreach_use_safe(use, def) {
+      nir_instr *instr = nir_src_use_instr(use);
+
+      if (instr->type == nir_instr_type_deref) {
+         /* Handled later. */
+         continue;
+      } else if (instr->type == nir_instr_type_intrinsic) {
+         nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+
+         switch (intrin->intrinsic) {
+         case nir_intrinsic_load_deref:
+         case nir_intrinsic_store_deref:
+         case nir_intrinsic_load_deref_block_intel:
+         case nir_intrinsic_store_deref_block_intel:
+         case nir_intrinsic_deref_atomic:
+         case nir_intrinsic_deref_atomic_swap:
+         case nir_intrinsic_deref_buffer_array_length:
+         case nir_intrinsic_deref_mode_is:
+         case nir_intrinsic_launch_mesh_workgroups_with_payload_deref:
+            /* These support any address format. */
+            continue;
+
+         default:
+            break;
+         }
+      }
+
+      if (!addr) {
+         b->cursor = nir_after_instr(nir_def_instr(def));
+         addr =
+            nir_build_convert_address_format(b, def, state->to, state->from);
+      }
+
+      nir_src_rewrite(use, addr);
+   }
+}
+
+static void
+maybe_convert_src(nir_builder *b, nir_src *src,
+                  convert_address_format_state *state)
+{
+   if (has_address_format(src->ssa, state->to)) {
+      return;
+   }
+
+   b->cursor = nir_before_instr(nir_src_use_instr(src));
+   nir_def *addr =
+      nir_build_convert_address_format(b, src->ssa, state->from, state->to);
+   nir_src_rewrite(src, addr);
+
+   if (nir_src_use_instr(src)->type == nir_instr_type_alu) {
+      nir_alu_src *alu_src = nir_src_as_alu_src(src);
+
+      for (unsigned c = 0; c < addr->num_components; c++) {
+         alu_src->swizzle[c] = c;
+      }
+   }
+}
+
+static bool
+convert_deref(nir_builder *b, nir_deref_instr *deref, void *data)
+{
+   convert_address_format_state *state = data;
+
+   if (!nir_deref_mode_is_in_set(deref, state->modes)) {
+      return false;
+   }
+
+   if (deref->deref_type == nir_deref_type_cast) {
+      maybe_convert_src(b, &deref->parent, state);
+   }
+
+   assert(deref->deref_type == nir_deref_type_var ||
+          has_address_format(deref->parent.ssa, state->to));
+   assert(has_address_format(&deref->def, state->from));
+
+   unsigned from_bit_size = nir_address_format_bit_size(state->from);
+   unsigned to_bit_size = nir_address_format_bit_size(state->to);
+
+   /* Array index needs to match the pointer size so convert if necessary. */
+   if ((deref->deref_type == nir_deref_type_array ||
+        deref->deref_type == nir_deref_type_ptr_as_array) &&
+       from_bit_size != to_bit_size) {
+      b->cursor = nir_before_instr(&deref->instr);
+      nir_def *index = NULL;
+
+      if (to_bit_size == 32 && nir_src_is_const(deref->arr.index)) {
+         int64_t const_index = nir_src_as_int(deref->arr.index);
+
+         if (const_index >= INT32_MIN && const_index <= INT32_MAX) {
+            index = nir_imm_int(b, const_index);
+         }
+      }
+
+      if (!index) {
+         index = nir_i2iN(b, deref->arr.index.ssa, to_bit_size);
+      }
+
+      nir_src_rewrite(&deref->arr.index, index);
+   }
+
+   convert_def(b, &deref->def, state);
+   return true;
+}
+
+/* Convert derefs with the given modes from using the from format to using the
+ * to format. All uses of converted derefs other than loads/stores will be
+ * converted back to the from format.
+ */
+bool
+nir_convert_address_format(nir_shader *shader, nir_variable_mode modes,
+                           nir_address_format from, nir_address_format to)
+{
+   convert_address_format_state state = {
+      .modes = modes,
+      .from = from,
+      .to = to,
+   };
+
+   return nir_shader_deref_pass(shader, convert_deref,
+                                nir_metadata_control_flow, &state);
+}

src/compiler/nir/meson.build
@@ -106,6 +106,7 @@ else
   'nir_builtin_builder.c',
   'nir_builtin_builder.h',
   'nir_conversion_builder.h',
+  'nir_convert_address_format.c',
   'nir_clip_cull_distance_io_utils.c',
   'nir_clone.c',
   'nir_constant_expressions.h',

src/compiler/nir/nir.h
@@ -5771,6 +5771,12 @@ bool nir_lower_explicit_io(nir_shader *shader,
                            nir_variable_mode modes,
                            nir_address_format);
 
+bool nir_convert_address_format(nir_shader *shader, nir_variable_mode modes,
+                                nir_address_format from, nir_address_format to);
+nir_def *nir_build_convert_address_format(nir_builder *b, nir_def *addr,
+                                          nir_address_format from,
+                                          nir_address_format to);
+
 typedef enum {
    /* Use open-coded funnel shifts for each component. */
    nir_mem_access_shift_method_scalar,

commit b759fa487f944aac50ac7c8857f7ff6a870e8b21

nir/convert_address_format: Split convert_def into two passes
If we need to convert some use's back to the original address format,
we cannot create the new instructions as part of iterating the uses,
as this itself would create a new use which should not be re-written.

Split this loop into two passes, with the first one simply collecting
the uses that need to be rewritten, and a second one which actually
does the re-writing.

Fixes: e71449798191 ("nir: add nir_convert_address_format pass")
Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43214>

src/compiler/nir/nir_convert_address_format.c
@@ -6,6 +6,8 @@
 #include "nir.h"
 #include "nir_builder.h"
 
+#include "util/u_dynarray.h"
+
 typedef struct {
    nir_variable_mode modes;
    nir_address_format from;
@@ -63,8 +65,13 @@ convert_def(nir_builder *b, nir_def *def, convert_address_format_state *state)
 
    /* Check if all uses support the new address format. If not, convert them
     * back to the old format.
+    *
+    * This is done in two passes, so that we aren't creating a new use of
+    * the def (ie. the address conversion) while we are still iterating the
+    * original uses.
     */
-   nir_def *addr = NULL;
+
+   struct util_dynarray rewrites = UTIL_DYNARRAY_INIT;
 
    nir_foreach_use_safe(use, def) {
       nir_instr *instr = nir_src_use_instr(use);
@@ -93,14 +100,28 @@ convert_def(nir_builder *b, nir_def *def, convert_address_format_state *state)
          }
       }
 
+      util_dynarray_append(&rewrites, use);
+   }
+
+   nir_def *addr = NULL;
+
+   /* Second pass, now that we've found all the original use's that need
+    * conversion, we can safely create the address conversion (which
+    * would have created a new use that _shouldn't_ be re-written if
+    * we did this as part of the first loop), and re-write the needed
+    * uses:
+    */
+
+   util_dynarray_foreach (&rewrites, nir_src *, usep) {
       if (!addr) {
          b->cursor = nir_after_instr(nir_def_instr(def));
          addr =
             nir_build_convert_address_format(b, def, state->to, state->from);
       }
-
-      nir_src_rewrite(use, addr);
+      nir_src_rewrite(*usep, addr);
    }
+
+   util_dynarray_fini(&rewrites);
 }
 
 static void