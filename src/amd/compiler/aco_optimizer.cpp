bminor/mesa-mesa

commit b798ace443402715a48037374336968f7d3c26bb

aco/optimizer: fix skip_smem_offset_align with non temp register operands
Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38830>

src/amd/compiler/aco_optimizer.cpp
@@ -1796,12 +1796,21 @@ skip_smem_offset_align(opt_ctx& ctx, SMEM_instruction* smem, uint32_t align)
       return;
 
    uint32_t mask = ~(align - 1u);
-   if (bitwise_instr->operands[0].constantEquals(mask) &&
-       bitwise_instr->operands[1].isOfType(op.regClass().type()))
-      op.setTemp(bitwise_instr->operands[1].getTemp());
-   else if (bitwise_instr->operands[1].constantEquals(mask) &&
-            bitwise_instr->operands[0].isOfType(op.regClass().type()))
-      op.setTemp(bitwise_instr->operands[0].getTemp());
+   for (unsigned i = 0; i < 2; i++) {
+      Operand new_op = bitwise_instr->operands[!i];
+      if (!bitwise_instr->operands[i].constantEquals(mask) ||
+          !new_op.isOfType(op.regClass().type()))
+         continue;
+
+      if (new_op.isTemp()) {
+         op.setTemp(op.getTemp());
+      } else {
+         assert(new_op.isFixed());
+         op = new_op;
+      }
+
+      return;
+   }
 }
 
 void

sailfishos-mirror/mesa

commit b47e255b21fa382759c9c68ccbc53da037452670

aco/optimizer: fix skip_smem_offset_align
We actually have to update the temporary.

Foz-DB Navi48:
Totals from 318 (0.15% of 209101) affected shaders:
Instrs: 334072 -> 333210 (-0.26%); split: -0.27%, +0.01%
CodeSize: 1743652 -> 1740388 (-0.19%); split: -0.20%, +0.01%
Latency: 3467824 -> 3467554 (-0.01%); split: -0.01%, +0.00%
InvThroughput: 517308 -> 517239 (-0.01%)
SClause: 10408 -> 10383 (-0.24%); split: -0.25%, +0.01%
Copies: 29332 -> 29325 (-0.02%); split: -0.03%, +0.00%
Branches: 9747 -> 9745 (-0.02%)
PreSGPRs: 17170 -> 17166 (-0.02%)
SALU: 66476 -> 65933 (-0.82%)

Fixes: b798ace4434 ("aco/optimizer: fix skip_smem_offset_align with non temp register operands")
Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Marek Olšák <maraeo@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43415>

src/amd/compiler/aco_optimizer.cpp
@@ -1920,7 +1920,7 @@ skip_smem_offset_align(opt_ctx& ctx, SMEM_instruction* smem, uint32_t align)
          continue;
 
       if (new_op.isTemp()) {
-         op.setTemp(op.getTemp());
+         op.setTemp(new_op.getTemp());
       } else {
          assert(new_op.isFixed());
          op = new_op;