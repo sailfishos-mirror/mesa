sailfishos-mirror/mesa

commit ddbdc9826344d121f608fc77ede21a0d9cc2eb2f

radeonsi/vcn: Add vcn_5_0_2 support
Signed-off-by: Sonny Jiang <sonjiang@amd.com>
Reviewed-by: Boyuan Zhang <Boyuan.Zhang@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41430>

src/amd/common/ac_gpu_info.c
@@ -489,8 +489,6 @@ ac_fill_hw_ip_info(struct radeon_info *info, const struct drm_amdgpu_info_device
          return false;
 
       info->ip[ip_type].num_queues = util_bitcount(ip_info->available_rings);
-   } else {
-      return false;
    }
 
    /* Gfx6-8 don't set ip_discovery_version. */
@@ -865,6 +863,9 @@ ac_identify_chip(struct radeon_info *info, const struct drm_amdgpu_info_device *
          break;
       case VCN_IP_VERSION(5, 0, 1):
          info->vcn_ip_version = VCN_5_0_1;
+         break;
+      case VCN_IP_VERSION(5, 0, 2):
+         info->vcn_ip_version = VCN_5_0_2;
          break;
       case VCN_IP_VERSION(5, 3, 0):
          info->vcn_ip_version = VCN_5_3_0;

src/amd/common/ac_surface.c
@@ -631,7 +631,8 @@ bool ac_get_supported_modifiers(const struct radeon_info *info,
       ADD_MOD(DRM_FORMAT_MOD_LINEAR)
       break;
    }
-   case GFX12: {
+   case GFX12:
+   case GFX12_1: {
       /* Chip properties no longer affect tiling, and there is no distinction between displayable
        * and non-displayable anymore. (DCC settings may affect displayability though)
        *

src/amd/common/ac_vcn_dec.c
@@ -2374,6 +2374,7 @@ ac_vcn_create_video_decoder(const struct radeon_info *info, struct ac_video_dec_
       dec->addr_mode = RDECODE_ARRAY_MODE_ADDRLIB_SEL_GFX9;
       dec->av1_version = RDECODE_AV1_VER_2;
       break;
+   case VCN_5_0_2:
    case VCN_5_3_0:
       dec->addr_mode = RDECODE_ARRAY_MODE_ADDRLIB_SEL_GFX11;
       dec->av1_version = RDECODE_AV1_VER_2;

src/amd/common/ac_video.c
@@ -18,7 +18,8 @@ check_vcn_fw_version(struct radeon_info *info, uint32_t dec, uint32_t enc, uint3
 static bool
 vcn_dec_only(struct radeon_info *info)
 {
-   return info->vcn_ip_version == VCN_4_0_3 || info->vcn_ip_version == VCN_5_0_1;
+   return info->vcn_ip_version == VCN_4_0_3 || info->vcn_ip_version == VCN_5_0_1 ||
+          info->vcn_ip_version == VCN_5_0_2;
 }
 
 static bool
@@ -534,15 +535,19 @@ vcn_jpeg_caps(struct radeon_info *info)
    cap->max_active_refs = 0;
    cap->bitstream_size_alignment = 128;
    cap->bitstream_address_alignment = 256;
-   cap->mjpeg.roi_crop = info->vcn_ip_version == VCN_4_0_3 || info->vcn_ip_version == VCN_5_0_1;
+   cap->mjpeg.roi_crop = info->vcn_ip_version == VCN_4_0_3 || info->vcn_ip_version == VCN_5_0_1 ||
+                         info->vcn_ip_version == VCN_5_0_2;
    cap->formats.nv12 = 1;
    cap->formats.y8u8y8v8_422 = 1;
    cap->formats.y8_400 = 1;
    cap->formats.y8_u8_v8_444 = info->vcn_ip_version >= VCN_2_0_0;
    cap->formats.y8_u8_v8_440 = info->vcn_ip_version >= VCN_2_0_0;
-   cap->formats.r8g8b8a8 = info->vcn_ip_version == VCN_4_0_3 || info->vcn_ip_version == VCN_5_0_1;
-   cap->formats.a8r8g8b8 = info->vcn_ip_version == VCN_4_0_3 || info->vcn_ip_version == VCN_5_0_1;
-   cap->formats.r8_g8_b8 = info->vcn_ip_version == VCN_4_0_3 || info->vcn_ip_version == VCN_5_0_1;
+   cap->formats.r8g8b8a8 = info->vcn_ip_version == VCN_4_0_3 || info->vcn_ip_version == VCN_5_0_1 ||
+                           info->vcn_ip_version == VCN_5_0_2;
+   cap->formats.a8r8g8b8 = info->vcn_ip_version == VCN_4_0_3 || info->vcn_ip_version == VCN_5_0_1 ||
+                           info->vcn_ip_version == VCN_5_0_2;
+   cap->formats.r8_g8_b8 = info->vcn_ip_version == VCN_4_0_3 || info->vcn_ip_version == VCN_5_0_1 ||
+                           info->vcn_ip_version == VCN_5_0_2;
 }
 
 static void

src/amd/common/amd_family.h
@@ -224,6 +224,7 @@ enum vcn_version{
 
    VCN_5_0_0,
    VCN_5_0_1,
+   VCN_5_0_2,
    VCN_5_3_0,
 };
 

commit e3ac5454af48e9e5c78b68c933c15481206b9b5f

ac/gpu_info: fix detecting AMD BC250 as RDNA1
Previously, ac_fill_hw_ip_info() was skipped for unexisting queues
but since the commit mentioned below, they are no longer skipped.

That was causing AMD BC250 (GFX1013) to be detected as GFX10.3 instead
of GFX10.

Fixes: ddbdc982634 ("radeonsi/vcn: Add vcn_5_0_2 support")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43465>

src/amd/common/ac_gpu_info.c
@@ -505,7 +505,8 @@ ac_fill_hw_ip_info(struct radeon_info *info, const struct drm_amdgpu_info_device
       if (device_info->family == FAMILY_NV &&
             (ASICREV_IS(device_info->external_rev, NAVI10) ||
             ASICREV_IS(device_info->external_rev, NAVI12) ||
-            ASICREV_IS(device_info->external_rev, NAVI14)))
+            ASICREV_IS(device_info->external_rev, NAVI14) ||
+            ASICREV_IS(device_info->external_rev, GFX1013)))
          info->ip[AMD_IP_GFX].ver_minor = info->ip[AMD_IP_COMPUTE].ver_minor = 1;
       else if (device_info->family == FAMILY_NV ||
                device_info->family == FAMILY_VGH ||