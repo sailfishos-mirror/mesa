b057153fb11d130c5efe665cd0bc04bd1d9be737
Marc Alcala Prieto <marc.alcalaprieto@arm.com>
2026-08-06 08:53:27 +0200

    panfrost/model: Fix v11 model rates
    Make use of MODEL_RATES_X and set the right rates.
    
    Fixes: 73210bf69df ("panfrost: enable v11 gpu models")
    Reviewed-by: Jakob Sinclair <jakob.sinclair@arm.com>
    Acked-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
    Acked-by: Erik Faye-Lund <erik.faye-lund@arm.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44111>

--- src/panfrost/model/pan_model.c
+++ src/panfrost/model/pan_model.c
@@ -101,9 +101,9 @@ const struct pan_model pan_model_list[] = {
    VALHALL_MODEL(PAN_PROD_ID(10, 12, 4), 4, "G310v5",   "G710", MODEL_ANISO(ALL),  MODEL_TB_SIZES(32768, 16384),
                                               MODEL_RATES_X(4, 8,  8,  64,  64, 16)),
    VALHALL_MODEL(PAN_PROD_ID(11, 8, 2), 4,  "G715",   "G715", MODEL_ANISO(ALL),  MODEL_TB_SIZES(32768, 16384),
-                                              MODEL_RATES(4, 8,  128)),
+                                              MODEL_RATES_X(4, 8, 32,  64, 128, 16)),
    VALHALL_MODEL(PAN_PROD_ID(11, 8, 3), 4,  "G615",   "G715", MODEL_ANISO(ALL),  MODEL_TB_SIZES(32768, 16384),
-                                              MODEL_RATES(4, 8,  128)),
+                                              MODEL_RATES_X(4, 8, 32,  64, 128, 16)),
 
    FIFTHGEN_MODEL(PAN_PROD_ID(12, 8, 0), 4, "G720",  "G720", MODEL_ANISO(ALL),  MODEL_TB_SIZES(65536, 32768),
                                               MODEL_RATES_X(4, 8, 32,  64, 128, 8)),
