sailfishos-mirror/mesa

commit fd11e4b4d31adde062b9f19a8eec82f06f832f00

intel: switch shader hash to 64bit value
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41748>

src/intel/dev/intel_debug.c
@@ -214,7 +214,7 @@ uint64_t intel_debug_batch_frame_stop = -1;
 
 uint32_t intel_debug_bkp_before_draw_count = 0;
 uint32_t intel_debug_bkp_after_draw_count = 0;
-uint32_t intel_shader_dump_filter = 0;
+uint64_t intel_shader_dump_filter = 0;
 
 uint32_t intel_debug_bkp_before_dispatch_count = 0;
 uint32_t intel_debug_bkp_after_dispatch_count = 0;

src/gallium/drivers/iris/iris_context.h
@@ -549,7 +549,7 @@ struct iris_uncompiled_shader {
    unsigned char nir_blake3[BLAKE3_KEY_LEN];
 
    /* Hash value based on shader source program */
-   unsigned source_hash;
+   uint64_t source_hash;
 
    unsigned program_id;
 

src/gallium/drivers/iris/iris_program.c
@@ -2177,14 +2177,14 @@ iris_compile_tcs(struct iris_screen *screen,
       options = screen->elk->nir_options[MESA_SHADER_TESS_CTRL];
    struct elk_tcs_prog_key elk_key = iris_to_elk_tcs_key(screen, key);
 #endif
-   uint32_t source_hash;
+   uint64_t source_hash;
 
    if (ish) {
       nir = nir_shader_clone(mem_ctx, ish->nir);
       source_hash = ish->source_hash;
    } else {
       nir = iris_create_passthrough_tcs(mem_ctx, options, key);
-      source_hash = *(uint32_t*)nir->info.source_blake3;
+      source_hash = *(uint64_t*)nir->info.source_blake3;
    }
 
    debug_archiver *debug_archiver =

src/intel/compiler/brw/brw_compiler.h
@@ -496,7 +496,7 @@ struct brw_stage_prog_data {
    /** Number of GRF registers used. */
    unsigned grf_used;
 
-   uint32_t source_hash;
+   uint64_t source_hash;
 };
 
 /**
@@ -1319,7 +1319,7 @@ struct brw_compile_params {
 
    uint64_t debug_flag;
 
-   uint32_t source_hash;
+   uint64_t source_hash;
 
    debug_archiver *archiver;
 };

src/intel/compiler/brw/brw_generator.cpp
@@ -1377,7 +1377,7 @@ brw_generator::generate_code(const brw_shader &s,
 
       for (unsigned i = 0; i < ARRAY_SIZE(files); i++) {
          if (!files[i]) continue;
-         fprintf(files[i], "Native code for %s (src_hash 0x%08x) (blake3 %s)\n"
+         fprintf(files[i], "Native code for %s (src_hash 0x%016" PRIx64 ") (blake3 %s)\n"
                  "SIMD%d shader: %d instructions. %d loops. %u cycles. "
                  "%d:%d spills:fills, %u sends, "
                  "scheduled with mode %s. "

src/intel/dev/intel_debug.h
@@ -143,7 +143,7 @@ extern uint32_t intel_debug_bkp_before_dispatch_count;
 extern uint32_t intel_debug_bkp_after_dispatch_count;
 extern uint64_t intel_debug_batch_frame_start;
 extern uint64_t intel_debug_batch_frame_stop;
-extern uint32_t intel_shader_dump_filter;
+extern uint64_t intel_shader_dump_filter;
 
 #define INTEL_SIMD(type, size)        (!!(intel_simd & (DEBUG_ ## type ## _SIMD ## size)))
 

src/intel/ds/intel_tracepoints.py
@@ -61,7 +61,7 @@ def define_tracepoints(args):
         for arg in tp_args:
             # Manually handle display_as_hex by modifying the format
             if "hash" in arg.var:
-                arg = Arg(type=arg.type, var=arg.var, c_format='%#x')  # Convert to hex format
+                arg = Arg(type=arg.type, var=arg.var, c_format='0x%" PRIx64 "')  # Convert to hex format
             processed_args.append(arg)
 
         Tracepoint('intel_begin_{0}'.format(name),
@@ -185,42 +185,42 @@ def define_tracepoints(args):
     # Various draws/dispatch, Anv & Iris
     draw_tp('draw',
             tp_args=[Arg(type='uint32_t', var='count', c_format='%u'),
-                     Arg(type='uint32_t', var='vs_hash', c_format='%u'),
-                     Arg(type='uint32_t', var='fs_hash', c_format='%u')])
+                     Arg(type='uint64_t', var='vs_hash'),
+                     Arg(type='uint64_t', var='fs_hash')])
     draw_tp('draw_multi',
             tp_args=[Arg(type='uint32_t', var='count', c_format='%u'),
-                     Arg(type='uint32_t', var='vs_hash', c_format='%u'),
-                     Arg(type='uint32_t', var='fs_hash', c_format='%u')])
+                     Arg(type='uint64_t', var='vs_hash'),
+                     Arg(type='uint64_t', var='fs_hash')])
     draw_tp('draw_indexed',
             tp_args=[Arg(type='uint32_t', var='count', c_format='%u'),
-                     Arg(type='uint32_t', var='vs_hash', c_format='%u'),
-                     Arg(type='uint32_t', var='fs_hash', c_format='%u')])
+                     Arg(type='uint64_t', var='vs_hash'),
+                     Arg(type='uint64_t', var='fs_hash')])
     draw_tp('draw_indexed_multi',
             tp_args=[Arg(type='uint32_t', var='count', c_format='%u'),
-                     Arg(type='uint32_t', var='vs_hash', c_format='%u'),
-                     Arg(type='uint32_t', var='fs_hash', c_format='%u')])
+                     Arg(type='uint64_t', var='vs_hash'),
+                     Arg(type='uint64_t', var='fs_hash')])
     draw_tp('draw_indirect_byte_count',
             tp_args=[Arg(type='uint32_t', var='instance_count', c_format='%u'),
-                     Arg(type='uint32_t', var='vs_hash', c_format='%u'),
-                     Arg(type='uint32_t', var='fs_hash', c_format='%u')])
+                     Arg(type='uint64_t', var='vs_hash'),
+                     Arg(type='uint64_t', var='fs_hash')])
     draw_tp('draw_indirect',
             tp_args=[Arg(type='uint32_t', var='draw_count', c_format='%u'),
-                     Arg(type='uint32_t', var='vs_hash', c_format='%u'),
-                     Arg(type='uint32_t', var='fs_hash', c_format='%u')])
+                     Arg(type='uint64_t', var='vs_hash'),
+                     Arg(type='uint64_t', var='fs_hash')])
     draw_tp('draw_indexed_indirect',
             tp_args=[Arg(type='uint32_t', var='draw_count', c_format='%u'),
-                     Arg(type='uint32_t', var='vs_hash', c_format='%u'),
-                     Arg(type='uint32_t', var='fs_hash', c_format='%u')])
+                     Arg(type='uint64_t', var='vs_hash'),
+                     Arg(type='uint64_t', var='fs_hash')])
     draw_tp('draw_indirect_count',
             tp_args=[Arg(type='uint32_t', var='draw_count', c_format='%u',
                          is_indirect=True),
-                     Arg(type='uint32_t', var='vs_hash', c_format='%u'),
-                     Arg(type='uint32_t', var='fs_hash', c_format='%u')])
+                     Arg(type='uint64_t', var='vs_hash'),
+                     Arg(type='uint64_t', var='fs_hash')])
     draw_tp('draw_indexed_indirect_count',
             tp_args=[Arg(type='uint32_t', var='draw_count', c_format='%u',
                          is_indirect=True),
-                     Arg(type='uint32_t', var='vs_hash', c_format='%u'),
-                     Arg(type='uint32_t', var='fs_hash', c_format='%u')])
+                     Arg(type='uint64_t', var='vs_hash'),
+                     Arg(type='uint64_t', var='fs_hash')])
 
     draw_tp('draw_mesh',
             tp_args=[Arg(type='uint32_t', var='group_x', c_format='%u'),
@@ -236,14 +236,15 @@ def define_tracepoints(args):
                  tp_args=[Arg(type='uint32_t', var='group_x', c_format='%u'),
                           Arg(type='uint32_t', var='group_y', c_format='%u'),
                           Arg(type='uint32_t', var='group_z', c_format='%u'),
-                          Arg(type='uint32_t', var='cs_hash', c_format='%u')],
+                          Arg(type='uint64_t', var='cs_hash')],
                  compute=True)
+
 
     begin_end_tp('compute_indirect',
                  tp_args=[ArgStruct(type='VkDispatchIndirectCommand', var='size',
                           is_indirect=True, c_format="%ux%ux%u",
                           fields=['x', 'y', 'z']),
-                          Arg(type='uint32_t', var='cs_hash', c_format='%u')],
+                          Arg(type='uint64_t', var='cs_hash')],
                  compute=True)
 
     # Used to identify copies generated by utrace

src/intel/vulkan/anv_shader.c
@@ -54,7 +54,7 @@ anv_device_dump_shader_variant(struct anv_device *device,
 
    if (device->shader_dump.archive != NULL) {
       char filename[80];
-      snprintf(filename, sizeof(filename), "0x%08x-%s%s",
+      snprintf(filename, sizeof(filename), "0x%016"PRIx64"-%s%s",
                shader->prog_data->source_hash,
                _mesa_shader_stage_to_abbrev(shader->vk.stage),
                variant);

commit 3010172160d3fcfe8c2d81c87829727f4c8e0aa9

intel: Preserve 64-bit hashes in shader dump filter
Fixes: fd11e4b4d31a ("intel: switch shader hash to 64bit value")
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43608>

src/intel/compiler/brw/brw_private.h
@@ -171,7 +171,7 @@ int brw_simd_select_for_workgroup_size(const struct intel_device_info *devinfo,
                                        const struct brw_cs_prog_data *prog_data,
                                        const unsigned *sizes);
 
-bool brw_should_print_shader(const nir_shader *shader, uint64_t debug_flag, uint32_t source_hash);
+bool brw_should_print_shader(const nir_shader *shader, uint64_t debug_flag, uint64_t source_hash);
 
 void brw_prog_data_init(struct brw_stage_prog_data *prog_data,
                         const struct brw_compile_params *params);

src/intel/compiler/brw/brw_shader.cpp
@@ -1022,7 +1022,7 @@ brw_shader_phase_update(brw_shader &s, enum brw_shader_phase phase)
    brw_validate(s);
 }
 
-bool brw_should_print_shader(const nir_shader *shader, uint64_t debug_flag, uint32_t source_hash)
+bool brw_should_print_shader(const nir_shader *shader, uint64_t debug_flag, uint64_t source_hash)
 {
    if (intel_shader_dump_filter && intel_shader_dump_filter != source_hash) {
       return false;

src/intel/dev/intel_debug.c
@@ -242,7 +242,7 @@ process_intel_debug_variable_once(void)
       debug_get_num_option("INTEL_DEBUG_BKP_AFTER_DRAW_COUNT", 0);
 
    intel_shader_dump_filter =
-      debug_get_num_option("INTEL_SHADER_DUMP_FILTER", 0);
+      debug_get_unsigned_option("INTEL_SHADER_DUMP_FILTER", 0);
 
    intel_debug_bkp_before_dispatch_count =
       debug_get_num_option("INTEL_DEBUG_BKP_BEFORE_DISPATCH_COUNT", 0);