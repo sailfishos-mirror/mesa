sailfishos-mirror/mesa

commit f16bbbf3fcd37ea774d2eee161140cb2aa3ba8c8

wsi/metal: always force 3 drawables for smoother presentation
Reviewed-by: squidbus <squidbus@proton.me>
Signed-off-by: Aitor Camacho <aitor@lunarg.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42811>

src/vulkan/wsi/wsi_common_metal_layer.m
@@ -166,7 +166,8 @@ wsi_metal_layer_configure(const CAMetalLayer *metal_layer,
       /* So we can blit to the drawable */
       metal_layer.framebufferOnly = NO;
 
-      metal_layer.maximumDrawableCount = image_count;
+      /* Force recommended 3 drawables for smoother presentation */
+      metal_layer.maximumDrawableCount = 3u;
       metal_layer.drawableSize = (CGSize){.width = width, .height = height};
       metal_layer.opaque = enable_opaque;
       metal_layer.displaySyncEnabled = !enable_immediate;

commit cd6e6adf19961c5873feb8c81458d095b6d0dec7

wsi/metal: Correctly report minImageCount instead of forcing 3 drawables always
Fixes: f16bbbf3fcd ("wsi/metal: always force 3 drawables for smoother presentation")

Reviewed-by: squidbus <squidbus@proton.me>
Signed-off-by: Aitor Camacho <aitor@lunarg.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43564>

src/vulkan/wsi/wsi_common_metal.c
@@ -59,7 +59,10 @@ wsi_metal_surface_get_capabilities(VkIcdSurfaceBase *surface,
    if (!caps->surfaceCapabilities.currentExtent.width && !caps->surfaceCapabilities.currentExtent.height)
       caps->surfaceCapabilities.currentExtent.width = caps->surfaceCapabilities.currentExtent.height = UINT32_MAX;
 
-   caps->surfaceCapabilities.minImageCount = 2;
+   /* Force recommended 3 drawables, otherwise the OS frame pacing may slow
+    * down presentation.
+    */
+   caps->surfaceCapabilities.minImageCount = 3;
    caps->surfaceCapabilities.maxImageCount = 3;
 
    caps->surfaceCapabilities.minImageExtent = (VkExtent2D) { 1, 1 };

src/vulkan/wsi/wsi_common_metal_layer.m
@@ -165,8 +165,7 @@ wsi_metal_layer_configure(const CAMetalLayer *metal_layer,
       metal_layer.allowsNextDrawableTimeout = YES;
       metal_layer.framebufferOnly = framebuffer_only;
 
-      /* Force recommended 3 drawables for smoother presentation */
-      metal_layer.maximumDrawableCount = 3u;
+      metal_layer.maximumDrawableCount = image_count;
       metal_layer.drawableSize = (CGSize){.width = width, .height = height};
       metal_layer.opaque = enable_opaque;
       metal_layer.displaySyncEnabled = !enable_immediate;