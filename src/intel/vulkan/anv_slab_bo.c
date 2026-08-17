d4c0389ea30de3601eb690ba5d93cb1c46f419c2
José Roberto de Souza <jose.souza@intel.com>
2026-08-17 10:05:28 -0700

    anv: Allow bos with ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE to be allocated in slab
    There is no reason to not support it, this flag would be supported by
    ANV_BO_SLAB_HEAP_LMEM_SMEM.
    
    Reviewed-by: Ivan Briano <ivan.briano@intel.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43829>

--- src/intel/vulkan/anv_slab_bo.c
+++ src/intel/vulkan/anv_slab_bo.c
@@ -37,7 +37,6 @@ anv_bo_alloc_flags_to_slab_heap(struct anv_device *device,
                                            ANV_BO_ALLOC_FIXED_ADDRESS |
                                            ANV_BO_ALLOC_CLIENT_VISIBLE_ADDRESS |
                                            ANV_BO_ALLOC_DESCRIPTOR_POOL |
-                                           ANV_BO_ALLOC_LOCAL_MEM_CPU_VISIBLE |
                                            ANV_BO_ALLOC_SCANOUT |
                                            ANV_BO_ALLOC_PROTECTED |
                                            ANV_BO_ALLOC_DYNAMIC_VISIBLE_POOL |
