da908233d84aa05f058b3115ba49258f7239543e
Rob Clark <rob.clark@oss.qualcomm.com>
2026-08-17 08:04:22 -0700

    ci: Uprev OpenCL-CTS
    Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43824>

--- .gitlab-ci/container/build-opencl-cts.sh
+++ .gitlab-ci/container/build-opencl-cts.sh
@@ -11,7 +11,7 @@ section_start opencl-cts "Building OpenCL-CTS"
 
 ci_tag_build_time_check "OPENCL_CTS_TAG"
 
-REV="349973878bd92be01eefc489206d73bdbe5e7aed"
+REV="43111c0c7c7ffcdbacc5775a6baff8016bada9e2"
 MESA_SOURCE_DIR="$PWD"
 OPENCL_CTS_S3_ARTIFACT="opencl-cts.tar.zst"
 ARTIFACT_PATH="${DATA_STORAGE_PATH}/opencl-cts/${DEBIAN_ARCH}/${OPENCL_CTS_TAG}.tar.zst"
