72b7fcb67c570c4600e20fa8fcceb66170a21bb2
David Rosca <david.rosca@amd.com>
2026-08-18 10:39:54 +0200

    radeonsi/mm: Set PIPE_BIND_SHARED for video buffers
    This avoids reallocation on export. Also avoids suballocation
    and BO reuse to ensure new surface is zeroed on recent kernel.
    
    Reviewed-by: Benjamin Cheng <benjamin.cheng@amd.com>
    Reviewed-by: Marek Olšák <maraeo@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43837>

--- src/gallium/drivers/radeonsi/mm/si_video.c
+++ src/gallium/drivers/radeonsi/mm/si_video.c
@@ -169,7 +169,6 @@ struct pipe_video_buffer *si_video_buffer_create_with_modifiers(struct pipe_cont
 struct pipe_video_buffer *si_video_buffer_create(struct pipe_context *pipe,
                                                  const struct pipe_video_buffer *tmpl)
 {
-   struct si_screen *sscreen = (struct si_screen *)pipe->screen;
    struct pipe_video_buffer vidbuf = *tmpl;
    uint64_t *modifiers = NULL;
    int modifiers_count = 0;
@@ -177,13 +176,9 @@ struct pipe_video_buffer *si_video_buffer_create(struct pipe_context *pipe,
    if (vidbuf.bind & (PIPE_BIND_VIDEO_DECODE_DPB | PIPE_BIND_VIDEO_ENCODE_DPB))
       return vl_video_buffer_create_as_resource(pipe, &vidbuf, NULL, 0);
 
-   /* Ensure resource_get_handle doesn't need to reallocate the texture
-    * which would fail with compute-only context.
-    * This is only needed with AMD_DEBUG=tmz because in this case the frontend
-    * is not aware of the buffer being created as protected.
-    */
-   if (sscreen->debug_flags & DBG(TMZ) && !(vidbuf.bind & PIPE_BIND_PROTECTED))
-      vidbuf.bind |= PIPE_BIND_SHARED;
+   /* Avoid reallocation when exporting the surface, avoid suballocation and
+    * BO reuse to ensure new surface is always zeroed. */
+   vidbuf.bind |= PIPE_BIND_SHARED;
 
    if (pipe->screen->resource_create_with_modifiers && !(vidbuf.bind & PIPE_BIND_LINEAR)) {
       pipe->screen->query_dmabuf_modifiers(pipe->screen, vidbuf.buffer_format, 0,
