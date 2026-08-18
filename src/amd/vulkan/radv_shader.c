sailfishos-mirror/mesa

commit fb774cf7a1ee4640b820427384e4085b2e6f1086

radv: run nir_opt_reassociate_for_fma for VS/GS too
Now that radv always treats position as invariant, this should be
less problematic.

Foz-DB Navi48:
Totals from 11069 (5.29% of 209079) affected shaders:
MaxWaves: 312992 -> 311956 (-0.33%); split: +0.05%, -0.39%
Instrs: 6360079 -> 6307746 (-0.82%); split: -0.85%, +0.02%
CodeSize: 32300388 -> 32205652 (-0.29%); split: -0.36%, +0.07%
VGPRs: 602376 -> 606444 (+0.68%); split: -0.22%, +0.89%
Latency: 40077440 -> 39912991 (-0.41%); split: -0.47%, +0.06%
InvThroughput: 6556737 -> 6524683 (-0.49%); split: -0.50%, +0.01%
VClause: 125675 -> 125640 (-0.03%); split: -0.40%, +0.37%
SClause: 123594 -> 123433 (-0.13%); split: -0.16%, +0.03%
Copies: 434459 -> 436258 (+0.41%); split: -0.86%, +1.27%
Branches: 106401 -> 106424 (+0.02%)
PreSGPRs: 406849 -> 407034 (+0.05%); split: -0.00%, +0.05%
PreVGPRs: 444213 -> 448907 (+1.06%); split: -0.06%, +1.12%
VALU: 3594645 -> 3548684 (-1.28%); split: -1.30%, +0.02%
SALU: 907910 -> 907459 (-0.05%); split: -0.06%, +0.01%

Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42820>

src/amd/vulkan/radv_shader.c
@@ -282,8 +282,7 @@ radv_optimize_nir_algebraic_early(nir_shader *nir)
 void
 radv_optimize_nir_algebraic_late(nir_shader *nir)
 {
-   if (nir->info.stage != MESA_SHADER_VERTEX && nir->info.stage != MESA_SHADER_GEOMETRY)
-      NIR_PASS(_, nir, nir_opt_reassociate_for_fma);
+   NIR_PASS(_, nir, nir_opt_reassociate_for_fma);
 
    /* Do late algebraic optimization to turn add(a,
     * neg(b)) back into subs, then the mandatory cleanup

src/gallium/drivers/zink/ci/traces-zink.yml
@@ -11,9 +11,9 @@ traces:
     gl-zink-anv-tgl:
       checksum: 9dc55abb7806f0116770a5a1a389c342
     zink-radv-vangogh:
-      checksum: 06fab4fbf6c48a03771524785af94469
+      checksum: 6bff1b79451bae925314952a8e5303cf
     zink-radv-gfx1201:
-      checksum: 06fab4fbf6c48a03771524785af94469
+      checksum: 6bff1b79451bae925314952a8e5303cf
 
   behdad-glyphy/glyphy-v2.trace:
     gl-zink-anv-adl:
@@ -273,9 +273,9 @@ traces:
     gl-zink-anv-tgl:
       checksum: b36c25e52624cbf8dab73b6acecb8e84
     zink-radv-vangogh:
-      checksum: efafc2996298a0c3f91a8dd0a718b32d
+      checksum: 300b70c951f44e6d321a97626ca52b8a
     zink-radv-gfx1201:
-      checksum: efafc2996298a0c3f91a8dd0a718b32d
+      checksum: 300b70c951f44e6d321a97626ca52b8a
 
   pathfinder/demo-v2.trace:
     gl-zink-anv-adl:
@@ -436,7 +436,7 @@ traces:
     gl-zink-anv-tgl:
       checksum: 54149c9a6560176436600bfe8fe2ae1f
     zink-radv-vangogh:
-      checksum: 9288cc3f5ef6ba6e45e85ef29aaea259
+      checksum: 051011af02dd7972f55db7d01d38a200
     zink-radv-gfx1201:
       checksum: 94e49e3e92171c6a1cb2a2ba3ef8c023
 

commit 1a12d7188b46703d433f4d909b0e8e0d6c5bbe5d

radv: disable nir_opt_reassociate_for_fma for VS if it's not the last pre FS shader
Invariant position doesn't cover generic VS outputs used
to compute position in later stages.

Foz-DB Navi48:
Totals from 1382 (0.66% of 209079) affected shaders:
MaxWaves: 39716 -> 39784 (+0.17%); split: +0.23%, -0.06%
Instrs: 1073096 -> 1078247 (+0.48%); split: -0.01%, +0.49%
CodeSize: 5312796 -> 5326488 (+0.26%); split: -0.04%, +0.30%
VGPRs: 78816 -> 78252 (-0.72%); split: -0.76%, +0.05%
Latency: 9499280 -> 9516170 (+0.18%); split: -0.01%, +0.18%
InvThroughput: 1859682 -> 1863445 (+0.20%); split: -0.00%, +0.20%
VClause: 14081 -> 14136 (+0.39%); split: -0.01%, +0.40%
SClause: 15515 -> 15566 (+0.33%); split: -0.05%, +0.38%
Copies: 74653 -> 74788 (+0.18%); split: -0.22%, +0.40%
PreSGPRs: 53221 -> 53213 (-0.02%)
PreVGPRs: 62840 -> 61985 (-1.36%); split: -1.38%, +0.02%
VALU: 513364 -> 517815 (+0.87%); split: -0.01%, +0.88%
SALU: 247709 -> 247755 (+0.02%); split: -0.00%, +0.02%

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/16049
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/16086
Fixes: fb774cf7a1e ("radv: run nir_opt_reassociate_for_fma for VS/GS too")
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43761>

src/amd/vulkan/radv_shader.c
@@ -282,7 +282,11 @@ radv_optimize_nir_algebraic_early(nir_shader *nir)
 void
 radv_optimize_nir_algebraic_late(nir_shader *nir)
 {
-   NIR_PASS(_, nir, nir_opt_reassociate_for_fma);
+   /* Invariant position doesn't cover generic VS outputs used
+    * to compute position in later stages.
+    */
+   if (nir->info.stage != MESA_SHADER_VERTEX || nir->info.next_stage == MESA_SHADER_FRAGMENT)
+      NIR_PASS(_, nir, nir_opt_reassociate_for_fma);
 
    /* Do late algebraic optimization to turn add(a,
     * neg(b)) back into subs, then the mandatory cleanup

src/gallium/drivers/zink/ci/traces-zink.yml
@@ -11,9 +11,9 @@ traces:
     gl-zink-anv-tgl:
       checksum: 9dc55abb7806f0116770a5a1a389c342
     zink-radv-vangogh:
-      checksum: 6bff1b79451bae925314952a8e5303cf
+      checksum: 06fab4fbf6c48a03771524785af94469
     zink-radv-gfx1201:
-      checksum: 6bff1b79451bae925314952a8e5303cf
+      checksum: 06fab4fbf6c48a03771524785af94469
 
   behdad-glyphy/glyphy-v2.trace:
     gl-zink-anv-adl:
@@ -273,9 +273,9 @@ traces:
     gl-zink-anv-tgl:
       checksum: b36c25e52624cbf8dab73b6acecb8e84
     zink-radv-vangogh:
-      checksum: 300b70c951f44e6d321a97626ca52b8a
+      checksum: efafc2996298a0c3f91a8dd0a718b32d
     zink-radv-gfx1201:
-      checksum: 300b70c951f44e6d321a97626ca52b8a
+      checksum: efafc2996298a0c3f91a8dd0a718b32d
 
   pathfinder/demo-v2.trace:
     gl-zink-anv-adl:
@@ -424,7 +424,7 @@ traces:
     gl-zink-anv-tgl:
       checksum: 54149c9a6560176436600bfe8fe2ae1f
     zink-radv-vangogh:
-      checksum: 051011af02dd7972f55db7d01d38a200
+      checksum: 9288cc3f5ef6ba6e45e85ef29aaea259
     zink-radv-gfx1201:
       checksum: 94e49e3e92171c6a1cb2a2ba3ef8c023
 