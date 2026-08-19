joshuaswarren/mesa-1

radv: add a workaround for broken multi viewport with UE5
UE5 (reproduced with 5.4 but it seems all 5.x versions are affected)
implements multi viewport incorrectly. See the comment below for more
explanations.

This has been reported in the SteamOS tracker here
https://gitlab.steamos.cloud/holo-team/tasks/-/work_items/2253.

This has been uncovered since 151b8104222 ("radv: fix emitting dynamic
viewports/scissors when the count is static") but it's definitely an
application bug.

Cc: mesa-stable
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43505>

src/amd/vulkan/radv_device.c
@@ -790,6 +790,9 @@ init_app_workarounds_entrypoints(struct radv_device *device, struct dispatch_tab
       SET_ENTRYPOINT(strange_brigade, CmdPipelineBarrier2);
    } else if (!strcmp(pdev->drirc.debug.app_layer, "gfxbench5")) {
       SET_ENTRYPOINT(gfxbench5, CmdPipelineBarrier2);
+   } else if (!strcmp(pdev->drirc.debug.app_layer, "ue5")) {
+      SET_ENTRYPOINT(ue5, CmdSetViewport);
+      SET_ENTRYPOINT(ue5, CmdSetScissor);
    }
 #undef SET_ENTRYPOINT
 

sailfishos-mirror/mesa

This has been uncovered since 9a07ccbc897 ("radv: fix emitting dynamic
(cherry picked from commit 0df67d43c8159854ef5e1b45155542a977666b72)
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43884>

src/amd/vulkan/radv_device.c
@@ -792,6 +792,9 @@ init_app_workarounds_entrypoints(struct radv_device *device, struct dispatch_tab
       SET_ENTRYPOINT(strange_brigade, CmdPipelineBarrier2);
    } else if (!strcmp(instance->drirc.debug.app_layer, "gfxbench5")) {
       SET_ENTRYPOINT(gfxbench5, CmdPipelineBarrier2);
+   } else if (!strcmp(instance->drirc.debug.app_layer, "ue5")) {
+      SET_ENTRYPOINT(ue5, CmdSetViewport);
+      SET_ENTRYPOINT(ue5, CmdSetScissor);
    }
 #undef SET_ENTRYPOINT
 