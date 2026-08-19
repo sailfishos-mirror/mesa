sailfishos-mirror/mesa

commit d02b25157c68804c4e6da613faf0f38a3bcca6d3

vulkan/android: force linear for mutable format
We need a better gralloc private query to forward the mutable bit along
with the format list for a private vendor usage bit, and leave the
decision to gralloc. For now, resolve mutable bit to CPU_WRITE_RARELY to
implicitly force LINEAR.

Cc: mesa-stable
Reviewed-by: Valentine Burley <valentine.burley@collabora.com>
Reviewed-by: Roman Stratiienko <r.stratiienko@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42444>

src/vulkan/runtime/vk_android.c
@@ -769,6 +769,14 @@ vk_image_usage_to_ahb_usage(const VkImageCreateFlags vk_create,
    if (vk_create & VK_IMAGE_CREATE_PROTECTED_BIT)
       ahb_usage |= AHARDWAREBUFFER_USAGE_PROTECTED_CONTENT;
 
+   /* XXX We need a better gralloc private query to forward the mutable bit
+    * along with the format list for a private vendor usage bit, and leave the
+    * decision to gralloc. For now, resolve mutable bit to CPU_WRITE_RARELY to
+    * implicitly force LINEAR.
+    */
+   if (vk_create & VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT)
+      ahb_usage |= AHARDWAREBUFFER_USAGE_CPU_WRITE_RARELY;
+
    /* No usage bits set - set at least one GPU usage. */
    if (ahb_usage == 0)
       ahb_usage = AHARDWAREBUFFER_USAGE_GPU_SAMPLED_IMAGE;

commit 491bb61a48c0b8d4d1084e944989b6977a76ae0d

vulkan/android: undo forcing linear for mutable format
The prior fix is too aggressive and has regressed ANGLE performance.

Fixes: d02b25157c6 ("vulkan/android: force linear for mutable format")
Reviewed-by: Juston Li <justonli@google.com>
Reviewed-by: Valentine Burley <valentine.burley@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43758>

src/vulkan/runtime/vk_android.c
@@ -806,14 +806,6 @@ vk_image_usage_to_ahb_usage(const VkImageCreateFlags2KHR vk_create,
    if (vk_create & VK_IMAGE_CREATE_PROTECTED_BIT)
       ahb_usage |= AHARDWAREBUFFER_USAGE_PROTECTED_CONTENT;
 
-   /* XXX We need a better gralloc private query to forward the mutable bit
-    * along with the format list for a private vendor usage bit, and leave the
-    * decision to gralloc. For now, resolve mutable bit to CPU_WRITE_RARELY to
-    * implicitly force LINEAR.
-    */
-   if (vk_create & VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT)
-      ahb_usage |= AHARDWAREBUFFER_USAGE_CPU_WRITE_RARELY;
-
    /* No usage bits set - set at least one GPU usage. */
    if (ahb_usage == 0)
       ahb_usage = AHARDWAREBUFFER_USAGE_GPU_SAMPLED_IMAGE;