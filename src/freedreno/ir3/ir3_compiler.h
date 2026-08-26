bminor/mesa-mesa

commit d1df3b4e392d9ec167b6639a77a735b01eff60a7

ir3: Limit CS lock/unlock quirk
We don't need this universally for everything >= gen7.

Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38450>

src/freedreno/ir3/ir3_compiler.h
@@ -210,6 +210,9 @@ struct ir3_compiler {
    /* Whether half register shared->non-shared moves are broken. */
    bool mov_half_shared_quirk;
 
+   /* Is lock/unlock sequence needed for CS? */
+   bool cs_lock_unlock_quirk;
+
    /* Whether movs is supported for subgroupBroadcast. */
    bool has_movs;
 

src/freedreno/ir3/ir3_compiler_nir.c
@@ -6035,7 +6035,7 @@ ir3_compile_shader_nir(struct ir3_compiler *compiler,
     */
    IR3_PASS(ir, ir3_legalize, so, &max_bary);
 
-   if (ctx->compiler->gen >= 7 && so->type == MESA_SHADER_COMPUTE) {
+   if (ctx->compiler->cs_lock_unlock_quirk && so->type == MESA_SHADER_COMPUTE) {
       struct ir3_instruction *end = ir3_find_end(so->ir);
       struct ir3_instruction *lock =
          ir3_build_instr(&ctx->build, OPC_LOCK, 0, 0);

src/freedreno/common/freedreno_dev_info.h
@@ -375,6 +375,9 @@ struct fd_dev_info {
        */
       bool reading_shading_rate_requires_smask_quirk;
 
+      /* Is lock/unlock sequence needed at end of compute shader? */
+      bool cs_lock_unlock_quirk;
+
       /* Whether the ray_intersection instruction is present. */
       bool has_ray_intersection;
 

src/freedreno/common/freedreno_devices.py
@@ -994,6 +994,7 @@ a7xx_gen1 = GPUProps(
         fs_must_have_non_zero_constlen_quirk = True,
         enable_tp_ubwc_flag_hint = True,
         reading_shading_rate_requires_smask_quirk = True,
+        cs_lock_unlock_quirk = True,
     )
 
 a7xx_gen2 = GPUProps(

sailfishos-mirror/mesa

commit 45e56e64cac5ea0bf732ad06f552c1a0d8ea3a25

freedreno/ir3: Actually pass cs_lock_unlock_quirk into the compiler.
Fixes: d1df3b4e392d ("ir3: Limit CS lock/unlock quirk")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44003>

src/freedreno/ir3/ir3_compiler.h
@@ -179,9 +179,6 @@ struct ir3_compiler {
    /* Whether SSBOs have descriptors for sampling with ISAM */
    bool has_isam_ssbo;
 
-   /* Is lock/unlock sequence needed for CS? */
-   bool cs_lock_unlock_quirk;
-
    /* True if the shfl instruction is supported. Needed for subgroup rotate and
     * (more efficient) shuffle.
     */

src/freedreno/ir3/ir3_compiler_nir.c
@@ -6193,7 +6193,7 @@ ir3_compile_shader_nir(struct ir3_compiler *compiler,
     */
    IR3_PASS(ir, ir3_legalize, so, &max_bary);
 
-   if (ctx->compiler->cs_lock_unlock_quirk && ir3_shader_compute(so)) {
+   if (ctx->compiler->info->props.cs_lock_unlock_quirk && ir3_shader_compute(so)) {
       struct ir3_instruction *end = ir3_find_end(so->ir);
       struct ir3_instruction *lock =
          ir3_build_instr(&ctx->build, OPC_LOCK, 0, 0);