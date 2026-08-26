d10ec46ad5dfbb69e5f2c5f6aad0c7d8483760a5
Danylo Piliaiev <dpiliaiev@igalia.com>
2026-08-26 13:02:15 +0200

    tu: Use p_atomic_read to read from query's slot->available
    Otherwise compiler could incorrectly optimize it.
    
    Cc: mesa-stable
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44015>

--- src/freedreno/vulkan/tu_query_pool.cc
+++ src/freedreno/vulkan/tu_query_pool.cc
@@ -41,11 +41,11 @@
  */
 #define STAT_COUNT ((__COUNTER_REG(A6XX, CSINVOCATIONS).reg - __COUNTER_REG(A6XX, IAVERTICES).reg) / 2 + 1)
 
-struct PACKED query_slot {
-   uint64_t available;
+struct alignas(8) PACKED query_slot {
+   alignas(8) uint64_t available;
 };
 
-struct PACKED occlusion_query_slot {
+struct alignas(8) PACKED occlusion_query_slot {
    struct query_slot common;
    uint64_t _padding0;
 
@@ -55,16 +55,16 @@ struct PACKED occlusion_query_slot {
    uint64_t _padding1;
 };
 
-struct PACKED timestamp_query_slot {
+struct alignas(8) PACKED timestamp_query_slot {
    struct query_slot common;
    uint64_t result;
 };
 
-struct PACKED primitive_slot_value {
+struct alignas(8) PACKED primitive_slot_value {
    uint64_t values[2];
 };
 
-struct PACKED pipeline_stat_query_slot {
+struct alignas(8) PACKED pipeline_stat_query_slot {
    struct query_slot common;
    uint64_t results[STAT_COUNT];
 
@@ -72,7 +72,7 @@ struct PACKED pipeline_stat_query_slot {
    uint64_t end[STAT_COUNT];
 };
 
-struct PACKED primitive_query_slot {
+struct alignas(8) PACKED primitive_query_slot {
    struct query_slot common;
    /* The result of transform feedback queries is two integer values:
     *   results[0] is the count of primitives written,
@@ -88,25 +88,25 @@ struct PACKED primitive_query_slot {
    struct primitive_slot_value end[4];
 };
 
-struct PACKED perfcntr_query_slot {
+struct alignas(8) PACKED perfcntr_query_slot {
    uint64_t result;
    uint64_t begin;
    uint64_t end;
 };
 
-struct PACKED perf_query_raw_slot {
+struct alignas(8) PACKED perf_query_raw_slot {
    struct query_slot common;
    struct perfcntr_query_slot perfcntr;
 };
 
-struct PACKED primitives_generated_query_slot {
+struct alignas(8) PACKED primitives_generated_query_slot {
    struct query_slot common;
    uint64_t result;
    uint64_t begin;
    uint64_t end;
 };
 
-struct PACKED accel_struct_slot {
+struct alignas(8) PACKED accel_struct_slot {
    struct query_slot common;
    uint64_t result;
 };
@@ -167,7 +167,7 @@ struct PACKED accel_struct_slot {
    (uint64_t *) ((char *) pool->bo->map + pool->query_stride * (query) +   \
                  sizeof(struct query_slot) + sizeof(type) * (i))
 
-#define query_is_available(slot) slot->available
+#define query_is_available(slot) p_atomic_read(&slot->available)
 
 static const VkPerformanceCounterUnitKHR
 fd_perfcntr_type_to_vk_unit[] = {
