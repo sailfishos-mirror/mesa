26ca9cf85a937881b95c4500445340ba0badd9d4
José Roberto de Souza <jose.souza@intel.com>
2026-08-31 12:01:26 +0300

    intel/brw/64bits: Add register spill and fill support
    Scratch surface state address are not loaded in the thread payload
    anymore when 64bits addressing mode is enabled. We use a relocated
    constant for the scratch surface instead.
    
    Changes in Iris and ANV are also need but will come in separated patches.
    
    Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43926>

--- src/intel/compiler/brw/brw_lower_fill_spill.cpp
+++ src/intel/compiler/brw/brw_lower_fill_spill.cpp
@@ -56,7 +56,7 @@ encode_const_offset(brw_send_inst *inst,
 
 static void
 brw_lower_lsc_fill(const intel_device_info *devinfo, brw_shader &s,
-                   brw_inst *inst)
+                   brw_scratch_inst *inst)
 {
    assert(devinfo->verx10 >= 125);
 
@@ -71,7 +71,7 @@ brw_lower_lsc_fill(const intel_device_info *devinfo, brw_shader &s,
    /* LSC is limited to SIMD16 (SIMD32 on Xe2) load/store but we can
     * load more using transpose messages.
     */
-   const bool use_transpose = inst->as_scratch()->use_transpose;
+   const bool use_transpose = inst->use_transpose;
    const brw_builder ubld = use_transpose ? bld.uniform() : bld;
 
    uint32_t desc = lsc_msg_desc(devinfo, LSC_OP_LOAD,
@@ -94,7 +94,7 @@ brw_lower_lsc_fill(const intel_device_info *devinfo, brw_shader &s,
    unspill_inst->sfid = GEN_SFID_UGM;
    unspill_inst->header_size = 0;
    unspill_inst->mlen = brw_lsc_msg_addr_len(devinfo, LSC_ADDR_SIZE_A32,
-                                         unspill_inst->exec_size);
+                                             unspill_inst->exec_size);
    unspill_inst->ex_mlen = 0;
    unspill_inst->size_written =
       brw_lsc_msg_dest_len(devinfo, LSC_DATA_SIZE_D32, bld.dispatch_width()) * REG_SIZE;
@@ -109,8 +109,8 @@ brw_lower_lsc_fill(const intel_device_info *devinfo, brw_shader &s,
                        unspill_inst->size_written / REG_SIZE,
                        unspill_inst->header_size));
 
-   if (inst->as_scratch()->use_base_offset)
-      encode_const_offset(unspill_inst, devinfo, LSC_OP_LOAD, inst->as_scratch()->offset);
+   if (inst->use_base_offset)
+      encode_const_offset(unspill_inst, devinfo, LSC_OP_LOAD, inst->offset);
 
    assert(unspill_inst->size_written == inst->size_written);
    assert(unspill_inst->size_read(devinfo, SEND_SRC_PAYLOAD1) == inst->size_read(devinfo, FILL_SRC_PAYLOAD1));
@@ -119,7 +119,63 @@ brw_lower_lsc_fill(const intel_device_info *devinfo, brw_shader &s,
 }
 
 static void
-brw_lower_lsc_spill(const intel_device_info *devinfo, brw_inst *inst)
+brw_lower_lsc64_fill(const intel_device_info *devinfo, brw_shader &s,
+                     brw_scratch_inst *inst)
+{
+   assert(devinfo->verx10 >= 350);
+
+   const brw_builder bld(inst);
+   brw_reg dst = inst->dst;
+   brw_reg offset = inst->src[FILL_SRC_PAYLOAD1];
+
+   const unsigned reg_size = inst->dst.component_size(inst->exec_size) /
+                             REG_SIZE;
+
+   /* LSC is limited to SIMD16 (SIMD32 on Xe2) load/store but we can
+    * load more using transpose messages.
+    */
+   const bool use_transpose = inst->use_transpose;
+   const brw_builder ubld = use_transpose ? bld.uniform() : bld;
+
+   brw_send_inst *unspill_inst = ubld.SEND();
+   unspill_inst->combined_desc = lsc_64bit_msg_desc(devinfo,
+                                                    GEN_SFID_UGM,
+                                                    LSC_OP_LOAD,
+                                                    LSC_ADDR_SIZE_A32,
+                                                    LSC_DATA_SIZE_D32,
+                                                    use_transpose ? reg_size * 8 : 1 /* num_channels */,
+                                                    use_transpose,
+                                                    LSC_CACHE(devinfo, LOAD, L1STATE_L3MOCS),
+                                                    0 /* scale_offset */,
+                                                    (inst->use_base_offset ? inst->offset : 0) / 4,
+                                                    0 /* surface_state_index */);;
+
+   unspill_inst->dst = dst;
+   unspill_inst->src[SENDG_SRC_IND_0_DESC] = brw_s0(BRW_TYPE_UQ, 7);
+   unspill_inst->src[SENDG_SRC_IND_1_DESC] = brw_reg();
+   unspill_inst->src[SEND_SRC_PAYLOAD1] = offset;
+   unspill_inst->src[SEND_SRC_PAYLOAD2] = brw_reg();
+
+   unspill_inst->sfid = GEN_SFID_UGM;
+   unspill_inst->header_size = 0;
+   unspill_inst->mlen = brw_lsc_msg_addr_len(devinfo, LSC_ADDR_SIZE_A32,
+                                             unspill_inst->exec_size);
+   unspill_inst->ex_mlen = 0;
+   unspill_inst->size_written =
+      brw_lsc_msg_dest_len(devinfo, LSC_DATA_SIZE_D32, bld.dispatch_width()) * REG_SIZE;
+   unspill_inst->has_side_effects = false;
+   unspill_inst->is_volatile = true;
+   unspill_inst->bindless_surface = true;
+   unspill_inst->efficient_64bit = true;
+
+   assert(unspill_inst->size_written == inst->size_written);
+   assert(unspill_inst->size_read(devinfo, SEND_SRC_PAYLOAD1) == inst->size_read(devinfo, FILL_SRC_PAYLOAD1));
+
+   inst->remove();
+}
+
+static void
+brw_lower_lsc_spill(const intel_device_info *devinfo, brw_scratch_inst *inst)
 {
    assert(devinfo->verx10 >= 125);
 
@@ -130,7 +186,7 @@ brw_lower_lsc_spill(const intel_device_info *devinfo, brw_inst *inst)
    const unsigned reg_size = src.component_size(bld.dispatch_width()) /
                              REG_SIZE;
 
-   assert(!inst->as_scratch()->use_transpose);
+   assert(!inst->use_transpose);
 
    const brw_reg ex_desc = build_ex_desc(bld, reg_size, false);
 
@@ -165,8 +221,58 @@ brw_lower_lsc_spill(const intel_device_info *devinfo, brw_inst *inst)
                        spill_inst->size_written / REG_SIZE,
                        spill_inst->header_size));
 
-   if (inst->as_scratch()->use_base_offset)
-      encode_const_offset(spill_inst, devinfo, LSC_OP_STORE, inst->as_scratch()->offset);
+   if (inst->use_base_offset)
+      encode_const_offset(spill_inst, devinfo, LSC_OP_STORE, inst->offset);
+
+   assert(spill_inst->size_written == inst->size_written);
+   assert(spill_inst->size_read(devinfo, SEND_SRC_PAYLOAD1) == inst->size_read(devinfo, SPILL_SRC_PAYLOAD1));
+   assert(spill_inst->size_read(devinfo, SEND_SRC_PAYLOAD2) == inst->size_read(devinfo, SPILL_SRC_PAYLOAD2));
+
+   inst->remove();
+}
+
+static void
+brw_lower_lsc64_spill(const intel_device_info *devinfo, brw_scratch_inst *inst)
+{
+   assert(devinfo->verx10 >= 350);
+
+   const brw_builder bld(inst);
+   brw_reg offset = inst->src[SPILL_SRC_PAYLOAD1];
+   brw_reg src = inst->src[SPILL_SRC_PAYLOAD2];
+
+   const unsigned reg_size = src.component_size(bld.dispatch_width()) /
+                             REG_SIZE;
+
+   assert(!inst->use_transpose);
+
+   brw_send_inst *spill_inst = bld.SEND();
+   spill_inst->combined_desc = lsc_64bit_msg_desc(devinfo,
+                                                  GEN_SFID_UGM,
+                                                  LSC_OP_STORE,
+                                                  LSC_ADDR_SIZE_A32,
+                                                  LSC_DATA_SIZE_D32,
+                                                  1 /* num_channels */,
+                                                  false /* use_transpose */,
+                                                  LSC_CACHE(devinfo, STORE, L1STATE_L3MOCS),
+                                                  0 /* scale_offset */,
+                                                  (inst->use_base_offset ? inst->offset : 0) / 4,
+                                                  0 /* surface_state_index */);
+
+   spill_inst->src[SENDG_SRC_IND_0_DESC] = brw_s0(BRW_TYPE_UQ, 7);
+   spill_inst->src[SENDG_SRC_IND_1_DESC] = brw_reg();
+   spill_inst->src[SEND_SRC_PAYLOAD1] = offset;
+   spill_inst->src[SEND_SRC_PAYLOAD2] = src;
+
+   spill_inst->sfid = GEN_SFID_UGM;
+   spill_inst->header_size = 0;
+   spill_inst->mlen = brw_lsc_msg_addr_len(devinfo, LSC_ADDR_SIZE_A32,
+                                       bld.dispatch_width());
+   spill_inst->ex_mlen = reg_size;
+   spill_inst->size_written = 0;
+   spill_inst->has_side_effects = true;
+   spill_inst->is_volatile = false;
+   spill_inst->bindless_surface = true;
+   spill_inst->efficient_64bit = true;
 
    assert(spill_inst->size_written == inst->size_written);
    assert(spill_inst->size_read(devinfo, SEND_SRC_PAYLOAD1) == inst->size_read(devinfo, SPILL_SRC_PAYLOAD1));
@@ -180,15 +286,32 @@ brw_lower_fill_and_spill(brw_shader &s)
 {
    bool progress = false;
 
+   if (s.key->use_efficient_64bit) {
+      brw_inst *first_inst =
+         s.cfg->first_block()->start();
+      const brw_builder ubld = brw_builder(first_inst).exec_all().group(1, 0);
+      ubld.emit(SHADER_OPCODE_MOV_RELOC_IMM, brw_s0(BRW_TYPE_UD, 14),
+                brw_imm_ud(BRW_SHADER_RELOC_SCRATCH64_SURFACE_LOW), brw_imm_ud(0));
+      ubld.emit(SHADER_OPCODE_MOV_RELOC_IMM, brw_s0(BRW_TYPE_UD, 15),
+                brw_imm_ud(BRW_SHADER_RELOC_SCRATCH64_SURFACE_HIGH), brw_imm_ud(0));
+   }
+
+
    foreach_block_and_inst_safe(block, brw_inst, inst, s.cfg) {
       switch (inst->opcode) {
       case SHADER_OPCODE_LSC_FILL:
-         brw_lower_lsc_fill(s.devinfo, s, inst);
+         if (s.key->use_efficient_64bit)
+            brw_lower_lsc64_fill(s.devinfo, s, inst->as_scratch());
+         else
+            brw_lower_lsc_fill(s.devinfo, s, inst->as_scratch());
          progress = true;
          break;
 
       case SHADER_OPCODE_LSC_SPILL:
-         brw_lower_lsc_spill(s.devinfo, inst);
+         if (s.key->use_efficient_64bit)
+            brw_lower_lsc64_spill(s.devinfo, inst->as_scratch());
+         else
+            brw_lower_lsc_spill(s.devinfo, inst->as_scratch());
          progress = true;
          break;
 
