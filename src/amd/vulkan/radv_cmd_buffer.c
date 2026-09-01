7833a592cee7749e53ff50caa53b0b43ff54ec01
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2026-09-01 11:33:49 +0200

    radv: use radv_get_src_stage_flags2() in radv_gang_barrier()
    No functional change.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44135>

--- src/amd/vulkan/radv_cmd_buffer.c
+++ src/amd/vulkan/radv_cmd_buffer.c
@@ -1762,7 +1762,7 @@ static void
 radv_gang_barrier(struct radv_cmd_buffer *cmd_buffer, VkPipelineStageFlags2 src_stage_mask,
                   VkPipelineStageFlags2 dst_stage_mask)
 {
-   src_stage_mask = vk_expand_src_stage_flags2(src_stage_mask);
+   src_stage_mask = radv_get_src_stage_flags2(src_stage_mask);
    dst_stage_mask = vk_expand_dst_stage_flags2(dst_stage_mask);
 
    /* Update flush bits from the main cmdbuf, except the stage flush. */
