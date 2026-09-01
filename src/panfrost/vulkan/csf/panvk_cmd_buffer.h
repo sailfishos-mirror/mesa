sailfishos-mirror/mesa

commit 41b5f980eb3d967d70a99b283ead9fdaeac43748

panvk: Use a full-screen barrier draw for FB barriers
should_split_render_pass()?  No!  You should not split the render pass.
You should never split the render pass!  There's a better way and it's
the DCD0.primitive_barrier bit.  This bit tells the fragment unit to
treat the current primitive as a barrier which forces it to wait until
all previous primitives for the covered fragments have executed before
executing this one.  This give us a nice, pipelined, way to do fragment
barriers that doesn't involve splitting the render pass.

This new approach also has the advantage that it works properly in
secondary command buffers as it needs to know nothing about the actual
rendering state since it's just a draw call.  Even the dimensions of the
primitive itself are determined by the hardware.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/14328
Reviewed-by: Olivia Lee <olivia.lee@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39051>

src/panfrost/vulkan/csf/panvk_cmd_buffer.h
@@ -157,7 +157,8 @@ struct panvk_cache_flush_info {
 };
 
 struct panvk_cs_deps {
-   bool needs_draw_flush;
+   bool needs_fb_barrier;
+
    struct {
       uint32_t wait_sb_mask;
       struct panvk_cache_flush_info cache_flush;
@@ -468,7 +469,7 @@ panvk_cache_flush_is_nop(const struct panvk_cache_flush_info *cache_flush)
 
 extern const struct vk_command_buffer_ops panvk_per_arch(cmd_buffer_ops);
 
-void panvk_per_arch(cmd_flush_draws)(struct panvk_cmd_buffer *cmdbuf);
+void panvk_per_arch(cmd_fb_barrier)(struct panvk_cmd_buffer *cmdbuf);
 
 #if PAN_ARCH == 10
 /* Match against all possible iter_sb values. The constant iter_sb value for

src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
@@ -2940,6 +2940,193 @@ panvk_per_arch(CmdBeginRendering)(VkCommandBuffer commandBuffer,
 }
 
 static void
+set_run_fullscreen_tiler_flags(struct cs_builder *b, uint32_t view_mask)
+{
+   struct mali_primitive_flags_packed tiler_flags;
+   pan_pack(&tiler_flags, PRIMITIVE_FLAGS, cfg) {
+      cfg.draw_mode = MALI_DRAW_MODE_TRIANGLES;
+      cfg.position_fifo_format = MALI_FIFO_FORMAT_BASIC;
+      cfg.view_mask = view_mask;
+   }
+
+   cs_update_vt_ctx(b)
+      cs_move32_to(b, cs_sr_reg32(b, IDVS, TILER_FLAGS), tiler_flags.opaque[0]);
+}
+
+static void
+cmd_run_fullscreen(struct panvk_cmd_buffer *cmdbuf,
+                   uint64_t dcd, bool ignore_scissor)
+{
+   const struct cs_tracing_ctx *tracing_ctx =
+      &cmdbuf->state.cs[PANVK_SUBQUEUE_VERTEX_TILER].tracing;
+   struct cs_builder *b =
+      panvk_get_cs_builder(cmdbuf, PANVK_SUBQUEUE_VERTEX_TILER);
+
+   if (!inherits_render_ctx(cmdbuf)) {
+      VkResult result = get_render_ctx(cmdbuf);
+      if (result != VK_SUCCESS)
+         return;
+   }
+
+   struct cs_index draw_ptr = cs_scratch_reg64(b, 0);
+   struct cs_index tf_tmp = cs_scratch_reg32(b, 2);
+#if PAN_ARCH >= 11
+   struct cs_index tf2_tmp = cs_scratch_reg32(b, 3);
+#endif
+#if PAN_ARCH >= 13
+   struct cs_index dcd0_tmp = cs_scratch_reg32(b, 4);
+   struct cs_index dcd1_tmp = cs_scratch_reg32(b, 5);
+   struct cs_index dcd2_tmp = cs_scratch_reg32(b, 6);
+#endif
+   struct cs_index scissor_tmp = cs_scratch_reg64(b, 8);
+   struct cs_index counter = cs_scratch_reg32(b, 10);
+   struct cs_index trace_regs = cs_scratch_reg_tuple(b, 12, 4);
+
+   cs_move64_to(b, draw_ptr, dcd);
+
+   /* We need to set our own tiler flags so save them off */
+   cs_move_reg32(b, tf_tmp, cs_sr_reg32(b, IDVS, TILER_FLAGS));
+
+#if PAN_ARCH >= 11
+   cs_move_reg32(b, tf2_tmp, cs_sr_reg32(b, IDVS, TILER_FLAGS2));
+   cs_update_vt_ctx(b) {
+      struct mali_primitive_flags_2_packed tiler_flags_2;
+      pan_pack(&tiler_flags_2, PRIMITIVE_FLAGS_2, cfg) {
+      }
+      cs_move32_to(b, cs_sr_reg32(b, IDVS, TILER_FLAGS2),
+                   tiler_flags_2.opaque[0]);
+   }
+#endif
+
+   /* Starting with v13, the DCD flag registers have to match the flag fields
+    * in the DrawCallDescriptor we provide in memory so we have to save them
+    * off.
+    */
+#if PAN_ARCH >= 13
+   cs_move_reg32(b, dcd0_tmp, cs_sr_reg32(b, IDVS, DCD0));
+   cs_move_reg32(b, dcd1_tmp, cs_sr_reg32(b, IDVS, DCD1));
+   cs_move_reg32(b, dcd2_tmp, cs_sr_reg32(b, IDVS, DCD2));
+
+   cs_update_vt_ctx(b) {
+      cs_load32_to(b, cs_sr_reg32(b, IDVS, DCD0), draw_ptr, 0);
+      cs_load32_to(b, cs_sr_reg32(b, IDVS, DCD1), draw_ptr, 1 * 4);
+      cs_load32_to(b, cs_sr_reg32(b, IDVS, DCD2), draw_ptr, 5 * 4);
+   }
+#endif
+
+   /* RUN_FULLSCREEN respects scissors but for some things like barrier draws,
+    * we want to ignore the scissor so we have to save off the old one and use
+    * a default.
+    */
+   if (ignore_scissor) {
+      struct mali_scissor_packed scissor;
+      pan_pack(&scissor, SCISSOR, cfg) {
+         cfg.scissor_minimum_x = 0;
+         cfg.scissor_minimum_y = 0;
+         cfg.scissor_maximum_x = UINT16_MAX;
+         cfg.scissor_maximum_y = UINT16_MAX;
+      }
+
+      cs_move_reg64(b, scissor_tmp, cs_sr_reg64(b, IDVS, SCISSOR_BOX));
+
+      cs_update_vt_ctx(b) {
+         cs_move64_to(b, cs_sr_reg64(b, IDVS, SCISSOR_BOX),
+                      scissor.opaque[0] | (uint64_t)scissor.opaque[1] << 32);
+      }
+   }
+
+   uint32_t layer_count = cmdbuf->state.gfx.render.layer_count;
+   uint32_t view_mask = cmdbuf->state.gfx.render.view_mask;
+   uint32_t tiler_count = DIV_ROUND_UP(layer_count, MAX_LAYERS_PER_TILER_DESC);
+   if (tiler_count > 1) {
+      struct cs_index tiler_ctx_addr = cs_sr_reg64(b, IDVS, TILER_CTX);
+
+      /* Multiview always fits inside a single tiler context */
+      assert(view_mask == 0);
+
+      /* Start off with a full view mask */
+      set_run_fullscreen_tiler_flags(b,
+         BITFIELD_MASK(MAX_LAYERS_PER_TILER_DESC));
+
+      cs_move32_to(b, counter, tiler_count);
+      cs_while(b, MALI_CS_CONDITION_GREATER, counter) {
+         cs_add32(b, counter, counter, -1);
+         cs_if(b, MALI_CS_CONDITION_EQUAL, counter) {
+            set_run_fullscreen_tiler_flags(b,
+               BITFIELD_MASK(layer_count % MAX_LAYERS_PER_TILER_DESC));
+         }
+
+         cs_trace_run_fullscreen(b, tracing_ctx, trace_regs, 0, draw_ptr);
+
+         cs_update_vt_ctx(b) {
+            cs_add64(b, tiler_ctx_addr, tiler_ctx_addr,
+                     pan_size(TILER_CONTEXT));
+         }
+      }
+
+      cs_update_vt_ctx(b) {
+         cs_add64(b, tiler_ctx_addr, tiler_ctx_addr,
+                  -(tiler_count * pan_size(TILER_CONTEXT)));
+      }
+   } else {
+      set_run_fullscreen_tiler_flags(b,
+         view_mask ? view_mask : BITFIELD_MASK(layer_count));
+
+      cs_trace_run_fullscreen(b, tracing_ctx, trace_regs, 0, draw_ptr);
+   }
+
+   cs_update_vt_ctx(b) {
+      cs_move_reg32(b, cs_sr_reg32(b, IDVS, TILER_FLAGS), tf_tmp);
+#if PAN_ARCH >= 11
+      cs_move_reg32(b, cs_sr_reg32(b, IDVS, TILER_FLAGS2), tf2_tmp);
+#endif
+#if PAN_ARCH >= 13
+      cs_move_reg32(b, cs_sr_reg32(b, IDVS, DCD0), dcd0_tmp);
+      cs_move_reg32(b, cs_sr_reg32(b, IDVS, DCD1), dcd1_tmp);
+      cs_move_reg32(b, cs_sr_reg32(b, IDVS, DCD2), dcd2_tmp);
+#endif
+      if (ignore_scissor)
+         cs_move_reg64(b, cs_sr_reg64(b, IDVS, SCISSOR_BOX), scissor_tmp);
+   }
+}
+
+void
+panvk_per_arch(cmd_fb_barrier)(struct panvk_cmd_buffer *cmdbuf)
+{
+   struct pan_ptr zsd = panvk_cmd_alloc_desc(cmdbuf, DEPTH_STENCIL);
+   if (!zsd.gpu)
+      return;
+
+   pan_cast_and_pack(zsd.cpu, DEPTH_STENCIL, cfg) {
+      cfg.stencil_test_enable = false;
+      cfg.depth_write_enable = false;
+      cfg.depth_function = MALI_FUNC_ALWAYS;
+   };
+
+   struct pan_ptr dcd = panvk_cmd_alloc_desc(cmdbuf, DRAW);
+   if (!dcd.gpu)
+      return;
+
+   pan_cast_and_pack(dcd.cpu, DRAW, cfg) {
+      cfg.flags_0.allow_forward_pixel_to_kill = false;
+      cfg.flags_0.allow_forward_pixel_to_be_killed = false;
+      cfg.flags_0.primitive_barrier = true;
+      cfg.flags_0.occlusion_query = MALI_OCCLUSION_MODE_DISABLED;
+
+      cfg.flags_2.read_mask = 0;
+      cfg.flags_2.write_mask = 0;
+#if PAN_ARCH >= 11
+      cfg.flags_2.no_shader_depth_read = true;
+      cfg.flags_2.no_shader_stencil_read = true;
+#endif
+
+      cfg.depth_stencil = zsd.gpu;
+   };
+
+   cmd_run_fullscreen(cmdbuf, dcd.gpu, true);
+}
+
+static void
 flush_tiling(struct panvk_cmd_buffer *cmdbuf)
 {
    struct cs_builder *b =
@@ -3372,30 +3559,6 @@ issue_fragment_jobs(struct panvk_cmd_buffer *cmdbuf)
 
 
    return VK_SUCCESS;
-}
-
-void
-panvk_per_arch(cmd_flush_draws)(struct panvk_cmd_buffer *cmdbuf)
-{
-   /* If there was no draw queued, we don't need to force a preload. */
-   if (cmdbuf->state.gfx.render.fbds.gpu || inherits_render_ctx(cmdbuf)) {
-      flush_tiling(cmdbuf);
-      issue_fragment_jobs(cmdbuf);
-      memset(&cmdbuf->state.gfx.render.fbds, 0,
-             sizeof(cmdbuf->state.gfx.render.fbds));
-      cmdbuf->state.gfx.render.tiler = 0;
-
-      panvk_per_arch(cmd_force_fb_preload)(cmdbuf, NULL);
-
-      /* We inherited the render context, and need to let the primary command
-       * buffer know that it's changed. */
-      cmdbuf->state.gfx.render.invalidate_inherited_ctx =
-         inherits_render_ctx(cmdbuf);
-
-      /* Re-emit the FB/Tiler descs if we inherited them. */
-      if (inherits_render_ctx(cmdbuf))
-         get_render_ctx(cmdbuf);
-   }
 }
 
 static void

src/panfrost/genxml/cs_builder.h
@@ -1731,6 +1731,13 @@ cs_move_reg32(struct cs_builder *b, struct cs_index dest, struct cs_index src)
 #endif
 }
 
+static inline void
+cs_move_reg64(struct cs_builder *b, struct cs_index dest, struct cs_index src)
+{
+   cs_move_reg32(b, cs_extract32(b, dest, 0), cs_extract32(b, src, 0));
+   cs_move_reg32(b, cs_extract32(b, dest, 1), cs_extract32(b, src, 1));
+}
+
 #if PAN_ARCH >= 11
 static inline void
 cs_and32(struct cs_builder *b, struct cs_index dest, struct cs_index src0,

src/panfrost/vulkan/csf/panvk_vX_cmd_buffer.c
@@ -386,72 +386,6 @@ add_memory_dependency(struct panvk_cache_flush_info *cache_flush,
    }
 }
 
-static bool
-frag_subqueue_needs_sidefx_barrier(VkAccessFlags2 src_access,
-                                   VkAccessFlags2 dst_access)
-{
-   bool src_reads_mem = src_access & (VK_ACCESS_2_SHADER_SAMPLED_READ_BIT |
-                                      VK_ACCESS_2_SHADER_STORAGE_READ_BIT |
-                                      VK_ACCESS_2_MEMORY_READ_BIT);
-   bool dst_reads_mem = dst_access & (VK_ACCESS_2_SHADER_SAMPLED_READ_BIT |
-                                      VK_ACCESS_2_SHADER_STORAGE_READ_BIT |
-                                      VK_ACCESS_2_MEMORY_READ_BIT);
-   bool src_writes_mem = src_access & (VK_ACCESS_2_MEMORY_WRITE_BIT |
-                                       VK_ACCESS_2_SHADER_STORAGE_WRITE_BIT);
-   bool dst_writes_mem = dst_access & (VK_ACCESS_2_MEMORY_WRITE_BIT |
-                                       VK_ACCESS_2_SHADER_STORAGE_WRITE_BIT);
-
-   /* If there's no read -> write, write -> write or write -> read
-    * memory dependency, we can skip, otherwise we have to split the
-    * render pass. We could possibly add the dependency at the draw level,
-    * using extra bits in the DCD2 flags to encode storage reads/writes and
-    * adding extra WAIT/WAIT_RESOURCE shader side, but we can't flush the
-    * texture cache, so it wouldn't work for SAMPLED_READ. Let's keep things
-    * simple and consider any side effect as requiring a split, until this
-    * proves to be a real bottleneck.
-    */
-   return (src_reads_mem && dst_writes_mem) ||
-          (src_writes_mem && dst_writes_mem) ||
-          (src_writes_mem && dst_reads_mem);
-}
-
-static bool
-should_split_render_pass(const uint32_t wait_masks[static PANVK_SUBQUEUE_COUNT],
-                         VkAccessFlags2 src_access, VkAccessFlags2 dst_access)
-{
-   /* From the Vulkan 1.3.301 spec:
-    *
-    *    VUID-vkCmdPipelineBarrier-None-07892
-    *
-    *    "If vkCmdPipelineBarrier is called within a render pass instance, the
-    *    source and destination stage masks of any memory barriers must only
-    *    include graphics pipeline stages"
-    *
-    * We only consider the tiler and the fragment subqueues here.
-    */
-
-   /* split if the tiler subqueue waits for the fragment subqueue */
-   if (wait_masks[PANVK_SUBQUEUE_VERTEX_TILER] &
-       BITFIELD_BIT(PANVK_SUBQUEUE_FRAGMENT))
-      return true;
-
-   if (wait_masks[PANVK_SUBQUEUE_FRAGMENT] &
-       BITFIELD_BIT(PANVK_SUBQUEUE_FRAGMENT)) {
-      /* split if the fragment subqueue self-waits with a feedback loop, because
-       * we lower subpassLoad to texelFetch
-       */
-      if ((src_access & (VK_ACCESS_2_COLOR_ATTACHMENT_WRITE_BIT |
-                         VK_ACCESS_2_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT)) &&
-          (dst_access & VK_ACCESS_2_INPUT_ATTACHMENT_READ_BIT))
-         return true;
-
-      if (frag_subqueue_needs_sidefx_barrier(src_access, dst_access))
-         return true;
-   }
-
-   return false;
-}
-
 static void
 collect_cache_flush_info(enum panvk_subqueue_id subqueue,
                          struct panvk_cache_flush_info *cache_flush,
@@ -466,49 +400,42 @@ collect_cache_flush_info(enum panvk_subqueue_id subqueue,
    add_memory_dependency(cache_flush, src_access, dst_access);
 }
 
-static bool
-can_skip_barrier(struct panvk_cmd_buffer *cmdbuf, const VkDependencyInfo *info,
-                 struct panvk_sync_scope src, struct panvk_sync_scope dst)
-{
-   bool inside_rp = cmdbuf->state.gfx.render.tiler || inherits_render_ctx(cmdbuf);
-   bool by_region = info->dependencyFlags & VK_DEPENDENCY_BY_REGION_BIT;
-
-   if (inside_rp && by_region &&
-       !frag_subqueue_needs_sidefx_barrier(src.access, dst.access))
-      return true;
-
-   return false;
-}
-
 static void
 collect_cs_deps(struct panvk_cmd_buffer *cmdbuf, const VkDependencyInfo *info,
                 struct panvk_sync_scope src, struct panvk_sync_scope dst,
                 struct panvk_cs_deps *deps)
 {
-   if (can_skip_barrier(cmdbuf, info, src, dst))
-      return;
-
    struct panvk_device *dev = to_panvk_device(cmdbuf->vk.base.device);
    uint32_t wait_masks[PANVK_SUBQUEUE_COUNT] = {0};
    add_execution_dependency(wait_masks, src.stages, dst.stages);
 
-   /* within a render pass */
    if (cmdbuf->state.gfx.render.tiler || inherits_render_ctx(cmdbuf)) {
-      if (should_split_render_pass(wait_masks, src.access, dst.access)) {
-         deps->needs_draw_flush = true;
-      } else {
-         /* skip the tiler subqueue self-wait because we use the same
-          * scoreboard slot for the idvs jobs
+      if (info->dependencyFlags & VK_DEPENDENCY_BY_REGION_BIT) {
+         /* Instead of doing an actual fragment job dependency, use a FB
+          * barrier instead.
           */
-         wait_masks[PANVK_SUBQUEUE_VERTEX_TILER] &=
-            ~BITFIELD_BIT(PANVK_SUBQUEUE_VERTEX_TILER);
-
-         /* skip the fragment subqueue self-wait because we emit the fragment
-          * job at the end of the render pass and there is nothing to wait yet
-          */
+         deps->needs_fb_barrier = true;
          wait_masks[PANVK_SUBQUEUE_FRAGMENT] &=
             ~BITFIELD_BIT(PANVK_SUBQUEUE_FRAGMENT);
       }
+
+      /* From the Vulkan 1.4.335 spec:
+       *
+       *    VUID-vkCmdPipelineBarrier-dependencyFlags-07891
+       *
+       *    "If vkCmdPipelineBarrier is called within a render pass
+       *    instance, and the source stage masks of any memory barriers
+       *    include framebuffer-space stages, then dependencyFlags must
+       *    include VK_DEPENDENCY_BY_REGION_BIT"
+       */
+      for (uint32_t i = 0; i < PANVK_SUBQUEUE_COUNT; i++)
+         assert(!(wait_masks[i] & BITFIELD_BIT(PANVK_SUBQUEUE_FRAGMENT)));
+
+      /* skip the tiler subqueue self-wait because we use the same
+       * scoreboard slot for the idvs jobs
+       */
+      wait_masks[PANVK_SUBQUEUE_VERTEX_TILER] &=
+         ~BITFIELD_BIT(PANVK_SUBQUEUE_VERTEX_TILER);
    }
 
    for (uint32_t i = 0; i < PANVK_SUBQUEUE_COUNT; i++) {
@@ -789,8 +716,8 @@ panvk_per_arch(CmdPipelineBarrier2)(VkCommandBuffer commandBuffer,
 
    panvk_per_arch(add_cs_deps)(cmdbuf, PANVK_BARRIER_STAGE_FIRST, pDependencyInfo, &deps, false);
 
-   if (deps.needs_draw_flush)
-      panvk_per_arch(cmd_flush_draws)(cmdbuf);
+   if (deps.needs_fb_barrier)
+      panvk_per_arch(cmd_fb_barrier)(cmdbuf);
 
    panvk_per_arch(emit_barrier)(cmdbuf, deps);
 
@@ -807,7 +734,7 @@ panvk_per_arch(CmdPipelineBarrier2)(VkCommandBuffer commandBuffer,
          cmdbuf, PANVK_BARRIER_STAGE_AFTER_LAYOUT_TRANSITION,
          pDependencyInfo, &trans_deps, false);
 
-      assert(!trans_deps.needs_draw_flush);
+      assert(!trans_deps.needs_fb_barrier);
 
       panvk_per_arch(emit_barrier)(cmdbuf, trans_deps);
    }

src/panfrost/vulkan/csf/panvk_vX_cmd_event.c
@@ -69,8 +69,8 @@ panvk_per_arch(CmdSetEvent2)(VkCommandBuffer commandBuffer, VkEvent _event,
 
    panvk_per_arch(add_cs_deps)(cmdbuf, PANVK_BARRIER_STAGE_FIRST, pDependencyInfo, &deps, true);
 
-   if (deps.needs_draw_flush)
-      panvk_per_arch(cmd_flush_draws)(cmdbuf);
+   /* vkCmdSetEvents() is not allowed to be called mid-render-pass */
+   assert(!deps.needs_fb_barrier);
 
    for (uint32_t i = 0; i < PANVK_SUBQUEUE_COUNT; i++) {
       struct cs_builder *b = panvk_get_cs_builder(cmdbuf, i);
@@ -165,7 +165,7 @@ panvk_per_arch(CmdWaitEvents2)(VkCommandBuffer commandBuffer,
    }
 
    if (needs_trans_barrier) {
-      assert(!trans_deps.needs_draw_flush);
+      assert(!trans_deps.needs_fb_barrier);
 
       panvk_per_arch(emit_barrier)(cmdbuf, trans_deps);
    }

commit 80925ed06c4752f105f332ac31f72c30d0d2ce03

panvk/csf: fix layered FB barrier in secondary cmdbufs
Previously cmd_fb_barrier would skip generating a barrier entirely on
inherited secondary cmdbufs, because the layer count is not known at
record-time and so the layer_count state is set to 0.

To handle this, we need to store the layer count to memory in the
primary cmdbuf and load it dynamically in the secondary cmdbuf. There is
a comment about this situation in launch_indirect_draw suggesting we use
some of the reserved words in the TD to store the layer count. I opted
to use the subqueue ctx instead, just because using the reserved words
sounds like a headache to debug if they ever get unreserved in new
hardware.

Signed-off-by: Olivia Lee <olivia.lee@collabora.com>
Fixes: 41b5f980eb3d (panvk: Use a full-screen barrier draw for FB barriers)
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43960>

src/panfrost/vulkan/csf/panvk_cmd_buffer.h
@@ -164,6 +164,14 @@ struct panvk_cs_subqueue_context {
       /* Timestamp queries that need to happen after the current rp. */
       struct cs_single_link_list ts_chain;
       struct cs_single_link_list ts_done_chain;
+      /* Fields used to pass layer count information from primary cmdbufs to
+       * secondary cmdbufs. */
+#if PAN_ARCH >= 14
+      uint32_t layer_count;
+#else
+      uint32_t td_count;
+      uint32_t last_td_fullscreen_tiler_flags;
+#endif
    } render;
    struct {
       uint32_t counter;

src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
@@ -1157,6 +1157,60 @@ cs_render_desc_ringbuf_move_ptr(struct cs_builder *b, uint32_t size,
 
 static bool get_first_provoking_vertex(struct panvk_cmd_buffer *cmdbuf);
 
+/* Write layer count information to the subqueue ctx so that it can be read by
+ * secondary cmdbufs */
+static void
+prepare_layer_count_inherited_ctx(struct panvk_cmd_buffer *cmdbuf)
+{
+   /* If multiview is enabled, then the view mask is included in
+    * VkCommandBufferInheritanceRenderingInfo, so the layer count will be
+    * known at record-time in the secondary cmdbuf */
+   if (cmdbuf->state.gfx.render.view_mask)
+      return;
+
+   struct cs_builder *b =
+      panvk_get_cs_builder(cmdbuf, PANVK_SUBQUEUE_VERTEX_TILER);
+
+   uint32_t layer_count = cmdbuf->state.gfx.render.layer_count;
+
+#if PAN_ARCH >= 14
+   struct cs_index layer_count_reg = cs_scratch_reg32(b, 0);
+   cs_move32_to(b, layer_count_reg, layer_count);
+   cs_store32(b, layer_count_reg, cs_subqueue_ctx_reg(b),
+              offsetof(struct panvk_cs_subqueue_context, render.layer_count));
+#else
+   uint32_t td_count = DIV_ROUND_UP(layer_count, MAX_LAYERS_PER_TILER_DESC);
+   uint32_t last_td_layers =
+      layer_count - (td_count - 1) * MAX_LAYERS_PER_TILER_DESC;
+   uint32_t last_td_view_mask = BITFIELD_RANGE(0, last_td_layers);
+
+   /* For RUN_FULLSCREEN, HW expects 0 for all flags. Only scissor_array_enable
+    * and the layer selection fields can be set to other values. */
+   struct mali_primitive_flags_packed last_td_tiler_flags = {0};
+   pan_pack(&last_td_tiler_flags, PRIMITIVE_FLAGS, cfg) {
+      /* These default to non-zero */
+      cfg.low_depth_cull = false;
+      cfg.high_depth_cull = false;
+      cfg.view_mask = last_td_view_mask;
+   }
+
+   /* The two fields are adjacent, so we can issue a single * write */
+   STATIC_ASSERT(
+      offsetof(struct panvk_cs_subqueue_context, render.last_td_fullscreen_tiler_flags) ==
+      offsetof(struct panvk_cs_subqueue_context, render.td_count) + 4);
+
+   struct cs_index values = cs_scratch_reg64(b, 0);
+   struct cs_index td_count_reg = cs_extract32(b, values, 0);
+   struct cs_index last_td_tiler_flags_reg = cs_extract32(b, values, 1);
+
+   cs_move32_to(b, td_count_reg, td_count);
+   cs_move32_to(b, last_td_tiler_flags_reg, last_td_tiler_flags.opaque[0]);
+
+   cs_store64(b, values, cs_subqueue_ctx_reg(b),
+              offsetof(struct panvk_cs_subqueue_context, render.td_count));
+#endif
+}
+
 static VkResult
 get_tiler_desc(struct panvk_cmd_buffer *cmdbuf)
 {
@@ -1359,6 +1413,8 @@ get_tiler_desc(struct panvk_cmd_buffer *cmdbuf)
                          -pan_size(TILER_CONTEXT));
       }
    }
+
+   prepare_layer_count_inherited_ctx(cmdbuf);
 
    /* Flush all stores to tiler_ctx_addr. */
    cs_flush_stores(b);
@@ -3761,13 +3817,13 @@ set_run_fullscreen_tiler_flags(struct cs_builder *b, uint32_t layer_index,
 #endif
 }
 
+/* If layer_count is 0, then reads the layer count dynamically from the
+ * subqueue context and clears all layers. */
 static void
 cmd_run_fullscreen(struct panvk_cmd_buffer *cmdbuf, uint64_t dcd,
                    bool ignore_scissor, uint32_t base_layer,
                    uint32_t layer_count)
 {
-   assert(layer_count > 0);
-
    const struct cs_tracing_ctx *tracing_ctx =
       &cmdbuf->state.cs[PANVK_SUBQUEUE_VERTEX_TILER].tracing;
    struct cs_builder *b =
@@ -3790,7 +3846,13 @@ cmd_run_fullscreen(struct panvk_cmd_buffer *cmdbuf, uint64_t dcd,
    struct cs_index dcd2_tmp = cs_scratch_reg32(b, 6);
 #endif
    struct cs_index scissor_tmp = cs_scratch_reg64(b, 8);
-   struct cs_index trace_regs = cs_scratch_reg_tuple(b, 12, 4);
+#if PAN_ARCH >= 14
+   struct cs_index layer = cs_scratch_reg32(b, 10);
+#else
+   struct cs_index render_ctx_values = cs_scratch_reg64(b, 10);
+   struct cs_index tiler_ctx_tmp = cs_scratch_reg64(b, 12);
+#endif
+   struct cs_index trace_regs = cs_scratch_reg_tuple(b, 14, 4);
 
    cs_move64_to(b, draw_ptr, dcd);
 
@@ -3856,7 +3918,82 @@ cmd_run_fullscreen(struct panvk_cmd_buffer *cmdbuf, uint64_t dcd,
    assert(layer_count <= render_layer_count - base_layer ||
           render_layer_count == 0);
 
-   if (render_view_mask) {
+   if (layer_count == 0) {
+      assert(base_layer == 0);
+
+#if PAN_ARCH >= 14
+      cs_load32_to(b, layer, cs_subqueue_ctx_reg(b),
+                   offsetof(struct panvk_cs_subqueue_context,
+                            render.layer_count));
+
+      /* view_mask is always 0, so we can set TILER_FLAGS2 outside of the
+       * loop */
+      struct mali_primitive_flags_2_packed tiler_flags_2;
+      pan_pack(&tiler_flags_2, PRIMITIVE_FLAGS_2, cfg) {
+         cfg.view_mask = 0;
+      }
+      cs_update_vt_ctx(b) {
+         cs_move32_to(b, cs_sr_reg32(b, IDVS, TILER_FLAGS2),
+                      tiler_flags_2.opaque[0]);
+      }
+
+      cs_while(b, MALI_CS_CONDITION_GREATER, layer) {
+         cs_add_imm32(b, layer, layer, -1);
+
+         /* Dynamic set_run_fullscreen_tiler_flags(b, layer, 0)
+          *
+          * The only field that needs to be set in TILER_FLAGS is layer_index,
+          * bits 24 to 32 */
+         cs_update_vt_ctx(b) {
+            cs_lshift_imm32(b, cs_sr_reg32(b, IDVS, TILER_FLAGS), layer, 24);
+         }
+
+         cs_trace_run_fullscreen(b, tracing_ctx, trace_regs, 0, draw_ptr);
+      }
+#else
+      struct cs_index tiler_ctx_addr = cs_sr_reg64(b, IDVS, TILER_CTX);
+
+      /* The two fields we want to read are adjacent, so we can issue a single
+       * read */
+      STATIC_ASSERT(
+         offsetof(struct panvk_cs_subqueue_context, render.last_td_fullscreen_tiler_flags) ==
+         offsetof(struct panvk_cs_subqueue_context, render.td_count) + 4);
+      struct cs_index td_count = cs_extract32(b, render_ctx_values, 0);
+      struct cs_index last_td_tiler_flags =
+         cs_extract32(b, render_ctx_values, 1);
+
+      cs_load64_to(b, render_ctx_values, cs_subqueue_ctx_reg(b),
+                   offsetof(struct panvk_cs_subqueue_context, render.td_count));
+
+      cs_add_imm64(b, tiler_ctx_tmp, tiler_ctx_addr, 0);
+
+      set_run_fullscreen_tiler_flags(b, MAX_LAYERS_PER_TILER_DESC, 0);
+
+      cs_while(b, MALI_CS_CONDITION_GREATER, td_count) {
+         cs_add_imm32(b, td_count, td_count, -1);
+
+         cs_if(b, MALI_CS_CONDITION_EQUAL, td_count) {
+            /* Last TD gets alternate tiler flags which may have a partial
+             * view mask */
+            cs_update_vt_ctx(b) {
+                cs_add_imm32(b, cs_sr_reg32(b, IDVS, TILER_FLAGS),
+                            last_td_tiler_flags, 0);
+            }
+         }
+
+         cs_trace_run_fullscreen(b, tracing_ctx, trace_regs, 0, draw_ptr);
+
+         cs_update_vt_ctx(b) {
+            cs_add_imm64(b, tiler_ctx_addr, tiler_ctx_addr,
+                         pan_size(TILER_CONTEXT));
+         }
+      }
+
+      cs_update_vt_ctx(b) {
+        cs_add_imm64(b, tiler_ctx_addr, tiler_ctx_tmp, 0);
+      }
+#endif
+   } else if (render_view_mask) {
       /* Multiview always fits inside a single tiler context */
       uint32_t view_mask =
          BITFIELD_RANGE(base_layer, layer_count) & render_view_mask;
@@ -3931,7 +4068,9 @@ cmd_run_fullscreen(struct panvk_cmd_buffer *cmdbuf, uint64_t dcd,
 void
 panvk_per_arch(cmd_fb_barrier)(struct panvk_cmd_buffer *cmdbuf)
 {
-   if (cmdbuf->state.gfx.render.layer_count == 0)
+   /* For inherited secondary cmdbufs, we fetch the layer count dynamically */
+   if (cmdbuf->state.gfx.render.layer_count == 0 &&
+       !inherits_render_ctx(cmdbuf))
       return;
 
    struct pan_ptr zsd = panvk_cmd_alloc_desc(cmdbuf, DEPTH_STENCIL);