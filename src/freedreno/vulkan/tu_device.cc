bminor/mesa-mesa

commit 2ab8eff511d640e50682965eb5d4cd9e28f78487

tu/a7xx: Implement VK_KHR_fragment_shading_rate
- A650+ - should be able to support pipelineFragmentShadingRate
          but in some other way than A7XX. Not implemented here.
- A7XX  - support pipelineFragmentShadingRate and attachmentFragmentShadingRate
- A740+ - support primitiveFragmentShadingRate

layeredShadingRateAttachments is unsupported at the moment due to tests
failure, but prop driver supports it.

Passes:
  dEQP-VK.fragment_shading_rate.*
On A750/A740

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30905>

src/freedreno/vulkan/tu_device.cc
@@ -166,6 +166,7 @@ get_device_extensions(const struct tu_physical_device *device,
       .KHR_external_semaphore = true,
       .KHR_external_semaphore_fd = true,
       .KHR_format_feature_flags2 = true,
+      .KHR_fragment_shading_rate = device->info->a6xx.has_attachment_shading_rate,
       .KHR_get_memory_requirements2 = true,
       .KHR_global_priority = true,
       .KHR_image_format_list = true,
@@ -467,6 +468,11 @@ tu_get_features(struct tu_physical_device *pdevice,
 
    /* VK_KHR_dynamic_rendering_local_read */
    features->dynamicRenderingLocalRead = true;
+
+   /* VK_KHR_fragment_shading_rate */
+   features->pipelineFragmentShadingRate = pdevice->info->a6xx.has_attachment_shading_rate;
+   features->primitiveFragmentShadingRate = pdevice->info->a7xx.has_primitive_shading_rate;
+   features->attachmentFragmentShadingRate = pdevice->info->a6xx.has_attachment_shading_rate;
 
    /* VK_KHR_index_type_uint8 */
    features->indexTypeUint8 = true;
@@ -1045,6 +1051,34 @@ tu_get_properties(struct tu_physical_device *pdevice,
    /* VK_KHR_compute_shader_derivatives */
    props->meshAndTaskShaderDerivatives = false;
 
+   /* VK_KHR_fragment_shading_rate */
+   if (pdevice->info->a6xx.has_attachment_shading_rate) {
+      props->minFragmentShadingRateAttachmentTexelSize = {8, 8};
+      props->maxFragmentShadingRateAttachmentTexelSize = {8, 8};
+   } else {
+      props->minFragmentShadingRateAttachmentTexelSize = {0, 0};
+      props->maxFragmentShadingRateAttachmentTexelSize = {0, 0};
+   }
+   props->maxFragmentShadingRateAttachmentTexelSizeAspectRatio = 1;
+   props->primitiveFragmentShadingRateWithMultipleViewports =
+      pdevice->info->a7xx.has_primitive_shading_rate;
+   /* A7XX TODO: dEQP-VK.fragment_shading_rate.*.srlayered.* are failing
+    * for some reason.
+    */
+   props->layeredShadingRateAttachments = false;
+   props->fragmentShadingRateNonTrivialCombinerOps = true;
+   props->maxFragmentSize = {4, 4};
+   props->maxFragmentSizeAspectRatio = 4;
+   props->maxFragmentShadingRateCoverageSamples = 16;
+   props->maxFragmentShadingRateRasterizationSamples = VK_SAMPLE_COUNT_4_BIT;
+   props->fragmentShadingRateWithShaderDepthStencilWrites = true;
+   props->fragmentShadingRateWithSampleMask = true;
+   props->fragmentShadingRateWithShaderSampleMask = true;
+   props->fragmentShadingRateWithConservativeRasterization = false;
+   props->fragmentShadingRateWithFragmentShaderInterlock = false;
+   props->fragmentShadingRateWithCustomSampleLocations = true;
+   props->fragmentShadingRateStrictMultiplyCombiner = true;
+
    /* VK_KHR_push_descriptor */
    props->maxPushDescriptors = MAX_PUSH_DESCRIPTORS;
 
@@ -1189,7 +1223,7 @@ tu_get_properties(struct tu_physical_device *pdevice,
    /* VK_KHR_maintenance6 */
    props->blockTexelViewCompatibleMultipleLayers = true;
    props->maxCombinedImageSamplerDescriptorCount = 1;
-   props->fragmentShadingRateClampCombinerInputs = false; /* TODO */
+   props->fragmentShadingRateClampCombinerInputs = true;
 
    /* VK_EXT_host_image_copy */
 
@@ -1768,6 +1802,39 @@ tu_GetPhysicalDeviceMemoryProperties2(VkPhysicalDevice pdev,
          break;
       }
    }
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL
+tu_GetPhysicalDeviceFragmentShadingRatesKHR(
+   VkPhysicalDevice physicalDevice,
+   uint32_t *pFragmentShadingRateCount,
+   VkPhysicalDeviceFragmentShadingRateKHR *pFragmentShadingRates)
+{
+   VK_OUTARRAY_MAKE_TYPED(VkPhysicalDeviceFragmentShadingRateKHR, out,
+                          pFragmentShadingRates, pFragmentShadingRateCount);
+
+#define append_rate(w, h, s)                                                        \
+   {                                                                                \
+      VkPhysicalDeviceFragmentShadingRateKHR rate = {                               \
+         .sType =                                                                   \
+            VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FRAGMENT_SHADING_RATE_PROPERTIES_KHR, \
+         .sampleCounts = s,                                                         \
+         .fragmentSize = { .width = w, .height = h },                               \
+      };                                                                            \
+      vk_outarray_append_typed(VkPhysicalDeviceFragmentShadingRateKHR, &out,        \
+                               r) *r = rate;                                        \
+   }
+
+   append_rate(4, 4, VK_SAMPLE_COUNT_1_BIT);
+   append_rate(4, 2, VK_SAMPLE_COUNT_1_BIT | VK_SAMPLE_COUNT_2_BIT);
+   append_rate(2, 2, VK_SAMPLE_COUNT_1_BIT | VK_SAMPLE_COUNT_2_BIT | VK_SAMPLE_COUNT_4_BIT);
+   append_rate(2, 1, VK_SAMPLE_COUNT_1_BIT | VK_SAMPLE_COUNT_2_BIT | VK_SAMPLE_COUNT_4_BIT);
+   append_rate(1, 2, VK_SAMPLE_COUNT_1_BIT | VK_SAMPLE_COUNT_2_BIT | VK_SAMPLE_COUNT_4_BIT);
+   append_rate(1, 1, ~0);
+
+#undef append_rate
+
+   return vk_outarray_status(&out);
 }
 
 static VkResult

src/freedreno/common/freedreno_dev_info.h
@@ -79,7 +79,7 @@ struct fd_dev_info {
       bool tess_use_shared;
 
       /* Does the hw support GL_QCOM_shading_rate? */
-      bool has_shading_rate;
+      bool has_legacy_pipeline_shading_rate;
 
       /* Whether a 16-bit descriptor can be used */
       bool storage_16bit;
@@ -185,6 +185,8 @@ struct fd_dev_info {
        * Happens with VK_EXT_attachment_feedback_loop_layout.
        */
       bool has_coherent_ubwc_flag_caches;
+
+      bool has_attachment_shading_rate;
 
       struct {
          uint32_t PC_POWER_CNTL;
@@ -302,6 +304,8 @@ struct fd_dev_info {
 
       /* Whether only 256 vec4 constants are available for compute */
       bool compute_constlen_quirk;
+
+      bool has_primitive_shading_rate;
    } a7xx;
 };
 

src/freedreno/common/freedreno_devices.py
@@ -416,7 +416,7 @@ a6xx_gen4 = A6XXProps(
         has_cp_reg_write = False,
         has_8bpp_ubwc = False,
         has_lpac = True,
-        has_shading_rate = True,
+        has_legacy_pipeline_shading_rate = True,
         has_getfiberid = True,
         has_dp2acc = True,
         has_dp4acc = True,
@@ -842,7 +842,6 @@ a7xx_base = A6XXProps(
         has_separate_chroma_filter = True,
         has_sample_locations = True,
         has_lpac = True,
-        has_shading_rate = True,
         has_getfiberid = True,
         has_dp2acc = True,
         has_dp4acc = True,
@@ -857,6 +856,7 @@ a7xx_base = A6XXProps(
         has_isam_v = True,
         has_ssbo_imm_offsets = True,
         has_early_preamble = True,
+        has_attachment_shading_rate = True,
     )
 
 a7xx_gen1 = A7XXProps(
@@ -875,6 +875,7 @@ a7xx_gen2 = A7XXProps(
         # this hint set. Match them for better compatibility by default.
         enable_tp_ubwc_flag_hint = False,
         has_64b_ssbo_atomics = True,
+        has_primitive_shading_rate = True,
     )
 
 a7xx_gen3 = A7XXProps(
@@ -895,6 +896,7 @@ a7xx_gen3 = A7XXProps(
         ubwc_coherency_quirk = True,
         has_persistent_counter = True,
         has_64b_ssbo_atomics = True,
+        has_primitive_shading_rate = True,
     )
 
 a730_magic_regs = dict(
@@ -950,11 +952,6 @@ a730_raw_magic_regs = [
         [A6XXRegs.REG_A7XX_RB_UNKNOWN_8E79,   0x00000000],
         [A6XXRegs.REG_A7XX_RB_UNKNOWN_8899,   0x00000000],
         [A6XXRegs.REG_A7XX_RB_UNKNOWN_88F5,   0x00000000],
-
-        # Shading rate group
-        [A6XXRegs.REG_A6XX_RB_FSR_CONFIG,   0x00000000],
-        [A6XXRegs.REG_A7XX_SP_FSR_CONFIG, 0x00000000],
-        [A6XXRegs.REG_A7XX_GRAS_FSR_CONFIG, 0x00000000],
     ]
 
 a740_magic_regs = dict(
@@ -1021,12 +1018,7 @@ a740_raw_magic_regs = [
         [A6XXRegs.REG_A7XX_RB_UNKNOWN_88F5,   0x00000000],
         [A6XXRegs.REG_A7XX_RB_UNKNOWN_8C34,   0x00000000],
 
-        # Shading rate group
-        [A6XXRegs.REG_A6XX_RB_FSR_CONFIG,   0x00000000],
-        [A6XXRegs.REG_A7XX_SP_FSR_CONFIG, 0x00000000],
         [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8008, 0x00000000],
-        [A6XXRegs.REG_A7XX_GRAS_FSR_CONFIG, 0x00000000],
-        [A6XXRegs.REG_A7XX_GRAS_FSR_BUFFER_DESC, 0x00000000],
     ]
 
 add_gpus([
@@ -1137,11 +1129,7 @@ add_gpus([
             [A6XXRegs.REG_A7XX_RB_UNKNOWN_88F5,   0x00000000],
             [A6XXRegs.REG_A7XX_RB_UNKNOWN_8C34,   0x00000000],
 
-            # Shading rate group
-            [A6XXRegs.REG_A6XX_RB_FSR_CONFIG,   0x00000000],
-            [A6XXRegs.REG_A7XX_SP_FSR_CONFIG, 0x00000000],
             [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8008, 0x00000000],
-            [A6XXRegs.REG_A7XX_GRAS_FSR_CONFIG, 0x00000000],
         ],
     ))
 
@@ -1238,12 +1226,6 @@ add_gpus([
             [A6XXRegs.REG_A7XX_RB_UNKNOWN_8E79,   0x00000000],
             [A6XXRegs.REG_A7XX_RB_UNKNOWN_8899,   0x00000000],
             [A6XXRegs.REG_A7XX_RB_UNKNOWN_88F5,   0x00000000],
-
-            # Shading rate group
-            [A6XXRegs.REG_A6XX_RB_FSR_CONFIG,   0x00000000],
-            [A6XXRegs.REG_A7XX_SP_FSR_CONFIG, 0x00000000],
-            [A6XXRegs.REG_A7XX_GRAS_FSR_CONFIG, 0x00000000],
-            [A6XXRegs.REG_A7XX_GRAS_FSR_BUFFER_DESC, 0x00000000],
         ],
     ))
 
@@ -1349,11 +1331,7 @@ add_gpus([
             [A6XXRegs.REG_A7XX_RB_UNKNOWN_88F5,   0x00000000],
             [A6XXRegs.REG_A7XX_RB_UNKNOWN_8C34,   0x00000000],
 
-            # Shading rate group
-            [A6XXRegs.REG_A6XX_RB_FSR_CONFIG,   0x00000000],
-            [A6XXRegs.REG_A7XX_SP_FSR_CONFIG, 0x00000000],
             [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8008, 0x00000000],
-            [A6XXRegs.REG_A7XX_GRAS_FSR_CONFIG, 0x00000000],
 
             [0x930a, 0],
             [0x960a, 1],

src/freedreno/vulkan/tu_clear_blit.cc
@@ -1610,8 +1610,13 @@ r3d_setup(struct tu_cmd_buffer *cmd,
    tu_cs_emit_regs(cs, A6XX_GRAS_LRZ_CNTL(0));
    tu_cs_emit_regs(cs, A6XX_RB_LRZ_CNTL(0));
 
-   if (CHIP >= A7XX)
+   if (CHIP >= A7XX) {
       tu_cs_emit_regs(cs, A7XX_GRAS_LRZ_DEPTH_BUFFER_INFO());
+
+      tu_cs_emit_regs(cs, A6XX_RB_FSR_CONFIG());
+      tu_cs_emit_regs(cs, A7XX_SP_FSR_CONFIG());
+      tu_cs_emit_regs(cs, A7XX_GRAS_FSR_CONFIG());
+   }
 
    tu_cs_emit_write_reg(cs, REG_A6XX_GRAS_SC_CNTL,
                         A6XX_GRAS_SC_CNTL_CCUSINGLECACHELINESIZE(2));

src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -803,11 +803,20 @@ tu_cs_emit_draw_state(struct tu_cs *cs, uint32_t id, struct tu_draw_state state)
       enable_mask = CP_SET_DRAW_STATE__0_SYSMEM;
       break;
    case TU_DRAW_STATE_DYNAMIC + TU_DYNAMIC_STATE_PRIM_MODE_SYSMEM:
-      /* By also applying the state during binning we ensure that there
-       * is no rotation applied, by previous A6XX_GRAS_SC_CNTL::rotation.
-       */
-      enable_mask =
-         CP_SET_DRAW_STATE__0_SYSMEM | CP_SET_DRAW_STATE__0_BINNING;
+      if (!cs->device->physical_device->info->a6xx.has_coherent_ubwc_flag_caches) {
+         /* By also applying the state during binning we ensure that there
+         * is no rotation applied, by previous A6XX_GRAS_SC_CNTL::rotation.
+         */
+         enable_mask =
+            CP_SET_DRAW_STATE__0_SYSMEM | CP_SET_DRAW_STATE__0_BINNING;
+      } else {
+         static_assert(TU_DYNAMIC_STATE_PRIM_MODE_SYSMEM ==
+                       TU_DYNAMIC_STATE_A7XX_FRAGMENT_SHADING_RATE);
+         enable_mask = CP_SET_DRAW_STATE__0_GMEM |
+                       CP_SET_DRAW_STATE__0_SYSMEM |
+                       CP_SET_DRAW_STATE__0_BINNING;
+      }
+
       break;
    default:
       enable_mask = CP_SET_DRAW_STATE__0_GMEM |
@@ -2542,6 +2551,7 @@ tu_reset_cmd_buffer(struct vk_command_buffer *vk_cmd_buffer,
    cmd_buffer->state.max_vbs_bound = 0;
 
    cmd_buffer->vsc_initialized = false;
+   cmd_buffer->prev_fsr_is_null = false;
 
    ralloc_free(cmd_buffer->patchpoints_ctx);
    cmd_buffer->patchpoints_ctx = NULL;
@@ -3709,6 +3719,17 @@ tu_CmdBindPipeline(VkCommandBuffer commandBuffer,
       cmd->state.dirty |= TU_CMD_DIRTY_FEEDBACK_LOOPS | TU_CMD_DIRTY_LRZ;
    }
 
+   if (pipeline->program.writes_shading_rate !=
+          cmd->state.pipeline_writes_shading_rate ||
+       pipeline->program.reads_shading_rate !=
+          cmd->state.pipeline_reads_shading_rate) {
+      cmd->state.pipeline_writes_shading_rate =
+         pipeline->program.writes_shading_rate;
+      cmd->state.pipeline_reads_shading_rate =
+         pipeline->program.reads_shading_rate;
+      cmd->state.dirty |= TU_CMD_DIRTY_SHADING_RATE;
+   }
+
    bool raster_order_attachment_access =
       pipeline->output.raster_order_attachment_access ||
       pipeline->ds.raster_order_attachment_access;
@@ -4586,6 +4607,49 @@ tu7_emit_subpass_clear(struct tu_cmd_buffer *cmd, struct tu_resolve_group *resol
    }
 }
 
+static void
+tu7_emit_subpass_shading_rate(struct tu_cmd_buffer *cmd,
+                              const struct tu_subpass *subpass,
+                              struct tu_cs *cs)
+{
+   if (subpass->fsr_attachment == VK_ATTACHMENT_UNUSED) {
+      tu_cs_emit_regs(cs, A7XX_GRAS_FSR_BUFFER_DESC(),
+                      A7XX_GRAS_FSR_BUFFER_SIZE());
+      tu_cs_emit_regs(cs, A7XX_GRAS_FSR_BUFFER_PITCH());
+      tu_cs_emit_regs(cs, A7XX_GRAS_FSR_BUFFER_BASE());
+      /* We need to invalidate cache when changing to NULL FSR attachment, but
+       * only once.
+       */
+      if (!cmd->prev_fsr_is_null) {
+         tu_emit_raw_event_write<A7XX>(cmd, cs, LRZ_Q_CACHE_INVALIDATE,
+                                       false);
+         cmd->prev_fsr_is_null = true;
+      }
+      return;
+   }
+
+   const struct tu_image_view *iview =
+      cmd->state.attachments[subpass->fsr_attachment];
+   assert(iview->vk.format == VK_FORMAT_R8_UINT);
+
+   tu_cs_emit_regs(
+      cs,
+      A7XX_GRAS_FSR_BUFFER_DESC(.layered = true,
+                                .tile_mode =
+                                   (a6xx_tile_mode) iview->image->layout[0]
+                                      .tile_mode, ),
+      A7XX_GRAS_FSR_BUFFER_SIZE(.width = iview->view.width,
+                                .height = iview->view.height));
+   tu_cs_emit_regs(
+      cs, A7XX_GRAS_FSR_BUFFER_PITCH(.pitch = iview->view.pitch,
+                                     .array_pitch = iview->view.layer_size));
+   tu_cs_emit_regs(cs,
+                   A7XX_GRAS_FSR_BUFFER_BASE(.qword = iview->view.base_addr));
+
+   tu_emit_raw_event_write<A7XX>(cmd, cs, LRZ_Q_CACHE_INVALIDATE, false);
+   cmd->prev_fsr_is_null = false;
+}
+
 /* emit loads, clears, and mrt/zs/msaa/ubwc state for the subpass that is
  * starting (either at vkCmdBeginRenderPass2() or vkCmdNextSubpass2())
  *
@@ -4612,6 +4676,10 @@ tu_emit_subpass_begin(struct tu_cmd_buffer *cmd)
    tu6_emit_zs<CHIP>(cmd, cmd->state.subpass, &cmd->draw_cs);
    tu6_emit_mrt<CHIP>(cmd, cmd->state.subpass, &cmd->draw_cs);
    tu6_emit_render_cntl<CHIP>(cmd, cmd->state.subpass, &cmd->draw_cs, false);
+
+   if (CHIP >= A7XX) {
+      tu7_emit_subpass_shading_rate(cmd, cmd->state.subpass, &cmd->draw_cs);
+   }
 
    tu_set_input_attachments(cmd, cmd->state.subpass);
 
@@ -4786,6 +4854,15 @@ tu_CmdBeginRendering(VkCommandBuffer commandBuffer,
 
    if (cmd->dynamic_pass.has_fdm)
       cmd->patchpoints_ctx = ralloc_context(NULL);
+
+   a = cmd->dynamic_subpass.fsr_attachment;
+   if (a != VK_ATTACHMENT_UNUSED) {
+      const VkRenderingFragmentShadingRateAttachmentInfoKHR *fsr_info =
+         vk_find_struct_const(pRenderingInfo->pNext,
+                              RENDERING_FRAGMENT_SHADING_RATE_ATTACHMENT_INFO_KHR);
+      VK_FROM_HANDLE(tu_image_view, view, fsr_info->imageView);
+      cmd->state.attachments[a] = view;
+   }
 
    tu_choose_gmem_layout(cmd);
 

src/freedreno/vulkan/tu_cmd_buffer.h
@@ -73,8 +73,9 @@ enum tu_cmd_dirty_bits
    TU_CMD_DIRTY_RAST_ORDER = BIT(12),
    TU_CMD_DIRTY_FEEDBACK_LOOPS = BIT(13),
    TU_CMD_DIRTY_FS = BIT(14),
+   TU_CMD_DIRTY_SHADING_RATE = BIT(15),
    /* all draw states were disabled and need to be re-enabled: */
-   TU_CMD_DIRTY_DRAW_STATE = BIT(15)
+   TU_CMD_DIRTY_DRAW_STATE = BIT(16)
 };
 
 /* There are only three cache domains we have to care about: the CCU, or
@@ -514,6 +515,8 @@ struct tu_cmd_state
    bool raster_order_attachment_access_valid;
    bool blit_cache_cleaned;
    VkImageAspectFlags pipeline_feedback_loops;
+   bool pipeline_writes_shading_rate;
+   bool pipeline_reads_shading_rate;
 
    bool pipeline_blend_lrz, pipeline_bandwidth;
    uint32_t pipeline_draw_states;
@@ -571,11 +574,11 @@ struct tu_cmd_buffer
 
    struct tu_descriptor_state descriptors[MAX_BIND_POINTS];
 
-   struct tu_render_pass_attachment dynamic_rp_attachments[2 * (MAX_RTS + 1) + 1];
+   struct tu_render_pass_attachment dynamic_rp_attachments[2 * (MAX_RTS + 1) + 2];
    struct tu_subpass_attachment dynamic_color_attachments[MAX_RTS];
    struct tu_subpass_attachment dynamic_input_attachments[MAX_RTS + 1];
    struct tu_subpass_attachment dynamic_resolve_attachments[MAX_RTS + 1];
-   const struct tu_image_view *dynamic_attachments[2 * (MAX_RTS + 1) + 1];
+   const struct tu_image_view *dynamic_attachments[2 * (MAX_RTS + 1) + 2];
    VkClearValue dynamic_clear_values[2 * (MAX_RTS + 1)];
 
    struct tu_render_pass dynamic_pass;
@@ -613,6 +616,8 @@ struct tu_cmd_buffer
    uint32_t vsc_prim_strm_pitch;
    uint64_t vsc_draw_strm_va, vsc_draw_strm_size_va, vsc_prim_strm_va;
    bool vsc_initialized;
+
+   bool prev_fsr_is_null;
 };
 VK_DEFINE_HANDLE_CASTS(tu_cmd_buffer, vk.base, VkCommandBuffer,
                        VK_OBJECT_TYPE_COMMAND_BUFFER)

src/freedreno/vulkan/tu_formats.cc
@@ -265,6 +265,9 @@ tu_physical_device_get_format_properties(
    if (vk_format == VK_FORMAT_D32_SFLOAT_S8_UINT)
       linear = 0;
 
+   if (vk_format == VK_FORMAT_R8_UINT)
+      optimal |= VK_FORMAT_FEATURE_2_FRAGMENT_SHADING_RATE_ATTACHMENT_BIT_KHR;
+
 end:
    out_properties->linearTilingFeatures = linear;
    out_properties->optimalTilingFeatures = optimal;
@@ -509,6 +512,14 @@ tu_get_image_format_properties(
       if (!(format_feature_flags &
             (VK_FORMAT_FEATURE_2_COLOR_ATTACHMENT_BIT |
              VK_FORMAT_FEATURE_2_DEPTH_STENCIL_ATTACHMENT_BIT))) {
+         return tu_image_unsupported_format(pImageFormatProperties);
+      }
+   }
+
+   if (image_usage &
+       VK_IMAGE_USAGE_FRAGMENT_SHADING_RATE_ATTACHMENT_BIT_KHR) {
+      if (!(format_feature_flags &
+            VK_FORMAT_FEATURE_2_FRAGMENT_SHADING_RATE_ATTACHMENT_BIT_KHR)) {
          return tu_image_unsupported_format(pImageFormatProperties);
       }
    }

src/freedreno/vulkan/tu_image.cc
@@ -662,6 +662,13 @@ tu_image_init(struct tu_device *device, struct tu_image *image,
                                                TILE6_LINEAR) != WZYX)
       image->force_linear_tile = true;
 
+   /* Some kind of HW limitation. */
+   if (pCreateInfo->usage &
+          VK_IMAGE_USAGE_FRAGMENT_SHADING_RATE_ATTACHMENT_BIT_KHR &&
+       image->vk.extent.width < 16) {
+      image->force_linear_tile = true;
+   }
+
    if (image->force_linear_tile ||
        !ubwc_possible(device, image->vk.format, pCreateInfo->imageType,
                       pCreateInfo->usage, image->vk.stencil_usage,

sailfishos-mirror/mesa

commit 4c36bbc8518e4991c616aee2a24bf1d10b3c0910

tu: Fix the sType of the returned VkFragmentShadingRateKHRs.
Fixes: 2ab8eff511d6 ("tu/a7xx: Implement VK_KHR_fragment_shading_rate")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44152>

src/freedreno/vulkan/tu_device.cc
@@ -2209,7 +2209,7 @@ tu_GetPhysicalDeviceFragmentShadingRatesKHR(
    {                                                                                \
       VkPhysicalDeviceFragmentShadingRateKHR rate = {                               \
          .sType =                                                                   \
-            VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FRAGMENT_SHADING_RATE_PROPERTIES_KHR, \
+            VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FRAGMENT_SHADING_RATE_KHR,            \
          .sampleCounts = s,                                                         \
          .fragmentSize = { .width = w, .height = h },                               \
       };                                                                            \