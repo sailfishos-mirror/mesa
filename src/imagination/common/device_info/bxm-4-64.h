bminor/mesa-mesa

commit 1e548195d2391273331d1254fcb6da9a247fe0b7

pvr: feature promotion to core from derived
RGX_FEATURE_USC_ITR_PARALLEL_INSTANCES is no longer a derived feature
for Volcanic, it is a core feature. This improvement is related to the
introduction of Volcanic device information.

Signed-off-by: leonperianu <leon.perianu@imgtec.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38423>

src/imagination/common/device_info/bxm-4-64.h
@@ -54,6 +54,7 @@ static const struct pvr_device_features pvr_device_features_36_V_104_182 = {
    .has_usc_f16sop_u8 = true,
    .has_usc_itrsmp = true,
    .has_usc_itrsmp_enhanced = true,
+   .has_usc_itr_parallel_instances = true,
    .has_usc_min_output_registers_per_pix = true,
    .has_usc_pixel_partition_mask = true,
    .has_usc_slots = true,
@@ -79,6 +80,7 @@ static const struct pvr_device_features pvr_device_features_36_V_104_182 = {
    .tile_size_y = 16U,
    .tpu_parallel_instances = 4U,
    .unified_store_depth = 256U,
+   .usc_itr_parallel_instances = 16U,
    .usc_min_output_registers_per_pix = 2U,
    .usc_slots = 64U,
    .uvs_banks = 8U,
@@ -89,9 +91,6 @@ static const struct pvr_device_features pvr_device_features_36_V_104_182 = {
 
    /* Derived features. */
    .has_s8xe = true,
-   .has_usc_itr_parallel_instances = true,
-
-   .usc_itr_parallel_instances = 16U,
 };
 
 static const struct pvr_device_enhancements

src/imagination/common/device_info/ge8300.h
@@ -45,6 +45,7 @@ static const struct pvr_device_features pvr_device_features_22_V_54_30 = {
    .has_usc_f16sop_u8 = true,
    .has_usc_itrsmp = true,
    .has_usc_itrsmp_enhanced = true,
+   .has_usc_itr_parallel_instances = true,
    .has_usc_min_output_registers_per_pix = true,
    .has_usc_slots = true,
    .has_uvs_banks = true,
@@ -67,6 +68,7 @@ static const struct pvr_device_features pvr_device_features_22_V_54_30 = {
    .tile_size_y = 16U,
    .tpu_parallel_instances = 4U,
    .unified_store_depth = 208U,
+   .usc_itr_parallel_instances = 16U,
    .usc_min_output_registers_per_pix = 2U,
    .usc_slots = 64U,
    .uvs_banks = 4U,
@@ -75,9 +77,6 @@ static const struct pvr_device_features pvr_device_features_22_V_54_30 = {
    .vdm_cam_size = 64U,
 
    .has_s8xe = true,
-   .has_usc_itr_parallel_instances = true,
-
-   .usc_itr_parallel_instances = 16U,
 };
 
 static const struct pvr_device_enhancements

src/imagination/common/device_info/axe-1-16m.h
@@ -62,6 +62,7 @@ static const struct pvr_device_features pvr_device_features_33_V_11_3 = {
    .has_tpu_parallel_instances = true,
    .has_unified_store_depth = true,
    .has_usc_f16sop_u8 = true,
+   .has_usc_itr_parallel_instances = true,
    .has_usc_min_output_registers_per_pix = true,
    .has_usc_pixel_partition_mask = true,
    .has_usc_slots = true,
@@ -86,6 +87,7 @@ static const struct pvr_device_features pvr_device_features_33_V_11_3 = {
    .tile_size_y = 16U,
    .tpu_parallel_instances = 1U,
    .unified_store_depth = 64U,
+   .usc_itr_parallel_instances = 4U,
    .usc_min_output_registers_per_pix = 1U,
    .usc_slots = 14U,
    .uvs_banks = 2U,
@@ -94,9 +96,6 @@ static const struct pvr_device_features pvr_device_features_33_V_11_3 = {
    .vdm_cam_size = 32U,
 
    .has_s8xe = true,
-   .has_usc_itr_parallel_instances = true,
-
-   .usc_itr_parallel_instances = 4U,
 };
 
 static const struct pvr_device_enhancements pvr_device_enhancements_33_15_11_3 = {

src/imagination/common/device_info/bxe-2-32.h
@@ -53,6 +53,8 @@ static const struct pvr_device_features pvr_device_features_36_V_52_182 = {
    .has_usc_f16sop_u8 = true,
    .has_usc_itrsmp = true,
    .has_usc_itrsmp_enhanced = true,
+   .has_usc_itr_parallel_instances = true,
+
    .has_usc_min_output_registers_per_pix = true,
    .has_usc_pixel_partition_mask = true,
    .has_usc_slots = true,
@@ -78,6 +80,7 @@ static const struct pvr_device_features pvr_device_features_36_V_52_182 = {
    .tile_size_y = 16U,
    .tpu_parallel_instances = 2U,
    .unified_store_depth = 128U,
+   .usc_itr_parallel_instances = 8U,
    .usc_min_output_registers_per_pix = 2U,
    .usc_slots = 32U,
    .uvs_banks = 4U,
@@ -87,9 +90,6 @@ static const struct pvr_device_features pvr_device_features_36_V_52_182 = {
    .xpu_max_slaves = 3U,
 
    .has_s8xe = true,
-   .has_usc_itr_parallel_instances = true,
-
-   .usc_itr_parallel_instances = 8U,
 };
 
 static const struct pvr_device_enhancements

src/imagination/common/device_info/bxe-4-32.h
@@ -54,6 +54,7 @@ static const struct pvr_device_features pvr_device_features_36_V_54_182 = {
    .has_usc_itrsmp = true,
    .has_usc_itrsmp_enhanced = true,
    .has_usc_f16sop_u8 = true,
+   .has_usc_itr_parallel_instances = true,
    .has_usc_min_output_registers_per_pix = true,
    .has_usc_pixel_partition_mask = true,
    .has_usc_slots = true,
@@ -79,6 +80,7 @@ static const struct pvr_device_features pvr_device_features_36_V_54_182 = {
    .tile_size_y = 16U,
    .tpu_parallel_instances = 4U,
    .unified_store_depth = 208U,
+   .usc_itr_parallel_instances = 16U,
    .usc_min_output_registers_per_pix = 2U,
    .usc_slots = 64U,
    .uvs_banks = 8U,
@@ -88,9 +90,6 @@ static const struct pvr_device_features pvr_device_features_36_V_54_182 = {
    .xpu_max_slaves = 3U,
 
    .has_s8xe = true,
-   .has_usc_itr_parallel_instances = true,
-
-   .usc_itr_parallel_instances = 16U,
 };
 
 static const struct pvr_device_enhancements

src/imagination/common/device_info/bxs-4-64.h
@@ -72,6 +72,7 @@ static const struct pvr_device_features pvr_device_features_36_V_104_796 = {
    .has_usc_f16sop_u8 = true,
    .has_usc_itrsmp = true,
    .has_usc_itrsmp_enhanced = true,
+   .has_usc_itr_parallel_instances = true,
    .has_usc_min_output_registers_per_pix = true,
    .has_usc_pixel_partition_mask = true,
    .has_usc_slots = true,
@@ -98,6 +99,7 @@ static const struct pvr_device_features pvr_device_features_36_V_104_796 = {
    .tile_size_y = 16U,
    .tpu_parallel_instances = 4U,
    .unified_store_depth = 256U,
+   .usc_itr_parallel_instances = 16U,
    .usc_min_output_registers_per_pix = 2U,
    .usc_slots = 64U,
    .uvs_banks = 8U,
@@ -107,9 +109,6 @@ static const struct pvr_device_features pvr_device_features_36_V_104_796 = {
    .xpu_max_slaves = 3U,
 
    .has_s8xe = true,
-   .has_usc_itr_parallel_instances = true,
-
-   .usc_itr_parallel_instances = 16U,
 };
 
 static const struct pvr_device_enhancements

src/imagination/common/device_info/g6110.h
@@ -34,6 +34,7 @@ static const struct pvr_device_features pvr_device_features_5_V_1_46 = {
    .has_tile_size_y = true,
    .has_tpu_parallel_instances = true,
    .has_unified_store_depth = true,
+   .has_usc_itr_parallel_instances = true,
    .has_usc_min_output_registers_per_pix = true,
    .has_usc_slots = true,
    .has_uvs_banks = true,
@@ -56,6 +57,7 @@ static const struct pvr_device_features pvr_device_features_5_V_1_46 = {
    .tile_size_y = 32U,
    .tpu_parallel_instances = 4U,
    .unified_store_depth = 208U,
+   .usc_itr_parallel_instances = 16U,
    .usc_min_output_registers_per_pix = 2U,
    .usc_slots = 56U,
    .uvs_banks = 8U,
@@ -64,9 +66,6 @@ static const struct pvr_device_features pvr_device_features_5_V_1_46 = {
    .vdm_cam_size = 64U,
 
    .has_requires_fb_cdc_zls_setup = true,
-   .has_usc_itr_parallel_instances = true,
-
-   .usc_itr_parallel_instances = 16U,
 };
 
 static const struct pvr_device_enhancements pvr_device_enhancements_5_9_1_46 = {

src/imagination/common/device_info/ge7800.h
@@ -36,6 +36,7 @@ static const struct pvr_device_features pvr_device_features_15_V_1_64 = {
       .has_tpu_parallel_instances = true,
       .has_unified_store_depth = true,
       .has_usc_f16sop_u8 = true,
+      .has_usc_itr_parallel_instances = true,
       .has_usc_min_output_registers_per_pix = true,
       .has_usc_slots = true,
       .has_uvs_banks = true,
@@ -58,6 +59,7 @@ static const struct pvr_device_features pvr_device_features_15_V_1_64 = {
       .tile_size_y = 32U,
       .tpu_parallel_instances = 4U,
       .unified_store_depth = 208U,
+      .usc_itr_parallel_instances = 16U,
       .usc_min_output_registers_per_pix = 2U,
       .usc_slots = 56U,
       .uvs_banks = 8U,
@@ -66,9 +68,6 @@ static const struct pvr_device_features pvr_device_features_15_V_1_64 = {
       .vdm_cam_size = 128U,
 
       .has_requires_fb_cdc_zls_setup = true,
-      .has_usc_itr_parallel_instances = true,
-
-      .usc_itr_parallel_instances = 16U,
 };
 
 static const struct pvr_device_enhancements

sailfishos-mirror/mesa

commit cff05c145647497ed5b9bc83754dc067ce4a6d39

pvr: fixup ordering of (has_)usc_itr_parallel_instances in dev info
The 'usc_itr_parallel_instances' feature was promoted from a derived to a core
feature in Volcanic. As such, the existing device info was updated to reflect
this, but a couple of places where missed. Update the remaining places.

Fixes: 1e548195d23 ("pvr: feature promotion to core from derived")
Signed-off-by: Frank Binns <frank.binns@imgtec.com>
Reviewed-by: Icenowy Zheng <zhengxingda@iscas.ac.cn>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44091>

src/imagination/common/device_info/bxm-4-64.h
@@ -169,6 +169,7 @@ static const struct pvr_device_features pvr_device_features_36_V_104_183 = {
    .has_usc_f16sop_u8 = true,
    .has_usc_itrsmp = true,
    .has_usc_itrsmp_enhanced = true,
+   .has_usc_itr_parallel_instances = true,
    .has_usc_min_output_registers_per_pix = true,
    .has_usc_pixel_partition_mask = true,
    .has_usc_slots = true,
@@ -194,6 +195,7 @@ static const struct pvr_device_features pvr_device_features_36_V_104_183 = {
    .tile_size_y = 16U,
    .tpu_parallel_instances = 4U,
    .unified_store_depth = 256U,
+   .usc_itr_parallel_instances = 16U,
    .usc_min_output_registers_per_pix = 2U,
    .usc_slots = 64U,
    .uvs_banks = 8U,
@@ -204,9 +206,6 @@ static const struct pvr_device_features pvr_device_features_36_V_104_183 = {
 
    /* Derived features. */
    .has_s8xe = true,
-   .has_usc_itr_parallel_instances = true,
-
-   .usc_itr_parallel_instances = 16U,
 };
 
 static const struct pvr_device_enhancements

src/imagination/common/device_info/ge8300.h
@@ -169,6 +169,7 @@ static const struct pvr_device_features pvr_device_features_22_V_54_38 = {
    .has_usc_f16sop_u8 = true,
    .has_usc_itrsmp = true,
    .has_usc_itrsmp_enhanced = true,
+   .has_usc_itr_parallel_instances = true,
    .has_usc_min_output_registers_per_pix = true,
    .has_usc_slots = true,
    .has_uvs_banks = true,
@@ -191,6 +192,7 @@ static const struct pvr_device_features pvr_device_features_22_V_54_38 = {
    .tile_size_y = 16U,
    .tpu_parallel_instances = 4U,
    .unified_store_depth = 208U,
+   .usc_itr_parallel_instances = 16U,
    .usc_min_output_registers_per_pix = 2U,
    .usc_slots = 64U,
    .uvs_banks = 4U,
@@ -199,9 +201,6 @@ static const struct pvr_device_features pvr_device_features_22_V_54_38 = {
    .vdm_cam_size = 64U,
 
    .has_s8xe = true,
-   .has_usc_itr_parallel_instances = true,
-
-   .usc_itr_parallel_instances = 16U,
 };
 
 static const struct pvr_device_enhancements