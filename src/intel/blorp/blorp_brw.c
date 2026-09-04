sailfishos-mirror/mesa

commit 37bca51d2fcc1b1e1a143e50575800780aa12d17

blorp: enable efficient 64bit support
CoAuthored-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43926>

src/intel/blorp/blorp_brw.c
@@ -40,6 +40,13 @@ blorp_nir_options_brw(struct blorp_context *blorp,
    return &compiler->nir_options[stage];
 }
 
+static nir_def *
+blorp_nir_rt_write(nir_builder *b, signed rt, void *data)
+{
+   assert(rt == 0);
+   return nir_load_push_data_intel(b, 1, 64, nir_imm_int(b, 0), .base = 0, .range = 8);
+}
+
 static struct blorp_program
 blorp_compile_fs_brw(struct blorp_context *blorp, void *mem_ctx,
                      struct nir_shader *nir,
@@ -51,6 +58,9 @@ blorp_compile_fs_brw(struct blorp_context *blorp, void *mem_ctx,
    const struct brw_compiler *compiler = blorp->compiler->brw;
 
    struct brw_fs_prog_data *fs_prog_data = rzalloc(mem_ctx, struct brw_fs_prog_data);
+
+   if (blorp->config.use_efficient_64bit)
+      fs_prog_data->base.push_sizes[0] = 32;
 
    nir_shader_gather_info(nir, nir_shader_get_entrypoint(nir));
 
@@ -70,6 +80,7 @@ blorp_compile_fs_brw(struct blorp_context *blorp, void *mem_ctx,
 
    struct brw_fs_prog_key wm_key;
    memset(&wm_key, 0, sizeof(wm_key));
+   wm_key.base.use_efficient_64bit = blorp->config.use_efficient_64bit;
    wm_key.multisample_fbo = multisample_fbo ? INTEL_ALWAYS : INTEL_NEVER;
    wm_key.nr_color_regions = 1;
 
@@ -86,6 +97,8 @@ blorp_compile_fs_brw(struct blorp_context *blorp, void *mem_ctx,
          .debug_flag = DEBUG_BLORP,
          .archiver = archiver,
       },
+
+      .rt_write_cb = blorp->config.use_efficient_64bit ? blorp_nir_rt_write : NULL,
 
       .use_rep_send = use_repclear,
       .max_polygons = 1,
@@ -238,6 +251,8 @@ blorp_compile_cs_brw(struct blorp_context *blorp, void *mem_ctx,
    struct brw_cs_prog_key cs_key;
    memset(&cs_key, 0, sizeof(cs_key));
 
+   cs_key.base.use_efficient_64bit = blorp->config.use_efficient_64bit;
+
    debug_archiver *archiver =
       blorp_debug_archiver_open(mem_ctx, nir, key, key_size);
 
@@ -285,7 +300,8 @@ blorp_params_get_layer_offset_vs_brw(struct blorp_batch *batch,
 {
    struct blorp_context *blorp = batch->blorp;
    struct layer_offset_vs_key blorp_key;
-   BLORP_KEY_INIT(blorp_key, BLORP_SHADER_TYPE_LAYER_OFFSET_VS,
+   BLORP_KEY_INIT(blorp_key, blorp,
+                  BLORP_SHADER_TYPE_LAYER_OFFSET_VS,
                   BLORP_SHADER_PIPELINE_RENDER);
 
    struct brw_fs_prog_data *fs_prog_data = params->fs_prog_data;

src/intel/blorp/blorp.c
@@ -284,6 +284,7 @@ blorp_params_init(struct blorp_params *params,
    params->num_samples = 1;
    params->num_draw_buffers = 1;
    params->num_layers = 1;
+   params->use_efficient_64bit = blorp->config.use_efficient_64bit;
 }
 
 void

src/intel/blorp/blorp.h
@@ -71,6 +71,7 @@ struct blorp_config {
    bool use_mesh_shading;
    bool use_unrestricted_depth_range;
    bool use_cached_dynamic_states;
+   bool use_efficient_64bit;
 };
 
 enum blorp_dynamic_state {

src/intel/blorp/blorp_blit.c
@@ -173,6 +173,9 @@ struct blorp_builder {
    const struct blorp_context *blorp;
    const struct blorp_blit_prog_key *key;
 
+   nir_def *surface_states;
+   nir_def *sampler_state;
+
    /* Input values from blorp_wm_inputs */
    nir_variable *v_bounds_rect;
    nir_variable *v_rect_grid;
@@ -193,6 +196,24 @@ blorp_builder_init(struct blorp_builder *bb,
 {
    bb->key = key;
    bb->blorp = blorp;
+
+   if (blorp->config.use_efficient_64bit) {
+      if (b->shader->info.stage == MESA_SHADER_FRAGMENT) {
+         bb->surface_states =
+            nir_load_push_data_intel(b, 2, 32, nir_imm_int(b, 0),
+                                     .base = 0, .range = 8);
+         bb->sampler_state =
+            nir_load_push_data_intel(b, 2, 32, nir_imm_int(b, 0),
+                                     .base = 8, .range = 8);
+      } else {
+         bb->surface_states =
+            nir_load_inline_data_intel(b, 2, 32, nir_imm_int(b, 0),
+                                       .base = BLORP_INLINE_PARAM_SURFACES_LDW, .range = 8);
+         bb->sampler_state =
+            nir_load_inline_data_intel(b, 2, 32, nir_imm_int(b, 0),
+                                       .base = BLORP_INLINE_PARAM_SAMPLER_LDW, .range = 8);
+      }
+   }
 
 #define LOAD_INPUT(name, type)\
    bb->v_##name = BLORP_CREATE_NIR_INPUT(b->shader, blit.name, type);
@@ -301,12 +322,27 @@ blorp_create_nir_tex_instr(nir_builder *b, struct blorp_builder *bb,
    unsigned src_idx = 0;
    if (src.src.ssa != NULL)
       tex->src[src_idx++] = src;
-   tex->src[src_idx++] = nir_tex_src_for_ssa(
-      nir_tex_src_texture_offset,
-      nir_imm_int(b, binding));
-   tex->src[src_idx++] = nir_tex_src_for_ssa(
-      nir_tex_src_sampler_offset,
-      nir_imm_int(b, BLORP_SAMPLER_INDEX));
+   if (bb->key->base.efficient_64bit) {
+      tex->src[src_idx++] = nir_tex_src_for_ssa(
+         nir_tex_src_texture_handle,
+         nir_vec2(b,
+                  nir_iadd_imm(b,
+                               nir_channel(b, bb->surface_states, 0),
+                               align(bb->blorp->isl_dev->ss.size,
+                                     bb->blorp->isl_dev->ss.align) *
+                               binding),
+                  nir_channel(b, bb->surface_states, 1)));
+      tex->src[src_idx++] = nir_tex_src_for_ssa(
+         nir_tex_src_sampler_handle,
+         bb->sampler_state);
+   } else {
+      tex->src[src_idx++] = nir_tex_src_for_ssa(
+         nir_tex_src_texture_offset,
+         nir_imm_int(b, binding));
+      tex->src[src_idx++] = nir_tex_src_for_ssa(
+         nir_tex_src_sampler_offset,
+         nir_imm_int(b, BLORP_SAMPLER_INDEX));
+   }
 
    /* To properly handle 3-D and 2-D array textures, we pull the Z component
     * from an input.  TODO: This is a bit magic; we should probably make this
@@ -1680,16 +1716,29 @@ blorp_build_nir_shader(struct blorp_context *blorp,
 
          store_pos = nir_vector_insert_imm(&b, store_pos, z_pos, 2);
       }
-      nir_image_store(&b, nir_imm_int(&b, 0),
-                      nir_pad_vector_imm_int(&b, store_pos, 0, 4),
-                      sample_idx,
-                      nir_pad_vector_imm_int(&b, color, 0, 4),
-                      nir_imm_int(&b, 0),
-                      .image_dim = key->dst_samples > 1 ?
-                                   GLSL_SAMPLER_DIM_MS:
-                                   GLSL_SAMPLER_DIM_2D,
-                      .image_array = true,
-                      .access = ACCESS_NON_READABLE);
+      if (key->base.efficient_64bit) {
+         nir_bindless_image_store(&b, bb.surface_states,
+                                  nir_pad_vector_imm_int(&b, store_pos, 0, 4),
+                                  sample_idx,
+                                  nir_pad_vector_imm_int(&b, color, 0, 4),
+                                  nir_imm_int(&b, 0),
+                                  .image_dim = key->dst_samples > 1 ?
+                                               GLSL_SAMPLER_DIM_MS:
+                                  GLSL_SAMPLER_DIM_2D,
+                                  .image_array = true,
+                                  .access = ACCESS_NON_READABLE);
+      } else {
+         nir_image_store(&b, nir_imm_int(&b, 0),
+                         nir_pad_vector_imm_int(&b, store_pos, 0, 4),
+                         sample_idx,
+                         nir_pad_vector_imm_int(&b, color, 0, 4),
+                         nir_imm_int(&b, 0),
+                         .image_dim = key->dst_samples > 1 ?
+                                      GLSL_SAMPLER_DIM_MS:
+                                      GLSL_SAMPLER_DIM_2D,
+                         .image_array = true,
+                         .access = ACCESS_NON_READABLE);
+      }
    } else if (key->dst_usage == ISL_SURF_USAGE_RENDER_TARGET_BIT) {
       nir_variable *color_out =
          nir_variable_create(b.shader, nir_var_shader_out,
@@ -2974,7 +3023,7 @@ blorp_blit(struct blorp_batch *batch,
       isl_format_get_layout(params.src.view.format);
 
    struct blorp_blit_prog_key key;
-   BLORP_KEY_INIT(key, BLORP_SHADER_TYPE_BLIT,
+   BLORP_KEY_INIT(key, batch->blorp, BLORP_SHADER_TYPE_BLIT,
                   (compute ? BLORP_SHADER_PIPELINE_COMPUTE :
                              BLORP_SHADER_PIPELINE_RENDER));
    key.filter = filter;
@@ -3527,7 +3576,7 @@ blorp_copy(struct blorp_batch *batch,
                            dst_layer, ISL_FORMAT_UNSUPPORTED, true);
 
    struct blorp_blit_prog_key key;
-   BLORP_KEY_INIT(key, BLORP_SHADER_TYPE_COPY,
+   BLORP_KEY_INIT(key, batch->blorp, BLORP_SHADER_TYPE_COPY,
                   (compute ? BLORP_SHADER_PIPELINE_COMPUTE :
                              BLORP_SHADER_PIPELINE_RENDER));
    key.filter = BLORP_FILTER_NONE;

src/intel/blorp/blorp_clear.c
@@ -70,7 +70,8 @@ blorp_params_get_clear_kernel_fs(struct blorp_batch *batch,
    struct blorp_context *blorp = batch->blorp;
 
    struct blorp_const_color_prog_key blorp_key;
-   BLORP_KEY_INIT(blorp_key, BLORP_SHADER_TYPE_CLEAR,
+   BLORP_KEY_INIT(blorp_key, blorp,
+                  BLORP_SHADER_TYPE_CLEAR,
                   BLORP_SHADER_PIPELINE_RENDER);
    blorp_key.is_fast_clear = is_fast_clear;
    blorp_key.use_simd16_replicated_data = use_replicated_data;
@@ -134,7 +135,8 @@ blorp_params_get_clear_kernel_cs(struct blorp_batch *batch,
    struct blorp_context *blorp = batch->blorp;
 
    struct blorp_const_color_prog_key blorp_key;
-   BLORP_KEY_INIT(blorp_key, BLORP_SHADER_TYPE_CLEAR,
+   BLORP_KEY_INIT(blorp_key, blorp,
+                  BLORP_SHADER_TYPE_CLEAR,
                   BLORP_SHADER_PIPELINE_COMPUTE);
    blorp_key.use_simd16_replicated_data = false;
    blorp_key.clear_rgb_as_red = clear_rgb_as_red;
@@ -189,16 +191,35 @@ blorp_params_get_clear_kernel_cs(struct blorp_batch *batch,
                          nir_imm_int(&b, 0));
    }
 
-   nir_image_store(&b, nir_imm_int(&b, 0),
-                   nir_pad_vector_imm_int(&b, dst_pos, 0, 4),
-                   sample_idx,
-                   nir_pad_vector_imm_int(&b, color, 0, 4),
-                   nir_imm_int(&b, 0),
-                   .image_dim = params->num_samples > 1 ?
-                                GLSL_SAMPLER_DIM_MS :
-                                GLSL_SAMPLER_DIM_2D,
-                   .image_array = true,
-                   .access = ACCESS_NON_READABLE);
+   if (blorp->config.use_efficient_64bit) {
+      nir_bindless_image_store(&b,
+                               b.shader->info.stage == MESA_SHADER_FRAGMENT ?
+                               nir_load_push_data_intel(
+                                  &b, 2, 32, nir_imm_int(&b, 0), .base = 0, .range = 8) :
+                               nir_load_inline_data_intel(
+                                  &b, 2, 32, nir_imm_int(&b, 0),
+                                  .base = BLORP_INLINE_PARAM_SURFACES_LDW, .range = 8),
+                               nir_pad_vector_imm_int(&b, dst_pos, 0, 4),
+                               sample_idx,
+                               nir_pad_vector_imm_int(&b, color, 0, 4),
+                               nir_imm_int(&b, 0),
+                               .image_dim = params->num_samples > 1 ?
+                                            GLSL_SAMPLER_DIM_MS :
+                                            GLSL_SAMPLER_DIM_2D,
+                               .image_array = true,
+                               .access = ACCESS_NON_READABLE);
+   } else {
+      nir_image_store(&b, nir_imm_int(&b, 0),
+                      nir_pad_vector_imm_int(&b, dst_pos, 0, 4),
+                      sample_idx,
+                      nir_pad_vector_imm_int(&b, color, 0, 4),
+                      nir_imm_int(&b, 0),
+                      .image_dim = params->num_samples > 1 ?
+                                   GLSL_SAMPLER_DIM_MS :
+                                   GLSL_SAMPLER_DIM_2D,
+                      .image_array = true,
+                      .access = ACCESS_NON_READABLE);
+   }
 
    nir_pop_if(&b, NULL);
 
@@ -1515,7 +1536,8 @@ blorp_params_get_mcs_partial_resolve_kernel(struct blorp_batch *batch,
    struct blorp_context *blorp = batch->blorp;
 
    struct blorp_mcs_partial_resolve_key blorp_key;
-   BLORP_KEY_INIT(blorp_key, BLORP_SHADER_TYPE_MCS_PARTIAL_RESOLVE,
+   BLORP_KEY_INIT(blorp_key, blorp,
+                  BLORP_SHADER_TYPE_MCS_PARTIAL_RESOLVE,
                   BLORP_SHADER_PIPELINE_RENDER);
    blorp_key.indirect_clear_color = params->dst.clear_color_addr.buffer != NULL;
    blorp_key.int_format = isl_format_has_int_channel(params->dst.view.format);

src/intel/blorp/blorp_elk.c
@@ -204,7 +204,8 @@ blorp_ensure_sf_program_elk(struct blorp_batch *batch,
       return true;
 
    struct blorp_sf_key key;
-   BLORP_KEY_INIT(key, BLORP_SHADER_TYPE_GFX4_SF,
+   BLORP_KEY_INIT(key, blorp,
+                  BLORP_SHADER_TYPE_GFX4_SF,
                   BLORP_SHADER_PIPELINE_RENDER);
 
    /* Everything gets compacted in vertex setup, so we just need a
@@ -271,7 +272,8 @@ blorp_params_get_layer_offset_vs_elk(struct blorp_batch *batch,
 {
    struct blorp_context *blorp = batch->blorp;
    struct layer_offset_vs_key blorp_key;
-   BLORP_KEY_INIT(blorp_key, BLORP_SHADER_TYPE_LAYER_OFFSET_VS,
+   BLORP_KEY_INIT(blorp_key, blorp,
+                  BLORP_SHADER_TYPE_LAYER_OFFSET_VS,
                   BLORP_SHADER_PIPELINE_RENDER);
 
    struct elk_fs_prog_data *fs_prog_data = params->fs_prog_data;

src/intel/blorp/blorp_indirect_copy.c
@@ -300,7 +300,8 @@ blorp_copy_memory_to_image_indirect(struct blorp_batch *batch,
    int dimensions = img_blorp_surf->surf->dim + 1;
 
    struct blorp_indirect_copy_mem2img_key key;
-   BLORP_KEY_INIT(key, BLORP_SHADER_TYPE_COPY_INDIRECT,
+   BLORP_KEY_INIT(key, batch->blorp,
+                  BLORP_SHADER_TYPE_COPY_INDIRECT,
                   BLORP_SHADER_PIPELINE_COMPUTE);
    key.dimensions = dimensions;
    key.forced_layer_or_z = forced_layer_or_z;

commit 6c27f841103f521b25645696e5c7430a3e96b1e4

blorp: compile VS with efficient 64bit if needed
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 37bca51d2f ("blorp: enable efficient 64bit support")
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44239>

src/intel/blorp/blorp_brw.c
@@ -136,7 +136,11 @@ blorp_compile_vs_brw(struct blorp_context *blorp, void *mem_ctx,
                        nir->info.separate_shader,
                        1);
 
-   struct brw_vs_prog_key vs_key = { 0, };
+   struct brw_vs_prog_key vs_key = {
+      .base = {
+         .use_efficient_64bit = blorp->config.use_efficient_64bit,
+      },
+   };
 
    debug_archiver *archiver =
       blorp_debug_archiver_open(mem_ctx, nir, key, key_size);