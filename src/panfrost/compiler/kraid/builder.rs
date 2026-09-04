sailfishos-mirror/mesa

commit 13e6e00b1a56a21392bdd43c8f1154eb4063cfa4

kraid: Implement derivatives
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43321>

src/panfrost/compiler/kraid/builder.rs
@@ -3,6 +3,7 @@
 
 use std::f32::consts::PI;
 
+use crate::data_type::*;
 use crate::ir::*;
 use crate::ops::*;
 use crate::ssa_value::{AllocSSA, SSAValueAllocator};
@@ -81,6 +82,11 @@ pub trait Builder {
             offset: offset.try_into().unwrap(),
         });
     }
+}
+
+pub enum DerivativeAxis {
+    X,
+    Y,
 }
 
 pub trait SSABuilder: Builder + AllocSSA {
@@ -208,6 +214,120 @@ pub trait SSABuilder: Builder + AllocSSA {
             _ => panic!("Unsupported bit size: {bits}"),
         }
         dst_vec
+    }
+
+    fn and(&mut self, dst_type: DataType, x: Src, y: Src) -> SSARef {
+        let dst = self.alloc_ref(dst_type.total_bits().into());
+        self.push_op(OpShiftLop {
+            dst: dst.clone().into(),
+            dst_type,
+            shift_op: ShiftOp::None,
+            logic_op: LogicOp::And,
+            not_result: false,
+            src0: x,
+            shift: 0_u8.into(),
+            src2: y,
+        });
+        dst
+    }
+
+    fn derivative(
+        &mut self,
+        dst_type: DataType,
+        src: Src,
+        axis: DerivativeAxis,
+        coarse: bool,
+        sign_is_ignored: bool,
+    ) -> SSAValue {
+        let bits = dst_type.bits();
+        let comps = dst_type.comps();
+        debug_assert!(bits == 16 || bits == 32);
+        debug_assert!(bits * comps <= 32);
+
+        let lane = match axis {
+            DerivativeAxis::X => 1_u8,
+            DerivativeAxis::Y => 2_u8,
+        };
+
+        let left = self.alloc_ssa(dst_type.total_bits());
+        let right = self.alloc_ssa(dst_type.total_bits());
+        if coarse {
+            self.push_op(OpClper {
+                dst: left.into(),
+                subgroup: SubgroupSize::Subgroup4,
+                lane_op: ClperLaneOp::None,
+                inactive: ClperInactiveResult::Zero,
+                data: src.clone(),
+                lane: 0_u8.into(),
+            });
+            self.push_op(OpClper {
+                dst: right.into(),
+                subgroup: SubgroupSize::Subgroup4,
+                lane_op: ClperLaneOp::None,
+                inactive: ClperInactiveResult::Zero,
+                data: src.clone(),
+                lane: lane.into(),
+            });
+        } else {
+            self.copy_i32_to(left.into(), src.clone());
+            self.push_op(OpClper {
+                dst: right.into(),
+                subgroup: SubgroupSize::Subgroup4,
+                lane_op: ClperLaneOp::Xor,
+                inactive: ClperInactiveResult::Zero,
+                data: src.clone(),
+                lane: lane.into(),
+            });
+        }
+
+        let diff = self.alloc_ssa(32);
+        self.push_op(OpFAdd {
+            dst: diff.into(),
+            dst_type,
+            round: FRound::NearestEven,
+            clamp: FClamp::None,
+            srcs: [Src::from(right), Src::from(left).fneg()],
+        });
+
+        if coarse || sign_is_ignored {
+            return diff;
+        }
+
+        // If the user cares about the sign, we need to take into account
+        // the fact that left/right (or top/bottom) might be inverted.
+        // Instead of using a couple CSEL, we just invert the sign bit with
+        //
+        //  sign_bit = XOR(sign_bit, axis_bit(lane_id)).
+        let lane = self.model().fau().special(SpecialFAU::LaneId).unwrap();
+        let mut lane = Src::from(lane);
+        if bits == 16 {
+            lane = lane.half(0);
+        }
+
+        let shift = match axis {
+            DerivativeAxis::X => bits - 1_u8,
+            DerivativeAxis::Y => bits - 2_u8,
+        };
+
+        // Clear the low bit before the shift if this is the Y-axis we want.
+        // We skip it on the X-axis, because the lshift-by-31 will get us a
+        // clean mask.
+        if matches!(axis, DerivativeAxis::Y) {
+            lane = self.and(DataType::u(bits), 2_u8.into(), lane).into();
+        }
+
+        let dst = self.alloc_ssa(32);
+        self.push_op(OpShiftLop {
+            dst: dst.into(),
+            dst_type: DataType::get(comps, NumericType::UnsignedInteger, bits),
+            shift_op: ShiftOp::LShift,
+            logic_op: LogicOp::Xor,
+            not_result: false,
+            src0: lane,
+            shift: shift.into(),
+            src2: diff.into(),
+        });
+        dst
     }
 
     /// Computes base**arg

src/panfrost/compiler/kraid/nir.rs
@@ -1501,6 +1501,41 @@ impl<'a> ShaderFromNir<'a> {
                     idx,
                 });
             }
+            nir_intrinsic_ddx
+            | nir_intrinsic_ddx_fine
+            | nir_intrinsic_ddx_coarse
+            | nir_intrinsic_ddy
+            | nir_intrinsic_ddy_fine
+            | nir_intrinsic_ddy_coarse => {
+                let dst_type = DataType::get(
+                    intrin.def.num_components,
+                    NumericType::Float,
+                    intrin.def.bit_size,
+                );
+                let axis = match intrin.intrinsic {
+                    nir_intrinsic_ddx
+                    | nir_intrinsic_ddx_fine
+                    | nir_intrinsic_ddx_coarse => DerivativeAxis::X,
+                    nir_intrinsic_ddy
+                    | nir_intrinsic_ddy_fine
+                    | nir_intrinsic_ddy_coarse => DerivativeAxis::Y,
+                    _ => unreachable!(),
+                };
+                let coarse = matches!(
+                    intrin.intrinsic,
+                    nir_intrinsic_ddx_coarse | nir_intrinsic_ddy_coarse
+                );
+                let sign_is_ignored =
+                    unsafe { nir_def_all_uses_ignore_sign_bit(&intrin.def) };
+                let ssa = b.derivative(
+                    dst_type,
+                    self.get_src(&srcs[0]),
+                    axis,
+                    coarse,
+                    sign_is_ignored,
+                );
+                self.set_ssa(&intrin.def, vec![ssa]);
+            }
             nir_intrinsic_global_atomic => {
                 let atom_op = match intrin.atomic_op() {
                     nir_atomic_op_iadd => AtomOp::IAdd,

commit d5ce535d1a07be6393251d4505b2e427da46cfb4

kraid/builder: Fix 16-bit derivatives
16-bit derivatives were not tested and produced illegal instructions.
Since scalarize_ddx is true, we can simplify things for now, in the
future we might want to compute 2x16 derivatives at once.

Fixes: 13e6e00b1a5 ("kraid: Implement derivatives")
Signed-off-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44182>

src/panfrost/compiler/kraid/builder.rs
@@ -245,15 +245,15 @@ pub trait SSABuilder: Builder + AllocSSA {
         let bits = dst_type.bits();
         let comps = dst_type.comps();
         debug_assert!(bits == 16 || bits == 32);
-        debug_assert!(bits * comps <= 32);
+        debug_assert!(comps == 1);
 
         let lane = match axis {
             DerivativeAxis::X => 1_u8,
             DerivativeAxis::Y => 2_u8,
         };
 
-        let left = self.alloc_ssa(dst_type.total_bits());
-        let right = self.alloc_ssa(dst_type.total_bits());
+        let left = self.alloc_ssa(32);
+        let right = self.alloc_ssa(32);
         if coarse {
             self.push_op(OpClper {
                 dst: left.into(),
@@ -283,13 +283,23 @@ pub trait SSABuilder: Builder + AllocSSA {
             });
         }
 
-        let diff = self.alloc_ssa(32);
+        // CLPER works in 32-bits, but we actually only need 16 bits
+        let swiz = if bits == 16 {
+            Swizzle::replicate_half(0)
+        } else {
+            Swizzle::NONE
+        };
+
+        let diff = self.alloc_ssa(dst_type.total_bits());
         self.push_op(OpFAdd {
             dst: diff.into(),
             dst_type,
             round: FRound::NearestEven,
             clamp: FClamp::None,
-            srcs: [Src::from(right), Src::from(left).fneg()],
+            srcs: [
+                Src::from(right).swizzle(swiz),
+                Src::from(left).swizzle(swiz).fneg(),
+            ],
         });
 
         if coarse || sign_is_ignored {
@@ -316,10 +326,11 @@ pub trait SSABuilder: Builder + AllocSSA {
         // We skip it on the X-axis, because the lshift-by-31 will get us a
         // clean mask.
         if matches!(axis, DerivativeAxis::Y) {
-            lane = self.and(DataType::u(bits), 2_u8.into(), lane).into();
+            let mask = Src::from(2_u32).swizzle(swiz);
+            lane = self.and(DataType::u(bits), mask, lane).into();
         }
 
-        let dst = self.alloc_ssa(32);
+        let dst = self.alloc_ssa(dst_type.total_bits());
         self.push_op(OpShiftLop {
             dst: dst.into(),
             dst_type: DataType::get(comps, NumericType::UnsignedInteger, bits),