sailfishos-mirror/mesa

commit c933dcfc2a83076ee16ea6a73d65e6a1f1994108

pan/nir_opt_push_ubo: Re-order pushed UBO entries
This is a direct port the re-ordering algorithm from the bifrost pass

Shader-db results on v13 (VS only):

    total instrs in shared programs: 757920 -> 731737 (-3.45%)
    instrs in affected programs: 212663 -> 186480 (-12.31%)
    helped: 3641
    HURT: 81

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43476>

src/panfrost/compiler/pan_nir_opt_push_ubo.c
@@ -27,6 +27,10 @@ struct pushable_ubo {
 
 static_assert(PAN_MAX_PUSH <= 256, "We assume an FAU index fits in a uint8_t");
 
+typedef struct {
+   BITSET_DECLARE(row, PAN_MAX_PUSH);
+} adjacency_row;
+
 struct opt_push_ubo_ctx {
    struct pan_fau_layout *fau;
 
@@ -39,6 +43,9 @@ struct opt_push_ubo_ctx {
    /* Per block analysis */
    unsigned nr_ubos;
    struct pushable_ubo *ubos;
+
+   /* Interference graph for push re-ordering */
+   adjacency_row adjacency[PAN_MAX_PUSH];
 };
 
 struct ubo_range {
@@ -172,6 +179,157 @@ pick_ubo_push_words(struct opt_push_ubo_ctx *ctx)
    }
 }
 
+/*
+ * Create an undirected graph where nodes are 32-bit uniform indices and edges
+ * represent that two nodes are used in the same instruction.
+ *
+ * The graph is constructed as an adjacency matrix stored in ctx->adjacency.
+ */
+static bool
+analyze_alu_intr(nir_builder *b, nir_alu_instr *alu, void *data)
+{
+   struct opt_push_ubo_ctx *ctx = data;
+   if (nir_op_is_vec_or_mov(alu->op))
+      return false;
+
+   uint8_t nodes[NIR_MAX_VEC_COMPONENTS * 2];
+   uint8_t node_count = 0;
+
+   for (unsigned i = 0; i < nir_op_infos[alu->op].num_inputs; i++) {
+      /* We only care about the first swizzle component since this pass should
+       * be run after we've already reduced ALU widths down to where we only
+       * really access one word per ALU op unless it's 64-bit.
+       *
+       * In theory, it might be useful to chase [un]pack but this pass is
+       * only ever used for OpenGL where most uniforms are 32-bit.
+       */
+      nir_scalar s =
+         nir_scalar_resolved(alu->src[i].src.ssa, alu->src[i].swizzle[0]);
+
+      nir_instr *s_instr = nir_def_instr(s.def);
+      if (s_instr->type != nir_instr_type_intrinsic)
+         continue;
+
+      const struct ubo_range range =
+         get_pushable_ubo_range(nir_instr_as_intrinsic(s_instr), ctx);
+      if (range.ubo_idx < 0 || range.nr_words < 0)
+         continue;
+
+      assert(BITSET_TEST(ctx->ubos[range.ubo_idx].pushed, range.word));
+      uint8_t fau_word = ctx->ubos[range.ubo_idx].range_idx[range.word];
+      assert(fau_word < PAN_MAX_PUSH);
+      assert(!BITSET_TEST(ctx->fau->is_const, fau_word));
+      assert(ctx->fau->words[fau_word].relocation.ubo == range.ubo_idx);
+      assert(ctx->fau->words[fau_word].relocation.offset == range.word * 4);
+
+      /* Offset by the swizzle, if any.  We only care about the first swizzle
+       * component since this pass should be run after we've already reduced
+       * ALU widths down to where we only really access one word per ALU op
+       * unless it's 64-bit.
+       */
+      fau_word += (s.comp * s.def->bit_size) / 32;
+
+      nodes[node_count++] = fau_word;
+      if (s.def->bit_size == 64)
+         nodes[node_count++] = fau_word + 1;
+   }
+
+   /* Create clique connecting nodes[] */
+   for (unsigned i = 0; i < node_count; ++i) {
+      for (unsigned j = 0; j < node_count; ++j) {
+         if (i == j)
+            continue;
+
+         unsigned x = nodes[i], y = nodes[j];
+
+         /* Add undirected edge between the nodes */
+         BITSET_SET(ctx->adjacency[x].row, y);
+         BITSET_SET(ctx->adjacency[y].row, x);
+      }
+   }
+
+   return false;
+}
+
+/* Find the connected component containing `node` with depth-first search */
+static void
+find_component(const adjacency_row *adjacency, BITSET_WORD *visited,
+               uint8_t *component, uint8_t *size, uint8_t node)
+{
+   uint32_t neighbour;
+
+   BITSET_SET(visited, node);
+   component[(*size)++] = node;
+
+   BITSET_FOREACH_SET(neighbour, adjacency[node].row, PAN_MAX_PUSH) {
+      if (!BITSET_TEST(visited, neighbour)) {
+         find_component(adjacency, visited, component, size, neighbour);
+      }
+   }
+}
+
+/*
+ * Optimization pass to reorder uniforms. The goal is to reduce the number of
+ * moves we emit when lowering FAU. The pass groups uniforms used by the same
+ * ALU instruction.
+ *
+ * The pass works by creating a graph of pushed uniforms, where edges denote
+ * the "both 32-bit uniforms required by the same instruction" relationship.
+ * This is done by analyze_alu_intr() above.  We then perform depth-first
+ * search on this graph to find the connected components, where each connected
+ * component is a cluster of uniforms that are used together. We then select
+ * pairs of uniforms from each connected component.  The remaining unpaired
+ * uniforms (from components of odd sizes) are paired together arbitrarily.
+ */
+static void
+reorder_ubo_push_words(struct opt_push_ubo_ctx *ctx)
+{
+   BITSET_DECLARE(visited, PAN_MAX_PUSH) = {0};
+
+   uint8_t ordering[PAN_MAX_PUSH] = {0};
+   uint8_t unpaired[PAN_MAX_PUSH] = {0};
+   uint8_t pushed = 0, unpaired_count = 0;
+
+   for (unsigned i = 0; i < ctx->fau->count; i++) {
+      /* We're the only thing to push anything so far */
+      assert(!BITSET_TEST(ctx->fau->is_const, i));
+      if (BITSET_TEST(visited, i))
+         continue;
+
+      uint8_t component[PAN_MAX_PUSH] = {0};
+      uint8_t size = 0;
+      find_component(ctx->adjacency, visited, component, &size, i);
+
+      /* If there is an odd number of uses, at least one use must be
+       * unpaired. Arbitrarily take the last one.
+       */
+      if (size % 2)
+         unpaired[unpaired_count++] = component[--size];
+
+      /* The rest of uses are paired */
+      assert((size % 2) == 0);
+
+      /* Push the paired uses */
+      assert(pushed + (unsigned)size < PAN_MAX_PUSH);
+      typed_memcpy(ordering + pushed, component, size);
+      pushed += size;
+   }
+
+   /* Push unpaired nodes at the end */
+   typed_memcpy(ordering + pushed, unpaired, unpaired_count);
+   pushed += unpaired_count;
+
+   assert(pushed == ctx->fau->count);
+
+   union pan_fau_entry fau_words[PAN_MAX_PUSH];
+   typed_memcpy(fau_words, ctx->fau->words, ctx->fau->count);
+
+   for (unsigned i = 0; i < pushed; i++) {
+      assert(ordering[i] < ctx->fau->count);
+      ctx->fau->words[i] = fau_words[ordering[i]];
+   }
+}
+
 static bool
 lower_ubo_intr(nir_builder *b, nir_intrinsic_instr *load, void *data)
 {
@@ -198,20 +356,27 @@ lower_ubo_intr(nir_builder *b, nir_intrinsic_instr *load, void *data)
       }
    }
 
-   uint32_t fau_word = ubo->range_idx[range.word];
-   assert(!BITSET_TEST(ctx->fau->is_const, fau_word));
-   assert(ctx->fau->words[fau_word].relocation.ubo == range.ubo_idx);
-   assert(ctx->fau->words[fau_word].relocation.offset == range.word * 4);
-
-   const uint16_t align_mul = nir_intrinsic_align_mul(load);
-   const uint16_t align_offset = nir_intrinsic_align_offset(load);
-
    b->cursor = nir_before_instr(&load->instr);
-   nir_def *val = nir_load_push_constant(b, load->def.num_components,
-                                            load->def.bit_size,
-                                            nir_imm_int(b, fau_word * 4),
-                                            .align_mul = align_mul,
-                                            .align_offset = align_offset);
+
+   /* After re-ordering, we can't use word_idx anymore and we have to just
+    * search for the result.  We could theoretically plumb the ordering
+    * array through here but that would get fragile.
+    *
+    * We also can't assume that load_ubo are contiguous so we need to break
+    * it into per-word loads.  Fortunately, Kraid should be able to clean up
+    * this mess.
+    */
+   nir_def *words[NIR_MAX_VEC_COMPONENTS * 2];
+   for (unsigned w = 0; w < range.nr_words; w++) {
+      uint32_t fau_word =
+         pan_lookup_pushed_ubo(ctx->fau, range.ubo_idx, (range.word + w) * 4);
+      words[w] = nir_load_push_constant(b, 1, 32, nir_imm_int(b, fau_word * 4),
+                                        .align_mul = 4, .align_offset = 0);
+   }
+
+   nir_def *val = nir_extract_bits(b, words, range.nr_words, 0,
+                                   load->def.num_components,
+                                   load->def.bit_size);
    nir_def_replace(&load->def, val);
 
    return true;
@@ -239,6 +404,11 @@ pan_nir_opt_push_ubo(nir_shader *nir,
       add_blend_constants(&ctx);
 
    pick_ubo_push_words(&ctx);
+
+   /* Analyze ALU instructions to build the interference graph */
+   nir_shader_alu_pass(nir, analyze_alu_intr, nir_metadata_all, &ctx);
+
+   reorder_ubo_push_words(&ctx);
 
    bool progress = nir_shader_intrinsics_pass(nir, lower_ubo_intr,
                                               nir_metadata_control_flow, &ctx);

commit fd47e278dc0f851d7b7beeb68984b1a98e5993de

pan/nir_opt_push_ubo: Don't reorder blend constants
Fixes: c933dcfc2a8 ("pan/nir_opt_push_ubo: Re-order pushed UBO entries")
Signed-off-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44182>

src/panfrost/compiler/pan_nir_opt_push_ubo.c
@@ -34,6 +34,9 @@ typedef struct {
 struct opt_push_ubo_ctx {
    struct pan_fau_layout *fau;
 
+   /* Start index of what we can reorder */
+   unsigned reord_start;
+
    /* Mask of UBOs which may be pushed */
    uint32_t pushable_ubos;
 
@@ -229,6 +232,10 @@ analyze_alu_intr(nir_builder *b, nir_alu_instr *alu, void *data)
        */
       fau_word += (s.comp * s.def->bit_size) / 32;
 
+      /* Ignore non-reorderable words */
+      if (fau_word < ctx->reord_start)
+         continue;
+
       nodes[node_count++] = fau_word;
       if (s.def->bit_size == 64)
          nodes[node_count++] = fau_word + 1;
@@ -288,9 +295,9 @@ reorder_ubo_push_words(struct opt_push_ubo_ctx *ctx)
 
    uint8_t ordering[PAN_MAX_PUSH] = {0};
    uint8_t unpaired[PAN_MAX_PUSH] = {0};
-   uint8_t pushed = 0, unpaired_count = 0;
+   uint8_t pushed = ctx->reord_start, unpaired_count = 0;
 
-   for (unsigned i = 0; i < ctx->fau->count; i++) {
+   for (unsigned i = ctx->reord_start; i < ctx->fau->count; i++) {
       /* We're the only thing to push anything so far */
       assert(!BITSET_TEST(ctx->fau->is_const, i));
       if (BITSET_TEST(visited, i))
@@ -324,7 +331,7 @@ reorder_ubo_push_words(struct opt_push_ubo_ctx *ctx)
    union pan_fau_entry fau_words[PAN_MAX_PUSH];
    typed_memcpy(fau_words, ctx->fau->words, ctx->fau->count);
 
-   for (unsigned i = 0; i < pushed; i++) {
+   for (unsigned i = ctx->reord_start; i < pushed; i++) {
       assert(ordering[i] < ctx->fau->count);
       ctx->fau->words[i] = fau_words[ordering[i]];
    }
@@ -402,6 +409,7 @@ pan_nir_opt_push_ubo(nir_shader *nir,
    /* We first pick the blend constants, those cannot be reordered */
    if (nir->info.stage == MESA_SHADER_FRAGMENT)
       add_blend_constants(&ctx);
+   ctx.reord_start = fau->count;
 
    pick_ubo_push_words(&ctx);
 