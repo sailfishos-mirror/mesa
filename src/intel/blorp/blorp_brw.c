sailfishos-mirror/mesa

commit 54947c0f3794da2602afffa6a18ffefca1d1c755

brw/nir: prepare for render target writes in efficient 64bit mode
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43926>

src/intel/compiler/intel_nir_lower_fragment_outputs.c
@@ -81,7 +81,8 @@ gather_colour_components(nir_builder *b,
 }
 
 static void
-insert_rt_store(nir_builder *b, struct frag_out_ctx *ctx, signed target)
+insert_rt_store(nir_builder *b, struct frag_out_ctx *ctx, signed target,
+                intel_nir_rt_write_cb cb, void *cb_data)
 {
    const unsigned src0_alpha_loc =
       FRAG_RESULT_DATA0 + (ctx->replicate_alpha ? 0 : MAX2(target, 0));
@@ -102,7 +103,9 @@ insert_rt_store(nir_builder *b, struct frag_out_ctx *ctx, signed target)
    if (colour == NULL && dual_colour != NULL)
       colour = dual_colour;
 
-   nir_store_render_target_intel(b, colour, dual_colour, src0_alpha,
+   nir_store_render_target_intel(b,
+                                 cb != NULL ? cb(b, target, cb_data) : nir_undef(b, 1, 64),
+                                 colour, dual_colour, src0_alpha,
                                  ctx->outputs[FRAG_RESULT_SAMPLE_MASK],
                                  ctx->outputs[FRAG_RESULT_DEPTH],
                                  ctx->outputs[FRAG_RESULT_STENCIL],
@@ -112,7 +115,9 @@ insert_rt_store(nir_builder *b, struct frag_out_ctx *ctx, signed target)
 bool
 intel_nir_lower_fragment_outputs(nir_shader *shader,
                                  unsigned nr_colour_regions,
-                                 bool replicate_alpha)
+                                 bool replicate_alpha,
+                                 intel_nir_rt_write_cb cb,
+                                 void *cb_data)
 {
    nir_function_impl *impl = nir_shader_get_entrypoint(shader);
    nir_builder b_ = nir_builder_at(nir_after_impl(impl));
@@ -133,7 +138,7 @@ intel_nir_lower_fragment_outputs(nir_shader *shader,
    if (ctx.dual_blend) {
       gather_colour_components(b, &ctx, FRAG_RESULT_DATA0, undef);
       gather_colour_components(b, &ctx, FRAG_RESULT_DUAL_SRC_BLEND, undef);
-      insert_rt_store(b, &ctx, 0);
+      insert_rt_store(b, &ctx, 0, cb, cb_data);
       return true;
    }
    ctx.outputs[FRAG_RESULT_DUAL_SRC_BLEND] = nir_undef(b, 4, 32);
@@ -141,7 +146,7 @@ intel_nir_lower_fragment_outputs(nir_shader *shader,
    bool written = false;
    for (unsigned i = 0; i < nr_colour_regions; i++) {
       if (gather_colour_components(b, &ctx, FRAG_RESULT_DATA0 + i, undef)) {
-         insert_rt_store(b, &ctx, i);
+         insert_rt_store(b, &ctx, i, cb, cb_data);
          written = true;
       }
    }
@@ -154,7 +159,7 @@ intel_nir_lower_fragment_outputs(nir_shader *shader,
          ctx.colour[FRAG_RESULT_DATA0][c] = nir_get_scalar(undef, 0);
       gather_colour_components(b, &ctx, FRAG_RESULT_DATA0, undef);
 
-      insert_rt_store(b, &ctx, -1);
+      insert_rt_store(b, &ctx, -1, NULL, NULL);
    }
 
    return true;

src/compiler/nir/nir_intrinsics.py
@@ -2786,8 +2786,8 @@ intrinsic("load_reloc_const_intel", dest_comp=1, bit_sizes=[32],
           indices=[PARAM_IDX, BASE], flags=[CAN_ELIMINATE, CAN_REORDER])
 
 # Write a render target
-# src[] = { color, dual_color, src0_alpha, omask, depth, stencil }
-intrinsic("store_render_target_intel", [4, 4, 1, 1, 1, 1], indices=[TARGET], bit_sizes=[32, 32, 32, 32, 32, 32])
+# src[] = { surface, color, dual_color, src0_alpha, omask, depth, stencil }
+intrinsic("store_render_target_intel", [1, 4, 4, 1, 1, 1, 1], indices=[TARGET], bit_sizes=[64, 32, 32, 32, 32, 32, 32])
 
 # Shuffle with an offset in bytes instead of a lane index.
 # src[] = { payload, lane offset in bytes }

src/intel/compiler/brw/brw_compile_fs.cpp
@@ -1299,7 +1299,9 @@ brw_compile_fs(const struct brw_compiler *compiler,
                 key->alpha_test_replicate_alpha ||
                 (key->nr_color_regions > 1 &&
                  alpha_to_coverage_enabled(nir, key->alpha_to_coverage) &&
-                 !(nir->info.outputs_written & BITFIELD64_BIT(FRAG_RESULT_SAMPLE_MASK))));
+                 !(nir->info.outputs_written & BITFIELD64_BIT(FRAG_RESULT_SAMPLE_MASK))),
+                params->rt_write_cb,
+                params->rt_write_data);
 
    int per_primitive_offsets[VARYING_SLOT_MAX];
    memset(per_primitive_offsets, -1, sizeof(per_primitive_offsets));

src/intel/compiler/brw/brw_compiler.h
@@ -18,6 +18,7 @@
 #include "util/shader_stats.h"
 #include "util/u_math.h"
 #include "brw_isa_info.h"
+#include "compiler/intel_nir.h"
 #include "compiler/intel_shader_enums.h"
 #include "compiler/intel_nir.h"
 #include "nir_shader_compiler_options.h"
@@ -1363,6 +1364,9 @@ struct brw_compile_fs_params {
     */
    void *wa_18019110168_data;
    nir_def *(*wa_18019110168_load_per_primitive_remap_table_offset)(nir_builder *b, void *data);
+
+   void *rt_write_data;
+   intel_nir_rt_write_cb rt_write_cb;
 };
 
 /**

src/intel/compiler/brw/brw_from_nir.cpp
@@ -4037,25 +4037,25 @@ brw_from_nir_emit_fs_intrinsic(nir_to_brw_state &ntb,
          ntb.bld.emit(SHADER_OPCODE_HALT_TARGET);
 
       brw_reg sources[FB_WRITE_LOGICAL_NUM_SRCS];
-      sources[FB_WRITE_LOGICAL_SRC_COLOR0] = get_nir_src(ntb, instr->src[0], -1);
-      if (!nir_src_is_undef(instr->src[1]))
-          sources[FB_WRITE_LOGICAL_SRC_COLOR1] = get_nir_src(ntb, instr->src[1], -1);
+      sources[FB_WRITE_LOGICAL_SRC_COLOR0] = get_nir_src(ntb, instr->src[1], -1);
+      if (!nir_src_is_undef(instr->src[2]))
+          sources[FB_WRITE_LOGICAL_SRC_COLOR1] = get_nir_src(ntb, instr->src[2], -1);
 
       /* If our alpha happens to match src0_alpha, we can skip sending it, as
        * the hardware will use our alpha in that case.
        */
-      if (!nir_src_is_undef(instr->src[2]) &&
+      if (!nir_src_is_undef(instr->src[3]) &&
           nir_intrinsic_target(instr) != 0 &&
-          !scalars_equal(nir_scalar_resolved(instr->src[2].ssa, 0),
-                         nir_scalar_resolved(instr->src[0].ssa, 3)))
-         sources[FB_WRITE_LOGICAL_SRC_SRC0_ALPHA] = get_nir_src(ntb, instr->src[2], 0);
+          !scalars_equal(nir_scalar_resolved(instr->src[3].ssa, 0),
+                         nir_scalar_resolved(instr->src[1].ssa, 3)))
+         sources[FB_WRITE_LOGICAL_SRC_SRC0_ALPHA] = get_nir_src(ntb, instr->src[3], 0);
 
-      if (!nir_src_is_undef(instr->src[3]) && fs_prog_data->uses_omask)
-         sources[FB_WRITE_LOGICAL_SRC_OMASK] = get_nir_src(ntb, instr->src[3], 0);
-      if (!nir_src_is_undef(instr->src[4]))
-         sources[FB_WRITE_LOGICAL_SRC_SRC_DEPTH] = get_nir_src(ntb, instr->src[4], 0);
+      if (!nir_src_is_undef(instr->src[4]) && fs_prog_data->uses_omask)
+         sources[FB_WRITE_LOGICAL_SRC_OMASK] = get_nir_src(ntb, instr->src[4], 0);
       if (!nir_src_is_undef(instr->src[5]))
-         sources[FB_WRITE_LOGICAL_SRC_SRC_STENCIL] = get_nir_src(ntb, instr->src[5], 0);
+         sources[FB_WRITE_LOGICAL_SRC_SRC_DEPTH] = get_nir_src(ntb, instr->src[5], 0);
+      if (!nir_src_is_undef(instr->src[6]))
+         sources[FB_WRITE_LOGICAL_SRC_SRC_STENCIL] = get_nir_src(ntb, instr->src[6], 0);
 
       brw_fb_write_inst *write = bld.emit(FS_OPCODE_FB_WRITE_LOGICAL, brw_reg(),
                                           sources, ARRAY_SIZE(sources))->as_fb_write();

src/intel/compiler/intel_nir.h
@@ -18,6 +18,8 @@ struct intel_device_info;
                              INTEL_TESS_CONFIG_##field##_OFFSET,        \
                              INTEL_TESS_CONFIG_##field##_SIZE)
 
+typedef nir_def *(*intel_nir_rt_write_cb)(nir_builder *, signed rt, void *data);
+
 void intel_nir_apply_tcs_quads_workaround(nir_shader *nir);
 bool intel_nir_rebase_const_offset_ubo_loads(nir_shader *shader);
 bool intel_nir_blockify_uniform_loads(nir_shader *shader,
@@ -28,7 +30,9 @@ bool intel_nir_cleanup_resource_intel(nir_shader *shader);
 
 bool intel_nir_lower_fragment_outputs(nir_shader *shader,
                                       unsigned nr_colour_regions,
-                                      bool replicate_alpha);
+                                      bool replicate_alpha,
+                                      intel_nir_rt_write_cb cb,
+                                      void *cb_data);
 bool intel_nir_lower_non_uniform_barycentric_at_sample(nir_shader *nir);
 bool intel_nir_lower_non_uniform_resource_intel(nir_shader *shader);
 bool intel_nir_lower_patch_vertices_in(nir_shader *shader,

src/intel/compiler/jay/jay_from_nir.c
@@ -1054,12 +1054,12 @@ jay_emit_fb_write(struct nir_to_jay_state *nj, nir_intrinsic_instr *intr)
    jay_builder *b = &nj->bld;
    const struct brw_fs_prog_data *prog_data = &nj->s->prog_data->fs;
    const struct intel_device_info *devinfo = b->shader->devinfo;
-   jay_def colour = nj_src(intr->src[0]);
+   jay_def colour = nj_src(intr->src[1]);
    jay_def dual_colour = jay_null();
-   jay_def src0_alpha = optional_src(intr->src[2]);
-   jay_def omask = optional_src(intr->src[3]);
-   jay_def depth = optional_src(intr->src[4]);
-   jay_def stencil = optional_src(intr->src[5]);
+   jay_def src0_alpha = optional_src(intr->src[3]);
+   jay_def omask = optional_src(intr->src[4]);
+   jay_def depth = optional_src(intr->src[5]);
+   jay_def stencil = optional_src(intr->src[6]);
 
    int target = MAX2(((signed) nir_intrinsic_target(intr)), 0);
    const bool last = !nir_instr_next(&intr->instr);
@@ -1081,13 +1081,13 @@ jay_emit_fb_write(struct nir_to_jay_state *nj, nir_intrinsic_instr *intr)
    /* If our alpha happens to match src0_alpha, we can skip sending it,
     * as the hardware will use our alpha in that case.
     */
-   if (scalars_equal(nir_scalar_resolved(intr->src[2].ssa, 0),
-                     nir_scalar_resolved(intr->src[0].ssa, 3)))
+   if (scalars_equal(nir_scalar_resolved(intr->src[3].ssa, 0),
+                     nir_scalar_resolved(intr->src[1].ssa, 3)))
       src0_alpha = jay_null();
 
    if (prog_data->dual_src_blend) {
       assert(b->shader->dispatch_width == 16);
-      dual_colour = nj_src(intr->src[1]);
+      dual_colour = nj_src(intr->src[2]);
       src0_alpha = jay_null();
    }
 

src/intel/compiler/jay/jay_nir.c
@@ -933,7 +933,8 @@ jay_process_nir(const struct intel_device_info *devinfo,
                    key->fs.alpha_test_replicate_alpha ||
                    (key->fs.nr_color_regions > 1 &&
                     alpha_to_coverage_enabled(nir, key->fs.alpha_to_coverage) &&
-                    !(nir->info.outputs_written & BITFIELD64_BIT(FRAG_RESULT_SAMPLE_MASK))));
+                    !(nir->info.outputs_written & BITFIELD64_BIT(FRAG_RESULT_SAMPLE_MASK))),
+                   NULL, NULL);
 
       /* nir_lower_terminate_to_demote will hamper our ability to schedule
        * terminates (since it turns them into real control flow), so run

commit 12fffa1edab7eb72a724feb5f0169d4ba9394f52

brw: always let the driver handle -1 render targets
Even with no color writes, the HW might need a valid surface state
with width/height matching depth attachment etc... in some cases :
  - alpha to coverage
  - sample mask
  - depth/stencil only output

Fixes tests like :

dEQP-VK.renderpasses.dynamic_rendering.complete_secondary_cmd_buff.depth_stencil_resolve.samples_2.d16_unorm.depth_average_stencil_none_testing_depth

in efficient 64bit mode

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 54947c0f37 ("brw/nir: prepare for render target writes in efficient 64bit mode")
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44251>

src/intel/blorp/blorp_brw.c
@@ -43,7 +43,6 @@ blorp_nir_options_brw(struct blorp_context *blorp,
 static nir_def *
 blorp_nir_rt_write(nir_builder *b, signed rt, void *data)
 {
-   assert(rt == 0);
    return nir_load_push_data_intel(b, 1, 64, nir_imm_int(b, 0), .base = 0, .range = 8);
 }
 

src/intel/compiler/intel_nir_lower_fragment_outputs.c
@@ -159,7 +159,7 @@ intel_nir_lower_fragment_outputs(nir_shader *shader,
          ctx.colour[FRAG_RESULT_DATA0][c] = nir_get_scalar(undef, 0);
       gather_colour_components(b, &ctx, FRAG_RESULT_DATA0, undef);
 
-      insert_rt_store(b, &ctx, -1, NULL, NULL);
+      insert_rt_store(b, &ctx, -1, cb, cb_data);
    }
 
    return true;

src/intel/vulkan/anv_shader_compile.c
@@ -1051,15 +1051,26 @@ rt_write_efficient_64bit(nir_builder *b, signed rt, void *data)
    const struct anv_pipeline_bind_map *bind_map = &shader_data->bind_map;
    const struct vk_color_attachment_location_state *cal = shader_data->fs_color_map;
 
+   if (rt < 0 && shader_data->bind_map.surface_count == 0)
+      return nir_imm_int64(b, 0);
+
    nir_def *color_offset =
       nir_load_push_data_intel(b, 1, 32, nir_imm_int(b, 0),
                                .base = anv_drv_const_offset(drv_data.gfx.fs_color_offset) -
                                        bind_map->push_ranges[0].start * 32,
                                .range = anv_drv_const_size(drv_data.gfx.fs_color_offset));
+
    if (rt >= 0) {
       if (cal != NULL) {
-         color_offset = nir_iadd_imm(
-            b, color_offset, (cal->color_map[rt] + 1) * ANV_SURFACE_STATE_SIZE);
+         /* If the attachment is unused, keep the RT write on the first render
+          * target item which is the null surface.
+          */
+         for (uint32_t i = 0; i < ARRAY_SIZE(cal->color_map); i++) {
+            if (cal->color_map[i] == rt) {
+               color_offset = nir_iadd_imm(
+                  b, color_offset, (i + 1) * ANV_SURFACE_STATE_SIZE);
+            }
+         }
       } else {
          nir_def *map =
             nir_load_push_data_intel(b, 1, 32, nir_imm_int(b, 0),

src/intel/vulkan/genX_gfx_state.c
@@ -2374,7 +2374,7 @@ update_fs_color_map(struct anv_gfx_dynamic_state *hw_state,
    uint32_t map = 0;
    for (uint32_t i = 0; i < MAX_RTS; i++) {
       if (dyn->cal.color_map[i] != MESA_VK_ATTACHMENT_UNUSED)
-         map |= (1 + dyn->cal.color_map[i]) << (i * 4);
+         map |= (1 + i) << (dyn->cal.color_map[i] * 4);
    }
 
    SET(FS_COLOR_MAP, fs_color_map, map);