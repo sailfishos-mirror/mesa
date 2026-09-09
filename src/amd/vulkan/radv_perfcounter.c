b684ab3d87ac6a971a763fdcbb89311e1323d45a
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2026-09-09 08:42:11 +0200

    radv: emit a full barrier when waiting for perf counters
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44309>

--- src/amd/vulkan/radv_perfcounter.c
+++ src/amd/vulkan/radv_perfcounter.c
@@ -595,23 +595,13 @@ radv_pc_wait_idle(struct radv_cmd_buffer *cmd_buffer)
    const struct radv_physical_device *pdev = radv_device_physical(device);
    struct radv_cmd_stream *cs = cmd_buffer->cs;
 
-   radeon_begin(cs);
+   enum radv_cmd_flush_bits flush_bits = RADV_CMD_FLAG_CS_PARTIAL_FLUSH | RADV_CMD_FLAG_VS_PARTIAL_FLUSH |
+                                         RADV_CMD_FLAG_PS_PARTIAL_FLUSH | RADV_CMD_FLAG_FLUSH_AND_INV_CB |
+                                         RADV_CMD_FLAG_FLUSH_AND_INV_DB | RADV_CMD_FLAG_PFP_SYNC_ME;
+   enum rgp_flush_bits sqtt_flush_bits = 0;
 
-   radeon_event_write(V_028A90_CS_PARTIAL_FLUSH);
-
-   const uint32_t coher_size_hi = pdev->info.gfx_level >= GFX11 ? 0xffffff : 0xff;
-
-   radeon_emit(PKT3(PKT3_ACQUIRE_MEM, 6, 0));
-   radeon_emit(0);          /* CP_COHER_CNTL */
-   radeon_emit(0xffffffff); /* CP_COHER_SIZE */
-   radeon_emit(coher_size_hi); /* CP_COHER_SIZE_HI */
-   radeon_emit(0);          /* CP_COHER_BASE */
-   radeon_emit(0);          /* CP_COHER_BASE_HI */
-   radeon_emit(0x0000000A); /* POLL_INTERVAL */
-   radeon_emit(0);          /* GCR_CNTL */
-   radeon_end();
-
-   ac_emit_cp_pfp_sync_me(cs->b, false);
+   radv_cs_emit_cache_flush(device->ws, cs, pdev->info.gfx_level, &cmd_buffer->gfx9_fence_idx,
+                            cmd_buffer->gfx9_fence_va, flush_bits, &sqtt_flush_bits, 0);
 }
 
 static void
