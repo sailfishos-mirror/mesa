bdafc8c6a1400c15c0b350d052481e18d31a235a
Calder Young <calder.young@intel.com>
2026-09-10 13:44:53 -0700

    intel: Document the page fault reporting enums more
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44379>

--- src/intel/common/intel_pagefault.h
+++ src/intel/common/intel_pagefault.h
@@ -7,22 +7,41 @@
 #include <stdint.h>
 
 enum intel_pagefault_access {
+   /** Faults caused by read access */
    INTEL_PAGEFAULT_ACCESS_READ,
+
+   /** Faults caused by write access */
    INTEL_PAGEFAULT_ACCESS_WRITE,
+
+   /** Faults caused by atomic access */
    INTEL_PAGEFAULT_ACCESS_ATOMIC,
 };
 
 enum intel_pagefault_type {
+   /** The page or the page table was not present */
    INTEL_PAGEFAULT_TYPE_NOT_PRESENT,
+
+   /** The page was present, but was not writable */
    INTEL_PAGEFAULT_TYPE_WRITE_ACCESS,
+
+   /** The page was present, but was not atomic-able */
    INTEL_PAGEFAULT_TYPE_ATOMIC_ACCESS,
 };
 
 enum intel_pagefault_level {
+   /** GraphicsAddress[20:12] - Page Table Entry */
    INTEL_PAGEFAULT_LEVEL_PTE,
+
+   /** GraphicsAddress[29:21] - Page Directory Entry */
    INTEL_PAGEFAULT_LEVEL_PDE,
+
+   /** GraphicsAddress[38:30] - Page Directory Pointer */
    INTEL_PAGEFAULT_LEVEL_PDP,
+
+   /** GraphicsAddress[47:39] - Page Map Level 4 */
    INTEL_PAGEFAULT_LEVEL_PML4,
+
+   /** GraphicsAddress[56:48] - Page Map Level 5 */
    INTEL_PAGEFAULT_LEVEL_PML5,
 };
 
