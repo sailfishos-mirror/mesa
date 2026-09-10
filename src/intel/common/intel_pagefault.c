7be30b341ec3f007dc6f1671b5fa5e75ed97138c
Calder Young <calder.young@intel.com>
2026-09-10 12:28:48 -0700

    intel: Add page fault reporting enums for source id
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44379>

--- src/intel/common/intel_pagefault.c
+++ src/intel/common/intel_pagefault.c
@@ -38,3 +38,37 @@ intel_pagefault_level_to_string(enum intel_pagefault_level level)
    };
    return lookup[level];
 }
+
+const char *
+intel_pagefault_src_to_string(enum intel_pagefault_src src)
+{
+   static const char *const lookup[] = {
+      [INTEL_PAGEFAULT_SRC_AMFS_SCRATCH_ACCESS] = "AMFS Scratch Access",
+      [INTEL_PAGEFAULT_SRC_AMFS_STATE] = "AMFS State",
+      [INTEL_PAGEFAULT_SRC_BINDING_TABLE] = "Binding Table Access",
+      [INTEL_PAGEFAULT_SRC_BLITTER_ACCESS] = "Blitter Engine Access",
+      [INTEL_PAGEFAULT_SRC_CCS_SURFACE_ACCESS] = "Compression Control Surface Access",
+      [INTEL_PAGEFAULT_SRC_CS_INSTRUCTION_FETCH] = "Command Streamer Instruction Fetch",
+      [INTEL_PAGEFAULT_SRC_CS_MEMORY_ACCESS] = "Command Streamer Memory Access",
+      [INTEL_PAGEFAULT_SRC_DATAPORT_ACCESS] = "Load/Store Cache Access",
+      [INTEL_PAGEFAULT_SRC_DATAPORT_STATE] = "Load/Store Cache State",
+      [INTEL_PAGEFAULT_SRC_DEPTH_ACCESS] = "Depth Cache Access",
+      [INTEL_PAGEFAULT_SRC_DEPTH_STATE] = "Depth Cache State",
+      [INTEL_PAGEFAULT_SRC_EU_INSTRUCTION_FETCH] = "Execution Unit Instruction Fetch",
+      [INTEL_PAGEFAULT_SRC_MEDIA_CODEC_BITSTREAM] = "Media Codec Bitstream",
+      [INTEL_PAGEFAULT_SRC_MEDIA_CODEC_FEEDBACK] = "Media Codec Feedback",
+      [INTEL_PAGEFAULT_SRC_MEDIA_CODEC_SURFACE] = "Media Codec Surface",
+      [INTEL_PAGEFAULT_SRC_MEDIA_ENHANCE_ACCESS] = "Media Enhancement Access",
+      [INTEL_PAGEFAULT_SRC_OA_FEEDBACK] = "OA Counter Feedback",
+      [INTEL_PAGEFAULT_SRC_PAGE_WALKER] = "GAM Page Walker",
+      [INTEL_PAGEFAULT_SRC_PIXELPORT_ACCESS] = "Render Cache Access",
+      [INTEL_PAGEFAULT_SRC_PIXELPORT_STATE] = "Render Cache State",
+      [INTEL_PAGEFAULT_SRC_RASTERIZER_STATE] = "Rasterizer State",
+      [INTEL_PAGEFAULT_SRC_SAMPLER_ACCESS] = "Sampler Cache Access",
+      [INTEL_PAGEFAULT_SRC_SAMPLER_STATE] = "Sampler Cache State",
+      [INTEL_PAGEFAULT_SRC_TRANSFORM_FEEDBACK] = "Transform Feedback",
+      [INTEL_PAGEFAULT_SRC_VERTEX_FETCH] = "Vertex Fetch",
+      [INTEL_PAGEFAULT_SRC_UNKNOWN] = "Unknown",
+   };
+   return lookup[src];
+}
--- src/intel/common/intel_pagefault.h
+++ src/intel/common/intel_pagefault.h
@@ -45,12 +45,93 @@ enum intel_pagefault_level {
    INTEL_PAGEFAULT_LEVEL_PML5,
 };
 
+enum intel_pagefault_src {
+   /** Access to the AMFS scratch space */
+   INTEL_PAGEFAULT_SRC_AMFS_SCRATCH_ACCESS,
+
+   /** Fetching the AMFS indirect state */
+   INTEL_PAGEFAULT_SRC_AMFS_STATE,
+
+   /** Fetching binding table indicies */
+   INTEL_PAGEFAULT_SRC_BINDING_TABLE,
+
+   /** Access performed via the blitter */
+   INTEL_PAGEFAULT_SRC_BLITTER_ACCESS,
+
+   /** Access to a compression control surface */
+   INTEL_PAGEFAULT_SRC_CCS_SURFACE_ACCESS,
+
+   /** Fetching the command stream */
+   INTEL_PAGEFAULT_SRC_CS_INSTRUCTION_FETCH,
+
+   /** Command streamer MI, indirect dispatch, postsync, context save */
+   INTEL_PAGEFAULT_SRC_CS_MEMORY_ACCESS,
+
+   /** Access performed via the LSC/HDC, sampler route-to-LSC, and raytracing */
+   INTEL_PAGEFAULT_SRC_DATAPORT_ACCESS,
+
+   /** Fetching surface states for the dataport */
+   INTEL_PAGEFAULT_SRC_DATAPORT_STATE,
+
+   /** Access to a depth buffer and HIZ */
+   INTEL_PAGEFAULT_SRC_DEPTH_ACCESS,
+
+   /** Fetching state of a depth buffer */
+   INTEL_PAGEFAULT_SRC_DEPTH_STATE,
+
+   /** Fetching shader execution unit instructions */
+   INTEL_PAGEFAULT_SRC_EU_INSTRUCTION_FETCH,
+
+   /** Access to an image/video coding bitstream and pointer */
+   INTEL_PAGEFAULT_SRC_MEDIA_CODEC_BITSTREAM,
+
+   /** Feedback from video coding (histogram, etc) */
+   INTEL_PAGEFAULT_SRC_MEDIA_CODEC_FEEDBACK,
+
+   /** Access to an image/video coding reference or output surface */
+   INTEL_PAGEFAULT_SRC_MEDIA_CODEC_SURFACE,
+
+   /** Access to an image/video enhancement state or surface */
+   INTEL_PAGEFAULT_SRC_MEDIA_ENHANCE_ACCESS,
+
+   /** Observation architecture feedback */
+   INTEL_PAGEFAULT_SRC_OA_FEEDBACK,
+
+   /** Accessing the page tables (PPGTT, GGTT, TRTT) */
+   INTEL_PAGEFAULT_SRC_PAGE_WALKER,
+
+   /** Access to a render target */
+   INTEL_PAGEFAULT_SRC_PIXELPORT_ACCESS,
+
+   /** Fetching the state of a render target */
+   INTEL_PAGEFAULT_SRC_PIXELPORT_STATE,
+
+   /** Fetching the state of the rasterization pipeline */
+   INTEL_PAGEFAULT_SRC_RASTERIZER_STATE,
+
+   /** Fetching surface and sampler states for the sampler */
+   INTEL_PAGEFAULT_SRC_SAMPLER_ACCESS,
+
+   /** Access performed via the sampler */
+   INTEL_PAGEFAULT_SRC_SAMPLER_STATE,
+
+   /** Transform feedback (stream output) */
+   INTEL_PAGEFAULT_SRC_TRANSFORM_FEEDBACK,
+
+   /** Fetching vertex data from memory */
+   INTEL_PAGEFAULT_SRC_VERTEX_FETCH,
+
+   /** Unknown or undefined source */
+   INTEL_PAGEFAULT_SRC_UNKNOWN,
+};
+
 struct intel_pagefault_info {
     uint64_t address;
     uint32_t precision;
     enum intel_pagefault_access access;
     enum intel_pagefault_type type;
     enum intel_pagefault_level level;
+    enum intel_pagefault_src src;
 };
 
 struct intel_pagefault_buffer {
@@ -64,3 +145,5 @@ const char *
 intel_pagefault_type_to_string(enum intel_pagefault_type type);
 const char *
 intel_pagefault_level_to_string(enum intel_pagefault_level level);
+const char *
+intel_pagefault_src_to_string(enum intel_pagefault_src src);
