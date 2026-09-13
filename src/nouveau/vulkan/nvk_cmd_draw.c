sailfishos-mirror/mesa

commit 4b089c54cbcaf84f8dcbbbff15006c9cf5c6324e

nvk: enable stencil compression
We don't have evidence of this helping anything, but I've noticed we don't
set it and on other vendor's hardware it improves stencil throughput and
might do the same on Nvidia as well.

Reviewed-by: Mary Guillemard <mary@mary.zone>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43783>

src/nouveau/vulkan/nvk_cmd_draw.c
@@ -269,6 +269,10 @@ nvk_push_draw_state_init(struct nvk_queue *queue, struct nv_push *p)
    P_IMMD(p, NV9097, SET_RENDER_ENABLE_C, MODE_TRUE);
 
    P_IMMD(p, NV9097, SET_Z_COMPRESSION, ENABLE_TRUE);
+   if (pdev->info.cls_eng3d >= MAXWELL_B) {
+      P_IMMD(p, NVB197, SET_STENCIL_COMPRESSION, ENABLE_TRUE);
+   }
+
    P_MTHD(p, NV9097, SET_COLOR_COMPRESSION(0));
    for (unsigned i = 0; i < 8; i++)
       P_NV9097_SET_COLOR_COMPRESSION(p, i, ENABLE_TRUE);
@@ -1453,6 +1457,9 @@ nvk_CmdBeginRendering(VkCommandBuffer commandBuffer,
       P_IMMD(p, NV9097, SET_ZT_LAYER, base_array_layer);
 
       P_IMMD(p, NV9097, SET_Z_COMPRESSION, image->is_compressed);
+      if (nvk_cmd_buffer_3d_cls(cmd) >= MAXWELL_B) {
+         P_IMMD(p, NVB197, SET_STENCIL_COMPRESSION, image->is_compressed);
+      }
 
       if (nvk_cmd_buffer_3d_cls(cmd) >= MAXWELL_B) {
          P_IMMD(p, NVC597, SET_ZT_SPARSE, {

commit f6f4f66c975c4ced2547e78e20c3b71850765ab5

nvk: Increase pushbuf limit in CmdBeginRendering
Fixes an assertion failure in unoptimized builds, eg. with
dEQP-VK.fragment_shading_rate.renderpass2.monolithic.fragdepth_clear.dynamic.attachment.shaderrate.keep.keep.151x431.samples1.ms

Fixes: 4b089c54cbc ("nvk: enable stencil compression")
Reviewed-by: Mary Guillemard <mary@mary.zone>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44418>

src/nouveau/vulkan/nvk_cmd_draw.c
@@ -1228,7 +1228,7 @@ nvk_CmdBeginRendering(VkCommandBuffer commandBuffer,
 
    const size_t zcull_count = 47;
    struct nv_push *p = nvk_cmd_buffer_push(
-      cmd, NVK_MAX_RTS * 12 + 44 + zcull_count
+      cmd, NVK_MAX_RTS * 12 + 46 + zcull_count
    );
 
    P_IMMD(p, NV9097, SET_MME_SHADOW_SCRATCH(NVK_MME_SCRATCH_VIEW_MASK),