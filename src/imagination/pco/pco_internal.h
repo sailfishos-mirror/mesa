sailfishos-mirror/mesa

commit c71ce9239b381ab30c753a244592f3f5b39055e3

pco: Specific lowering for shader_float_controls2
Support for NaN and signed-zero preservation is incomplete in our
hardware.
Lower instructions with incorrect behaviour when NaN or signed-zero
preservation is required to variants that correct that behaviour.

Signed-off-by: Radu Costas <radu.costas@imgtec.com>
Reviewed-by: Simon Perretta <simon.perretta@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43860>

src/imagination/pco/pco_internal.h
@@ -1819,6 +1819,7 @@ bool pco_nir_link_multiview(nir_shader *producer,
 bool pco_nir_lower_algebraic(nir_shader *shader);
 bool pco_nir_lower_algebraic_late(nir_shader *shader);
 bool pco_nir_lower_alpha_to_coverage(nir_shader *shader);
+bool pco_nir_lower_alu(nir_shader *shader);
 bool pco_nir_lower_atomics(nir_shader *shader, pco_data *data);
 bool pco_nir_lower_barriers(nir_shader *shader, pco_data *data);
 void pco_nir_lower_clip_cull_vars(nir_shader *shader);
@@ -3231,6 +3232,7 @@ static inline bool pco_should_skip_pass(const char *pass)
 #define pco_zero pco_ref_hwreg(0, PCO_REG_CLASS_CONST)
 #define pco_false pco_zero
 
+/** Float negative zero. */
 #define pco_nzero pco_ref_hwreg(141, PCO_REG_CLASS_CONST)
 
 /** Integer one. */

src/imagination/pco/pco_nir.c
@@ -54,10 +54,10 @@ static const nir_shader_compiler_options nir_options = {
    .lower_find_lsb = true,
    .lower_fquantize2f16 = true,
    .lower_flrp32 = true,
+   .lower_fminmax_signed_zero = true,
    .lower_fmod = true,
    .lower_fpow = true,
    .lower_fsqrt = true,
-   .lower_ftrunc = true,
    .lower_iadd_sat = true,
    .lower_ifind_msb = true,
    .lower_layer_fs_input_to_sysval = true,
@@ -529,6 +529,7 @@ static void pco_nir_opt(pco_ctx *ctx, nir_shader *nir, pco_data *data, bool alge
 
       NIR_PASS(progress, nir, nir_opt_phi_precision);
       NIR_PASS(progress, nir, nir_lower_alu);
+      NIR_PASS(progress, nir, pco_nir_lower_alu);
       NIR_PASS(progress, nir, nir_lower_pack);
 
       if (algebraic) {

src/imagination/pco/pco_nir_alu.c
@@ -1,0 +1,101 @@
+/*
+ * Copyright © 2026 Imagination Technologies Ltd.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+/**
+ * \file pco_nir_alu.c
+ *
+ * \brief PCO NIR per-ALU instruction pass.
+ */
+#include "compiler/nir/nir.h"
+#include "compiler/nir/nir_builder.h"
+#include "compiler/nir/nir_builder_opcodes.h"
+#include "compiler/nir/nir_opcodes.h"
+#include "compiler/nir/nir_builtin_builder.h"
+#include "nir_defines.h"
+#include "pco_internal.h"
+
+/**
+ * \brief Lowers ALU instructions with float_controls2 decorations.
+ *
+ * \param[in,out] b NIR builder.
+ * \param[in,out] alu ALU instruction to lower.
+ * \param[in,out] cb_data additional data.
+ * \return True if the pass made progress.
+ */
+static bool
+pco_nir_lower_alu_instr(nir_builder *b, nir_alu_instr *alu, void *cb_data)
+{
+   if (!nir_alu_instr_is_signed_zero_inf_nan_preserve(alu))
+      return false;
+
+   uint32_t old_fp_math_ctrl = b->fp_math_ctrl;
+   switch (alu->op) {
+   case nir_op_fmax:
+   case nir_op_fmin:
+      /* All hardware comparison instructions return false if any operand is NaN
+       * so a NaN check before is needed.
+       * Directly return the other operand if one of the operands is NaN,
+       * otherwise return the operation result.
+       */
+      if (!nir_alu_instr_is_nan_preserve(alu))
+         break;
+
+      b->cursor = nir_after_instr(&alu->instr);
+      b->fp_math_ctrl = alu->fp_math_ctrl;
+      alu->fp_math_ctrl &= ~nir_fp_preserve_nan;
+
+      nir_def *src0_def = nir_ssa_for_alu_src(b, alu, 0);
+      nir_def *src1_def = nir_ssa_for_alu_src(b, alu, 1);
+
+      nir_def *fminmax_nan =
+         nir_bcsel(b,
+                   nir_fisnan(b, src0_def),
+                   src1_def,
+                   nir_bcsel(b, nir_fisnan(b, src1_def), src0_def, &alu->def));
+      nir_def_rewrite_uses_after(&alu->def, fminmax_nan);
+
+      b->fp_math_ctrl = old_fp_math_ctrl;
+      return true;
+
+   case nir_op_fsign:
+      /* Explicitly check for -0.0 as fsign requires that to be the result in
+       * case the input is also -0.0.
+       */
+      if (!nir_alu_instr_is_signed_zero_preserve(alu))
+         break;
+
+      b->fp_math_ctrl = alu->fp_math_ctrl;
+      b->cursor = nir_after_instr(&alu->instr);
+      nir_def *alu_src = nir_ssa_for_alu_src(b, alu, 0);
+      nir_def *fsign_sz = nir_bcsel(b,
+                                    nir_feq(b, alu_src, nir_imm_float(b, -0.0)),
+                                    nir_imm_float(b, -0.0),
+                                    &alu->def);
+      alu->fp_math_ctrl &= ~nir_fp_preserve_signed_zero;
+      nir_def_rewrite_uses_after(&alu->def, fsign_sz);
+      b->fp_math_ctrl = old_fp_math_ctrl;
+      return true;
+
+   default:
+      break;
+   }
+
+   return false;
+}
+
+/**
+ * \brief Pass that lowers ALU instructions based on special conditions.
+ *
+ * \param[in,out] shader NIR shader.
+ * \return True if the pass made progress.
+ */
+bool pco_nir_lower_alu(nir_shader *shader)
+{
+   return nir_shader_alu_pass(shader,
+                              pco_nir_lower_alu_instr,
+                              nir_metadata_control_flow,
+                              NULL);
+}

src/imagination/pco/meson.build
@@ -16,6 +16,7 @@ libpowervr_compiler_files = files(
   'pco_ir.c',
   'pco_legalize.c',
   'pco_nir.c',
+  'pco_nir_alu.c',
   'pco_nir_compute.c',
   'pco_nir_io.c',
   'pco_nir_lower_null_descriptors.c',

src/imagination/pco/pco_nir_algebraic.py
@@ -32,6 +32,13 @@ def lowered_fround_even(src):
 
 lower_algebraic.append((('fround_even', a), lowered_fround_even(a)))
 
+# Since fceil/ffloor preserve signed-zero, this ftrunc lowering does so too.
+lower_ftrunc = [
+    (('ftrunc', a) , ('bcsel', ('flt', a, 0.0), ('fceil', a), ('ffloor', a)))
+]      
+
+lower_algebraic.extend(lower_ftrunc)
+
 lower_insert_extract = [
    (('insert_u8', 'a@32', b), ('bitfield_insert', 0, a, ('imul', b, 8), 8)),
    (('insert_u16', 'a@32', b), ('bitfield_insert', 0, a, ('imul', b, 16), 16)),

commit d104f8675d4b46cb8013a405ef5a81c46f70670d

pco: special-case tanh inf handling on cores with rtz rounding
On cores with rtz rounding, the accumulated loss of accuracy from the tanh
emulation will result in tanh(+/-inf) != +/-1.0 (+/-0.999999... instead).
This result is not acceptable if the preserve_inf flag is set.

Add special-casing for this combination of hardware and math flags.

Fixes: c71ce9239b3 ("pco: Specific lowering for shader_float_controls2")
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Acked-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44391>

src/imagination/pco/pco_internal.h
@@ -1819,7 +1819,7 @@ bool pco_nir_link_multiview(nir_shader *producer,
 bool pco_nir_lower_algebraic(nir_shader *shader);
 bool pco_nir_lower_algebraic_late(nir_shader *shader);
 bool pco_nir_lower_alpha_to_coverage(nir_shader *shader);
-bool pco_nir_lower_alu(nir_shader *shader);
+bool pco_nir_lower_alu(nir_shader *shader, pco_ctx *ctx);
 bool pco_nir_lower_atomics(nir_shader *shader, pco_data *data);
 bool pco_nir_lower_barriers(nir_shader *shader, pco_data *data);
 void pco_nir_lower_clip_cull_vars(nir_shader *shader);

src/imagination/pco/pco_nir.c
@@ -44,6 +44,7 @@ static const nir_shader_compiler_options nir_options = {
 
    .has_f2i32_rtne = true,
    .has_fused_comp_and_csel = true,
+   .has_tanh = true,
 
    .instance_id_includes_base_index = true,
 
@@ -529,7 +530,7 @@ static void pco_nir_opt(pco_ctx *ctx, nir_shader *nir, pco_data *data, bool alge
 
       NIR_PASS(progress, nir, nir_opt_phi_precision);
       NIR_PASS(progress, nir, nir_lower_alu);
-      NIR_PASS(progress, nir, pco_nir_lower_alu);
+      NIR_PASS(progress, nir, pco_nir_lower_alu, ctx);
       NIR_PASS(progress, nir, nir_lower_pack);
 
       if (algebraic) {

src/imagination/pco/pco_nir_alu.c
@@ -28,8 +28,12 @@
 static bool
 pco_nir_lower_alu_instr(nir_builder *b, nir_alu_instr *alu, void *cb_data)
 {
-   if (!nir_alu_instr_is_signed_zero_inf_nan_preserve(alu))
+   pco_ctx *ctx = cb_data;
+
+   if (!nir_alu_instr_is_signed_zero_inf_nan_preserve(alu) &&
+       alu->op != nir_op_ftanh) {
       return false;
+   }
 
    uint32_t old_fp_math_ctrl = b->fp_math_ctrl;
    switch (alu->op) {
@@ -79,6 +83,37 @@ pco_nir_lower_alu_instr(nir_builder *b, nir_alu_instr *alu, void *cb_data)
       b->fp_math_ctrl = old_fp_math_ctrl;
       return true;
 
+   case nir_op_ftanh:
+      b->cursor = nir_before_instr(&alu->instr);
+      b->fp_math_ctrl = alu->fp_math_ctrl;
+      nir_def *src = nir_ssa_for_alu_src(b, alu, 0);
+
+      /* Use the built-in emulation. */
+      nir_def *tanh = nir_tanh_emulated(b, src);
+
+      /* If the current core has RTZ rounding for f32, and we need an accurate
+       * result for src = +/-inf, then special-case to avoid accuracy loss.
+       */
+      bool needs_inf_handling =
+         !PVR_HAS_FEATURE(ctx->dev_info, usc_alu_roundingmode_rne) &&
+         (alu->fp_math_ctrl & nir_fp_preserve_inf);
+
+      if (needs_inf_handling) {
+         nir_def *finf = nir_imm_float(b, INFINITY);
+         nir_def *is_inf = nir_feq(b, nir_fabs(b, src), finf);
+
+         /* tanh(+/-inf) = +/-1.0f */
+         tanh = nir_bcsel(b,
+                          is_inf,
+                          nir_copysign(b, nir_imm_float(b, 1.0f), src),
+                          tanh);
+      }
+
+      nir_def_replace(&alu->def, tanh);
+
+      b->fp_math_ctrl = old_fp_math_ctrl;
+      return true;
+
    default:
       break;
    }
@@ -90,12 +125,13 @@ pco_nir_lower_alu_instr(nir_builder *b, nir_alu_instr *alu, void *cb_data)
  * \brief Pass that lowers ALU instructions based on special conditions.
  *
  * \param[in,out] shader NIR shader.
+ * \param[in] ctx PCO compiler context.
  * \return True if the pass made progress.
  */
-bool pco_nir_lower_alu(nir_shader *shader)
+bool pco_nir_lower_alu(nir_shader *shader, pco_ctx *ctx)
 {
    return nir_shader_alu_pass(shader,
                               pco_nir_lower_alu_instr,
                               nir_metadata_control_flow,
-                              NULL);
+                              ctx);
 }