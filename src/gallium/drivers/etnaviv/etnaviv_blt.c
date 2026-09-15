bminor/mesa-mesa

commit 59e35590d3e2c04e2d8ace81a6f8129fd5c0e197

etnaviv: blt: Support 128 bit clear operations
Passes dEQP-GLES3.functional.fbo.color.clear.*32*.

Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37597>

src/gallium/drivers/etnaviv/etnaviv_blt.c
@@ -290,6 +290,7 @@ etna_blit_clear_color_blt(struct pipe_context *pctx, unsigned idx,
    uint64_t new_clear_value = etna_clear_blit_pack_rgba(dst->format, color);
    bool fast_clear = etna_blt_will_fastclear(dst_level, scissor_state);
    int msaa_xscale = 1, msaa_yscale = 1;
+   bool is_128bit_format = format_is_128bit(dst->format);
 
    translate_samples_to_xyscale(dst->texture->nr_samples,
                                 &msaa_xscale, &msaa_yscale);
@@ -333,7 +334,20 @@ etna_blit_clear_color_blt(struct pipe_context *pctx, unsigned idx,
       clr.rect_h = dst_level->height * msaa_yscale;
    }
 
+   if (is_128bit_format) {
+      clr.clear_value[0] = color->ui[0];
+      clr.clear_value[1] = color->ui[1];
+   }
+
    emit_blt_clearimage(ctx->stream, &clr);
+
+   if (is_128bit_format) {
+      clr.clear_value[0] = color->ui[2];
+      clr.clear_value[1] = color->ui[3];
+      clr.dest.addr.offset += (dst_level->size * dst_level->depth) / 2;
+
+      emit_blt_clearimage(ctx->stream, &clr);
+   }
 
    /* This made the TS valid */
    if (dst_level->ts_size) {

sailfishos-mirror/mesa

commit f9f8f777d01feee8f46fcd1ba037ee51203becda

etnaviv: blt: Clear each 128-bit plane with its own clear bits
A 128-bit clear writes the R,G plane and the B,A plane in two passes. Both
passes used the clear bits of the whole texel, but the B and A shifts do
not fit into 64 bits. As a result, masking R also cleared B and masking G
also cleared A.

Compute the clear bits for each plane from its two channels.

Fixes KHR-GLES31.core.draw_buffers_indexed.color_masks.

Fixes: 59e35590d3e ("etnaviv: blt: Support 128 bit clear operations")
Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Reviewed-by: Daniel Lang <dalang@gmx.at>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44454>

src/gallium/drivers/etnaviv/etnaviv_blt.c
@@ -445,12 +445,12 @@ etna_blit_clear_color_blt(struct pipe_context *pctx, unsigned idx,
    struct etna_resource *dst_res = etna_resource_get_render_compatible(pctx, dst->texture);
    struct etna_resource_level *dst_level = &dst_res->levels[dst->level];
    uint64_t new_clear_value = etna_clear_blit_pack_rgba(dst->format, color, ctx->screen);
-   const uint64_t clear_bits =
+   bool is_128bit_format = format_is_128bit(dst->format);
+   const uint64_t clear_bits = is_128bit_format ? 0 :
       etna_calculate_clear_bits(translate_pe_internal_format(dst->format, ctx->screen), clear_mask);
    bool fast_clear = etna_blt_will_fastclear(dst_level, scissor_state, clear_mask, 0xf);
    bool use_ts = etna_framebuffer_rt_use_ts(ctx, idx);
    int msaa_xscale = 1, msaa_yscale = 1;
-   bool is_128bit_format = format_is_128bit(dst->format);
 
    translate_samples_to_xyscale(dst->texture->nr_samples,
                                 &msaa_xscale, &msaa_yscale);
@@ -498,15 +498,25 @@ etna_blit_clear_color_blt(struct pipe_context *pctx, unsigned idx,
    }
 
    if (is_128bit_format) {
+      const uint64_t rg_bits =
+         etna_calculate_clear_bits(translate_format_128bit_to_64bit(dst->format), clear_mask);
+
       clr.clear_value[0] = color->ui[0];
       clr.clear_value[1] = color->ui[1];
+      clr.clear_bits[0] = rg_bits;
+      clr.clear_bits[1] = rg_bits >> 32;
    }
 
    emit_blt_clearimage(ctx, &clr);
 
    if (is_128bit_format) {
+      const uint64_t ba_bits =
+         etna_calculate_clear_bits(translate_format_128bit_to_64bit(dst->format), clear_mask >> 2);
+
       clr.clear_value[0] = color->ui[2];
       clr.clear_value[1] = color->ui[3];
+      clr.clear_bits[0] = ba_bits;
+      clr.clear_bits[1] = ba_bits >> 32;
       clr.dest.addr.offset += etna_resource_level_second_plane_offset(dst_level);
 
       emit_blt_clearimage(ctx, &clr);