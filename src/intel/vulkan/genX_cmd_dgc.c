cfa542d659a74e077155f954d18b90e079fa7994
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2026-09-16 10:53:04 +0300

    anv: add missing constant cache invalidation in DGC path
    The generation shader might generate push constant data.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: mesa-stable
    Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44478>

--- src/intel/vulkan/genX_cmd_dgc.c
+++ src/intel/vulkan/genX_cmd_dgc.c
@@ -817,6 +817,7 @@ void genX(CmdExecuteGeneratedCommandsEXT)(
                                    VK_PIPELINE_STAGE_2_FRAGMENT_SHADER_BIT_KHR,
                                    VK_PIPELINE_STAGE_2_TOP_OF_PIPE_BIT,
                                    ANV_PIPE_DATA_CACHE_FLUSH_BIT |
+                                   ANV_PIPE_CONSTANT_CACHE_INVALIDATE_BIT |
                                    ANV_PIPE_CS_STALL_BIT,
                                    "after generated commands");
          genX(cmd_buffer_apply_pipe_flushes)(cmd_buffer);
@@ -948,6 +949,7 @@ void genX(CmdExecuteGeneratedCommandsEXT)(
                                    VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT_KHR,
                                    VK_PIPELINE_STAGE_2_TOP_OF_PIPE_BIT,
                                    ANV_PIPE_DATA_CACHE_FLUSH_BIT |
+                                   ANV_PIPE_CONSTANT_CACHE_INVALIDATE_BIT |
                                    ANV_PIPE_CS_STALL_BIT,
                                    "after generated commands");
          genX(cmd_buffer_apply_pipe_flushes)(cmd_buffer);
@@ -1069,6 +1071,7 @@ void genX(CmdExecuteGeneratedCommandsEXT)(
                                    VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT_KHR,
                                    VK_PIPELINE_STAGE_2_TOP_OF_PIPE_BIT,
                                    ANV_PIPE_DATA_CACHE_FLUSH_BIT |
+                                   ANV_PIPE_CONSTANT_CACHE_INVALIDATE_BIT |
                                    ANV_PIPE_CS_STALL_BIT,
                                    "after generated commands");
          genX(cmd_buffer_apply_pipe_flushes)(cmd_buffer);
