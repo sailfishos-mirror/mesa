78d021ffb702f4386e76a15751503dc6928c6dce
Matt Turner <mattst88@gmail.com>
2026-09-16 06:55:54 -0400

    mesa/st: use concrete formats for lowered YUV plane views
    Since 2ca46c7a7ac, the lowered YUV plane resources are created with
    concrete formats (R8G8, B8G8R8A8, R8G8B8A8, R8G8B8X8), but the sampler
    views and support checks for them still use the endian aliases RG88,
    BGRA8888, RGBA8888 and RGBX8888. On big-endian those resolve to the
    byte-swapped formats, so chroma is read from the wrong channels.
    
    Use the concrete formats. No change on little-endian.
    
    The P010 view has the same mismatch but needs byte swapping, not a
    different format.
    
    Fixes 19 weston client-buffer and color-representation-renderer
    subtests on ppc64 with llvmpipe.
    
    Fixes: 2ca46c7a7ac ("dri: fix __DRI_IMAGE_FORMAT* to PIPE_FORMAT* mappings")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44127>

--- src/mesa/state_tracker/st_atom_texture.c
+++ src/mesa/state_tracker/st_atom_texture.c
@@ -200,7 +200,7 @@ st_get_sampler_views(struct st_context *st,
             break;
 
          /* we need one additional R8G8 view: */
-         tmpl.format = PIPE_FORMAT_RG88_UNORM;
+         tmpl.format = PIPE_FORMAT_R8G8_UNORM;
          tmpl.swizzle_g = PIPE_SWIZZLE_Y;   /* tmpl from Y plane is R8 */
          extra = u_bit_scan(&free_slots);
          sampler_views[extra] =
@@ -213,7 +213,7 @@ st_get_sampler_views(struct st_context *st,
             break;
 
          /* we need one additional R8G8 view: */
-         tmpl.format = PIPE_FORMAT_RG88_UNORM;
+         tmpl.format = PIPE_FORMAT_R8G8_UNORM;
          tmpl.swizzle_g = PIPE_SWIZZLE_Y;   /* tmpl from Y plane is R8 */
          extra = u_bit_scan(&free_slots);
          sampler_views[extra] =
@@ -226,7 +226,7 @@ st_get_sampler_views(struct st_context *st,
             break;
 
          /* we need one additional R8G8 view: */
-         tmpl.format = PIPE_FORMAT_RG88_UNORM;
+         tmpl.format = PIPE_FORMAT_R8G8_UNORM;
          tmpl.swizzle_g = PIPE_SWIZZLE_Y;   /* tmpl from Y plane is R8 */
          extra = u_bit_scan(&free_slots);
          sampler_views[extra] =
@@ -237,7 +237,7 @@ st_get_sampler_views(struct st_context *st,
       case PIPE_FORMAT_NV24:
       case PIPE_FORMAT_NV42:
          /* we need one additional R8G8 view: */
-         tmpl.format = PIPE_FORMAT_RG88_UNORM;
+         tmpl.format = PIPE_FORMAT_R8G8_UNORM;
          tmpl.swizzle_g = PIPE_SWIZZLE_Y;   /* tmpl from Y plane is R8 */
          extra = u_bit_scan(&free_slots);
          sampler_views[extra] =
@@ -300,8 +300,8 @@ st_get_sampler_views(struct st_context *st,
             /* no additional views needed */
             break;
 
-         /* we need one additional BGRA8888 view of the chroma samples: */
-         tmpl.format = PIPE_FORMAT_BGRA8888_UNORM;
+         /* we need one additional B8G8R8A8 view of the chroma samples: */
+         tmpl.format = PIPE_FORMAT_B8G8R8A8_UNORM;
          tmpl.swizzle_b = PIPE_SWIZZLE_Z;
          tmpl.swizzle_a = PIPE_SWIZZLE_W;
          extra = u_bit_scan(&free_slots);
@@ -318,8 +318,8 @@ st_get_sampler_views(struct st_context *st,
             /* no additional views needed */
             break;
 
-         /* we need one additional RGBA8888 view of the chroma samples: */
-         tmpl.format = PIPE_FORMAT_RGBA8888_UNORM;
+         /* we need one additional R8G8B8A8 view of the chroma samples: */
+         tmpl.format = PIPE_FORMAT_R8G8B8A8_UNORM;
          tmpl.swizzle_b = PIPE_SWIZZLE_Z;
          tmpl.swizzle_a = PIPE_SWIZZLE_W;
          extra = u_bit_scan(&free_slots);
