sailfishos-mirror/mesa

commit ff5352b4900737d62d8e3fa5878ebc6c381aace0

ir3: Fix ballot_components for subgroups smaller than 32
ballot_components becomes 0 when max_subgroup_size is smaller than 32.
Clamp it to a minimum of 1.

Fixes: 639d7946c71 ("freedreno: Add initial A702 support")
Signed-off-by: Valentine Burley <valentine.burley@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43010>

src/freedreno/ir3/ir3_nir.c
@@ -1053,7 +1053,7 @@ ir3_nir_post_finalize(struct ir3_shader *shader)
       nir_lower_subgroups_options options = {
             .subgroup_size = subgroup_size,
             .ballot_bit_size = 32,
-            .ballot_components = max_subgroup_size / 32,
+            .ballot_components = MAX2(1, max_subgroup_size / 32),
             .lower_to_scalar = true,
             .lower_vote_feq = true,
             .lower_vote_ieq = true,

commit d161978a18d476b83b319afdfec8b66b9b7c8356

ir3: Lower quad votes
ir3 has no quad vote instruction. nir_lower_subgroups already creates
quad_vote_all/any when lowering boolean reductions with a cluster size
of 4, which is reachable on a702 because it is the only ir3 target with
ballot_components == 1. Those reached the backend unlowered:

  Unhandled intrinsic type: quad_vote_all

Fixes: ff5352b4900 ("ir3: Fix ballot_components for subgroups smaller than 32")
Signed-off-by: Valentine Burley <valentine.burley@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44144>

src/freedreno/ir3/ir3_nir.c
@@ -1073,6 +1073,7 @@ ir3_nir_post_finalize(struct ir3_shader *shader)
             .lower_rotate_to_shuffle = !compiler->has_shfl,
             .lower_rotate_clustered_to_shuffle = true,
             .lower_inverse_ballot = true,
+            .lower_quad_vote = true,
             .lower_reduce = true,
             .filter = ir3_nir_lower_subgroups_filter,
             .filter_data = compiler,