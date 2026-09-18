sailfishos-mirror/mesa

commit 5da1715de0469934aa952eae4723ce9de2bbb61a

anv: stop using compute blorp shaders on Xe2+
There is no cost of pipeline switching on Xe2+.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44059>

src/intel/vulkan/anv_blorp.c
@@ -160,11 +160,18 @@ anv_device_finish_blorp(struct anv_device *device)
    blorp_finish(&device->blorp.context);
 }
 
+struct anv_blorp_batch_params {
+   enum blorp_batch_flags flags;
+   bool allow_3d_or_compute_variant;
+};
+
 static void
 anv_blorp_batch_init(struct anv_cmd_buffer *cmd_buffer,
-                     struct blorp_batch *batch, enum blorp_batch_flags flags)
+                     struct blorp_batch *batch,
+                     const struct anv_blorp_batch_params *params)
 {
    VkQueueFlags queue_flags = cmd_buffer->queue_family->queueFlags;
+   enum blorp_batch_flags flags = params->flags;
 
    if (queue_flags & VK_QUEUE_GRAPHICS_BIT) {
       /* blorp runs on render engine by default */
@@ -174,6 +181,17 @@ anv_blorp_batch_init(struct anv_cmd_buffer *cmd_buffer,
       flags |= BLORP_BATCH_USE_BLITTER;
    } else {
       UNREACHABLE("unknown queue family");
+   }
+
+   /* For platforms prior to Gfx20 where switching pipeline mode is a
+    * significant cost, try to stay in the same pipeline mode.
+    */
+   if ((queue_flags & VK_QUEUE_GRAPHICS_BIT) &&
+       params->allow_3d_or_compute_variant &&
+       cmd_buffer->device->info->ver < 20 &&
+       (cmd_buffer->state.current_pipeline ==
+        cmd_buffer->device->physical->gpgpu_pipeline_value)) {
+      flags |= BLORP_BATCH_USE_COMPUTE;
    }
 
    /* Can't have both flags at the same time. */
@@ -750,7 +768,8 @@ void anv_CmdCopyImage2(
 
    anv_blorp_require_rcs(cmd_buffer, src_image, dst_image) {
       struct blorp_batch batch;
-      anv_blorp_batch_init(cmd_buffer, &batch, 0);
+      anv_blorp_batch_init(cmd_buffer, &batch,
+                           &(struct anv_blorp_batch_params) {});
 
       for (unsigned r = 0; r < pCopyImageInfo->regionCount; r++) {
          copy_image(cmd_buffer, &batch,
@@ -906,7 +925,10 @@ void anv_CmdCopyMemoryToImageKHR(
 
    anv_cmd_require_rcs(cmd_buffer, blorp_execute_on_companion) {
       struct blorp_batch batch;
-      anv_blorp_batch_init(cmd_buffer, &batch, BLORP_BATCH_SRC_UNPADDED);
+      anv_blorp_batch_init(cmd_buffer, &batch,
+                           &(struct anv_blorp_batch_params) {
+                              .flags = BLORP_BATCH_SRC_UNPADDED,
+                           });
 
       for (unsigned r = 0; r < pCopyMemoryInfo->regionCount; r++) {
          const VkDeviceMemoryImageCopyKHR *region = &pCopyMemoryInfo->pRegions[r];
@@ -996,7 +1018,8 @@ void anv_CmdCopyImageToMemoryKHR(
 
    anv_cmd_require_rcs(cmd_buffer, blorp_execute_on_companion) {
       struct blorp_batch batch;
-      anv_blorp_batch_init(cmd_buffer, &batch, 0);
+      anv_blorp_batch_init(cmd_buffer, &batch,
+                           &(struct anv_blorp_batch_params) {});
 
       for (unsigned r = 0; r < pCopyMemoryInfo->regionCount; r++) {
          const VkDeviceMemoryImageCopyKHR *region = &pCopyMemoryInfo->pRegions[r];
@@ -1188,7 +1211,8 @@ void anv_CmdBlitImage2(
    ANV_FROM_HANDLE(anv_image, dst_image, pBlitImageInfo->dstImage);
 
    struct blorp_batch batch;
-   anv_blorp_batch_init(cmd_buffer, &batch, 0);
+   anv_blorp_batch_init(cmd_buffer, &batch,
+                        &(struct anv_blorp_batch_params) {});
 
    for (unsigned r = 0; r < pBlitImageInfo->regionCount; r++) {
       blit_image(cmd_buffer, &batch,
@@ -1244,10 +1268,10 @@ anv_cmd_copy_addr(struct anv_cmd_buffer *cmd_buffer,
 
    struct blorp_batch batch;
    anv_blorp_batch_init(cmd_buffer, &batch,
-                        BLORP_BATCH_SRC_UNPADDED |
-                        (cmd_buffer->state.current_pipeline ==
-                         cmd_buffer->device->physical->gpgpu_pipeline_value ?
-                         BLORP_BATCH_USE_COMPUTE : 0));
+                        &(struct anv_blorp_batch_params) {
+                           .flags = BLORP_BATCH_SRC_UNPADDED,
+                           .allow_3d_or_compute_variant = true,
+                        });
 
    copy_memory(device, &batch, src_addr, dst_addr, size);
 
@@ -1263,10 +1287,10 @@ void anv_CmdCopyMemoryKHR(
 
    struct blorp_batch batch;
    anv_blorp_batch_init(cmd_buffer, &batch,
-                        BLORP_BATCH_SRC_UNPADDED |
-                        (cmd_buffer->state.current_pipeline ==
-                         cmd_buffer->device->physical->gpgpu_pipeline_value ?
-                         BLORP_BATCH_USE_COMPUTE : 0));
+                        &(struct anv_blorp_batch_params) {
+                           .flags = BLORP_BATCH_SRC_UNPADDED,
+                           .allow_3d_or_compute_variant = true,
+                        });
 
    for (unsigned r = 0; r < pCopyMemoryInfo->regionCount; r++) {
       const VkDeviceMemoryCopyKHR *region = &pCopyMemoryInfo->pRegions[r];
@@ -1294,10 +1318,10 @@ anv_cmd_buffer_update_addr(
 {
    struct blorp_batch batch;
    anv_blorp_batch_init(cmd_buffer, &batch,
-                        BLORP_BATCH_SRC_UNPADDED |
-                        (cmd_buffer->state.current_pipeline ==
-                         cmd_buffer->device->physical->gpgpu_pipeline_value ?
-                         BLORP_BATCH_USE_COMPUTE : 0));
+                        &(struct anv_blorp_batch_params) {
+                           .flags = BLORP_BATCH_SRC_UNPADDED,
+                           .allow_3d_or_compute_variant = true,
+                        });
 
    /* We can't quite grab a full block because the state stream needs a
     * little data at the top to build its linked list.
@@ -1382,9 +1406,9 @@ anv_cmd_buffer_fill_area(struct anv_cmd_buffer *cmd_buffer,
 {
    struct blorp_batch batch;
    anv_blorp_batch_init(cmd_buffer, &batch,
-                        cmd_buffer->state.current_pipeline ==
-                        cmd_buffer->device->physical->gpgpu_pipeline_value ?
-                        BLORP_BATCH_USE_COMPUTE : 0);
+                        &(struct anv_blorp_batch_params) {
+                           .allow_3d_or_compute_variant = true,
+                        });
 
    union isl_color_value color = {
       .u32 = { data, data, data, data },
@@ -1583,7 +1607,8 @@ void anv_CmdClearColorImage(
 
    anv_blorp_require_rcs(cmd_buffer, NULL, image) {
       struct blorp_batch batch;
-      anv_blorp_batch_init(cmd_buffer, &batch, 0);
+      anv_blorp_batch_init(cmd_buffer, &batch,
+                           &(struct anv_blorp_batch_params) {});
 
       struct anv_format_plane src_format =
          anv_get_format_aspect(cmd_buffer->device->physical, image->vk.format,
@@ -1704,7 +1729,7 @@ void anv_CmdClearDepthStencilImage(
    ANV_FROM_HANDLE(anv_image, image, image_h);
 
    struct blorp_batch batch;
-   anv_blorp_batch_init(cmd_buffer, &batch, 0);
+   anv_blorp_batch_init(cmd_buffer, &batch, &(struct anv_blorp_batch_params) {});
    assert((batch.flags & BLORP_BATCH_USE_COMPUTE) == 0);
 
    struct blorp_surf depth, stencil;
@@ -2303,7 +2328,10 @@ void anv_CmdClearAttachments(
       anv_cmd_emit_conditional_render_predicate(cmd_buffer);
       flags |= BLORP_BATCH_PREDICATE_ENABLE;
    }
-   anv_blorp_batch_init(cmd_buffer, &batch, flags);
+   anv_blorp_batch_init(cmd_buffer, &batch,
+                        &(struct anv_blorp_batch_params) {
+                           .flags = flags,
+                        });
 
    for (uint32_t a = 0; a < attachmentCount; ++a) {
       if (pAttachments[a].aspectMask & VK_IMAGE_ASPECT_ANY_COLOR_BIT_ANV) {
@@ -2341,7 +2369,7 @@ anv_image_msaa_resolve(struct anv_cmd_buffer *cmd_buffer,
                        enum blorp_filter filter)
 {
    struct blorp_batch batch;
-   anv_blorp_batch_init(cmd_buffer, &batch, 0);
+   anv_blorp_batch_init(cmd_buffer, &batch, &(struct anv_blorp_batch_params) {});
    assert((batch.flags & BLORP_BATCH_USE_COMPUTE) == 0);
 
    assert(src_image->vk.image_type == VK_IMAGE_TYPE_2D);
@@ -2468,7 +2496,7 @@ anv_attachment_external_resolve(struct anv_cmd_buffer *cmd_buffer,
                                 false, &src_surf);
 
    struct blorp_batch batch;
-   anv_blorp_batch_init(cmd_buffer, &batch, 0);
+   anv_blorp_batch_init(cmd_buffer, &batch, &(struct anv_blorp_batch_params) {});
 
    for (uint8_t i = 0; i < ycbcr_info->n_planes; i++) {
       VkImageAspectFlags aspect_mask = plane_aspects[i];
@@ -2698,7 +2726,7 @@ anv_image_clear_color(struct anv_cmd_buffer *cmd_buffer,
    assert(image->vk.samples == 1 || image->n_planes == 1);
 
    struct blorp_batch batch;
-   anv_blorp_batch_init(cmd_buffer, &batch, 0);
+   anv_blorp_batch_init(cmd_buffer, &batch, &(struct anv_blorp_batch_params) {});
 
    struct blorp_surf surf;
    get_blorp_surf_for_anv_image(cmd_buffer, image, aspect,
@@ -2733,7 +2761,7 @@ anv_image_clear_depth_stencil(struct anv_cmd_buffer *cmd_buffer,
    assert(layer_count > 0);
 
    struct blorp_batch batch;
-   anv_blorp_batch_init(cmd_buffer, &batch, 0);
+   anv_blorp_batch_init(cmd_buffer, &batch, &(struct anv_blorp_batch_params) {});
    assert((batch.flags & BLORP_BATCH_USE_COMPUTE) == 0);
 
    struct blorp_surf depth = {};
@@ -2806,7 +2834,7 @@ anv_image_hiz_op(struct anv_cmd_buffer *cmd_buffer,
    assert(plane == 0);
 
    struct blorp_batch batch;
-   anv_blorp_batch_init(cmd_buffer, &batch, 0);
+   anv_blorp_batch_init(cmd_buffer, &batch, &(struct anv_blorp_batch_params) {});
    assert((batch.flags & BLORP_BATCH_USE_COMPUTE) == 0);
 
    struct blorp_surf surf;
@@ -2833,7 +2861,7 @@ anv_image_hiz_clear(struct anv_cmd_buffer *cmd_buffer,
                     const VkClearDepthStencilValue *clear_value)
 {
    struct blorp_batch batch;
-   anv_blorp_batch_init(cmd_buffer, &batch, 0);
+   anv_blorp_batch_init(cmd_buffer, &batch, &(struct anv_blorp_batch_params) {});
    assert((batch.flags & BLORP_BATCH_USE_COMPUTE) == 0);
 
    anv_fast_clear_depth_stencil(cmd_buffer, &batch, image, aspects,
@@ -2854,7 +2882,9 @@ anv_image_mcs_op(struct anv_cmd_buffer *cmd_buffer,
 {
    struct blorp_batch batch;
    anv_blorp_batch_init(cmd_buffer, &batch,
-                        BLORP_BATCH_PREDICATE_ENABLE * predicate);
+                        &(struct anv_blorp_batch_params) {
+                           .flags = BLORP_BATCH_PREDICATE_ENABLE * predicate,
+                        });
    assert((batch.flags & BLORP_BATCH_USE_COMPUTE) == 0);
 
    exec_mcs_op(cmd_buffer, &batch, image, format, swizzle, aspect,
@@ -2874,7 +2904,9 @@ anv_image_ccs_op(struct anv_cmd_buffer *cmd_buffer,
 {
    struct blorp_batch batch;
    anv_blorp_batch_init(cmd_buffer, &batch,
-                        BLORP_BATCH_PREDICATE_ENABLE * predicate);
+                        &(struct anv_blorp_batch_params) {
+                           .flags = BLORP_BATCH_PREDICATE_ENABLE * predicate,
+                        });
    assert((batch.flags & BLORP_BATCH_USE_COMPUTE) == 0);
 
    exec_ccs_op(cmd_buffer, &batch, image, format, swizzle, aspect, level,
@@ -2909,10 +2941,11 @@ anv_CmdCopyMemoryIndirectKHR(
 
    assert(!anv_cmd_buffer_is_blitter_queue(cmd_buffer));
 
-   enum blorp_batch_flags blorp_flags = BLORP_BATCH_USE_COMPUTE;
-
    struct blorp_batch batch;
-   anv_blorp_batch_init(cmd_buffer, &batch, blorp_flags);
+   anv_blorp_batch_init(cmd_buffer, &batch,
+                        &(struct anv_blorp_batch_params) {
+                           .flags = BLORP_BATCH_USE_COMPUTE,
+                        });
 
    blorp_copy_memory_indirect(&batch, indirect_buf_addr, copy_count, stride);
 
@@ -2949,9 +2982,11 @@ anv_CmdCopyMemoryToImageIndirectKHR(
 
    assert(!anv_cmd_buffer_is_blitter_queue(cmd_buffer));
 
-   enum blorp_batch_flags blorp_flags = BLORP_BATCH_USE_COMPUTE;
    struct blorp_batch batch;
-   anv_blorp_batch_init(cmd_buffer, &batch, blorp_flags);
+   anv_blorp_batch_init(cmd_buffer, &batch,
+                        &(struct anv_blorp_batch_params) {
+                           .flags = BLORP_BATCH_USE_COMPUTE,
+                        });
 
    for (int c = 0; c < copy_count; c++) {
       const VkImageSubresourceLayers *img_subresource =

commit 67499b3069c94ca94ea9e28615c7cc1e12d40682

anv: fixup query clear/copy flushing on Xe2+
The decision made in 5da1715de0 ("anv: stop using compute blorp
shaders on Xe2+") has an impact on query cleared/copied using blorp
but this was not reflected correctly in the query clear/copy flush
tracking.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 5da1715de0 ("anv: stop using compute blorp shaders on Xe2+")
Acked-by: Sagar Ghuge <sagar.ghuge@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44387>

src/intel/vulkan/anv_blorp.c
@@ -186,13 +186,9 @@ anv_blorp_batch_init(struct anv_cmd_buffer *cmd_buffer,
    /* For platforms prior to Gfx20 where switching pipeline mode is a
     * significant cost, try to stay in the same pipeline mode.
     */
-   if ((queue_flags & VK_QUEUE_GRAPHICS_BIT) &&
-       params->allow_3d_or_compute_variant &&
-       cmd_buffer->device->info->ver < 20 &&
-       (cmd_buffer->state.current_pipeline ==
-        cmd_buffer->device->physical->gpgpu_pipeline_value)) {
+   if (params->allow_3d_or_compute_variant &&
+       anv_cmd_buffer_blorp_uses_compute(cmd_buffer))
       flags |= BLORP_BATCH_USE_COMPUTE;
-   }
 
    /* Can't have both flags at the same time. */
    assert((flags & BLORP_BATCH_USE_BLITTER) == 0 ||
@@ -985,16 +981,11 @@ static void
 anv_add_buffer_write_pending_bits(struct anv_cmd_buffer *cmd_buffer,
                                   const char *reason)
 {
-   const struct intel_device_info *devinfo = cmd_buffer->device->info;
-
    if (anv_cmd_buffer_is_blitter_queue(cmd_buffer))
       return;
 
    cmd_buffer->state.queries.buffer_write_bits |=
-      (cmd_buffer->state.current_pipeline ==
-       cmd_buffer->device->physical->gpgpu_pipeline_value) ?
-      ANV_QUERY_COMPUTE_WRITES_PENDING_BITS :
-      ANV_QUERY_RENDER_TARGET_WRITES_PENDING_BITS(devinfo);
+      anv_cmd_buffer_shader_query_sync_bits(cmd_buffer);
 }
 
 void anv_CmdCopyImageToMemoryKHR(

src/intel/vulkan/anv_cmd_buffer.c
@@ -391,26 +391,20 @@ anv_cmd_emit_conditional_render_predicate(struct anv_cmd_buffer *cmd_buffer)
 }
 
 static void
-clear_pending_query_bits(enum anv_query_bits *query_bits,
+clear_pending_query_bits(enum anv_pipe_bits *query_bits,
                          enum anv_pipe_bits flushed_bits)
 {
-   if (flushed_bits & ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT)
-      *query_bits &= ~ANV_QUERY_WRITES_RT_FLUSH;
+   /* Remove all the flushed bits except CS_STALL */
+   *query_bits &= ~flushed_bits | ANV_PIPE_CS_STALL_BIT;
 
-   if (flushed_bits & ANV_PIPE_TILE_CACHE_FLUSH_BIT)
-      *query_bits &= ~ANV_QUERY_WRITES_TILE_FLUSH;
-
-   if ((flushed_bits & ANV_PIPE_DATA_CACHE_FLUSH_BIT) &&
-       (flushed_bits & ANV_PIPE_HDC_PIPELINE_FLUSH_BIT) &&
-       (flushed_bits & ANV_PIPE_UNTYPED_DATAPORT_CACHE_FLUSH_BIT))
-      *query_bits &= ~ANV_QUERY_WRITES_TILE_FLUSH;
-
-   /* Once RT/TILE have been flushed, we can consider the CS_STALL flush */
-   if ((*query_bits & (ANV_QUERY_WRITES_TILE_FLUSH |
-                       ANV_QUERY_WRITES_RT_FLUSH |
-                       ANV_QUERY_WRITES_DATA_FLUSH)) == 0 &&
+   /* Only once there is no more flush bits consider the CS_STALL */
+   if ((*query_bits & (ANV_PIPE_TILE_CACHE_FLUSH_BIT |
+                       ANV_PIPE_HDC_PIPELINE_FLUSH_BIT |
+                       ANV_PIPE_UNTYPED_DATAPORT_CACHE_FLUSH_BIT |
+                       ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT |
+                       ANV_PIPE_DATA_CACHE_FLUSH_BIT)) == 0 &&
        (flushed_bits & (ANV_PIPE_END_OF_PIPE_SYNC_BIT | ANV_PIPE_CS_STALL_BIT)))
-      *query_bits &= ~ANV_QUERY_WRITES_CS_STALL;
+      *query_bits &= ~ANV_PIPE_CS_STALL_BIT;
 }
 
 void

src/intel/vulkan/anv_private.h
@@ -4290,18 +4290,6 @@ enum anv_query_bits {
    (ANV_QUERY_WRITES_DATA_FLUSH | \
     ANV_QUERY_WRITES_CS_STALL)
 
-#define ANV_PIPE_QUERY_BITS(pending_query_bits) ( \
-   ((pending_query_bits & ANV_QUERY_WRITES_RT_FLUSH) ?   \
-    ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT : 0) | \
-   ((pending_query_bits & ANV_QUERY_WRITES_TILE_FLUSH) ?   \
-    ANV_PIPE_TILE_CACHE_FLUSH_BIT : 0) | \
-   ((pending_query_bits & ANV_QUERY_WRITES_CS_STALL) ?   \
-    ANV_PIPE_CS_STALL_BIT : 0) | \
-   ((pending_query_bits & ANV_QUERY_WRITES_DATA_FLUSH) ?  \
-    (ANV_PIPE_DATA_CACHE_FLUSH_BIT | \
-     ANV_PIPE_HDC_PIPELINE_FLUSH_BIT | \
-     ANV_PIPE_UNTYPED_DATAPORT_CACHE_FLUSH_BIT) : 0))
-
 #define ANV_PIPE_FLUSH_BITS ( \
    ANV_PIPE_DEPTH_CACHE_FLUSH_BIT | \
    ANV_PIPE_DATA_CACHE_FLUSH_BIT | \
@@ -4863,21 +4851,20 @@ struct anv_cmd_state {
 
    struct {
       /**
-       * Tracks operations susceptible to interfere with queries in the
-       * destination buffer of vkCmdCopyQueryResults, we need those operations to
-       * have completed before we do the work of vkCmdCopyQueryResults.
+       * Tracks synchronization bits that will need to be flushed before doing
+       * work in vkCmdCopyQueryResults.
        */
-      enum anv_query_bits                          buffer_write_bits;
+      enum anv_pipe_bits                        buffer_write_bits;
 
       /**
-       * Tracks clear operations of query buffers that can interact with
-       * vkCmdQueryBegin*, vkCmdWriteTimestamp*,
+       * Tracks query pool clear synchronization bits that will need to be
+       * flushed before doing work in vkCmdQueryBegin*, vkCmdWriteTimestamp*,
        * vkCmdWriteAccelerationStructuresPropertiesKHR, etc...
        *
        * We need the clearing of the buffer completed before with write data with
        * the command streamer or a shader.
        */
-      enum anv_query_bits                          clear_bits;
+      enum anv_pipe_bits                        clear_bits;
    } queries;
 
    /** Tracks whether 3DSTATE_BINDING_TABLE_POINTERS_* instructions need
@@ -5397,6 +5384,43 @@ void
 anv_cmd_buffer_update_pending_query_bits(struct anv_cmd_buffer *cmd_buffer,
                                          enum anv_pipe_bits flushed_bits);
 
+static inline bool
+anv_cmd_buffer_blorp_uses_compute(const struct anv_cmd_buffer *cmd_buffer)
+{
+   if (anv_cmd_buffer_is_compute_queue(cmd_buffer))
+      return true;
+   if (!anv_cmd_buffer_is_render_queue(cmd_buffer))
+      return false;
+   return cmd_buffer->device->info->ver < 20 &&
+      cmd_buffer->state.current_pipeline == cmd_buffer->device->physical->gpgpu_pipeline_value;
+}
+
+static inline enum anv_pipe_bits
+anv_cmd_buffer_shader_query_sync_bits(const struct anv_cmd_buffer *cmd_buffer)
+{
+   const struct anv_device *device = cmd_buffer->device;
+   /* Xe2+ always uses the 3D pipeline for clearing
+    *
+    * Pre-Xe2, the clearing writes are in compute if we're in gpgpu mode on
+    * the render engine or on the compute engine.
+    */
+   const bool op_uses_compute = anv_cmd_buffer_blorp_uses_compute(cmd_buffer);
+
+   enum anv_pipe_bits bits = ANV_PIPE_CS_STALL_BIT;
+   if (op_uses_compute) {
+      bits |= device->info->ver > 12 ?
+         (ANV_PIPE_UNTYPED_DATAPORT_CACHE_FLUSH_BIT | ANV_PIPE_HDC_PIPELINE_FLUSH_BIT) :
+         ANV_PIPE_DATA_CACHE_FLUSH_BIT;
+   } else {
+      bits |= ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT;
+   }
+
+   if (!ANV_DEVINFO_HAS_COHERENT_L3_CS(device->info))
+      bits |= op_uses_compute ? ANV_PIPE_DATA_CACHE_FLUSH_BIT : ANV_PIPE_TILE_CACHE_FLUSH_BIT;
+
+   return bits;
+}
+
 void
 anv_cmd_buffer_bind_shaders(struct vk_command_buffer *cmd_buffer,
                             uint32_t stage_count,
@@ -5595,19 +5619,6 @@ anv_shader_internal_get_pointer(const struct anv_device *device,
       (device->physical->va.shader_heap.addr + shader->kernel.offset) :
       shader->kernel.offset;
 }
-
-struct anv_pipeline_executable {
-   mesa_shader_stage stage;
-
-   struct genisa_stats stats;
-
-   char *nir;
-   char *disasm;
-};
-
-enum anv_pipeline_type {
-   ANV_PIPELINE_RAY_TRACING,
-};
 
 void anv_shader_init_uuid(struct anv_physical_device *device);
 

src/intel/vulkan/genX_cmd_buffer.c
@@ -4379,7 +4379,7 @@ end_command_buffer(struct anv_cmd_buffer *cmd_buffer,
          anv_add_pending_pipe_bits(cmd_buffer,
                                    VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT,
                                    VK_PIPELINE_STAGE_2_TOP_OF_PIPE_BIT,
-                                   ANV_PIPE_QUERY_BITS(cmd_buffer->state.queries.clear_bits),
+                                   cmd_buffer->state.queries.clear_bits,
                                    "query clear flush prior command buffer end");
       }
 
@@ -4496,7 +4496,7 @@ genX(CmdExecuteCommands)(
       anv_add_pending_pipe_bits(container,
                                 VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT,
                                 VK_PIPELINE_STAGE_2_TOP_OF_PIPE_BIT,
-                                ANV_PIPE_QUERY_BITS(container->state.queries.clear_bits),
+                                container->state.queries.clear_bits,
                                 "query clear flush prior to secondary buffer");
    }
 
@@ -5921,7 +5921,7 @@ genX(flush_pipeline_select)(struct anv_cmd_buffer *cmd_buffer,
       anv_add_pending_pipe_bits(cmd_buffer,
                                 VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT,
                                 VK_PIPELINE_STAGE_2_TOP_OF_PIPE_BIT,
-                                ANV_PIPE_QUERY_BITS(cmd_buffer->state.queries.clear_bits),
+                                cmd_buffer->state.queries.clear_bits,
                                 "query clear flush prior to GPGPU");
    }
 

src/intel/vulkan/genX_query.c
@@ -886,17 +886,7 @@ void genX(CmdResetQueryPool)(
                                anv_query_address(pool, firstQuery),
                                queryCount * pool->stride, 0);
 
-      /* The pending clearing writes are in compute if we're in gpgpu mode on
-       * the render engine or on the compute engine.
-       */
-      if (anv_cmd_buffer_is_compute_queue(cmd_buffer) ||
-          cmd_buffer->state.current_pipeline == pdevice->gpgpu_pipeline_value) {
-         cmd_buffer->state.queries.clear_bits =
-            ANV_QUERY_COMPUTE_WRITES_PENDING_BITS;
-      } else {
-         cmd_buffer->state.queries.clear_bits =
-            ANV_QUERY_RENDER_TARGET_WRITES_PENDING_BITS(&pdevice->info);
-      }
+      cmd_buffer->state.queries.clear_bits |= anv_cmd_buffer_shader_query_sync_bits(cmd_buffer);
 
       trace_intel_end_query_clear_blorp(&cmd_buffer->trace, queryCount);
       return;
@@ -1109,11 +1099,9 @@ append_query_clear_flush(struct anv_cmd_buffer *cmd_buffer,
       return false;
 
    anv_add_pending_pipe_bits(cmd_buffer,
-                             VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT |
-                             VK_PIPELINE_STAGE_2_FRAGMENT_SHADER_BIT,
+                             VK_PIPELINE_STAGE_2_TRANSFER_BIT_KHR,
                              VK_PIPELINE_STAGE_2_TOP_OF_PIPE_BIT,
-                             ANV_PIPE_QUERY_BITS(
-                                cmd_buffer->state.queries.clear_bits),
+                             cmd_buffer->state.queries.clear_bits,
                              reason);
    return true;
 }
@@ -1773,20 +1761,11 @@ copy_query_results_with_cs(struct anv_cmd_buffer *cmd_buffer,
                            uint32_t query_count,
                            VkQueryResultFlags flags)
 {
-   enum anv_pipe_bits needed_flushes = 0;
-
-   trace_intel_begin_query_copy_cs(&cmd_buffer->trace);
-
-   /* If render target writes are ongoing, request a render target cache flush
-    * to ensure proper ordering of the commands from the 3d pipe and the
-    * command streamer.
-    */
-
-   const enum anv_query_bits query_bits =
+   enum anv_pipe_bits needed_flushes =
       cmd_buffer->state.queries.buffer_write_bits |
       cmd_buffer->state.queries.clear_bits;
 
-   needed_flushes |= ANV_PIPE_QUERY_BITS(query_bits);
+   trace_intel_begin_query_copy_cs(&cmd_buffer->trace);
 
    /* Occlusion & timestamp queries are written using a PIPE_CONTROL and
     * because we're about to copy values from MI commands, we need to stall
@@ -1941,17 +1920,16 @@ copy_query_results_with_shader(struct anv_cmd_buffer *cmd_buffer,
    }
 
    if ((cmd_buffer->state.queries.buffer_write_bits |
-        cmd_buffer->state.queries.clear_bits) & ANV_QUERY_WRITES_RT_FLUSH) {
+        cmd_buffer->state.queries.clear_bits) & ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT)
       wait_stages |= VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT;
-      needed_flushes |= ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT;
-   }
-
    if ((cmd_buffer->state.queries.buffer_write_bits |
-        cmd_buffer->state.queries.clear_bits) & ANV_QUERY_WRITES_DATA_FLUSH) {
+        cmd_buffer->state.queries.clear_bits) & (ANV_PIPE_UNTYPED_DATAPORT_CACHE_FLUSH_BIT |
+                                                 ANV_PIPE_HDC_PIPELINE_FLUSH_BIT |
+                                                 ANV_PIPE_DATA_CACHE_FLUSH_BIT))
       wait_stages |= VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT;
-      needed_flushes |= (ANV_PIPE_HDC_PIPELINE_FLUSH_BIT |
-                         ANV_PIPE_UNTYPED_DATAPORT_CACHE_FLUSH_BIT);
-   }
+
+   needed_flushes |= cmd_buffer->state.queries.buffer_write_bits |
+                     cmd_buffer->state.queries.clear_bits;
 
    /* Flushes for the queries to complete */
    if (flags & VK_QUERY_RESULT_WAIT_BIT) {
@@ -1989,7 +1967,7 @@ copy_query_results_with_shader(struct anv_cmd_buffer *cmd_buffer,
    VkResult ret =
       anv_device_get_internal_shader(
          cmd_buffer->device,
-         cmd_buffer->state.current_pipeline == GPGPU ?
+         anv_cmd_buffer_blorp_uses_compute(cmd_buffer) ?
          ANV_INTERNAL_KERNEL_COPY_QUERY_RESULTS_COMPUTE :
          ANV_INTERNAL_KERNEL_COPY_QUERY_RESULTS_FRAGMENT,
          &copy_kernel);