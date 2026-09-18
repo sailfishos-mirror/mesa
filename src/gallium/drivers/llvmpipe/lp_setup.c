aerisarn/mesa-uwp

commit 5a1d3bcf26df5b303fbfdc34584c982efb020ade

llvmpipe: Add a linear rasterizer optimized for 2D rendering.
This change adds:

- an alternative rasterizer, which rasterizes bins in a left->right &
  top->bottom linear fashion;

- triangle -> rectangle detection;

- 1:1 blit detection;

- a special TGSI -> LLVM IR code generation that uses 8-bit SSE integers
  in AoS fashion (as opposed to 32bits floats.)

Altogether these changes yield a 2x to 3x performance improvement for 2D
workloads.  It was designed to render Windows 7 Aero and other Windows
built-in 3D applications (like Windows Media Player, Internet Explorer
11, UWP applications) with minimum CPU utilization, but it should be
generally applicable to other 2D-on-3D applications, like desktop
compositors, HTML browsers, 3D based UI toolkits, etc.

This was mostly the brainchild of Keith Whitwell back in 2010.  I wrote
TGSI -> AoS translation.  And many others added bug-fixes and
enhancements over the years: Roland Scheidegger, Brian Paul, and James
Benton.

Known issues:

- piglit spec@!opengl 1.1@quad-invariance will warn that "left and right
half should match" due to rounding error difference

- These optimized paths to kick in is that depth-buffer must not be
used, so some applications which want to benefit from these improvements
might need to be modified to ensure they use painter's algorithm instead
of depth-buffers.

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Reviewed-by: Brian Paul <brianp@vmware.com>
Acked-by: Keith Whitwell <keithw@vmware.com>

v2: Incorporate Dave Airlie feedback: cleanup LP_DEBUG_xx; shrink 3+
empty lines.
v3: silence unused var warning, adapt to new upstream code (point setup)

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11969>

src/gallium/drivers/llvmpipe/lp_setup.c
@@ -39,6 +39,7 @@
 #include "util/u_inlines.h"
 #include "util/u_memory.h"
 #include "util/u_pack_color.h"
+#include "util/u_cpu_detect.h"
 #include "util/u_viewport.h"
 #include "draw/draw_pipe.h"
 #include "util/os_time.h"
@@ -53,6 +54,7 @@
 #include "lp_setup_context.h"
 #include "lp_screen.h"
 #include "lp_state.h"
+#include "lp_jit.h"
 #include "frontend/sw_winsys.h"
 
 #include "draw/draw_context.h"
@@ -84,6 +86,7 @@ lp_setup_get_empty_scene(struct lp_setup_context *setup)
 
    lp_scene_begin_binning(setup->scene, &setup->fb);
 
+   setup->scene->permit_linear_rasterizer = setup->permit_linear_rasterizer;
 }
 
 
@@ -96,6 +99,20 @@ first_triangle( struct lp_setup_context *setup,
    assert(setup->state == SETUP_ACTIVE);
    lp_setup_choose_triangle( setup );
    setup->triangle( setup, v0, v1, v2 );
+}
+
+static boolean
+first_rectangle( struct lp_setup_context *setup,
+                 const float (*v0)[4],
+                 const float (*v1)[4],
+                 const float (*v2)[4],
+                 const float (*v3)[4],
+                 const float (*v4)[4],
+                 const float (*v5)[4])
+{
+   assert(setup->state == SETUP_ACTIVE);
+   lp_setup_choose_rect( setup );
+   return setup->rect( setup, v0, v1, v2, v3, v4, v5 );
 }
 
 static void
@@ -117,7 +134,8 @@ first_point( struct lp_setup_context *setup,
    setup->point( setup, v0 );
 }
 
-void lp_setup_reset( struct lp_setup_context *setup )
+void
+lp_setup_reset( struct lp_setup_context *setup )
 {
    unsigned i;
 
@@ -145,6 +163,7 @@ void lp_setup_reset( struct lp_setup_context *setup )
    setup->line = first_line;
    setup->point = first_point;
    setup->triangle = first_triangle;
+   setup->rect = first_rectangle;
 }
 
 
@@ -576,6 +595,7 @@ lp_setup_set_triangle_state( struct lp_setup_context *setup,
    setup->ccw_is_frontface = ccw_is_frontface;
    setup->cullmode = cull_mode;
    setup->triangle = first_triangle;
+   setup->rect = first_rectangle;
    setup->multisample = multisample;
    setup->pixel_offset = half_pixel_center ? 0.5f : 0.0f;
    setup->bottom_edge_rule = bottom_edge_rule;
@@ -600,6 +620,7 @@ lp_setup_set_line_state( struct lp_setup_context *setup,
 void 
 lp_setup_set_point_state( struct lp_setup_context *setup,
                           float point_size,
+                          boolean point_tri_clip,
                           boolean point_size_per_vertex,
                           uint sprite_coord_enable,
                           uint sprite_coord_origin,
@@ -610,6 +631,7 @@ lp_setup_set_point_state( struct lp_setup_context *setup,
    setup->point_size = point_size;
    setup->sprite_coord_enable = sprite_coord_enable;
    setup->sprite_coord_origin = sprite_coord_origin;
+   setup->point_tri_clip = point_tri_clip;
    setup->point_size_per_vertex = point_size_per_vertex;
    setup->legacy_points = !point_quad_rasterization;
 }
@@ -833,6 +855,7 @@ lp_setup_set_rasterizer_discard(struct lp_setup_context *setup,
       setup->line = first_line;
       setup->point = first_point;
       setup->triangle = first_triangle;
+      setup->rect = first_rectangle;
    }
 }
 
@@ -846,6 +869,24 @@ lp_setup_set_vertex_info(struct lp_setup_context *setup,
 }
 
 
+void 
+lp_setup_set_linear_mode( struct lp_setup_context *setup,
+                          boolean mode )
+{
+   /* The linear rasterizer requires sse2 both at compile and runtime,
+    * in particular for the code in lp_rast_linear_fallback.c.  This
+    * is more than ten-year-old technology, so it's a reasonable
+    * baseline.
+    */
+#if defined(PIPE_ARCH_SSE)
+   setup->permit_linear_rasterizer = (mode &&
+                                      util_get_cpu_caps()->has_sse2);
+#else
+   setup->permit_linear_rasterizer = FALSE;
+#endif
+}
+
+
 /**
  * Called during state validation when LP_NEW_VIEWPORT is set.
  */
@@ -855,12 +896,33 @@ lp_setup_set_viewports(struct lp_setup_context *setup,
                        const struct pipe_viewport_state *viewports)
 {
    struct llvmpipe_context *lp = llvmpipe_context(setup->pipe);
+   float half_height, x0, y0;
    unsigned i;
 
    LP_DBG(DEBUG_SETUP, "%s\n", __FUNCTION__);
 
    assert(num_viewports <= PIPE_MAX_VIEWPORTS);
    assert(viewports);
+
+   /*
+    * Linear rasterizer path for scissor/viewport intersection.
+    *
+    * Calculate "scissor" rect from the (first) viewport.
+    * Just like stored scissor rects need inclusive coords.
+    * For rounding, assume half pixel center (d3d9 should not end up
+    * with fractional viewports) - quite obviously for msaa we'd need
+    * fractional values here (and elsewhere for the point bounding box).
+    *
+    * See: lp_setup.c::try_update_scene_state
+    */
+   half_height = fabsf(viewports[0].scale[1]);
+   x0 = viewports[0].translate[0] - viewports[0].scale[0];
+   y0 = viewports[0].translate[1] - half_height;
+   setup->vpwh.x0 = (int)(x0 + 0.5f);
+   setup->vpwh.x1 = (int)(viewports[0].scale[0] * 2.0f + x0 - 0.5f);
+   setup->vpwh.y0 = (int)(y0 + 0.5f);
+   setup->vpwh.y1 = (int)(half_height * 2.0f + y0 - 0.5f);
+   setup->dirty |= LP_SETUP_NEW_SCISSOR;
 
    /*
     * For use in lp_state_fs.c, propagate the viewport values for all viewports.
@@ -882,7 +944,7 @@ lp_setup_set_viewports(struct lp_setup_context *setup,
 
 
 /**
- * Called during state validation when LP_NEW_SAMPLER_VIEW is set.
+ * Called directly by llvmpipe_set_sampler_views
  */
 void
 lp_setup_set_fragment_sampler_views(struct lp_setup_context *setup,
@@ -1032,7 +1094,6 @@ lp_setup_set_fragment_sampler_views(struct lp_setup_context *setup,
    setup->dirty |= LP_SETUP_NEW_FS;
 }
 
-
 /**
  * Called during state validation when LP_NEW_SAMPLER is set.
  */
@@ -1064,6 +1125,8 @@ lp_setup_set_fragment_sampler_state(struct lp_setup_context *setup,
 
    setup->dirty |= LP_SETUP_NEW_FS;
 }
+
+
 
 
 /**
@@ -1320,11 +1383,43 @@ try_update_scene_state( struct lp_setup_context *setup )
 
    if (setup->dirty & LP_SETUP_NEW_SCISSOR) {
       unsigned i;
+      setup->scissor_or_vp_clip = setup->scissor_test;
+
       for (i = 0; i < PIPE_MAX_VIEWPORTS; ++i) {
          setup->draw_regions[i] = setup->framebuffer;
          if (setup->scissor_test) {
             u_rect_possible_intersection(&setup->scissors[i],
                                          &setup->draw_regions[i]);
+         }
+      }
+      if (setup->permit_linear_rasterizer) {
+         /* NOTE: this only takes first vp into account. */
+         boolean need_vp_scissoring = !!memcmp(&setup->vpwh, &setup->framebuffer,
+                                               sizeof(setup->framebuffer));
+         assert(setup->viewport_index_slot < 0);
+         setup->scissor_or_vp_clip |= need_vp_scissoring;
+         if (need_vp_scissoring) {
+            u_rect_possible_intersection(&setup->vpwh,
+                                         &setup->draw_regions[0]);
+         }
+      }
+      else if (setup->point_tri_clip) {
+         /*
+          * for d3d-style point clipping, we're going to need
+          * the fake vp scissor too. Hence do the intersection with vp,
+          * but don't indicate this. As above this will only work for first vp
+          * which should be ok because we instruct draw to only skip point
+          * clipping when there's only one viewport (this works because d3d10
+          * points are always single pixel).
+          * (Also note that if we have permit_linear_rasterizer this will
+          * cause large points to always get vp scissored, regardless the
+          * point_tri_clip setting.)
+          */
+         boolean need_vp_scissoring = !!memcmp(&setup->vpwh, &setup->framebuffer,
+                                               sizeof(setup->framebuffer));
+         if (need_vp_scissoring) {
+            u_rect_possible_intersection(&setup->vpwh,
+                                         &setup->draw_regions[0]);
          }
       }
    }

src/gallium/drivers/llvmpipe/lp_setup.h
@@ -88,7 +88,8 @@ lp_setup_set_line_state( struct lp_setup_context *setup,
 
 void 
 lp_setup_set_point_state( struct lp_setup_context *setup,
-                          float point_size,                          
+                          float point_size,
+                          boolean point_tri_clip,
                           boolean point_size_per_vertex,
                           uint sprite_coord_enable,
                           uint sprite_coord_origin,
@@ -167,6 +168,10 @@ lp_setup_set_rasterizer_discard( struct lp_setup_context *setup,
 void
 lp_setup_set_vertex_info( struct lp_setup_context *setup, 
                           struct vertex_info *info );
+
+void
+lp_setup_set_linear_mode( struct lp_setup_context *setup, 
+                          boolean permit_linear_rasterizer );
 
 void
 lp_setup_begin_query(struct lp_setup_context *setup,

src/gallium/drivers/llvmpipe/lp_state_derived.c
@@ -173,6 +173,73 @@ compute_vertex_info(struct llvmpipe_context *llvmpipe)
 }
 
 
+static void
+check_linear_rasterizer( struct llvmpipe_context *lp )
+{
+   boolean bgr8;
+   boolean permit_linear;
+   boolean single_vp;
+   boolean clipping_changed = FALSE;
+
+   bgr8 = (lp->framebuffer.nr_cbufs == 1 &&
+           lp->framebuffer.cbufs[0]->texture->target == PIPE_TEXTURE_2D &&
+           (lp->framebuffer.cbufs[0]->format == PIPE_FORMAT_B8G8R8A8_UNORM ||
+            lp->framebuffer.cbufs[0]->format == PIPE_FORMAT_B8G8R8X8_UNORM));
+
+   /* permit_linear means guardband, hence fake scissor, which we can only
+    * handle if there's just one vp. */
+   single_vp = lp->viewport_index_slot < 0;
+   permit_linear = (!lp->framebuffer.zsbuf &&
+                    bgr8 &&
+                    single_vp);
+
+   /* Tell draw that we're happy doing our own x/y clipping.
+    */
+   if (lp->permit_linear_rasterizer != permit_linear) {
+      lp->permit_linear_rasterizer = permit_linear;
+      lp_setup_set_linear_mode(lp->setup, permit_linear);
+      clipping_changed = TRUE;
+   }
+
+   if (lp->single_vp != single_vp) {
+      lp->single_vp = single_vp;
+      clipping_changed = TRUE;
+   }
+
+   /* Disable xy clipping in linear mode.
+    *
+    * Use a guard band if we don't have zsbuf.  Could enable
+    * guardband always - this just to be conservative.
+    *
+    * Because we have a layering violation where the draw module emits
+    * state changes to the driver while we're already inside a draw
+    * call, need to be careful about when we make calls back to the
+    * draw module.  Hence the clipping_changed flag which is as much
+    * to prevent flush recursion as it is to short-circuit noop state
+    * changes.
+    */
+   if (clipping_changed) {
+      draw_set_driver_clipping(lp->draw,
+                               FALSE,
+                               FALSE,
+                               permit_linear,
+                               single_vp);
+   }
+}
+
+
+/**
+ * Handle state changes before clears.
+ * Called just prior to clearing (pipe::clear()).
+ */
+void llvmpipe_update_derived_clear( struct llvmpipe_context *llvmpipe )
+{
+   if (llvmpipe->dirty & (LP_NEW_FS |
+                          LP_NEW_FRAMEBUFFER))
+      check_linear_rasterizer(llvmpipe);
+}
+
+
 /**
  * Handle state changes.
  * Called just prior to drawing anything (pipe::draw_arrays(), etc).
@@ -292,6 +359,8 @@ void llvmpipe_update_derived( struct llvmpipe_context *llvmpipe )
                              PIPE_MAX_VIEWPORTS,
                              llvmpipe->viewports);
    }
+
+   llvmpipe_update_derived_clear(llvmpipe);
 
    llvmpipe->dirty = 0;
 }

src/gallium/auxiliary/gallivm/lp_bld_sample_aos.c
@@ -454,7 +454,7 @@ lp_build_sample_image_nearest(struct lp_build_sample_context *bld,
    LLVMValueRef s_float, t_float = NULL, r_float = NULL;
    LLVMValueRef x_stride;
    LLVMValueRef x_offset, offset;
-   LLVMValueRef x_subcoord, y_subcoord, z_subcoord;
+   LLVMValueRef x_subcoord, y_subcoord = NULL, z_subcoord;
 
    lp_build_context_init(&i32, bld->gallivm, lp_type_int_vec(32, bld->vector_width));
 
@@ -778,7 +778,7 @@ lp_build_sample_image_linear(struct lp_build_sample_context *bld,
    LLVMValueRef y_offset0, y_offset1;
    LLVMValueRef z_offset0, z_offset1;
    LLVMValueRef offset[2][2][2]; /* [z][y][x] */
-   LLVMValueRef x_subcoord[2], y_subcoord[2], z_subcoord[2];
+   LLVMValueRef x_subcoord[2], y_subcoord[2] = {NULL, NULL}, z_subcoord[2];
    unsigned x, y, z;
 
    lp_build_context_init(&i32, bld->gallivm, lp_type_int_vec(32, bld->vector_width));

sailfishos-mirror/mesa

commit 9acc3e3aceb6addc499ee74b62021ee8e17ec34e

llvmpipe: don't skip draw's viewport clipping without the linear rasterizer
When the framebuffer allows the linear rasterizer, llvmpipe tells draw
to use a guard band for x/y clipping and relies on setup to scissor to
the viewport instead. Setup only does that when permit_linear_rasterizer
is set, which lp_setup_set_linear_mode() refuses without SSE2. On other
architectures draw doesn't clip to the viewport and setup doesn't
scissor to it, so primitives are drawn outside the viewport.

Only request guard band clipping when the linear rasterizer is
supported.

Fixes: 5a1d3bcf26d ("llvmpipe: Add a linear rasterizer optimized for 2D rendering.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44529>

src/gallium/drivers/llvmpipe/lp_setup.c
@@ -856,9 +856,8 @@ lp_setup_set_vertex_info(struct lp_setup_context *setup,
 }
 
 
-void
-lp_setup_set_linear_mode(struct lp_setup_context *setup,
-                         bool mode)
+bool
+lp_setup_linear_rasterizer_supported(void)
 {
    /* The linear rasterizer requires sse2 both at compile and runtime,
     * in particular for the code in lp_rast_linear_fallback.c.  This
@@ -866,11 +865,19 @@ lp_setup_set_linear_mode(struct lp_setup_context *setup,
     * baseline.
     */
 #if DETECT_ARCH_SSE
-   setup->permit_linear_rasterizer = (mode &&
-                                      util_get_cpu_caps()->has_sse2);
+   return util_get_cpu_caps()->has_sse2;
 #else
-   setup->permit_linear_rasterizer = false;
+   return false;
 #endif
+}
+
+
+void
+lp_setup_set_linear_mode(struct lp_setup_context *setup,
+                         bool mode)
+{
+   setup->permit_linear_rasterizer = mode &&
+                                     lp_setup_linear_rasterizer_supported();
 }
 
 

src/gallium/drivers/llvmpipe/lp_setup.h
@@ -149,6 +149,9 @@ void
 lp_setup_set_vertex_info(struct lp_setup_context *setup,
                          struct vertex_info *info);
 
+bool
+lp_setup_linear_rasterizer_supported(void);
+
 void
 lp_setup_set_linear_mode(struct lp_setup_context *setup,
                          bool permit_linear_rasterizer);

src/gallium/drivers/llvmpipe/lp_state_derived.c
@@ -236,11 +236,13 @@ check_linear_rasterizer(struct llvmpipe_context *lp)
         lp->framebuffer.cbufs[0].format == PIPE_FORMAT_R8G8B8X8_UNORM));
 
    /* permit_linear means guardband, hence fake scissor, which we can only
-    * handle if there's just one vp. */
+    * handle if there's just one vp, and only when the linear rasterizer is
+    * available, since setup scissors to the viewport only in that case. */
    const bool single_vp = lp->viewport_index_slot < 0;
    const bool permit_linear = (!lp->framebuffer.zsbuf.texture &&
                                valid_cb_format &&
-                               single_vp);
+                               single_vp &&
+                               lp_setup_linear_rasterizer_supported());
 
    /* Tell draw that we're happy doing our own x/y clipping.
     */