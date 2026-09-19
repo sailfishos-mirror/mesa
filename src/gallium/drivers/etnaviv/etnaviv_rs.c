8d47e58918eedfe3ad875c94334b0105b04e375d
Christian Gmeiner <cgmeiner@igalia.com>
2026-09-19 10:41:06 +0200

    etnaviv: rs: Handle partial clears of depth32f formats
    Emulated depth32f formats were missing from the clear bit selection,
    causing depth-only clears to overwrite stencil and vice versa.
    
    Fixes KHR-GLES3.packed_depth_stencil.{clear_buffer,verify_read_pixels}.depth32f_stencil8
    
    Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
    Reviewed-by: Daniel Lang <dalang@gmx.at>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44615>

--- src/gallium/drivers/etnaviv/etnaviv_rs.c
+++ src/gallium/drivers/etnaviv/etnaviv_rs.c
@@ -455,10 +455,12 @@ etna_blit_clear_zs_rs(struct pipe_context *pctx, struct pipe_surface *dst,
    switch (dst->format) {
    case PIPE_FORMAT_Z16_UNORM:
    case PIPE_FORMAT_X8Z24_UNORM:
+   case PIPE_FORMAT_Z32_FLOAT:
       clear_bits_depth = 0xffff;
       clear_bits_stencil = 0;
       break;
    case PIPE_FORMAT_S8_UINT_Z24_UNORM:
+   case PIPE_FORMAT_Z32_FLOAT_S8X24_UINT:
       clear_bits_depth = 0xeeee;
       clear_bits_stencil = 0x1111;
       break;
