de2d71856d8cf1785e38e1b1839a18d6c98611fd
Christian Gmeiner <cgmeiner@igalia.com>
2026-09-19 09:50:29 +0200

    etnaviv: blt: Handle partial clears of depth32f formats
    Emulated depth32f formats were missing from the clear bit selection,
    causing depth-only clears to overwrite stencil and vice versa
    
    Fixes KHR-GLES3.packed_depth_stencil.{verify_read_pixels,clear_buffer}.depth32f_stencil8
    
    Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
    Reviewed-by: Daniel Lang <dalang@gmx.at>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44615>

--- src/gallium/drivers/etnaviv/etnaviv_blt.c
+++ src/gallium/drivers/etnaviv/etnaviv_blt.c
@@ -570,10 +570,12 @@ etna_blit_clear_zs_blt(struct pipe_context *pctx, struct pipe_surface *dst,
    switch (dst->format) {
    case PIPE_FORMAT_Z16_UNORM:
    case PIPE_FORMAT_X8Z24_UNORM:
+   case PIPE_FORMAT_Z32_FLOAT:
       clear_bits_depth = 0xffffffff;
       clear_bits_stencil = 0x00000000;
       break;
    case PIPE_FORMAT_S8_UINT_Z24_UNORM:
+   case PIPE_FORMAT_Z32_FLOAT_S8X24_UINT:
       clear_bits_depth = 0xffffff00;
       clear_bits_stencil = clear_mask;
       break;
