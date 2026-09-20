sailfishos-mirror/mesa

commit e45e9e136c9eb37ee1d1b854fa658e7aeca595bb

radv: remove the VRS rate output if VRS flat shading overrides it
the output has no effect with VRS flat shading

Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42242>

src/amd/vulkan/radv_pipeline_graphics.c
@@ -2678,6 +2678,29 @@ radv_graphics_shaders_compile(const struct radv_compiler_info *compiler_info, st
 
    radv_fill_shader_info(compiler_info, RADV_PIPELINE_GRAPHICS, gfx_state, stages, active_nir_stages);
 
+   /* Remove the primitive shading rate output if VRS flat shading overrides it. */
+   radv_foreach_stage (i, active_nir_stages) {
+      if (!radv_is_last_vgt_stage(&stages[i]))
+         continue;
+
+      struct radv_shader_stage *fs_stage = &stages[MESA_SHADER_FRAGMENT];
+
+      if (fs_stage && fs_stage->info.ps.allow_flat_shading) {
+         stages[i].info.force_vrs_per_vertex = false;
+
+         if (stages[i].info.outinfo.writes_primitive_shading_rate ||
+             stages[i].info.outinfo.writes_primitive_shading_rate_per_primitive) {
+            NIR_PASS(_, stages[i].nir, nir_remove_outputs, MESA_SHADER_FRAGMENT, 0, VARYING_BIT_PRIMITIVE_SHADING_RATE);
+
+            stages[i].info.outinfo.writes_primitive_shading_rate = false;
+            stages[i].info.outinfo.writes_primitive_shading_rate_per_primitive = false;
+            stages[i].nir->info.outputs_written &= ~VARYING_BIT_PRIMITIVE_SHADING_RATE;
+            stages[i].nir->info.per_primitive_outputs &= ~VARYING_BIT_PRIMITIVE_SHADING_RATE;
+         }
+      }
+      break;
+   }
+
    radv_declare_pipeline_args(compiler_info, stages, gfx_state, active_nir_stages, debug);
 
    radv_foreach_stage (i, active_nir_stages) {

commit 3e7549d1a2fedaba560b56a91d73f16d54bfbef9

radv: fix checking whether a FS is present
likely no effect on behavior because all the info fields are not set
when FS is absent

Fixes: e45e9e136c9eb37e - radv: remove the VRS rate output if VRS flat shading overrides it

Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44526>

src/amd/vulkan/radv_pipeline_graphics.c
@@ -2750,7 +2750,7 @@ radv_graphics_shaders_compile(const struct radv_compiler_info *compiler_info, st
 
       struct radv_shader_stage *fs_stage = &stages[MESA_SHADER_FRAGMENT];
 
-      if (fs_stage && (fs_stage->info.ps.allow_flat_shading || fs_stage->info.ps.force_disable_vrs)) {
+      if (fs_stage->nir && (fs_stage->info.ps.allow_flat_shading || fs_stage->info.ps.force_disable_vrs)) {
          stages[i].info.force_vrs_per_vertex = false;
 
          if (stages[i].info.outinfo.writes_primitive_shading_rate ||
@@ -2762,7 +2762,8 @@ radv_graphics_shaders_compile(const struct radv_compiler_info *compiler_info, st
             stages[i].nir->info.outputs_written &= ~VARYING_BIT_PRIMITIVE_SHADING_RATE;
             stages[i].nir->info.per_primitive_outputs &= ~VARYING_BIT_PRIMITIVE_SHADING_RATE;
          }
-      } else if (fs_stage && fs_stage->info.ps.disallow_force_vrs_per_vertex && stages[i].info.force_vrs_per_vertex) {
+      } else if (fs_stage->nir && fs_stage->info.ps.disallow_force_vrs_per_vertex &&
+                 stages[i].info.force_vrs_per_vertex) {
          stages[i].info.force_vrs_per_vertex = false;
          stages[i].info.outinfo.writes_primitive_shading_rate = false;
 