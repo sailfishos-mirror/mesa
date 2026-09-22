d09fa965f9055efea5f0d3c162a76e567329c485
Lakshman Chandu Kondreddy <lkondred@qti.qualcomm.com>
2026-09-22 20:45:08 +0530

    freedreno: Add chip-id support for Adreno X2-85
    Enable basic functionality of Adreno X2-85 in freedreno.
    Verified weston,glmark2-es2-wayland and vkmark.
    
    Signed-off-by: Lakshman Chandu Kondreddy <lkondred@qti.qualcomm.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44710>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1630,6 +1630,7 @@ add_gpus([
 
 add_gpus([
         GPUId(chip_id=0xffff44070031, name="Adreno (TM) X2-85"),
+        GPUId(chip_id=0xffff44060030, name="Adreno (TM) X2-85"),
     ], A6xxGPUInfo(
         CHIP.A8XX,
         [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen2],
