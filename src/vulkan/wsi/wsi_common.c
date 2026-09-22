sailfishos-mirror/mesa

commit 3d71e6553ed8ff5fc02e2c4a6681b72a037e7ae0

wsi: correct the erroneous assertion
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15452
Fixes: d50937f137201e1d90ce ("vulkan/wsi: Implement prime in a completely generic way")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41647>

src/vulkan/wsi/wsi_common.c
@@ -2738,7 +2738,7 @@ wsi_create_buffer_blit_context(const struct wsi_swapchain *chain,
 
    VkMemoryRequirements reqs;
    wsi->GetBufferMemoryRequirements(chain->device, image->blit.buffer, &reqs);
-   assert(reqs.size <= info->linear_size);
+   assert(reqs.size >= info->linear_size);
 
    struct wsi_memory_allocate_info memory_wsi_info = {
       .sType = VK_STRUCTURE_TYPE_WSI_MEMORY_ALLOCATE_INFO_MESA,

commit 6ac7466d40be92bfec4fe3dfe21376a6af45a8e0

vulkan/wsi: support allocating more memory for buffers
Some Vulkan drivers need some padding for buffers of some specific
sizes, e.g. the opensource pvr Vulkan driver. These situations are
currently not properly handled, and leads to Vulkan usage violations.

Use the memory size returned by vkGetBufferMemoryRequirements() for
buffer memory allocations.

Fixes: 3d71e6553ed8 ("wsi: correct the erroneous assertion")
Signed-off-by: Icenowy Zheng <uwu@icenowy.me>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36266>

src/vulkan/wsi/wsi_common.c
@@ -3134,14 +3134,14 @@ wsi_create_buffer_blit_context(const struct wsi_swapchain *chain,
    VkMemoryAllocateInfo buf_mem_info = {
       .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
       .pNext = &buf_mem_dedicated_info,
-      .allocationSize = info->linear_size,
+      .allocationSize = reqs.size,
       .memoryTypeIndex =
          info->select_blit_dst_memory_type(wsi, reqs.memoryTypeBits),
    };
 
    void *sw_host_ptr = NULL;
    if (info->alloc_shm)
-      sw_host_ptr = info->alloc_shm(image, info->linear_size);
+      sw_host_ptr = info->alloc_shm(image, reqs.size);
 
    VkExportMemoryAllocateInfo memory_export_info;
    VkImportMemoryHostPointerInfoEXT host_ptr_info;