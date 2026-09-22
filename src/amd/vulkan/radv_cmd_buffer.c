sailfishos-mirror/mesa

commit 9ea322b5524849828e29332075ab120588b04d95

radv: let the PS epilog determine DB_SHADER_CONTROL for mrtz, not the shader
This is needed to make it possible for PS epilogs to disable depth/
stencil/samplemask/etc. outputs.

Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43025>

src/amd/vulkan/radv_cmd_buffer.c
@@ -9044,6 +9044,16 @@ radv_bind_ps_epilog(struct radv_cmd_buffer *cmd_buffer)
    radv_cs_add_buffer(device->ws, cs->b, ps_epilog->bo);
 
    cmd_buffer->state.dirty |= RADV_CMD_DIRTY_PS_EPILOG_STATE;
+
+   /* The PS epilog might eliminate these outputs depending on dynamic states. Even if mrtz is fully eliminated,
+    * we still need to update DB_SHADER_CONTROL to disable those outputs.
+    *
+    * These flags only indicate that the PS epilog is responsible for handling mrtz. The flags don't mean that
+    * mrtz is present.
+    */
+   if (ps_epilog->key.ps.has_depth_output || ps_epilog->key.ps.has_stencil_output ||
+       ps_epilog->key.ps.has_sample_mask_output || ps_epilog->key.ps.alpha_to_coverage_via_mrtz)
+      cmd_buffer->state.dirty |= RADV_CMD_DIRTY_DB_SHADER_CONTROL;
 }
 
 /* This function binds/unbinds a shader to the cmdbuffer state. */
@@ -12611,6 +12621,9 @@ radv_emit_db_shader_control(struct radv_cmd_buffer *cmd_buffer)
 
    if (ps) {
       db_shader_control = ps->regs.ps.db_shader_control;
+
+      if (ps->info.ps.exports_mrtz_via_epilog)
+         db_shader_control |= cmd_buffer->state.ps_epilog->db_shader_control;
    } else {
       db_shader_control = S_02880C_CONSERVATIVE_Z_EXPORT(V_02880C_EXPORT_ANY_Z) |
                           S_02880C_Z_ORDER(V_02880C_EARLY_Z_THEN_LATE_Z) |

src/amd/vulkan/radv_shader.c
@@ -1995,13 +1995,17 @@ radv_precompute_registers_hw_fs(struct radv_device *device, struct radv_shader *
    const bool disable_rbplus = pdev->info.has_rbplus && !pdev->info.rbplus_allowed;
 
    regs->ps.db_shader_control =
-      S_02880C_Z_EXPORT_ENABLE(info->ps.writes_z) | S_02880C_STENCIL_TEST_VAL_EXPORT_ENABLE(info->ps.writes_stencil) |
-      S_02880C_KILL_ENABLE(info->ps.can_discard) | S_02880C_MASK_EXPORT_ENABLE(mask_export_enable) |
-      S_02880C_CONSERVATIVE_Z_EXPORT(conservative_z_export) | S_02880C_Z_ORDER(z_order) |
-      S_02880C_DEPTH_BEFORE_SHADER(info->ps.early_fragment_test) |
+      S_02880C_KILL_ENABLE(info->ps.can_discard) | S_02880C_CONSERVATIVE_Z_EXPORT(conservative_z_export) |
+      S_02880C_Z_ORDER(z_order) | S_02880C_DEPTH_BEFORE_SHADER(info->ps.early_fragment_test) |
       S_02880C_PRE_SHADER_DEPTH_COVERAGE_ENABLE(info->ps.post_depth_coverage) |
       S_02880C_EXEC_ON_HIER_FAIL(info->ps.writes_memory) | S_02880C_EXEC_ON_NOOP(info->ps.writes_memory) |
       S_02880C_DUAL_QUAD_DISABLE(disable_rbplus) | S_02880C_PRIMITIVE_ORDERED_PIXEL_SHADER(info->ps.pops);
+
+   if (!info->ps.exports_mrtz_via_epilog) {
+      regs->ps.db_shader_control |= S_02880C_Z_EXPORT_ENABLE(info->ps.writes_z) |
+                                    S_02880C_STENCIL_TEST_VAL_EXPORT_ENABLE(info->ps.writes_stencil) |
+                                    S_02880C_MASK_EXPORT_ENABLE(mask_export_enable);
+   }
 
    if (pdev->info.gfx_level >= GFX12) {
       regs->ps.spi_ps_in_control = S_028640_PS_W32_EN(info->wave_size == 32);
@@ -3200,6 +3204,7 @@ radv_shader_part_create(struct radv_device *device, struct radv_shader_part_bina
 
    shader_part->spi_shader_col_format = binary->info.spi_shader_col_format;
    shader_part->cb_shader_mask = binary->info.cb_shader_mask;
+   shader_part->db_shader_control = binary->info.db_shader_control;
    shader_part->spi_shader_z_format = binary->info.spi_shader_z_format;
 
    if (pdev->info.gfx_level >= GFX11)
@@ -3758,6 +3763,9 @@ radv_create_ps_epilog(struct radv_device *device, const struct radv_ps_epilog_ke
 
    binary->info.spi_shader_col_format = key->spi_shader_col_format;
    binary->info.cb_shader_mask = ac_get_cb_shader_mask(key->spi_shader_col_format);
+   binary->info.db_shader_control = S_02880C_Z_EXPORT_ENABLE(key->has_depth_output) |
+                                    S_02880C_STENCIL_TEST_VAL_EXPORT_ENABLE(key->has_stencil_output) |
+                                    S_02880C_MASK_EXPORT_ENABLE(key->has_sample_mask_output);
    binary->info.spi_shader_z_format = key->spi_shader_z_format;
 
    epilog = radv_shader_part_create(device, binary, info.wave_size);

src/amd/vulkan/radv_shader.h
@@ -374,6 +374,7 @@ struct radv_shader_part_binary {
    struct {
       uint32_t spi_shader_col_format;
       uint32_t cb_shader_mask;
+      uint32_t db_shader_control;
       uint32_t spi_shader_z_format;
    } info;
 
@@ -486,6 +487,7 @@ struct radv_shader_part {
    bool nontrivial_divisors;
    uint32_t spi_shader_col_format;
    uint32_t cb_shader_mask;
+   uint32_t db_shader_control;
    uint32_t spi_shader_z_format;
    uint32_t inst_pref_size;
    uint64_t upload_seq;

commit 8e4cf1b2903b49266d2d5c27b645183cb9ee31d7

radv: fix DB_SHADER_CONTROL tracking with mixed epilog/no-epilog draws
Consider the following:

1. Draw with depth export, uses epilog
2. Draw without depth export, no epilog
3. Draw with depth export, uses epilog

All three ps have the same db_shader_control, so binding the new
ps does not set RADV_CMD_DIRTY_DB_SHADER_CONTROL.

However, when 1 and 3 use the same ps epilog, the epilog bound
is still the same, so the code responsible for setting
RADV_CMD_DIRTY_DB_SHADER_CONTROL is skipped.

Fixes: 9ea322b5524 ("radv: let the PS epilog determine DB_SHADER_CONTROL for mrtz, not the shader")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/16305
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/16363
Reviewed-by: Marek Olšák <maraeo@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44568>

src/amd/vulkan/radv_cmd_buffer.c
@@ -9368,7 +9368,8 @@ radv_bind_fragment_shader(struct radv_cmd_buffer *cmd_buffer, const struct radv_
 
    if (!previous_ps || previous_ps->regs.ps.db_shader_control != ps->regs.ps.db_shader_control ||
        previous_ps->info.ps.pops_is_per_sample != ps->info.ps.pops_is_per_sample ||
-       previous_ps->info.ps.uses_fbfetch_output != ps->info.ps.uses_fbfetch_output)
+       previous_ps->info.ps.uses_fbfetch_output != ps->info.ps.uses_fbfetch_output ||
+       previous_ps->info.ps.has_epilog != ps->info.ps.has_epilog)
       cmd_buffer->state.dirty |= RADV_CMD_DIRTY_DB_SHADER_CONTROL;
 }
 