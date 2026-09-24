sailfishos-mirror/mesa

commit 07b737cb4dae02540697c62c88b31efc7497afb3

radv: implement queryResultStatusSupport for enc queues
Since we only have the atomic dependent write memory, we need to reuse
the encode feedback mechanism to know when the query is available.

Signed-off-by: Benjamin Cheng <benjamin.cheng@amd.com>
Reviewed-by: David Rosca <david.rosca@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42949>

src/amd/vulkan/radv_query.c
@@ -1896,7 +1896,7 @@ radv_create_query_pool(struct radv_device *device, const VkQueryPoolCreateInfo *
                                 : sizeof(struct radv_query_pool);
 
    struct radv_query_pool *pool =
-      vk_alloc2(&device->vk.alloc, pAllocator, pool_struct_size, 8, VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+      vk_zalloc2(&device->vk.alloc, pAllocator, pool_struct_size, 8, VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
 
    if (!pool)
       return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
@@ -1973,6 +1973,14 @@ radv_create_query_pool(struct radv_device *device, const VkQueryPoolCreateInfo *
          pool->stride = 16;
       }
       break;
+   case VK_QUERY_TYPE_RESULT_STATUS_ONLY_KHR:
+      {
+         const VkVideoProfileInfoKHR *profile = vk_find_struct_const(pCreateInfo->pNext, VIDEO_PROFILE_INFO_KHR);
+         assert(profile->videoCodecOperation == VK_VIDEO_CODEC_OPERATION_ENCODE_H264_BIT_KHR ||
+                profile->videoCodecOperation == VK_VIDEO_CODEC_OPERATION_ENCODE_H265_BIT_KHR ||
+                profile->videoCodecOperation == VK_VIDEO_CODEC_OPERATION_ENCODE_AV1_BIT_KHR);
+      }
+      FALLTHROUGH;
    case VK_QUERY_TYPE_VIDEO_ENCODE_FEEDBACK_KHR:
       pool->stride = 48;
       break;
@@ -2042,6 +2050,7 @@ radv_get_rel_timeout_for_query(VkQueryType type)
    case VK_QUERY_TYPE_PRIMITIVES_GENERATED_EXT:
    case VK_QUERY_TYPE_TRANSFORM_FEEDBACK_STREAM_EXT:
    case VK_QUERY_TYPE_MESH_PRIMITIVES_GENERATED_EXT:
+   case VK_QUERY_TYPE_RESULT_STATUS_ONLY_KHR:
    case VK_QUERY_TYPE_VIDEO_ENCODE_FEEDBACK_KHR:
       return radv_get_tdr_timeout_for_ip(AMD_IP_GFX) * 2;
    default:
@@ -2367,6 +2376,7 @@ radv_GetQueryPoolResults(VkDevice _device, VkQueryPool queryPool, uint32_t first
          }
          break;
       }
+      case VK_QUERY_TYPE_RESULT_STATUS_ONLY_KHR:
       case VK_QUERY_TYPE_VIDEO_ENCODE_FEEDBACK_KHR: {
          const bool write_memory =
             pdev->info.video_caps.queue[AMD_IP_VCN_ENC].write_memory == AC_VIDEO_WRITE_MEMORY_SUPPORT_FULL;
@@ -2608,6 +2618,7 @@ emit_begin_query(struct radv_cmd_buffer *cmd_buffer, struct radv_query_pool *poo
    case VK_QUERY_TYPE_MESH_PRIMITIVES_GENERATED_EXT:
       radv_begin_ms_prim_query(cmd_buffer, va);
       break;
+   case VK_QUERY_TYPE_RESULT_STATUS_ONLY_KHR:
    case VK_QUERY_TYPE_VIDEO_ENCODE_FEEDBACK_KHR:
       cmd_buffer->video.feedback_query_va = va;
       break;
@@ -2642,6 +2653,7 @@ emit_end_query(struct radv_cmd_buffer *cmd_buffer, struct radv_query_pool *pool,
    case VK_QUERY_TYPE_MESH_PRIMITIVES_GENERATED_EXT:
       radv_end_ms_prim_query(cmd_buffer, va, avail_va);
       break;
+   case VK_QUERY_TYPE_RESULT_STATUS_ONLY_KHR:
    case VK_QUERY_TYPE_VIDEO_ENCODE_FEEDBACK_KHR:
       cmd_buffer->video.feedback_query_va = 0;
       break;

src/amd/vulkan/radv_physical_device.c
@@ -3102,7 +3102,8 @@ radv_GetPhysicalDeviceQueueFamilyProperties2(VkPhysicalDevice physicalDevice, ui
          }
          case VK_STRUCTURE_TYPE_QUEUE_FAMILY_QUERY_RESULT_STATUS_PROPERTIES_KHR: {
             VkQueueFamilyQueryResultStatusPropertiesKHR *prop = (VkQueueFamilyQueryResultStatusPropertiesKHR *)ext;
-            prop->queryResultStatusSupport = VK_FALSE;
+            prop->queryResultStatusSupport =
+               pQueueFamilyProperties[i].queueFamilyProperties.queueFlags & VK_QUEUE_VIDEO_ENCODE_BIT_KHR;
             break;
          }
          case VK_STRUCTURE_TYPE_QUEUE_FAMILY_VIDEO_PROPERTIES_KHR: {

commit 5338eb32de4615498f4029531a459cdd8ad00bbf

radv/query: Allow creating "noop" pools
Query pools of type VK_QUERY_TYPE_RESULT_STATUS_ONLY_KHR are allowed to
be created as long as a single queue supports it. In our case we only
support it for video encode usages, which require the chaining of a
VkVideoProfileInfoKHR. This means we need to allow apps to create a
result status query pool that cannot be used in any command buffer, and
always returns NOT_READY when queried from the CPU.

Fixes: 07b737cb4da ("radv: implement queryResultStatusSupport for enc queues")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/16379
Signed-off-by: Benjamin Cheng <benjamin.cheng@amd.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44634>

src/amd/vulkan/radv_query.c
@@ -1855,6 +1855,9 @@ static void
 radv_reset_query_pool(struct radv_device *device, struct radv_query_pool *pool, uint32_t first_query,
                       uint32_t query_count)
 {
+   if (!pool->bo)
+      return;
+
    const struct radv_physical_device *pdev = radv_device_physical(device);
    uint32_t value = query_clear_value(pool->vk.query_type);
    uint32_t *data = (uint32_t *)(pool->ptr + first_query * pool->stride);
@@ -1876,10 +1879,11 @@ radv_destroy_query_pool(struct radv_device *device, const VkAllocationCallbacks 
    if (pool->vk.query_type == VK_QUERY_TYPE_PERFORMANCE_QUERY_KHR)
       radv_pc_deinit_query_pool((struct radv_pc_query_pool *)pool);
 
-   if (pool->bo)
+   if (pool->bo) {
       radv_bo_destroy(device, &pool->vk.base, pool->bo);
+      radv_rmv_log_resource_destroy(device, (uint64_t)radv_query_pool_to_handle(pool));
+   }
 
-   radv_rmv_log_resource_destroy(device, (uint64_t)radv_query_pool_to_handle(pool));
    vk_query_pool_finish(&pool->vk);
    vk_free2(&device->vk.alloc, pAllocator, pool);
 }
@@ -1973,12 +1977,10 @@ radv_create_query_pool(struct radv_device *device, const VkQueryPoolCreateInfo *
       }
       break;
    case VK_QUERY_TYPE_RESULT_STATUS_ONLY_KHR:
-      {
-         const VkVideoProfileInfoKHR *profile = vk_find_struct_const(pCreateInfo->pNext, VIDEO_PROFILE_INFO_KHR);
-         assert(profile->videoCodecOperation == VK_VIDEO_CODEC_OPERATION_ENCODE_H264_BIT_KHR ||
-                profile->videoCodecOperation == VK_VIDEO_CODEC_OPERATION_ENCODE_H265_BIT_KHR ||
-                profile->videoCodecOperation == VK_VIDEO_CODEC_OPERATION_ENCODE_AV1_BIT_KHR);
-      }
+      if (pool->vk.video_profile.op != VK_VIDEO_CODEC_OPERATION_ENCODE_H264_BIT_KHR &&
+          pool->vk.video_profile.op != VK_VIDEO_CODEC_OPERATION_ENCODE_H265_BIT_KHR &&
+          pool->vk.video_profile.op != VK_VIDEO_CODEC_OPERATION_ENCODE_AV1_BIT_KHR)
+         goto out;
       FALLTHROUGH;
    case VK_QUERY_TYPE_VIDEO_ENCODE_FEEDBACK_KHR:
       /* base encode feedback size */
@@ -2025,8 +2027,10 @@ radv_create_query_pool(struct radv_device *device, const VkQueryPoolCreateInfo *
    if (pCreateInfo->flags & VK_QUERY_POOL_CREATE_RESET_BIT_KHR)
       radv_reset_query_pool(device, pool, 0, pool->vk.query_count);
 
+out:
    *pQueryPool = radv_query_pool_to_handle(pool);
-   radv_rmv_log_query_pool_create(device, *pQueryPool);
+   if (pool->bo)
+      radv_rmv_log_query_pool_create(device, *pQueryPool);
    return VK_SUCCESS;
 }
 
@@ -2392,6 +2396,15 @@ radv_GetQueryPoolResults(VkDevice _device, VkQueryPool queryPool, uint32_t first
          break;
       }
       case VK_QUERY_TYPE_RESULT_STATUS_ONLY_KHR:
+         if (!pool->bo) {
+            /* this is a no-op pool that cannot be used */
+            if (flags & VK_QUERY_RESULT_64_BIT)
+               *(uint64_t *)data = VK_QUERY_RESULT_STATUS_NOT_READY_KHR;
+            else
+               *(uint32_t *)data = VK_QUERY_RESULT_STATUS_NOT_READY_KHR;
+            goto out;
+         }
+      FALLTHROUGH;
       case VK_QUERY_TYPE_VIDEO_ENCODE_FEEDBACK_KHR: {
          const bool write_memory =
             pdev->info.video_caps.queue[AMD_IP_VCN_ENC].write_memory == AC_VIDEO_WRITE_MEMORY_SUPPORT_FULL;
@@ -2462,6 +2475,7 @@ radv_GetQueryPoolResults(VkDevice _device, VkQueryPool queryPool, uint32_t first
       }
    }
 
+out:
    if (result == VK_ERROR_DEVICE_LOST)
       vk_device_set_lost(&device->vk, "GetQueryPoolResults timed out");
 
@@ -2570,6 +2584,9 @@ radv_CmdResetQueryPool(VkCommandBuffer commandBuffer, VkQueryPool queryPool, uin
    const struct radv_physical_device *pdev = radv_device_physical(device);
    uint32_t value = query_clear_value(pool->vk.query_type);
    uint32_t flush_bits = 0;
+
+   if (!pool->bo)
+      return;
 
    if (cmd_buffer->qf == RADV_QUEUE_VIDEO_DEC || cmd_buffer->qf == RADV_QUEUE_VIDEO_ENC)
       /* video queries don't work like this */