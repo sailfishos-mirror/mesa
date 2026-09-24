aras-p/glsl-optimizer

commit 42d31bf39095c8377a107510b90b3fea5fb3cc76

mesa: added glGet code for GL_ARB_transform_feedback2

src/mesa/main/get.c
@@ -291,6 +291,7 @@ EXTRA_EXT(EXT_compiled_vertex_array);
 EXTRA_EXT(ARB_sync);
 EXTRA_EXT(ARB_vertex_shader);
 EXTRA_EXT(EXT_transform_feedback);
+EXTRA_EXT(ARB_transform_feedback2);
 EXTRA_EXT(EXT_pixel_buffer_object);
 EXTRA_EXT(ARB_vertex_program);
 EXTRA_EXT2(NV_point_sprite, ARB_point_sprite);
@@ -1207,6 +1208,14 @@ static const struct value_desc values[] = {
      CONTEXT_INT(Const.MaxTransformFeedbackSeparateComponents),
      extra_EXT_transform_feedback },
 
+   /* GL_ARB_transform_feedback2 */
+   { GL_TRANSFORM_FEEDBACK_BUFFER_PAUSED, LOC_CUSTOM, TYPE_BOOLEAN, 0,
+     extra_ARB_transform_feedback2 },
+   { GL_TRANSFORM_FEEDBACK_BUFFER_ACTIVE, LOC_CUSTOM, TYPE_BOOLEAN, 0,
+     extra_ARB_transform_feedback2 },
+   { GL_TRANSFORM_FEEDBACK_BINDING, LOC_CUSTOM, TYPE_INT, 0,
+     extra_ARB_transform_feedback2 },
+
    /* GL 3.0 */
    { GL_NUM_EXTENSIONS, LOC_CUSTOM, TYPE_INT, 0, extra_version_30 },
    { GL_MAJOR_VERSION, CONTEXT_INT(VersionMajor), extra_version_30 },
@@ -1573,6 +1582,15 @@ find_custom_value(GLcontext *ctx, const struct value_desc *d, union value *v)
       break;
    case GL_TRANSFORM_FEEDBACK_BUFFER_BINDING:
       v->value_int = ctx->TransformFeedback.CurrentBuffer->Name;
+      break;
+   case GL_TRANSFORM_FEEDBACK_BUFFER_PAUSED:
+      v->value_int = ctx->TransformFeedback.CurrentObject->Paused;
+      break;
+   case GL_TRANSFORM_FEEDBACK_BUFFER_ACTIVE:
+      v->value_int = ctx->TransformFeedback.CurrentObject->Active;
+      break;
+   case GL_TRANSFORM_FEEDBACK_BINDING:
+      v->value_int = ctx->TransformFeedback.CurrentObject->Name;
       break;
    case GL_CURRENT_PROGRAM:
       v->value_int =

sailfishos-mirror/mesa

commit bf56cbeaad0709cf03de3a47746fc73557de9533

mesa: store boolean custom glGet values in value_bool
GL_TRANSFORM_FEEDBACK_BUFFER_ACTIVE, GL_TRANSFORM_FEEDBACK_BUFFER_PAUSED,
GL_DEBUG_OUTPUT and GL_DEBUG_OUTPUT_SYNCHRONOUS are TYPE_BOOLEAN in
get_hash_params.py, so the value is read back through value_bool, but
find_custom_value() stored them in value_int. That only works on
little-endian hosts, where the first byte of the int is its low byte.

Fixes the ext_transform_feedback2 "misc. api queries" piglit test on
ppc64.

Fixes: 42d31bf3909 ("mesa: added glGet code for GL_ARB_transform_feedback2")
Fixes: aa37cbdff7b ("mesa: Allow Get*() of several forgotten IsEnabled() pnames.")
Reviewed-by: Marek Olšák <maraeo@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44531>

src/mesa/main/get.c
@@ -1119,10 +1119,10 @@ find_custom_value(struct gl_context *ctx, const struct value_desc *d, union valu
                         ctx->TransformFeedback.CurrentBuffer->Name : 0;
       break;
    case GL_TRANSFORM_FEEDBACK_BUFFER_PAUSED:
-      v->value_int = ctx->TransformFeedback.CurrentObject->Paused;
+      v->value_bool = ctx->TransformFeedback.CurrentObject->Paused;
       break;
    case GL_TRANSFORM_FEEDBACK_BUFFER_ACTIVE:
-      v->value_int = ctx->TransformFeedback.CurrentObject->Active;
+      v->value_bool = ctx->TransformFeedback.CurrentObject->Active;
       break;
    case GL_TRANSFORM_FEEDBACK_BINDING:
       v->value_int = ctx->TransformFeedback.CurrentObject->Name;
@@ -1238,6 +1238,8 @@ find_custom_value(struct gl_context *ctx, const struct value_desc *d, union valu
    /* GL_KHR_DEBUG */
    case GL_DEBUG_OUTPUT:
    case GL_DEBUG_OUTPUT_SYNCHRONOUS:
+      v->value_bool = _mesa_get_debug_state_int(ctx, d->pname);
+      break;
    case GL_DEBUG_LOGGED_MESSAGES:
    case GL_DEBUG_NEXT_LOGGED_MESSAGE_LENGTH:
    case GL_DEBUG_GROUP_STACK_DEPTH: