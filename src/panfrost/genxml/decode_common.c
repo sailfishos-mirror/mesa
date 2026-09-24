sailfishos-mirror/mesa

commit 9ff413aa28e7d91b8b88a97adf439f252df72496

panfrost: build v11 support
This wires up v11 support for the shared panfrost code.

Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41803>

src/panfrost/genxml/decode_common.c
@@ -417,6 +417,9 @@ pandecode_interpret_cs(struct pandecode_context *ctx, uint64_t queue_gpu_va,
    case 10:
       pandecode_interpret_cs_v10(ctx, queue_gpu_va, size, gpu_id, regs);
       break;
+   case 11:
+      pandecode_interpret_cs_v11(ctx, queue_gpu_va, size, gpu_id, regs);
+      break;
    case 12:
       pandecode_interpret_cs_v12(ctx, queue_gpu_va, size, gpu_id, regs);
       break;

src/panfrost/genxml/decode.h
@@ -120,6 +120,13 @@ void pandecode_cs_binary_v10(struct pandecode_context *ctx, uint64_t bin,
 void pandecode_cs_trace_v10(struct pandecode_context *ctx, uint64_t trace,
                             uint32_t trace_size, uint64_t gpu_id);
 
+void pandecode_interpret_cs_v11(struct pandecode_context *ctx, uint64_t queue,
+                                uint32_t size, uint64_t gpu_id, uint32_t *regs);
+void pandecode_cs_binary_v11(struct pandecode_context *ctx, uint64_t bin,
+                             uint32_t bin_size);
+void pandecode_cs_trace_v11(struct pandecode_context *ctx, uint64_t trace,
+                            uint32_t trace_size, uint64_t gpu_id);
+
 void pandecode_interpret_cs_v12(struct pandecode_context *ctx, uint64_t queue,
                                 uint32_t size, uint64_t gpu_id, uint32_t *regs);
 void pandecode_cs_binary_v12(struct pandecode_context *ctx, uint64_t bin,

src/panfrost/genxml/gen_macros.h
@@ -55,6 +55,9 @@
 #elif (PAN_ARCH == 10)
 #define GENX(X) X##_v10
 #include "genxml/v10_pack.h"
+#elif (PAN_ARCH == 11)
+#define GENX(X) X##_v11
+#include "genxml/v11_pack.h"
 #elif (PAN_ARCH == 12)
 #define GENX(X) X##_v12
 #include "genxml/v12_pack.h"

src/panfrost/genxml/meson.build
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 pan_packers = []
-foreach packer : ['common', 'v4', 'v5', 'v6', 'v7', 'v9', 'v10', 'v12', 'v13', 'v14']
+foreach packer : ['common', 'v4', 'v5', 'v6', 'v7', 'v9', 'v10', 'v11', 'v12', 'v13', 'v14']
   pan_packers += custom_target(
     packer + '_pack.h',
     input : ['gen_pack.py', packer + '.xml'],
@@ -20,7 +20,7 @@ idep_pan_packers = declare_dependency(
 
 libpanfrost_decode_per_arch = []
 
-foreach ver : ['4', '5', '6', '7', '9', '10', '12', '13', '14']
+foreach ver : ['4', '5', '6', '7', '9', '10', '11', '12', '13', '14']
   libpanfrost_decode_per_arch += static_library(
     'pandecode-arch-v' + ver,
     ['decode.c', 'decode_jm.c', 'decode_csf.c', pan_packers],

src/panfrost/lib/meson.build
@@ -4,7 +4,7 @@
 
 subdir('kmod')
 
-pixel_format_versions = ['5', '6', '7', '9', '10', '12', '13', '14']
+pixel_format_versions = ['5', '6', '7', '9', '10', '11', '12', '13', '14']
 libpanfrost_pixel_format = []
 
 deps_for_libpanfrost = [dep_libdrm, idep_pan_packers, idep_mesautil, libpanfrost_model_dep]
@@ -22,7 +22,7 @@ endforeach
 
 libpanfrost_per_arch = []
 
-foreach ver : ['4', '5', '6', '7', '9', '10', '12', '13', '14']
+foreach ver : ['4', '5', '6', '7', '9', '10', '11', '12', '13', '14']
   libpanfrost_per_arch += static_library(
     'pan-arch-v' + ver,
     [

src/panfrost/lib/pan_format.h
@@ -174,6 +174,8 @@ extern const struct pan_blendable_format
 extern const struct pan_blendable_format
    pan_blendable_formats_v10[PIPE_FORMAT_COUNT];
 extern const struct pan_blendable_format
+   pan_blendable_formats_v11[PIPE_FORMAT_COUNT];
+extern const struct pan_blendable_format
    pan_blendable_formats_v12[PIPE_FORMAT_COUNT];
 extern const struct pan_blendable_format
    pan_blendable_formats_v13[PIPE_FORMAT_COUNT];
@@ -193,6 +195,7 @@ pan_blendable_format_table(unsigned arch)
    FMT_TABLE(7);
    FMT_TABLE(9);
    FMT_TABLE(10);
+   FMT_TABLE(11);
    FMT_TABLE(12);
    FMT_TABLE(13);
    FMT_TABLE(14);
@@ -209,6 +212,7 @@ extern const struct pan_format pan_pipe_format_v6[PIPE_FORMAT_COUNT];
 extern const struct pan_format pan_pipe_format_v7[PIPE_FORMAT_COUNT];
 extern const struct pan_format pan_pipe_format_v9[PIPE_FORMAT_COUNT];
 extern const struct pan_format pan_pipe_format_v10[PIPE_FORMAT_COUNT];
+extern const struct pan_format pan_pipe_format_v11[PIPE_FORMAT_COUNT];
 extern const struct pan_format pan_pipe_format_v12[PIPE_FORMAT_COUNT];
 extern const struct pan_format pan_pipe_format_v13[PIPE_FORMAT_COUNT];
 extern const struct pan_format pan_pipe_format_v14[PIPE_FORMAT_COUNT];
@@ -224,6 +228,7 @@ pan_format_table(unsigned arch)
    FMT_TABLE(7);
    FMT_TABLE(9);
    FMT_TABLE(10);
+   FMT_TABLE(11);
    FMT_TABLE(12);
    FMT_TABLE(13);
    FMT_TABLE(14);

src/panfrost/libpan/libpan.h
@@ -24,6 +24,8 @@
 #include "libpan_v9.h"
 #elif (PAN_ARCH == 10)
 #include "libpan_v10.h"
+#elif (PAN_ARCH == 11)
+#include "libpan_v11.h"
 #elif (PAN_ARCH == 12)
 #include "libpan_v12.h"
 #elif (PAN_ARCH == 13)

src/panfrost/libpan/libpan_shaders.h
@@ -22,6 +22,8 @@
 #include "libpan_shaders_v9.h"
 #elif (PAN_ARCH == 10)
 #include "libpan_shaders_v10.h"
+#elif (PAN_ARCH == 11)
+#include "libpan_shaders_v11.h"
 #elif (PAN_ARCH == 12)
 #include "libpan_shaders_v12.h"
 #elif (PAN_ARCH == 13)

commit c9c207bfd3d389ad74d7ce723d244983c8b5fd0c

panfrost: add the missing v11 cases to the arch switches
pan_mod_get_handler(), pandecode_cs_binary() and pandecode_cs_trace() were
not updated when v11 got wired up, so on a v11 GPU they hit
UNREACHABLE("Unsupported arch") as soon as an image modifier is looked up
or a command stream is dumped with PANVK_DEBUG=dump/trace. The v11
variants they dispatch to are already built.

Fixes: 9ff413aa28e ("panfrost: build v11 support")
Assisted-by: Claude Code (Claude Opus 5.5)
Reviewed-by: Aksel Hjerpbakk <aksel.hjerpbakk@arm.com>
Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44659>

src/panfrost/genxml/decode_common.c
@@ -446,6 +446,9 @@ pandecode_cs_binary(struct pandecode_context *ctx, uint64_t bin_gpu_va,
    case 10:
       pandecode_cs_binary_v10(ctx, bin_gpu_va, size);
       break;
+   case 11:
+      pandecode_cs_binary_v11(ctx, bin_gpu_va, size);
+      break;
    case 12:
       pandecode_cs_binary_v12(ctx, bin_gpu_va, size);
       break;
@@ -471,6 +474,9 @@ pandecode_cs_trace(struct pandecode_context *ctx, uint64_t trace_gpu_va,
    switch (pan_arch(gpu_id)) {
    case 10:
       pandecode_cs_trace_v10(ctx, trace_gpu_va, size, gpu_id);
+      break;
+   case 11:
+      pandecode_cs_trace_v11(ctx, trace_gpu_va, size, gpu_id);
       break;
    case 12:
       pandecode_cs_trace_v12(ctx, trace_gpu_va, size, gpu_id);

src/panfrost/lib/pan_mod.h
@@ -106,6 +106,7 @@ const struct pan_mod_handler *pan_mod_get_handler_v6(uint64_t modifier);
 const struct pan_mod_handler *pan_mod_get_handler_v7(uint64_t modifier);
 const struct pan_mod_handler *pan_mod_get_handler_v9(uint64_t modifier);
 const struct pan_mod_handler *pan_mod_get_handler_v10(uint64_t modifier);
+const struct pan_mod_handler *pan_mod_get_handler_v11(uint64_t modifier);
 const struct pan_mod_handler *pan_mod_get_handler_v12(uint64_t modifier);
 const struct pan_mod_handler *pan_mod_get_handler_v13(uint64_t modifier);
 const struct pan_mod_handler *pan_mod_get_handler_v14(uint64_t modifier);
@@ -126,6 +127,8 @@ pan_mod_get_handler(unsigned arch, uint64_t modifier)
       return pan_mod_get_handler_v9(modifier);
    case 10:
       return pan_mod_get_handler_v10(modifier);
+   case 11:
+      return pan_mod_get_handler_v11(modifier);
    case 12:
       return pan_mod_get_handler_v12(modifier);
    case 13: